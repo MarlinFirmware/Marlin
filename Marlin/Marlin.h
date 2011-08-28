461e267a66bd5eb9bb5edf1b5ac7908de0f6afa6
Bernhard Kubicek <kubicek@gmx.at>
2011-08-28 15:35:47 +0200

    pre-update

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -85,7 +85,7 @@ float analog2tempBed(int raw);
 
 #if E_ENABLE_PIN > -1
 	#define  enable_e() WRITE(E_ENABLE_PIN, E_ENABLE_ON)
-	#define disable_e() WRITE(E_ENABLE_PIN,!E_ENABLE_ON)
+	#define disable_e() WRITE(E_ENABLE_PIN,E_ENABLE_ON)
 #else
 	#define enable_e() ;
 	#define disable_e() ;
