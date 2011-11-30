7a4be21b4f3b28f1730e02c7837b8698251c877a
Bernhard <bkubicek@x201.(none)>
2011-11-30 08:32:28 +0100

    made it compile if arduino decides to include files in a differnt order. DEC HEX and stuff now defined in MarlinSerial.h

--- Marlin/MarlinSerial.h
+++ Marlin/MarlinSerial.h
@@ -24,7 +24,16 @@
 
 #include <inttypes.h>
 #include <Stream.h>
+#include <string.h>
 #define  FORCE_INLINE __attribute__((always_inline)) inline
+
+#include "WString.h"
+
+#define DEC 10
+#define HEX 16
+#define OCT 8
+#define BIN 2
+#define BYTE 0
 
 
 
