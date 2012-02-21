e536198364fecc3563d0eaa379df75617246e18d
Erik van der Zalm <erik@vdzalm.eu>
2012-02-21 20:36:43 +0100

    Small fixes after merge

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -183,13 +183,6 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 #define SDSUPPORT // Enable SD Card Support in Hardware Console
 
 //#define ULTIPANEL
-// If you are using a RAMPS board or cheap E-bay purchased boards that do not detect when an SD card is inserted
-// You can get round this by connecting a push button or single throw switch to the pin defined as SDCARDCARDDETECT 
-// in the pins.h file.  When using a push button pulling the pin to ground this will need inverted.  This setting should
-// be commented out otherwise
-#define SDCARDDETECTINVERTED 
-
-#define ULTIPANEL
 #ifdef ULTIPANEL
   #define NEWPANEL  //enable this if you have a click-encoder panel
   #define SDSUPPORT
