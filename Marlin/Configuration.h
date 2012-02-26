d54cd1eb443514c01520b5517a07de249b32f4bb
Erik van der Zalm <erik@vdzalm.eu>
2012-02-26 22:05:38 +0100

    SDSUPPORT default disabled

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -188,7 +188,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 
 //LCD and SD support
 //#define ULTRA_LCD  //general lcd support, also 16x2
-#define SDSUPPORT // Enable SD Card Support in Hardware Console
+//#define SDSUPPORT // Enable SD Card Support in Hardware Console
 
 //#define ULTIPANEL
 #ifdef ULTIPANEL
