2e749d60c357eb6f3393479dcfe80044d7f30d3b
Erik van der Zalm <erik@vdzalm.eu>
2012-03-12 20:32:14 +0100

    Added DISABLE_MAX_ENDSTOPS

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -952,6 +952,12 @@
   #define _E2_PINS
 #endif
 
+#ifdef DISABLE_MAX_ENDSTOPS
+#define X_MAX_PIN          -1
+#define Y_MAX_PIN          -1
+#define Z_MAX_PIN          -1
+#endif
+
 #define SENSITIVE_PINS {0, 1, X_STEP_PIN, X_DIR_PIN, X_ENABLE_PIN, X_MIN_PIN, X_MAX_PIN, Y_STEP_PIN, Y_DIR_PIN, Y_ENABLE_PIN, Y_MIN_PIN, Y_MAX_PIN, Z_STEP_PIN, Z_DIR_PIN, Z_ENABLE_PIN, Z_MIN_PIN, Z_MAX_PIN, LED_PIN, PS_ON_PIN, \
                         HEATER_BED_PIN, FAN_PIN,                  \
                         _E0_PINS _E1_PINS _E2_PINS             \
