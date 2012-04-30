5b94e5e275d78f9c5e0b8f3d1e75e21a8239b055
Erik van der Zalm <erik@vdzalm.eu>
2012-04-30 13:36:19 +0200

    Added temperature window to M109. This is needed for systems with no overshoot PID.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -58,6 +58,7 @@
 // Actual temperature must be close to target for this long before M109 returns success
 #define TEMP_RESIDENCY_TIME 10	// (seconds)
 #define TEMP_HYSTERESIS 3       // (degC) range of +/- temperatures considered "close" to the target one
+#define TEMP_WINDOW     1       // (degC) Window around target to start the recidency timer x degC early.
 
 // The minimal temperature defines the temperature below which the heater will not be enabled It is used
 // to check that the wiring to the thermistor is not broken. 
