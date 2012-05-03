2987ccdbd6cb4f3c49729c63d2f7cf7d702f5143
MaikStohn <git@stohn.de>
2012-05-03 14:28:17 +0200

    Moved LCD initialization out of constructor
    Since the class "MainMenu" was used within a static variable the
    initialization of the object (constructor call) was done before Arduino
    library startup. It always caused a crash when using AVRStudio with
    JTAG debugger (caused from calling the LCD initialization / the lot of
    I/O work / the stack used during this calls). By moving the LCD_INIT
    out of the constructor and using an explicit call inside of Arduino
    setup() implementation immediately fixed all problems and the JTAG
    debugger runs fine.

--- Marlin/ultralcd.h
+++ Marlin/ultralcd.h
@@ -134,11 +134,12 @@
   char *ftostr3(const float &x);
 
 
-
+  #define LCD_INIT lcd_init();
   #define LCD_MESSAGE(x) lcd_status(x);
   #define LCD_MESSAGEPGM(x) lcd_statuspgm(MYPGM(x));
   #define LCD_STATUS lcd_status()
 #else //no lcd
+  #define LCD_INIT
   #define LCD_STATUS
   #define LCD_MESSAGE(x)
   #define LCD_MESSAGEPGM(x)
