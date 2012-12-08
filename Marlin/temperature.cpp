c5d4f6ff8ea6a076fd54ddf869145b39d0a51956
Michael Moon <triffid.hunter@gmail.com>
2012-12-08 11:20:31 +1100

    unchecked use of lcd_buttons_update, wrap ifdef ULTIPANEL around uses

--- Marlin/temperature.cpp
+++ Marlin/temperature.cpp
@@ -953,7 +953,9 @@ ISR(TIMER0_COMPB_vect)
         ADMUX = ((1 << REFS0) | (TEMP_0_PIN & 0x07));
         ADCSRA |= 1<<ADSC; // Start conversion
       #endif
-      lcd_buttons_update();
+      #ifdef ULTIPANEL
+        lcd_buttons_update();
+      #endif
       temp_state = 1;
       break;
     case 1: // Measure TEMP_0
@@ -975,7 +977,9 @@ ISR(TIMER0_COMPB_vect)
         ADMUX = ((1 << REFS0) | (TEMP_BED_PIN & 0x07));
         ADCSRA |= 1<<ADSC; // Start conversion
       #endif
-      lcd_buttons_update();
+      #ifdef ULTIPANEL
+        lcd_buttons_update();
+      #endif
       temp_state = 3;
       break;
     case 3: // Measure TEMP_BED
@@ -994,7 +998,9 @@ ISR(TIMER0_COMPB_vect)
         ADMUX = ((1 << REFS0) | (TEMP_1_PIN & 0x07));
         ADCSRA |= 1<<ADSC; // Start conversion
       #endif
-      lcd_buttons_update();
+      #ifdef ULTIPANEL
+        lcd_buttons_update();
+      #endif
       temp_state = 5;
       break;
     case 5: // Measure TEMP_1
@@ -1013,7 +1019,9 @@ ISR(TIMER0_COMPB_vect)
         ADMUX = ((1 << REFS0) | (TEMP_2_PIN & 0x07));
         ADCSRA |= 1<<ADSC; // Start conversion
       #endif
-      lcd_buttons_update();
+      #ifdef ULTIPANEL
+        lcd_buttons_update();
+      #endif
       temp_state = 7;
       break;
     case 7: // Measure TEMP_2
