b92c2964955f6637685d0cceac1d3ab0957c252f
Yuri Valentini <yuroller@gmail.com>
2013-01-10 12:23:20 +0100

    Panelolu pin assignment for sanguinololu from http://reprap.org/wiki/Panelolu

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -723,6 +723,39 @@
 #define SDSS               24
 #endif
 
+ #ifdef ULTRA_LCD
+   #ifdef NEWPANEL
+     //we have no buzzer installed
+     #define BEEPER -1
+     //LCD Pins
+     #define LCD_PINS_RS        4
+     #define LCD_PINS_ENABLE    17
+     #define LCD_PINS_D4        30
+     #define LCD_PINS_D5        29
+     #define LCD_PINS_D6        28
+     #define LCD_PINS_D7        27
+     
+     //The encoder and click button
+     #define BTN_EN1 11  //must be a hardware interrupt pin
+     #define BTN_EN2 10 //must be hardware interrupt pin
+     #define BTN_ENC 16  //the switch
+     //not connected to a pin
+     #define SDCARDDETECT -1
+     
+     //from the same bit in the RAMPS Newpanel define
+     //encoder rotation values
+     #define encrot0 0
+     #define encrot1 2
+     #define encrot2 3
+     #define encrot3 1
+     
+     #define BLEN_C 2
+     #define BLEN_B 1
+     #define BLEN_A 0
+     
+   #endif //Newpanel
+ #endif //Ultipanel
+ 
 #endif
 
 
