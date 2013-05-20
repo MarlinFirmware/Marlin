98d0d4819107fe05dde5561efd652d56f676e688
xifle <felix@viki.local>
2013-05-20 14:52:35 +0200

    fixed merge

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -76,7 +76,6 @@ static void menu_action_setting_edit_callback_float51(const char* pstr, float* p
 static void menu_action_setting_edit_callback_float52(const char* pstr, float* ptr, float minValue, float maxValue, menuFunc_t callbackFunc);
 static void menu_action_setting_edit_callback_long5(const char* pstr, unsigned long* ptr, unsigned long minValue, unsigned long maxValue, menuFunc_t callbackFunc);
 
-#define ENCODER_STEPS_PER_MENU_ITEM 5
 #define ENCODER_FEEDRATE_DEADZONE 10
 
 #if !defined(LCD_I2C_VIKI)
