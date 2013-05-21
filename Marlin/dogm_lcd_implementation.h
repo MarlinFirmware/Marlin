bd33cf4a9fec458f98d940bfb572cd893630e5ca
Maik Stohn <marlin@stohn.de>
2013-05-21 17:58:34 +0200

    fixed typo / added "const" to defines for newer gcc compatibility

--- Marlin/dogm_lcd_implementation.h
+++ Marlin/dogm_lcd_implementation.h
@@ -19,7 +19,7 @@
 * Implementation of the LCD display routines for a DOGM128 graphic display. These are common LCD 128x64 pixel graphic displays.
 **/
 
-ifdef ULTIPANEL
+#ifdef ULTIPANEL
 #define BLEN_A 0
 #define BLEN_B 1
 #define BLEN_C 2
