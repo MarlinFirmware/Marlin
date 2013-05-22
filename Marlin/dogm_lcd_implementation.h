d2644e9a6e78407c981ba6ba9280369a68f0e35f
Robert F-C <rfairliecuninghame@yahoo.com>
2013-05-22 21:33:23 +1000

    Revert and extend previous change pin check change
    Reviert previous change of #if BLAH_PIN > 0 to #if defined(BLAH_PIN) &&
    BLAH_PIN > -1. Unfortunately some times pin 0 is used. For my sins I've
    gone through and replaced all unsafe checks of #if BLAH_PIN > -1 with
    the safe version.

--- Marlin/dogm_lcd_implementation.h
+++ Marlin/dogm_lcd_implementation.h
@@ -279,7 +279,7 @@ static void lcd_implementation_status_screen()
  // Fan
  u8g.setFont(FONT_STATUSMENU);
  u8g.setPrintPos(104,27);
- #if FAN_PIN > 0
+ #if defined(FAN_PIN) && FAN_PIN > -1
  u8g.print(itostr3(int((fanSpeed*100)/256 + 1)));
  u8g.print("%");
  #else
