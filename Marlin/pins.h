5345358be2132bde00ed0105fb793f109b0d859c
Johann Rocholl <johann@rocholl.net>
2013-08-07 23:35:09 -0700

    Make Marlin work with Azteeg X3 (RAMPS compatible) on Lindsey's Toshiba.

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -360,19 +360,19 @@
     #define X_STEP_PIN         54
     #define X_DIR_PIN          55
     #define X_ENABLE_PIN       38
-    #define X_MIN_PIN           3
+    #define X_MIN_PIN          -1 // unused for deltabot, was 3
     #define X_MAX_PIN           2
 
     #define Y_STEP_PIN         60
     #define Y_DIR_PIN          61
     #define Y_ENABLE_PIN       56
-    #define Y_MIN_PIN          14
+    #define Y_MIN_PIN          -1 // unused for deltabot, was 14
     #define Y_MAX_PIN          15
 
     #define Z_STEP_PIN         46
     #define Z_DIR_PIN          48
     #define Z_ENABLE_PIN       62
-    #define Z_MIN_PIN          18
+    #define Z_MIN_PIN          18 // autolevel for deltabot
     #define Z_MAX_PIN          19
 
     #define Z2_STEP_PIN        36
