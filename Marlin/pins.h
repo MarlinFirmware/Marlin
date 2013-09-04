23af4462fc1c93e67140798aebe1f6efcfbca036
alexborro <alexborro@gmail.com>
2013-09-04 13:39:05 -0300

    Merging my Prusa I3 configs
    (keeping my configs here make development easier)

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -393,7 +393,7 @@
   #endif
 
   #if MOTHERBOARD == 33 || MOTHERBOARD == 35
-    #define FAN_PIN            9 // (Sprinter config)
+    #define FAN_PIN            -1 // (Sprinter config)
   #else
     #define FAN_PIN            4 // IO pin. Buffer needed
   #endif
