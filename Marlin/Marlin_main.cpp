f525e15299f16d003b192f3b204cfbadc3ebf917
dumle29 <dumle29@gmail.com>
2013-10-29 18:44:56 +0100

    PS_ON configurable boot state
    Allows the user to select wheter or not the PSU should be turned on or
    kept in standby when marlin boots

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -366,7 +366,11 @@ void setup_powerhold()
   #endif
   #if defined(PS_ON_PIN) && PS_ON_PIN > -1
     SET_OUTPUT(PS_ON_PIN);
-    WRITE(PS_ON_PIN, PS_ON_AWAKE);
+	#if defined(PS_DEFAULT_OFF)
+	  WRITE(PS_ON_PIN, PS_ON_ASLEEP);
+    #else
+	  WRITE(PS_ON_PIN, PS_ON_AWAKE);
+	#endif
   #endif
 }
 
