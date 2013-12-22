4c25da3776bc06da5469d7246210aa6d93a911be
Cylindric <mark@hanfordonline.co.uk>
2013-12-22 00:06:57 +0000

    Updated print dimensions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -302,13 +302,13 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
-#define min_software_endstops true // If true, axis won't move to coordinates less than HOME_POS.
+#define min_software_endstops false // If true, axis won't move to coordinates less than HOME_POS.
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 185
+#define X_MAX_POS 180
 #define X_MIN_POS 0
-#define Y_MAX_POS 185
+#define Y_MAX_POS 180
 #define Y_MIN_POS 0
 #define Z_MAX_POS 180
 #define Z_MIN_POS 0
