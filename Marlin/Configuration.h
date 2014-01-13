b58c676d668bea123426ad427f12a8d0c689ad36
Michal Vondracek <info@michalvondracek.cz>
2014-01-14 00:23:37 +0100

    4th definition for thermistor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -124,6 +124,7 @@
 #define TEMP_SENSOR_0 72
 #define TEMP_SENSOR_1 72
 #define TEMP_SENSOR_2 72
+#define TEMP_SENSOR_3 72
 #define TEMP_SENSOR_BED 72
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
