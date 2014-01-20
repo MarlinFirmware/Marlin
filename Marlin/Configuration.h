5933ffbd41b04683551be6da669854dde1e8569b
Cylindric <mark@hanfordonline.co.uk>
2014-01-20 19:16:13 +0000

    Updated max travel distance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -307,9 +307,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 180
+#define X_MAX_POS 200
 #define X_MIN_POS 0
-#define Y_MAX_POS 180
+#define Y_MAX_POS 200
 #define Y_MIN_POS 0
 #define Z_MAX_POS 180
 #define Z_MIN_POS 0
