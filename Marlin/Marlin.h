196e3d4048420bf4ef0d0b715da000fca755fea4
whosawhatsis <whosawhatsis@gmail.com>
2014-01-31 08:43:11 -0800

    Fixed math
    This is why I wanted to sleep on the code I wrote while falling asleep
    rather than immediately submitting a pull request.

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -202,7 +202,7 @@ extern float homing_feedrate[];
 extern bool axis_relative_modes[];
 extern int feedmultiply;
 extern int extrudemultiply; // Sets extrude multiply factor (in percent)
-extern float filament_area[EXTRUDERS]; // cross-sectional area of filament (in cubic millimeters)
+extern float volumetric_multiplier[EXTRUDERS]; // reciprocal of cross-sectional area of filament (in square millimeters), stored this way to reduce computational burden in planner
 extern float current_position[NUM_AXIS] ;
 extern float add_homeing[3];
 #ifdef DELTA
