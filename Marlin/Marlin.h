0b33f42c1a99a09ece6b48f30f8f1dcad49f4301
hg42@gmx.net <hg42@gmx.net>
2014-02-17 23:12:11 +0100

    DELTA_RADIUS: M666: add R code for radius, store it in EEPROM
    --HG--
    extra : source : 9bca92c9aba14841c717c22b0513d60a1171d856

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -209,6 +209,7 @@ extern float current_position[NUM_AXIS] ;
 extern float add_homeing[3];
 #ifdef DELTA
 extern float endstop_adj[3];
+extern float delta_radius;
 #endif
 extern float min_pos[3];
 extern float max_pos[3];
