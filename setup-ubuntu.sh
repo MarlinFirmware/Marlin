3fec47fee93c7c4ceb06022c61119906f7472102
Gabe Rosenhouse <gabe@missionst.com>
2014-02-19 15:10:01 -0800

    Git required for tests to run

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -9,5 +9,5 @@ export ARDUINO_INSTALL_DIR=/usr/share/arduino
 export S3_REGION=us-west-2
 export S3_BUCKET=missionst-blobs
 
-apt-get install -yq make arduino-core curl
+apt-get install -yq make arduino-core curl git
 curl https://s3-$S3_REGION.amazonaws.com/$S3_BUCKET/teensy-cores.tgz | tar -zx -C $ARDUINO_INSTALL_DIR/hardware/
