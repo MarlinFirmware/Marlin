6c10dc1ed72ca4d69238f8efd0eb47d635db5df0
Gabe Rosenhouse <gabe@missionst.com>
2014-02-19 14:09:23 -0800

    tweak z probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -378,7 +378,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER -19
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -23
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -2
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.8
 
   #define Z_RAISE_BEFORE_HOMING 5       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
