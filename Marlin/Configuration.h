d3f305332a37fc830cbfd9d8bb9a09c793b526e0
Gabe Rosenhouse <gabe@missionst.com>
2014-02-24 10:06:12 -0800

    Allowable range now includes default value

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -463,8 +463,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define CUSTOM_M_CODES
 #ifdef CUSTOM_M_CODES
   #define CUSTOM_M_CODE_SET_Z_PROBE_OFFSET 851
-  #define Z_PROBE_OFFSET_RANGE_MIN -2
-  #define Z_PROBE_OFFSET_RANGE_MAX 0
+  #define Z_PROBE_OFFSET_RANGE_MIN -15
+  #define Z_PROBE_OFFSET_RANGE_MAX -5
 #endif
 
 
