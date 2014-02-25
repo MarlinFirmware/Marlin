4dae19a5593b5c4619a976bb2f1b9a1e9762ec94
Chris Trinh <chris@synctv.com>
2014-02-24 23:50:05 -0800

    Disable use of all temp sensors in configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -129,7 +129,7 @@
 // 147 is Pt100 with 4k7 pullup
 // 110 is Pt100 with 1k pullup (non standard)
 
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 0
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_BED 0
