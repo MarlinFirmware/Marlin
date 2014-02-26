00913f7ffa0a706e27bf6edf16be44a9b8b69364
Erik van der Zalm <erik@vdzalm.eu>
2014-02-26 17:29:27 +0100

    Only increase EEPROM version for DELTA printers.

--- Marlin/ConfigurationStore.cpp
+++ Marlin/ConfigurationStore.cpp
@@ -37,7 +37,11 @@ void _EEPROM_readData(int &pos, uint8_t* value, uint8_t size)
 // the default values are used whenever there is a change to the data, to prevent
 // wrong data being written to the variables.
 // ALSO:  always make sure the variables in the Store and retrieve sections are in the same order.
+#ifdef DELTA
 #define EEPROM_VERSION "V11"
+#else
+#define EEPROM_VERSION "V10"
+#endif
 
 #ifdef EEPROM_SETTINGS
 void Config_StoreSettings() 
