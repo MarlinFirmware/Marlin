921c4f1590cdc80791cc396aa90064e663f9a764
paciotti <sergio.fabbrini@gmail.com>
2014-03-08 22:35:05 +0100

    Update Configuration_adv.h
    Increase Z_HOME_RETRACT_MM to 2mm default to avoid end stop not switching during retract.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -215,7 +215,7 @@
 //homing hits the endstop, then retracts by this distance, before it tries to slowly bump again:
 #define X_HOME_RETRACT_MM 5
 #define Y_HOME_RETRACT_MM 5
-#define Z_HOME_RETRACT_MM 1
+#define Z_HOME_RETRACT_MM 2
 //#define QUICK_HOME  //if this is defined, if both x and y are to be homed, a diagonal move will be performed initially.
 
 #define AXIS_RELATIVE_MODES {false, false, false, false}
