f7036253e57f2baee87fefc3143b23942f43994b
Matthew Schick <matthew.schick@gmail.com>
2014-03-15 11:42:53 -0400

    Add feedback from the M207/M208 commands
    Signed-off-by: Matthew Schick <matthew.schick@gmail.com>

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2355,6 +2355,14 @@ void process_commands()
       {
         retract_zlift = code_value() ;
       }
+    SERIAL_PROTOCOL(MSG_OK);
+    SERIAL_PROTOCOL(" S:");
+    SERIAL_PROTOCOL(retract_length);
+    SERIAL_PROTOCOL(" F:");
+    SERIAL_PROTOCOL(retract_feedrate);
+    SERIAL_PROTOCOL(" Z:");
+    SERIAL_PROTOCOL(retract_zlift);
+    SERIAL_PROTOCOLLN("");
     }break;
     case 208: // M208 - set retract recover length S[positive mm surplus to the M207 S*] F[feedrate mm/sec]
     {
@@ -2366,6 +2374,12 @@ void process_commands()
       {
         retract_recover_feedrate = code_value() ;
       }
+    SERIAL_PROTOCOL(MSG_OK);
+    SERIAL_PROTOCOL(" S:");
+    SERIAL_PROTOCOL(retract_recover_length);
+    SERIAL_PROTOCOL(" F:");
+    SERIAL_PROTOCOL(retract_recover_feedrate);
+    SERIAL_PROTOCOLLN("");
     }break;
     case 209: // M209 - S<1=true/0=false> enable automatic retract detect if the slicer did not support G10/11: every normal extrude-only move will be classified as retract depending on the direction.
     {
