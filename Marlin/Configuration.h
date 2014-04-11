f346b9beeaceeda0afc220b96d0f87f97f6a1d2b
spinorkit <kit.adams@gmail.com>
2014-04-11 15:35:06 +1200

    Fixed warnings due to multiple #defines

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -184,9 +184,9 @@
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 // Ultimaker
-    #define  DEFAULT_Kp 22.2
-    #define  DEFAULT_Ki 1.08
-    #define  DEFAULT_Kd 114
+//    #define  DEFAULT_Kp 22.2
+//    #define  DEFAULT_Ki 1.08
+//    #define  DEFAULT_Kd 114
 
 // MakerGear
 //    #define  DEFAULT_Kp 7.0
