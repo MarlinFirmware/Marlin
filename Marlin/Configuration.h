3161740df9b952f627fd9ad1e132e7eaf723fc0e
Nutz95 <hexman@free.fr>
2014-04-13 17:03:20 +0200

    This table is made for thermistor 3950 (can be found on ebay for cheap) it's caracteristics are :
    R25℃ ;100KΩ±1%
    
    B Value(R25/50℃)：3990K 1%
    Dissipation Factor(mW/℃)：1.1~1.6 In still Air
    Thermal Time Constant(S)：10~17 In Still Air
    Operating temperature range：  (-50~+260°C)
    Dimension：  1.8mm Dia X4.1mm
    Lead Length  ：30mm
    
    This table was found on the following blogs:
    http://cae2100.wordpress.com/2014/03/08/beta3950-thermistor-table-for-marlin/
    
    and
    
    http://microfabricator.com/articles/view/id/531ad7e59aad9d3131000000/beta-3950-thermistor-table-for-marlin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -115,6 +115,7 @@
 // 8 is 100k 0603 SMD Vishay NTCS0603E3104FXT (4.7k pullup)
 // 9 is 100k GE Sensing AL03006-58.2K-97-G1 (4.7k pullup)
 // 10 is 100k RS thermistor 198-961 (4.7k pullup)
+// 11 is 100k beta 3950 1% thermistor (4.7k pullup)
 // 20 is the PT100 circuit found in the Ultimainboard V2.x
 // 60 is 100k Maker's Tool Works Kapton Bed Thermistor beta=3950
 //
