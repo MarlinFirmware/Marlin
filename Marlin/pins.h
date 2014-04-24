7a0e805210118cbcccedebad53ef0f0fa0b4de92
AndySze <andy.sze.mail@gmail.com>
2014-04-24 23:00:26 +0800

    fix fan pin defines

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -2586,8 +2586,9 @@
  #define LED_PIN 13
 
  #define FAN_PIN 39
- #define FAN2_PIN 35
- #define CONTROLLERFAN_PIN 35
+ #define FAN1_PIN 35
+ #define FAN2_PIN 36
+ #define CONTROLLERFAN_PIN 36
  #define PS_ON_PIN -1
 
  #define HEATER_0_PIN 29 // EXTRUDER 1
