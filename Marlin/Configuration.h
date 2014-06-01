5b8ec281d59e8507d376eaecbf6b036ad1ef56de
boelle <bo.herrmannsen@gmail.com>
2014-06-01 15:03:35 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -25,7 +25,7 @@
 
 // This determines the communication speed of the printer
 // This determines the communication speed of the printer
-#define BAUDRATE 900600
+#define BAUDRATE 921600
 
 // This enables the serial port associated to the Bluetooth interface
 //#define BTENABLED              // Enable BT interface on AT90USB devices
