3c927901a4dcc4fb4bbfd6bf914a35f8277165db
Dim3nsioneer <Dim3nsioneer@gmx.ch>
2014-06-02 17:02:10 +0200

    Implementation of FW extruder change retract
    FW retraction is extended onto swap retraction invoked by 'G10 S1'.
    Bookkeeping of the retract state of all extruders allows for having one
    extruder fw standard retracted while another extruder is swap retracted.
    An LCD menu item for the swap retract and recover length was added.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -410,9 +410,11 @@ const unsigned int dropsegments=5; //everything with less than this number of st
 #ifdef FWRETRACT
   #define MIN_RETRACT 0.1                //minimum extruded mm to accept a automatic gcode retraction attempt
   #define RETRACT_LENGTH 3               //default retract length (positive mm)
+  #define RETRACT_LENGTH_SWAP 13         //default swap retract length (positive mm), for extruder change
   #define RETRACT_FEEDRATE 45            //default feedrate for retracting (mm/s)
   #define RETRACT_ZLIFT 0                //default retract Z-lift
   #define RETRACT_RECOVER_LENGTH 0       //default additional recover length (mm, added to retract length when recovering)
+  #define RETRACT_RECOVER_LENGTH_SWAP 0  //default additional swap recover length (mm, added to retract length when recovering from extruder change)
   #define RETRACT_RECOVER_FEEDRATE 8     //default feedrate for recovering from retraction (mm/s)
 #endif
 
