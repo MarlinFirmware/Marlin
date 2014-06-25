b153be9294bd961594009cdfb00e03c822e44752
Steve Kelly <kd2cca@gmail.com>
2014-06-25 10:52:38 -0400

    set XY max feedrate to 140

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -444,7 +444,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // default settings
 // Syncromesh (400*16)/(3.81*15)
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {111.99468232964598,111.99468232964598,3200,773.0849338289399}  // default steps per unit for Ultimaker
-#define DEFAULT_MAX_FEEDRATE          {600, 600, 1.5, 22}    // (mm/sec)
+#define DEFAULT_MAX_FEEDRATE          {140, 140, 1.5, 22}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {3000,3000,50,1000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          1200    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
