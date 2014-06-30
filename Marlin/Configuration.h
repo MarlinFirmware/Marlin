da671cddab2d4a5f57bfab3d3529903ce0f0c4f8
alexborro <alexborro@gmail.com>
2014-06-30 15:22:37 -0300

    Add "Thermal Runaway Protection" feature
    This is a feature to protect your printer from burn up in flames if it
    has a thermistor coming off place (this happened to a friend of mine
    recently and motivated me writing this feature).
    
    The issue: If a thermistor come off, it will read a lower temperature
    than actual. The system will turn the heater on forever, burning up the
    filament and anything
    else around.
    
    After the temperature reaches the target for the first time, this
    feature will start measuring for how long the current temperature stays
    below the target minus _HYSTERESIS (set_temperature -
    THERMAL_RUNAWAY_PROTECTION_HYSTERESIS).
    
    If it stays longer than _PERIOD, it means the thermistor temperature
    cannot catch up with the target, so something *may be* wrong. Then, to
    be on the safe side, the system will he halt.
    
    Bear in mind the count down will just start AFTER the first time the
    thermistor temperature is over the target, so you will have no problem
    if your extruder heater takes 2 minutes to hit the target on heating.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -251,6 +251,44 @@
 #define EXTRUDE_MINTEMP 170
 #define EXTRUDE_MAXLENGTH (X_MAX_LENGTH+Y_MAX_LENGTH) //prevent extrusion of very large distances.
 
+/*================== Thermal Runaway Protection ==============================
+This is a feature to protect your printer from burn up in flames if it has
+a thermistor coming off place (this happened to a friend of mine recently and
+motivated me writing this feature).
+
+The issue: If a thermistor come off, it will read a lower temperature than actual.
+The system will turn the heater on forever, burning up the filament and anything
+else around.
+
+After the temperature reaches the target for the first time, this feature will 
+start measuring for how long the current temperature stays below the target 
+minus _HYSTERESIS (set_temperature - THERMAL_RUNAWAY_PROTECTION_HYSTERESIS).
+
+If it stays longer than _PERIOD, it means the thermistor temperature
+cannot catch up with the target, so something *may be* wrong. Then, to be on the
+safe side, the system will he halt.
+
+Bear in mind the count down will just start AFTER the first time the 
+thermistor temperature is over the target, so you will have no problem if
+your extruder heater takes 2 minutes to hit the target on heating.
+
+*/
+// If you want to enable this feature for all your extruder heaters,
+// uncomment the 2 defines below:
+
+// Parameters for all extruder heaters
+//#define THERMAL_RUNAWAY_PROTECTION_PERIOD 40 //in seconds
+//#define THERMAL_RUNAWAY_PROTECTION_HYSTERESIS 4 // in degree Celsius
+
+// If you want to enable this feature for your bed heater,
+// uncomment the 2 defines below:
+
+// Parameters for the bed heater
+//#define THERMAL_RUNAWAY_PROTECTION_BED_PERIOD 20 //in seconds
+//#define THERMAL_RUNAWAY_PROTECTION_BED_HYSTERESIS 2 // in degree Celsius
+//===========================================================================
+
+
 //===========================================================================
 //=============================Mechanical Settings===========================
 //===========================================================================
