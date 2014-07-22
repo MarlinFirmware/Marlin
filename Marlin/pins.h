4b78238dc91f1e7a7ed1ce1c998496523ab03821
Jack Minardi <jack@minardi.org>
2014-07-22 17:04:13 -0400

    add pin number to pins.h

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -2136,6 +2136,7 @@
 #define PS_ON_PIN          4
 #define KILL_PIN           -1 //80 with Smart Controller LCD
 #define SUICIDE_PIN        -1  //PIN that has to be turned on right after start, to keep power flowing.
+#define RESUME_PIN         18
 
 #ifdef ULTRA_LCD
   #define KILL_PIN 80
