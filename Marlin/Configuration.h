e35251aad09dedd33c5fbc0b857a6ed98b143894
Roxy-3DPrintBoard <Roxanne.Rae.Neufeld@gmail.com>
2014-08-10 10:18:54 -0500

    Fix Default Configuration so Auto Bed Leveling is off
    This snapshot has the Auto Bed Leveling turned off because most Marlin
    users do not have that feature.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -375,8 +375,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define Z_MAX_LENGTH (Z_MAX_POS - Z_MIN_POS)
 //============================= Bed Auto Leveling ===========================
 
-#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
-#define Z_PROBE_REPEATABILITY_TEST  // Delete the comment to enable (remove // at the start of the line)
+//#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
+#define Z_PROBE_REPEATABILITY_TEST  // If not commented out, Z-Probe Repeatability test will be included if Auto Bed Leveling is Enabled.
 
 #ifdef ENABLE_AUTO_BED_LEVELING
 
