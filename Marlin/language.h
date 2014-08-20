d78d1d82d8b0944629df283f908e9029bb53f3dd
fmalpartida <fmalpartida@gmail.com>
2014-08-21 00:44:23 +0200

    Clening up

--- Marlin/language.h
+++ Marlin/language.h
@@ -22,7 +22,7 @@
 // 13 Basque-Euskera
 
 #ifndef LANGUAGE_CHOICE
-	#define LANGUAGE_CHOICE 1  // Pick your language from the list above
+	#define LANGUAGE_CHOICE 5  // Pick your language from the list above
 #endif
 
 #define PROTOCOL_VERSION "1.0"
