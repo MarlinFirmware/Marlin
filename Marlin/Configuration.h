1c314dd968ad1eea374766c9befe7cf0310ae1ed
Jack Minardi <jack@minardi.org>
2014-10-21 14:08:03 -0400

    adjust X-max for new part and change input current

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -331,7 +331,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 160
+#define X_MAX_POS 120
 #define X_MIN_POS 0
 #define Y_MAX_POS 60
 #define Y_MIN_POS 0
