d84934d8c5bdb7f8298760ce487da4c9c33bc438
Filip Mulier <filipmulier@q.com>
2014-11-10 21:43:58 -0600

    Display Filament Sensor data on 20x4 LCD
    Changes to support displaying the real-time filament width and the
    volume factor on a 20x4 LCD.  The data is displayed on the 4th line.
    First the status message is displayed for 5 seconds, and then the
    filament data is displayed.  The status message can be seen by
    re-selecting the info screen in the menu.

--- Marlin/ultralcd.h
+++ Marlin/ultralcd.h
@@ -44,6 +44,10 @@
   extern int absPreheatFanSpeed;
   
   extern bool cancel_heatup;
+  
+  #ifdef FILAMENT_LCD_DISPLAY
+        extern unsigned long message_millis;
+  #endif
     
   void lcd_buzz(long duration,uint16_t freq);
   bool lcd_clicked();
@@ -109,6 +113,7 @@ char *ftostr3(const float &x);
 char *ftostr31ns(const float &x); // float to string without sign character
 char *ftostr31(const float &x);
 char *ftostr32(const float &x);
+char *ftostr12ns(const float &x); 
 char *ftostr5(const float &x);
 char *ftostr51(const float &x);
 char *ftostr52(const float &x);
