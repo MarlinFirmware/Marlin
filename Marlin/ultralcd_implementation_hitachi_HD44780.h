4c2acbc660ecf0f58a1ae4e79599cdb24d540586
Scott Lahteine <sourcetree@thinkyhead.com>
2014-12-13 07:00:16 -0800

    Group progress-bar bitmaps together and loop.

--- Marlin/ultralcd_implementation_hitachi_HD44780.h
+++ Marlin/ultralcd_implementation_hitachi_HD44780.h
@@ -278,7 +278,8 @@ static void lcd_implementation_init()
 
 // Characters for a progress bar
 #if defined(LCD_PROGRESS_BAR) && defined(SDSUPPORT)
-    byte progress1[8] = {
+    byte progress[3][8] = {
+      {
         B00000,
         B10000,
         B10000,
@@ -287,8 +288,8 @@ static void lcd_implementation_init()
         B10000,
         B10000,
         B00000
-    };
-    byte progress2[8] = {
+      },
+      {
         B00000,
         B10100,
         B10100,
@@ -297,8 +298,8 @@ static void lcd_implementation_init()
         B10100,
         B10100,
         B00000
-    };
-    byte progress3[8] = {
+      },
+      {
         B00000,
         B10101,
         B10101,
@@ -307,6 +308,7 @@ static void lcd_implementation_init()
         B10101,
         B10101,
         B00000
+      }
     };
 #else
     byte degree[8] =
@@ -367,9 +369,7 @@ static void lcd_implementation_init()
 #endif
 
 #if defined(LCD_PROGRESS_BAR) && defined(SDSUPPORT)
-    lcd.createChar(LCD_STR_PROGRESS[0], progress1);
-    lcd.createChar(LCD_STR_PROGRESS[1], progress2);
-    lcd.createChar(LCD_STR_PROGRESS[2], progress3);
+    for (int i=3; i--;) lcd.createChar(LCD_STR_PROGRESS[i], progress[i]);
 #else
     lcd.createChar(LCD_STR_DEGREE[0], degree);
     lcd.createChar(LCD_STR_UPLEVEL[0], uplevel);
