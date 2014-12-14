0b63f9ae5d46a39ab7b0d3643b498239fe01fe37
Scott Lahteine <sourcetree@thinkyhead.com>
2014-12-13 20:08:42 -0800

    More accurate doc, other Configuration_adv.h

--- Marlin/example_configurations/makibox/Configuration_adv.h
+++ Marlin/example_configurations/makibox/Configuration_adv.h
@@ -296,6 +296,16 @@
 // using:
 //#define MENU_ADDAUTOSTART
 
+// Show a progress bar on the LCD when printing from SD
+//  This needs 3 custom characters, so refresh ∫ and degree ° are
+//  replaced by HD44780 Japanese extended character set glyphs,
+//  while uplevel is replaced by caret ^
+//#define LCD_PROGRESS_BAR
+
+#ifdef LCD_PROGRESS_BAR
+    #define PROGRESS_BAR_INTERVAL 2000
+#endif
+
 // The hardware watchdog should reset the microcontroller disabling all outputs, in case the firmware gets stuck and doesn't do temperature regulation.
 //#define USE_WATCHDOG
 
