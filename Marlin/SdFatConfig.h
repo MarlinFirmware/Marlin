60598b4cdda4132ad642ba42e60777e04c494f5a
Scott Lahteine <sourcetree@thinkyhead.com>
2014-12-17 20:22:33 -0800

    A few constants where they belong

--- Marlin/SdFatConfig.h
+++ Marlin/SdFatConfig.h
@@ -115,6 +115,8 @@ uint8_t const SOFT_SPI_SCK_PIN = 13;
 #define FILENAME_LENGTH 13
 /** Number of VFAT entries used. Every entry has 13 UTF-16 characters */
 #define MAX_VFAT_ENTRIES (2)
+/** Number of UTF-16 characters per entry */
+#define FILENAME_LENGTH 13
 /** Total size of the buffer used to store the long filenames */
 #define LONG_FILENAME_LENGTH (FILENAME_LENGTH*MAX_VFAT_ENTRIES+1)
 #endif  // SdFatConfig_h
