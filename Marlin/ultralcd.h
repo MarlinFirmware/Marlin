edac64e7d2a54a201e6d819263b8b2b2d317800a
wgm4321 <wgm4321@live.com>
2014-12-28 20:43:14 -0500

    Add Filament menu and add Filament/Retract settings to config store.

--- Marlin/ultralcd.h
+++ Marlin/ultralcd.h
@@ -118,6 +118,7 @@ char *ftostr3(const float &x);
 char *ftostr31ns(const float &x); // float to string without sign character
 char *ftostr31(const float &x);
 char *ftostr32(const float &x);
+char *ftostr43(const float &x);
 char *ftostr12ns(const float &x); 
 char *ftostr32sp(const float &x); // remove zero-padding from ftostr32
 char *ftostr5(const float &x);
