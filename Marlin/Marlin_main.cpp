be6f7f81add157e2dc7ae0ea29d6e52458dc53fb
Wurstnase <tonnhofer@gmail.com>
2014-12-29 16:31:00 +0100

    change filament issue
    make a more general solution

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -204,6 +204,7 @@ CardReader card;
 #endif
 float homing_feedrate[] = HOMING_FEEDRATE;
 bool axis_relative_modes[] = AXIS_RELATIVE_MODES;
+bool ignore_blocks_queued = false;
 int feedmultiply=100; //100->1 200->2
 int saved_feedmultiply;
 int extrudemultiply=100; //100->1 200->2
@@ -3636,7 +3637,9 @@ case 404:  //M404 Enter the nominal filament width (3mm, 1.75mm ) N<3.0> or disp
         while(!lcd_clicked()){
           cnt++;
           manage_heater();
+		  ignore_blocks_queued = true;
           manage_inactivity();
+		  ignore_blocks_queued = false;
           lcd_update();
           if(cnt==0)
           {
@@ -4355,7 +4358,7 @@ void manage_inactivity()
   if(stepper_inactive_time)  {
     if( (millis() - previous_millis_cmd) >  stepper_inactive_time )
     {
-      if(blocks_queued() == false) {
+      if(blocks_queued() == false && ignore_blocks_queued != true) {
         disable_x();
         disable_y();
         disable_z();
