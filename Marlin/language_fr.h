210240fc61ce6d8b3b87bb63f0368baba211a629
Caleb Anderson <robotrising@gmail.com>
2014-12-29 10:13:00 -0700

    Delta calibration menu based on http://minow.blogspot.com/index.html#4918805519571907051 To test/enable, uncomment the #define DELTA_CALIBRATION_MENU in Configuration.h

--- Marlin/language_fr.h
+++ Marlin/language_fr.h
@@ -128,4 +128,12 @@
 #define MSG_RECTRACT                        "Rectract"
 #define MSG_STEPPER_RELEASED                "RELACHE."
 
+#ifdef DELTA_CALIBRATION_MENU
+    #define MSG_DELTA_CALIBRATE             "Delta Calibration"
+    #define MSG_DELTA_CALIBRATE_X           "Calibrate X"
+    #define MSG_DELTA_CALIBRATE_Y           "Calibrate Y"
+    #define MSG_DELTA_CALIBRATE_Z           "Calibrate Z"
+    #define MSG_DELTA_CALIBRATE_CENTER      "Calibrate Center"
+#endif // DELTA_CALIBRATION_MENU
+
 #endif // LANGUAGE_FR_H
