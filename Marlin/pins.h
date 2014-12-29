5be0ad5937d26ccbaf5e8cc589d7671735ef67d6
Bernhard Kubicek <kubicek@gmx.at>
2014-12-29 17:27:14 +0100

    Addes servo0 pin ultimaker

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -1400,6 +1400,7 @@
 #define PS_ON_PIN          12
 #define KILL_PIN           -1
 #define SUICIDE_PIN        54  //PIN that has to be turned on right after start, to keep power flowing.
+#define SERVO0_PIN         13  // untested
 
 #ifdef ULTRA_LCD
 
