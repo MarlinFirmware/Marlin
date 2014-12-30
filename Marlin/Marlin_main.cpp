6314ecb82d018156e27eb8f0492db0f4f4d7b363
bkubicek <kubicektgmx.at>
2014-12-30 08:04:06 +0100

    fixed compilation

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -4332,7 +4332,7 @@ void handle_status_leds(void) {
 }
 #endif
 
-void manage_inactivity(bool ignore_stepper_queue=false)
+void manage_inactivity(bool ignore_stepper_queue/*=false*/) //default argument set in Marlin.h
 {
 	
 #if defined(KILL_PIN) && KILL_PIN > -1
