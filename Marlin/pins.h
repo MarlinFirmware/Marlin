71f97a0f1e0ede7cd4fa18424780eb3ccbff89b0
Scott Lahteine <sourcetree@thinkyhead.com>
2015-01-23 10:37:39 -0800

    Borrow from Ultimaker to fix MAX6675 SPI conflict
    Changes to temperature.cpp from Ultimaker fork, intended to address
    #1226 and #1227

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -904,7 +904,7 @@
   #define SCK_PIN          52
   #define MISO_PIN         50
   #define MOSI_PIN         51
-  #define MAX6675_SS       66// Do not use pin 53 if there is even the remote possibility of using Dsplay/SD card
+  #define MAX6675_SS       66// Do not use pin 53 if there is even the remote possibility of using Display/SD card
 #else
   #define MAX6675_SS       66// Do not use pin 49 as this is tied to the switch inside the SD card socket to detect if there is an SD card present
 #endif
