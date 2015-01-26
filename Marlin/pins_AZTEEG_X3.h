574a5ab72afc5e566f052bdd16eda88a7b94eb9c
Scott Lahteine <sourcetree@thinkyhead.com>
2015-01-25 18:40:00 -0800

    Split up pins to make it more manageable
    Hopefully this is helpful to organization and future expansion….

---
+++ Marlin/pins_AZTEEG_X3.h
@@ -0,0 +1,15 @@
+/**
+ * Arduino Mega pin assignments
+ *
+ * RAMPS_OLD 
+ * RAMPS_13_EFB 
+ * RAMPS_13_EEB 
+ * RAMPS_13_EFF 
+ * RAMPS_13_EEF
+ * 3DRAG
+ * K8200
+ * AZTEEG_X3
+ * AZTEEG_X3_PRO
+ */
+
+#include "pins_RAMPS_13.h"
