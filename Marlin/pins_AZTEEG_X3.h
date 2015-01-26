aa23e75eb84ab4d83f6d8a9584683c1997e5b886
Scott Lahteine <sourcetree@thinkyhead.com>
2015-01-25 18:42:16 -0800

    Cleanup heading

--- Marlin/pins_AZTEEG_X3.h
+++ Marlin/pins_AZTEEG_X3.h
@@ -1,15 +1,5 @@
 /**
- * Arduino Mega pin assignments
- *
- * RAMPS_OLD 
- * RAMPS_13_EFB 
- * RAMPS_13_EEB 
- * RAMPS_13_EFF 
- * RAMPS_13_EEF
- * 3DRAG
- * K8200
- * AZTEEG_X3
- * AZTEEG_X3_PRO
+ * AZTEEG_X3 Arduino Mega with RAMPS v1.3 pin assignments
  */
 
 #include "pins_RAMPS_13.h"
