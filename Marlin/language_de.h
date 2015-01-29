c0a24a5264c6a140dd469f0e64274d5dd526e496
AnHardt <github@kitelab.de>
2015-01-29 13:12:55 +0100

    MSG_ENDSTOP_ABORT = 'Endstop abort' > 'Endstop Abbr. Ein'

--- Marlin/language_de.h
+++ Marlin/language_de.h
@@ -116,7 +116,7 @@
 #define MSG_BABYSTEP_X                      "Babystep X"
 #define MSG_BABYSTEP_Y                      "Babystep Y"
 #define MSG_BABYSTEP_Z                      "Babystep Z"
-#define MSG_ENDSTOP_ABORT                   "Endstop abort"
+#define MSG_ENDSTOP_ABORT                   "Endstop Abbr. Ein"
 
 #ifdef DELTA_CALIBRATION_MENU
     #define MSG_DELTA_CALIBRATE             "Delta Calibration"
