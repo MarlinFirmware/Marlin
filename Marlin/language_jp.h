88f1166a492e6d48d259ab42b921118e11a3ba6f
Scott Lahteine <sourcetree@thinkyhead.com>
2015-02-19 18:35:47 -0800

    Make sure user specifies Japanese HD44780

--- Marlin/language_jp.h
+++ Marlin/language_jp.h
@@ -12,6 +12,9 @@
 #define KANA
 
 #ifndef REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+  #ifndef DISPLAY_CHARSET_HD44780_JAPAN
+    #error "Kana requires DISPLAY_CHARSET_HD44780_JAPAN to be set in Configuration.h"
+  #endif
   #include "language_jp_1.h" // HD44780
 #else
   #include "language_jp_1.h" // DOGM
