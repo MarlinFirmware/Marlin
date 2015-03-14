1d25d510bcdb0d162ac983f8befd0d6c2649079e
chrono <chrono@open-resource.org>
2015-03-14 09:48:30 +0000

    Upstream Merge Fixes

--- Marlin/example_configurations/Felix/Configuration_DUAL.h
+++ Marlin/example_configurations/Felix/Configuration_DUAL.h
@@ -263,15 +263,15 @@ your extruder heater takes 2 minutes to hit the target on heating.
 // uncomment the 2 defines below:
 
 // Parameters for all extruder heaters
-#define THERMAL_RUNAWAY_PROTECTION_PERIOD 60 //in seconds
-#define THERMAL_RUNAWAY_PROTECTION_HYSTERESIS 5 // in degree Celsius
+//#define THERMAL_RUNAWAY_PROTECTION_PERIOD 60 //in seconds
+//#define THERMAL_RUNAWAY_PROTECTION_HYSTERESIS 5 // in degree Celsius
 
 // If you want to enable this feature for your bed heater,
 // uncomment the 2 defines below:
 
 // Parameters for the bed heater
-#define THERMAL_RUNAWAY_PROTECTION_BED_PERIOD 30 //in seconds
-#define THERMAL_RUNAWAY_PROTECTION_BED_HYSTERESIS 5// in degree Celsius
+//#define THERMAL_RUNAWAY_PROTECTION_BED_PERIOD 30 //in seconds
+//#define THERMAL_RUNAWAY_PROTECTION_BED_HYSTERESIS 5// in degree Celsius
 
 
 //===========================================================================
