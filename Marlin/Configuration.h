78090275a9999f8fd4a335576891f7087a2c304b
Scott Lahteine <sourcetree@thinkyhead.com>
2015-03-26 13:52:48 -0700

    Fix probe range editing
    - Increase the probe offset range to -20…20
    - Apply the range limits to the menu item

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -534,8 +534,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #ifdef CUSTOM_M_CODES
   #ifdef ENABLE_AUTO_BED_LEVELING
     #define CUSTOM_M_CODE_SET_Z_PROBE_OFFSET 851
-    #define Z_PROBE_OFFSET_RANGE_MIN -15
-    #define Z_PROBE_OFFSET_RANGE_MAX -5
+    #define Z_PROBE_OFFSET_RANGE_MIN -20
+    #define Z_PROBE_OFFSET_RANGE_MAX 20
   #endif
 #endif
 
