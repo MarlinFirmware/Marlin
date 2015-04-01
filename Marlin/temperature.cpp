488666832bb83f2ab3764a0fda8654bd2e8e7a71
AnHardt <github@kitelab.de>
2015-04-01 13:40:05 +0200

    Remove the previously introduced ()

--- Marlin/temperature.cpp
+++ Marlin/temperature.cpp
@@ -1539,8 +1539,8 @@ ISR(TIMER0_COMPB_vect) {
       #else
         #define GE0 >=
       #endif
-      if ((current_temperature_raw[0]) GE0 (maxttemp_raw[0])) max_temp_error(0);
-      if ((minttemp_raw[0]) GE0 (current_temperature_raw[0])) min_temp_error(0);
+      if (current_temperature_raw[0] GE0 maxttemp_raw[0]) max_temp_error(0);
+      if (minttemp_raw[0] GE0 current_temperature_raw[0]) min_temp_error(0);
     #endif
 
     #if HAS_TEMP_1
@@ -1549,8 +1549,8 @@ ISR(TIMER0_COMPB_vect) {
       #else
         #define GE1 >=
       #endif
-      if ((current_temperature_raw[1]) GE1 (maxttemp_raw[1])) max_temp_error(1);
-      if ((minttemp_raw[1]) GE1 (current_temperature_raw[1])) min_temp_error(1);
+      if (current_temperature_raw[1] GE1 maxttemp_raw[1]) max_temp_error(1);
+      if (minttemp_raw[1] GE1 current_temperature_raw[1]) min_temp_error(1);
     #endif // TEMP_SENSOR_1
 
     #if HAS_TEMP_2
@@ -1559,8 +1559,8 @@ ISR(TIMER0_COMPB_vect) {
       #else
         #define GE2 >=
       #endif
-      if ((current_temperature_raw[2]) GE2 (maxttemp_raw[2])) max_temp_error(2);
-      if ((minttemp_raw[2]) GE2 (current_temperature_raw[2])) min_temp_error(2);
+      if (current_temperature_raw[2] GE2 (maxttemp_raw[2]) max_temp_error(2);
+      if (minttemp_raw[2] GE2 current_temperature_raw[2]) min_temp_error(2);
     #endif // TEMP_SENSOR_2
 
     #if HAS_TEMP_3
@@ -1569,8 +1569,8 @@ ISR(TIMER0_COMPB_vect) {
       #else
         #define GE3 >=
       #endif
-      if ((current_temperature_raw[3]) GE3 (maxttemp_raw[3])) max_temp_error(3);
-      if ((minttemp_raw[3]) GE3 (current_temperature_raw[3])) min_temp_error(3);
+      if (current_temperature_raw[3] GE3 maxttemp_raw[3]) max_temp_error(3);
+      if (minttemp_raw[3] GE3 current_temperature_raw[3]) min_temp_error(3);
     #endif // TEMP_SENSOR_3
 
     #if HAS_TEMP_BED
