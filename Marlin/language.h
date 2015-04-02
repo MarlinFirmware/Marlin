916f59e35f1408a65b68267d0b2ec9d62ccb31b4
Chris Roadfeldt <chris@roadfeldt.com>
2015-04-01 19:22:05 -0500

    Spaces not tabs in language.h. Catch unlikely but possible error and head crash when using Z_PROBE_REPEATABILITY_TEST

--- Marlin/language.h
+++ Marlin/language.h
@@ -138,7 +138,7 @@
 #define MSG_Z_MIN                           "z_min: "
 #define MSG_Z_MAX                           "z_max: "
 #define MSG_Z2_MAX                          "z2_max: "
-#define MSG_Z_PROBE							"z_probe: "
+#define MSG_Z_PROBE                         "z_probe: "
 #define MSG_M119_REPORT                     "Reporting endstop status"
 #define MSG_ENDSTOP_HIT                     "TRIGGERED"
 #define MSG_ENDSTOP_OPEN                    "open"
