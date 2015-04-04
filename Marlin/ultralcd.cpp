c32ab0d0d8741f637ac8cb3dc424e48ffa462f20
AnHardt <github@kitelab.de>
2015-04-04 18:33:36 +0200

    Revert "Testloop cleanup"
    This reverts commit 6361404b91dcf8ba657b1f1e16d9717a8e280ae6.

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -1299,6 +1299,8 @@ void lcd_update() {
     }
   #endif//CARDINSERTED
 
+  static uint8_t dotcounter = 63;
+  
   uint32_t ms = millis();
   if (ms > lcd_next_update_millis) {
 
@@ -1364,11 +1366,12 @@ void lcd_update() {
       (*currentMenu)();
       if (lcdDrawUpdate) {
         do {
+          if (!dotcounter) dotcounter = 63;
           lcd_setFont(FONT_MENU);
           u8g.setPrintPos(125, 0);
-          if (blink % 2) u8g.setColorIndex(1); else u8g.setColorIndex(0); // Set color for the alive dot
-          u8g.drawPixel(127, 63); // draw alive dot
-          u8g.setColorIndex(1); // black on white
+  //        if (blink % 2) u8g.setColorIndex(1); else u8g.setColorIndex(0); // Set color for the alive dot
+          u8g.drawPixel(127, dotcounter--); // draw alive dot
+  //        u8g.setColorIndex(1); // black on white
           (*currentMenu)();
         } while( u8g.nextPage() );
       }
