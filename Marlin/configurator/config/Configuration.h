0c9c6220cae84083c19fd0e0d179d6d0aeec9177
Scott Lahteine <sourcetree@thinkyhead.com>
2015-04-07 20:38:43 -0700

    Set version to "1.0.3 dev"

--- Marlin/configurator/config/Configuration.h
+++ Marlin/configurator/config/Configuration.h
@@ -40,7 +40,7 @@ Here are some standard links for getting your machine calibrated:
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_VERSION "1.0.2"
+#define STRING_VERSION "1.0.3 dev"
 #define STRING_URL "reprap.org"
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
 #define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
