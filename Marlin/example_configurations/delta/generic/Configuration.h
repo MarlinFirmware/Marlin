585eee6549a879fefee4cc1adb4cc200536f57bd
Natealus <natealus@hotmail.com>
2015-04-08 18:57:30 -0600

    Z Probe inverting not present in example configs, causes error
    The line const bool Z_PROBE_ENDSTOP_INVERTING = false; // set to true to
    invert the logic of the endstop is in the main configuration.h but
    wasn't present in the example configuration.h's. Causes a compiling
    error with Z Probe enabled. Just added it to all the configs. :)

--- Marlin/example_configurations/delta/generic/Configuration.h
+++ Marlin/example_configurations/delta/generic/Configuration.h
@@ -1,4 +1,4 @@
-#ifndef CONFIGURATION_H
+﻿#ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
 #include "boards.h"
@@ -355,6 +355,7 @@ const bool Z_MIN_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 const bool X_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
 const bool Y_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
 const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
+const bool Z_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
 //#define DISABLE_MAX_ENDSTOPS
 #define DISABLE_MIN_ENDSTOPS // Deltas only use min endstops for probing
 
