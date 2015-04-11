9113e788413fef362532bd6aba7cecb51a43b7e7
Scott Lahteine <sourcetree@thinkyhead.com>
2015-04-11 01:36:24 -0700

    machine name for hephestos too

--- Marlin/example_configurations/Hephestos/Configuration.h
+++ Marlin/example_configurations/Hephestos/Configuration.h
@@ -64,7 +64,8 @@ Here are some standard links for getting your machine calibrated:
 
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
-
+// #define CUSTOM_MACHINE_NAME "This RepRap"
+ 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
 // #define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
