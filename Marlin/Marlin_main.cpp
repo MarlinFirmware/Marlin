1d466b4eec1f32263fac5a3c6c2c37b5ec8f463f
Scott Lahteine <sourcetree@thinkyhead.com>
2015-04-27 20:55:19 -0700

    Fix homing Z position
    Add `sync_plan_position()` after `axis_is_at_home(axis)` to keep the
    planner position in sync when homing.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -1596,6 +1596,7 @@ static void homeaxis(AxisEnum axis) {
 
     // Set the axis position to its home position (plus home offsets)
     axis_is_at_home(axis);
+    sync_plan_position();
 
     destination[axis] = current_position[axis];
     feedrate = 0.0;
