0758a11f2567abc259be7dce54b07dae726c7ec6
Victor Andueza <victor.andueza@bq.com>
2015-04-28 17:06:50 +0200

    Added Icon class

---
+++ Marlin/Icon.cpp
@@ -0,0 +1,9 @@
+
+namespace screen
+{
+	Icon :: Icon (uint8_t width, uint8_t height, const unsigned char* bitmap)
+		: m_width(width)
+		, m_height(height)
+		, m_bitmap(bitmap)
+		{ }
+}
---
+++ Marlin/Icon.h
@@ -0,0 +1,19 @@
+#ifndef ICON_H
+#define ICON_H
+
+
+namespace screen
+{
+	class Icon 
+	{
+		public:
+			Icon(uint8_t width, uint8_t height, const unsigned char* bitmap);
+
+		protected:
+			uint8_t m_width;
+			uint8_t m_height;
+			const unsigned char* m_bitmap;
+
+	};
+}
+#endif
