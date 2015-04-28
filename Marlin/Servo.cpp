e44965933885da6f598436259e838e3576c7eeae
Bernardo <pbccmm@gmail.com>
2015-04-27 23:30:11 -0300

    Update Servo.cpp
    Updated "servo.h" to "Servo.h"

--- Marlin/Servo.cpp
+++ Marlin/Servo.cpp
@@ -48,7 +48,7 @@
 #include <avr/interrupt.h>
 #include <Arduino.h>
 
-#include "servo.h"
+#include "Servo.h"
 
 #define usToTicks(_us)    (( clockCyclesPerMicrosecond()* _us) / 8)     // converts microseconds to tick (assumes prescale of 8)  // 12 Aug 2009
 #define ticksToUs(_ticks) (( (unsigned)_ticks * 8)/ clockCyclesPerMicrosecond() ) // converts from ticks back to microseconds
