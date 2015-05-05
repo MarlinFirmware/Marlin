4a7b86d5e52aae0de98301deb9ad05dc10de6382
brupje <brupje@gmail.com>
2015-05-05 11:03:20 +0200

    Make Marlin actually compile when #define REPRAPWORLD_KEYPAD is uncommented

--- Marlin/ultralcd_implementation_hitachi_HD44780.h
+++ Marlin/ultralcd_implementation_hitachi_HD44780.h
@@ -6,11 +6,11 @@
 * When selecting the Russian language, a slightly different LCD implementation is used to handle UTF8 characters.
 **/
 
-#ifndef REPRAPWORLD_KEYPAD
-  extern volatile uint8_t buttons;  //the last checked buttons in a bit array.
-#else
-  extern volatile uint16_t buttons;  //an extended version of the last checked buttons in a bit array.
-#endif
+//#ifndef REPRAPWORLD_KEYPAD
+//  extern volatile uint8_t buttons;  //the last checked buttons in a bit array.
+//#else
+  extern volatile uint8_t buttons;  //an extended version of the last checked buttons in a bit array.
+//#endif
 
 ////////////////////////////////////
 // Setup button and encode mappings for each panel (into 'buttons' variable
