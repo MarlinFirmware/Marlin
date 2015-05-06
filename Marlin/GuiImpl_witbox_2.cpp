ccaef22ddeab2bca26852cd0f8da45973d26a543
Ivan Galvez Junquera <ivan.galvez@bq.com>
2015-05-06 10:02:21 +0200

    More instantiations of Screens and Icons.

--- Marlin/GuiImpl_witbox_2.cpp
+++ Marlin/GuiImpl_witbox_2.cpp
@@ -11,34 +11,34 @@ namespace screen
 	///////////////////////
 	// Instantiate Icons //
 	///////////////////////
-	Icon icon_cold_hover = Icon(icon_width, icon_heigth, bits_cold_hover);
-	Icon icon_cold_normal = Icon(icon_width, icon_heigth, bits_cold_normal);
-	Icon icon_filament_1_hover = Icon(icon_width, icon_heigth, bits_filament_1_hover);
-	Icon icon_filament_1_normal = Icon(icon_width, icon_heigth, bits_filament_1_normal);
-	Icon icon_filament_2_hover = Icon(icon_width, icon_heigth, bits_filament_2_hover);
-	Icon icon_filament_2_normal = Icon(icon_width, icon_heigth, bits_filament_2_normal);
-	Icon icon_heat_hover = Icon(icon_width, icon_heigth, bits_heat_hover);
-	Icon icon_heat_normal = Icon(icon_width, icon_heigth, bits_heat_normal);
-	Icon icon_homing_hover = Icon(icon_width, icon_heigth, bits_homing_hover);
-	Icon icon_homing_normal = Icon(icon_width, icon_heigth, bits_homing_normal);
-	Icon icon_info_normal = Icon(icon_width, icon_heigth, bits_info_normal);
-	Icon icon_info_hover = Icon(icon_width, icon_heigth, bits_info_hover);
-	Icon icon_leveling_hover = Icon(icon_width, icon_heigth, bits_leveling_hover);
-	Icon icon_leveling_normal = Icon(icon_width, icon_heigth, bits_leveling_normal);
-	Icon icon_lightled_disable_hover = Icon(icon_width, icon_heigth, bits_lightled_disable_hover);
+	Icon icon_cold_hover              = Icon(icon_width, icon_heigth, bits_cold_hover);
+	Icon icon_cold_normal             = Icon(icon_width, icon_heigth, bits_cold_normal);
+	Icon icon_filament_1_hover        = Icon(icon_width, icon_heigth, bits_filament_1_hover);
+	Icon icon_filament_1_normal       = Icon(icon_width, icon_heigth, bits_filament_1_normal);
+	Icon icon_filament_2_hover        = Icon(icon_width, icon_heigth, bits_filament_2_hover);
+	Icon icon_filament_2_normal       = Icon(icon_width, icon_heigth, bits_filament_2_normal);
+	Icon icon_heat_hover              = Icon(icon_width, icon_heigth, bits_heat_hover);
+	Icon icon_heat_normal             = Icon(icon_width, icon_heigth, bits_heat_normal);
+	Icon icon_homing_hover            = Icon(icon_width, icon_heigth, bits_homing_hover);
+	Icon icon_homing_normal           = Icon(icon_width, icon_heigth, bits_homing_normal);
+	Icon icon_info_normal             = Icon(icon_width, icon_heigth, bits_info_normal);
+	Icon icon_info_hover              = Icon(icon_width, icon_heigth, bits_info_hover);
+	Icon icon_leveling_hover          = Icon(icon_width, icon_heigth, bits_leveling_hover);
+	Icon icon_leveling_normal         = Icon(icon_width, icon_heigth, bits_leveling_normal);
+	Icon icon_lightled_disable_hover  = Icon(icon_width, icon_heigth, bits_lightled_disable_hover);
 	Icon icon_lightled_disable_normal = Icon(icon_width, icon_heigth, bits_lightled_disable_normal);
-	Icon icon_lightled_hover = Icon(icon_width, icon_heigth, bits_lightled_hover);
-	Icon icon_lightled_normal = Icon(icon_width, icon_heigth, bits_lightled_normal);
-	Icon icon_moveaxis_hover = Icon(icon_width, icon_heigth, bits_moveaxis_hover);
-	Icon icon_moveaxis_normal = Icon(icon_width, icon_heigth, bits_moveaxis_normal);
-	Icon icon_nosd_hover = Icon(icon_width, icon_heigth, bits_nosd_hover);
-	Icon icon_nosd_normal = Icon(icon_width, icon_heigth, bits_nosd_normal);
-	Icon icon_sd_hover = Icon(icon_width, icon_heigth, bits_sd_hover);
-	Icon icon_sd_normal = Icon(icon_width, icon_heigth, bits_sd_normal);
-	Icon icon_steppers_hover = Icon(icon_width, icon_heigth, bits_steppers_hover);
-	Icon icon_steppers_normal = Icon(icon_width, icon_heigth, bits_steppers_normal);
-	Icon icon_steppers_off_hover = Icon(icon_width, icon_heigth, bits_steppers_off_hover);
-	Icon icon_steppers_off_normal = Icon(icon_width, icon_heigth, bits_steppers_off_normal);
+	Icon icon_lightled_hover          = Icon(icon_width, icon_heigth, bits_lightled_hover);
+	Icon icon_lightled_normal         = Icon(icon_width, icon_heigth, bits_lightled_normal);
+	Icon icon_moveaxis_hover          = Icon(icon_width, icon_heigth, bits_moveaxis_hover);
+	Icon icon_moveaxis_normal         = Icon(icon_width, icon_heigth, bits_moveaxis_normal);
+	Icon icon_nosd_hover              = Icon(icon_width, icon_heigth, bits_nosd_hover);
+	Icon icon_nosd_normal             = Icon(icon_width, icon_heigth, bits_nosd_normal);
+	Icon icon_sd_hover                = Icon(icon_width, icon_heigth, bits_sd_hover);
+	Icon icon_sd_normal               = Icon(icon_width, icon_heigth, bits_sd_normal);
+	Icon icon_steppers_hover          = Icon(icon_width, icon_heigth, bits_steppers_hover);
+	Icon icon_steppers_normal         = Icon(icon_width, icon_heigth, bits_steppers_normal);
+	Icon icon_steppers_off_hover      = Icon(icon_width, icon_heigth, bits_steppers_off_hover);
+	Icon icon_steppers_off_normal     = Icon(icon_width, icon_heigth, bits_steppers_off_normal);
 
 	/////////////////////////
 	// Instantiate Screens //
@@ -91,41 +91,79 @@ namespace screen
 
 	Screen * GuiBuild()
 	{
-		//Create screens 
-		//SD card screens
+		// Build the UI
+		//Main Menu
 		screen_main.add(screen_SD_list);
+		screen_main.add(screen_unload_select);
+		screen_main.add(screen_load_select);
+		screen_main.add(screen_level_confirm);
+		screen_main.add(screen_autohome);
+		screen_main.add(screen_stepper);
+		screen_main.add(screen_move);
+		screen_main.add(screen_temperature);
+		screen_main.add(screen_light);
+		screen_main.add(screen_info);
+		//SD Card List
 		screen_SD_list.add(screen_SD_confirm);
+		screen_SD_list.icon(icon_nosd_normal);
+		screen_SD_list.icon(icon_nosd_hover);
+		screen_SD_list.icon(icon_sd_normal);
+		screen_SD_list.icon(icon_sd_hover);
+		//SD Card Confirm
 		screen_SD_confirm.add(screen_SD_list);
 		screen_SD_confirm.add(screen_main);
-		//Unload filament screens
-		screen_main.add(screen_unload_select);
+		//Unload Filament Select
 		screen_unload_select.add(screen_unload_heating);
+		screen_unload_select.icon(icon_filament_1_normal);
+		screen_unload_select.icon(icon_filament_1_hover);
+		//Unload Filament Heating
 		screen_unload_heating.add(screen_unload_pull);
+		//Unload Filament Pull
 		screen_unload_pull.add(screen_unload_confirm);
+		//Unload Filament Confirm
 		screen_unload_confirm.add(screen_unload_pull);
 		screen_unload_confirm.add(screen_main);
-		//Load filament screens
-		screen_main.add(screen_load_select);
+		//Load Filament Select
 		screen_load_select.add(screen_load_heating);
+		screen_load_select.icon(icon_filament_2_normal);
+		screen_load_select.icon(icon_filament_2_hover);
+		//Load Filament Heating
 		screen_load_heating.add(screen_load_pull);
+		//Load Filament Pull
 		screen_load_pull.add(screen_load_confirm);
+		//Load Filament Confirm
 		screen_load_confirm.add(screen_load_pull);
 		screen_load_confirm.add(screen_main);
-		//Level screen
-		screen_main.add(screen_level_confirm);
+		//Level Plate
 		screen_level_confirm.add(screen_main);
-		screen_main.add(screen_autohome);
+		screen_level_confirm.icon(icon_leveling_normal);
+		screen_level_confirm.icon(icon_leveling_hover);
+		//AutoHome
 		screen_autohome.add(screen_main);
-		screen_main.add(screen_stepper);
+		screen_autohome.icon(icon_homing_normal);
+		screen_autohome.icon(icon_homing_hover);
+		//Stepper
 		screen_stepper.add(screen_main);
-		screen_main.add(screen_move);
+		screen_stepper.icon(icon_steppers_normal);
+		screen_stepper.icon(icon_steppers_hover);
+		//Move Axis
 		screen_move.add(screen_main);
-		screen_main.add(screen_temperature);
+		screen_move.icon(icon_moveaxis_normal);
+		screen_move.icon(icon_moveaxis_hover);
+		//Temperature
 		screen_temperature.add(screen_main);
-		screen_main.add(screen_light);
+		screen_temperature.icon(icon_heat_normal);
+		screen_temperature.icon(icon_heat_hover);
+		//Light
 		screen_light.add(screen_main);
-		screen_main.add(screen_info);
+		screen_light.icon(icon_lightled_disable_normal);
+		screen_light.icon(icon_lightled_disable_hover);
+		screen_light.icon(icon_lightled_normal);
+		screen_light.icon(icon_lightled_hover);
+		//Info
 		screen_info.add(screen_main);
+		screen_info.icon(icon_info_normal);
+		screen_info.icon(icon_info_hover);
 
 		return new ScreenMenu(screen_main);
 	}
