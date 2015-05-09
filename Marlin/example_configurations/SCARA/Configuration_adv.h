cb60e3d4ed9cc7f6d2301f8fd351ecf7dea67cdf
Scott Lahteine <sourcetree@thinkyhead.com>
2015-05-08 22:25:51 -0700

    Document WATCH_TEMP_PERIOD

--- Marlin/example_configurations/SCARA/Configuration_adv.h
+++ Marlin/example_configurations/SCARA/Configuration_adv.h
@@ -17,8 +17,8 @@
 //// Heating sanity check:
 // This waits for the watch period in milliseconds whenever an M104 or M109 increases the target temperature
 // If the temperature has not increased at the end of that period, the target temperature is set to zero.
-// It can be reset with another M104/M109. This check is also only triggered if the target temperature and the current temperature
-//  differ by at least 2x WATCH_TEMP_INCREASE
+// It can be reset with another M104/M109. This check is also only triggered if the target temperature and
+// the current temperature differ by at least 2x WATCH_TEMP_INCREASE
 //#define WATCH_TEMP_PERIOD 40000 //40 seconds
 //#define WATCH_TEMP_INCREASE 10  //Heat up at least 10 degree in 20 seconds
 
