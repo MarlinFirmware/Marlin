a665d128e0456e3bddf39de0793a3a17dfcd20e0
Christian Bohn <christianbohn@posteo.de>
2015-05-11 23:22:19 +0200

    sled improvements
    - create sled pin definition
    - G28 Works with sled

--- Marlin/Conditionals.h
+++ Marlin/Conditionals.h
@@ -276,6 +276,13 @@
     #define MAX_PROBE_Y (min(Y_MAX_POS, Y_MAX_POS + Y_PROBE_OFFSET_FROM_EXTRUDER))
   #endif
 
+   /**
+    * Sled Options
+    */ 
+  #ifdef Z_PROBE_SLED
+    #define Z_SAFE_HOMING
+  #endif
+  
   /**
    * MAX_STEP_FREQUENCY differs for TOSHIBA
    */
