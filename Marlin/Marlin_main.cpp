b417d4315b525d5a3d7ed28b0c952cbbcab7d1a9
Scott Lahteine <sourcetree@thinkyhead.com>
2015-05-12 04:18:15 -0700

    SCARA_SEGMENTS_PER_SECOND

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -363,6 +363,7 @@ bool target_direction;
 #endif
 
 #ifdef SCARA
+  float delta_segments_per_second = SCARA_SEGMENTS_PER_SECOND;
   static float delta[3] = { 0 };
   float axis_scaling[3] = { 1, 1, 1 };    // Build size scaling, default to 1
 #endif
--- Marlin/example_configurations/SCARA/Configuration.h
+++ Marlin/example_configurations/SCARA/Configuration.h
@@ -30,7 +30,7 @@ Here are some standard links for getting your machine calibrated:
 // You might need Z-Min endstop on SCARA-Printer to use this feature. Actually untested!
 // Uncomment to use Morgan scara mode
 #define SCARA  
-#define scara_segments_per_second 200 //careful, two much will decrease performance...
+#define SCARA_SEGMENTS_PER_SECOND 200 // If movement is choppy try lowering this value
 // Length of inner support arm
 #define Linkage_1 150 //mm      Preprocessor cannot handle decimal point...
 // Length of outer support arm     Measure arm lengths precisely and enter 
