0ce355d9e7bc76a9aea4bf723b746494839bf261
Scott Lahteine <sourcetree@thinkyhead.com>
2015-05-14 03:33:38 -0700

    Use MSG_OK in M105

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -3211,7 +3211,7 @@ inline void gcode_M105() {
   if (setTargetedHotend(105)) return;
 
   #if HAS_TEMP_0 || HAS_TEMP_BED || defined(HEATER_0_USES_MAX6675)
-    SERIAL_PROTOCOLPGM("ok");
+    SERIAL_PROTOCOLPGM(MSG_OK);
     #if HAS_TEMP_0
       SERIAL_PROTOCOLPGM(" T:");
       SERIAL_PROTOCOL_F(degHotend(target_extruder), 1);
