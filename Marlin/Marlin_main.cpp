a70c4705833b0385df5b9568036ec9b9a4f54857
Scott Lahteine <sourcetree@thinkyhead.com>
2015-05-26 16:27:07 -0700

    Flush the serial buffer after G29 or M48
    - Hosts may send lots of `M105` requests during a `G29` or other
    lengthy command, filling up the serial queue and breaking subsequent
    communication. This change adds a call to `FlushSerialRequestResend()`
    to the end of `G29` and `M48` – two commands that block the queue for a
    long time.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2797,6 +2797,8 @@ inline void gcode_G28() {
       enqueuecommands_P(PSTR(Z_PROBE_END_SCRIPT));
       st_synchronize();
     #endif
+
+    FlushSerialRequestResend();
   }
 
   #ifndef Z_PROBE_SLED
@@ -3360,6 +3362,8 @@ inline void gcode_M42() {
     SERIAL_PROTOCOLPGM("Standard Deviation: ");
     SERIAL_PROTOCOL_F(sigma, 6);
     SERIAL_EOL; SERIAL_EOL;
+
+    FlushSerialRequestResend();
   }
 
 #endif // ENABLE_AUTO_BED_LEVELING && Z_PROBE_REPEATABILITY_TEST
