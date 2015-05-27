7e9a5a8e6769e6e4d308f023dcc0baf581fc1226
Scott Lahteine <sourcetree@thinkyhead.com>
2015-05-26 18:35:00 -0700

    Apply ENABLED macro to LCD/SD sources

--- Marlin/ultralcd.h
+++ Marlin/ultralcd.h
@@ -4,7 +4,7 @@
 #include "Marlin.h"
 #include "buzzer.h"
 
-#ifdef ULTRA_LCD
+#if ENABLED(ULTRA_LCD)
   int lcd_strlen(char *s);
   int lcd_strlen_P(const char *s);
   void lcd_update();
@@ -16,15 +16,15 @@
   void lcd_reset_alert_level();
   bool lcd_detected(void);
 
-  #ifdef LCD_USE_I2C_BUZZER
+  #if ENABLED(LCD_USE_I2C_BUZZER)
     void lcd_buzz(long duration, uint16_t freq);
   #endif
 
-  #if defined(LCD_PROGRESS_BAR) && PROGRESS_MSG_EXPIRE > 0
+  #if ENABLED(LCD_PROGRESS_BAR) && PROGRESS_MSG_EXPIRE > 0
     void dontExpireStatus();
   #endif
 
-  #ifdef DOGLCD
+  #if ENABLED(DOGLCD)
     extern int lcd_contrast;
     void lcd_setcontrast(uint8_t value);
   #endif
@@ -35,10 +35,10 @@
   #define LCD_UPDATE_INTERVAL 100
   #define LCD_TIMEOUT_TO_STATUS 15000
 
-  #ifdef ULTIPANEL
+  #if ENABLED(ULTIPANEL)
     void lcd_buttons_update();
     extern volatile uint8_t buttons;  //the last checked buttons in a bit array.
-    #ifdef REPRAPWORLD_KEYPAD
+    #if ENABLED(REPRAPWORLD_KEYPAD)
       extern volatile uint8_t buttons_reprapworld_keypad; // to store the keypad shift register values
     #endif
   #else
@@ -54,7 +54,7 @@
 
   extern bool cancel_heatup;
   
-  #ifdef FILAMENT_LCD_DISPLAY
+  #if ENABLED(FILAMENT_LCD_DISPLAY)
     extern millis_t previous_lcd_status_ms;
   #endif
   void lcd_quick_feedback(); // Audible feedback for a button click - could also be visual
@@ -62,13 +62,13 @@
 
   void lcd_ignore_click(bool b=true);
 
-  #ifdef NEWPANEL
+  #if ENABLED(NEWPANEL)
     #define EN_C BIT(BLEN_C)
     #define EN_B BIT(BLEN_B)
     #define EN_A BIT(BLEN_A)
 
     #define LCD_CLICKED (buttons&EN_C)
-    #ifdef REPRAPWORLD_KEYPAD
+    #if ENABLED(REPRAPWORLD_KEYPAD)
   	  #define EN_REPRAPWORLD_KEYPAD_F3 (BIT(BLEN_REPRAPWORLD_KEYPAD_F3))
   	  #define EN_REPRAPWORLD_KEYPAD_F2 (BIT(BLEN_REPRAPWORLD_KEYPAD_F2))
   	  #define EN_REPRAPWORLD_KEYPAD_F1 (BIT(BLEN_REPRAPWORLD_KEYPAD_F1))
