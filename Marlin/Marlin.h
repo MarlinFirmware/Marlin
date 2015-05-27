a4f2f1a831a925908fda6c4e697e6e50d3626bb1
Scott Lahteine <sourcetree@thinkyhead.com>
2015-05-26 18:28:25 -0700

    Add macros.h with helpers for overridable options

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -32,14 +32,6 @@
   #include "WProgram.h"
 #endif
 
-#define BIT(b) (1<<(b))
-#define TEST(n,b) (((n)&BIT(b))!=0)
-#define SET_BIT(n,b,value) (n) ^= ((-value)^(n)) & (BIT(b))
-#define RADIANS(d) ((d)*M_PI/180.0)
-#define DEGREES(r) ((d)*180.0/M_PI)
-#define NOLESS(v,n) do{ if (v < n) v = n; }while(0)
-#define NOMORE(v,n) do{ if (v > n) v = n; }while(0)
-
 typedef unsigned long millis_t;
 
 // Arduino < 1.0.0 does not define this, so we need to do it ourselves
