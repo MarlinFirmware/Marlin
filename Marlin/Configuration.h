e86f2342c057cdc624a07a11ed26bb653c790bda
Jonathan Bastien-Filiatrault <joe@x2a.org>
2015-05-28 11:30:56 -0400

    Separate the maximum integral power for the bed from PID_INTEGRAL_DRIVE_MAX / PID_MAX.
    My PID_INTEGRAL_DRIVE_MAX was at 96, which was insufficient for my bed
    to reach its intended temperature. The head requires a low maximum power since
    I run on 24 volts and it has a somewhat low thermal mass.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -243,6 +243,8 @@ Here are some standard links for getting your machine calibrated:
 //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
 #ifdef PIDTEMPBED
+    #define PID_BED_INTEGRAL_DRIVE_MAX MAX_BED_POWER //limit for the integral term
+
 //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
     #define  DEFAULT_bedKp 10.00
