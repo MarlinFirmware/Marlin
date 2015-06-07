8ca2902294f5f7ca89e3b2d20288d229ed23ab30
Richard Wackerbarth <rkw@dataplex.net>
2015-06-07 04:40:52 -0500

    Deprecate Support for Arduino IDE < 1.0.5
    Note that newer versions of the IDE can still
    be made to support compilation for the older boards.

--- Marlin/blinkm.h
+++ Marlin/blinkm.h
@@ -2,12 +2,8 @@
   blinkm.h
   Library header file for BlinkM library
  */
-#if ARDUINO >= 100
-  #include "Arduino.h"
-#else
-  #include "WProgram.h"
-#endif
 
+#include "Arduino.h"
 #include "Wire.h"
 
 void SendColors(byte red, byte grn, byte blu);
