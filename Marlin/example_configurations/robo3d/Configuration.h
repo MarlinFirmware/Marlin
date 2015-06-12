516dc54e59e0f6df8a57a9a2b22ad40bd410b22e
WheresWaldo <bud.hammerton@gmail.com>
2015-06-12 10:24:34 -0400

    Update Configuration.h
    CUSTOM_MENDEL_NAME was 1 character too long.

--- Marlin/example_configurations/robo3d/Configuration.h
+++ Marlin/example_configurations/robo3d/Configuration.h
@@ -48,7 +48,7 @@
 #endif
 
 // Define this to set a custom name for your generic Mendel,
-#define CUSTOM_MENDEL_NAME "Robo3D v1.0.2+"	//ROBO-BH
+#define CUSTOM_MENDEL_NAME "Robo3D v1.0.2"	//ROBO-BH
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
