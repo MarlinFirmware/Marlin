73ea86509832c6299ef3341e2913a62d974496aa
AnHardt <github@kitelab.de>
2015-06-16 23:00:43 +0200

    Rename and repair MELZI_1284 (PR#2301)
    to MELZI_MAKR3D.
    
    Fix for #2288
    As the normal MELZI also can have a 1284.

--- Marlin/pins_MELZI_MAKR3D.h
+++ Marlin/pins_MELZI_MAKR3D.h
@@ -2,10 +2,8 @@
  * Melzi with ATmega1284 (MaKr3d version) pin assignments
  */
 
-#define MELZI
-
 #undef MOTHERBOARD
-#define MOTHERBOARD BOARD_SANGUINOLOLU_11
+#define MOTHERBOARD MELZI
 #define SANGUINOLOLU_V_1_2
 
 #if defined(__AVR_ATmega1284P__)
