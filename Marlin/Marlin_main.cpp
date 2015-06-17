1d4f83cb0d2f002ae95ff8cdbf889b4f5e2882f7
AnHardt <github@kitelab.de>
2015-06-17 02:34:24 +0200

    Split buzzing again
    in a LCD dependant
    and a pin dependant function.
    
    lcd in lcd.buzz() is unknown in Marlin_main.cpp,
    while ultralcd.cpp is not compiled in for an on-board buzzer.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -6622,3 +6622,22 @@ void calculate_volumetric_multipliers() {
   for (int i=0; i<EXTRUDERS; i++)
     volumetric_multiplier[i] = calculate_volumetric_multiplier(filament_size[i]);
 }
+
+#if HAS_BUZZER
+  void buzz(long duration, uint16_t freq) {
+    if (freq > 0) {
+      #ifdef LCD_USE_I2C_BUZZER
+        lcd_buzz(duration, freq);
+      #elif defined(BEEPER) && BEEPER >= 0 // on-board buzzers have no further condition
+        SET_OUTPUT(BEEPER);
+        tone(BEEPER, freq, duration);
+        delay(duration);
+      #else
+        delay(duration);
+      #endif
+    }
+    else {
+      delay(duration);
+    }
+  }
+#endif
