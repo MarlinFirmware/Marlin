03b08d2c2893097ba4369e341773eb07d3e7e77b
anatom74 <tomaz.kerin@siol.net>
2015-06-18 18:32:44 +0200

    popravek offset autobedlevel

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -510,7 +510,7 @@ const bool Z_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the logic 
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER -62  // Probe on: -left  +right
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -20     // Probe on: -front +behind
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -9.63 // -below (always!)
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -9.50 // -below (always!)
 
   #define Z_RAISE_BEFORE_HOMING 15       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
