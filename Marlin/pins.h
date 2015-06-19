b588f8569d8ffaeb174464998e23f58cac9f0417
Aerospacesmith <keanan_smith@hotmail.com>
2015-06-19 11:45:46 -0500

    Remove Azteeg X3 Pro conflict of Fan and Heater_1
    Removes a default conflict on Azteeg X3 Pro boards with the Fan and second heater (Heater_1). Change sets fan to 8th heater terminal on board.

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -655,8 +655,10 @@
     #define LED_PIN            13
   #endif
 
-  #if MB(RAMPS_13_EFB) || MB(RAMPS_13_EFF) || MB(AZTEEG_X3) || MB(AZTEEG_X3_PRO)
+  #if MB(RAMPS_13_EFB) || MB(RAMPS_13_EFF) || MB(AZTEEG_X3)
     #define FAN_PIN            9 // (Sprinter config)
+  #elif MB(AZTEEG_X3_PRO)
+    #define FAN_PIN            11 // Last Heater Pin on board
   #else
     #define FAN_PIN            4 // IO pin. Buffer needed
   #endif
