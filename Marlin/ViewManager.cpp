0eb7757dccaa0a2fcf7ee7a1f3f508a83c2770d6
Ivan Galvez Junquera <ivan.galvez@bq.com>
2015-06-29 12:47:30 +0200

    Interface for ViewManager to keep track of last focused item.

--- Marlin/ViewManager.cpp
+++ Marlin/ViewManager.cpp
@@ -20,8 +20,14 @@ namespace screen
 		return m_active_view;
 	}
 
+	void ViewManager::setLastFocus(uint16_t last_focus)
+	{
+		m_last_focus = last_focus;
+	}
+
 	ViewManager::ViewManager()
 		: m_active_view(NULL)
+		, m_last_focus(0)
 	{ }
 
 	ViewManager::~ViewManager()
