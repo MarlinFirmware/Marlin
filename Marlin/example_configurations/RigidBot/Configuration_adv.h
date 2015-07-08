c2d353e048ae06a6e7f062968c4781f2f3a3ed44
Scott Lahteine <sourcetree@thinkyhead.com>
2015-07-08 15:32:03 -0700

    RigidBot CONTROLLERFAN_PIN in Configuration_adv.h

--- Marlin/example_configurations/RigidBot/Configuration_adv.h
+++ Marlin/example_configurations/RigidBot/Configuration_adv.h
@@ -85,7 +85,7 @@
 //This is for controlling a fan to cool down the stepper drivers
 //it will turn on when any driver is enabled
 //and turn off after the set amount of seconds from last driver being disabled again
-#define CONTROLLERFAN_PIN -1 //Pin used for the fan to cool controller (-1 to disable)
+#define CONTROLLERFAN_PIN 4 //Pin used for the fan to cool motherboard (-1 to disable) // RigidBot: Fans/Water Pump to cool the hotend cool side.
 #define CONTROLLERFAN_SECS 60 //How many seconds, after all motors were disabled, the fan should run
 #define CONTROLLERFAN_SPEED 255  // == full speed
 
