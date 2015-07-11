af44d3e4dcc8fa824a4af9e8d4e7575bbe068522
Scott Lahteine <sourcetree@thinkyhead.com>
2015-07-10 19:56:53 -0700

    Move Y_STOP_PIN for Printrboard with SD (PR#2420)

--- Marlin/pins_PRINTRBOARD.h
+++ Marlin/pins_PRINTRBOARD.h
@@ -45,7 +45,11 @@
 #endif
 
 #define X_STOP_PIN         35
-#define Y_STOP_PIN          8
+#ifdef SDSUPPORT
+  #define Y_STOP_PIN       37 // Move Ystop to Estop socket
+#else
+  #define Y_STOP_PIN        8 // Ystop in Ystop socket
+#endif
 #define Z_STOP_PIN         36
 #define TEMP_0_PIN          1  // Extruder / Analog pin numbering
 #define TEMP_BED_PIN        0  // Bed / Analog pin numbering
@@ -67,7 +71,7 @@
 #define ALARM_PIN          -1
 
 #ifndef SDSUPPORT
-// these pins are defined in the SD library if building with SD support
+  // these pins are defined in the SD library if building with SD support
   #define SCK_PIN           9
   #define MISO_PIN         11
   #define MOSI_PIN         10
