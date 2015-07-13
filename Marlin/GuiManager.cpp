6b16e44e202611797928a43981136f2a807a11b5
Joaquín Herrero <joaquin.herrero@bq.com>
2015-07-13 16:10:59 +0200

    Added HelpersC++ file to avoid helpers dependency with GuiManager.

--- Marlin/GuiManager.cpp
+++ Marlin/GuiManager.cpp
@@ -18,14 +18,6 @@
 
 #include <avr/wdt.h>
 
-//C++ helpers for Arduino
-
-__extension__ typedef int __guard __attribute__((mode (__DI__)));
-extern "C" int __cxa_guard_acquire(__guard *g) { return !*(char *)(g); };
-extern "C" void __cxa_guard_release (__guard *g) { *(char *)g = 1; };
-extern "C" void __cxa_guard_abort (__guard *) {}; 
-//extern "C" void __cxa_pure_virtual() { while (1); }
-
 /////////////////////////////////////////////////////////////////////////
 //                          Marlin interface                           //
 /////////////////////////////////////////////////////////////////////////
