1ae152ae34f25c520b7fdb9f3efe5a1cf59f3fae
Scott Lahteine <sourcetree@thinkyhead.com>
2015-07-15 16:10:23 -0700

    Remove PID examples from preconfigured configs

--- Marlin/example_configurations/delta/kossel_pro/Configuration.h
+++ Marlin/example_configurations/delta/kossel_pro/Configuration.h
@@ -209,26 +209,10 @@ Here are some standard links for getting your machine calibrated:
   #define PID_INTEGRAL_DRIVE_MAX PID_MAX  //limit for the integral term
   #define K1 0.95 //smoothing factor within the PID
 
-  // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
-  // Ultimaker
-  //#define  DEFAULT_Kp 22.2
-  //#define  DEFAULT_Ki 1.08
-  //#define  DEFAULT_Kd 114
-
   // Kossel Pro
   #define  DEFAULT_Kp 19.30
   #define  DEFAULT_Ki 3.51
   #define  DEFAULT_Kd 26.56
-
-  // MakerGear
-  //#define  DEFAULT_Kp 7.0
-  //#define  DEFAULT_Ki 0.1
-  //#define  DEFAULT_Kd 12
-
-  // Mendel Parts V9 on 12V
-  //#define  DEFAULT_Kp 63.0
-  //#define  DEFAULT_Ki 2.25
-  //#define  DEFAULT_Kd 440
 
 #endif // PIDTEMP
 
@@ -265,18 +249,6 @@ Here are some standard links for getting your machine calibrated:
   #define  DEFAULT_bedKp 370.25
   #define  DEFAULT_bedKi 62.77
   #define  DEFAULT_bedKd 545.98
-
-  //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  //#define  DEFAULT_bedKp 10.00
-  //#define  DEFAULT_bedKi .023
-  //#define  DEFAULT_bedKd 305.4
-
-  //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  //from pidautotune
-  //#define  DEFAULT_bedKp 97.1
-  //#define  DEFAULT_bedKi 1.41
-  //#define  DEFAULT_bedKd 1675.16
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
