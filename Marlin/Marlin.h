4d07fcc616a636c221aef11d215d76231407ff11
Eric Kuzmenko <eric.gralco@gmail.com>
2015-07-17 09:23:31 -0500

    Forgot to remove "<<<<<<< HEAD".

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -248,7 +248,6 @@ extern int extruder_multiplier[EXTRUDERS]; // sets extrude multiply factor (in p
 extern float filament_size[EXTRUDERS]; // cross-sectional area of filament (in millimeters), typically around 1.75 or 2.85, 0 disables the volumetric calculations for the extruder.
 extern float volumetric_multiplier[EXTRUDERS]; // reciprocal of cross-sectional area of filament (in square millimeters), stored this way to reduce computational burden in planner
 extern float current_position[NUM_AXIS];
-<<<<<<< HEAD
 extern float home_offset[3]; // axis[n].home_offset
 extern float min_pos[3]; // axis[n].min_pos
 extern float max_pos[3]; // axis[n].max_pos
