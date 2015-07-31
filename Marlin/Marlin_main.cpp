918d7fd18b5bf07657ed13f7d4fcdd9303198d56
Scott Lahteine <sourcetree@thinkyhead.com>
2015-07-30 20:47:45 -0700

    Fix compiler warning about signed compare

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -3082,7 +3082,7 @@ inline void gcode_M42() {
     if (code_seen('P') && pin_status >= 0 && pin_status <= 255)
       pin_number = code_value_short();
 
-    for (int8_t i = 0; i < COUNT(sensitive_pins); i++) {
+    for (uint8_t i = 0; i < COUNT(sensitive_pins); i++) {
       if (sensitive_pins[i] == pin_number) {
         pin_number = -1;
         break;
@@ -4222,7 +4222,7 @@ inline void gcode_M226() {
 
     if (pin_state >= -1 && pin_state <= 1) {
 
-      for (int8_t i = 0; i < COUNT(sensitive_pins); i++) {
+      for (uint8_t i = 0; i < COUNT(sensitive_pins); i++) {
         if (sensitive_pins[i] == pin_number) {
           pin_number = -1;
           break;
