562c364ab058a405d65b0d2e7ccb8a168306769d
Richard Wackerbarth <rkw@dataplex.net>
2015-08-01 10:03:46 -0500

    Supply a VERSION in a short form for displays
    The long descriptor will still be available in M115

--- Marlin/configurator/config/language.h
+++ Marlin/configurator/config/language.h
@@ -73,8 +73,8 @@
   #define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
 #endif
 
-#ifndef BUILD_VERSION
-  #define BUILD_VERSION "V1; Sprinter/grbl mashup for gen6"
+#ifndef DETAILED_BUILD_VERSION
+  #error BUILD_VERSION Information must be specified
 #endif
 
 #ifndef MACHINE_UUID
@@ -122,7 +122,7 @@
 #define MSG_HEATING_COMPLETE                "Heating done."
 #define MSG_BED_HEATING                     "Bed Heating."
 #define MSG_BED_DONE                        "Bed done."
-#define MSG_M115_REPORT                     "FIRMWARE_NAME:Marlin " BUILD_VERSION " SOURCE_CODE_URL:" SOURCE_CODE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
+#define MSG_M115_REPORT                     "FIRMWARE_NAME:Marlin " DETAILED_BUILD_VERSION " SOURCE_CODE_URL:" SOURCE_CODE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
 #define MSG_COUNT_X                         " Count X: "
 #define MSG_ERR_KILLED                      "Printer halted. kill() called!"
 #define MSG_ERR_STOPPED                     "Printer stopped due to errors. Fix the error and use M999 to restart. (Temperature is reset. Set it after restarting)"
