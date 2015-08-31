db27e67580a126d2c398fb0f60065e1bb0701e9e
Scott Lahteine <sourcetree@thinkyhead.com>
2015-08-30 19:05:29 -0700

    Apply PID_ADD_EXTRUSION_RATE to extras

--- Marlin/configurator/config/language.h
+++ Marlin/configurator/config/language.h
@@ -199,6 +199,7 @@
 #define MSG_PID_DEBUG_PTERM                 " pTerm "
 #define MSG_PID_DEBUG_ITERM                 " iTerm "
 #define MSG_PID_DEBUG_DTERM                 " dTerm "
+#define MSG_PID_DEBUG_CTERM                 " cTerm "
 #define MSG_INVALID_EXTRUDER_NUM            " - Invalid extruder number !"
 
 #define MSG_HEATER_BED                      "bed"
