7ae5fb749c83cb23466b33f8f34766729b0f2170
KiteLab <KiteLab@users.noreply.github.com>
2015-09-22 20:23:21 +0200

    Update Default_Version.h
    Update Default_Version.h to 1.1.0 rc1

--- Marlin/Default_Version.h
+++ Marlin/Default_Version.h
@@ -6,9 +6,9 @@
 // #error "You must specify the following parameters related to your distribution"
 
 #if true
-#define SHORT_BUILD_VERSION "1.1.0 dev"
-#define DETAILED_BUILD_VERSION "1.1.0 Development From Archive"
-#define STRING_DISTRIBUTION_DATE "2015-08-00 12:00"
+#define SHORT_BUILD_VERSION "1.1.0 rc1"
+#define DETAILED_BUILD_VERSION "1.1.0 rc1 From Archive"
+#define STRING_DISTRIBUTION_DATE "2015-09-19 12:00"
 // It might also be appropriate to define a location where additional information can be found
 #define SOURCE_CODE_URL  "http:// ..."
 #endif
