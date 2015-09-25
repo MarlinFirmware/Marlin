1cd0d1b93fc99906e29f24f6d68552b9853bcf93
Ivan Galvez Junquera <ivan.galvez@bq.com>
2015-09-25 08:58:12 +0200

    Mark version 2.0.0b3.

--- Marlin/config/witbox/Configuration.h
+++ Marlin/config/witbox/Configuration.h
@@ -8,10 +8,10 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define MACHINE_NAME "BQ Witbox"
+#define MACHINE_NAME "Witbox"
 #define FIRMWARE_URL "http://www.bq.com/gb/support/witbox"
 #define SOURCE_CODE_URL "http://github.com/bq/Marlin"
-#define FIRMWARE_VER "2.0.0"
+#define FIRMWARE_VER "2.0.0b3"
 #define BUILD_VER ""
 
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
