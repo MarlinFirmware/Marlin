c2d4a1db5864b2d03ad699ba7cd4a33e352f9104
Scott Lahteine <sourcetree@thinkyhead.com>
2015-10-02 23:19:13 -0700

    Keep "astyled" elements in vector_3.*

--- Marlin/vector_3.h
+++ Marlin/vector_3.h
@@ -22,41 +22,39 @@
 #if ENABLED(AUTO_BED_LEVELING_FEATURE)
 class matrix_3x3;
 
-struct vector_3
-{
-	float x, y, z;
+struct vector_3 {
+  float x, y, z;
 
-        vector_3();
-	vector_3(float x, float y, float z);
+  vector_3();
+  vector_3(float x, float y, float z);
 
-	static vector_3 cross(vector_3 a, vector_3 b);
+  static vector_3 cross(vector_3 a, vector_3 b);
 
-	vector_3 operator+(vector_3 v);
-	vector_3 operator-(vector_3 v);
-	void normalize();
-	float get_length();
-	vector_3 get_normal();
+  vector_3 operator+(vector_3 v);
+  vector_3 operator-(vector_3 v);
+  void normalize();
+  float get_length();
+  vector_3 get_normal();
 
-	void debug(const char title[]);
-	
-	void apply_rotation(matrix_3x3 matrix);
+  void debug(const char title[]);
+
+  void apply_rotation(matrix_3x3 matrix);
 };
 
-struct matrix_3x3
-{
-	float matrix[9];
+struct matrix_3x3 {
+  float matrix[9];
 
-	static matrix_3x3 create_from_rows(vector_3 row_0, vector_3 row_1, vector_3 row_2);
-	static matrix_3x3 create_look_at(vector_3 target);
-	static matrix_3x3 transpose(matrix_3x3 original);
+  static matrix_3x3 create_from_rows(vector_3 row_0, vector_3 row_1, vector_3 row_2);
+  static matrix_3x3 create_look_at(vector_3 target);
+  static matrix_3x3 transpose(matrix_3x3 original);
 
-	void set_to_identity();
+  void set_to_identity();
 
-	void debug(const char title[]);
+  void debug(const char title[]);
 };
 
 
-void apply_rotation_xyz(matrix_3x3 rotationMatrix, float &x, float& y, float& z);
+void apply_rotation_xyz(matrix_3x3 rotationMatrix, float& x, float& y, float& z);
 #endif // AUTO_BED_LEVELING_FEATURE
 
 #endif // VECTOR_3_H
