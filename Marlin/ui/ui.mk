7bd96aff328dfb1ce908de6e7f04bcf8915fa4b0
Ivan Galvez Junquera <ivan.galvez@bq.com>
2015-10-13 17:28:12 +0200

    Move most of UI files to specific folder.

---
+++ Marlin/ui/ui.mk
@@ -0,0 +1,22 @@
+CXXSRC += \
+	Icon.cpp \
+	Option.cpp \
+	OptionLaunch.cpp \
+	OptionToggle.cpp \
+	Screen.cpp \
+	ScreenAbout.cpp \
+	ScreenComplete.cpp \
+	ScreenCooldown.cpp \
+	ScreenEmergency.cpp \
+	ScreenFile.cpp \
+	ScreenInactivity.cpp \
+	ScreenLanguage.cpp \
+	ScreenList.cpp \
+	ScreenMenu.cpp \
+	ScreenPrint.cpp \
+	ScreenSerial.cpp \
+	ScreenSetting.cpp \
+	ScreenSplash.cpp \
+	ScreenSwitch.cpp \
+	ScreenTemperature.cpp \
+	ScreenTransition.cpp 
