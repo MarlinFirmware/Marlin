7143b1ca2e11fcf99485326334646c70f779c4ff
Joaquín Herrero <joaquin.herrero@bq.com>
2015-10-14 08:57:39 +0200

    Added 'Auto' mode to light on witbox 2. Fixes MARLIN-113.
    Changes:
    
            - OptionToogle now is a template class.
    	- Removed OptionToogle.cpp from Makefile.
    	- Modified LightManager. Now it has mode and state member variables.
    	- Modified StorageManager to storage mode on uint8_t type from LightManager.
    	- Modified PrintManager. Now manages light led on 'resetInactivity'. Reworked 'inactivityTriggered' method.

--- Marlin/OptionToggle.h
+++ Marlin/OptionToggle.h
@@ -32,30 +32,104 @@
 #include "Observer.h"
 #include "Functor.h"
 
+#include "GuiPainter.h"
+#include "ViewManager.h"
+
 namespace screen
 {
 	typedef enum
 	{
-		ON,
 		OFF,
+		ON,
+		AUTO,
 		NUM_STATES
 	} ToggleState_t;
 
-	class OptionToggle : public Option , public Functor<void>, public Observer<bool>
+	template <typename T>
+		class OptionToggle : public Option , public Functor<void>, public Observer<T>
 	{
 		public:
-			OptionToggle(option::Size const & size, const char * text, Functor<void>::FuncPtr fptr = do_nothing, Subject<bool> * model = 0);
+			OptionToggle(option::Size const & size, const char * text, Functor<void>::FuncPtr fptr = do_nothing, Subject<T> * model = 0);
 			virtual ~OptionToggle();
 
 			const char * text() const;
 			virtual void draw();
 			virtual void press();
 
-			void update(bool value);
+			void update(T value);
 
 		private:
 			ToggleState_t m_state;
 	};
+
+	template <typename T>
+	OptionToggle<T>::OptionToggle(option::Size const & size, const char * text, Functor<void>::FuncPtr fptr, Subject<T> * model)
+		: Option(size, text)
+		, Functor<void>(fptr)
+		, Observer<T>(model)
+	{
+		this->connect();
+	}
+
+	template <typename T>
+	OptionToggle<T>::~OptionToggle()
+	{ }
+
+	template <typename T>
+	const char * OptionToggle<T>::text() const
+	{
+		return m_text;
+	}
+
+	template <typename T>
+	void OptionToggle<T>::draw()
+	{
+		Area draw_area = painter.getWorkingArea();
+		painter.setPrintPos(draw_area.x_init, draw_area.y_init);
+		painter.print_P(m_text);
+
+		switch(m_state)
+		{
+			case ON:
+				painter.setPrintPos(draw_area.x_end - 4*6, draw_area.y_init);
+				painter.print("[On]");
+				break;
+			case OFF:
+				painter.setPrintPos(draw_area.x_end - 5*6, draw_area.y_init);
+				painter.print("[Off]");
+				break;
+			case AUTO:
+				painter.setPrintPos(draw_area.x_end - 6*6, draw_area.y_init);
+				painter.print("[Auto]");
+				break;
+			default:
+				break;
+		}
+	}
+
+	template <typename T>
+	void OptionToggle<T>::press()
+	{
+		this->action();
+	}
+
+	template <typename T>
+	void OptionToggle<T>::update(T value)
+	{
+		if(value == 0)
+		{
+			m_state = OFF;
+		}
+		else if(value == 1)
+		{
+			m_state = ON;
+		}
+		else if(value == 2)
+		{
+			m_state = AUTO;
+		}
+		ViewManager::getInstance().displayRefresh();
+	}
 	
 }
 
