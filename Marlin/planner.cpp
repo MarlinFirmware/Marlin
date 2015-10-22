36d195ab56a8fff9184c201b615139ec21593b05
Joaquín Herrero <joaquin.herrero@bq.com>
2015-10-22 16:55:33 +0200

    Fix renamed variables for block steps on planner.cpp

--- Marlin/planner.cpp
+++ Marlin/planner.cpp
@@ -924,7 +924,7 @@ Having the real displacement of the head, we can calculate the total movement le
 
   // Compute and limit the acceleration rate for the trapezoid generator.  
   float steps_per_mm = block->step_event_count / block->millimeters;
-  long bsx = block->steps[X_AXIS], bsy = block->steps[Y_AXIS], bsz = block->steps[Z_AXIS], bse = block->steps[E_AXIS];
+  long bsx = block->steps_x, bsy = block->steps_y, bsz = block->steps_z, bse = block->steps_e;
   if (bsx == 0 && bsy == 0 && bsz == 0) {
     block->acceleration_st = ceil(retract_acceleration * steps_per_mm); // convert to: acceleration steps/sec^2
   }
