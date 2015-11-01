80584d06d35f3b5161775f9dda038686d409adb6
AnHardt <github@kitelab.de>
2015-11-01 19:37:28 +0100

    Suppress Z_MIN_PROBE_REPEATABILITY_TEST for DELTA and SCARA configurations (PR#2747)
    For now Z_MIN_PROBE_REPEATABILITY_TEST is not possible for DELTA or SCARA
    configurations.
    This PR comments the feature out, where it is commented in by default.

--- Marlin/example_configurations/delta/kossel_pro/Configuration.h
+++ Marlin/example_configurations/delta/kossel_pro/Configuration.h
@@ -467,7 +467,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 
 //#define AUTO_BED_LEVELING_FEATURE // Delete the comment to enable (remove // at the start of the line)
 //#define DEBUG_LEVELING_FEATURE
-#define Z_MIN_PROBE_REPEATABILITY_TEST  // If not commented out, Z-Probe Repeatability test will be included if Auto Bed Leveling is Enabled.
+//#define Z_MIN_PROBE_REPEATABILITY_TEST  // If not commented out, Z-Probe Repeatability test will be included if Auto Bed Leveling is Enabled.
 
 #if ENABLED(AUTO_BED_LEVELING_FEATURE)
 
