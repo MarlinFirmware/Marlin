58d5f4a977aa67b0debdb46e74038c03069523f9
Ivan Galvez Junquera <ivan.galvez@bq.com>
2015-11-02 08:59:04 +0100

    Added header.

--- Marlin/GuiBitmaps_witbox_2.h
+++ Marlin/GuiBitmaps_witbox_2.h
@@ -1,7 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file GuiBitmaps_witbox_2.h
+///
+/// \author Ivan Galvez Junquera
+///         Ruy Garcia
+///         Victor Andueza 
+///         Joaquin Herrero
+///
+/// \brief Bitmaps for Witbox 2 and Hephestos 2 UI
+///
+/// Copyright (c) 2015 BQ - Mundo Reader S.L.
+/// http://www.bq.com
+///
+/// This file is free software; you can redistribute it and/or modify
+/// it under the terms of either the GNU General Public License version 2 or 
+/// later or the GNU Lesser General Public License version 2.1 or later, both
+/// as published by the Free Software Foundation.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+/// DEALINGS IN THE SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef GUI_BITMAPS_WITBOX_2_H
 #define GUI_BITMAPS_WITBOX_2_H
 
 #include <stdint.h>
+
 #include "Marlin.h" //Required for PROGMEM macro
 
 const static uint8_t icon_width = 24;
