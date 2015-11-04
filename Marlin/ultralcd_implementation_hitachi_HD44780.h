ba9ab4315f738e881d3991ef6ae29a3586a73164
AnHardt <github@kitelab.de>
2015-11-04 14:19:33 +0100

    Activate warning about possible reduced accuracy by default
    Renamed `WARN_REDUCED_ACCURACY` to `DISABLE_REDUCED_ACCURACY_WARNING`
    
    Changed the condition for blinking from
    ```
    #if ENABLED(WARN_REDUCED_ACCURACY)
    ```
    to
    ```
    #if DISABLED(DISABLE_REDUCED_ACCURACY_WARNING)
    ```

--- Marlin/ultralcd_implementation_hitachi_HD44780.h
+++ Marlin/ultralcd_implementation_hitachi_HD44780.h
@@ -624,8 +624,8 @@ static void lcd_implementation_status_screen() {
         else {
           if (!axis_homed[X_AXIS])
             lcd_printPGM(PSTR("?"));
-          else
-            #if ENABLED(WARN_REDUCED_ACCURACY)
+          else 
+            #if DISABLED(DISABLE_REDUCED_ACCURACY_WARNING)
               if (!axis_known_position[X_AXIS])
                 lcd_printPGM(PSTR(" "));
               else
@@ -641,8 +641,8 @@ static void lcd_implementation_status_screen() {
         else {
           if (!axis_homed[Y_AXIS])
             lcd_printPGM(PSTR("?"));
-          else
-            #if ENABLED(WARN_REDUCED_ACCURACY)
+          else 
+            #if DISABLED(DISABLE_REDUCED_ACCURACY_WARNING)
               if (!axis_known_position[Y_AXIS])
                 lcd_printPGM(PSTR(" "));
               else
@@ -661,8 +661,8 @@ static void lcd_implementation_status_screen() {
     else {
       if (!axis_homed[Z_AXIS])
         lcd_printPGM(PSTR("?"));
-      else
-        #if ENABLED(WARN_REDUCED_ACCURACY)
+      else 
+        #if DISABLED(DISABLE_REDUCED_ACCURACY_WARNING)
           if (!axis_known_position[Z_AXIS])
             lcd_printPGM(PSTR(" "));
           else
