3dc2da19bd53a896460112339416ee48aec4034c
Ivan Galvez Junquera <ivan.galvez@bq.com>
2015-11-10 16:39:37 +0100

    Added file headers.

--- Marlin/GuiPainter.h
+++ Marlin/GuiPainter.h
@@ -1,3 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file GuiPainter.h
+///
+/// \author Ivan Galvez Junquera
+///         Ruy Garcia
+///         Victor Andueza 
+///         Joaquin Herrero
+///
+/// \brief Painting interface class to U8glib
+///
+/// Copyright (c) 2015 BQ - Mundo Reader S.L.
+/// http://www.bq.com
+///
+/// This file is free software; you can redistribute it and/or modify
+/// it under the terms of either the GNU General Public License version 2 or 
+/// later or the GNU Lesser General Public License version 2.1 or later, both
+/// as published by the Free Software Foundation.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+/// DEALINGS IN THE SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef GUI_PAINTER_H
 #define GUI_PAINTER_H
 
@@ -6,7 +33,6 @@
 // Painting library
 #include "Singleton.h"
 #include "ultralcd_st7920_u8glib_rrd.h"
-
 
 // Specify the printer UI implementation
 #include "GuiImpl_witbox_2.h"
@@ -46,7 +72,7 @@ namespace screen
 		BOTH,
 		NUM_SCREEN_TYPES,
 	} ArrowType_t;
-	
+
 	typedef enum
 	{
 		BODY_FONT,
