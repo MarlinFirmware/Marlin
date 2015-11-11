8c1f16efc2ce52ada766ee5be85c0cf0439be1f0
Koldo de Miguel <koldo.demiguel@bq.com>
2015-11-11 13:17:27 +0100

    SDCache manages SD browsing and caching

---
+++ Marlin/SDCache.h
@@ -0,0 +1,65 @@
+#ifndef SD_CACHE_H
+#define SD_CACHE_H
+
+#include "Singleton.h"
+#include "cardreader.h"
+
+#ifndef SD_CACHE_SIZE
+	#define SD_CACHE_SIZE LCD_HEIGHT+1
+#endif //SD_CACHE_SIZE
+
+typedef struct {
+		uint8_t type;	//make enum for action type
+		void * action;		///probably can be removed
+		char filename[13];
+		char longFilename[LONG_FILENAME_LENGTH];
+} cache_entry;
+
+class SDCache
+{
+	public:
+		typedef Singleton<SDCache> single;
+
+	public:
+		SDCache();
+		
+		void ReloadCache();
+		void updateCachePosition(int16_t index);
+		uint8_t press(uint16_t index);
+		
+		inline cache_entry const * getSelectedEntry() { return window_cache_begin + m_selected_file; };
+		inline uint16_t getListLenght() { return m_list_length; };
+		inline bool getFolderIsRoot() { return m_folder_is_root; };
+		inline char * getDirectoryName() { return m_directory; };
+		inline bool showingFirstItem() { return !m_window_min; };
+		inline bool maxDirectoryReached() { return (m_directory_depth == MAX_DIR_DEPTH-1); };
+		
+	private:
+		void changeDir();
+		void updateDirectoryName();
+
+	public:
+		//cache to show, based on partial iterators to cache
+		const cache_entry * window_cache_begin;
+		const cache_entry * window_cache_end;
+		
+	private:
+		cache_entry m_cache[SD_CACHE_SIZE]; //MAX_CACHE_SIZE
+		bool m_cache_update;
+		uint16_t m_index;
+		uint16_t m_list_length;
+		uint8_t m_selected_file;
+
+		char m_directory[20];
+		bool m_folder_is_root;
+		uint8_t	m_directory_depth;
+		
+		uint8_t m_cache_size;
+		uint16_t m_cache_min;
+		uint16_t m_cache_max;
+		uint8_t m_window_size;
+		uint16_t m_window_min;
+		uint16_t m_window_max;
+};
+
+#endif //SD_CACHE_H
