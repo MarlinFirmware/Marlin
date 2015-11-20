e26bf72e990b284e57a05220da876534d432c84e
Ruy García <ruy.garcia@bq.com>
2015-11-20 11:50:47 +0100

    Create headers for all files developed by BQ.

--- Marlin/AutoLevelManager.h
+++ Marlin/AutoLevelManager.h
@@ -1,3 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file AutoLevelManager.h
+///
+/// \author Ivan Galvez Junquera
+///         Ruy Garcia
+///         Victor Andueza
+///         Joaquin Herrero
+///
+/// \brief Management class for the state of the autolevel setting.
+///
+/// Copyright (c) 2015 BQ - Mundo Reader S.L.
+/// http://www.bq.com
+///
+/// This file is free software; you can redistribute it and/or modify
+/// it under the terms of either the GNU General Public License version 2 or
+/// later or the GNU Lesser General Public License version 2.1 or later, both
+/// as published by the Free Software Foundation.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef AUTOLEVEL_MANAGER_H
 #define AUTOLEVEL_MANAGER_H
 
--- Marlin/StepperClass.h
+++ Marlin/StepperClass.h
@@ -1,5 +1,29 @@
-#ifndef STEPPER_H
-#define STEPPER_H
+///////////////////////////////////////////////////////////////////////////////
+/// \file StepperClass.h
+///
+/// \author Ruy Garcia
+///
+/// \brief Abstraction class for steppers.
+///
+/// Copyright (c) 2015 BQ - Mundo Reader S.L.
+/// http://www.bq.com
+///
+/// This file is free software; you can redistribute it and/or modify
+/// it under the terms of either the GNU General Public License version 2 or
+/// later or the GNU Lesser General Public License version 2.1 or later, both
+/// as published by the Free Software Foundation.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef STEPPER_CLASS_H
+#define STEPPER_CLASS_H
 
 #include <stdint.h>
 
@@ -22,4 +46,4 @@ class Stepper
 		bool m_enable_polarity;
 };
 
-#endif //STEPPER_H
+#endif //STEPPER_CLASS_H
