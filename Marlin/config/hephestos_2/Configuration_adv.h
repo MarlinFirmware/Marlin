3a6a68617725d9d2753767c22afd64415385b4ba
Joaquín Herrero Herrero <joaquin.herrero@bq.com>
2015-11-30 16:16:18 +0100

    Disabled "SLOWDOWN" option on 2º gen.
    This options produces a noticeable drop in speed causing the same effect as a stop by an empty buffer.

--- Marlin/config/hephestos_2/Configuration_adv.h
+++ Marlin/config/hephestos_2/Configuration_adv.h
@@ -254,7 +254,7 @@
 #define DEFAULT_MINSEGMENTTIME        20000
 
 // If defined the movements slow down when the look ahead buffer is only half full
-#define SLOWDOWN
+//#define SLOWDOWN
 
 // Frequency limit
 // See nophead's blog for more info
