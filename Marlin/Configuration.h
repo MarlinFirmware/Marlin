f4b600628b5edcb4cf68e9bab02d38f71c38fcee
woodencase01 <philippecarrier@hotmail.ca>
2016-01-12 14:26:21 -0500

    Modified extruder steps per mm for DyzeXtruder
    Modified to the right values with microstep settings.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -479,7 +479,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200.0*8/3,760*1.1}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200.0*8/3,250}  // Setting for DyzeXtruder, 16 microstepping
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
