8ea55323d86caeeca504928ae19d4bfd888d14c5
unknown <bdrake@MTI114-LT.mainstream-tech.com>
2016-02-15 14:52:04 -0600

    Fix for not raising Z before homing X and Y

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2308,7 +2308,8 @@ inline void gcode_G28() {
           }
         #endif
 
-      #elif DISABLED(Z_SAFE_HOMING) && defined(Z_RAISE_BEFORE_HOMING) && Z_RAISE_BEFORE_HOMING > 0
+      #elif defined(Z_RAISE_BEFORE_HOMING) && Z_RAISE_BEFORE_HOMING > 0
+//      #elif DISABLED(Z_SAFE_HOMING) && defined(Z_RAISE_BEFORE_HOMING) && Z_RAISE_BEFORE_HOMING > 0
 
         // Raise Z before homing any other axes
         // (Does this need to be "negative home direction?" Why not just use Z_RAISE_BEFORE_HOMING?)
