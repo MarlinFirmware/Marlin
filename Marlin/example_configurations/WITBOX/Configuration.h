36ef671f2d21bfeb9f0845cea1f55c475b562736
Scott Lahteine <sourcetree@thinkyhead.com>
2016-02-28 23:23:17 -0800

    Update configurations to use RAMPS 1.4

--- Marlin/example_configurations/WITBOX/Configuration.h
+++ Marlin/example_configurations/WITBOX/Configuration.h
@@ -70,7 +70,7 @@ Here are some standard links for getting your machine calibrated:
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_13_EFB
+  #define MOTHERBOARD BOARD_RAMPS_14_EFB
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
