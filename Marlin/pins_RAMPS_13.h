7e7c5b85859364c1149c68840b1a06bc6cc9653b
Scott Lahteine <sourcetree@thinkyhead.com>
2016-02-28 23:10:15 -0800

    Handle RAMPS 1.3 as the special case

---
+++ Marlin/pins_RAMPS_13.h
@@ -0,0 +1,16 @@
+/**
+ * Arduino Mega with RAMPS v1.3 pin assignments
+ *
+ * Applies to the following boards:
+ *
+ *  RAMPS_13_EFB (Extruder, Fan, Bed)
+ *  RAMPS_13_EEB (Extruder, Extruder, Bed)
+ *  RAMPS_13_EFF (Extruder, Fan, Fan)
+ *  RAMPS_13_EEF (Extruder, Extruder, Fan)
+ *  RAMPS_13_SF  (Spindle, Controller Fan)
+ * 
+ */
+
+#define IS_RAMPS_13
+
+#include "pins_RAMPS_14.h"
---
+++ Marlin/pins_RAMPS_13_EFB.h
@@ -0,0 +1,9 @@
+/**
+ * Arduino Mega with RAMPS v1.3 pin assignments
+ *
+ *  RAMPS_13_EFB (Extruder, Fan, Bed)
+ */
+
+#define IS_RAMPS_13
+
+#include "pins_RAMPS_14_EFB.h"
