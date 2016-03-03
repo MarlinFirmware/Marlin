02b131b552240887e5cd0964969ac8f61f5406e2
jbrazio <jbrazio@gmail.com>
2016-03-03 13:29:07 +0000

    Replaced stock BQ PID values with fine tuned ones generated using M303

--- Marlin/example_configurations/Hephestos_2/Configuration.h
+++ Marlin/example_configurations/Hephestos_2/Configuration.h
@@ -208,21 +208,15 @@ Here are some standard links for getting your machine calibrated:
   #define PID_INTEGRAL_DRIVE_MAX PID_MAX  //limit for the integral term
   #define K1 0.95 //smoothing factor within the PID
 
-  // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
-  // Ultimaker
-  #define  DEFAULT_Kp 10.7
-  #define  DEFAULT_Ki 0.45
-  #define  DEFAULT_Kd 3
+  // Tuned PID values using M303
+  #define  DEFAULT_Kp 18.92
+  #define  DEFAULT_Ki 1.37
+  #define  DEFAULT_Kd 65.57
 
-  // MakerGear
-  //#define  DEFAULT_Kp 7.0
-  //#define  DEFAULT_Ki 0.1
-  //#define  DEFAULT_Kd 12
-
-  // Mendel Parts V9 on 12V
-  //#define  DEFAULT_Kp 63.0
-  //#define  DEFAULT_Ki 2.25
-  //#define  DEFAULT_Kd 440
+  // BQ firmware stock PID values
+  //#define  DEFAULT_Kp 10.7
+  //#define  DEFAULT_Ki 0.45
+  //#define  DEFAULT_Kd 3
 
 #endif // PIDTEMP
 
