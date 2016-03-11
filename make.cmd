2b5f62607d90252d67c1dfd1d42057b2d8d7b778
Koldo de Miguel <koldo.demiguel@bq.com>
2016-03-11 14:19:07 +0100

    make.cmd searches for arduino in c:/arduino

--- make.cmd
+++ make.cmd
@@ -11,21 +11,13 @@ cls
 :: variables
 SET me=%~n0
 
-IF NOT DEFINED ProgramFiles(x86) (
-	SET "program_files=%ProgramFiles%"
-) ELSE (
-	SET "program_files=%ProgramFiles(x86)%"
-)
-ECHO %program_files%
-
 SET "arduino_path=%program_files%\Arduino"
-ECHO %arduino_path%
 
 IF EXIST "%arduino_path%" (
 	ECHO Arduino installation found: "%arduino_path%"
 ) ELSE (
-	ECHO Arduino installation not found.
-	@EXIT /b 1
+	ECHO Arduino installation not found in "%arduino_path%".
+	goto exit
 )
 
 SET "avr_tools=%arduino_path%\hardware\tools\avr\bin"
