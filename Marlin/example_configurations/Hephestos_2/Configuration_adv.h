c14271d5e5924093666570e0219bffce51a15bf3
jbrazio <jbrazio@gmail.com>
2016-03-21 18:20:29 +0000

    Update the Hephestos 2 default config

--- Marlin/example_configurations/Hephestos_2/Configuration_adv.h
+++ Marlin/example_configurations/Hephestos_2/Configuration_adv.h
@@ -63,7 +63,7 @@
   // if Kc is chosen well, the additional required power due to increased melting should be compensated.
   #define PID_ADD_EXTRUSION_RATE
   #if ENABLED(PID_ADD_EXTRUSION_RATE)
-    #define DEFAULT_Kc (1) //heating power=Kc*(e_speed)
+    #define DEFAULT_Kc (100) //heating power=Kc*(e_speed)
     #define LPQ_MAX_LEN 50
   #endif
 #endif
@@ -278,7 +278,7 @@
 #define DEFAULT_MINSEGMENTTIME        20000
 
 // If defined the movements slow down when the look ahead buffer is only half full
-//#define SLOWDOWN
+#define SLOWDOWN
 
 // Frequency limit
 // See nophead's blog for more info
