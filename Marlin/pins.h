21de5f9ff5a0b3d8fc6387cc3dfbd1dec9823498
João Brázio <jbrazio@gmail.com>
2016-04-02 09:28:58 +0100

    Fixes #3313 : Wrong board def in pins.h

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -151,7 +151,7 @@
   #include "pins_AJ4P.h"
 #elif MB(MKS_13)
   #include "pins_MKS_13.h"
-#elif MB(BOARD_SAINSMART_2IN1)
+#elif MB(SAINSMART_2IN1)
   #include "pins_SAINSMART_2IN1.h"
 #else
   #error Unknown MOTHERBOARD value set in Configuration.h
