04b2abb6aa4723a1cd3e262a12f6b7b409fb8719
João Brázio <jbrazio@gmail.com>
2016-04-02 06:50:02 +0100

    Fixes #3312: Missing all icons from status screen

--- Marlin/dogm_lcd_implementation.h
+++ Marlin/dogm_lcd_implementation.h
@@ -306,10 +306,14 @@ static void lcd_implementation_status_screen() {
 
   bool blink = lcd_blink();
 
-  #if HAS_FAN0
-    // Symbols menu graphics, animated fan
-    u8g.drawBitmapP(9, 1, STATUS_SCREENBYTEWIDTH, STATUS_SCREENHEIGHT, blink && fanSpeeds[0] ? status_screen0_bmp : status_screen1_bmp);
-  #endif
+  // Symbols menu graphics, animated fan
+  u8g.drawBitmapP(9, 1, STATUS_SCREENBYTEWIDTH, STATUS_SCREENHEIGHT,
+    #if HAS_FAN0
+      blink && fanSpeeds[0] ? status_screen0_bmp : status_screen1_bmp
+    #else
+      status_screen0_bmp
+    #endif
+  );
 
   #if ENABLED(SDSUPPORT)
     // SD Card Symbol
