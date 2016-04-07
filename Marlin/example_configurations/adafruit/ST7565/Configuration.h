3ba26a29955f08a58d1234f3f603ad18c39fa4f9
Scott Lahteine <sourcetree@thinkyhead.com>
2016-04-06 17:30:23 -0700

    Add DEFAULT_KEEPALIVE_INTERVAL setting

--- Marlin/example_configurations/adafruit/ST7565/Configuration.h
+++ Marlin/example_configurations/adafruit/ST7565/Configuration.h
@@ -721,9 +721,12 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 // Host Keepalive
 //
 // By default Marlin will send a busy status message to the host
-// every 10 seconds when it can't accept commands.
+// every couple of seconds when it can't accept commands.
 //
 //#define DISABLE_HOST_KEEPALIVE // Enable this option if your host doesn't like keepalive messages.
+#if DISABLED(DISABLE_HOST_KEEPALIVE)
+  #define DEFAULT_KEEPALIVE_INTERVAL 2 // Number of seconds between "busy" messages. Set with M113.
+#endif
 
 //
 // M100 Free Memory Watcher
