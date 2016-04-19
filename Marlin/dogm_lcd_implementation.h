787d788533611e21ee34542fdba1c2df74722c9c
Scott Lahteine <sourcetree@thinkyhead.com>
2016-04-18 19:34:59 -0700

    Reduce heater status code if no bed

--- Marlin/dogm_lcd_implementation.h
+++ Marlin/dogm_lcd_implementation.h
@@ -287,9 +287,11 @@ FORCE_INLINE void _draw_centered_temp(int temp, int x, int y) {
 }
 
 FORCE_INLINE void _draw_heater_status(int x, int heater) {
-  bool isBed = heater < 0;
-
-  lcd_setFont(FONT_STATUSMENU);
+  #if HAS_TEMP_BED
+    bool isBed = heater < 0;
+  #else
+    const bool isBed = false;
+  #endif
 
   _draw_centered_temp((isBed ? degTargetBed() : degTargetHotend(heater)) + 0.5, x, 7);
 
@@ -351,8 +353,10 @@ static void lcd_implementation_status_screen() {
   // Extruders
   for (int i = 0; i < EXTRUDERS; i++) _draw_heater_status(5 + i * 25, i);
 
-  // Heatbed
-  if (EXTRUDERS < 4) _draw_heater_status(81, -1);
+  // Heated bed
+  #if EXTRUDERS < 4 && HAS_TEMP_BED
+    _draw_heater_status(81, -1);
+  #endif
 
   // Fan
   lcd_setFont(FONT_STATUSMENU);
