7928969f28aa6c99ad5542491da5222a1d4487a3
Scott Lahteine <sourcetree@thinkyhead.com>
2016-04-18 18:01:08 -0700

    Fix Witbox endstop configuration

--- Marlin/example_configurations/WITBOX/Configuration.h
+++ Marlin/example_configurations/WITBOX/Configuration.h
@@ -352,11 +352,11 @@
 // Specify here all the endstop connectors that are connected to any endstop or probe.
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
-#define USE_XMIN_PLUG
-#define USE_YMIN_PLUG
+//#define USE_XMIN_PLUG
+//#define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
-//#define USE_XMAX_PLUG
-//#define USE_YMAX_PLUG
+#define USE_XMAX_PLUG
+#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
 
 // coarse Endstop Settings
