01621fcdfc0f8824e24ad67b525b5dc7428e88c1
Torsten Kurbad <github@tk-webart.de>
2016-04-22 08:10:00 +0200

    Finetuning

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -1425,7 +1425,7 @@ static void setup_for_endstop_move() {
     #if ENABLED(DELTA)
       #if ENABLED(FSR_BED_LEVELING)
         // move down slowly until you find the bed
-        feedrate = 400;
+        feedrate = 600;
         float step = 0.05;
         int direction = -1;
         while (touching_print_surface()) {
