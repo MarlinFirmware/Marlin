2fc0c68a4a405df6421d8d2e69e1374c97d4a1e2
Koldo de Miguel <koldo.demiguel@bq.com>
2016-04-22 10:56:58 +0200

    rest position updated for hephestos zum printers

--- Marlin/config/hephestos_zum/Configuration_adv.h
+++ Marlin/config/hephestos_zum/Configuration_adv.h
@@ -96,8 +96,8 @@
 //===========================================================================
 
 // Rest Position
-#define POSITION_REST_X 0.0
-#define POSITION_REST_Y 210.0
+#define POSITION_REST_X 10.0
+#define POSITION_REST_Y 10.0
 #define POSITION_REST_Z  15.0
 
 // Load/Unload Filament Position
