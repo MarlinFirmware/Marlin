571c8bab882f8514faa1ae00f0f08987da4f18a2
Torsten Kurbad <github@tk-webart.de>
2016-04-24 15:09:34 +0200

    Tweak Z-Probe parameters

--- Marlin/example_configurations/delta/deltaprintr_ks_rg/e3dv6_pt100+heatbed+fsrboard/Configuration.h
+++ Marlin/example_configurations/delta/deltaprintr_ks_rg/e3dv6_pt100+heatbed+fsrboard/Configuration.h
@@ -573,7 +573,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
 
 #define AUTO_BED_LEVELING_FEATURE // Delete the comment to enable (remove // at the start of the line)
 #define DEBUG_LEVELING_FEATURE
-//#define Z_MIN_PROBE_REPEATABILITY_TEST  // If not commented out, Z Probe Repeatability test will be included if Auto Bed Leveling is Enabled.
+#define Z_MIN_PROBE_REPEATABILITY_TEST  // If not commented out, Z Probe Repeatability test will be included if Auto Bed Leveling is Enabled.
 
 #if ENABLED(AUTO_BED_LEVELING_FEATURE)
 
@@ -641,7 +641,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
   //  (0,0)
   #define X_PROBE_OFFSET_FROM_EXTRUDER 0.0   // X offset: -left  [of the nozzle] +right
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 0.0   // Y offset: -front [of the nozzle] +behind
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER 0.125  // Z offset: -below [the nozzle] (increase if the first layer is too thin)
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER 0.50  // Z offset: -below [the nozzle] (increase if the first layer is too thin)
 
   #define XY_TRAVEL_SPEED 8000         // X and Y axis travel speed between probes, in mm/min.
 
