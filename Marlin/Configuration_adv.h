1120eca6a48562219a9683c800e092ed03d92650
gralco <kuzmenko@alephobjects.com>
2016-04-25 13:04:59 -0600

    It's okay for WATCH_TEMP_PERIOD to be above 32 since watch_heater_next_ms is now defined as millis_t and millis_t is typedef'ed as an unsigned long not int (WATCH_TEMP_PERIOD's limit is therefore 4294967s).

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -73,7 +73,7 @@
    * but only if the current temperature is far enough below the target for a reliable test.
    *
    * If you get false positives for "Heating failed" increase WATCH_TEMP_PERIOD and/or decrease WATCH_TEMP_INCREASE
-   * WATCH_TEMP_INCREASE should not be below 2 and WATCH_TEMP_PERIOD should not be above 32.
+   * WATCH_TEMP_INCREASE should not be below 2.
    */
   #define WATCH_TEMP_PERIOD 20                // Seconds
   #define WATCH_TEMP_INCREASE 2               // Degrees Celsius
