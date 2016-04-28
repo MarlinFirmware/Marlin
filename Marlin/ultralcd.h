eb10218d10935edd708de2d71996ff55a141f9f1
Petr Zahradnik <clexpert@clexpert.cz>
2016-04-28 20:41:47 +0200

    Message enum for M600 filament change correct

--- Marlin/ultralcd.h
+++ Marlin/ultralcd.h
@@ -67,14 +67,14 @@
     extern volatile uint8_t buttons;  //the last checked buttons in a bit array.
     #if ENABLED(FILAMENT_CHANGE_ENABLE)
       enum FilamentChangeMessage {
-        FILAMENT_CHANGE_MESSAGE_INIT = 0,
-        FILAMENT_CHANGE_MESSAGE_UNLOAD = 1,
-        FILAMENT_CHANGE_MESSAGE_INSERT = 2,
-        FILAMENT_CHANGE_MESSAGE_LOAD = 3,
-        FILAMENT_CHANGE_MESSAGE_EXTRUDE = 4,
-        FILAMENT_CHANGE_MESSAGE_OPTION = 5,
-        FILAMENT_CHANGE_MESSAGE_RESUME = 6,
-        FILAMENT_CHANGE_MESSAGE_STATUS = 7
+        FILAMENT_CHANGE_MESSAGE_INIT,
+        FILAMENT_CHANGE_MESSAGE_UNLOAD,
+        FILAMENT_CHANGE_MESSAGE_INSERT,
+        FILAMENT_CHANGE_MESSAGE_LOAD,
+        FILAMENT_CHANGE_MESSAGE_EXTRUDE,
+        FILAMENT_CHANGE_MESSAGE_OPTION,
+        FILAMENT_CHANGE_MESSAGE_RESUME,
+        FILAMENT_CHANGE_MESSAGE_STATUS
       };
       void lcd_filament_change_show_message(FilamentChangeMessage message);
     #endif     
