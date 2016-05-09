56dd31ea3c1ce41c7b98cc7a7fa53742ec446afe
Scott Lahteine <sourcetree@thinkyhead.com>
2016-05-09 13:12:31 -0700

    Use code_value_long for potentially large values
    Addressing #3711

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -3797,7 +3797,7 @@ inline void gcode_M17() {
    */
   inline void gcode_M26() {
     if (card.cardOK && code_seen('S'))
-      card.setIndex(code_value_short());
+      card.setIndex(code_value_long());
   }
 
   /**
@@ -3869,7 +3869,7 @@ inline void gcode_M31() {
       card.openFile(namestartpos, true, call_procedure);
 
       if (code_seen('S') && seen_pointer < namestartpos) // "S" (must occur _before_ the filename!)
-        card.setIndex(code_value_short());
+        card.setIndex(code_value_long());
 
       card.startFileprint();
 
