ea37239b20f9fecce0d43ab292c54a821ae637ce
Torsten Kurbad <github@tk-webart.de>
2016-05-19 11:27:15 +0200

    Negated TOWER TRIM values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -401,9 +401,9 @@
   #define DELTA_PRINTABLE_RADIUS 127.0
 
   // Tower offsets
-  #define DELTA_DIAGONAL_ROD_TRIM_TOWER_1 -2.775    // X
-  #define DELTA_DIAGONAL_ROD_TRIM_TOWER_2 -2.775    // Y
-  #define DELTA_DIAGONAL_ROD_TRIM_TOWER_1 -1.142    // Z
+  #define DELTA_DIAGONAL_ROD_TRIM_TOWER_1 2.775    // X
+  #define DELTA_DIAGONAL_ROD_TRIM_TOWER_2 2.775    // Y
+  #define DELTA_DIAGONAL_ROD_TRIM_TOWER_1 1.142    // Z
 
 #endif // DELTA
 
