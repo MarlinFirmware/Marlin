2207001333f362aba3b838d0681203ed9844cfcc
AnHardt <github@kitelab.de>
2016-05-19 20:52:11 +0200

    No compromises for the manage_heater()
    No compromises for the manage_heater(). manage_heater() will return immediately when there is nothing to do, but needs a constant detaT to work proper.
    Calling idle() only every 200ms results in a display update every ~2 seconds - that should be enough.
    For the other functionalities in idle() and manage_inactivity() 200ms is a lot but hopefully works.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -448,11 +448,11 @@
 // @section extras
 
 // Arc interpretation settings:
-#define ARC_SUPPORT  // Disabling this saves ~2660bytes
+#define ARC_SUPPORT  // Disabling this saves ~2738 bytes
 #define MM_PER_ARC_SEGMENT 1
 #define N_ARC_CORRECTION 25
 
-// Support for G5 with XYZE destination and IJPQ offsets
+// Support for G5 with XYZE destination and IJPQ offsets. Needs ~2666 bytes
 //#define BEZIER_CURVE_SUPPORT
 
 const unsigned int dropsegments = 5; //everything with less than this number of steps will be ignored as move and joined with the next movement
