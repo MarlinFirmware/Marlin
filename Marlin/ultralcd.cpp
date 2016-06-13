4c141b258661df639175db26b5692e2b95948511
edwilliams16 <ed@edwilliams.org>
2016-06-13 07:37:27 -1000

    Update ultralcd.cpp
    Reduce FLOP count and improve rounding. Reset initial distance done when changing axes.

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -575,19 +575,18 @@ static void lcd_status_screen() {
 
   #if ENABLED(BABYSTEPPING)
 
-    int babysteps_done = 0;
+    long babysteps_done = 0;
     float distance_done = 0.0f;
 
     static void _lcd_babystep(const AxisEnum axis, const char* msg) {
       ENCODER_DIRECTION_NORMAL();
       if (encoderPosition) {
         int babystep_increment = (int32_t)encoderPosition * BABYSTEP_MULTIPLICATOR;
-        float distance_increment = (float) babystep_increment / (float) planner.axis_steps_per_mm[axis];
         encoderPosition = 0;
         lcdDrawUpdate = LCDVIEW_REDRAW_NOW;
         thermalManager.babystep_axis(axis, babystep_increment);
         babysteps_done += babystep_increment;
-        distance_done += distance_increment;
+        distance_done = (float) ( (1000 * babysteps_done) / planner.axis_steps_per_mm[axis] ) / 1000.0f;// 1000 to print 3 Dec Places
       }
       if (lcdDrawUpdate) lcd_implementation_drawedit(msg, ftostr43sign(distance_done));
       if (LCD_CLICKED) lcd_goto_previous_menu(true);
@@ -596,11 +595,11 @@ static void lcd_status_screen() {
     #if ENABLED(BABYSTEP_XY)
       static void _lcd_babystep_x() { _lcd_babystep(X_AXIS, PSTR(MSG_BABYSTEPPING_X)); }
       static void _lcd_babystep_y() { _lcd_babystep(Y_AXIS, PSTR(MSG_BABYSTEPPING_Y)); }
-      static void lcd_babystep_x() { babysteps_done = 0; lcd_goto_screen(_lcd_babystep_x); }
-      static void lcd_babystep_y() { babysteps_done = 0; lcd_goto_screen(_lcd_babystep_y); }
+      static void lcd_babystep_x() { babysteps_done = 0; distance_done = 0.0f; lcd_goto_screen(_lcd_babystep_x); }
+      static void lcd_babystep_y() { babysteps_done = 0; distance_done = 0.0f; lcd_goto_screen(_lcd_babystep_y); }
     #endif
     static void _lcd_babystep_z() { _lcd_babystep(Z_AXIS, PSTR(MSG_BABYSTEPPING_Z)); }
-    static void lcd_babystep_z() { babysteps_done = 0; lcd_goto_screen(_lcd_babystep_z); }
+    static void lcd_babystep_z() { babysteps_done = 0; distance_done = 0.0f; lcd_goto_screen(_lcd_babystep_z); }
 
   #endif //BABYSTEPPING
 
