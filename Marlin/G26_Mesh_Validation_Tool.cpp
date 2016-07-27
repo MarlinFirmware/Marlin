131786c9e19d5c7db79ef1d140718d979006ccd8
Roxy-3D <Roxy-3D@users.noreply.github.com>
2016-07-27 11:12:54 -0500

    Update G26_Mesh_Validation_Tool.cpp

--- Marlin/G26_Mesh_Validation_Tool.cpp
+++ Marlin/G26_Mesh_Validation_Tool.cpp
@@ -50,7 +50,7 @@
 // L #  Layer		Layer height.  (Height of nozzle above bed)  If not specified .20mm will be used.
 //
 // M #  Multiplier	Retraction Multiplier.   Normally not needed.  Retraction defaults to 1.0mm and 
-			un-retraction is at 1.2mm   These numbers will be scaled by the specified amount
+//			un-retraction is at 1.2mm   These numbers will be scaled by the specified amount
 //
 // N #  Nozzle		Used to control the size of nozzle diameter.  If not specified, a .4mm nozzle is assumed.
 //
