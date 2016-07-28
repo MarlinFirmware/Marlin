f92b6a8a7cdf021fd5a08d930fb29d7cac855413
fabtopia <fabtopia@users.noreply.github.com>
2016-07-28 16:59:41 +0200

    Callibrated E steps per unit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -760,7 +760,7 @@
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {2*80,2*80,2*3200,300}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {2*80,2*80,2*3200,274}
 #define DEFAULT_MAX_FEEDRATE          {300, 300, 2.5, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {3000,3000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
