c8488779a80b68999b20fc5a829cde0614c37cfd
Jan Grewe <jan@faked.org>
2016-08-09 01:56:01 +0200

    Disable TEMP_SENSOR_BED to keep people from shooting themselves in the foot

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -208,7 +208,7 @@
 // extruder temperature is above/below EXTRUDER_AUTO_FAN_TEMPERATURE.
 // Multiple extruders can be assigned to the same pin in which case
 // the fan will turn on when any selected extruder is above the threshold.
-#define EXTRUDER_0_AUTO_FAN_PIN 9
+#define EXTRUDER_0_AUTO_FAN_PIN RAMPS_D9_PIN
 #define EXTRUDER_1_AUTO_FAN_PIN -1
 #define EXTRUDER_2_AUTO_FAN_PIN -1
 #define EXTRUDER_3_AUTO_FAN_PIN -1
