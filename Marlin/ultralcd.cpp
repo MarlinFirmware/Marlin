61e35eaf3c43b35fc08b787e479860a1b6f4b669
Scott Lahteine <sourcetree@thinkyhead.com>
2016-08-08 20:53:18 -0700

    Fix: LCD displays SD status at startup

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -2550,7 +2550,6 @@ void lcd_update() {
 
     bool sd_status = IS_SD_INSERTED;
     if (sd_status != lcd_sd_status && lcd_detected()) {
-      lcd_sd_status = sd_status;
 
       if (sd_status) {
         card.initsd();
@@ -2561,6 +2560,7 @@ void lcd_update() {
         if (lcd_sd_status != 2) LCD_MESSAGEPGM(MSG_SD_REMOVED);
       }
 
+      lcd_sd_status = sd_status;
       lcdDrawUpdate = LCDVIEW_CLEAR_CALL_REDRAW;
       lcd_implementation_init( // to maybe revive the LCD if static electricity killed it.
         #if ENABLED(LCD_PROGRESS_BAR)
