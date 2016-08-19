c634823311e0b7daf011c4254dec1bbf014877f3
Scott Lahteine <sourcetree@thinkyhead.com>
2016-08-19 05:05:48 -0500

    Do Delta radius check without sqrt

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -3667,8 +3667,7 @@ inline void gcode_G28() {
 
           #if ENABLED(DELTA)
             // Avoid probing outside the round or hexagonal area of a delta printer
-            float distance_from_center = HYPOT(xProbe, yProbe);
-            if (distance_from_center > DELTA_PROBEABLE_RADIUS) continue;
+            if (sq(xProbe) + sq(yProbe) > sq(DELTA_PROBEABLE_RADIUS)) continue;
           #endif
 
           float measured_z = probe_pt(xProbe, yProbe, stow_probe_after_each, verbose_level);
