de19d9e157cb1361946336a8d1ff2e78d4d88b2f
Roxy-3D <Roxy-3D@users.noreply.github.com>
2016-09-17 18:58:06 -0500

    z_offset fix along with numerous other changes
    Also, the mesh_buffer_line() routine has been rewritten to eliminate the recursive nature it had.   The reason is for lines that cross a Mesh Line Boundary, many floating point operations were cut out of the calculations.
    
    This file has many of the higher speed support routines in that replace the use of calc_z()

--- Marlin/Bed_Leveling.cpp
+++ Marlin/Bed_Leveling.cpp
@@ -24,33 +24,62 @@
 #include "math.h"
 
 #if ENABLED(UNIFIED_BED_LEVELING_FEATURE)
-//#include "Bed_Leveling.h"
+#include "Bed_Leveling.h"
+
+
+// These variables used to be declared inside the bed_leveling class.  We are going to still declare
+// them within the .cpp file for bed leveling.   But there is only one instance of the bed leveling
+// object and we can get rid of a level of inderection by not making them 'member data'.  So, in the
+// interest of speed, we do it this way.    When we move to a 32-Bit processor, they can be moved 
+// back inside the bed leveling class.
+
+float last_specified_z;
+float fade_scaling_factor_for_current_height;
+float z_values[MESH_NUM_X_POINTS][MESH_NUM_Y_POINTS];
+float mesh_index_to_X_location[MESH_NUM_X_POINTS+1];	// +1 just because of paranoia that we might end up on the
+float mesh_index_to_Y_location[MESH_NUM_Y_POINTS+1];	// the last Mesh Line and that is the start of a whole new cell
+
+
 
 bed_leveling::bed_leveling()    {
+	int i;
+	for (i = 0; i<=MESH_NUM_X_POINTS; i++)	// We go one past what we expect to ever need for safety
+		mesh_index_to_X_location[i] = ((double)MESH_MIN_X) + (((double)MESH_X_DIST) * ((double)i));
+	for (i = 0; i<=MESH_NUM_Y_POINTS; i++)	// We go one past what we expect to ever need for safety
+		mesh_index_to_Y_location[i] = ((double)MESH_MIN_Y) + (((double)MESH_Y_DIST) * ((double)i));
+
 	this->reset();
 }
 
+
 void bed_leveling::store_state()    {
 int k;
-	k = E2END - sizeof( bed_leveling_mesh.state );
-	eeprom_write_block( (void *) &bed_leveling_mesh.state , (void *) k, sizeof(bed_leveling_mesh.state) );
+	k = E2END - sizeof( blm.state );
+	eeprom_write_block( (void *) &blm.state , (void *) k, sizeof(blm.state) );
 	return;
 }
 
 void bed_leveling::load_state()    {
 int k;
-	k = E2END - sizeof( bed_leveling_mesh.state );
-	eeprom_read_block( (void *) &bed_leveling_mesh.state , (void *) k, sizeof(bed_leveling_mesh.state) );
+	k = E2END - sizeof( blm.state );
+	eeprom_read_block( (void *) &blm.state , (void *) k, sizeof(blm.state) );
 	if ( this->sanity_check() != 0 ) {
 	   SERIAL_PROTOCOLLNPGM("?In load_state() sanity_check() failed. \n");
 	}
+												// These lines can go away in a few weeks.  They are just 
+												// to make sure people updating thier firmware won't be using
+	if (blm.state.G29_Fade_Height_Multiplier != 1.0/blm.state.G29_Correction_Fade_Height) {	// an incomplete Bed_Leveling.state structure.   For speed 
+		blm.state.G29_Fade_Height_Multiplier = 1.0 / blm.state.G29_Correction_Fade_Height;// we now multiply by the inverse of the Fade Height instead of 
+		store_state();									// dividing by it.   Soon... all of the old structures will be 
+	}											// updated, but until then, we try to ease the transition 
+												// for our Beta testers.
 	return;
 }
 
 void bed_leveling::load_mesh(int m) {
 int j, k;
-	k = E2END - sizeof( bed_leveling_mesh.state );
-	j = (k - Unified_Bed_Leveling_EEPROM_start) / sizeof( bed_leveling_mesh.z_values );
+	k = E2END - sizeof( blm.state );
+	j = (k - Unified_Bed_Leveling_EEPROM_start) / sizeof( z_values );
 
 	if ( m == -1 ) {
 		SERIAL_PROTOCOLLNPGM("?No mesh saved in EEPROM.  Zeroing mesh in memory.\n");
@@ -58,16 +87,17 @@ int j, k;
 		return;
 	}
 
-	if ( m<0 || m>j || Unified_Bed_Leveling_EEPROM_start<=0) {
+	if ( m<0 || m>=j || Unified_Bed_Leveling_EEPROM_start<=0) {
 		SERIAL_PROTOCOLLNPGM("?EEPROM storage not available to load mesh.\n");
 		return;
 	}
 
-	j = k-(m+1)*sizeof(bed_leveling_mesh.z_values);	
-	eeprom_read_block( (void *) &bed_leveling_mesh.z_values , (void *) j, sizeof(bed_leveling_mesh.z_values) );
+	j = k-(m+1)*sizeof(z_values);	
+	eeprom_read_block( (void *) &z_values , (void *) j, sizeof(z_values) );
 
-	SERIAL_ECHOPAIR("Mesh ", m);
-	SERIAL_PROTOCOLPGM(" loaded from offset 0x");
+	SERIAL_PROTOCOLPGM("Mesh loaded from slot ");
+	SERIAL_PROTOCOL( m );
+	SERIAL_PROTOCOLPGM("  at offset 0x");
 	prt_hex_word(j);
 	SERIAL_PROTOCOLPGM("\n");
 }
@@ -75,18 +105,27 @@ int j, k;
 void bed_leveling:: store_mesh(int m) {
 int j, k;
 
-	k = E2END - sizeof( bed_leveling_mesh.state );
-	j = (k - Unified_Bed_Leveling_EEPROM_start) / sizeof( bed_leveling_mesh.z_values );
+	k = E2END - sizeof( state );
+	j = (k - Unified_Bed_Leveling_EEPROM_start) / sizeof( z_values );
 
-	if ( m<0 || m>j || Unified_Bed_Leveling_EEPROM_start<=0) {
+	if ( m<0 || m>=j || Unified_Bed_Leveling_EEPROM_start<=0) {
 		SERIAL_PROTOCOLLNPGM("?EEPROM storage not available to load mesh.\n");
+SERIAL_PROTOCOL(m);
+SERIAL_PROTOCOL(" mesh slots available.\n"  );
+SERIAL_PROTOCOLPAIR("\nE2END     : ", E2END  );
+SERIAL_PROTOCOLPAIR("\nk         : ", k  );
+SERIAL_PROTOCOLPAIR("\nj         : ", j  );
+SERIAL_PROTOCOLPAIR("\nm         : ", m  );
+SERIAL_PROTOCOL("\n");
 		return;
 	}
 
-	j = k-(m+1)*sizeof(bed_leveling_mesh.z_values);	
-	eeprom_write_block( (const void *) &bed_leveling_mesh.z_values , (void *) j, sizeof(bed_leveling_mesh.z_values) );
+	j = k-(m+1)*sizeof( z_values);	
+	eeprom_write_block( (const void *) &z_values , (void *) j, sizeof(z_values) );
 
-	SERIAL_PROTOCOLPGM("Mesh saved at offset 0x");
+	SERIAL_PROTOCOLPGM("Mesh saved in slot ");
+	SERIAL_PROTOCOL( m );
+	SERIAL_PROTOCOLPGM("  at offset 0x");
 	prt_hex_word(j);
 	SERIAL_PROTOCOLPGM("\n");
 }
@@ -96,8 +135,10 @@ void bed_leveling::reset() {
     this->state.z_offset = 0;
     for (int x=0; x<MESH_NUM_X_POINTS; x++)
 	for (int y=0; y<MESH_NUM_Y_POINTS; y++)
-		this->z_values[x][y] = 0.0;
+		z_values[x][y] = 0.0;
 
+    last_specified_z = -999.9;				// We can't pre-initialize these values in the declaration 
+    fade_scaling_factor_for_current_height = 0.0;	// due to C++11 constraints  
     return;
 }
 
@@ -110,7 +151,7 @@ SERIAL_EOL;
     this->state.z_offset = 0;
     for (int x=0; x<MESH_NUM_X_POINTS; x++)
 	for (int y=0; y<MESH_NUM_Y_POINTS; y++)
-		this->z_values[x][y] = NAN;
+		z_values[x][y] = NAN;
 
     return;
 }
@@ -126,8 +167,8 @@ int i, j;
 	SERIAL_ECHOPAIR(",", MESH_NUM_Y_POINTS-1);
 	SERIAL_ECHO(")    ");
 
-	current_xi = bed_leveling_mesh.get_cell_index_x(current_position[X_AXIS] + MESH_X_DIST/2.0);
-	current_yi = bed_leveling_mesh.get_cell_index_y(current_position[Y_AXIS] + MESH_Y_DIST/2.0);
+	current_xi = blm.get_cell_index_x(current_position[X_AXIS] + MESH_X_DIST/2.0);
+	current_yi = blm.get_cell_index_y(current_position[Y_AXIS] + MESH_Y_DIST/2.0);
 
 	for(i=0; i<MESH_NUM_X_POINTS-1; i++)  {
 		SERIAL_ECHO("                 ");
@@ -153,7 +194,7 @@ int i, j;
 
 	for(j=MESH_NUM_Y_POINTS-1; j>=0; j--)  {
 		for(i=0; i<MESH_NUM_X_POINTS; i++)  {
-			f = this->z_values[i][j];
+			f = z_values[i][j];
 			if (i==current_xi && j==current_yi) 		// is the nozzle here?  if so, mark the number
 				SERIAL_PROTOCOL("[");
 			 else 
@@ -254,8 +295,8 @@ int bed_leveling::sanity_check() {
 	   error_flag++;
 	}
 
-	k = E2END - sizeof( bed_leveling_mesh.state );
-	j = (k - Unified_Bed_Leveling_EEPROM_start) / sizeof( bed_leveling_mesh.z_values );
+	k = E2END - sizeof( blm.state );
+	j = (k - Unified_Bed_Leveling_EEPROM_start) / sizeof( z_values );
 
 	if (j<1) {
 	  SERIAL_PROTOCOLLNPGM("?No EEPROM storage available for a mesh of this size.\n");
