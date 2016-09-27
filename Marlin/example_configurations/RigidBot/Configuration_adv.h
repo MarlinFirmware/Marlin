66e638df1a64127a9f9239059ec010fa3fbef424
esenapaj <creation0004@gmail.com>
2016-09-28 04:15:55 +0900

    Fix the PR #4899 (ABL: Enable by type. Bilinear for all.) etc.
    About Configuration.h:
    ・Fix the PR #4899 (ABL: Enable by type. Bilinear for all.)
    Remove Duplicated contents
    
    ・Fix the PR #4305 (Custom boot screen feature improvement)
    Revert from "during boot" to "during bootup" in all the example
    Configuration.h
    
    ・Fix the PR #4207 (Clean up, simplify and generalize the Allen-key-probe
    code.)
    Resolve and relocate the duplicated definitions in Z_PROBE_ALLEN_KEY
    section
    
    ・Follow-up the PR #4805 (Additional documentation of Configuration.h)
    Add forgotten changes to all the example Configuration.h
    
    Adjust spacing
    
    About Configuration_adv.h:
    Add missing description of SLOWDOWN for DELTA
    
    Adjust spacing

--- Marlin/example_configurations/RigidBot/Configuration_adv.h
+++ Marlin/example_configurations/RigidBot/Configuration_adv.h
@@ -553,7 +553,7 @@
 // The number of linear motions that can be in the plan at any give time.
 // THE BLOCK_BUFFER_SIZE NEEDS TO BE A POWER OF 2, i.g. 8,16,32 because shifts and ors are used to do the ring-buffering.
 #if ENABLED(SDSUPPORT)
-  #define BLOCK_BUFFER_SIZE 16   // SD,LCD,Buttons take more memory, block buffer needs to be smaller
+  #define BLOCK_BUFFER_SIZE 16 // SD,LCD,Buttons take more memory, block buffer needs to be smaller
 #else
   #define BLOCK_BUFFER_SIZE 16 // maximize block buffer
 #endif
