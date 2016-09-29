d019e3afd4250a8cb727e71f2e7346cc097f5871
Scott Lahteine <sourcetree@thinkyhead.com>
2016-09-29 02:17:33 -0500

    Apply changes to all configs

--- Marlin/example_configurations/delta/kossel_pro/Configuration.h
+++ Marlin/example_configurations/delta/kossel_pro/Configuration.h
@@ -287,7 +287,6 @@
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
   #define PID_FUNCTIONAL_RANGE 50 // If the temperature difference between the target temperature and the actual temperature
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
-  #define PID_INTEGRAL_DRIVE_MAX PID_MAX  //limit for the integral term
   #define K1 0.95 //smoothing factor within the PID
 
   // Kossel Pro
@@ -322,8 +321,6 @@
 #if ENABLED(PIDTEMPBED)
 
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
-
-  #define PID_BED_INTEGRAL_DRIVE_MAX MAX_BED_POWER //limit for the integral term
 
   //Kossel Pro heated bed plate with borosilicate glass
   //from pidautotune (M303 E-1 S60 C8)
