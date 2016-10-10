2e46b23c736ddfa3bd7cb0dc3e66f9530581c62c
esenapaj <creation0004@gmail.com>
2016-10-10 14:22:17 +0900

    Fix compilation errors

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -4690,7 +4690,7 @@ inline void gcode_M42() {
           pin_state[pin - first_pin] = digitalRead(pin);
       }
 
-      #if ENABLED(EMERGENCY_PARSER)
+      #if ENABLED(EMERGENCY_PARSER) && DISABLED(ULTIPANEL)
         wait_for_user = true;
       #endif
 
@@ -4708,7 +4708,7 @@ inline void gcode_M42() {
           }
         }
 
-        #if ENABLED(EMERGENCY_PARSER)
+        #if ENABLED(EMERGENCY_PARSER) && DISABLED(ULTIPANEL)
           if (!wait_for_user) break;
         #endif
 
