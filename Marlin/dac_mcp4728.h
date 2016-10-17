b80cc0cbb5957f2b29db514609adfbd251eeba0a
Scott Lahteine <sourcetree@thinkyhead.com>
2016-10-17 15:09:38 -0500

    Fix DAC current code

--- Marlin/dac_mcp4728.h
+++ Marlin/dac_mcp4728.h
@@ -60,7 +60,7 @@ uint16_t mcp4728_getValue(uint8_t channel);
 uint8_t mcp4728_fastWrite();
 uint8_t mcp4728_simpleCommand(byte simpleCommand);
 uint16_t mcp4728_getDrvPct(uint8_t channel);
-void mcp4728_setDrvPct(int16_t pct[XYZE]);
+void mcp4728_setDrvPct(uint16_t pct[XYZE]);
 
 #endif
 #endif // DAC_MCP4728_H
