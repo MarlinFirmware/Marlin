abbedadca39d850c889608dc95af6dce25ca1f2e
Your Correct Name <sourcetree@thinkyhead.com>
2016-11-07 04:08:25 -0600

    Patch spacing in bilinear grid report

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2385,7 +2385,7 @@ static void clean_up_after_endstop_or_probe_move() {
     }
     SERIAL_EOL;
     for (uint8_t y = 0; y < ABL_GRID_POINTS_Y; y++) {
-      if (y < 9) SERIAL_PROTOCOLCHAR(' ');
+      if (y < 10) SERIAL_PROTOCOLCHAR(' ');
       SERIAL_PROTOCOL((int)y);
       for (uint8_t x = 0; x < ABL_GRID_POINTS_X; x++) {
         SERIAL_PROTOCOLCHAR(' ');
