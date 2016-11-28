ee26c4e04aa721e43a34e4ca920b42466718a636
Iván Fernández <ifernandez@redsauce.net>
2016-11-28 17:40:59 +0100

    New machine name to reflect version

--- Marlin/example_configurations/ThingiBOX/IONv2/Configuration.h
+++ Marlin/example_configurations/ThingiBOX/IONv2/Configuration.h
@@ -126,7 +126,7 @@
 
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
-#define CUSTOM_MACHINE_NAME "Prusa i3 ION"
+#define CUSTOM_MACHINE_NAME "Thingibox IONv2"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
