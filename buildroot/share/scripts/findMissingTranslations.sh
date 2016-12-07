29bc83ead8c69b882a6a6c5dfc5d0de949553504
Scott Lahteine <sourcetree@thinkyhead.com>
2016-12-06 22:48:26 -0600

    Optimize findMissingTranslations.sh

--- buildroot/share/scripts/findMissingTranslations.sh
+++ buildroot/share/scripts/findMissingTranslations.sh
@@ -8,31 +8,33 @@
 #
 # If no language codes are specified then all languages will be checked
 #
-IGNORE_DEFINES="LANGUAGE_EN_H MAPPER_NON SIMULATE_ROMFONT DISPLAY_CHARSET_ISO10646_1 MSG_X MSG_Y MSG_Z MSG_E MSG_H1 MSG_H2 MSG_H3 MSG_H4 MSG_MOVE_E1 MSG_MOVE_E2 MSG_MOVE_E3 MSG_MOVE_E4 MSG_N1 MSG_N2 MSG_N3 MSG_N4 MSG_DIAM_E1 MSG_DIAM_E2 MSG_DIAM_E3 MSG_DIAM_E4 MSG_E1 MSG_E2 MSG_E3 MSG_E4"
 
 [ -d "Marlin" ] && cd "Marlin"
 
-LANG="$@"
-FILES=$(ls language_*.h | grep -v language_en.h | sed -E 's/language_([^\.]+)\.h/\1/')
+FILES=$(ls language_*.h | grep -v -E "(_en|_test)\.h" | sed -E 's/language_([^\.]+)\.h/\1/')
 declare -A STRING_MAP
+
+# Get files matching the given arguments
+TEST_LANGS=$FILES
+if [[ -n $@ ]]; then
+  TEST_LANGS=""
+  for K in "$@"; do
+    for F in $FILES; do
+      [[ "$F" != "${F%$K*}" ]] && TEST_LANGS="$TEST_LANGS $F"
+    done
+  done
+fi
 
 echo -n "Building list of missing strings..."
 
-for i in $(awk '/#define/{print $2}' language_en.h); do
+for i in $(awk '/#ifndef/{print $2}' language_en.h); do
+  [[ $i == "LANGUAGE_EN_H" ]] && continue
   LANG_LIST=""
-  for j in $FILES; do
-    [[ $j == "test" ]] && continue
-    [[ -n $LANG && ! "${j}" =~ $LANG ]] && continue
-    t=$(grep -c "define ${i} " language_${j}.h)
-    if [[ $t -eq 0 ]]; then
-      for k in ${IGNORE_DEFINES}; do
-        [[ $k == $i ]] && continue 2
-      done
-      LANG_LIST="$LANG_LIST $j"
-    fi
+  for j in $TEST_LANGS; do
+    [[ $(grep -c " ${i} " language_${j}.h) -eq 0 ]] && LANG_LIST="$LANG_LIST $j"
   done
   [[ -z $LANG_LIST ]] && continue
-  STRING_MAP["$i"]="$LANG_LIST"
+  STRING_MAP[$i]=$LANG_LIST
 done
 
 echo
