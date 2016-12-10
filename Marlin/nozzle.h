e188b680113bca4a2943323a4851b90fa70c1f50
Colin Gilgenbach <colin@gilgenbach.net>
2016-12-09 18:08:48 -0700

    Fixed variable naming

--- Marlin/nozzle.h
+++ Marlin/nozzle.h
@@ -29,8 +29,8 @@
 #if ENABLED(NOZZLE_CLEAN_FEATURE)
   constexpr float nozzle_clean_start_point[4] = NOZZLE_CLEAN_START_POINT,
                   nozzle_clean_end_point[4] = NOZZLE_CLEAN_END_POINT,
-                  nozzle_clean_length = fabs(NOZZLE_CLEAN_START_POINT[X_AXIS] - NOZZLE_CLEAN_END_POINT[X_AXIS]), //abs x size of wipe pad
-                  nozzle_clean_height = fabs(NOZZLE_CLEAN_START_POINT[Y_AXIS] - NOZZLE_CLEAN_END_POINT[Y_AXIS]); //abs y size of wipe pad
+                  nozzle_clean_length = fabs(nozzle_clean_start_point[X_AXIS] - nozzle_clean_end_point[X_AXIS]), //abs x size of wipe pad
+                  nozzle_clean_height = fabs(nozzle_clean_start_point[Y_AXIS] - nozzle_clean_end_point[Y_AXIS]); //abs y size of wipe pad
   constexpr bool NOZZLE_CLEAN_HORIZONTAL = nozzle_clean_length >= nozzle_clean_height; //whether to zig-zag horizontally or vertically
 #endif //NOZZLE_CLEAN_FEATURE
 
