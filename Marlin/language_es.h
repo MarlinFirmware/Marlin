c9660f58bc03f2274cfa55b875ef5ae5a8deac45
yhfudev <yhfudev@gmail.com>
2016-12-12 08:50:41 -0500

    fix multiple definitions of message macros in the language-es.h

--- Marlin/language_es.h
+++ Marlin/language_es.h
@@ -178,16 +178,19 @@
 #define MSG_INFO_EXTRUDERS                  "Extrusores"
 #define MSG_INFO_BAUDRATE                   "Baudios"
 #define MSG_INFO_PROTOCOL                   "Protocolo"
-#define MSG_INFO_PRINT_COUNT                "Conteo de impresion"
-#define MSG_INFO_COMPLETED_PRINTS           "Completadas"
-#define MSG_INFO_PRINT_TIME                 "Tiempo total de imp."
-#define MSG_INFO_PRINT_LONGEST              "Impresion mas larga"
-#define MSG_INFO_PRINT_FILAMENT             "Total de Extrusion"
-#define MSG_INFO_PRINT_COUNT                "Impresiones"
-#define MSG_INFO_COMPLETED_PRINTS           "Completadas"
-#define MSG_INFO_PRINT_TIME                 "Total"
-#define MSG_INFO_PRINT_LONGEST              "Mas larga"
-#define MSG_INFO_PRINT_FILAMENT             "Extrusion"
+#if LCD_WIDTH > 19
+  #define MSG_INFO_PRINT_COUNT              "Conteo de impresion"
+  #define MSG_INFO_COMPLETED_PRINTS         "Completadas"
+  #define MSG_INFO_PRINT_TIME               "Tiempo total de imp."
+  #define MSG_INFO_PRINT_LONGEST            "Impresion mas larga"
+  #define MSG_INFO_PRINT_FILAMENT           "Total de Extrusion"
+#else
+  #define MSG_INFO_PRINT_COUNT              "Impresiones"
+  #define MSG_INFO_COMPLETED_PRINTS         "Completadas"
+  #define MSG_INFO_PRINT_TIME               "Total"
+  #define MSG_INFO_PRINT_LONGEST            "Mas larga"
+  #define MSG_INFO_PRINT_FILAMENT           "Extrusion"
+#endif
 #define MSG_INFO_MIN_TEMP                   "Temperatura minima"
 #define MSG_INFO_MAX_TEMP                   "Temperatura maxima"
 #define MSG_INFO_PSU                        "Fuente de poder"
@@ -204,13 +207,13 @@
 
 #if LCD_HEIGHT >= 4
   // Up to 3 lines allowed
-  #define MSG_FILAMENT_CHANGE_INIT_2          "del filamento"
-  #define MSG_FILAMENT_CHANGE_INIT_3          "cambiar"
-  #define MSG_FILAMENT_CHANGE_INSERT_3        "para continuar..."
+  #define MSG_FILAMENT_CHANGE_INIT_2        "del filamento"
+  #define MSG_FILAMENT_CHANGE_INIT_3        "cambiar"
+  #define MSG_FILAMENT_CHANGE_INSERT_3      "para continuar..."
 #else // LCD_HEIGHT < 4
   // Up to 2 lines allowed
-  #define MSG_FILAMENT_CHANGE_INIT_2          "del fil. cambiar"
-  #define MSG_FILAMENT_CHANGE_INSERT_1        "Inserte filamento"
+  #define MSG_FILAMENT_CHANGE_INIT_2        "del fil. cambiar"
+  #define MSG_FILAMENT_CHANGE_INSERT_1      "Inserte filamento"
 #endif // LCD_HEIGHT < 4
 
 #define MSG_FILAMENT_CHANGE_UNLOAD_1        "Esperado por"
