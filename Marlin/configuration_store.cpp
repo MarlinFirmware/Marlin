3bd76f60e4b7bedfd2c953b084a589fa8214ba03
Scott Lahteine <sourcetree@thinkyhead.com>
2016-12-14 05:13:25 -0800

    Report EEPROM data size, not final index

--- Marlin/configuration_store.cpp
+++ Marlin/configuration_store.cpp
@@ -240,6 +240,7 @@ void Config_Postprocess() {
 
     const uint8_t esteppers = E_STEPPERS;
     EEPROM_WRITE(esteppers);
+
     EEPROM_WRITE(planner.axis_steps_per_mm);
     EEPROM_WRITE(planner.max_feedrate_mm_s);
     EEPROM_WRITE(planner.max_acceleration_mm_per_s2);
@@ -439,7 +440,7 @@ void Config_Postprocess() {
 
       // Report storage size
       SERIAL_ECHO_START;
-      SERIAL_ECHOPAIR("Settings Stored (", eeprom_size);
+      SERIAL_ECHOPAIR("Settings Stored (", eeprom_size - (EEPROM_OFFSET));
       SERIAL_ECHOLNPGM(" bytes)");
     }
   }
@@ -680,7 +681,7 @@ void Config_Postprocess() {
           Config_Postprocess();
           SERIAL_ECHO_START;
           SERIAL_ECHO(version);
-          SERIAL_ECHOPAIR(" stored settings retrieved (", eeprom_index);
+          SERIAL_ECHOPAIR(" stored settings retrieved (", eeprom_index - (EEPROM_OFFSET));
           SERIAL_ECHOLNPGM(" bytes)");
         }
       }
