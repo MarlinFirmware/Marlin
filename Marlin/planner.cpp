fb08f644dc5c9456030ec45e3ef287dbdb26827f
Bob Cousins <bobcousins42@googlemail.com>
2016-12-17 23:12:55 +0000

    Apply HAL functions to planner and stepper

--- Marlin/planner.cpp
+++ Marlin/planner.cpp
@@ -1123,7 +1123,7 @@ void Planner::_buffer_line(const float &a, const float &b, const float &c, const
   }
   block->acceleration_steps_per_s2 = accel;
   block->acceleration = accel / steps_per_mm;
-  block->acceleration_rate = (long)(accel * 16777216.0 / ((F_CPU) * 0.125)); // * 8.388608
+  block->acceleration_rate = (long)(accel * 16777216.0 / HAL_TIMER_RATE);
 
   // Initial limit on the segment entry velocity
   float vmax_junction;
