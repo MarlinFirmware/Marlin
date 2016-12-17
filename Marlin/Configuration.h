15bb729107bef1039a04352768ddb58f3b082897
Tannoo <tannoo@msn.com>
2016-12-17 10:33:40 -0700

    Add files via upload

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1465,14 +1465,52 @@
  * 
  * This feature is for reading the ambient temperature and humidity near or
  * around the printer.
- * 
+ * Default pin = A3 (AUX1 connector pin3 (outside row))
  *  
  * */
-#define DHT_ENABLE
+//#define DHT_ENABLE
 #if ENABLED(DHT_ENABLE)
-    #define DHT_TYPE 22     //DHT type 11, 21, or 22
-    #define DHT_PIN 5       //Set pin for the DHT sensor. Default = 5 (Servo3 pin)
+    #define DHT_TYPE 22    //DHT type 11, 21, or 22
+    #define DHT_PIN A3     //Set pin for the DHT sensor.
 #endif //DHT_ENABLE
+
+
+/** 
+  //===========================================================================
+  //========================== RGB LED Strip Control ==========================
+  //===========================================================================
+ * 
+ * 
+ * This feature is to control an RGB LED Strip with the PWM pins on the Arduino
+ * Pins 4, 5, 6 or 11
+ * 
+ * *** CAUTION ***
+ * Requires MOFSET Chip between PWM lines and LED's as the arduino cannot
+ * handle the current the LED's will require - Not doing this will destroy
+ * your arduino
+ * *** CAUTION ***
+ *  
+ * */
+//#define RGB_STRIP
+#if ENABLED(RGB_STRIP)
+  #define RGB_LED_R_PIN 5
+  #define RGB_LED_G_PIN 6
+  #define RGB_LED_B_PIN 11
+#endif
+
+/**
+  //===========================================================================
+  //=========================== Printer Event LEDs ============================
+  //===========================================================================
+ *  When printing, the LEDs will display printer status
+ *  - LEDs will gradually change from blue to violet as the heated bed gets to target temp
+ *  - LEDs will gradually change from violet to red as the hotend gets to temperature
+ *  - LEDs will change to white to illuminate work surface
+ *  - LEDs will change to green once print has finished
+ *  - LEDs will turn off after the print has finished and the user has pushed a button 
+ * */
+#if ENABLED(BLINKM) || ENABLED(RGB_LED) || ENABLED(RGB_STRIP)
+  #define PRINTER_EVENT_LEDS
 #endif
 
 #endif // CONFIGURATION_H
