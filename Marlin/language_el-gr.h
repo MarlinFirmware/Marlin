8871298563946ed73f701021ab956cc2f91d00b6
android444 <github@c64.com.pl>
2016-12-26 08:57:53 +0100

    Add preheat for populat materials
    Add preheat for PET, RUBBER, HIPS and NYLON. Update all languages and
    example configuration files

--- Marlin/language_el-gr.h
+++ Marlin/language_el-gr.h
@@ -62,6 +62,66 @@
 #define MSG_PREHEAT_2_ALL                   MSG_PREHEAT_2 _UxGT(" όλα")
 #define MSG_PREHEAT_2_BEDONLY               MSG_PREHEAT_2 _UxGT(" Bed")
 #define MSG_PREHEAT_2_SETTINGS              MSG_PREHEAT_2 _UxGT(" επιβεβαίωση")
+#ifndef MSG_PREHEAT_3
+  #define MSG_PREHEAT_3                       _UxGT("Preheat PET")
+#endif
+#ifndef MSG_PREHEAT_3_N
+  #define MSG_PREHEAT_3_N                     MSG_PREHEAT_3 _UxGT(" ")
+#endif
+#ifndef MSG_PREHEAT_3_ALL
+  #define MSG_PREHEAT_3_ALL                   MSG_PREHEAT_3 _UxGT(" All")
+#endif
+#ifndef MSG_PREHEAT_3_BEDONLY
+  #define MSG_PREHEAT_3_BEDONLY               MSG_PREHEAT_3 _UxGT(" Bed")
+#endif
+#ifndef MSG_PREHEAT_3_SETTINGS
+  #define MSG_PREHEAT_3_SETTINGS              MSG_PREHEAT_3 _UxGT(" conf")
+#endif
+#ifndef MSG_PREHEAT_4
+  #define MSG_PREHEAT_4                       _UxGT("Preheat RUBBER")
+#endif
+#ifndef MSG_PREHEAT_4_N
+  #define MSG_PREHEAT_4_N                     MSG_PREHEAT_4 _UxGT(" ")
+#endif
+#ifndef MSG_PREHEAT_4_ALL
+  #define MSG_PREHEAT_4_ALL                   MSG_PREHEAT_4 _UxGT(" All")
+#endif
+#ifndef MSG_PREHEAT_4_BEDONLY
+  #define MSG_PREHEAT_4_BEDONLY               MSG_PREHEAT_4 _UxGT(" Bed")
+#endif
+#ifndef MSG_PREHEAT_4_SETTINGS
+  #define MSG_PREHEAT_4_SETTINGS              MSG_PREHEAT_4 _UxGT(" conf")
+#endif
+#ifndef MSG_PREHEAT_5
+  #define MSG_PREHEAT_5                       _UxGT("Preheat HIPS")
+#endif
+#ifndef MSG_PREHEAT_5_N
+  #define MSG_PREHEAT_5_N                     MSG_PREHEAT_5 _UxGT(" ")
+#endif
+#ifndef MSG_PREHEAT_5ALL
+  #define MSG_PREHEAT_5_ALL                   MSG_PREHEAT_5 _UxGT(" All")
+#endif
+#ifndef MSG_PREHEAT_5_BEDONLY
+  #define MSG_PREHEAT_5_BEDONLY               MSG_PREHEAT_5 _UxGT(" Bed")
+#endif
+#ifndef MSG_PREHEAT_5_SETTINGS
+  #define MSG_PREHEAT_5_SETTINGS              MSG_PREHEAT_5 _UxGT(" conf")
+#endif
+#ifndef MSG_PREHEAT_6
+  #define MSG_PREHEAT_6                       _UxGT("Preheat NYLON")
+#endif
+#ifndef MSG_PREHEAT_6_N
+  #define MSG_PREHEAT_6_N                     MSG_PREHEAT_6 _UxGT(" ")
+#endif
+#ifndef MSG_PREHEAT_6_ALL
+  #define MSG_PREHEAT_6_ALL                   MSG_PREHEAT_6 _UxGT(" All")
+#endif
+#ifndef MSG_PREHEAT_6_BEDONLY
+  #define MSG_PREHEAT_6_BEDONLY               MSG_PREHEAT_6 _UxGT(" Bed")
+#endif
+#ifndef MSG_PREHEAT_6_SETTINGS
+  #define MSG_PREHEAT_6_SETTINGS              MSG_PREHEAT_6 _UxGT(" conf")
+#endif
 #define MSG_COOLDOWN                        _UxGT("Μειωση θερμοκρασιας")
 #define MSG_SWITCH_PS_ON                    _UxGT("Ενεργοποίηση")
 #define MSG_SWITCH_PS_OFF                   _UxGT("Απενεργοποίηση")
