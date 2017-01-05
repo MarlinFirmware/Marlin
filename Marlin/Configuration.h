cd0dcaffeb64c6ddc5e4782bd1d09d3fbaaa4e0f
anatom74 <tomaz.kerin@siol.net>
2017-01-05 17:55:48 +0100

    offset change
    from 3.60 to 3.45

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -602,7 +602,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER -20  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -4  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -3.60   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -3.45   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
