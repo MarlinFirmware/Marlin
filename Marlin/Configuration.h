8f48411689c438e43ae9380ad298cfd32e75a6ba
Autonumous <autonumous@icloud.com>
2017-01-09 23:27:50 +0000

    BB'ify
    probe updates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -592,7 +592,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER -25 //10  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 2.6 //0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.6 //0   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 3000 //8000
