2150ed3c3125646e0c7fcea10b93bc87ee06a58a
Autonumous <autonumous@icloud.com>
2017-01-11 04:34:47 +0000

    z probe update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -592,7 +592,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER -25 //10  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.6 //0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.2 //-2.6 //0   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 10000 //3000 //8000
@@ -668,7 +668,7 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   0 //3 // Z_PROBE_DEPLOY_HEIGHT? // 15 //0 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES 2 //3 // Z_PROBE_TRAVEL_HEIGHT? // 5 // Z Clearance between probe points
+#define Z_CLEARANCE_BETWEEN_PROBES 3//2 //3 // Z_PROBE_TRAVEL_HEIGHT? // 5 // Z Clearance between probe points
 
 //
 // For M851 give a range for adjusting the Z probe offset
@@ -881,7 +881,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS -38 //0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 2.6 //0 // Distance between the nozzle to printbed after homing
+#define MANUAL_Z_HOME_POS 2.2 //2.6 //0 // Distance between the nozzle to printbed after homing
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
