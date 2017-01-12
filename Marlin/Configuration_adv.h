9ede67fbc41defbdff36474c9ce320514ef450da
Bob-the-Kuhn <bob.kuhn@att.net>
2017-01-11 19:06:03 -0600

    G38 optional double touch
    Made the double touch portion a conditional compile based on the
    PROBE_DOUBLE_TOUCH flag.
    
    ==============================================
    
    Bugfix
    
    The current G38 only stopped a move if it involved the Z axis.
    
    Moved all the G38 code to it's own section and put it where it would
    always be executed no matter what axis was moving or if the endstop was
    enabled.
    
    Also added a comment to configuration_adv to alert the user the double
    tap had to be turned on.
    
    ==============================================
    
    Change G38 back to using Z_MIN_PROBE
    
    There's no Z_MIN endstop if Z_DUAL_ENDSTOPS is enabled and you have them
    set to the top of the gantry.
    
    G38 started out as using the Z_MIN_PROBE pin.  I don't remember why we
    changed it to the Z_MIN endstop.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -649,6 +649,7 @@
 //#define BEZIER_CURVE_SUPPORT
 
 // G38.2 and G38.3 Probe Target
+// ENABLE PROBE_DOUBLE_TOUCH if you want G38 to double touch
 //#define G38_PROBE_TARGET
 #if ENABLED(G38_PROBE_TARGET)
   #define G38_MINIMUM_MOVE 0.0275 // minimum distance in mm that will produce a move (determined using the print statement in check_move)
