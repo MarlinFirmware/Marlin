65d4e73b90aef8e232073bdc1b167e77fef04736
Martin Rice <mrice411@gmail.com>
2017-01-28 12:38:52 -0500

    Added pica board support in boards and pins

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -172,6 +172,8 @@
   #include "pins_MKS_13.h"
 #elif MB(SAINSMART_2IN1)
   #include "pins_SAINSMART_2IN1.h"
+#elif MB(PICA) || MB(PICA_REVB)
+  #include "pins_PICA.h"
 #else
   #error "Unknown MOTHERBOARD value set in Configuration.h"
 #endif
