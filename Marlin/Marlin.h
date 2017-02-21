f140fb3536d2d84de62ee421e3ab6f5a87e1d36b
Christopher Reimer <mail+gitproject@c-reimer.de>
2017-02-21 11:43:56 +0100

    Remove power_on signal from all stepper enables

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -39,7 +39,6 @@
 #include "types.h"
 #include "fastio.h"
 #include "utility.h"
-#include "power.h"
 
 
 #ifdef USBCON
@@ -123,35 +122,35 @@ void manage_inactivity(bool ignore_stepper_queue = false);
 #endif
 
 #if HAS_X2_ENABLE
-  #define  enable_x() do{ powerManager.power_on(); X_ENABLE_WRITE( X_ENABLE_ON); X2_ENABLE_WRITE( X_ENABLE_ON); }while(0)
+  #define  enable_x() do{ X_ENABLE_WRITE( X_ENABLE_ON); X2_ENABLE_WRITE( X_ENABLE_ON); }while(0)
   #define disable_x() do{ X_ENABLE_WRITE(!X_ENABLE_ON); X2_ENABLE_WRITE(!X_ENABLE_ON); axis_known_position[X_AXIS] = false; }while(0)
 #elif HAS_X_ENABLE
-  #define  enable_x() do{ powerManager.power_on(); X_ENABLE_WRITE( X_ENABLE_ON); }while(0)
+  #define  enable_x() X_ENABLE_WRITE( X_ENABLE_ON)
   #define disable_x() do{ X_ENABLE_WRITE(!X_ENABLE_ON); axis_known_position[X_AXIS] = false; }while(0)
 #else
-  #define  enable_x() powerManager.power_on()
+  #define  enable_x() NOOP
   #define disable_x() NOOP
 #endif
 
 #if HAS_Y2_ENABLE
-  #define  enable_y() do{ powerManager.power_on(); Y_ENABLE_WRITE( Y_ENABLE_ON); Y2_ENABLE_WRITE(Y_ENABLE_ON); }while(0)
+  #define  enable_y() do{ Y_ENABLE_WRITE( Y_ENABLE_ON); Y2_ENABLE_WRITE(Y_ENABLE_ON); }while(0)
   #define disable_y() do{ Y_ENABLE_WRITE(!Y_ENABLE_ON); Y2_ENABLE_WRITE(!Y_ENABLE_ON); axis_known_position[Y_AXIS] = false; }while(0)
 #elif HAS_Y_ENABLE
-  #define  enable_y() do{ powerManager.power_on(); Y_ENABLE_WRITE( Y_ENABLE_ON); }while(0)
+  #define  enable_y() Y_ENABLE_WRITE( Y_ENABLE_ON)
   #define disable_y() do{ Y_ENABLE_WRITE(!Y_ENABLE_ON); axis_known_position[Y_AXIS] = false; }while(0)
 #else
-  #define  enable_y() powerManager.power_on()
+  #define  enable_y() NOOP
   #define disable_y() NOOP
 #endif
 
 #if HAS_Z2_ENABLE
-  #define  enable_z() do{ powerManager.power_on(); Z_ENABLE_WRITE( Z_ENABLE_ON); Z2_ENABLE_WRITE(Z_ENABLE_ON); }while(0)
+  #define  enable_z() do{ Z_ENABLE_WRITE( Z_ENABLE_ON); Z2_ENABLE_WRITE(Z_ENABLE_ON); }while(0)
   #define disable_z() do{ Z_ENABLE_WRITE(!Z_ENABLE_ON); Z2_ENABLE_WRITE(!Z_ENABLE_ON); axis_known_position[Z_AXIS] = false; }while(0)
 #elif HAS_Z_ENABLE
-  #define  enable_z() do{ powerManager.power_on(); Z_ENABLE_WRITE( Z_ENABLE_ON); }while(0)
+  #define  enable_z() Z_ENABLE_WRITE( Z_ENABLE_ON)
   #define disable_z() do{ Z_ENABLE_WRITE(!Z_ENABLE_ON); axis_known_position[Z_AXIS] = false; }while(0)
 #else
-  #define  enable_z() powerManager.power_on()
+  #define  enable_z() NOOP
   #define disable_z() NOOP
 #endif
 
@@ -161,53 +160,53 @@ void manage_inactivity(bool ignore_stepper_queue = false);
    * Mixing steppers synchronize their enable (and direction) together
    */
   #if MIXING_STEPPERS > 3
-    #define  enable_e0() { powerManager.power_on(); E0_ENABLE_WRITE( E_ENABLE_ON); E1_ENABLE_WRITE( E_ENABLE_ON); E2_ENABLE_WRITE( E_ENABLE_ON); E3_ENABLE_WRITE( E_ENABLE_ON); }
+    #define  enable_e0() { E0_ENABLE_WRITE( E_ENABLE_ON); E1_ENABLE_WRITE( E_ENABLE_ON); E2_ENABLE_WRITE( E_ENABLE_ON); E3_ENABLE_WRITE( E_ENABLE_ON); }
     #define disable_e0() { E0_ENABLE_WRITE(!E_ENABLE_ON); E1_ENABLE_WRITE(!E_ENABLE_ON); E2_ENABLE_WRITE(!E_ENABLE_ON); E3_ENABLE_WRITE(!E_ENABLE_ON); }
   #elif MIXING_STEPPERS > 2
-    #define  enable_e0() { powerManager.power_on(); E0_ENABLE_WRITE( E_ENABLE_ON); E1_ENABLE_WRITE( E_ENABLE_ON); E2_ENABLE_WRITE( E_ENABLE_ON); }
+    #define  enable_e0() { E0_ENABLE_WRITE( E_ENABLE_ON); E1_ENABLE_WRITE( E_ENABLE_ON); E2_ENABLE_WRITE( E_ENABLE_ON); }
     #define disable_e0() { E0_ENABLE_WRITE(!E_ENABLE_ON); E1_ENABLE_WRITE(!E_ENABLE_ON); E2_ENABLE_WRITE(!E_ENABLE_ON); }
   #else
-    #define  enable_e0() { powerManager.power_on(); E0_ENABLE_WRITE( E_ENABLE_ON); E1_ENABLE_WRITE( E_ENABLE_ON); }
+    #define  enable_e0() { E0_ENABLE_WRITE( E_ENABLE_ON); E1_ENABLE_WRITE( E_ENABLE_ON); }
     #define disable_e0() { E0_ENABLE_WRITE(!E_ENABLE_ON); E1_ENABLE_WRITE(!E_ENABLE_ON); }
   #endif
-  #define  enable_e1() powerManager.power_on()
+  #define  enable_e1() NOOP
   #define disable_e1() NOOP
-  #define  enable_e2() powerManager.power_on()
+  #define  enable_e2() NOOP
   #define disable_e2() NOOP
-  #define  enable_e3() powerManager.power_on()
+  #define  enable_e3() NOOP
   #define disable_e3() NOOP
 
 #else // !MIXING_EXTRUDER
 
   #if HAS_E0_ENABLE
-    #define  enable_e0() { powerManager.power_on(); E0_ENABLE_WRITE( E_ENABLE_ON); }
+    #define  enable_e0() E0_ENABLE_WRITE( E_ENABLE_ON)
     #define disable_e0() E0_ENABLE_WRITE(!E_ENABLE_ON)
   #else
-    #define  enable_e0() powerManager.power_on()
+    #define  enable_e0() NOOP
     #define disable_e0() NOOP
   #endif
 
   #if E_STEPPERS > 1 && HAS_E1_ENABLE
-    #define  enable_e1() { powerManager.power_on() E1_ENABLE_WRITE( E_ENABLE_ON); }
+    #define  enable_e1() E1_ENABLE_WRITE( E_ENABLE_ON)
     #define disable_e1() E1_ENABLE_WRITE(!E_ENABLE_ON)
   #else
-    #define  enable_e1() powerManager.power_on()
+    #define  enable_e1() NOOP
     #define disable_e1() NOOP
   #endif
 
   #if E_STEPPERS > 2 && HAS_E2_ENABLE
-    #define  enable_e2() { powerManager.power_on(); E2_ENABLE_WRITE( E_ENABLE_ON); }
+    #define  enable_e2() E2_ENABLE_WRITE( E_ENABLE_ON)
     #define disable_e2() E2_ENABLE_WRITE(!E_ENABLE_ON)
   #else
-    #define  enable_e2() powerManager.power_on()
+    #define  enable_e2() NOOP
     #define disable_e2() NOOP
   #endif
 
   #if E_STEPPERS > 3 && HAS_E3_ENABLE
-    #define  enable_e3() { powerManager.power_on(); E3_ENABLE_WRITE( E_ENABLE_ON); }
+    #define  enable_e3() E3_ENABLE_WRITE( E_ENABLE_ON)
     #define disable_e3() E3_ENABLE_WRITE(!E_ENABLE_ON)
   #else
-    #define  enable_e3() powerManager.power_on()
+    #define  enable_e3() NOOP
     #define disable_e3() NOOP
   #endif
 
