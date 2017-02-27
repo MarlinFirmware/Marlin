47729918fc7c76cf58013b6b77fb440cf29f719e
Michael Henke <mhenke@seamgen.com>
2017-02-27 12:58:18 -0800

    enable ENSURE_SMOOTH_MOVES

--- Marlin/example_configurations/delta/flsun_kossel_mini/Configuration_adv.h
+++ Marlin/example_configurations/delta/flsun_kossel_mini/Configuration_adv.h
@@ -217,12 +217,13 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
+
 #define E0_AUTO_FAN_PIN -1
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 50
-#define EXTRUDER_AUTO_FAN_SPEED   255  // == full speed
+#define EXTRUDER_AUTO_FAN_SPEED   255  // 255 == full speed
 
 // Define a pin to turn case light on/off
 //#define CASE_LIGHT_PIN 4
@@ -529,7 +530,7 @@
 //    interrupts (without LCD update). By enforcing a minimum time-per-move, the buffer is prevented from
 //    draining.
 //
-//#define ENSURE_SMOOTH_MOVES
+#define ENSURE_SMOOTH_MOVES
 #if ENABLED(ENSURE_SMOOTH_MOVES)
   //#define ALWAYS_ALLOW_MENU      // If enabled, the menu will always be responsive.
                                    // WARNING: Menu navigation during short moves may cause stuttering!
