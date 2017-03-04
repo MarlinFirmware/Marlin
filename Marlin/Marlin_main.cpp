e6c499b2d9d5fb9c0e3f87551d503f3c9656f904
benlye <ben@lye.co.nz>
2017-03-04 17:58:10 +0000

    Update Marlin_main.cpp
    Remove unnecessary line break from M48 V1 output.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -5196,8 +5196,8 @@ inline void gcode_M42() {
             SERIAL_PROTOCOLPGM(" range: ");
             SERIAL_PROTOCOL_F(max-min, 3);
           }
+          SERIAL_EOL;
         }
-        SERIAL_EOL;
       }
 
     } // End of probe loop
