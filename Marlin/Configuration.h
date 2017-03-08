585a75c44a0013e6bdedd57925170da717712f13
Scott Lahteine <sourcetree@thinkyhead.com>
2017-03-07 22:46:12 -0600

    Configure with a RAMPS-like board by default

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -130,7 +130,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_RAMPS4DUE_EFB
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
