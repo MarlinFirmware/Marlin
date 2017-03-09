d212f68215f762e58bc23639708d57ee5fbb81b6
Scott Lahteine <sourcetree@thinkyhead.com>
2017-03-08 18:53:18 -0600

    Attempt to prevent watchdog reset (failed)

--- Marlin/src/HAL/HAL_DUE/watchdog_Due.h
+++ Marlin/src/HAL/HAL_DUE/watchdog_Due.h
@@ -27,8 +27,11 @@
 
 // Arduino Due core now has watchdog support
 
+// Initialize watchdog with a 4 second interrupt time
 void watchdog_init();
 
+// Reset watchdog. MUST be called at least every 4 seconds after the
+// first watchdog_init or AVR will go into emergency procedures.
 inline void watchdog_reset() { watchdogReset(); }
 
 #endif /* WATCHDOG_DUE_H */
