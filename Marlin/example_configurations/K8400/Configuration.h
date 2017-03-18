e0d2c7448b0f652146aadb64945e0bf68be68fe6
Scott Lahteine <sourcetree@thinkyhead.com>
2017-03-18 12:20:26 -0500

    Patch configs UBL followup

--- Marlin/example_configurations/K8400/Configuration.h
+++ Marlin/example_configurations/K8400/Configuration.h
@@ -843,28 +843,28 @@
 
 #elif ENABLED(MESH_BED_LEVELING)
 
-//===========================================================================
-//=================================== Mesh ==================================
-//===========================================================================
+  //===========================================================================
+  //=================================== Mesh ==================================
+  //===========================================================================
 
-  #define MANUAL_PROBE_Z_RANGE 4 // Z after Home, bed somewhere below but above 0.0.
+  #define MANUAL_PROBE_Z_RANGE 4 // Z Range centered on Z_MIN_POS for LCD Z adjustment
   #define MESH_INSET 10          // Mesh inset margin on print area
   #define MESH_NUM_X_POINTS 3    // Don't use more than 7 points per axis, implementation limited.
   #define MESH_NUM_Y_POINTS 3
 
-  //#define MESH_G28_REST_ORIGIN // After homing all axes ('G28' or 'G28 XYZ') rest at origin [0,0,0]
+  //#define MESH_G28_REST_ORIGIN // After homing all axes ('G28' or 'G28 XYZ') rest Z at Z_MIN_POS
 
   //#define MANUAL_BED_LEVELING  // Add display menu option for bed leveling.
 
   #if ENABLED(MANUAL_BED_LEVELING)
     #define MBL_Z_STEP 0.025     // Step size while manually probing Z axis.
-  #endif  // MANUAL_BED_LEVELING
+  #endif
 
 #elif ENABLED(AUTO_BED_LEVELING_UBL)
 
-//===========================================================================
-//========================= Unified Bed Leveling ============================
-//===========================================================================
+  //===========================================================================
+  //========================= Unified Bed Leveling ============================
+  //===========================================================================
 
   #define UBL_MESH_INSET 1          // Mesh inset margin on print area
   #define UBL_MESH_NUM_X_POINTS 10  // Don't use more than 15 points per axis, implementation limited.
