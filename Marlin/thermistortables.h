be7dd903824773dbad558d2f058067b69adbafdb
Scott Lahteine <sourcetree@thinkyhead.com>
2017-03-18 10:17:44 -0500

    Add Thermistor 75: MGB18 silicon pad

--- Marlin/thermistortables.h
+++ Marlin/thermistortables.h
@@ -103,6 +103,9 @@
 #if ANY_THERMISTOR_IS(70) // bqh2 stock thermistor
   #include "thermistortable_70.h"
 #endif
+#if ANY_THERMISTOR_IS(75) // Many of the generic silicon heat pads use the MGB18-104F39050L32 Thermistor
+  #include "thermistortable_75.h"
+#endif
 #if ANY_THERMISTOR_IS(110) // Pt100 with 1k0 pullup
   #include "thermistortable_110.h"
 #endif
