79ec3deb989d0f45b2f965bca5ee0339c76f4538
MaP <vgr.map@gmail.com>
2017-03-19 03:08:27 +0100

    Add basic support for ZRIB v2 & 3 board

---
+++ Marlin/pins_ZRIB_2.h
@@ -0,0 +1,46 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#if HOTENDS > 2
+  #error "Zrib v2 supports up to 2 hotends. Comment this line to keep going."
+#endif
+
+#ifndef BOARD_NAME
+  #define BOARD_NAME "ZRIB v2"
+#endif
+
+#define IS_RAMPS_EFB
+#define MOSFET_D_PIN 6  //FAN_1_PIN
+
+#include "pins_RAMPS.h"
+
+#if HOTENDS == 2
+  #undef HEATER_1_PIN
+  #HEATER_1_PIN 7
+#endif
+
+//switch Z_MIN_ZMAX and Z_MAX_PIN for auto bed leveling
+#ifdef Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+  #undef Z_MIN_PIN
+  #define Z_MIN_PIN 19
+#endif
+
