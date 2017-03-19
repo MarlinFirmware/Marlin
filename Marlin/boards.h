596a36dac4486466fc33012969cdeda15fe9a1fa
MaP <vgr.map@gmail.com>
2017-03-19 03:16:16 +0100

    Suport for ZRIB v2 & v3 board

--- Marlin/boards.h
+++ Marlin/boards.h
@@ -94,6 +94,7 @@
 #define BOARD_BAM_DICE          401  // 2PrintBeta BAM&DICE with STK drivers
 #define BOARD_BAM_DICE_DUE      402  // 2PrintBeta BAM&DICE Due with STK drivers
 #define BOARD_BQ_ZUM_MEGA_3D    503  // bq ZUM Mega 3D
+#define BOARD_ZRIB_2            153  // ZRIB v2 and v3 board
 
 #define MB(board) (MOTHERBOARD==BOARD_##board)
 
