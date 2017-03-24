d9ee81003ade9eeb72344087f99d46832abf0996
sirukin <bryan.vandenbrink@gmail.com>
2017-03-23 21:08:38 -0700

    Updated Configuration.h to point to the correct board. "BT7272A" not "BT727A" Also updated pins.h to point to the correct board in boards.h Naming convention does not use BOARD_BT7272A but just the "BT7272A" portion.

--- Marlin/example_configurations/BT7272A/Configuration.h
+++ Marlin/example_configurations/BT7272A/Configuration.h
@@ -130,7 +130,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BT727A
+  #define MOTHERBOARD BOARD_BT7272A
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
