a3c177806ff1de9b15b9bcba3cc4ebde933fb806
Scott Lahteine <sourcetree@thinkyhead.com>
2017-04-09 03:24:49 -0500

    Patch configs comment formatting

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -419,10 +419,10 @@
 //#define DIGIPOT_MOTOR_CURRENT {135,135,135,135,135} // Values 0-255 (RAMBO 135 = ~0.75A, 185 = ~1A)
 //#define DAC_MOTOR_CURRENT_DEFAULT { 70, 80, 90, 80 } // Default drive percent - X, Y, Z, E axis
 
-/*  5DPRINT & AZTEEG_X3_PRO */
-//#define DIGIPOT_I2C  // uncomment to enable
+// Uncomment to enable an I2C based DIGIPOT like on the Azteeg X3 Pro
+//#define DIGIPOT_I2C
 #define DIGIPOT_I2C_NUM_CHANNELS 8 // 5DPRINT: 4     AZTEEG_X3_PRO: 8
-/* actual motor currents in Amps, need as many here as DIGIPOT_I2C_NUM_CHANNELS */
+// Actual motor currents in Amps, need as many here as DIGIPOT_I2C_NUM_CHANNELS
 #define DIGIPOT_I2C_MOTOR_CURRENTS {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}  //  AZTEEG_X3_PRO
 
 //===========================================================================
