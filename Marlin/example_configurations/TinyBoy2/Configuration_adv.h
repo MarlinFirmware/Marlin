52c3ace102c447f5f93b31e5efb5b4b8302fc233
Scott Lahteine <sourcetree@thinkyhead.com>
2017-04-13 16:29:43 -0500

    Add BABYSTEP_ZPROBE_OFFSET as an optional feature

--- Marlin/example_configurations/TinyBoy2/Configuration_adv.h
+++ Marlin/example_configurations/TinyBoy2/Configuration_adv.h
@@ -585,6 +585,7 @@
   #define BABYSTEP_XY              // Also enable X/Y Babystepping. Not supported on DELTA!
   #define BABYSTEP_INVERT_Z false  // Change if Z babysteps should go the other way
   #define BABYSTEP_MULTIPLICATOR 1 // Babysteps are very small. Increase for faster motion.
+  //#define BABYSTEP_ZPROBE_OFFSET // Enable to combine M851 and Babystepping
   //#define DOUBLECLICK_FOR_Z_BABYSTEPPING // Double-click on the Status Screen for Z Babystepping.
   #define DOUBLECLICK_MAX_INTERVAL 1250 // Maximum interval between clicks, in milliseconds.
                                         // Note: Extra time may be added to mitigate controller latency.
