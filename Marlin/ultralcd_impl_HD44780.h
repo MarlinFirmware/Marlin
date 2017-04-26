4d96d3eab17b987e85b87460d9c5c483865d1895
Scott Lahteine <thinkyhead@users.noreply.github.com>
2017-04-25 23:37:28 -0500

    Bootscreen delay after clear, then set chars
    Fixes #6426

--- Marlin/ultralcd_impl_HD44780.h
+++ Marlin/ultralcd_impl_HD44780.h
@@ -530,15 +530,15 @@ void lcd_print(char c) { charset_mapper(c); }
       safe_delay(2000);
     #endif
 
-    /*
     lcd.clear();
 
+    safe_delay(100);
+    
     lcd_set_custom_characters(
       #if ENABLED(LCD_PROGRESS_BAR)
         false
       #endif
     );
-    //*/
   }
 
 #endif // SHOW_BOOTSCREEN
