6ce7f9fc7bca2b3a5f7b74a709577fcdfafa09bb
benlye <ben@lye.co.nz>
2017-04-28 19:10:58 +0100

    Fix issue in itostr4sign with numbers <100
    The first character in the string wasn't blanked if the number went from >= 100 to <100, resulting in artefacts in the displayed string.

--- Marlin/utility.cpp
+++ Marlin/utility.cpp
@@ -87,13 +87,13 @@ void safe_delay(millis_t ms) {
         conv[5] = DIGIMOD(xx, 10);
       }
       else {
+        conv[3] = ' ';
         conv[4] = ' ';
         if (xx >= 10) {
           conv[4] = neg ? '-' : ' ';
           conv[5] = DIGIMOD(xx, 10);
         }
         else {
-          conv[4] = ' ';
           conv[5] = neg ? '-' : ' ';
         }
       }
