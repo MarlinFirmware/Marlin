3370329751a21130c97f2b0c621ad8a75dd449b7
Brian <bgort@users.noreply.github.com>
2017-05-11 10:48:16 -0400

    Fix broken EEPROM save/load

--- Marlin/utility.cpp
+++ Marlin/utility.cpp
@@ -37,8 +37,8 @@ void safe_delay(millis_t ms) {
 #if ENABLED(EEPROM_SETTINGS)
 
   void crc16(uint16_t *crc, const void * const data, uint16_t cnt) {
-    uint8_t *ptr = (uint8_t*)data;
-    while (cnt-- > 0) {
+    uint8_t *ptr = (uint8_t *)data;
+    while (cnt--) {
       *crc = (uint16_t)(*crc ^ (uint16_t)(((uint16_t)*ptr++) << 8));
       for (uint8_t x = 0; x < 8; x++)
         *crc = (uint16_t)((*crc & 0x8000) ? ((uint16_t)(*crc << 1) ^ 0x1021) : (*crc << 1));
