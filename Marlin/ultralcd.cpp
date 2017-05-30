2c7a39bc0e8b33faf7ede260473ccafef99c4bb4
Scott Lahteine <github@thinkyhead.com>
2017-05-29 19:50:53 -0500

    Fix LCD print aborted message

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -718,7 +718,8 @@ void kill_screen(const char* lcd_msg) {
         for (uint8_t i = 0; i < FAN_COUNT; i++) fanSpeeds[i] = 0;
       #endif
       wait_for_heatup = false;
-      lcd_setstatusPGM(PSTR(MSG_PRINT_PAUSED), -1);
+      lcd_setstatusPGM(PSTR(MSG_PRINT_ABORTED), -1);
+      lcd_return_to_status();
     }
 
   #endif // SDSUPPORT
