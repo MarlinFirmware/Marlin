6411d65c48b60c1fc08e90263a44becaf1f7727a
Scott Lahteine <github@thinkyhead.com>
2017-06-04 12:24:09 -0500

    Fix Morgan SCARA S2 calculation

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -12003,7 +12003,7 @@ void prepare_move_to_destination() {
     else
       C2 = (HYPOT2(sx, sy) - (L1_2 + L2_2)) / (2.0 * L1 * L2);
 
-    S2 = sqrt(sq(C2) - 1);
+    S2 = sqrt(1 - sq(C2));
 
     // Unrotated Arm1 plus rotated Arm2 gives the distance from Center to End
     SK1 = L1 + L2 * C2;
