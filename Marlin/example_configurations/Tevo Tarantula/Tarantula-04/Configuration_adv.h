5799ca554100edb46632153c7c98a0eee7c03320
Jim Brown <jb.github@rcairgallery.com>
2017-06-06 10:29:02 -0400

    Update example config

--- Marlin/example_configurations/Tevo Tarantula/Tarantula-04/Configuration_adv.h
+++ Marlin/example_configurations/Tevo Tarantula/Tarantula-04/Configuration_adv.h
@@ -511,7 +511,7 @@
   #endif
 
   // Show a progress bar on HD44780 LCDs for SD printing
-  #define LCD_PROGRESS_BAR
+  //#define LCD_PROGRESS_BAR
 
   #if ENABLED(LCD_PROGRESS_BAR)
     // Amount of time (ms) to show the bar
