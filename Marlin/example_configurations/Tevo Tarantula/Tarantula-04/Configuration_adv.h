815c18abed29e1f38e8cf5c54160abf684ec6c41
Jim Brown <jb.github@rcairgallery.com>
2017-06-07 09:30:47 -0400

    Update example configs

--- Marlin/example_configurations/Tevo Tarantula/Tarantula-04/Configuration_adv.h
+++ Marlin/example_configurations/Tevo Tarantula/Tarantula-04/Configuration_adv.h
@@ -1224,7 +1224,7 @@
  *  - M206 and M428 are disabled.
  *  - G92 will revert to its behavior from Marlin 1.0.
  */
-#define NO_WORKSPACE_OFFSETS
+//#define NO_WORKSPACE_OFFSETS
 
 /**
  * Set the number of proportional font spaces required to fill up a typical character space.
