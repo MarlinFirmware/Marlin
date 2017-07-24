3873d077d368eff982171819eca019f480835f93
grawi-sikorsky <grawires@gmail.com>
2017-07-24 14:36:33 +0200

    no message

--- Marlin/boards.h
+++ Marlin/boards.h
@@ -97,6 +97,7 @@
 #define BOARD_BAM_DICE_DUE      402  // 2PrintBeta BAM&DICE Due with STK drivers
 #define BOARD_BQ_ZUM_MEGA_3D    503  // bq ZUM Mega 3D
 #define BOARD_ZRIB_V20          504  // zrib V2.0 control board (Chinese knock off RAMPS replica)
+#define BOARD_PRINTOH3_MKS		666  // Printo H3 MKS with Oled Controller
 
 #define MB(board) (MOTHERBOARD==BOARD_##board)
 
