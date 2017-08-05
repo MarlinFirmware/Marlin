0fd2923a9f817ce2e97d31fcea0db04846724c50
Florian Heilmann <Florian.Heilmann@gmx.net>
2017-08-05 09:58:38 +0200

    Fix compilation with ADVANCED_PAUSE_FEATURE without SDSUPPORT (#7412)
    * Fix compilation of ADVANCED_PAUSE_FEATURE without SDSUPPORT
    
    * Fix Identation
    
    * Make use of IS_SD_PRINTING macro
    
    as per @MagoKimbra

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -2412,7 +2412,7 @@ void kill_screen(const char* lcd_msg) {
     // Change filament
     //
     #if ENABLED(ADVANCED_PAUSE_FEATURE)
-      if (!thermalManager.tooColdToExtrude(active_extruder) && !card.sdprinting)
+      if (!thermalManager.tooColdToExtrude(active_extruder) && !IS_SD_PRINTING)
         MENU_ITEM(function, MSG_FILAMENTCHANGE, lcd_enqueue_filament_change);
     #endif
 
