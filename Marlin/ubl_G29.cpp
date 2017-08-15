121704abb58c4945515c2dc59477288fba8023b7
Roxy-3D <Roxanne.Rae.Neufeld@gmail.com>
2017-08-15 16:39:57 -0500

    Work around previous regressions...

--- Marlin/ubl_G29.cpp
+++ Marlin/ubl_G29.cpp
@@ -1134,8 +1134,10 @@
     }
 
     if (!WITHIN(RAW_X_POSITION(g29_x_pos), X_MIN_BED, X_MAX_BED)) {  // if X & Y are not valid, use center of the bed values
+      g29_x_pos = (X_MIN_BED + X_MAX_BED) / 2.0;
 
     if (!WITHIN(RAW_Y_POSITION(g29_y_pos), Y_MIN_BED, Y_MAX_BED)) {  // if X & Y are not valid, use center of the bed values
+      g29_y_pos = (Y_MIN_BED + Y_MAX_BED) / 2.0;
 
     if (err_flag) return UBL_ERR;
 
