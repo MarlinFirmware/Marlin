3f0b38ed5df318cfd28ac7433c21f2c4ceab5ddc
Roxy-3D <Roxy-3D@users.noreply.github.com>
2017-08-25 17:03:07 -0500

    Add Max7219 LED Matrix Debug Support (#7563)
    * Add Max7219 LED Matrix Debug Support
    
    The Max7219 8x8 LED Matrix's are very helpful for debugging new code.
    And for that matter, just trying to maximize printer settings without
    causing stuttering.
    
    The displays are very inexpensive (under $2.00 with shipping) and
    provide a lot of help when trying to debug complicated code.
    
    * Try to keep Makefile up to date.

--- Marlin/example_configurations/delta/FLSUN/auto_calibrate/Configuration_adv.h
+++ Marlin/example_configurations/delta/FLSUN/auto_calibrate/Configuration_adv.h
@@ -1384,4 +1384,30 @@
 
 #endif // I2C_POSITION_ENCODERS
 
+/**
+  * Debug LED's using an 8x8 LED Matrix driven by a Max7219 chip.   Fully assembled versions are available on
+  * eBay for under $2.00 (including shipping) and only require 3 signal wires.
+  *
+  * Check out auctions similar to this: https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=332349290049&_sacat=0
+  */
+
+//#define MAX7219_DEBUG
+#if ENABLED(MAX7219_DEBUG)
+  #define Max7219_clock   64  // 77 on Re-ARM       // Configuration of the 3 pins to control the display
+  #define Max7219_data_in 57  // 78 on Re-ARM
+  #define Max7219_load    44  // 79 on Re-ARM
+
+  /*
+   * These are sample debug features that can be turned on and configured for your use.
+   * The developer will need to manage the use of the various LED's in the 8x8 matrix to avoid conflicts.
+   */
+  #define MAX7219_DEBUG_PRINTER_ALIVE    // Blink corner LED of 8x8 matrix from idle() routine if firmware is functioning
+  #define MAX7219_DEBUG_STEPPER_HEAD  3  // Display row position of stepper queue head on this line and the next line of LED matrix
+  #define MAX7219_DEBUG_STEPPER_TAIL  5  // Display row position of stepper queue tail on this line and the next line of LED matrix
+
+  #define MAX7219_DEBUG_STEPPER_QUEUE 0  // Display row position of stepper queue depth on this line and the next line of LED matrix
+                                         // If you have stuttering on your Delta printer, this option may help you understand how
+                                         // various tweaks you make to your configuration are affecting the printer.
+#endif
+
 #endif // CONFIGURATION_ADV_H
