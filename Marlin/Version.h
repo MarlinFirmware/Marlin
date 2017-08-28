9c00cff89836706776f6353635368a47cc5bf881
Scott Lahteine <github@thinkyhead.com>
2017-08-28 18:46:39 -0500

    Version 1.1.5

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -35,7 +35,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "1.1.4"
+  #define SHORT_BUILD_VERSION "1.1.5"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -48,7 +48,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2017-07-04 12:00"
+  #define STRING_DISTRIBUTION_DATE "2017-08-28 12:00"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
