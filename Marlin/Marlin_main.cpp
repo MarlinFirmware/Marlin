83e464920c283678dd0babf9a2dcc05ebc376b01
Ivan Kalafatic <ikalafat@gmail.com>
2017-09-02 23:17:53 +0200

    Laser PWM pin also acts as Enable (PWM = 0 means laser is disabled), and that M3/M4 S0 should set PWM to 0, as well as M5 command

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -6075,6 +6075,7 @@ inline void gcode_G92() {
         const float spindle_laser_power = parser.floatval('S');
         if (spindle_laser_power == 0) {
           WRITE(SPINDLE_LASER_ENABLE_PIN, !SPINDLE_LASER_ENABLE_INVERT);                                    // turn spindle off (active low)
+          analogWrite(SPINDLE_LASER_PWM_PIN, 255 & 0xFF);                                               // only write low byte
           delay_for_power_down();
         }
         else {
@@ -6102,6 +6103,7 @@ inline void gcode_G92() {
   inline void gcode_M5() {
     stepper.synchronize();
     WRITE(SPINDLE_LASER_ENABLE_PIN, !SPINDLE_LASER_ENABLE_INVERT);
+    analogWrite(SPINDLE_LASER_PWM_PIN, 0 & 0xFF);                                               // only write low byte
     delay_for_power_down();
   }
 
