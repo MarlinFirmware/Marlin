7dd0a467325e75eafb19fc7e8b1ccce78686069b
Scott Lahteine <github@thinkyhead.com>
2017-09-06 06:28:32 -0500

    Language updates

--- Marlin/src/lcd/language/language_bg.h
+++ Marlin/src/lcd/language/language_bg.h
@@ -24,7 +24,7 @@
  * Bulgarian
  *
  * LCD Menu Messages
- * See also https://github.com/MarlinFirmware/Marlin/wiki/LCD-Language
+ * See also http://marlinfw.org/docs/development/lcd_language.html
  *
  */
 #ifndef LANGUAGE_BG_H
