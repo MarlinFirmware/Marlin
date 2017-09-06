1de79c453c4e87818564c7b12bfc3e72d61e613f
Scott Lahteine <github@thinkyhead.com>
2017-09-06 06:28:32 -0500

    Pins updates

--- Marlin/src/pins/pins_RAMPS.h
+++ Marlin/src/pins/pins_RAMPS.h
@@ -63,7 +63,7 @@
 //
 // Servos
 //
-#ifdef IS_RAMPS_13
+#if ENABLED(IS_RAMPS_13)
   #define SERVO0_PIN        7 // RAMPS_13 // Will conflict with BTN_EN2 on LCD_I2C_VIKI
 #else
   #define SERVO0_PIN       11
