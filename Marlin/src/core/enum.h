142d8aae567d6646c01e6336789a1f6e69db3f43
Scott Lahteine <github@thinkyhead.com>
2017-09-07 22:08:13 -0500

    Move debugging to serial.*

--- Marlin/src/core/enum.h
+++ Marlin/src/core/enum.h
@@ -69,22 +69,6 @@ typedef enum {
   TEMPUNIT_F
 } TempUnit;
 
-/**
- * Debug flags
- * Not yet widely applied
- */
-enum DebugFlags {
-  DEBUG_NONE          = 0,
-  DEBUG_ECHO          = _BV(0), ///< Echo commands in order as they are processed
-  DEBUG_INFO          = _BV(1), ///< Print messages for code that has debug output
-  DEBUG_ERRORS        = _BV(2), ///< Not implemented
-  DEBUG_DRYRUN        = _BV(3), ///< Ignore temperature setting and E movement commands
-  DEBUG_COMMUNICATION = _BV(4), ///< Not implemented
-  DEBUG_LEVELING      = _BV(5), ///< Print detailed output for homing and leveling
-  DEBUG_MESH_ADJUST   = _BV(6), ///< UBL bed leveling
-  DEBUG_ALL           = 0xFF
-};
-
 enum EndstopEnum {
   X_MIN,
   Y_MIN,
