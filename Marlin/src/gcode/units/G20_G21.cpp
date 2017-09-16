a1ee1628e58cb53d945bc32e8ed5c68f3b1964ce
Scott Lahteine <github@thinkyhead.com>
2017-09-15 23:18:05 -0500

    Move G20_G21 to cpp

--- Marlin/src/gcode/units/G20_G21.cpp
+++ Marlin/src/gcode/units/G20_G21.cpp
@@ -20,12 +20,20 @@
  *
  */
 
+#include "../../inc/MarlinConfig.h"
+
+#if ENABLED(INCH_MODE_SUPPORT)
+
+#include "../gcode.h"
+
 /**
  * G20: Set input mode to inches
  */
-void gcode_G20() { parser.set_input_linear_units(LINEARUNIT_INCH); }
+void GcodeSuite::G20() { parser.set_input_linear_units(LINEARUNIT_INCH); }
 
 /**
  * G21: Set input mode to millimeters
  */
-void gcode_G21() { parser.set_input_linear_units(LINEARUNIT_MM); }
+void GcodeSuite::G21() { parser.set_input_linear_units(LINEARUNIT_MM); }
+
+#endif // INCH_MODE_SUPPORT
