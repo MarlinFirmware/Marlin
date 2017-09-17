0c7ba52451195c9ad5b77a6accb92c8d5aa9b58d
Scott Lahteine <github@thinkyhead.com>
2017-09-16 23:15:33 -0500

    Move M149 to cpp

--- Marlin/src/gcode/units/M149.cpp
+++ Marlin/src/gcode/units/M149.cpp
@@ -20,11 +20,19 @@
  *
  */
 
+#include "../../inc/MarlinConfig.h"
+
+#if ENABLED(TEMPERATURE_UNITS_SUPPORT)
+
+#include "../gcode.h"
+
 /**
  * M149: Set temperature units
  */
-void gcode_M149() {
+void GcodeSuite::M149() {
        if (parser.seenval('C')) parser.set_input_temp_units(TEMPUNIT_C);
   else if (parser.seenval('K')) parser.set_input_temp_units(TEMPUNIT_K);
   else if (parser.seenval('F')) parser.set_input_temp_units(TEMPUNIT_F);
 }
+
+#endif // TEMPERATURE_UNITS_SUPPORT
