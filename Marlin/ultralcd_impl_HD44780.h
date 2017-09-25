adb27cdad27e1be02ffedecfb033181ae0ec2c06
MasterPIC <armando_unina@yahoo.it>
2017-09-25 10:07:18 +0200

    Update ultralcd_impl_HD44780.h
    Leveling status shown by LCD

--- Marlin/ultralcd_impl_HD44780.h
+++ Marlin/ultralcd_impl_HD44780.h
@@ -790,13 +790,16 @@ static void lcd_implementation_status_screen() {
     lcd.setCursor(LCD_WIDTH - 8, 1);
     _draw_axis_label(Z_AXIS, PSTR(MSG_Z), blink);
     lcd.print(ftostr52sp(FIXFLOAT(current_position[Z_AXIS])));
-    if (leveling_is_active())
-      lcd.write('_'); //leveling flag
-    else if (blink)
-      lcd.write(' ');
-    else
-      lcd.write('_');
-
+    
+    #if HAS_LEVELING
+      if (leveling_is_active())
+        lcd.write('_'); //leveling flag
+      else if (blink)
+        lcd.write('_');
+      else
+        lcd.write('?');
+    #endif
+  
   #endif // LCD_HEIGHT > 2
 
   //
