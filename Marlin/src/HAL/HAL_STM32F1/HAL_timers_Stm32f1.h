b8b632a7e0699aa40e894ab6893036c670d953f1
xC0000005 <32139633+xC0000005@users.noreply.github.com>
2017-09-29 11:29:42 -0700

    Switch to Timer4 to avoid a hard reset on STM32F103C6 boards
    On bluepill STM32F103C6 boards, using Timer5 results in a error() vector call. Switch to 4 since these are both general purpose, 16 bit timers.

--- Marlin/src/HAL/HAL_STM32F1/HAL_timers_Stm32f1.h
+++ Marlin/src/HAL/HAL_STM32F1/HAL_timers_Stm32f1.h
@@ -48,7 +48,7 @@
 #define HAL_TIMER_TYPE uint16_t
 #define HAL_TIMER_TYPE_MAX 0xFFFF
 
-#define STEP_TIMER_NUM 5  // index of timer to use for stepper
+#define STEP_TIMER_NUM 4  // index of timer to use for stepper
 #define STEP_TIMER_CHAN 1 // Channel of the timer to use for compare and interrupts
 #define TEMP_TIMER_NUM 2  // index of timer to use for temperature
 #define TEMP_TIMER_CHAN 1 // Channel of the timer to use for compare and interrupts
