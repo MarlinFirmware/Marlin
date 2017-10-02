30813ca047dd0d372e4c3f2969f8eaf553813d3c
studiodyne <diabolomix@hotmail.fr>
2017-10-02 13:47:19 +0200

    Update ultralcd.cpp

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -3569,6 +3569,7 @@ void kill_screen(const char* lcd_msg) {
         MENU_ITEM_EDIT(float52, MSG_CONTROL_RETRACT_RECOVER_SWAP, &swap_retract_recover_length, -100, 100);
       #endif
       MENU_ITEM_EDIT(float3, MSG_CONTROL_RETRACT_RECOVERF, &retract_recover_feedrate_mm_s, 1, 999);
+      MENU_ITEM_EDIT(float3, "Lg Unret Feed", &swap_retract_recover_feedrate_mm_s, 1, 999);
       END_MENU();
     }
 
