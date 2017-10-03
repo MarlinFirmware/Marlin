c993242b2e410aaacc62ac4af9269304a867e4a8
xC0000005 <32139633+xC0000005@users.noreply.github.com>
2017-10-03 06:48:32 -0700

    Remove pinmode hack
    Remove the pin mode hack that I was using to init PA0.
    
    Updated Readme.md

--- Marlin/src/HAL/HAL_STM32F1/HAL_Stm32f1.cpp
+++ Marlin/src/HAL/HAL_STM32F1/HAL_Stm32f1.cpp
@@ -179,12 +179,6 @@ void HAL_adc_init(void)
   adc.setScanMode();
   adc.setContinuous();
   adc.startConversion();
-
-  // HACK - Not needed once the pin mapping/fastio stuff is done
-  for (int i = 0; i < ADC_PIN_COUNT; i++)
-  {
-    pinMode(adc_pins[i], INPUT_ANALOG);
-  }
 }
 
 void HAL_adc_start_conversion (const uint8_t adc_pin) {
