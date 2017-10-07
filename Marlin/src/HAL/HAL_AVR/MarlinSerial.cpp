eb96d37da0ad8a2df8cd8540c235f08096bbbea8
GMagician <GMagician@users.noreply.github.com>
2017-10-07 22:20:00 +0200

    Serial double echo fix
    Fix double character sent when TX buffer size is 0 on mega2560

--- Marlin/src/HAL/HAL_AVR/MarlinSerial.cpp
+++ Marlin/src/HAL/HAL_AVR/MarlinSerial.cpp
@@ -482,8 +482,6 @@
   #else // TX_BUFFER_SIZE == 0
 
     void MarlinSerial::write(const uint8_t c) {
-      while (!TEST(M_UCSRxA, M_UDREx)) { /* nada */ }
-      M_UDRx = c;
       #if ENABLED(SERIAL_XON_XOFF)
         // Do a priority insertion of an XON/XOFF char, if needed.
         const uint8_t state = xon_xoff_state;
