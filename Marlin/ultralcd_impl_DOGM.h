006fc7858b6891280f10613e9538e891feff6bda
Unknown <davejohnson3000@gmail.com>
2017-10-07 05:54:53 -0700

    Auto-enable DISABLE_REDUCED_ACCURACY_WARNING if HOME_AFTER_DEACTIVATE enabled
    If HOME_AFTER_DEACTIVATE, there is no need to warn on LCD since printer will home prior to starting print.  Saves 60 bytes PROGMEM.
    add HD44780 too

--- Marlin/ultralcd_impl_DOGM.h
+++ Marlin/ultralcd_impl_DOGM.h
@@ -420,10 +420,12 @@ FORCE_INLINE void _draw_axis_label(const AxisEnum axis, const char* const pstr, 
     if (!axis_homed[axis])
       u8g.print('?');
     else {
-      #if DISABLED(DISABLE_REDUCED_ACCURACY_WARNING)
-        if (!axis_known_position[axis])
-          u8g.print(' ');
-        else
+      #if DISABLED(HOME_AFTER_DEACTIVATE)
+        #if DISABLED(DISABLE_REDUCED_ACCURACY_WARNING)
+          if (!axis_known_position[axis])
+            u8g.print(' ');
+          else
+        #endif
       #endif
       lcd_printPGM(pstr);
     }
