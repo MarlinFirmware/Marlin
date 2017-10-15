1332b58d0031e52ffca84e0e2fe16fd0be9c118a
Scott Lahteine <thinkyhead@users.noreply.github.com>
2017-10-14 19:18:43 -0500

    More generic strings in configs

--- Marlin/example_configurations/Micromake C1/metalsearch's C1/Configuration.h
+++ Marlin/example_configurations/Micromake C1/metalsearch's C1/Configuration.h
@@ -74,7 +74,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(10/10/2017 MetalSearch for Micromake C1 128 STEP with capacitive probe)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(MetalSearch, Micromake C1 enhanced)" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
@@ -124,7 +124,7 @@
 
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
-#define CUSTOM_MACHINE_NAME "Imprimante"
+#define CUSTOM_MACHINE_NAME "Micromake C1"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
