9b3f27f02a27f2375104212daabd8be1d429d9ae
MetalSearch <metalsearch@gmail.com>
2017-10-21 07:24:22 +0200

    Fix:EndStops doesn't working with BOARD_RAMPS_14_EFB on Micromake C1 (#8041)
    [1.1.x] Fix EndStops don't work with BOARD_RAMPS_14_EFB on Micromake C1

--- Marlin/boards.h
+++ Marlin/boards.h
@@ -100,6 +100,7 @@
 #define BOARD_BAM_DICE_DUE      402  // 2PrintBeta BAM&DICE Due with STK drivers
 #define BOARD_BQ_ZUM_MEGA_3D    503  // bq ZUM Mega 3D
 #define BOARD_ZRIB_V20          504  // zrib V2.0 control board (Chinese knock off RAMPS replica)
+#define BOARD_MAKEBOARD_MINI    431  // MakeBoard Mini v2.1.2 is a control board sold by MicroMake
 
 #define MB(board) (MOTHERBOARD==BOARD_##board)
 
