4e445f2c55c5b7a862ea6cca173113588c0b5b8d
Scott Lahteine <github@thinkyhead.com>
2017-10-21 00:50:08 -0500

    Keep Micromake C1 name on LCD?

--- Marlin/example_configurations/Micromake/C1/basic/Configuration.h
+++ Marlin/example_configurations/Micromake/C1/basic/Configuration.h
@@ -124,7 +124,7 @@
 
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
-//#define CUSTOM_MACHINE_NAME "3D Printer"
+#define CUSTOM_MACHINE_NAME "Micromake C1"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
