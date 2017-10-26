e06951cace242937a59404c36b357cbd3bfbd5c7
studiodyne <diabolomix@hotmail.fr>
2017-10-26 23:54:42 +0200

    Update ultralcd.cpp

--- Marlin/src/lcd/ultralcd.cpp
+++ Marlin/src/lcd/ultralcd.cpp
@@ -3713,6 +3713,9 @@ void kill_screen(const char* lcd_msg) {
         MENU_ITEM_EDIT(float52, MSG_CONTROL_RETRACT_RECOVER_SWAP, &fwretract.swap_retract_recover_length, -100, 100);
       #endif
       MENU_ITEM_EDIT(float3, MSG_CONTROL_RETRACT_RECOVERF, &fwretract.retract_recover_feedrate_mm_s, 1, 999);
+      #if EXTRUDERS > 1
+        MENU_ITEM_EDIT(float3, MSG_CONTROL_RETRACT_RECOVER_SWAPF, &fwretract.swap_retract_recover_feedrate_mm_s, 1, 999);//steeve
+	    #endif
       END_MENU();
     }
 
