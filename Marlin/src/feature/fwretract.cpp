0c7d4661453ac08e94313c43bb878391e57a4951
studiodyne <diabolomix@hotmail.fr>
2017-10-26 23:42:49 +0200

    Update fwretract.cpp

--- Marlin/src/feature/fwretract.cpp
+++ Marlin/src/feature/fwretract.cpp
@@ -158,7 +158,7 @@ void FWRetract::retract(const bool retracting
     if (hop_amount) {
       float temp_feedrate_mm_s=feedrate_mm_s; // backup the current feedrate 
       feedrate_mm_s = planner.max_feedrate_mm_s[Z_AXIS]; // Z feedrate to max
-	    current_position[Z_AXIS] -= retract_zlift;  // Pretend current pos is lower. Next move raises Z.
+      current_position[Z_AXIS] -= retract_zlift;  // Pretend current pos is lower. Next move raises Z.
       SYNC_PLAN_POSITION_KINEMATIC();             // Set the planner to the new position
       prepare_move_to_destination();              // Raise up to the old current pos
       feedrate_mm_s = temp_feedrate_mm_s  ; //feedrate restoration
@@ -170,7 +170,6 @@ void FWRetract::retract(const bool retracting
     const float move_e = swapping ? swap_retract_length + swap_retract_recover_length : retract_length + retract_recover_length;
     current_position[E_AXIS] -= move_e / planner.volumetric_multiplier[active_extruder];
     sync_plan_position_e();
-
     prepare_move_to_destination();  // Recover E
   }
 
