2296d988450eedfeb446c35282a16d7d01652541
Scott Lahteine <github@thinkyhead.com>
2017-10-26 22:46:11 -0500

    Tweak neopixel self-test

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -1013,13 +1013,13 @@ void servo_init() {
       pixels.show(); // initialize to all off
 
       #if ENABLED(NEOPIXEL_STARTUP_TEST)
-        delay(2000);
+        safe_delay(1000);
         set_neopixel_color(pixels.Color(255, 0, 0, 0));  // red
-        delay(2000);
+        safe_delay(1000);
         set_neopixel_color(pixels.Color(0, 255, 0, 0));  // green
-        delay(2000);
+        safe_delay(1000);
         set_neopixel_color(pixels.Color(0, 0, 255, 0));  // blue
-        delay(2000);
+        safe_delay(1000);
       #endif
       set_neopixel_color(pixels.Color(NEO_WHITE));       // white
     }
