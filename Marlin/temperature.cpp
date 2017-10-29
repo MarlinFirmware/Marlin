859c88219d6811cd8c70ebf7b295dd9de8d0be46
Scott Lahteine <thinkyhead@users.noreply.github.com>
2017-10-29 04:21:43 -0500

    Update temperature.cpp

--- Marlin/temperature.cpp
+++ Marlin/temperature.cpp
@@ -2067,12 +2067,13 @@ void Temperature::isr() {
 
     for (uint8_t e = 0; e < COUNT(temp_dir); e++) {
       const int16_t tdir = temp_dir[e], rawtemp = current_temperature_raw[e] * tdir;
-      bool heater_on =
-        # if ENABLED(PIDTEMP)
-          soft_pwm_amount[e] > 0;
-        # else
-          target_temperature[e] > 0;
-        # endif
+      const bool heater_on =
+        #if ENABLED(PIDTEMP)
+          soft_pwm_amount[e] > 0
+        #else
+          target_temperature[e] > 0
+        #endif
+      ;
       if (rawtemp > maxttemp_raw[e] * tdir && heater_on) max_temp_error(e);
       if (rawtemp < minttemp_raw[e] * tdir && !is_preheating(e) && heater_on) {
         #ifdef MAX_CONSECUTIVE_LOW_TEMPERATURE_ERROR_ALLOWED
@@ -2092,12 +2093,13 @@ void Temperature::isr() {
       #else
         #define GEBED >=
       #endif
-      bool bed_on =
-        # if ENABLED(PIDTEMPBED)
-          soft_pwm_amount_bed > 0;
-        # else
-          target_temperature_bed > 0;
-        # endif
+      const bool bed_on =
+        #if ENABLED(PIDTEMPBED)
+          soft_pwm_amount_bed > 0
+        #else
+          target_temperature_bed > 0
+        #endif
+      ;
       if (current_temperature_bed_raw GEBED bed_maxttemp_raw && bed_on) max_temp_error(-1);
       if (bed_minttemp_raw GEBED current_temperature_bed_raw && bed_on) min_temp_error(-1);
     #endif
