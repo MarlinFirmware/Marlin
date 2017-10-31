34160889f2e07d611429d98ea54a20f646034083
studiodyne <diabolomix@hotmail.fr>
2017-10-31 08:30:07 +0100

    Update pause.cpp

--- Marlin/src/feature/pause.cpp
+++ Marlin/src/feature/pause.cpp
@@ -381,8 +381,6 @@ void resume_print(const float &load_length/*=0*/, const float &initial_extrude_l
   #ifdef PAUSE_RET
     #if ENABLED(FWRETRACT)
       do_pause_e_move(PAUSE_RET, RETRACT_RECOVER_FEEDRATE);    
-    #elif  PAUSE_EXTRUDE_F
-      do_pause_e_move(PAUSE_RET, PAUSE_EXTRUDE_F); 
     #else 
       do_pause_e_move(PAUSE_RET, 15); 
     #endif
