c4a0b941d57d349f13ee693b72e44e004a2cd30c
stuartmp <stuartmp@internode.on.net>
2017-11-02 05:59:39 +1000

    Fixed un declared variable - PREHEAT_3_TEMP_HOTEND

--- Marlin/configuration_store.cpp
+++ Marlin/configuration_store.cpp
@@ -468,9 +468,9 @@ void MarlinSettings::postprocess() {
     #endif
 
     #if DISABLED(ULTIPANEL)
-      constexpr int lcd_preheat_hotend_temp[3] = { PREHEAT_1_TEMP_HOTEND, PREHEAT_2_TEMP_HOTEND, PREHEAT_3_TEMP_HOTEND },
-                    lcd_preheat_bed_temp[3] = { PREHEAT_1_TEMP_BED, PREHEAT_2_TEMP_BED, PREHEAT_3_TEMP_BED },
-                    lcd_preheat_fan_speed[3] = { PREHEAT_1_FAN_SPEED, PREHEAT_2_FAN_SPEED , PREHEAT_3_FAN_SPEED};  //Was [2]
+      constexpr int lcd_preheat_hotend_temp[3] = { PREHEAT_1_TEMP_HOTEND, PREHEAT_2_TEMP_HOTEND, PREHEAT_3_TEMP_HOTEND };
+                    lcd_preheat_bed_temp[3] = { PREHEAT_1_TEMP_BED, PREHEAT_2_TEMP_BED, PREHEAT_3_TEMP_BED };
+                    lcd_preheat_fan_speed[3] = { PREHEAT_1_FAN_SPEED, PREHEAT_2_FAN_SPEED , PREHEAT_3_FAN_SPEED}; 
     #endif
   
     EEPROM_WRITE(lcd_preheat_hotend_temp);
@@ -1251,14 +1251,15 @@ void MarlinSettings::reset() {
   #if ENABLED(ULTIPANEL)
     lcd_preheat_hotend_temp[0] = PREHEAT_1_TEMP_HOTEND;
     lcd_preheat_hotend_temp[1] = PREHEAT_2_TEMP_HOTEND;
-	lcd_preheat_hotend_temp[2] = PREHEAT_3_TEMP_HOTEND;
+	  lcd_preheat_hotend_temp[2] = PREHEAT_3_TEMP_HOTEND;
 	
     lcd_preheat_bed_temp[0] = PREHEAT_1_TEMP_BED;
     lcd_preheat_bed_temp[1] = PREHEAT_2_TEMP_BED;
-	lcd_preheat_bed_temp[2] = PREHEAT_3_TEMP_BED;
+	  lcd_preheat_bed_temp[2] = PREHEAT_3_TEMP_BED;
+   
     lcd_preheat_fan_speed[0] = PREHEAT_1_FAN_SPEED;
     lcd_preheat_fan_speed[1] = PREHEAT_2_FAN_SPEED;
-	lcd_preheat_fan_speed[2] = PREHEAT_3_FAN_SPEED;
+	  lcd_preheat_fan_speed[2] = PREHEAT_3_FAN_SPEED;
   #endif
 
   #if HAS_LCD_CONTRAST
