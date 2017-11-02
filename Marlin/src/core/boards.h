eb13b8d985aaf8d83fbfef91d09420ba907f910a
Scott Lahteine <github@thinkyhead.com>
2017-11-02 17:06:08 -0500

    Add MKS Gen L board

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -52,6 +52,7 @@
 #define BOARD_BAM_DICE_DUE      402   // 2PrintBeta BAM&DICE Due with STK drivers
 #define BOARD_MKS_BASE          40    // MKS BASE 1.0
 #define BOARD_MKS_13            47    // MKS v1.3 or 1.4 (maybe higher)
+#define BOARD_MKS_GEN_L         53    // MKS GEN L
 #define BOARD_ZRIB_V20          504   // zrib V2.0 control board (Chinese knock off RAMPS replica)
 #define BOARD_FELIX2            37    // Felix 2.0+ Electronics Board (RAMPS like)
 #define BOARD_RIGIDBOARD        42    // Invent-A-Part RigidBoard
