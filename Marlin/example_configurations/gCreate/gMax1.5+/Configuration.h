c9f88297b8058a5c7eac1bfbeb3bfbf46a378d18
Roxy-3D <Roxy-3D@users.noreply.github.com>
2017-11-12 13:09:10 -0600

    Provide more clearance for BL-Touch probe

--- Marlin/example_configurations/gCreate/gMax1.5+/Configuration.h
+++ Marlin/example_configurations/gCreate/gMax1.5+/Configuration.h
@@ -695,7 +695,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -17    // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -10    // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.87  // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.25  // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 7500
