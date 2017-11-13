e2724c68ab712b0a47b3e51abfe835c7cf94435f
Vitaliy Sapyanov <vynt-1@yandex.ru>
2017-11-14 00:32:43 +0300

    Update pins_RURAMPS4D.h
    Add and bugfix pins

--- Marlin/src/pins/pins_RURAMPS4D.h
+++ Marlin/src/pins/pins_RURAMPS4D.h
@@ -29,9 +29,7 @@
  *
  *  Differences between
  *     RADDS | RuRAMPS4DUE
- *       A13 | A9/D63 (shares the same pin with AUX2_4PIN)
- *       A14 | A10/D64 (shares the same pin with AUX2_5PIN)
- *       A15 | NC
+ *           |
  */
 
 #ifndef __SAM3X8E__
@@ -39,15 +37,13 @@
 #endif
 
 #ifndef BOARD_NAME
-  #define BOARD_NAME "RuRAMPS4Due"
+  #define BOARD_NAME       "RuRAMPS4Due"
 #endif
+#define RURAMPS4DUE        // #1550 in boards.h
 
 #undef INVERTED_HEATER_PINS
 #undef INVERTED_BED_PINS
 #undef INVERTED_FAN_PINS
-
-#define I2C_EEPROM
-//#define SPI_EEPROM
 
 //
 // Servos
@@ -77,18 +73,18 @@
 //
 // Steppers
 //
-#define X_STEP_PIN         37
+#define X_STEP_PIN         37 // Support Extension Board
 #define X_DIR_PIN          36
 #define X_ENABLE_PIN       38
 #define X_CS_PIN           -1
 
-#define Y_STEP_PIN         32
+#define Y_STEP_PIN         32 // Support Extension Board
 #define Y_DIR_PIN          35
 #define Y_ENABLE_PIN       34
 #define Y_CS_PIN           -1
 
-#define Z_STEP_PIN         30
-#define Z_DIR_PIN          2
+#define Z_STEP_PIN         30 // Support Extension Board
+#define Z_DIR_PIN           2
 #define Z_ENABLE_PIN       33
 #define Z_CS_PIN           -1
 
@@ -102,48 +98,43 @@
 #define E1_ENABLE_PIN      26
 #define E1_CS_PIN          -1
 
-#define E2_STEP_PIN        25 //14 //sys0724
-#define E2_DIR_PIN         23 //15
-#define E2_ENABLE_PIN      27 //61
+#define E2_STEP_PIN        25
+#define E2_DIR_PIN         23
+#define E2_ENABLE_PIN      27
 #define E2_CS_PIN          -1
 
-// For Extension Board V2
-// http://doku.radds.org/dokumentation/extension-board
-//#define E3_STEP_PIN        35
-//#define E3_DIR_PIN         33
-//#define E3_ENABLE_PIN      37
-//#define E3_CS_PIN           6
+#define E3_STEP_PIN        15 // Only For Extension Board
+#define E3_DIR_PIN         14
+#define E3_ENABLE_PIN      61
+#define E3_CS_PIN          -1
 
-//#define Z2_STEP_PIN        29
-//#define Z2_DIR_PIN         27
-//#define Z2_ENABLE_PIN      31
-//#define Z2_CS_PIN          39
-
-// Microstepping pins - Mapping not from fastio.h (?)
-//#define E3_MS1_PIN         67
-//#define E3_MS2_PIN         68
-//#define E3_MS3_PIN         69
-//#define Z2_MS1_PIN         67 // shared with E3_MS1_PIN
-//#define Z2_MS2_PIN         68 // shared with E3_MS2_PIN
-//#define Z2_MS3_PIN         69 // shared with E3_MS3_PIN
+// For Future: Microstepping pins - Mapping not from fastio.h (?)
+//#define E3_MS1_PIN         ?
+//#define E3_MS2_PIN         ?
+//#define E3_MS3_PIN         ?
+//#define Z2_MS1_PIN         ? // shared with E3_MS1_PIN
+//#define Z2_MS2_PIN         ? // shared with E3_MS2_PIN
+//#define Z2_MS3_PIN         ? // shared with E3_MS3_PIN
 
 //
-// Temperature Sensors
+// Limit Switches
 //
-#define TEMP_0_PIN          0  // ANALOG NUMBERING #54
-#define TEMP_1_PIN          1  // ANALOG NUMBERING #55
-#define TEMP_2_PIN          2  // ANALOG NUMBERING #56
-#define TEMP_3_PIN          3  // ANALOG NUMBERING #57
-#define TEMP_BED_PIN        4  // ANALOG NUMBERING #58
+#define X_MIN_PIN          45
+#define X_MAX_PIN          39
+#define Y_MIN_PIN          46
+#define Y_MAX_PIN          41
+#define Z_MIN_PIN          47
+#define Z_MAX_PIN          43
 
-/*
-// SPI for Max6675 or Max31855 Thermocouple
-#if DISABLED(SDSUPPORT)
-  #define MAX6675_SS       53
-#else
-  #define MAX6675_SS       49
+#if !ENABLED(Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN)
+  #define Z_PROBE_PIN      49
+#endif // else Z_PROBE_PIN = Z_MIN_PIN
+
+#if ENABLED(FILAMENT_RUNOUT_SENSOR)
+  #ifndef FIL_RUNOUT_PIN
+    #define FIL_RUNOUT_PIN   Y_MIN_PIN
+  #endif
 #endif
-*/
 
 //
 // Heaters / Fans
@@ -151,18 +142,73 @@
 #define HEATER_0_PIN       13
 #define HEATER_1_PIN       12
 #define HEATER_2_PIN       11
-#define HEATER_BED_PIN      7    // BED H1
+#define HEATER_BED_PIN      7  // BED H1
 
 #define FAN_PIN	            9
 #define FAN1_PIN            8
+#define CONTROLLER_FAN_PIN -1
+
+//
+// Temperature Sensors
+//
+#define TEMP_0_PIN          0  // ANALOG A0
+#define TEMP_1_PIN          1  // ANALOG A1
+#define TEMP_2_PIN          2  // ANALOG A2
+#define TEMP_3_PIN          3  // ANALOG A2
+#define TEMP_BED_PIN        4  // ANALOG A3
+//Thermocouple Use Analog Pins
+#if ENABLED(VER_WITH_THERMOCOUPLE) // If Nead, define is in Configuration.h
+  #define TEMP_4_PIN        5  // A5
+  #define TEMP_5_PIN        6  // A6 (Marlin 2.0 not support)
+#endif
+
+// SPI for Max6675 or Max31855 Thermocouple
+//#if DISABLED(SDSUPPORT)
+//  #define MAX6675_SS        53
+//#else
+//  #define MAX6675_SS        49
+//#endif
+
+//
+// Servos
+//
+#define SERVO0_PIN          5
+#define SERVO1_PIN          3
+#define SERVO2_PIN         -1
+#define SERVO3_PIN         -1
 
 //
 // Misc. Functions
 //
-#define SDSS                4   // 4,10,52 if using HW SPI.
-#define LED_PIN            13
+#define SDSS                4 // 4,10,52 if using HW SPI.
+#define LED_PIN            -1 // 13 - HEATER_0_PIN
+#define SDPOWER	           -1
+#define PS_ON_PIN          -1 // 65
+#define KILL_PIN           -1
 
-//#define FIL_RUNOUT_PIN    Y_MIN_PIN
+// MKS TFT / Nextion Use internal USART-1
+#define TFT_LCD_MODULE_COM        1
+#define TFT_LCD_MODULE_BAUDRATE   115600
+
+// ESP WiFi Use internal USART-2
+#define ESP_WIFI_MODULE_COM       2
+#define ESP_WIFI_MODULE_BAUDRATE  115600
+#define ESP_WIFI_MODULE_RESET_PIN -1
+#define PIGGY_GPIO_PIN            -1
+
+//
+// EEPROM
+//
+#define E2END 0x8000  // 32Kb (24lc256)
+#define I2C_EEPROM    // EEPROM on I2C-0
+//#define EEPROM_SD   // EEPROM on SDCARD
+//#define SPI_EEPROM  // EEPROM on SPI-0
+//#define SPI_CHAN_EEPROM1        ?
+//#define SPI_EEPROM1_CS          ?
+// 2K EEPROM
+//#define SPI_EEPROM2_CS          ?
+// 32Mb FLASH
+//#define SPI_FLASH_CS            ?
 
 //
 // LCD / Controller
@@ -186,39 +232,38 @@
     #define SD_DETECT_PIN   51
 
   #elif ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-    #define LCD_PINS_RS     52   //?
-    #define LCD_PINS_ENABLE 53   //?
-    #define LCD_PINS_D4     48   //?
-    #define LCD_PINS_D5     50   //?
-    #define LCD_PINS_D6     52   //?
-    #define LCD_PINS_D7     53   //?
+    #define LCD_PINS_RS     52
+    #define LCD_PINS_ENABLE 53
+    #define LCD_PINS_D4     48
+    #define LCD_PINS_D5     50
+    #define LCD_PINS_D6     52
+    #define LCD_PINS_D7     53
 
     #define BEEPER_PIN      62
 
-    #define BTN_EN1         44   //!
-    #define BTN_EN2         42   //!
+    #if ENABLED(REPRAP_DISCOUNT_SMART_CONTROLLER)
+     #define BTN_EN1        44
+     #define BTN_EN2        42
+     #define BTN_ENC        40
+     #define SD_DETECT_PIN  51
+    #endif
+  #elif ENABLED(SSD1306_OLED_I2C_CONTROLLER)
+    #define BTN_EN1         44
+    #define BTN_EN2         42
     #define BTN_ENC         40
+    #define BEEPER_PIN      62
+    #define LCD_SDSS        10
     #define SD_DETECT_PIN   51
 
-  #elif ENABLED(SSD1306_OLED_I2C_CONTROLLER)
-
-    #define BTN_EN1         44   //?
-    #define BTN_EN2         42   //?
-    #define BTN_ENC         40   //?
-    #define SD_DETECT_PIN   51   //?
-
-    #define BEEPER_PIN      62   //?
-    #define LCD_SDSS        10   //?
-
-  #elif ENABLED(SPARK_FULL_GRAPHICS)   //!!! - check!
-
-    #define LCD_PINS_D4     29
-    #define LCD_PINS_ENABLE 27
-    #define LCD_PINS_RS     25
-    #define BTN_EN1         35
-    #define BTN_EN2         33
-    #define BTN_ENC         37
-
+  #elif ENABLED(SPARK_FULL_GRAPHICS)
+    //http://doku.radds.org/dokumentation/other-electronics/sparklcd/
+    #error "Oops! SPARK_FULL_GRAPHICS not supported with this board"
+    //#define LCD_PINS_D4     29//?
+    //#define LCD_PINS_ENABLE 27//?
+    //#define LCD_PINS_RS     25//?
+    //#define BTN_EN1         35//?
+    //#define BTN_EN2         33//?
+    //#define BTN_ENC         37//?
   #endif // SPARK_FULL_GRAPHICS
 
 #endif // ULTRA_LCD
