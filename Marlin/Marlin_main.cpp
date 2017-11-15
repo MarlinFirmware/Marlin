c7bd2933561236b8448f82146ed0075498436987
RowanMeara <rrm89@cornell.edu>
2017-11-14 18:52:58 -0800

    Fix G28 Bug

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -4087,7 +4087,11 @@ inline void gcode_G28(const bool always_home_all) {
 
       if (home_all || homeX || homeY) {
         // Raise Z before homing any other axes and z is not already high enough (never lower z)
-        destination[Z_AXIS] = Z_HOMING_HEIGHT;
+        // We should never move more than Z_HOMING_HEIGHT distance.
+        if (current_position[Z_AXIS] < 0.0f)
+          destination[Z_AXIS] = current_position[Z_AXIS] + Z_HOMING_HEIGHT;
+        else
+          destination[Z_AXIS] = Z_HOMING_HEIGHT;
         if (destination[Z_AXIS] > current_position[Z_AXIS]) {
 
           #if ENABLED(DEBUG_LEVELING_FEATURE)
