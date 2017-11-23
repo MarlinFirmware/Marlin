2c1cd41d17ffe47318fb5d0bb6d16b941ad94411
peteroon <33668694+peteroon@users.noreply.github.com>
2017-11-23 17:41:41 -0500

    probe faster

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -693,7 +693,7 @@
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 4000
+#define XY_PROBE_SPEED 5000
 
 // Speed for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
