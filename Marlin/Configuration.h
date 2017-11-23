c42855abc95b4c4a18c02c923b9e7a90cfacb824
peteroon <33668694+peteroon@users.noreply.github.com>
2017-11-23 16:25:02 -0500

    change insets, make probing super slow

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -699,7 +699,7 @@
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
 
 // Speed for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
+#define Z_PROBE_SPEED_SLOW Z_PROBE_SPEED_FAST
 
 // The number of probes to perform at each point.
 //   Set to 2 for a fast/slow probe, using the second probe result.
