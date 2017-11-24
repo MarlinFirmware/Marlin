c1fce2daee984906324ce275fbc2e38cab952680
peteroon <33668694+peteroon@users.noreply.github.com>
2017-11-24 14:26:50 -0500

    no bed trolling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -449,7 +449,7 @@
  */
 
 #define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
-#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
+//#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
 
 //===========================================================================
 //============================= Mechanical Settings =========================
