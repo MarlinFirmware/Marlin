06c403939473e815de2f897b9f7c56f4c65805b9
peteroon <33668694+peteroon@users.noreply.github.com>
2017-11-24 14:39:43 -0500

    make homing origin 0,0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -785,12 +785,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 450
-#define Y_BED_SIZE 450
+#define X_BED_SIZE 540
+#define Y_BED_SIZE 495
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -90
-#define Y_MIN_POS -45
+#define X_MIN_POS 0
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
@@ -1036,7 +1036,7 @@
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing when homing all axes (G28).
 // - Prevent Z homing when the Z probe is outside bed area.
 //
-#define Z_SAFE_HOMING
+//#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT (X_MIN_POS)    // X point for Z homing when homing all axis (G28).
