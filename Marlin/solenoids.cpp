25f04192f217757f80d24a1b1571c1a6dd66412f
pizzyflavin <pizzyflavin@gmail.com>
2017-11-30 11:28:05 -0500

    Update solenoid files to work with new implementation

--- Marlin/solenoids.cpp
+++ Marlin/solenoids.cpp
@@ -1,14 +1,11 @@
 /*
  *  Source file for solenoids.
  *
- *  Contains functions to open and close solenoids
+ *  Contains functions to activate and deactivate solenoids
  *
  *  Author: Ricky Rininger
  *  Date:   25 July, 2017
  */
-
-#include <avr/io.h>
-#include <stdint.h>
 
 #include "solenoids.h"
 
@@ -17,27 +14,44 @@
 //===========================================================================
 
 /*
- *  Open given solenoid
+ *  Initialize solenoids
  */
-void solenoid_open(uint8_t solenoid) {
-    switch (solenoid) {
+void solenoid_init(void) {
+    // Set all outputs low
+    SOLENOID_0_PORT &= ~SOLENOID_0;
+    SOLENOID_1_PORT &= ~SOLENOID_1;
+    SOLENOID_2_PORT &= ~SOLENOID_2;
+    SOLENOID_3_PORT &= ~SOLENOID_3;
+
+    // Set pin direction as output
+    SOLENOID_0_DDR |= SOLENOID_0;
+    SOLENOID_1_DDR |= SOLENOID_1;
+    SOLENOID_2_DDR |= SOLENOID_2;
+    SOLENOID_3_DDR |= SOLENOID_3;
+
+    // Set all solenoids to "inactive"
+    // (remember, index starts at 1 and not 0)
+    for (uint8_t solenoid = 1; solenoid <= MAX_SOLENOIDS; solenoid++) {
+        solenoid_deactivate(solenoid);
+    }
+}
+
+/*
+ *  Activate given solenoid
+ */
+void solenoid_activate(uint8_t solenoid) {
+    switch (solenoid - 1) {
         case 0:
-            SOL_PORT_A |= SOL_0;
+            SOLENOID_0_PORT |= SOLENOID_0;
             break;
         case 1:
-            SOL_PORT_A |= SOL_1;
+            SOLENOID_1_PORT |= SOLENOID_1;
             break;
         case 2:
-            SOL_PORT_A |= SOL_2;
+            SOLENOID_2_PORT |= SOLENOID_2;
             break;
         case 3:
-            SOL_PORT_A |= SOL_3;
-            break;
-        case 4:
-            SOL_PORT_B |= SOL_4;
-            break;
-        case 5:
-            SOL_PORT_B |= SOL_5;
+            SOLENOID_3_PORT |= SOLENOID_3;
             break;
         default:
             // Do nothing
@@ -46,27 +60,21 @@ void solenoid_open(uint8_t solenoid) {
 }
 
 /*
- *  Close given solenoid
+ *  Deactivate given solenoid
  */
-void solenoid_close(uint8_t solenoid) {
-    switch (solenoid) {
+void solenoid_deactivate(uint8_t solenoid) {
+    switch (solenoid - 1) {
         case 0:
-            SOL_PORT_A &= ~SOL_0;
+            SOLENOID_0_PORT &= ~SOLENOID_0;
             break;
         case 1:
-            SOL_PORT_A &= ~SOL_1;
+            SOLENOID_1_PORT &= ~SOLENOID_1;
             break;
         case 2:
-            SOL_PORT_A &= ~SOL_2;
+            SOLENOID_2_PORT &= ~SOLENOID_2;
             break;
         case 3:
-            SOL_PORT_A &= ~SOL_3;
-            break;
-        case 4:
-            SOL_PORT_B &= ~SOL_4;
-            break;
-        case 5:
-            SOL_PORT_B &= ~SOL_5;
+            SOLENOID_3_PORT &= ~SOLENOID_3;
             break;
         default:
             // Do nothing
--- Marlin/solenoids.h
+++ Marlin/solenoids.h
@@ -6,6 +6,7 @@
  *
  *
  ***************************************************************************/
+#include <avr/io.h>
 
 #ifndef SOLENOIDS_H_
 #define SOLENOIDS_H_
@@ -13,38 +14,47 @@
 //===========================================================================
 // Pin Definitions
 //===========================================================================
-#define SOL_PORT_A      (PORTH)     // PORT for SOL_0 - SOL_2
-#define SOL_DDR_A       (DDRH)      // DDR for SOL_0 - SOL_2
 
-#define SOL_0           (_BV(6))    // PORT H PIN 6
-#define SOL_1           (_BV(5))    // PORT H PIN 5
-#define SOL_2           (_BV(4))    // PORT H PIN 4
-#define SOL_3           (_BV(3))    // PORT H PIN 3
+// Constant Definitions
+#define MAX_SOLENOIDS           (4)
 
-#define SOL_PORT_B      (PORTE)     // SOL 4 and 5 connectors uses PORTE
-#define SOL_DDR_B       (DDRE)      // ^ uses DDRE
+// Port Definitions
+#define SOLENOID_0_PORT         PORTJ
+#define SOLENOID_1_PORT         PORTJ
+#define SOLENOID_2_PORT         PORTJ
+#define SOLENOID_3_PORT         PORTJ
 
-#define SOL_4           (_BV(5))    // PORT E PIN 5
-#define SOL_5           (_BV(4))    // PORT E PIN 4
+// DDR Definitions
+#define SOLENOID_0_DDR          DDRJ
+#define SOLENOID_1_DDR          DDRJ
+#define SOLENOID_2_DDR          DDRJ
+#define SOLENOID_3_DDR          DDRJ
 
-//===========================================================================
-// Properties
-//===========================================================================
-#define NUM_SOLENOIDS   (6)
+// Pin Definitions
+#define SOLENOID_0              (1 << 7)
+#define SOLENOID_1              (1 << 4)
+#define SOLENOID_2              (1 << 5)
+#define SOLENOID_3              (1 << 6)
+
 
 //===========================================================================
 // Function Prototypes
 //===========================================================================
 
 /*
- *  Open given solenoid
+ *  Init solenoids
  */
-void solenoid_open(uint8_t solenoid);
+void solenoid_init();
 
 /*
- *  Close given solenoid
+ *  Activate given solenoid
  */
-void solenoid_close(uint8_t solenoid);
+void solenoid_activate(uint8_t solenoid);
+
+/*
+ *  Deactivate given solenoid
+ */
+void solenoid_deactivate(uint8_t solenoid);
 
 #endif // SOLENOIDS_H
 
