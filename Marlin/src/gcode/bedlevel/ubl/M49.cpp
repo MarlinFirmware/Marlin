f4b16a3bacbe24434c09105f25ccf62fd23a330c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2017-12-03 03:34:56 -0600

    Update M49.cpp

--- Marlin/src/gcode/bedlevel/ubl/M49.cpp
+++ Marlin/src/gcode/bedlevel/ubl/M49.cpp
@@ -34,8 +34,7 @@
 void GcodeSuite::M49() {
   g26_debug_flag ^= true;
   SERIAL_PROTOCOLPGM("G26 Debug: ");
-  serialprintPGM(g26_debug_flag ? PSTR("On") : PSTR("Off"));
-  SERIAL_EOL();
+  serialprintPGM(g26_debug_flag ? PSTR("On\n") : PSTR("Off\n"));
 }
 
 #endif // G26_MESH_VALIDATION
