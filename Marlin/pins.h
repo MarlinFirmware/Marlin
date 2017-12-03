f36fdc74ac5220d705862483a8c4b45e3297166e
kein0r <github@dummgebabbel.de>
2017-12-03 14:20:01 -0500

    Added RAMPS v1.4 Plus board
    Conflicts:
    	Marlin/boards.h

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -71,6 +71,21 @@
 #elif MB(RAMPS_14_SF)
   #define IS_RAMPS_SF
   #include "pins_RAMPS.h"
+#elif MB(RAMPS_PLUS_EFB)
+  #define IS_RAMPS_EFB
+  #include "pins_RAMPS_PLUS.h"
+#elif MB(RAMPS_PLUS_EEB)
+  #define IS_RAMPS_EEB
+  #include "pins_RAMPS_PLUS.h"
+#elif MB(RAMPS_PLUS_EFF)
+  #define IS_RAMPS_EFF
+  #include "pins_RAMPS_PLUS.h"
+#elif MB(RAMPS_PLUS_EEF)
+  #define IS_RAMPS_EEF
+  #include "pins_RAMPS_PLUS.h"
+#elif MB(RAMPS_PLUS_SF)
+  #define IS_RAMPS_SF
+  #include "pins_RAMPS_PLUS.h"
 
 //
 // RAMPS Derivatives - ATmega1280, ATmega2560
