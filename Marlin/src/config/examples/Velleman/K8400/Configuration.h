69d49a2438203fc50695009a23902c60ff5cf35a
Scott Lahteine <github@thinkyhead.com>
2017-12-09 06:29:14 -0600

    Shorter segments may be better

--- Marlin/src/config/examples/Velleman/K8400/Configuration.h
+++ Marlin/src/config/examples/Velleman/K8400/Configuration.h
@@ -889,7 +889,8 @@
   #define ENABLE_LEVELING_FADE_HEIGHT
 
   // For Cartesian machines, instead of dividing moves on mesh boundaries,
-  // split up moves into short segments like a Delta.
+  // split up moves into short segments like a Delta. This follows the
+  // contours of the bed more closely than edge-to-edge straight moves.
   #define SEGMENT_LEVELED_MOVES
   #define LEVELED_SEGMENT_LENGTH 5.0 // (mm) Length of all segments (except the last one)
 
