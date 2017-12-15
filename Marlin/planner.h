f38f2ace5a82cd612ff699a3cd1617fe2ad16a3d
Scott Lahteine <thinkyhead@users.noreply.github.com>
2017-12-15 14:23:20 -0600

    Correct unskew, after all

--- Marlin/planner.h
+++ Marlin/planner.h
@@ -359,7 +359,7 @@ class Planner {
 
       FORCE_INLINE static void unskew(float &cx, float &cy, const float &cz) {
         if (WITHIN(cx, X_MIN_POS, X_MAX_POS) && WITHIN(cy, Y_MIN_POS, Y_MAX_POS)) {
-          const float sx = cx + cy * xy_skew_factor + cz * (xz_skew_factor - (xy_skew_factor * yz_skew_factor)),
+          const float sx = cx + cy * xy_skew_factor + cz * xz_skew_factor,
                       sy = cy + cz * yz_skew_factor;
           if (WITHIN(sx, X_MIN_POS, X_MAX_POS) && WITHIN(sy, Y_MIN_POS, Y_MAX_POS)) {
             cx = sx; cy = sy;
