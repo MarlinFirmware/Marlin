6678dfa2d7e009d7a406faeedd915676cc4ce184
Scott Lahteine <thinkyhead@users.noreply.github.com>
2017-12-19 20:45:53 -0600

    Update HAL_Stm32f1.cpp

--- Marlin/src/HAL/HAL_STM32F1/HAL_Stm32f1.cpp
+++ Marlin/src/HAL/HAL_STM32F1/HAL_Stm32f1.cpp
@@ -87,8 +87,7 @@ uint8 adc_pins[] = {
   #endif
 };
 
-enum TEMP_PINS
-{
+enum TEMP_PINS {
   #if HAS_TEMP_0
     TEMP_0,
   #endif
@@ -108,11 +107,10 @@ enum TEMP_PINS
     TEMP_BED,
   #endif
   #if ENABLED(FILAMENT_WIDTH_SENSOR)
-    FILWIDTH
+    FILWIDTH,
   #endif
     ADC_PIN_COUNT
 };
-
 
 uint16_t HAL_adc_results[ADC_PIN_COUNT];
 
@@ -197,27 +195,27 @@ void HAL_adc_init(void) {
 void HAL_adc_start_conversion(const uint8_t adc_pin) {
   TEMP_PINS pin_index;
   switch (adc_pin) {
-#if HAS_TEMP_0
-    case TEMP_0_PIN: pin_index = TEMP_0; break;
-#endif
-#if HAS_TEMP_1
-    case TEMP_1_PIN: pin_index = TEMP_1; break;
-#endif
-#if HAS_TEMP_2
-    case TEMP_2_PIN: pin_index = TEMP_2; break;
-#endif
-#if HAS_TEMP_3
-    case TEMP_3_PIN: pin_index = TEMP_3; break;
-#endif
-#if HAS_TEMP_4
-    case TEMP_4_PIN: pin_index = TEMP_4; break;
-#endif
-#if HAS_TEMP_BED
-    case TEMP_BED_PIN: pin_index = TEMP_BED; break;
-#endif
-#if ENABLED(FILAMENT_WIDTH_SENSOR)
-    case FILWIDTH_PIN: pin_index = FILWIDTH; break;
-#endif
+    #if HAS_TEMP_0
+      case TEMP_0_PIN: pin_index = TEMP_0; break;
+    #endif
+    #if HAS_TEMP_1
+      case TEMP_1_PIN: pin_index = TEMP_1; break;
+    #endif
+    #if HAS_TEMP_2
+      case TEMP_2_PIN: pin_index = TEMP_2; break;
+    #endif
+    #if HAS_TEMP_3
+      case TEMP_3_PIN: pin_index = TEMP_3; break;
+    #endif
+    #if HAS_TEMP_4
+      case TEMP_4_PIN: pin_index = TEMP_4; break;
+    #endif
+    #if HAS_TEMP_BED
+      case TEMP_BED_PIN: pin_index = TEMP_BED; break;
+    #endif
+    #if ENABLED(FILAMENT_WIDTH_SENSOR)
+      case FILWIDTH_PIN: pin_index = FILWIDTH; break;
+    #endif
   }
   HAL_adc_result = (HAL_adc_results[(int)pin_index] >> 2) & 0x3FF; // shift to get 10 bits only.
 }
