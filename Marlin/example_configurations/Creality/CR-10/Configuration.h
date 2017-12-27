d1cad5afa47cbf593f2174c6b58fc3102ac3d5e4
Scott Lahteine <github@thinkyhead.com>
2017-12-27 02:05:11 -0600

    Apply FILAMENT_LOAD_UNLOAD_GCODES to example configs

--- Marlin/example_configurations/Creality/CR-10/Configuration.h
+++ Marlin/example_configurations/Creality/CR-10/Configuration.h
@@ -1174,7 +1174,7 @@
  *    P1  Raise the nozzle always to Z-park height.
  *    P2  Raise the nozzle by Z-park amount, limited to Z_MAX_POS.
  */
-//#define NOZZLE_PARK_FEATURE
+#define NOZZLE_PARK_FEATURE
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z }
