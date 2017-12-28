d57309a7849f5e7a919476cc46a71bfb2b74b6ac
teemuatlut <teemu.mantykallio@live.fi>
2017-12-28 18:21:38 +0200

    Fix compile issue

--- Marlin/src/HAL/SPI.h
+++ Marlin/src/HAL/SPI.h
@@ -79,8 +79,6 @@ void spiSendBlock(uint8_t token, const uint8_t* buf);
 /** Begin SPI transaction, set clock, bit order, data mode */
 void spiBeginTransaction(uint32_t spiClock, uint8_t bitOrder, uint8_t dataMode);
 
-#endif // SPI_FULL_SPEED
-
 #ifdef TARGET_LPC1768
   #include "HAL_LPC1768/LPC_SPI.h"
 #endif
