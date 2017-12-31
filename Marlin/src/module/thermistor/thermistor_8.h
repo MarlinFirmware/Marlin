501fd37a7e5590547d2b9a9196af70df4aeeba59
Arne Husth <ah@io2.dk>
2017-12-31 16:59:32 +0100

    Update of comments in thermistor files. No functional changes.

--- Marlin/src/module/thermistor/thermistor_8.h
+++ Marlin/src/module/thermistor/thermistor_8.h
@@ -20,7 +20,7 @@
  *
  */
 
-// 100k 0603 SMD Vishay NTCS0603E3104FXT (4.7k pullup)
+// R25 = 100 kOhm, beta25 = 3950 K, 10 kOhm pull-up, NTCS0603E3104FHT
 const short temptable_8[][2] PROGMEM = {
   { OV(   1), 704 },
   { OV(  54), 216 },
