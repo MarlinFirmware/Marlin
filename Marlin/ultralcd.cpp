292d31123c49d8fba214d2201d197ae3b523b000
Roxy-3D <Roxy-3D@users.noreply.github.com>
2018-01-01 14:39:28 -0600

    Z BabyStepping should not be dependent on a bed leveling system being active

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -1131,10 +1131,7 @@ void kill_screen(const char* lcd_msg) {
 
           const float new_zoffset = zprobe_zoffset + planner.steps_to_mm[Z_AXIS] * babystep_increment;
           if (WITHIN(new_zoffset, Z_PROBE_OFFSET_RANGE_MIN, Z_PROBE_OFFSET_RANGE_MAX)) {
-
-            if (planner.leveling_active)
-              thermalManager.babystep_axis(Z_AXIS, babystep_increment);
-
+            thermalManager.babystep_axis(Z_AXIS, babystep_increment);
             zprobe_zoffset = new_zoffset;
             lcdDrawUpdate = LCDVIEW_CALL_REDRAW_NEXT;
           }
