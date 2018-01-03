d3139e47f8bae8c0e02540d08581890a1e2a2e0e
Tannoo <tannoo@msn.com>
2018-01-03 03:28:15 -0700

    [1.1.x] EEPROM init fix (#9002)

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -3127,8 +3127,7 @@ void kill_screen(const char* lcd_msg) {
   #if ENABLED(EEPROM_SETTINGS)
 
     static void lcd_init_eeprom() {
-      lcd_factory_settings();
-      settings.save();
+      lcd_completion_feedback(settings.init_eeprom());
       lcd_goto_previous_menu();
     }
 
