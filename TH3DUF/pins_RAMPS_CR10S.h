1b0f9bb50625bc4799ec5ffb7fa94ccd5a14c9b2
houseofbugs <tim@th3dstudio.com>
2018-01-03 19:44:44 -0600

    Trying to Fix CR-10S SD Issues

--- TH3DUF/pins_RAMPS_CR10S.h
+++ TH3DUF/pins_RAMPS_CR10S.h
@@ -389,7 +389,7 @@
       #endif
 
       #define BTN_ENC           35
-      #define SD_DETECT_PIN     49
+      #define SD_DETECT_PIN     -1
       #define KILL_PIN          41
 
       #if ENABLED(BQ_LCD_SMART_CONTROLLER)
