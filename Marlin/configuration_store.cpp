bf7a2b6f43ca4c412370d981403e170832cc22b6
Scott Lahteine <github@thinkyhead.com>
2018-01-03 20:40:59 -0600

    Write filler for disabled volumetrics

--- Marlin/configuration_store.cpp
+++ Marlin/configuration_store.cpp
@@ -587,6 +587,13 @@ void MarlinSettings::postprocess() {
         EEPROM_WRITE(dummy);
       }
 
+    #else
+
+      const bool volumetric_enabled = false;
+      dummy = DEFAULT_NOMINAL_FILAMENT_DIA;
+      EEPROM_WRITE(volumetric_enabled);
+      for (uint8_t q = MAX_EXTRUDERS; q--;) EEPROM_WRITE(dummy);
+
     #endif
 
     //
@@ -1087,6 +1094,11 @@ void MarlinSettings::postprocess() {
           EEPROM_READ(dummy);
           if (q < COUNT(planner.filament_size)) planner.filament_size[q] = dummy;
         }
+
+      #else
+
+        EEPROM_READ(dummyb);
+        for (uint8_t q=MAX_EXTRUDERS; q--;) EEPROM_READ(dummy);
 
       #endif
 
