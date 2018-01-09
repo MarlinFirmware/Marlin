b97a664b7b672d621ef9cdb8c61ac11673c4ac91
GMagician <3684609+GMagician@users.noreply.github.com>
2018-01-09 23:53:44 +0100

    1.1.x semitec 104NT-4 added (#9120)
    Table is very very similar to old 104-GT2 so I added only the name and recycled old ID

--- Marlin/example_configurations/Wanhao/Duplicator 6/Configuration.h
+++ Marlin/example_configurations/Wanhao/Duplicator 6/Configuration.h
@@ -253,7 +253,7 @@
  *     2 : 200k thermistor - ATC Semitec 204GT-2 (4.7k pullup)
  *     3 : Mendel-parts thermistor (4.7k pullup)
  *     4 : 10k thermistor !! do not use it for a hotend. It gives bad resolution at high temp. !!
- *     5 : 100K thermistor - ATC Semitec 104GT-2 (Used in ParCan & J-Head) (4.7k pullup)
+ *     5 : 100K thermistor - ATC Semitec 104GT-2/104NT-4-R025H42G (Used in ParCan & J-Head) (4.7k pullup)
  *     6 : 100k EPCOS - Not as accurate as table 1 (created using a fluke thermocouple) (4.7k pullup)
  *     7 : 100k Honeywell thermistor 135-104LAG-J01 (4.7k pullup)
  *    71 : 100k Honeywell thermistor 135-104LAF-J01 (4.7k pullup)
