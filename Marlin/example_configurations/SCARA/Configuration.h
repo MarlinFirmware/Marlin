2fcef3e8e2181d4eecb59d7c9323b6f5bb2f390a
Dave Johnson <davejohnson3000@gmail.com>
2018-01-09 16:41:02 -0800

    Enable USE_ZMIN_PLUG on SCARA example (#9125)

--- Marlin/example_configurations/SCARA/Configuration.h
+++ Marlin/example_configurations/SCARA/Configuration.h
@@ -484,10 +484,10 @@
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
 #define USE_YMIN_PLUG
-//#define USE_ZMIN_PLUG
+#define USE_ZMIN_PLUG
 //#define USE_XMAX_PLUG
 //#define USE_YMAX_PLUG
-#define USE_ZMAX_PLUG
+//#define USE_ZMAX_PLUG
 
 // coarse Endstop Settings
 //#define ENDSTOPPULLUPS // Comment this out (using // at the start of the line) to disable the endstop pullup resistors
