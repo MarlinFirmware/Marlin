891581c328fd8f67e8c745b505a29a32d2ed3794
Marvin Sinister <marvin.sinister@gmail.com>
2018-01-13 13:32:22 +0100

    fix type cast

--- Marlin/src/HAL/HAL_STM32F1/HAL_spi_Stm32f1.cpp
+++ Marlin/src/HAL/HAL_STM32F1/HAL_spi_Stm32f1.cpp
@@ -166,7 +166,8 @@ void spiSendBlock(uint8_t token, const uint8_t* buf) {
 
 /** Begin SPI transaction, set clock, bit order, data mode */
 void spiBeginTransaction(uint32_t spiClock, uint8_t bitOrder, uint8_t dataMode) {
-  spiConfig = SPISettings(spiClock, bitOrder, dataMode);
+  //spiConfig = SPISettings(spiClock, bitOrder, dataMode);
+  spiConfig = SPISettings(spiClock, (BitOrder)bitOrder, dataMode);
 
   SPI.beginTransaction(spiConfig);
 }
