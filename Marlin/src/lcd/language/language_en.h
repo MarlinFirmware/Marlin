f887719a409cb562d98b4d2b340de428246e7c2d
Scott Lahteine <github@thinkyhead.com>
2018-01-15 03:14:23 -0600

    Creality Ender board support

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -37,7 +37,7 @@
 #undef en
 
 #ifndef WELCOME_MSG
-  #define WELCOME_MSG                         MACHINE_NAME _UxGT(" ready.")
+  #define WELCOME_MSG                         MACHINE_NAME _UxGT(" Ready.")
 #endif
 #ifndef MSG_BACK
   #define MSG_BACK                            _UxGT("Back")
