ad0a1763850bb514ee98ed63536df542bbea51d2
Roxy-3D <Roxanne.Rae.Neufeld@gmail.com>
2018-01-16 22:55:07 -0600

    Regression fixes for UBL's G29 P1, G29 P2, G29 P4 and G26
    The changes to ultralcd.cpp for is_lcd_clicked() did not encompass the
    full functionality of UBL's G29 P1, P2 and P4.   It also broke G26's
    ability to abort in several of its phases.
    
    This is the first pass at fixing the problem.   It has been tested for
    correctness for several hours but more testing  needs to be done.
    There may be a few follow up patches to finish covering all the corner
    cases, but right now I need to merge this before any conflicts show up.
    
    Some of these changes will need to be moved over to the bugfix-v2.0.0
    branch.   That will happen a few days from now.

--- Marlin/ubl.h
+++ Marlin/ubl.h
@@ -65,7 +65,7 @@
 
   #if ENABLED(ULTRA_LCD)
     extern char lcd_status_message[];
-    void lcd_quick_feedback();
+    void lcd_quick_feedback(const bool clear_buttons);
   #endif
 
   #define MESH_X_DIST (float(MESH_MAX_X - (MESH_MIN_X)) / float(GRID_MAX_POINTS_X - 1))
@@ -91,7 +91,7 @@
       #if ENABLED(NEWPANEL)
         static void move_z_with_encoder(const float &multiplier);
         static float measure_point_with_encoder();
-        static float measure_business_card_thickness(const float&);
+        static float measure_business_card_thickness(const float);
         static void manually_probe_remaining_mesh(const float&, const float&, const float&, const float&, const bool);
         static void fine_tune_mesh(const float &rx, const float &ry, const bool do_ubl_mesh_map);
       #endif
