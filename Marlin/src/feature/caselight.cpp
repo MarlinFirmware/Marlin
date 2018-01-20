af49a469f32548b277ed155c1508d762e0b0a1a7
Scott Lahteine <github@thinkyhead.com>
2018-01-20 16:15:14 -0600

    Add option to use Neopixel as case light

--- Marlin/src/feature/caselight.cpp
+++ Marlin/src/feature/caselight.cpp
@@ -27,6 +27,17 @@
 uint8_t case_light_brightness = CASE_LIGHT_DEFAULT_BRIGHTNESS;
 bool case_light_on = CASE_LIGHT_DEFAULT_ON;
 
+#if ENABLED(CASE_LIGHT_USE_NEOPIXEL)
+  #include "leds/leds.h"
+  LEDColor case_light_color =
+    #ifdef CASE_LIGHT_NEOPIXEL_COLOR
+      CASE_LIGHT_NEOPIXEL_COLOR
+    #else
+      { 255, 255, 255, 255 }
+    #endif
+  ;
+#endif
+
 /**
  * The following are needed because ARM chips ignore a "WRITE(CASE_LIGHT_PIN,x)" command to the pins that
  * are directly controlled by the PWM module. In order to turn them off the brightness level needs to be
@@ -34,32 +45,39 @@ bool case_light_on = CASE_LIGHT_DEFAULT_ON;
  * to save it.
  */
 uint8_t case_light_brightness_sav;   // saves brighness info so can restore when "M355 S1" received
-bool case_light_arg_flag;  // flag to notify if S or P arguement type
+bool case_light_arg_flag;  // flag to notify if S or P argument type
 
 #ifndef INVERT_CASE_LIGHT
   #define INVERT_CASE_LIGHT false
 #endif
 
 void update_case_light() {
-  SET_OUTPUT(CASE_LIGHT_PIN);
 
   if (!(case_light_arg_flag && !case_light_on))
-    case_light_brightness_sav = case_light_brightness;  // save brightness except if this is an S0 arguement
+    case_light_brightness_sav = case_light_brightness;  // save brightness except if this is an S0 argument
   if (case_light_arg_flag && case_light_on)
-    case_light_brightness = case_light_brightness_sav;  // restore last brightens if this is an S1 arguement
+    case_light_brightness = case_light_brightness_sav;  // restore last brightens if this is an S1 argument
 
-  if (case_light_on) {
+  const uint8_t i = case_light_on ? case_light_brightness : 0, n10ct = INVERT_CASE_LIGHT ? 255 - i : i;
+
+  #if ENABLED(CASE_LIGHT_USE_NEOPIXEL)
+
+    leds.set_color(
+      MakeLEDColor(case_light_color.r, case_light_color.g, case_light_color.b, case_light_color.w, n10ct),
+      false
+    );
+
+  #else // !CASE_LIGHT_USE_NEOPIXEL
+
+    SET_OUTPUT(CASE_LIGHT_PIN);
     if (USEABLE_HARDWARE_PWM(CASE_LIGHT_PIN))
-      analogWrite(CASE_LIGHT_PIN, INVERT_CASE_LIGHT ? 255 - case_light_brightness : case_light_brightness);
-    else
-      WRITE(CASE_LIGHT_PIN, INVERT_CASE_LIGHT ? LOW : HIGH);
-  }
-  else {
-    if (USEABLE_HARDWARE_PWM(CASE_LIGHT_PIN))
-      analogWrite(CASE_LIGHT_PIN, INVERT_CASE_LIGHT ? 255 : 0);
-    else
-      WRITE(CASE_LIGHT_PIN, INVERT_CASE_LIGHT ? HIGH : LOW);
-  }
+      analogWrite(CASE_LIGHT_PIN, n10ct);
+    else {
+      const bool s = case_light_on ? !INVERT_CASE_LIGHT : INVERT_CASE_LIGHT;
+      WRITE(CASE_LIGHT_PIN, s ? HIGH : LOW);
+    }
+
+  #endif // !CASE_LIGHT_USE_NEOPIXEL
 }
 
 #endif // HAS_CASE_LIGHT
