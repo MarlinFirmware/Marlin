b7b61d1ce6652c4ae928238ce8277bee1e0d2371
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-01-20 09:17:00 -0600

    Date refresh

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -48,7 +48,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2017-12-25 12:00"
+  #define STRING_DISTRIBUTION_DATE "2018-01-20"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
