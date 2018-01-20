8190fdb26077d43a0ebb7f7b45417bfd367355e9
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-01-20 16:58:41 -0600

    [2.0.x] Option to use Neopixel as case light (#9270)
    * Add option to use Neopixel as case light
    
    * Apply CASE_LIGHT_USE_NEOPIXEL to example configs

--- Marlin/src/feature/caselight.h
+++ Marlin/src/feature/caselight.h
@@ -26,7 +26,7 @@
 extern uint8_t case_light_brightness;
 extern bool case_light_on;
 extern uint8_t case_light_brightness_sav;   // saves brighness info when case_light_on is false
-extern bool case_light_arg_flag;  // flag to notify if S or P arguement type
+extern bool case_light_arg_flag;  // flag to notify if S or P argument type
 
 void update_case_light();
 
