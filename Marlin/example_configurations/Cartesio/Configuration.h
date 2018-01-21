ac11fe6ddc1c1502dfe5b1922679d95d433bdd89
Scott Lahteine <github@thinkyhead.com>
2018-01-21 16:57:47 -0600

    Add thermistor 15 tuned for JGAurora A5

--- Marlin/example_configurations/Cartesio/Configuration.h
+++ Marlin/example_configurations/Cartesio/Configuration.h
@@ -268,6 +268,7 @@
  *    11 : 100k beta 3950 1% thermistor (4.7k pullup)
  *    12 : 100k 0603 SMD Vishay NTCS0603E3104FXT (4.7k pullup) (calibrated for Makibox hot bed)
  *    13 : 100k Hisens 3950  1% up to 300°C for hotend "Simple ONE " & "Hotend "All In ONE"
+ *    15 : 100k thermistor calibration for JGAurora A5 hotend
  *    20 : the PT100 circuit found in the Ultimainboard V2.x
  *    60 : 100k Maker's Tool Works Kapton Bed Thermistor beta=3950
  *    66 : 4.7M High Temperature thermistor from Dyze Design
