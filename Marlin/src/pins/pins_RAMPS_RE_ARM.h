7dfff509aac9b83eee34dcd35b3c6b99b0cd7347
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-01-22 01:46:16 -0600

    Update pins_RAMPS_RE_ARM.h

--- Marlin/src/pins/pins_RAMPS_RE_ARM.h
+++ Marlin/src/pins/pins_RAMPS_RE_ARM.h
@@ -199,11 +199,9 @@
 //
 // Průša i3 MK2 Multiplexer Support
 //
-#if ENABLED(MK2_MULTIPLEXER)
-  #define E_MUX0_PIN         P0_03   // ( 0) Z_CS_PIN
-  #define E_MUX1_PIN         P0_02   // ( 1) E0_CS_PIN
-  #define E_MUX2_PIN         P0_26   // (63) E1_CS_PIN
-#endif
+#define E_MUX0_PIN         P0_03   // ( 0) Z_CS_PIN
+#define E_MUX1_PIN         P0_02   // ( 1) E0_CS_PIN
+#define E_MUX2_PIN         P0_26   // (63) E1_CS_PIN
 
 /**
  * LCD / Controller
