3ab4736ac97edcac8c65fb086eb010d42188f77e
Scott Lahteine <github@thinkyhead.com>
2018-01-23 20:51:37 -0600

    Move some strings to PROGMEM

--- Marlin/gcode.cpp
+++ Marlin/gcode.cpp
@@ -276,7 +276,7 @@ void GCodeParser::unknown_command_error() {
     SERIAL_ECHO(codenum);
     SERIAL_ECHOLNPGM(")");
     #if ENABLED(FASTER_GCODE_PARSER)
-      SERIAL_ECHO(" args: \"");
+      SERIAL_ECHOPGM(" args: \"");
       for (char c = 'A'; c <= 'Z'; ++c)
         if (seen(c)) { SERIAL_CHAR(c); SERIAL_CHAR(' '); }
     #else
