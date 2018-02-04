dbd21899450629aad7c17c5f434748e1f66da399
Scott Lahteine <github@thinkyhead.com>
2018-02-03 20:06:52 -0600

    Pretty up Print.* within CMSIS

--- frameworks/CMSIS/LPC1768/lib/Print.h
+++ frameworks/CMSIS/LPC1768/lib/Print.h
@@ -1,56 +1,51 @@
-/*
-  Print.h - Base class that provides print() and println()
-  Copyright (c) 2008 David A. Mellis.  All right reserved.
+/**
+ * Print.h - Base class that provides print() and println()
+ * Copyright (c) 2008 David A. Mellis.  All right reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-#ifndef CMSIS_Print_h
-#define CMSIS_Print_h
-
+#ifndef _CMSIS_PRINT_H_
+#define _CMSIS_PRINT_H_
 
 #include <inttypes.h>
 #include <stdio.h> // for size_t
 #include <string.h>
 
-
 #include "Printable.h"
-
 
 #define DEC 10
 #define HEX 16
 #define OCT 8
 #define BIN 2
 
-class Print
-{
+class Print {
   private:
     int write_error;
     size_t printNumber(unsigned long, uint8_t);
     size_t printFloat(double, uint8_t);
   protected:
-    void setWriteError(int err = 1) { write_error = err; }
+    void setWriteError(const int err = 1) { write_error = err; }
   public:
     Print() : write_error(0) {}
-  
+
     int getWriteError() { return write_error; }
     void clearWriteError() { setWriteError(0); }
-  
+
     virtual size_t write(uint8_t) = 0;
     size_t write(const char *str) {
-		
       if (str == NULL) return 0;
       return write((const uint8_t *)str, strlen(str));
     }
@@ -58,7 +53,7 @@ class Print
     size_t write(const char *buffer, size_t size) {
       return write((const uint8_t *)buffer, size);
     }
-    
+
     size_t print(const char[]);
     size_t print(char);
     size_t print(unsigned char, int = DEC);
@@ -82,4 +77,4 @@ class Print
     size_t printf(const char *argList, ...);
 };
 
-#endif
+#endif // _CMSIS_PRINT_H_
