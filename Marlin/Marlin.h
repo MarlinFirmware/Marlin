21bfbb9d44389973b6d054cc31254bae8c3c39fd
Scott Lahteine <github@thinkyhead.com>
2018-02-04 17:24:23 -0600

    Apply standards for whole-file conditions

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -417,6 +417,7 @@ void report_current_position();
 
 #if HAS_BED_PROBE
   extern float zprobe_zoffset;
+  bool set_probe_deployed(const bool deploy);
   #define DEPLOY_PROBE() set_probe_deployed(true)
   #define STOW_PROBE() set_probe_deployed(false)
 #else
