2e29fd3d1c5c162577a898fec9f8f73d8b6d89f2
Woojin-Crive <dnldnwls1123@gmail.com>
2018-02-09 00:39:49 +0900

    allow faster e axis move
    25 to 40

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -536,7 +536,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 40 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
