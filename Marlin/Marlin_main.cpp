305d50ee8cea58af0fa77eea93f6c5f2bbe4b4bf
Festivejelly <NicholasIvorJohn@googlemail.com>
2018-02-10 22:08:47 +0000

    Refactored code a bit by adding the functionality inside and if

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2161,11 +2161,13 @@ static void clean_up_after_endstop_or_probe_move() {
       if (set_bltouch_deployed(true)) return true;
     #endif
 	
-	// Wait for bed to heat back up between probing
-	while(thermalManager.degBed() < thermalManager.degTargetBed()) 
-    {
-      safe_delay(1000);
-    }
+    #if ENABLED(PROBING_HEATERS_OFF)
+      // Wait for bed to heat back up between probing
+      while(thermalManager.degBed() < thermalManager.degTargetBed()) 
+      {
+        safe_delay(1000);
+      }
+    #endif
 
     #if QUIET_PROBING
       probing_pause(true);
