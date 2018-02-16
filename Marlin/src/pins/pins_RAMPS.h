c49844df66213dfab5dbcc12d772f0a4279f2141
Scott Lahteine <github@thinkyhead.com>
2018-02-16 01:43:35 -0600

    !ENABLED => DISABLED

--- Marlin/src/pins/pins_RAMPS.h
+++ Marlin/src/pins/pins_RAMPS.h
@@ -48,7 +48,7 @@
   #error "Oops!  Use 'BOARD_RAMPS_RE_ARM' to build for Re-ARM."
 #endif
 
-#if !ENABLED(IS_RAMPS_SMART) && !ENABLED(IS_RAMPS_DUO) && !ENABLED(IS_RAMPS4DUE) && !ENABLED(TARGET_LPC1768)
+#if DISABLED(IS_RAMPS_SMART) && DISABLED(IS_RAMPS_DUO) && DISABLED(IS_RAMPS4DUE) && DISABLED(TARGET_LPC1768)
   #if !defined(__AVR_ATmega1280__) && !defined(__AVR_ATmega2560__)
     #error "Oops!  Make sure you have 'Arduino Mega' selected from the 'Tools -> Boards' menu."
   #endif
