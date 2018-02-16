4b8cdfdb55a05de4cca2d84b38e1572c2d72defb
Scott Lahteine <github@thinkyhead.com>
2018-02-16 02:14:20 -0600

    Followup to #9663

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -992,6 +992,7 @@
 
   //#define UBL_Z_RAISE_WHEN_OFF_MESH 2.5 // When the nozzle is off the mesh, this value is used
                                           // as the Z-Height correction value.
+
 #elif ENABLED(MESH_BED_LEVELING)
 
   //===========================================================================
