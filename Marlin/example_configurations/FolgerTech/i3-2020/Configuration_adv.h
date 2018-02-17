3a1fedaee647350daeef73bd5e41c8f832b1397f
Scott Lahteine <github@thinkyhead.com>
2018-02-17 08:10:44 -0600

    Update LCD_SET_PROGRESS_MANUALLY in configs

--- Marlin/example_configurations/FolgerTech/i3-2020/Configuration_adv.h
+++ Marlin/example_configurations/FolgerTech/i3-2020/Configuration_adv.h
@@ -489,6 +489,9 @@
 // The timeout (in ms) to return to the status screen from sub-menus
 //#define LCD_TIMEOUT_TO_STATUS 15000
 
+// Add an 'M73' G-code to set the current percentage
+//#define LCD_SET_PROGRESS_MANUALLY
+
 /**
  * LED Control Menu
  * Enable this feature to add LED Control to the LCD menu
@@ -578,9 +581,6 @@
     // Add a menu item to test the progress bar:
     //#define LCD_PROGRESS_BAR_TEST
   #endif
-
-  // Add an 'M73' G-code to set the current percentage
-  //#define LCD_SET_PROGRESS_MANUALLY
 
   // This allows hosts to request long names for files and folders with M33
   //#define LONG_FILENAME_HOST_SUPPORT
