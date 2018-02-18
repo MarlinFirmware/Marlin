a31d6290e1fc28ef53f220e354e8f9e1873fe5fb
Scott Lahteine <github@thinkyhead.com>
2018-02-18 00:07:23 -0600

    Hephestos2 heated bed kit for all

--- Marlin/src/config/examples/Creality/CR-10S/Configuration_adv.h
+++ Marlin/src/config/examples/Creality/CR-10S/Configuration_adv.h
@@ -40,6 +40,17 @@
 //=============================Thermal Settings  ============================
 //===========================================================================
 
+//
+// Hephestos 2 24V heated bed upgrade kit.
+// https://store.bq.com/en/heated-bed-kit-hephestos2
+//
+//#define HEPHESTOS2_HEATED_BED_KIT
+#if ENABLED(HEPHESTOS2_HEATED_BED_KIT)
+  #undef TEMP_SENSOR_BED
+  #define TEMP_SENSOR_BED 70
+  #define HEATER_BED_INVERTING true
+#endif
+
 #if DISABLED(PIDTEMPBED)
   #define BED_CHECK_INTERVAL 5000 // ms between checks in bang-bang control
   #if ENABLED(BED_LIMIT_SWITCHING)
