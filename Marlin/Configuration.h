605c36c4fa15a816fa4711c42359e6ee64e13e6d
Ryan Zellars <allted@gmail.com>
2018-02-18 19:29:59 -0800

    bootscreen

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -90,7 +90,7 @@
 // boot image unmodified. For an example have a look at the bq Hephestos 2
 // example configuration folder.
 //
-//#define SHOW_CUSTOM_BOOTSCREEN
+#define SHOW_CUSTOM_BOOTSCREEN
 
 // Enable to show the bitmap in Marlin/_Statusscreen.h on the status screen.
 //#define CUSTOM_STATUS_SCREEN_IMAGE
