6aeda0a11640caa6b8b1c10c18079a2b72876566
xC0000005 <32139633+xC0000005@users.noreply.github.com>
2018-02-19 20:41:40 -0800

    Set version to 99 so it indicates this version is not malyan supported.

--- Marlin/src/lcd/malyanlcd.cpp
+++ Marlin/src/lcd/malyanlcd.cpp
@@ -431,7 +431,7 @@ void lcd_init() {
   write_to_lcd_P(PSTR("{SYS:STARTED}\r\n"));
 
   // send a version that says "unsupported"
-  write_to_lcd_P(PSTR("{VER:66}\r\n"));
+  write_to_lcd_P(PSTR("{VER:99}\r\n"));
 
   // No idea why it does this twice.
   write_to_lcd_P(PSTR("{SYS:STARTED}\r\n"));
