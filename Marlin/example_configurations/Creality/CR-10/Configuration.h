84f0ce47512c4939a5e10225b39a3bc89c070b59
Scott Lahteine <github@thinkyhead.com>
2018-02-21 20:18:40 -0600

    Some encoder steps handled in Conditionals_LCD.h

--- Marlin/example_configurations/Creality/CR-10/Configuration.h
+++ Marlin/example_configurations/Creality/CR-10/Configuration.h
@@ -1381,13 +1381,13 @@
 // This option overrides the default number of encoder pulses needed to
 // produce one step. Should be increased for high-resolution encoders.
 //
-#define ENCODER_PULSES_PER_STEP 4
+//#define ENCODER_PULSES_PER_STEP 1
 
 //
 // Use this option to override the number of step signals required to
 // move between next/prev menu items.
 //
-#define ENCODER_STEPS_PER_MENU_ITEM 1
+//#define ENCODER_STEPS_PER_MENU_ITEM 5
 
 /**
  * Encoder Direction Options
