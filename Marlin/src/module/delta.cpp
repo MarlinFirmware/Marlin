17467493612e187388fd66ed9ed457f2e49cd8c0
Scott Lahteine <github@thinkyhead.com>
2018-03-01 01:52:21 -0600

    Followup to #9868

--- Marlin/src/module/delta.cpp
+++ Marlin/src/module/delta.cpp
@@ -232,9 +232,9 @@ bool home_delta() {
 
   // Disable stealthChop if used. Enable diag1 pin on driver.
   #if ENABLED(SENSORLESS_HOMING)
-    sensorless_homing_per_axis(X_AXIS);
-    sensorless_homing_per_axis(Y_AXIS);
-    sensorless_homing_per_axis(Z_AXIS);
+    sensorless_homing_per_axis(A_AXIS);
+    sensorless_homing_per_axis(B_AXIS);
+    sensorless_homing_per_axis(C_AXIS);
   #endif
 
   // Move all carriages together linearly until an endstop is hit.
@@ -245,9 +245,9 @@ bool home_delta() {
 
   // Re-enable stealthChop if used. Disable diag1 pin on driver.
   #if ENABLED(SENSORLESS_HOMING)
-    sensorless_homing_per_axis(X_AXIS, false);
-    sensorless_homing_per_axis(Y_AXIS, false);
-    sensorless_homing_per_axis(Z_AXIS, false);
+    sensorless_homing_per_axis(A_AXIS, false);
+    sensorless_homing_per_axis(B_AXIS, false);
+    sensorless_homing_per_axis(C_AXIS, false);
   #endif
 
   // If an endstop was not hit, then damage can occur if homing is continued.
