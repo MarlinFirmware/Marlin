6b4a9281456175e98ff54be2a0fe6c0ad592b928
Bob-the-Kuhn <bob.kuhn@att.net>
2018-03-07 21:11:01 -0600

    2uS default step pulse (#9989)

--- Marlin/src/config/examples/wt150/Configuration_adv.h
+++ Marlin/src/config/examples/wt150/Configuration_adv.h
@@ -777,7 +777,8 @@
 
 // The minimum pulse width (in µs) for stepping a stepper.
 // Set this if you find stepping unreliable, or if using a very fast CPU.
-#define MINIMUM_STEPPER_PULSE 0 // (µs) The smallest stepper pulse allowed
+// 0 is OK for AVR, 0 is OK for A4989 drivers, 2 is needed for DRV8825 drivers
+#define MINIMUM_STEPPER_PULSE 2 // (µs)   DRV8825 on 32bit CPUs
 
 // @section temperature
 
