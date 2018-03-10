79e6d890d5422ab7e805e3290e65776da043bde3
Scott Lahteine <github@thinkyhead.com>
2018-03-09 23:19:00 -0600

    Add UNKNOWN_Z_NO_RAISE to example configs

--- Marlin/src/config/examples/Velleman/K8200/Configuration.h
+++ Marlin/src/config/examples/Velleman/K8200/Configuration.h
@@ -833,6 +833,8 @@
 
 //#define NO_MOTION_BEFORE_HOMING  // Inhibit movement until all axes have been homed
 
+//#define UNKNOWN_Z_NO_RAISE // Don't raise Z (lower the bed) if Z is "unknown." For beds that fall when Z is powered off.
+
 // K8200: it is usual to have clamps for the glass plate on the heatbed
 #define Z_HOMING_HEIGHT 4   // (in mm) Minimal z height before homing (G28) for Z clearance above the bed, clamps, ...
                             // Be sure you have this distance over your Z_MAX_POS in case.
