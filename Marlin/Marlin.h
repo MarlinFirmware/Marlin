0aa100a31ed0ec4757468f0a8d88370e74518362
Scott Lahteine <github@thinkyhead.com>
2018-03-11 05:51:14 -0500

    Add Z_AFTER_PROBING option
    Some fix-mounted probes need manual stowing. And after probing some may prefer to raise or lower the nozzle. This restores an old option but tailors it to allow raise or lower as preferred.

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -389,6 +389,11 @@ void report_current_position();
 #if HAS_BED_PROBE
   extern float zprobe_zoffset;
   bool set_probe_deployed(const bool deploy);
+  #ifdef Z_AFTER_PROBING
+    void move_z_after_probing();
+  #else
+    inline void move_z_after_probing() {}
+  #endif
   #define DEPLOY_PROBE() set_probe_deployed(true)
   #define STOW_PROBE() set_probe_deployed(false)
 #else
