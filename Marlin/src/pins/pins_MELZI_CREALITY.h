66898849103c315e254350c59d64df378d771392
Scott Lahteine <github@thinkyhead.com>
2018-03-17 02:40:20 -0500

    Easier override of ST7920 LCD timing

--- Marlin/src/pins/pins_MELZI_CREALITY.h
+++ Marlin/src/pins/pins_MELZI_CREALITY.h
@@ -54,9 +54,15 @@
 #define FIL_RUNOUT_PIN     -1 // Uses Beeper/LED Pin Pulled to GND
 
 // Alter timing for graphical display
-#define ST7920_DELAY_1 DELAY_2_NOP
-#define ST7920_DELAY_2 DELAY_2_NOP
-#define ST7920_DELAY_3 DELAY_2_NOP
+#ifndef ST7920_DELAY_1
+  #define ST7920_DELAY_1 DELAY_2_NOP
+#endif
+#ifndef ST7920_DELAY_2
+  #define ST7920_DELAY_2 DELAY_2_NOP
+#endif
+#ifndef ST7920_DELAY_3
+  #define ST7920_DELAY_3 DELAY_2_NOP
+#endif
 
 #if ENABLED(MINIPANEL)
   #undef DOGLCD_CS
