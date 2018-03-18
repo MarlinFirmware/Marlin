bdf2703b888547dbf61d8102d286f85d14a76d11
Chris Wyse <chris.wyse@cox.net>
2018-03-18 00:48:38 -0400

    Merged Configuration.h & Configuration_adv.h with 1.1.8.
    Original settings were from https://github.com/MarlinFirmware/Marlin/issues/7661.
    They were modified from the 1.1.x bugfix branch.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -715,7 +715,7 @@
  * Example: `M851 Z-5` with a CLEARANCE of 4  =>  9mm from bed to nozzle.
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
-#define Z_CLEARANCE_DEPLOY_PROBE   0 //10 // Z Clearance for Deploy/Stow
+#define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
 
 // For M851 give a range for adjusting the Z probe offset
