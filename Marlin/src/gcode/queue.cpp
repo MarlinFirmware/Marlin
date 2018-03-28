3d4fe22cd4952e38aad99bc6353963d9dbf1b8f2
Scott Lahteine <github@thinkyhead.com>
2018-03-28 13:45:47 -0500

    Require a checksum when writing to SD
    From #10204 by @adriancuzman

--- Marlin/src/gcode/queue.cpp
+++ Marlin/src/gcode/queue.cpp
@@ -335,10 +335,10 @@ inline void get_serial_commands() {
 
           gcode_LastN = gcode_N;
         }
-        #if ENABLED(SDSUPPORT)
-          else if (card.saving)
-            return gcode_line_error(PSTR(MSG_ERR_NO_CHECKSUM), i);
-        #endif
+        else if (card.saving) {
+          gcode_line_error(PSTR(MSG_ERR_NO_CHECKSUM));
+          return;
+        }
 
         // Movement commands alert when stopped
         if (IsStopped()) {
