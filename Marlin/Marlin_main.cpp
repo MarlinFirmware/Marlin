f84f7e5a382b37c75e37f9d76f86fff94e936f37
Adrian Cuzman <adriancuzman@users.noreply.github.com>
2018-03-28 20:43:41 +0200

    Require a checksum when writing to SD (#10204)
    When streaming commands to SD with XON/XOFF, errors can occur and cause incomplete commands to be written to the file. This change ensures that only commands with line numbers and checksum will be saved to SD.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -1070,6 +1070,10 @@ inline void get_serial_commands() {
 
         gcode_LastN = gcode_N;
       }
+      else if (card.saving) {
+        gcode_line_error(PSTR(MSG_ERR_NO_CHECKSUM));
+        return;
+      }
 
       // Movement commands alert when stopped
       if (IsStopped()) {
