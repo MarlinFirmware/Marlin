be50c11197ca4307c3931d5355f640a0bff9f32e
Ryan Zellars <allted@gmail.com>
2018-04-06 11:50:36 -0700

    Increase arc resolution to 0.15mm

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -752,7 +752,7 @@
 //
 #define ARC_SUPPORT               // Disable this feature to save ~3226 bytes
 #if ENABLED(ARC_SUPPORT)
-  #define MM_PER_ARC_SEGMENT  1   // Length of each arc segment
+  #define MM_PER_ARC_SEGMENT  .15   // Length of each arc segment
   #define N_ARC_CORRECTION   25   // Number of intertpolated segments between corrections
   //#define ARC_P_CIRCLES         // Enable the 'P' parameter to specify complete circles
   //#define CNC_WORKSPACE_PLANES  // Allow G2/G3 to operate in XY, ZX, or YZ planes
