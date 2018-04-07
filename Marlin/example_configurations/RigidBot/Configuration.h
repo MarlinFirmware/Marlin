e6336c03a19a160268e1b22289d6df8a41567799
Scott Lahteine <github@thinkyhead.com>
2018-04-06 22:57:16 -0500

    Add Chamber Temp options to example configs

--- Marlin/example_configurations/RigidBot/Configuration.h
+++ Marlin/example_configurations/RigidBot/Configuration.h
@@ -310,6 +310,7 @@
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_BED 1
+#define TEMP_SENSOR_CHAMBER 0
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
 #define DUMMY_THERMISTOR_998_VALUE 25
