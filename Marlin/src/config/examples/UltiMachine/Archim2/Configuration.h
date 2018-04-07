5b3bda1c164fe932517595dd1cd88669614fac81
Scott Lahteine <github@thinkyhead.com>
2018-04-06 22:54:12 -0500

    Add Chamber Temp options to example configs

--- Marlin/src/config/examples/UltiMachine/Archim2/Configuration.h
+++ Marlin/src/config/examples/UltiMachine/Archim2/Configuration.h
@@ -316,6 +316,7 @@
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_CHAMBER 0
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
 #define DUMMY_THERMISTOR_998_VALUE 25
