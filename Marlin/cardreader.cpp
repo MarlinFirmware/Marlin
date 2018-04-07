6f2676c175f4b01abfcdcac805a01854dd1e1f7f
Scott Lahteine <github@thinkyhead.com>
2018-04-06 20:12:40 -0500

    Reset progress bar when print ends
    Fix #10330
    Based on #10333 by @marcio-ao

--- Marlin/cardreader.cpp
+++ Marlin/cardreader.cpp
@@ -937,6 +937,9 @@ void CardReader::printingHasFinished() {
     #if ENABLED(SDCARD_SORT_ALPHA)
       presort();
     #endif
+    #if ENABLED(ULTRA_LCD) && ENABLED(LCD_SET_PROGRESS_MANUALLY)
+      progress_bar_percent = 0;
+    #endif
     #if ENABLED(SD_REPRINT_LAST_SELECTED_FILE)
       lcd_reselect_last_file();
     #endif
