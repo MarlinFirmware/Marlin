4911df33c03b280b93c4efb49d341a90ddae5aa1
LichtiMC <18006112+LichtiMC@users.noreply.github.com>
2018-04-09 12:48:06 +0200

    Provide additional information about SMART-RAMPS board
    I would have saved lots of time If I knew about these few things.

--- Marlin/src/pins/pins_RAMPS_SMART.h
+++ Marlin/src/pins/pins_RAMPS_SMART.h
@@ -49,6 +49,23 @@
  *       A15 | A11
  */
 
+/**
+ * Additional information:
+ * PIN 42 is connected via Jumper "EN_RESET" to the DUE reset pin. You have to remove the jumper 
+ * to use this pin for anything else than resetting the board.
+ * 
+ * RepRapDiscount Full Graphic Smart Controller works fine without additional configuration but uncommenting 
+ * "#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER" on AUX-4 with SMART ADAPTER, but you have to remove the AUX3 pin header
+ * on the smart adapter, as it physically doesn't fit to the AUX-3 header on the SMART-RAMPS Board. 
+ * If you also want to use the sd-card-reader you have to wire these PINS by yourself to AUX-3. (Software-SPI!)
+ * For using hardware SPI please read on. Additionally for SD-CARD-HARDWARE-SPI SDSS must be set to Pin 52 (Hardware-SPI-chip-select-pin)
+ * instead of default PIN 53. 
+ *
+ * If you would like to use the DUE hardware-SPI, this doesn't work without additional wiring, 
+ * because the SMART-RAMPS board does not breakout the 6-pin SPI Header from the DUE board.
+ * Google for "Arduino DUE Board Pinout" and you will see the right header.
+ */
+
 #ifndef __SAM3X8E__
   #error "Oops!  Make sure you have 'Arduino Due' selected from the 'Tools -> Boards' menu."
 #endif
