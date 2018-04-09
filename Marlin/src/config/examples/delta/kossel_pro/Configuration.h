82dafbba333379eaf62285bfa3641e1a495d7e21
Scott Lahteine <github@thinkyhead.com>
2018-04-09 01:46:44 -0500

    Add Z_PROBE_LOW_POINT to example configs

--- Marlin/src/config/examples/delta/kossel_pro/Configuration.h
+++ Marlin/src/config/examples/delta/kossel_pro/Configuration.h
@@ -896,6 +896,8 @@
 #define Z_CLEARANCE_BETWEEN_PROBES   5 // Z Clearance between probe points
 //#define Z_AFTER_PROBING            5 // Z position after probing is done
 
+#define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
+
 // For M851 give a range for adjusting the Z probe offset
 
 #define Z_PROBE_OFFSET_RANGE_MIN -15
