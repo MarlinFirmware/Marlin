8477c8677f1dbcec3ea76c27482b6adfdedf1050
Karl Andersson <karl@iaccess.se>
2018-04-11 21:33:53 +0200

    Invert y dir

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -818,7 +818,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR true
+#define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 
 // Enable this option for Toshiba stepper drivers
