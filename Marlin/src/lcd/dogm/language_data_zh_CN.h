e0cb8ff735705702c6e4e1b7125f1a5bb791244f
Scott Lahteine <github@thinkyhead.com>
2018-04-14 00:32:23 -0500

    u8g.h => U8glib.h
    Fix #10395
    Followup to #10213

--- Marlin/src/lcd/dogm/language_data_zh_CN.h
+++ Marlin/src/lcd/dogm/language_data_zh_CN.h
@@ -2,7 +2,7 @@
 /* This file is generated automatically, any changes to the file may be REPLACED by future processing. */
 /* Please turn to the tools call this script to change the contents! */
 
-#include <u8g.h>
+#include <U8glib.h>
 
 const u8g_fntpgm_uint8_t fontpage_69_191_191[41] U8G_FONT_SECTION("fontpage_69_191_191") = {
   0x00,0x06,0x0a,0x00,0xfe,0x00,0x00,0x00,0x00,0x00,0xbf,0xbf,0x00,0x0a,0x00,0x00,
