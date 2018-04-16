090e08ef3d9518ade388e1c074e765a3337b6e6a
Scott Lahteine <github@thinkyhead.com>
2018-04-16 01:42:03 -0500

    M221 with no parameters report flow

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -9327,6 +9327,14 @@ inline void gcode_M221() {
     planner.flow_percentage[target_extruder] = parser.value_int();
     planner.refresh_e_factor(target_extruder);
   }
+  else {
+    SERIAL_ECHO_START();
+    SERIAL_CHAR('E');
+    SERIAL_CHAR('0' + target_extruder);
+    SERIAL_ECHOPAIR(" Flow: ", planner.flow_percentage[target_extruder]);
+    SERIAL_CHAR('%');
+    SERIAL_EOL();
+  }
 }
 
 /**
