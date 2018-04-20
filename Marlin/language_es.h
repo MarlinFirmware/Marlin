0fb47cead0156b2bfae727f32cbdb27575255caf
Scott Lahteine <github@thinkyhead.com>
2018-04-20 18:02:00 -0500

    Remove obsolete strings

--- Marlin/language_es.h
+++ Marlin/language_es.h
@@ -166,7 +166,6 @@
 #define MSG_NO_CARD                         _UxGT("No hay tarjeta SD")
 #define MSG_DWELL                           _UxGT("Reposo...")
 #define MSG_USERWAIT                        _UxGT("Esperando ordenes")
-#define MSG_RESUMING                        _UxGT("Resumiendo impre.")
 #define MSG_PRINT_ABORTED                   _UxGT("Impresion cancelada")
 #define MSG_NO_MOVE                         _UxGT("Sin movimiento")
 #define MSG_KILLED                          _UxGT("Parada de emergencia")
@@ -206,9 +205,7 @@
 #define MSG_SHORT_HOUR                      _UxGT("h") // One character only
 #define MSG_SHORT_MINUTE                    _UxGT("m") // One character only
 #define MSG_HEATING                         _UxGT("Calentando...")
-#define MSG_HEATING_COMPLETE                _UxGT("Calentamiento listo")
 #define MSG_BED_HEATING                     _UxGT("Calentando Plat...")
-#define MSG_BED_DONE                        _UxGT("Plataforma Caliente")
 #define MSG_DELTA_CALIBRATE                 _UxGT("Calibracion Delta")
 #define MSG_DELTA_CALIBRATE_X               _UxGT("Calibrar X")
 #define MSG_DELTA_CALIBRATE_Y               _UxGT("Calibrar Y")
