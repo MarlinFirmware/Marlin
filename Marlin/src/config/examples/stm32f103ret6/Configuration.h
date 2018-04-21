413b699022ad22bcdf6559bc6e8b12a873776f4c
Scott Lahteine <github@thinkyhead.com>
2018-04-21 01:21:45 -0500

    Add WAIT_FOR_BED_HEATER to example configs
    Co-Authored-By: Festivejelly <nicholasivorjohn@googlemail.com>

--- Marlin/src/config/examples/stm32f103ret6/Configuration.h
+++ Marlin/src/config/examples/stm32f103ret6/Configuration.h
@@ -707,6 +707,9 @@
  * readings with inductive probes and piezo sensors.
  */
 //#define PROBING_HEATERS_OFF       // Turn heaters off when probing
+#if ENABLED(PROBING_HEATERS_OFF)
+  //#define WAIT_FOR_BED_HEATER     // Wait for bed to heat back up between probes (to improve accuracy)
+#endif
 //#define PROBING_FANS_OFF          // Turn fans off when probing
 
 // A probe that is deployed and stowed with a solenoid pin (SOL1_PIN)
