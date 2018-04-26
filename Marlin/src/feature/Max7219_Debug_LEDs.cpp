e1d07b63b924d53ad1be7dd658a8d18587db8def
Karl Andersson <karl@iaccess.se>
2018-04-26 21:02:42 +0200

    delayMicroseconds() cannot be called in critical section on the STM32 platform

--- Marlin/src/feature/Max7219_Debug_LEDs.cpp
+++ Marlin/src/feature/Max7219_Debug_LEDs.cpp
@@ -64,7 +64,7 @@
 static uint8_t LEDs[8] = { 0 };
 
 #ifdef CPU_32_BIT
-  #define MS_DELAY() delayMicroseconds(7)  // 32-bit processors need a delay to stabilize the signal
+void MS_DELAY() { DELAY_1US; }  // 32-bit processors need a delay to stabilize the signal
 #else
   #define MS_DELAY() DELAY_3_NOP
 #endif
