23e7055f8deeb94f4bdd39378388841996527f5a
Scott Lahteine <github@thinkyhead.com>
2018-04-28 20:28:40 -0500

    Modify FastIO error message

--- Marlin/fastio.h
+++ Marlin/fastio.h
@@ -54,7 +54,7 @@ typedef int8_t pin_t;
 #elif AVR_ATmega2561_FAMILY
   #include "fastio_1281.h"
 #else
-  #error "Pins for this chip not defined in Arduino.h! If you have a working pins definition, please contribute!"
+  #error "No FastIO definition for the selected AVR Board."
 #endif
 
 #include "macros.h"
