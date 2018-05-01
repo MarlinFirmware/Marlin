d94ba82828889837629a5bb78244f9b95820aecf
TerraBAS <TerraBAS@users.noreply.github.com>
2018-05-01 21:53:34 +0200

    Add Velleman RGB-LED strip support to K8400

--- Marlin/example_configurations/Velleman/K8400/Configuration.h
+++ Marlin/example_configurations/Velleman/K8400/Configuration.h
@@ -1776,13 +1776,13 @@
  * LED Type. Enable only one of the following two options.
  *
  */
-//#define RGB_LED
+#define RGB_LED
 //#define RGBW_LED
 
 #if ENABLED(RGB_LED) || ENABLED(RGBW_LED)
-  #define RGB_LED_R_PIN 34
-  #define RGB_LED_G_PIN 43
-  #define RGB_LED_B_PIN 35
+  #define RGB_LED_R_PIN 41
+  #define RGB_LED_G_PIN 40
+  #define RGB_LED_B_PIN 12
   #define RGB_LED_W_PIN -1
 #endif
 
