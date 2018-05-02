bee82137f371b07465c111ed8c79e4cd8f221ca1
Bob-the-Kuhn <bob.kuhn@att.net>
2018-05-02 03:29:04 -0500

    whitespace fix

--- Marlin/src/lcd/dogm/HAL_LCD_com_defines.h
+++ Marlin/src/lcd/dogm/HAL_LCD_com_defines.h
@@ -28,8 +28,6 @@
   uint8_t u8g_com_arduino_sw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);
   #define U8G_COM_HAL_SW_SPI_FN  u8g_com_arduino_sw_spi_fn
 
-
-
   #ifdef __SAM3X8E__
     uint8_t u8g_com_HAL_DUE_shared_hw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);
     #define U8G_COM_HAL_HW_SPI_FN u8g_com_HAL_DUE_shared_hw_spi_fn
