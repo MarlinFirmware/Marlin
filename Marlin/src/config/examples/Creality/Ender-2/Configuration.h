76da6cc54484133acd4b7cbb45fb2ffaa7228961
Scott Lahteine <github@thinkyhead.com>
2018-05-02 19:03:22 -0500

    Fix Ender 2 build volume

--- Marlin/src/config/examples/Creality/Ender-2/Configuration.h
+++ Marlin/src/config/examples/Creality/Ender-2/Configuration.h
@@ -850,8 +850,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 320
-#define Y_BED_SIZE 320
+#define X_BED_SIZE 150
+#define Y_BED_SIZE 150
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -859,7 +859,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 420
+#define Z_MAX_POS 200
 
 /**
  * Software Endstops
