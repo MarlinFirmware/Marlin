eeca5ab50cae17f03edb105179414008643efe2d
Manuel McLure <manuel@mclure.org>
2018-05-10 13:45:16 -0700

    Remove hidden dependencies on destination. Fixes #10686.

--- Marlin/src/gcode/motion/G2_G3.cpp
+++ Marlin/src/gcode/motion/G2_G3.cpp
@@ -226,7 +226,7 @@ void plan_arc(
   // As far as the parser is concerned, the position is now == target. In reality the
   // motion control system might still be processing the action and the real tool position
   // in any intermediate location.
-  set_current_from_destination();
+  COPY(current_position, cart);
 } // plan_arc
 
 /**
