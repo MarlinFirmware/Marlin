8a0fa641876fbb41b8f7a1e48d03b6e1dcf1ba23
Jim Brown <jb.github@rcairgallery.com>
2018-05-15 11:34:25 -0400

    Change babystep multiplicator

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -722,7 +722,7 @@
 #if ENABLED(BABYSTEPPING)
   //#define BABYSTEP_XY              // Also enable X/Y Babystepping. Not supported on DELTA!
   #define BABYSTEP_INVERT_Z false    // Change if Z babysteps should go the other way
-  #define BABYSTEP_MULTIPLICATOR 1   // Babysteps are very small. Increase for faster motion.
+  #define BABYSTEP_MULTIPLICATOR 10   // Babysteps are very small. Increase for faster motion.
   #if ENABLED(BLTOUCH) || ENABLED(SN04) || ENABLED(INDUCTIVE_NO) || ENABLED(INDUCTIVE_NC) || ENABLED(SERVO_PROBE) || ENABLED(NOZZLE_PROBE)
     #define BABYSTEP_ZPROBE_OFFSET     // Enable to combine M851 and Babystepping
   #endif
