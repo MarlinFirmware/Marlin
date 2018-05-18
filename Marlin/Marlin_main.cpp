1376d4c7544a51fa602efe6596d0b697d8e384ff
Ante Vukorepa <o.orcinus@gmail.com>
2018-05-19 01:08:36 +0200

    Fix G1 behaviour after tool unpark.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -11938,7 +11938,7 @@ void tool_change(const uint8_t tmp_extruder, const float fr_mm_s/*=0.0*/, bool n
             if (DEBUGGING(LEVELING)) DEBUG_POS("Move back", destination);
           #endif
           // Move back to the original (or tweaked) position
-          do_blocking_move_to(destination[X_AXIS], destination[Y_AXIS], destination[Z_AXIS]);
+          prepare_move_to_destination();
         }
         #if ENABLED(SWITCHING_NOZZLE)
           else {
