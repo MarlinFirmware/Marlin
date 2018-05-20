5a9792c0d38070ea53c9ec84bedd52141f20b3e9
Ante Vukorepa <o.orcinus@gmail.com>
2018-05-21 01:19:29 +0200

    Restore unparked status after unparking

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -11938,7 +11938,8 @@ void tool_change(const uint8_t tmp_extruder, const float fr_mm_s/*=0.0*/, bool n
             if (DEBUGGING(LEVELING)) DEBUG_POS("Move back", destination);
           #endif
           // Move back to the original (or tweaked) position
-          prepare_move_to_destination();
+          do_blocking_move_to(destination[X_AXIS], destination[Y_AXIS], destination[Z_AXIS]);
+          active_extruder_parked = false;
         }
         #if ENABLED(SWITCHING_NOZZLE)
           else {
