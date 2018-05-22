d2647c1f19c9658075e06d4c5f563312ae6aa794
Scott Lahteine <github@thinkyhead.com>
2018-05-22 00:53:00 -0500

    Fix G1 behaviour after tool unpark
    Fix #10777 for 2.0.x
    
    Co-Authored-By: Ante Vukorepa <o.orcinus@gmail.com>

--- Marlin/src/module/tool_change.cpp
+++ Marlin/src/module/tool_change.cpp
@@ -454,6 +454,9 @@ void tool_change(const uint8_t tmp_extruder, const float fr_mm_s/*=0.0*/, bool n
           #endif
           // Move back to the original (or tweaked) position
           do_blocking_move_to(destination[X_AXIS], destination[Y_AXIS], destination[Z_AXIS]);
+          #if ENABLED(DUAL_X_CARRIAGE)
+            active_extruder_parked = false;
+          #endif
         }
         #if ENABLED(SWITCHING_NOZZLE)
           else {
