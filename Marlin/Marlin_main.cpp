8ea99393432434daaf8ed0ee615789a91c53e1c1
Scott Lahteine <github@thinkyhead.com>
2018-05-22 00:47:56 -0500

    Fix DEBUG_LEVELING_FEATURE with MBL

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -3833,7 +3833,11 @@ inline void gcode_G4() {
       SERIAL_ECHOPGM("Mesh Bed Leveling");
       if (planner.leveling_active) {
         SERIAL_ECHOLNPGM(" (enabled)");
-        SERIAL_ECHOPAIR("MBL Adjustment Z", ftostr43sign(mbl.get_z(current_position[X_AXIS], current_position[Y_AXIS], 1.0), '+'));
+        SERIAL_ECHOPAIR("MBL Adjustment Z", ftostr43sign(mbl.get_z(current_position[X_AXIS], current_position[Y_AXIS]
+          #if ENABLED(ENABLE_LEVELING_FADE_HEIGHT)
+            , 1.0
+          #endif
+        ), '+'));
         #if ENABLED(ENABLE_LEVELING_FADE_HEIGHT)
           if (planner.z_fade_height) {
             SERIAL_ECHOPAIR(" (", ftostr43sign(
