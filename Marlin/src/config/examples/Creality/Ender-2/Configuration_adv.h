931ab4ec403092db36c44fb4ffd780e3d08184c0
Scott Lahteine <github@thinkyhead.com>
2018-05-26 02:19:28 -0500

    Rename Junction Deviation options

--- Marlin/src/config/examples/Creality/Ender-2/Configuration_adv.h
+++ Marlin/src/config/examples/Creality/Ender-2/Configuration_adv.h
@@ -446,8 +446,8 @@
 //
 //#define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
-  #define JUNCTION_DEVIATION_FACTOR 0.05
-  #define JUNCTION_ACCELERATION_FACTOR 1000
+  #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
+  #define JUNCTION_ACCELERATION 1000  // (mm/s²) Maximum centripetal acceleration
   //#define JUNCTION_DEVIATION_INCLUDE_E
 #endif
 
