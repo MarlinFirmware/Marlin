b6cd8d9cc6ed2ef49aaf3da2ab9a0915fc5e6602
Scott Lahteine <github@thinkyhead.com>
2018-05-29 11:39:03 -0500

    Set deltas to park on the left

--- Marlin/src/config/examples/delta/generic/Configuration.h
+++ Marlin/src/config/examples/delta/generic/Configuration.h
@@ -1386,7 +1386,7 @@
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z }
-  #define NOZZLE_PARK_POINT { (X_MIN_POS + 10), (Y_MAX_POS - 10), 20 }
+  #define NOZZLE_PARK_POINT { (X_MIN_POS + 10), 0, 20 }
   #define NOZZLE_PARK_XY_FEEDRATE 100   // X and Y axes feedrate in mm/s (also used for delta printers Z axis)
   #define NOZZLE_PARK_Z_FEEDRATE 5      // Z axis feedrate in mm/s (not used for delta printers)
 #endif
