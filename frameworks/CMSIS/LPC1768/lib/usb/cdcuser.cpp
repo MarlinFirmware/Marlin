0312c42f9d9d7df342547381bf3dd07097b3e4ca
Chris Pepper <p3p@p3psoft.co.uk>
2018-06-17 02:59:22 +0100

    [2.0.x] LPC176x Serial cleanup (#11032)

--- frameworks/CMSIS/LPC1768/lib/usb/cdcuser.cpp
+++ frameworks/CMSIS/LPC1768/lib/usb/cdcuser.cpp
@@ -211,7 +211,7 @@ void CDC_BulkIn(void) {
   if (numBytesAvail > 0) {
     numBytesAvail = numBytesAvail > (USB_CDC_BUFSIZE - 1) ? (USB_CDC_BUFSIZE - 1) : numBytesAvail;
     for(uint32_t i = 0; i < numBytesAvail; ++i) {
-      BulkBufIn[i] = usb_serial.transmit_buffer.read(); //todo: optimise
+      usb_serial.transmit_buffer.read(&BulkBufIn[i]);
     }
     USB_WriteEP(CDC_DEP_IN, &BulkBufIn[0], numBytesAvail);
   } else {
