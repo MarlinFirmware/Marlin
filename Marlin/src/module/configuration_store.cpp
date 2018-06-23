a67488ef4a642e92dfac04c14242c936baf1636a
GMagician <GMagician@users.noreply.github.com>
2018-06-24 00:09:35 +0200

    [2.0.x] Fix advanced pause init values
    Fixed for cycle used to initialize default values

--- Marlin/src/module/configuration_store.cpp
+++ Marlin/src/module/configuration_store.cpp
@@ -1904,7 +1904,7 @@ void MarlinSettings::reset(PORTARG_SOLO) {
   #endif
 
   #if ENABLED(ADVANCED_PAUSE_FEATURE)
-    for (uint8_t e = 0; e < E_STEPPERS; e++) {
+    for (uint8_t e = 0; e < EXTRUDERS; e++) {
       filament_change_unload_length[e] = FILAMENT_CHANGE_UNLOAD_LENGTH;
       filament_change_load_length[e] = FILAMENT_CHANGE_FAST_LOAD_LENGTH;
     }
