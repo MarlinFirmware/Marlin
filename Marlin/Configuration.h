f8576f5bdbaad84b9a38263d2c1259970575095f
Karl Andersson <karl@iaccess.se>
2018-06-28 08:15:37 +0200

    Changed max feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -611,7 +611,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 700, 700, 150, 200 }
+#define DEFAULT_MAX_FEEDRATE          { 700, 700, 15, 200 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
