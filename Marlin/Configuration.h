98e9648ed0c3130984119484b11296df06198bc1
Karl Andersson <karl@iaccess.se>
2018-06-29 20:10:17 +0200

    Adjusted bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -873,7 +873,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 250
+#define X_BED_SIZE 235
 #define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
