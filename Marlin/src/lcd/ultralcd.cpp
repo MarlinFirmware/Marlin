3349eb002010f64760124517eb8dd137ea5f7699
Scott Lahteine <github@thinkyhead.com>
2018-06-29 19:23:03 -0500

    Fix SKEW_CORRECTION compile error

--- Marlin/src/lcd/ultralcd.cpp
+++ Marlin/src/lcd/ultralcd.cpp
@@ -2567,7 +2567,7 @@ void lcd_quick_feedback(const bool clear_buttons) {
   #endif // AUTO_BED_LEVELING_UBL
 
 
-  #if ENABLED(LCD_BED_LEVELING) || (PLANNER_LEVELING && DISABLED(SLIM_LCD_MENUS))
+  #if ENABLED(LCD_BED_LEVELING) || (HAS_LEVELING && DISABLED(SLIM_LCD_MENUS))
     void _lcd_toggle_bed_leveling() { set_bed_leveling_enabled(!planner.leveling_active); }
   #endif
 
@@ -2691,7 +2691,7 @@ void lcd_quick_feedback(const bool clear_buttons) {
       #endif
           MENU_ITEM(submenu, MSG_BED_LEVELING, lcd_bed_leveling);
 
-    #elif PLANNER_LEVELING && DISABLED(SLIM_LCD_MENUS)
+    #elif HAS_LEVELING && DISABLED(SLIM_LCD_MENUS)
 
       #if DISABLED(PROBE_MANUALLY)
         MENU_ITEM(gcode, MSG_LEVEL_BED, PSTR("G28\nG29"));
