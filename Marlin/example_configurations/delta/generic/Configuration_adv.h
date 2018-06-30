1905551223698d8a2623976a334e0bb1495976e2
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-06-29 22:41:56 -0500

    Remove JUNCTION_DEVIATION_INCLUDE_E option (#11146)

--- Marlin/example_configurations/delta/generic/Configuration_adv.h
+++ Marlin/example_configurations/delta/generic/Configuration_adv.h
@@ -449,7 +449,6 @@
 //#define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
   #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
-  //#define JUNCTION_DEVIATION_INCLUDE_E
 #endif
 
 /**
