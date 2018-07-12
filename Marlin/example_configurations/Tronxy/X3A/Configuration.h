8f110190eba4f022118120010eb971209b82cfde
Scott Lahteine <github@thinkyhead.com>
2018-07-11 21:22:53 -0500

    Add Z_CLEARANCE_MULTI_PROBE to example configs

--- Marlin/example_configurations/Tronxy/X3A/Configuration.h
+++ Marlin/example_configurations/Tronxy/X3A/Configuration.h
@@ -777,9 +777,10 @@
  * Example: `M851 Z-5` with a CLEARANCE of 4  =>  9mm from bed to nozzle.
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
-#define Z_CLEARANCE_DEPLOY_PROBE   5 // Z Clearance for Deploy/Stow
+#define Z_CLEARANCE_DEPLOY_PROBE    5 // Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
-#define Z_AFTER_PROBING           5 // Z position after probing is done
+#define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
+#define Z_AFTER_PROBING             5 // Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
 
