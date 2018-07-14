e486326910615e0f77da57d4096a44c2c43b53d0
Scott Lahteine <github@thinkyhead.com>
2018-07-13 23:19:04 -0500

    Vellemann K8400 Y bed size is 180
    Co-Authored-By: kaimimue <kaimimue@users.noreply.github.com>

--- Marlin/src/config/examples/Velleman/K8400/Configuration.h
+++ Marlin/src/config/examples/Velleman/K8400/Configuration.h
@@ -868,7 +868,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define Y_BED_SIZE 180
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
--- Marlin/src/config/examples/Velleman/K8400/Dual-head/Configuration.h
+++ Marlin/src/config/examples/Velleman/K8400/Dual-head/Configuration.h
@@ -868,11 +868,11 @@
 
 // The size of the print bed
 #define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define Y_BED_SIZE 180
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 20
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
