888d319145edaaf4fb2ba6fa722f091f51725c1e
Scott Lahteine <github@thinkyhead.com>
2018-08-02 18:42:08 -0500

    Align some config options

--- Marlin/example_configurations/FolgerTech/i3-2020/Configuration_adv.h
+++ Marlin/example_configurations/FolgerTech/i3-2020/Configuration_adv.h
@@ -1615,7 +1615,7 @@
 //#define MAX7219_LOAD_PIN  36 // for RAMPS E1
 
   //#define MAX7219_GCODE          // Add the M7219 G-code to control the LED matrix
-  #define MAX7219_INIT_TEST 2      // Do a test pattern at initialization (Set to 2 for spiral)
+  #define MAX7219_INIT_TEST    2   // Do a test pattern at initialization (Set to 2 for spiral)
   #define MAX7219_NUMBER_UNITS 2   // Number of Max7219 units in chain.
   #define MAX7219_ROTATE     -90   // Rotate the display clockwise (in multiples of +/- 90°)
                                    // connector at:  right=0   bottom=-90  top=90  left=180
