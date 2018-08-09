016129b11337776d71617e16a6ba4e53ff5513b5
Michael Triffon <36581178+ModMike@users.noreply.github.com>
2018-08-09 11:18:25 -0400

    Update Configuration.h

--- Marlin/src/config/examples/Azteeg/X5 Mini WiFI/Configuration.h
+++ Marlin/src/config/examples/Azteeg/X5 Mini WiFI/Configuration.h
@@ -74,7 +74,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(ModmMike, Azteeg X5 Mini WiFi)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(ModmMike, Azteeg X5 Mini WiFI)" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
