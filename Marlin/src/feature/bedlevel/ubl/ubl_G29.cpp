009ba0247c8acd876683c21744168e5a4bb4ff78
Nils Hasenbanck <nils@hasenbanck.de>
2018-08-14 08:32:48 +0200

    Use the new const functions of the persistentStore api
    I cleaned up some functions by using the new const functions
    of the peristentStore api.

--- Marlin/src/feature/bedlevel/ubl/ubl_G29.cpp
+++ Marlin/src/feature/bedlevel/ubl/ubl_G29.cpp
@@ -1169,8 +1169,6 @@
    */
   void unified_bed_leveling::g29_eeprom_dump() {
     uint8_t cccc;
-    int kkkk;
-    uint16_t crc = 0;
 
     SERIAL_ECHO_START();
     SERIAL_ECHOLNPGM("EEPROM Dump:");
@@ -1180,8 +1178,7 @@
       print_hex_word(i);
       SERIAL_ECHOPGM(": ");
       for (uint16_t j = 0; j < 16; j++) {
-        kkkk = i + j;
-        persistentStore.read_data(kkkk, &cccc, sizeof(uint8_t), &crc);
+        persistentStore.read_data(i+j, &cccc, sizeof(uint8_t));
         print_hex_byte(cccc);
         SERIAL_ECHO(' ');
       }
