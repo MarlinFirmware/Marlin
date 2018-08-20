b0a8ce59bdc3d6f0c3470edff735340de77c710b
Roxy-3D <Roxy-3D@users.noreply.github.com>
2018-08-20 16:46:27 -0500

    Update Boards.h to include Formbot support

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -75,6 +75,7 @@
 #define BOARD_AZTEEG_X3_PRO     68    // Azteeg X3 Pro
 #define BOARD_ULTIMAIN_2        72    // Ultimainboard 2.x (Uses TEMP_SENSOR 20)
 #define BOARD_RUMBA             80    // Rumba
+#define BOARD_FORMBOT           81    // Formbot version 1.  A Rumba derivitive
 #define BOARD_BQ_ZUM_MEGA_3D    503   // bq ZUM Mega 3D
 #define BOARD_MAKEBOARD_MINI    431   // MakeBoard Mini v2.1.2 is a control board sold by MicroMake
 #define BOARD_TRIGORILLA_13     343   // TriGorilla Anycubic version 1.3 based on RAMPS EFB
