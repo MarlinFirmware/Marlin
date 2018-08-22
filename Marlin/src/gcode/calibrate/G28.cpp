593d5375eaf51f6c313c7d6b7a5edfa3a9e46a97
Colin Gilgenbach <colin@gilgenbach.net>
2018-08-21 22:42:55 -0600

    Fix G28 resetting DUAL_NOZZLE_DUPLICATION_MODE (#11605)
    There's no reason for G28 to reset DUAL_NOZZLE_DUPLICATION_MODE, because it only affects the E stepper, unlike DUAL_X_CARRIAGE which affects the X steppers as well.

--- Marlin/src/gcode/calibrate/G28.cpp
+++ Marlin/src/gcode/calibrate/G28.cpp
@@ -229,7 +229,7 @@ void GcodeSuite::G28(const bool always_home_all) {
     tool_change(0, 0, true);
   #endif
 
-  #if ENABLED(DUAL_X_CARRIAGE) || ENABLED(DUAL_NOZZLE_DUPLICATION_MODE)
+  #if ENABLED(DUAL_X_CARRIAGE)
     extruder_duplication_enabled = false;
   #endif
 
