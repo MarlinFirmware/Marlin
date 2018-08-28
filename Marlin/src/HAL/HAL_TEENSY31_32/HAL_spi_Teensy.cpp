664f8e8a62aacf67234387f2f329413d9356bfaf
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-08-27 21:33:28 -0500

    Update HAL_spi_Teensy.cpp

--- Marlin/src/HAL/HAL_TEENSY31_32/HAL_spi_Teensy.cpp
+++ Marlin/src/HAL/HAL_TEENSY31_32/HAL_spi_Teensy.cpp
@@ -1,4 +1,25 @@
-#if defined(__MK20DX256__)
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifdef __MK20DX256__
 
 #include "HAL.h"
 #include <SPI.h>
@@ -8,11 +29,14 @@
 
 static SPISettings spiConfig;
 
-// Standard SPI functions
-/** Initialise SPI bus */
+/**
+ * Standard SPI functions
+ */
+
+// Initialise SPI bus
 void spiBegin(void) {
   #if !PIN_EXISTS(SS)
-    #error SS_PIN not defined!
+    #error "SS_PIN not defined!"
   #endif
   SET_OUTPUT(SS_PIN);
   WRITE(SS_PIN, HIGH);
@@ -25,68 +49,66 @@ void spiBegin(void) {
     // set SS high - may be chip select for another SPI device
     #if SET_SPI_SS_HIGH
       WRITE(SS_PIN, HIGH);
-    #endif  // SET_SPI_SS_HIGH
+    #endif
     // set a default rate
     spiInit(SPI_HALF_SPEED); // 1
-  #endif  // SOFTWARE_SPI
+  #endif
 }
 
-/** Configure SPI for specified SPI speed */
+// Configure SPI for specified SPI speed
 void spiInit(uint8_t spiRate) {
-  // Use datarates Marlin uses
+  // Use data rates Marlin uses
   uint32_t clock;
   switch (spiRate) {
-  case SPI_FULL_SPEED:    clock = 10000000; break;
-  case SPI_HALF_SPEED:    clock =  5000000; break;
-  case SPI_QUARTER_SPEED: clock =  2500000; break;
-  case SPI_EIGHTH_SPEED:  clock =  1250000; break;
-  case SPI_SPEED_5:       clock =   625000; break;
-  case SPI_SPEED_6:       clock =   312500; break;
-  default:
-    clock = 4000000; // Default from the SPI libarary
+    case SPI_FULL_SPEED:    clock = 10000000; break;
+    case SPI_HALF_SPEED:    clock =  5000000; break;
+    case SPI_QUARTER_SPEED: clock =  2500000; break;
+    case SPI_EIGHTH_SPEED:  clock =  1250000; break;
+    case SPI_SPEED_5:       clock =   625000; break;
+    case SPI_SPEED_6:       clock =   312500; break;
+    default:                clock = 4000000; // Default from the SPI libarary
   }
   spiConfig = SPISettings(clock, MSBFIRST, SPI_MODE0);
   SPI.begin();
 }
 
-//------------------------------------------------------------------------------
-/** SPI receive a byte */
+// SPI receive a byte
 uint8_t spiRec(void) {
   SPI.beginTransaction(spiConfig);
-  uint8_t returnByte = SPI.transfer(0xFF);
+  const uint8_t returnByte = SPI.transfer(0xFF);
   SPI.endTransaction();
   return returnByte;
-//  SPDR = 0xFF;
-//  while (!TEST(SPSR, SPIF)) { /* Intentionally left empty */ }
-//  return SPDR;
+  //SPDR = 0xFF;
+  //while (!TEST(SPSR, SPIF)) { /* Intentionally left empty */ }
+  //return SPDR;
 }
-//------------------------------------------------------------------------------
-/** SPI read data  */
+
+// SPI read data
 void spiRead(uint8_t* buf, uint16_t nbyte) {
   SPI.beginTransaction(spiConfig);
   SPI.transfer(buf, nbyte);
   SPI.endTransaction();
-//if (nbyte-- == 0) return;
-//  SPDR = 0xFF;
-//for (uint16_t i = 0; i < nbyte; i++) {
-//  while (!TEST(SPSR, SPIF)) { /* Intentionally left empty */ }
-//  buf[i] = SPDR;
-//  SPDR = 0xFF;
-//}
-//while (!TEST(SPSR, SPIF)) { /* Intentionally left empty */ }
-//buf[nbyte] = SPDR;
+  //if (nbyte-- == 0) return;
+  //  SPDR = 0xFF;
+  //for (uint16_t i = 0; i < nbyte; i++) {
+  //  while (!TEST(SPSR, SPIF)) { /* Intentionally left empty */ }
+  //  buf[i] = SPDR;
+  //  SPDR = 0xFF;
+  //}
+  //while (!TEST(SPSR, SPIF)) { /* Intentionally left empty */ }
+  //buf[nbyte] = SPDR;
 }
-//------------------------------------------------------------------------------
-/** SPI send a byte */
+
+// SPI send a byte
 void spiSend(uint8_t b) {
   SPI.beginTransaction(spiConfig);
   SPI.transfer(b);
   SPI.endTransaction();
-//  SPDR = b;
-//  while (!TEST(SPSR, SPIF)) { /* Intentionally left empty */ }
+  //SPDR = b;
+  //while (!TEST(SPSR, SPIF)) { /* nada */ }
 }
-//------------------------------------------------------------------------------
-/** SPI send block  */
+
+// SPI send block
 void spiSendBlock(uint8_t token, const uint8_t* buf) {
   SPI.beginTransaction(spiConfig);
   SPDR = token;
@@ -101,10 +123,9 @@ void spiSendBlock(uint8_t token, const uint8_t* buf) {
 }
 
 
-/** Begin SPI transaction, set clock, bit order, data mode */
+// Begin SPI transaction, set clock, bit order, data mode
 void spiBeginTransaction(uint32_t spiClock, uint8_t bitOrder, uint8_t dataMode) {
   spiConfig = SPISettings(spiClock, bitOrder, dataMode);
-
   SPI.beginTransaction(spiConfig);
 }
 
