f6dbb8a792365b2acad65a47f4dc348c90a2d046
Karl Andersson <karl@iaccess.se>
2018-09-06 21:42:58 +0200

    Modified board id

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -227,7 +227,7 @@
 #define BOARD_TEENSY35_36       841   // Teensy3.5 and Teensy3.6
 #define BOARD_BEAST            1802   // STM32FxxxVxT6 Libmaple based stm32f4 controller
 #define BOARD_STM32F4          1804   // STM32 STM32GENERIC based STM32F4 controller
-#define BOARD_ARMED            1806   // STM32F407 based controller (Power outputs: Hotend0, Hotend1, Bed, Fan0, Fan1, Fan2)
+#define BOARD_ARMED            1807   // STM32F407 based controller (Power outputs: Hotend0, Hotend1, Bed, Fan0, Fan1, Fan2)
 
 //
 // ARM Cortex M7
