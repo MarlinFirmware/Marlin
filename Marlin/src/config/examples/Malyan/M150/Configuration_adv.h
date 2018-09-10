17e3d2778c21a0e903a2fbeb0f9745648e7f95af
teemuatlut <teemu.mantykallio@live.fi>
2018-09-11 00:05:33 +0300

    Update config files

--- Marlin/src/config/examples/Malyan/M150/Configuration_adv.h
+++ Marlin/src/config/examples/Malyan/M150/Configuration_adv.h
@@ -1268,7 +1268,7 @@
   #if ENABLED(SENSORLESS_HOMING)
     #define X_HOMING_SENSITIVITY  8
     #define Y_HOMING_SENSITIVITY  8
-    #define Z_HOMING_SENSITIVITY  8
+    //#define Z_HOMING_SENSITIVITY  8
   #endif
 
   /**
@@ -1307,7 +1307,7 @@
    */
   #define TMC_ADV() {  }
 
-#endif // TMC2130 || TMC2208
+#endif // HAS_TRINAMIC
 
 // @section L6470
 
