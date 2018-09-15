034719d5b33d0b2d5f4f905cdca2b59456d59a6d
Hannes Brandstaetter-Mueller <hannes.mueller@gmail.com>
2018-09-15 16:42:04 +0200

    Fix Anycubic Kossel config (probe crash)

--- Marlin/src/config/examples/delta/Anycubic/Kossel/Configuration.h
+++ Marlin/src/config/examples/delta/Anycubic/Kossel/Configuration.h
@@ -1017,8 +1017,8 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
-#define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
+#define Z_CLEARANCE_BETWEEN_PROBES 25 // Z Clearance between probe points
+#define Z_CLEARANCE_MULTI_PROBE    25 // Z Clearance between multiple probes
 #define Z_AFTER_PROBING            30 // Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
