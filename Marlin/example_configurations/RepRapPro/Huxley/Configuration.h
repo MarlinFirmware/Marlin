da5e69693d05ec00eee9e5136aae7f2390eab96d
Scott Lahteine <github@thinkyhead.com>
2018-09-15 23:17:41 -0500

    Update some config comments

--- Marlin/example_configurations/RepRapPro/Huxley/Configuration.h
+++ Marlin/example_configurations/RepRapPro/Huxley/Configuration.h
@@ -1963,9 +1963,7 @@ Black rubber belt(MXL), 18 - tooth aluminium pulley : 87.489 step per mm (Huxley
 // If the servo can't reach the requested position, increase it.
 #define SERVO_DELAY { 300 }
 
-// Servo deactivation
-//
-// With this option servos are powered only during movement, then turned off to prevent jitter.
+// Only power servos during movement, otherwise leave off to prevent jitter
 //#define DEACTIVATE_SERVOS_AFTER_MOVE
 
 #endif // CONFIGURATION_H
