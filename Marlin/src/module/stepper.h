ff51e7322d76c37ebefbbde98d11c6a837a5efbd
Cassiano Leal <cassianoleal@gmail.com>
2018-09-16 01:31:51 +0100

    Fix for Stepper::set_directions() compilation error (#11838)

--- Marlin/src/module/stepper.h
+++ Marlin/src/module/stepper.h
@@ -466,13 +466,12 @@ class Stepper {
       #endif
     }
 
-  private:
-
-    // Set the current position in steps
-    static void _set_position(const int32_t &a, const int32_t &b, const int32_t &c, const int32_t &e);
-
     // Set direction bits for all steppers
     static void set_directions();
+
+  private:
+    // Set the current position in steps
+    static void _set_position(const int32_t &a, const int32_t &b, const int32_t &c, const int32_t &e);
 
     FORCE_INLINE static uint32_t calc_timer_interval(uint32_t step_rate, uint8_t scale, uint8_t* loops) {
       uint32_t timer;
