5fbfbde532655e73b173d03676b42ad96880e4a1
Roxy-3D <Roxy-3D@users.noreply.github.com>
2018-09-17 10:41:30 -0500

    Fix example file to pull in correct pins file

--- Marlin/src/config/examples/Formbot/T-Rex_2+/Configuration.h
+++ Marlin/src/config/examples/Formbot/T-Rex_2+/Configuration.h
@@ -138,8 +138,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-//#define MOTHERBOARD BOARD_RUMBA
-#define MOTHERBOARD BOARD_FORMBOT
+  #define MOTHERBOARD BOARD_FORMBOT_TREX2
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
