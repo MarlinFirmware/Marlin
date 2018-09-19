cced13a2bbeba807290e11b5063673ebe1de606a
Scott Lahteine <github@thinkyhead.com>
2018-09-19 03:03:47 -0500

    Fix parking_extruder_tool_change
    Fix #11872

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -12316,7 +12316,7 @@ inline void invalid_extruder_error(const uint8_t e) {
 
 #if ENABLED(PARKING_EXTRUDER)
 
-  inline void parking_extruder_tool_change(const uint8_t tmp_extruder, const float fr_mm_s/*=0.0*/, bool no_move/*=false*/) {
+  inline void parking_extruder_tool_change(const uint8_t tmp_extruder, bool no_move) {
     constexpr float z_raise = PARKING_EXTRUDER_SECURITY_RAISE;
 
     if (!no_move) {
@@ -12480,7 +12480,8 @@ void tool_change(const uint8_t tmp_extruder, const float fr_mm_s/*=0.0*/, bool n
         #else // !DUAL_X_CARRIAGE
 
           set_destination_from_current();
-          #if ENABLED(PARKING_EXTRUDER) // Dual Parking extruder
+
+          #if ENABLED(PARKING_EXTRUDER)
             parking_extruder_tool_change(tmp_extruder, no_move);
           #endif
 
