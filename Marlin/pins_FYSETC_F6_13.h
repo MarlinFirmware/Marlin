6ce9bcae00ca0f683d7bc44487b92f04a5c6d818
Mitch Roote <mitch@r00t.ca>
2018-09-26 18:22:37 -0400

    Update RGB pins, formatting

--- Marlin/pins_FYSETC_F6_13.h
+++ Marlin/pins_FYSETC_F6_13.h
@@ -20,9 +20,9 @@
  *
  */
 
-/**
- * Fysetc F6 pin assignments
- */
+//
+// Fysetc F6 pin assignments
+//
 
 #ifndef __AVR_ATmega2560__
   #error "Oops!  Make sure you have 'FYSETC_F6' selected from the 'Tools -> Boards' menu."
@@ -74,12 +74,12 @@
 
 #define E2_STEP_PIN        59
 #define E2_DIR_PIN         57
-#define E2_ENABLE_PIN 40
+#define E2_ENABLE_PIN      40
 
-/* DIAG
- * DIAG is not directly connected to the MCU. Please close
- * the jumper next to the limit switch socket when using it.
- */
+//
+// DIAG is not directly connected to the MCU. Close
+// the jumper next to the limit switch socket when using it.
+//
 
 #define X_CS_PIN           70
 #define Y_CS_PIN           39
@@ -98,59 +98,56 @@
 
 
 #if HAS_DRIVER(TMC2208)
-/**
- * Software serial
- */
+  //Software serial
+  #define X_SERIAL_RX_PIN       71
+  #define X_SERIAL_TX_PIN       72
 
- #define X_SERIAL_RX_PIN       71
- #define X_SERIAL_TX_PIN       72
+  #define Y_SERIAL_RX_PIN       73
+  #define Y_SERIAL_TX_PIN       75
 
- #define Y_SERIAL_RX_PIN       73
- #define Y_SERIAL_TX_PIN       75
+  #define Z_SERIAL_RX_PIN       78
+  #define Z_SERIAL_TX_PIN       79
 
- #define Z_SERIAL_RX_PIN       78
- #define Z_SERIAL_TX_PIN       79
+  #define E0_SERIAL_RX_PIN      76
+  #define E0_SERIAL_TX_PIN      77
 
- #define E0_SERIAL_RX_PIN      76
- #define E0_SERIAL_TX_PIN      77
+  #define E1_SERIAL_RX_PIN      80
+  #define E1_SERIAL_TX_PIN      81
 
- #define E1_SERIAL_RX_PIN      80
- #define E1_SERIAL_TX_PIN      81
-
- #define E2_SERIAL_RX_PIN      22
- #define E2_SERIAL_TX_PIN      83
+  #define E2_SERIAL_RX_PIN      22
+  #define E2_SERIAL_TX_PIN      83
 #endif
 
 //
 // Temperature Sensors
 //
-#define TEMP_0_PIN         12   // Analog Input
-#define TEMP_1_PIN         13   // Analog Input
-#define TEMP_2_PIN         14   // Analog Input
-#define TEMP_BED_PIN       15   // Analog Input
+#define TEMP_0_PIN          12   // Analog Input
+#define TEMP_1_PIN          13   // Analog Input
+#define TEMP_2_PIN          14   // Analog Input
+#define TEMP_BED_PIN        15   // Analog Input
 
 //
 // Heaters / Fans
 //
-#define HEATER_0_PIN       5
-#define HEATER_1_PIN       6
-#define HEATER_2_PIN       7
-#define HEATER_BED_PIN     8
+#define HEATER_0_PIN        5
+#define HEATER_1_PIN        6
+#define HEATER_2_PIN        7
+#define HEATER_BED_PIN      8
 
-#define FAN_PIN            44
-#define FAN1_PIN           45
-#define FAN2_PIN           46
+#define FAN_PIN             44
+#define FAN1_PIN            45
+#define FAN2_PIN            46
 
 //
 // Misc. Functions
 //
-#define SDSS               53
-#define LED_PIN            13
+#define SDSS                53
+#define LED_PIN             13
 
 #if ENABLED(RGB_LED) || ENABLED(RGBW_LED)
-  #define RGB_LED_R_PIN    44
-  #define RGB_LED_G_PIN    45
-  #define RGB_LED_B_PIN    46
+  #define RGB_LED_R_PIN     3
+  #define RGB_LED_G_PIN     4
+  #define RGB_LED_B_PIN     9
   #define RGB_LED_W_PIN    -1
 #endif
 
@@ -166,34 +163,9 @@
   #define PS_ON_PIN         11
 #endif
 
-//#if ENABLED(CASE_LIGHT_ENABLE) && !PIN_EXISTS(CASE_LIGHT) && !defined(SPINDLE_LASER_ENABLE_PIN)
-//  #if !defined(NUM_SERVOS) || NUM_SERVOS == 0 // try to use servo connector first
-//    #define CASE_LIGHT_PIN   6      // MUST BE HARDWARE PWM
-//  #elif !(ENABLED(ULTRA_LCD) && ENABLED(NEWPANEL) \
-//      && (ENABLED(PANEL_ONE) || ENABLED(VIKI2) || ENABLED(miniVIKI) || ENABLED(MINIPANEL) || ENABLED(REPRAPWORLD_KEYPAD)))  // try to use AUX 2
-//    #define CASE_LIGHT_PIN   44     // MUST BE HARDWARE PWM
-//  #endif
-//#endif
-
 //
-// M3/M4/M5 - Spindle/Laser Control
+// LCDs and Controllers
 //
-//#if ENABLED(SPINDLE_LASER_ENABLE) && !PIN_EXISTS(SPINDLE_LASER_ENABLE)
-//  #if !defined(NUM_SERVOS) || NUM_SERVOS == 0 // try to use servo connector first
-//    #define SPINDLE_LASER_ENABLE_PIN  4  // Pin should have a pullup/pulldown!
-//    #define SPINDLE_LASER_PWM_PIN     6  // MUST BE HARDWARE PWM
-//    #define SPINDLE_DIR_PIN           5
-//  #elif !(ENABLED(ULTRA_LCD) && ENABLED(NEWPANEL) \
-//      && (ENABLED(PANEL_ONE) || ENABLED(VIKI2) || ENABLED(miniVIKI) || ENABLED(MINIPANEL) || ENABLED(REPRAPWORLD_KEYPAD)))  // try to use AUX 2
-//    #define SPINDLE_LASER_ENABLE_PIN 40  // Pin should have a pullup/pulldown!
-//    #define SPINDLE_LASER_PWM_PIN    44  // MUST BE HARDWARE PWM
-//    #define SPINDLE_DIR_PIN          65
-//  #endif
-//#endif
-
-//////////////////////////
-// LCDs and Controllers //
-//////////////////////////
 #define LCD_PINS_RS         16
 #define LCD_PINS_ENABLE     17
 #define LCD_PINS_D4         23
