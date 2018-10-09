a09653f6702e5dc8699c804a7471c5a5e0d5b15a
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-10-09 16:01:11 -0500

    Update Configuration.h

--- Marlin/example_configurations/Creality/Ender-3/Configuration.h
+++ Marlin/example_configurations/Creality/Ender-3/Configuration.h
@@ -605,7 +605,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.000, 80.000, 400.000, 93.00 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
 
 /**
  * Default Max Feed Rate (mm/s)
