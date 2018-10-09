1ee284d5131729e1736eda5b814e0c3f4d429a56
Scott Lahteine <github@thinkyhead.com>
2018-10-08 19:06:45 -0500

    Add SD_FIRMWARE_UPDATE to examples
    Co-Authored-By: timoxd7 <timoxd7@users.noreply.github.com>

--- Marlin/src/config/examples/MakerParts/Configuration_adv.h
+++ Marlin/src/config/examples/MakerParts/Configuration_adv.h
@@ -701,6 +701,22 @@
     #define USB_INTR_PIN       SD_DETECT_PIN
   #endif
 
+  /**
+   * When using a bootloader that supports SD-Firmware-Flashing,
+   * add a menu item to activate SD-FW-Update on the next reboot.
+   * 
+   * Requires ATMEGA2560 (Arduino Mega)
+   * 
+   * Tested with this bootloader:
+   *   https://github.com/FleetProbe/MicroBridge-Arduino-ATMega2560
+   */
+  //#define SD_FIRMWARE_UPDATE
+  #if ENABLED(SD_FIRMWARE_UPDATE)
+    #define SD_FIRMWARE_UPDATE_EEPROM_ADDR    0x1FF
+    #define SD_FIRMWARE_UPDATE_ACTIVE_VALUE   0xF0
+    #define SD_FIRMWARE_UPDATE_INACTIVE_VALUE 0xFF
+  #endif
+
 #endif // SDSUPPORT
 
 /**
