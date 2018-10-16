05bb014a59e1a8bebfc3391247ea785ba069a096
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-10-16 01:55:33 -0500

    Update Configuration.h

--- Marlin/example_configurations/delta/Anycubic/Kossel/Configuration.h
+++ Marlin/example_configurations/delta/Anycubic/Kossel/Configuration.h
@@ -25,8 +25,10 @@
  */
 // normal size or plus?
 //#define ANYCUBIC_KOSSEL_PLUS
+
 // Anycubic Probe version 1 or 2 see README.md; 0 for no probe
 #define ANYCUBIC_PROBE_VERSION 0
+
 // Heated Bed:
 // 0 ... no heated bed
 // 1 ... aluminium heated bed with "BuildTak-like" sticker
@@ -646,27 +648,11 @@
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
 #define X_MIN_ENDSTOP_INVERTING false  // set to true to invert the logic of the endstop.
 #define Y_MIN_ENDSTOP_INVERTING false  // set to true to invert the logic of the endstop.
-
-#if ANYCUBIC_PROBE_VERSION == 0 // if no probe installed, do not invert
-  #define Z_MIN_ENDSTOP_INVERTING false
-#elif ANYCUBIC_PROBE_VERSION == 1 // V1 probe is NO so invert the logic
-  #define Z_MIN_ENDSTOP_INVERTING true  // set to true to invert the logic of the endstop.
-#elif ANYCUBIC_PROBE_VERSION == 2 // V2 probe is NC so do not invert the logic
-  #define Z_MIN_ENDSTOP_INVERTING false
-#endif
-
+#define Z_MIN_ENDSTOP_INVERTING (ANYCUBIC_PROBE_VERSION + 0 == 1) // V1 is NO, V2 is NC
 #define X_MAX_ENDSTOP_INVERTING false  // set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING false  // set to true to invert the logic of the endstop.
 #define Z_MAX_ENDSTOP_INVERTING false  // set to true to invert the logic of the endstop.
-
-#if ANYCUBIC_PROBE_VERSION == 0 // if no probe installed, do not invert
-  #define Z_MIN_PROBE_ENDSTOP_INVERTING false
-#elif ANYCUBIC_PROBE_VERSION == 1 // V1 probe is NO so invert the logic
-  #define Z_MIN_PROBE_ENDSTOP_INVERTING true  // set to true to invert the logic of the endstop.
-#elif ANYCUBIC_PROBE_VERSION == 2 // V2 probe is NC so do not invert the logic
-  #define Z_MIN_PROBE_ENDSTOP_INVERTING false
-#endif
-
+#define Z_MIN_PROBE_ENDSTOP_INVERTING Z_MIN_ENDSTOP_INVERTING
 
 /**
  * Stepper Drivers
