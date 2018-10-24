e48cbff10dcc469501c4530e4e4d1da4993e7f49
Roman Moravčík <roman.moravcik@gmail.com>
2018-10-24 22:00:07 +0200

    Fixed redefinition warnings of MSG_HOTEND_TOO_COLD. (#12203)

--- Marlin/src/core/language.h
+++ Marlin/src/core/language.h
@@ -200,7 +200,7 @@
 #define MSG_ENDSTOPS_HIT                    "endstops hit: "
 #define MSG_ERR_COLD_EXTRUDE_STOP           " cold extrusion prevented"
 #define MSG_ERR_LONG_EXTRUDE_STOP           " too long extrusion prevented"
-#define MSG_HOTEND_TOO_COLD                 "Hotend too cold"
+#define MSG_ERR_HOTEND_TOO_COLD             "Hotend too cold"
 
 #define MSG_FILAMENT_CHANGE_HEAT            "Press button (or M108) to heat nozzle"
 #define MSG_FILAMENT_CHANGE_INSERT          "Insert filament and press button (or M108)"
