e8031b115249385700006146f273bcd220394ba9
Scott Lahteine <github@thinkyhead.com>
2018-10-25 11:12:24 -0500

    Add Custom Microstepping options

--- Marlin/src/config/examples/Creality/CR-8/Configuration_adv.h
+++ Marlin/src/config/examples/Creality/CR-8/Configuration_adv.h
@@ -478,6 +478,17 @@
  */
 //#define ADAPTIVE_STEP_SMOOTHING
 
+/**
+ * Custom Microstepping
+ * Override as-needed for your setup. Up to 3 MS pins are supported.
+ */
+//#define MICROSTEP1 LOW,LOW,LOW
+//#define MICROSTEP2 HIGH,LOW,LOW
+//#define MICROSTEP4 LOW,HIGH,LOW
+//#define MICROSTEP8 HIGH,HIGH,LOW
+//#define MICROSTEP16 LOW,LOW,HIGH
+//#define MICROSTEP32 HIGH,LOW,HIGH
+
 // Microstep setting (Only functional when stepper driver microstep pins are connected to MCU.
 #define MICROSTEP_MODES { 16, 16, 16, 16, 16, 16 } // [1,2,4,8,16]
 
