e666b91fa5b2032fd924490ad3da1ceab1a3a002
Scott Lahteine <github@thinkyhead.com>
2018-11-05 22:35:29 -0600

    Clean up GTM32_PRO_VB pins

--- Marlin/src/pins/pins_GTM32_PRO_VB.h
+++ Marlin/src/pins/pins_GTM32_PRO_VB.h
@@ -24,9 +24,9 @@
   #error "Oops!  Make sure you have an STM32F1 board selected from the 'Tools -> Boards' menu."
 #endif
 
-
 #define DEFAULT_MACHINE_NAME "STM32F103VET6"
 #define BOARD_NAME "GTM32 Pro VB"
+
 //#define DISABLE_DEBUG
 //#define DISABLE_JTAG
 //#define DISABLE_JTAGSWD
@@ -51,19 +51,13 @@
 #define X_DIR_PIN          PD13
 #define X_ENABLE_PIN       PA8
 
-
-//#undef  USB_DM
-//#undef  CAN_TX
-//#undef  USB_DP
-//#undef  CAN_RX
 #define Y_STEP_PIN         PA12
 #define Y_DIR_PIN          PA11
-//#undef BOARD_JTDI_PIN
-#define Y_ENABLE_PIN       PA15   
+#define Y_ENABLE_PIN       PA15
 
 #define Z_STEP_PIN         PD6
 #define Z_DIR_PIN          PD3
-#define Z_ENABLE_PIN       PB3    
+#define Z_ENABLE_PIN       PB3
 
 #define E0_STEP_PIN        PC14
 #define E0_DIR_PIN         PC13
@@ -78,73 +72,68 @@
 #define E2_ENABLE_PIN      PC4
 
 //
-// Endstops
+// Heaters / Fans
 //
-#define X_MIN_PIN          PE5
-#define X_MAX_PIN          PE4
+#define HEATER_0_PIN       PB0
+#define HEATER_BED_PIN     PB1
 
-#define Y_MIN_PIN          PE3
-#define Y_MAX_PIN          PE2
-
-#define Z_MIN_PIN          PE1
-#define Z_MAX_PIN          PE0
-
-//
-// Heaters 
-//
-#define HEATER_0_PIN       PB0   // EXTRUDER 1
-#define HEATER_1_PIN       -1	 // EXTRUDER 2
-#define HEATER_2_PIN       -1	 // EXTRUDER 3
-
-#define HEATER_BED_PIN     PB1	 // BED
-
-
-//
-// Fans
-//
-#define FAN0_PIN           PB7  // ToDo: Add functionality
-#define FAN1_PIN           PB8  // ToDo: Add functionality
-#define FAN2_PIN           PB9  // ToDo: Add functionality
+#define FAN0_PIN           PB7   // TODO: Add functionality
+#define FAN1_PIN           PB8   // TODO: Add functionality
+#define FAN2_PIN           PB9   // TODO: Add functionality
 
 //
 // Temperature Sensors
 //
-#define TEMP_BED_PIN       PC3   // PORT NUMBERING
 #define TEMP_0_PIN         PC2   // PORT NUMBERING
-#define TEMP_1_PIN         -1   // PORT NUMBERING
-#define TEMP_2_PIN	   -1   // PORT NUMBERING
+#define TEMP_BED_PIN       PC3   // PORT NUMBERING
 
 //
-// Beep
+// Misc. Functions
 //
-//#define BEEP               PB10
+#define LED_PWM            PD12   // External LED, pin 2 on LED labeled connector
+//#define RESET_PIN        NRST
+#define T_PEN              PE6
 
+#define BEEPER_PIN         PB10
+
+//
+// LCD / Controller
+//
 #if ENABLED(ULTRA_LCD)
 
-
   #if ENABLED(REPRAP_DISCOUNT_SMART_CONTROLLER)
-    #define LCD_PINS_RS         PE6   // CS chip select /SS chip slave select
-    #define LCD_PINS_ENABLE     PE14   // SID (MOSI)
-    #define LCD_PINS_D4         PD8   // SCK (CLK) clock
-    #define LCD_PINS_D5         PD9
-    #define LCD_PINS_D6         PD10
-    #define LCD_PINS_D7         PE15
-    #define BEEPER_PIN        PB10
+    #define LCD_PINS_RS    PE6   // CS chip select /SS chip slave select
+    #define LCD_PINS_ENABLE PE14 // SID (MOSI)
+    #define LCD_PINS_D4    PD8   // SCK (CLK) clock
+    #define LCD_PINS_D5    PD9
+    #define LCD_PINS_D6    PD10
+    #define LCD_PINS_D7    PE15
 
-    #define BTN_EN1           PE8
-    #define BTN_EN2           PE9
-    #define BTN_ENC           PE13
+    #define BTN_EN1        PE8
+    #define BTN_EN2        PE9
+    #define BTN_ENC        PE13
 
-    #define SD_DETECT_PIN     PC7
-    #define KILL_PIN          -1
-
+    #define SD_DETECT_PIN  PC7
+    #define KILL_PIN       -1
   #endif
 
+  #ifndef ST7920_DELAY_1
+    #define ST7920_DELAY_1 DELAY_NS(96)
+  #endif
+  #ifndef ST7920_DELAY_2
+    #define ST7920_DELAY_2 DELAY_NS(48)
+  #endif
+  #ifndef ST7920_DELAY_3
+    #define ST7920_DELAY_3 DELAY_NS(715)
+  #endif
+
+  //
+  // UART LCD Pins, if used
+  //
+  //#define LCD_PINS_D14   PD9   // RXD3
+  //#define LCD_PINS_D13   PD8   // TXD3
 
 #endif // ULTRA_LCD
-
-//#define RESET            NRST
-#define T_PEN              PE6
 
 //
 // SPI2 on LCD conn
@@ -166,19 +155,13 @@
 #define SD_CLK             PC12
 
 //
-// UART LCD Pins, if used
-//
-//#define LCD_PINS_D14          PD9           //	RXD3
-//#define LCD_PINS_D13          PD8           //	TXD3
-
-//
 // SPI Flash (on board Card Socket)
 //
 #define SPI1_NSS           PA4
 #define SPI1_SCK           PA5
 #define SPI1_MISO          PA6
 #define SPI1_MOSI          PA7
-			
+
 //
 // Debug
 //
@@ -188,27 +171,15 @@
 //
 // Wifi
 //
-#define USART2_RX          PA3			//default alternate function 
-#define USART2_TX          PA2			//default alternate function 
+#define USART2_RX          PA3   // Default alternate function
+#define USART2_TX          PA2   // Default alternate function
 
 //
-// MISC
+// For future use
 //
-#define LED_PWM            PD12		//external LED, pin 2 on LED labeled connector
-
-#ifndef ST7920_DELAY_1
-  #define ST7920_DELAY_1 DELAY_NS(96)
-#endif
-
-#ifndef ST7920_DELAY_2
-  #define ST7920_DELAY_2 DELAY_NS(48)
-#endif
-
-#ifndef ST7920_DELAY_3
-  #define ST7920_DELAY_3 DELAY_NS(715)
-#endif
-
-//
-// Boot
-//
+//#undef USB_DM
+//#undef CAN_TX
+//#undef USB_DP
+//#undef CAN_RX
+//#undef BOARD_JTDI_PIN
 //#define BOOT0            BOOT0
