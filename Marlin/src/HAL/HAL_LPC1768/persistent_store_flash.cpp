ebc6bece58393e2127aadf6c1426337c49a061a8
Andy Shaw <andy-git@gloomy-place.com>
2018-11-21 18:25:59 +0000

    Ensure correct alignment of ram buffer
    The LPC176X flash API requires that the RAM buffer used for write operations must be word aligned. This change ensures that this is the case.

--- Marlin/src/HAL/HAL_LPC1768/persistent_store_flash.cpp
+++ Marlin/src/HAL/HAL_LPC1768/persistent_store_flash.cpp
@@ -57,7 +57,7 @@ extern "C" {
 #define EEPROM_ERASE (0xff)
 #define SLOT_ADDRESS(sector, slot) (((uint8_t *)SECTOR_START(sector)) + slot * EEPROM_SIZE)
 
-static uint8_t ram_eeprom[EEPROM_SIZE];
+static uint8_t ram_eeprom[EEPROM_SIZE] __attribute__((aligned(4))) = {0};
 static bool eeprom_dirty = false;
 static int current_slot = 0;
 
