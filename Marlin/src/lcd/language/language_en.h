54bbc223c7d2f5923183476795708d3d2111d7bd
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-11-23 20:10:44 -0600

    Update language_en.h

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -28,11 +28,11 @@
  * See also http://marlinfw.org/docs/development/lcd_language.html
  *
  */
+#define THIS_LANGUAGES_SPECIAL_SYMBOLS        _UxGT("³")
 
 #define en 1234
 #if LCD_LANGUAGE == en
   #define NOT_EXTENDED_ISO10646_1_5X7
-  #define THIS_LANGUAGES_SPECIAL_SYMBOLS      _UxGT("³")
 #endif
 #undef en
 
