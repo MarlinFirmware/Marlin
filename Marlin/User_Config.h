fc00bd07672ddbf97d5b20bb3266680da999984b
XBrav <bryan.baker.xbrav@gmail.com>
2018-11-25 20:54:03 -0700

    New file to allow Configuration.h overrides

---
+++ Marlin/User_Config.h
@@ -0,0 +1,10 @@
+//===========================================================================
+//======================== USER CONFIGURATION OVERRIDES =====================
+//===========================================================================
+
+/*
+Add any overrides or enables to the default Configuration.h files here
+
+ie/ #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
+
+*/
