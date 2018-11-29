03ef2d6c82048564db284bc3965595c561bf5840
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-11-28 19:28:31 -0600

    Fix "paused" message and M125 called by M25 (#12551)

--- Marlin/src/lcd/ultralcd.cpp
+++ Marlin/src/lcd/ultralcd.cpp
@@ -26,9 +26,7 @@
 #if HAS_SPI_LCD || ENABLED(MALYAN_LCD) || ENABLED(EXTENSIBLE_UI)
   #include "ultralcd.h"
   MarlinUI ui;
-  #if ENABLED(SDSUPPORT)
-    #include "../sd/cardreader.h"
-  #endif
+  #include "../sd/cardreader.h"
   #if ENABLED(EXTENSIBLE_UI)
     #define START_OF_UTF8_CHAR(C) (((C) & 0xC0u) != 0x80u)
   #endif
@@ -1243,7 +1241,7 @@ void MarlinUI::update() {
     static const char printing[] PROGMEM = MSG_PRINTING;
     static const char welcome[] PROGMEM = WELCOME_MSG;
     PGM_P msg;
-    if (print_job_timer.isPaused())
+    if (!IS_SD_PRINTING() && print_job_timer.isPaused())
       msg = paused;
     #if ENABLED(SDSUPPORT)
       else if (IS_SD_PRINTING())
@@ -1257,4 +1255,4 @@ void MarlinUI::update() {
     set_status_P(msg, -1);
   }
 
-#endif
+#endif // HAS_SPI_LCD || EXTENSIBLE_UI
