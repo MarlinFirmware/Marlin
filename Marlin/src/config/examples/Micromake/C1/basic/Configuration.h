0947b92734446ce1f46eea6f753662a584f08ccc
Dave Johnson <davejohnson3000@gmail.com>
2018-12-05 15:21:40 -0800

    Build and CI fixes and optimizations (#12584)
    -Disable LDF "deep+" mode on AT90USB platforms. Appears not needed any longer (likely due to fix https://github.com/platformio/platformio-core/commit/7322df26ad8d43029b992a9bd3f3369840bc40da). Results in identical binary output and cuts compile time in half
    -Disable Cartesio config from CircleCI (compile failure)
    -Disable Geetech I3 Pro X GT2560 from CircleCI (compile failure)
    -Enable EEPROM on Micromake example config (fix compile failure)
    -Move FolgerTech/i3-2020 to AVR platform in CircleCI (fix CirculeCI build failure)
    -Disable various examples failing to build in CircleCI
    -Enable various examples no longer failing to build in CircleCI

--- Marlin/src/config/examples/Micromake/C1/basic/Configuration.h
+++ Marlin/src/config/examples/Micromake/C1/basic/Configuration.h
@@ -1288,7 +1288,7 @@
 // M501 - reads parameters from EEPROM (if you need reset them after you changed them temporarily).
 // M502 - reverts to the default "factory settings".  You still need to store them in EEPROM afterwards if you want to.
 //
-//#define EEPROM_SETTINGS // Enable for M500 and M501 commands
+#define EEPROM_SETTINGS // Enable for M500 and M501 commands
 //#define DISABLE_M503    // Saves ~2700 bytes of PROGMEM. Disable for release!
 #define EEPROM_CHITCHAT   // Give feedback on EEPROM commands. Disable to save PROGMEM.
 
