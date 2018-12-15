a4710eef38b6ec7a4241c3d2c1b9b3ed265027a5
jmz52 <alexander.gavrilenko@gmail.com>
2018-12-15 17:03:57 +0300

    Add support for MKS Robin board
    Implement initial support for MKS Robin (STM32F103ZET6) board.
    Custom build script is used to generate encrypted firmware compatible with original MSK Robin bootloader (i.e. safe firmware update from SD card and possibility to go back to original close-source firmware).

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -224,6 +224,7 @@
 #define BOARD_STM3R_MINI       1803   // STM32 Libmaple based stm32f1 controller
 #define BOARD_GTM32_PRO_VB     1805   // STM32f103VET6 controller
 #define BOARD_MORPHEUS         1806   // STM32F103C8/STM32F103CB Libmaple based stm32f1 controller
+#define BOARD_MKS_ROBIN        1808   // MKS Robin / STM32F103ZET6
 
 //
 // STM32 ARM Cortex-M4F
