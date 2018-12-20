d3efefe5c1fa4f5c869924363005c458ca1f741a
Scott Lahteine <thinkyhead@users.noreply.github.com>
2018-12-20 17:36:55 -0600

    Update IDE to MKS_BASE_1.4.h

--- docs/IDE to MKS_BASE_1.4.h
+++ docs/IDE to MKS_BASE_1.4.h
@@ -1,72 +1,72 @@
-	// PORTLIST		
-	// -------------------------------------------		
-	0  // PE0 ** Pin2  ** D0  ** PE0_D0 (USB Programing)
-	1  // PE1 ** Pin3  ** D1  ** PE1_D1 (USB Programing)
-	2  // PE4 ** Pin6  ** D2  ** X_MAX                                    ***X+
-	3  // PE5 ** Pin7  ** D3  ** X_MIN                                    ***X-
-	4  // PG5 ** Pin1  ** D4  ** E1_MS2
-	5  // PE3 ** Pin5  ** D5  ** X_MS1
-	6  // PH3 ** Pin15 ** D6  ** X_MS2
-	7  // PH4 ** Pin16 ** D7  ** HEATER_E1          ***E1
-	8  // PH5 ** Pin17 ** D8  ** HEATER_BED         ***HEAT BED
-	9  // PH6 ** Pin18 ** D9  ** HEATER_FAN         ***FAN
-	10 // PB4 ** Pin23 ** D10 ** HEATER_E0          ***E0
-	11 // PB5 ** Pin24 ** D11 **                    ***SERVOS
-	12 // PB6 ** Pin25 ** D12 **                    ***SERVOS
-	13 // PB7 ** Pin26 ** D13 ** LED (Onboard)
-	14 // PJ1 ** Pin64 ** D14 ** Y_MIN                                     ***Y-
-	15 // PJ0 ** Pin63 ** D15 ** Y_MAX                                     ***Y+
-	16 // PH1 ** Pin13 ** D16 ** LCD_RS                            ***EXP1
-	17 // PH0 ** Pin12 ** D17 ** LDC_EN                            ***EXP1
-	18 // PD3 ** Pin46 ** D18 ** Z_MIN                                     ***Z-
-	19 // PD2 ** Pin45 ** D19 ** Z_MAX                                     ***Z+
-	20 // PD1 ** Pin44 ** D20 ** USB_BZ             ***XS6
-	21 // PD0 ** Pin43 ** D21 ** USB_SS             ***XS6
-	22 // PA0 ** Pin78 ** D22 **
-	23 // PA1 ** Pin77 ** D23 ** LCD_D4                            ***EXP1
-	24 // PA2 ** Pin76 ** D24 ** E0_EN
-	25 // PA3 ** Pin75 ** D25 ** LCD_D5                            ***EXP1
-	26 // PA4 ** Pin74 ** D26 ** E0_STEP
-	27 // PA5 ** Pin73 ** D27 ** LCD_D6                            ***EXP1
-	28 // PA6 ** Pin72 ** D28 ** E0_DIR
-	29 // PA7 ** Pin71 ** D29 ** LCD_D7                            ***EXP1
-	30 // PC7 ** Pin60 ** D30 **
-	31 // PC6 ** Pin59 ** D31 ** BTN_EN1                   ***EXP2
-	32 // PC5 ** Pin58 ** D32 **
-	33 // PC4 ** Pin57 ** D33 ** BTN_EN2                   ***EXP2
-	34 // PC3 ** Pin56 ** D34 **
-	35 // PC2 ** Pin55 ** D35 ** BTN_ENC                           ***EXP1
-	36 // PC1 ** Pin54 ** D36 **
-	37 // PC0 ** Pin53 ** D37 ** BEEPER                            ***EXP1
-	38 // PD7 ** Pin50 ** D38 ** X_EN
-	39 // PG2 ** Pin70 ** D39 **
-	40 // PG1 ** Pin52 ** D40 **
-	41 // PG0 ** Pin51 ** D41 ** KILL                      ***EXP2
-	42 // PL7 ** Pin42 ** D42 **
-	43 // PL6 ** Pin41 ** D43 **
-	44 // PL5 ** Pin40 ** D44 **
-	45 // PL4 ** Pin39 ** D45 **
-	46 // PL3 ** Pin38 ** D46 ** Z_STEP
-	47 // PL2 ** Pin37 ** D47 **
-	48 // PL1 ** Pin36 ** D48 ** Z_DIR
-	49 // PL0 ** Pin35 ** D49 ** SD_DET                    ***EXP2
-	50 // PB3 ** Pin22 ** D50 ** SD_MISO    ***ICSP ***XS6 ***EXP2
-	51 // PB2 ** Pin21 ** D51 ** SD_MOSI    ***ICSP ***XS6 ***EXP2
-	52 // PB1 ** Pin20 ** D52 ** SD_SCK     ***ICSP ***XS6 ***EXP2
-	53 // PB0 ** Pin19 ** D53 ** SD_SS                     ***EXP2
-	54 // PF0 ** Pin97 ** A0  ** X_STEP
-	55 // PF1 ** Pin96 ** A1  ** X_DIR
-	56 // PF2 ** Pin95 ** A2  ** Y_EN
-	57 // PF3 ** Pin94 ** A3  ** E1_MS1
-	58 // PF4 ** Pin93 ** A4  ** Y_MS2
-	59 // PF5 ** Pin92 ** A5  ** Y_MS1
-	60 // PF6 ** Pin91 ** A6  ** Y_STEP
-	61 // PF7 ** Pin90 ** A7  ** Y_DIR
-	62 // PK0 ** Pin89 ** A8  ** Z_EN
-	63 // PK1 ** Pin88 ** A9  ** E0_MS1
-	64 // PK2 ** Pin87 ** A10 ** E0_MS2
-	65 // PK3 ** Pin86 ** A11 ** SDC11       ***XS3
-	66 // PK4 ** Pin85 ** A12 ** ADC12       ***XS3
-	67 // PK5 ** Pin84 ** A13 ** A13_THERM2         ***A13   (TEMP_0_PIN)
-	68 // PK6 ** Pin83 ** A14 ** A14_THERM1         ***A14   (TEMP_BED_PIN)
-	69 // PK7 ** Pin82 ** A15 ** A15_THERM0         ***A15   (TEMP_1_PIN)
+// PORTLIST		
+// -------------------------------------------		
+ 0 // PE0 ** Pin2  ** D0  ** PE0_D0 (USB Programing)
+ 1 // PE1 ** Pin3  ** D1  ** PE1_D1 (USB Programing)
+ 2 // PE4 ** Pin6  ** D2  ** X_MAX                                    ***X+
+ 3 // PE5 ** Pin7  ** D3  ** X_MIN                                    ***X-
+ 4 // PG5 ** Pin1  ** D4  ** E1_MS2
+ 5 // PE3 ** Pin5  ** D5  ** X_MS1
+ 6 // PH3 ** Pin15 ** D6  ** X_MS2
+ 7 // PH4 ** Pin16 ** D7  ** HEATER_E1          ***E1
+ 8 // PH5 ** Pin17 ** D8  ** HEATER_BED         ***HEAT BED
+ 9 // PH6 ** Pin18 ** D9  ** HEATER_FAN         ***FAN
+10 // PB4 ** Pin23 ** D10 ** HEATER_E0          ***E0
+11 // PB5 ** Pin24 ** D11 **                    ***SERVOS
+12 // PB6 ** Pin25 ** D12 **                    ***SERVOS
+13 // PB7 ** Pin26 ** D13 ** LED (Onboard)
+14 // PJ1 ** Pin64 ** D14 ** Y_MIN                                     ***Y-
+15 // PJ0 ** Pin63 ** D15 ** Y_MAX                                     ***Y+
+16 // PH1 ** Pin13 ** D16 ** LCD_RS                            ***EXP1
+17 // PH0 ** Pin12 ** D17 ** LDC_EN                            ***EXP1
+18 // PD3 ** Pin46 ** D18 ** Z_MIN                                     ***Z-
+19 // PD2 ** Pin45 ** D19 ** Z_MAX                                     ***Z+
+20 // PD1 ** Pin44 ** D20 ** USB_BZ             ***XS6
+21 // PD0 ** Pin43 ** D21 ** USB_SS             ***XS6
+22 // PA0 ** Pin78 ** D22 **
+23 // PA1 ** Pin77 ** D23 ** LCD_D4                            ***EXP1
+24 // PA2 ** Pin76 ** D24 ** E0_EN
+25 // PA3 ** Pin75 ** D25 ** LCD_D5                            ***EXP1
+26 // PA4 ** Pin74 ** D26 ** E0_STEP
+27 // PA5 ** Pin73 ** D27 ** LCD_D6                            ***EXP1
+28 // PA6 ** Pin72 ** D28 ** E0_DIR
+29 // PA7 ** Pin71 ** D29 ** LCD_D7                            ***EXP1
+30 // PC7 ** Pin60 ** D30 **
+31 // PC6 ** Pin59 ** D31 ** BTN_EN1                   ***EXP2
+32 // PC5 ** Pin58 ** D32 **
+33 // PC4 ** Pin57 ** D33 ** BTN_EN2                   ***EXP2
+34 // PC3 ** Pin56 ** D34 **
+35 // PC2 ** Pin55 ** D35 ** BTN_ENC                           ***EXP1
+36 // PC1 ** Pin54 ** D36 **
+37 // PC0 ** Pin53 ** D37 ** BEEPER                            ***EXP1
+38 // PD7 ** Pin50 ** D38 ** X_EN
+39 // PG2 ** Pin70 ** D39 **
+40 // PG1 ** Pin52 ** D40 **
+41 // PG0 ** Pin51 ** D41 ** KILL                      ***EXP2
+42 // PL7 ** Pin42 ** D42 **
+43 // PL6 ** Pin41 ** D43 **
+44 // PL5 ** Pin40 ** D44 **
+45 // PL4 ** Pin39 ** D45 **
+46 // PL3 ** Pin38 ** D46 ** Z_STEP
+47 // PL2 ** Pin37 ** D47 **
+48 // PL1 ** Pin36 ** D48 ** Z_DIR
+49 // PL0 ** Pin35 ** D49 ** SD_DET                    ***EXP2
+50 // PB3 ** Pin22 ** D50 ** SD_MISO    ***ICSP ***XS6 ***EXP2
+51 // PB2 ** Pin21 ** D51 ** SD_MOSI    ***ICSP ***XS6 ***EXP2
+52 // PB1 ** Pin20 ** D52 ** SD_SCK     ***ICSP ***XS6 ***EXP2
+53 // PB0 ** Pin19 ** D53 ** SD_SS                     ***EXP2
+54 // PF0 ** Pin97 ** A0  ** X_STEP
+55 // PF1 ** Pin96 ** A1  ** X_DIR
+56 // PF2 ** Pin95 ** A2  ** Y_EN
+57 // PF3 ** Pin94 ** A3  ** E1_MS1
+58 // PF4 ** Pin93 ** A4  ** Y_MS2
+59 // PF5 ** Pin92 ** A5  ** Y_MS1
+60 // PF6 ** Pin91 ** A6  ** Y_STEP
+61 // PF7 ** Pin90 ** A7  ** Y_DIR
+62 // PK0 ** Pin89 ** A8  ** Z_EN
+63 // PK1 ** Pin88 ** A9  ** E0_MS1
+64 // PK2 ** Pin87 ** A10 ** E0_MS2
+65 // PK3 ** Pin86 ** A11 ** SDC11       ***XS3
+66 // PK4 ** Pin85 ** A12 ** ADC12       ***XS3
+67 // PK5 ** Pin84 ** A13 ** A13_THERM2         ***A13   (TEMP_0_PIN)
+68 // PK6 ** Pin83 ** A14 ** A14_THERM1         ***A14   (TEMP_BED_PIN)
+69 // PK7 ** Pin82 ** A15 ** A15_THERM0         ***A15   (TEMP_1_PIN)
