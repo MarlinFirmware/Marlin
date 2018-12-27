3e459a071ac28a9c0e3f99d3a917ee6ea8118ccd
Roman Moravčík <roman.moravcik@gmail.com>
2018-12-27 18:40:34 +0100

    [1.1.x] Limit hotend / bed temperature to maxtemp-15. (#12702)

--- Marlin/temperature.h
+++ Marlin/temperature.h
@@ -414,7 +414,7 @@ class Temperature {
       #if ENABLED(AUTO_POWER_CONTROL)
         powerManager.power_on();
       #endif
-      target_temperature[HOTEND_INDEX] = celsius;
+      target_temperature[HOTEND_INDEX] = MIN(celsius, maxttemp[HOTEND_INDEX] - 15);
       #if WATCH_HOTENDS
         start_watching_heater(HOTEND_INDEX);
       #endif
@@ -449,7 +449,7 @@ class Temperature {
         #endif
         target_temperature_bed =
           #ifdef BED_MAXTEMP
-            MIN(celsius, BED_MAXTEMP)
+            MIN(celsius, BED_MAXTEMP - 15)
           #else
             celsius
           #endif
