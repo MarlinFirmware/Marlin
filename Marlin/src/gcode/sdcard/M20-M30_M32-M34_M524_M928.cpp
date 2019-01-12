ed3ab5e212b96e259c0fb1def783fde06c35d0c7
InsanityAutomation <38436470+InsanityAutomation@users.noreply.github.com>
2019-01-12 16:30:44 -0500

    Fix M24 not starting new job (#12887)

--- Marlin/src/gcode/sdcard/M20-M30_M32-M34_M524_M928.cpp
+++ Marlin/src/gcode/sdcard/M20-M30_M32-M34_M524_M928.cpp
@@ -92,18 +92,21 @@ void GcodeSuite::M24() {
   #endif
 
   #if ENABLED(PARK_HEAD_ON_PAUSE)
-    resume_print();
-  #else
-    if (card.isFileOpen()) {
-      card.startFileprint();
-      print_job_timer.start();
+    if (did_pause_print) {
+      resume_print();
+      return;
     }
+  #endif
 
-    ui.reset_status();
-    
-    #ifdef ACTION_ON_RESUME
-      SERIAL_ECHOLNPGM("//action:" ACTION_ON_RESUME);
-    #endif
+  if (card.isFileOpen()) {
+    card.startFileprint();
+    print_job_timer.start();
+  }
+
+  ui.reset_status();
+
+  #ifdef ACTION_ON_RESUME
+    SERIAL_ECHOLNPGM("//action:" ACTION_ON_RESUME);
   #endif
 }
 
