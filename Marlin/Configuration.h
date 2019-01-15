d0462c36eb3b3b482d806f3c0bcbf8d47ef2629c
mramsey <mramsey@senetco.com>
2019-01-14 21:43:30 -0500

    fixed servo pin location for bltouch sensor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1918,7 +1918,7 @@
  */
 
 #define NUM_SERVOS 3 // Servo index starts with 0 for M280 command
-#define SERVO0_PIN 2
+#define SERVO0_PIN 27
 // Delay (in milliseconds) before the next move will start, to give the servo time to reach its target angle.
 // 300ms is a good value but you can try less delay.
 // If the servo can't reach the requested position, increase it.
