dc65d96aca9b53001ea797152a1d6f2cffafe3cd
Ludy <Ludy87@users.noreply.github.com>
2019-01-21 06:47:28 +0100

    Fix TMC compile error (#12965)

--- Marlin/src/module/stepper_indirection.h
+++ Marlin/src/module/stepper_indirection.h
@@ -62,7 +62,9 @@
     #error "Update TMCStepper library to 0.2.2 or newer."
   #endif
 
-  #define __TMC_CLASS(MODEL, A, I) TMCMarlin<TMC##MODEL##Stepper, A, I>
+  #define ____TMC_CLASS(MODEL, A, I) TMCMarlin<TMC##MODEL##Stepper, A, I>
+  #define ___TMC_CLASS(MODEL, A, I) ____TMC_CLASS(MODEL, A, I)
+  #define __TMC_CLASS(MODEL, A, I) ___TMC_CLASS(_##MODEL, A, I)
   #define _TMC_CLASS(MODEL, L) __TMC_CLASS(MODEL, L)
   #define TMC_CLASS(ST) _TMC_CLASS(ST##_DRIVER_TYPE, TMC_##ST##_LABEL)
 
