988b5aa4e640a2ef551b4b017cbb3ba8354f8995
raTmole <ratmole@users.noreply.github.com>
2019-01-31 05:10:56 +0200

    Update GT2560 V3 pins (#13036)

--- Marlin/src/pins/pins_GT2560_V3.h
+++ Marlin/src/pins/pins_GT2560_V3.h
@@ -20,7 +20,6 @@
  *
  */
 
-
 /**
  * GT2560 V3.0 pin assignment
  */
@@ -47,6 +46,22 @@
 #define Y_MAX_PIN          26
 #define Z_MIN_PIN          30
 #define Z_MAX_PIN          32
+
+//
+// Runout Sensor
+//
+#ifndef FIL_RUNOUT_PIN
+  #define FIL_RUNOUT_PIN   66
+#endif
+#ifndef FIL_RUNOUT2_PIN
+  #define FIL_RUNOUT2_PIN  67
+#endif
+
+//
+// Power Recovery
+//
+#define POWER_LOSS_PIN     69   // Pin to detect power loss
+#define POWER_LOSS_STATE   LOW
 
 //
 // Z Probe (when not Z_MIN_PIN)
@@ -94,6 +109,9 @@
 #define HEATER_2_PIN        1
 #define HEATER_BED_PIN      4
 #define FAN_PIN             9
+//#define FAN1_PIN            8
+//#define FAN2_PIN            7
+
 
 //
 // Misc. Functions
