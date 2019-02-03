be952ac48da590d267a5f11ca2bae3c129e8f06f
Scott Lahteine <github@thinkyhead.com>
2019-02-03 01:23:16 -0600

    Tweaks for final merge

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -129,10 +129,10 @@ Stepper stepper; // Singleton
 
 // private:
 
-block_t* Stepper::current_block = NULL; // A pointer to the block currently being traced
+block_t* Stepper::current_block; // (= NULL) A pointer to the block currently being traced
 
-uint8_t Stepper::last_direction_bits = 0,
-        Stepper::axis_did_move = 0;
+uint8_t Stepper::last_direction_bits, // = 0
+        Stepper::axis_did_move; // = 0
 
 bool Stepper::abort_current_block;
 
@@ -2143,8 +2143,6 @@ void Stepper::init() {
     E_AXIS_INIT(5);
   #endif
 
-  set_directions();
-
   // Init Stepper ISR to 122 Hz for quick starting
   HAL_timer_start(STEP_TIMER_NUM, 122);
 
@@ -2152,11 +2150,12 @@ void Stepper::init() {
 
   sei();
 
-  // initialize the direction bits for first moves
-  last_direction_bits = 0;
-  SET_BIT_TO(last_direction_bits, X_AXIS, INVERT_X_DIR);
-  SET_BIT_TO(last_direction_bits, Y_AXIS, INVERT_Y_DIR);
-  SET_BIT_TO(last_direction_bits, Z_AXIS, INVERT_Z_DIR);
+  // Init direction bits for first moves
+  last_direction_bits = 0
+    | (INVERT_X_DIR ? _BV(X_AXIS) : 0)
+    | (INVERT_Y_DIR ? _BV(Y_AXIS) : 0)
+    | (INVERT_Z_DIR ? _BV(Z_AXIS) : 0);
+
   set_directions();
 }
 
