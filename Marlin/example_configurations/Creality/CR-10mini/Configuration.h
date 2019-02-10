24b7070bbbd1e0ddd1140f21a90e8fec15da2885
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-02-10 14:32:52 -0600

    CR10mini E steps of 95
    See https://www.patreon.com/posts/creality-cr10-24602624

--- Marlin/example_configurations/Creality/CR-10mini/Configuration.h
+++ Marlin/example_configurations/Creality/CR-10mini/Configuration.h
@@ -620,7 +620,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.60, 80, 400, 229.4 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.60, 80, 400, 95 }
 
 /**
  * Default Max Feed Rate (mm/s)
