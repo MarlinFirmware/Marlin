fcb16ceb29c3c5d9b5edcdefbb812f9fe5083e05
Scott Lahteine <github@thinkyhead.com>
2019-02-10 00:17:38 -0600

    Update default config

--- Marlin/src/config/default/Configuration_adv.h
+++ Marlin/src/config/default/Configuration_adv.h
@@ -1536,7 +1536,7 @@
    * Define you own with
    * { <off_time[1..15]>, <hysteresis_end[-3..12]>, hysteresis_start[1..8] }
    */
-  #define CHOPPER_TIMING { 4, -2, 1 }
+  #define CHOPPER_TIMING CHOPPER_DEFAULT_12V
 
   /**
    * Monitor Trinamic drivers for error conditions,
