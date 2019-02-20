a99877e2ef9e9c8864e267483ada9825e57f5215
Dev5994 <Dev5994@gmail.com>
2019-02-20 00:43:37 -0600

    Update SanityCheck.h

--- Marlin/src/inc/SanityCheck.h
+++ Marlin/src/inc/SanityCheck.h
@@ -1438,7 +1438,7 @@ static_assert(Y_MAX_LENGTH >= Y_BED_SIZE, "Movement bounds (Y_MIN_POS, Y_MAX_POS
  * Heating status LEDs
  */
 #if ENABLED(HEAT_STAT_LEDS) && (!defined(HEAT_STAT_LED_RED_PIN) && !defined(HEAT_STAT_LED_GREEN_PIN))
-  #error "HEAT_STAT_LEDS requires HEAT_STAT_RED_PIN or HEAT_STAT_GREEN_PIN, preferably both"
+  #error "HEAT_STAT_LEDS requires HEAT_STAT_LED_RED_PIN or HEAT_STAT_LED_GREEN_PIN, preferably both"
 #endif
 
 /**
