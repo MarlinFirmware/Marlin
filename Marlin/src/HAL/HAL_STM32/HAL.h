d1ebc3d06313c52114157e6181ba38ea3a5abaa5
Karl Andersson <karl@iaccess.se>
2019-02-23 09:01:44 +0100

    Fix include order to make SERIAL_PORT_2 work

--- Marlin/src/HAL/HAL_STM32/HAL.h
+++ Marlin/src/HAL/HAL_STM32/HAL.h
@@ -42,8 +42,6 @@
 #include "fastio_STM32.h"
 #include "watchdog_STM32.h"
 
-#include "HAL_timers_STM32.h"
-
 // --------------------------------------------------------------------------
 // Defines
 // --------------------------------------------------------------------------
@@ -100,6 +98,8 @@
 #else
   #define NUM_SERIAL 1
 #endif
+
+#include "HAL_timers_STM32.h"
 
 /**
  * TODO: review this to return 1 for pins that are not analog input
