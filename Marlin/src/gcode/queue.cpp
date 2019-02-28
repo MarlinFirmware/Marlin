7de6476b3d2d2a40b7d0cfc0fdafe077fbebc43f
Chris Pepper <p3p@p3psoft.co.uk>
2019-02-28 02:31:42 +0000

    Fix binary protocol error

--- Marlin/src/gcode/queue.cpp
+++ Marlin/src/gcode/queue.cpp
@@ -453,9 +453,8 @@ void gcode_line_error(PGM_P const err, const int8_t port) {
                 stream_state = StreamState::STREAM_HEADER; // defer packet confirmation to STREAM_HEADER state
               else {
                 if (bytes_received < stream_header.filesize) {
-                  stream_state = StreamState::PACKET_RESET; // reset and receive next packet
-                  SERIAL_ECHOLNPGM("ok");                   // transmit confirm packet received and valid token
-                  SERIAL_ECHOLN(packet.header.id);
+                  stream_state = StreamState::PACKET_RESET;  // reset and receive next packet
+                  SERIAL_ECHOLNPAIR("ok", packet.header.id); // transmit confirm packet received and valid token
                 }
                 else
                   stream_state = StreamState::STREAM_COMPLETE; // no more data required
