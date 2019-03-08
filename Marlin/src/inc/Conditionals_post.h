613f9fe8b4715859e004bf9fa5361940745e59b2
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-03-08 02:15:02 -0600

    Update Conditionals_post.h

--- Marlin/src/inc/Conditionals_post.h
+++ Marlin/src/inc/Conditionals_post.h
@@ -1216,10 +1216,8 @@
 /**
  * FAST PWM FAN Settings
  */
-#if ENABLED(FAST_PWM_FAN)
-  #ifndef FAST_PWM_FAN_FREQUENCY
-    #define FAST_PWM_FAN_FREQUENCY F_CPU/(2*255*1) // Fan frequency default
-  #endif
+#if ENABLED(FAST_PWM_FAN) && !defined(FAST_PWM_FAN_FREQUENCY)
+  #define FAST_PWM_FAN_FREQUENCY ((F_CPU) / (2 * 255 * 1)) // Fan frequency default
 #endif
 
 /**
