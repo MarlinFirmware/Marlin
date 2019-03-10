2efbcc90c5ccf41337625542e3add1ad3e747de4
Tobias Frost <coldtobi@users.noreply.github.com>
2019-03-10 14:07:41 +0100

    Fix wait_for_user compile error (#13352)

--- Marlin/src/feature/emergency_parser.h
+++ Marlin/src/feature/emergency_parser.h
@@ -31,10 +31,7 @@
   #include "host_actions.h"
 #endif
 
-// External references
-extern volatile bool wait_for_user, wait_for_heatup;
-void quickstop_stepper();
-void host_response_handler(const uint8_t response);
+#include "../Marlin.h"
 
 class EmergencyParser {
 
