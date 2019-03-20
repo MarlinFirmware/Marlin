28219c2cf83b2812beeed1f19b763426322aa2e6
Scott Lahteine <github@thinkyhead.com>
2019-03-20 16:44:16 -0500

    Apply LEVEL_CORNERS_HEIGHT to all configs

--- config/examples/delta/kossel_pro/Configuration.h
+++ config/examples/delta/kossel_pro/Configuration.h
@@ -1356,6 +1356,7 @@
 #if ENABLED(LEVEL_BED_CORNERS)
   #define LEVEL_CORNERS_INSET 30    // (mm) An inset for corner leveling
   #define LEVEL_CORNERS_Z_HOP  4.0  // (mm) Move nozzle up before moving between corners
+  #define LEVEL_CORNERS_HEIGHT 0.0  // (mm) Z height of nozzle at leveling points
   //#define LEVEL_CENTER_TOO        // Move to the center after the last corner
 #endif
 
