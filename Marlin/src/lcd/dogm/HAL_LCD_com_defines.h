6d9aaca0828cab5abf0541394ebe60f04d01c989
Alexander Gavrilenko <jmz52@users.noreply.github.com>
2019-03-29 20:40:24 +0300

    Fix compilation error for MKS Robin (#13506)

--- Marlin/src/lcd/dogm/HAL_LCD_com_defines.h
+++ Marlin/src/lcd/dogm/HAL_LCD_com_defines.h
@@ -47,7 +47,7 @@
   uint8_t u8g_com_HAL_LPC1768_ssd_hw_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);
   #define U8G_COM_SSD_I2C_HAL u8g_com_arduino_ssd_i2c_fn
 
-  #if defined(STM32F1) || defined(STM32F1xx)
+  #if defined(ARDUINO_ARCH_STM32F1)
     uint8_t u8g_com_stm32duino_fsmc_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);
     #define U8G_COM_HAL_FSMC_FN u8g_com_stm32duino_fsmc_fn
   #else
