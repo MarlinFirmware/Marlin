a3f67acd6dfe33f3533da2b2e819a8b5756e2ece
Robby Candra <bytegator@gmail.com>
2019-03-31 01:14:58 +0700

    Fix Power Loss Recovery file delete (#13522)

--- Marlin/src/gcode/feature/powerloss/M413.cpp
+++ Marlin/src/gcode/feature/powerloss/M413.cpp
@@ -50,7 +50,7 @@ void GcodeSuite::M413() {
     if (parser.seen('R') || parser.seen('L')) recovery.load();
     if (parser.seen('W')) recovery.save(true);
     if (parser.seen('P')) recovery.purge();
-    if (parser.seen('E')) serialprintPGM(recovery.exists() ? PSTR("BIN Exists\n") : PSTR("No BIN\n"));
+    if (parser.seen('E')) serialprintPGM(recovery.exists() ? PSTR("PLR Exists\n") : PSTR("No PLR\n"));
     if (parser.seen('V')) serialprintPGM(recovery.valid() ? PSTR("Valid\n") : PSTR("Invalid\n"));
   #endif
 }
