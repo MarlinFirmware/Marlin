de0046fc8be4aaa6b84390841f06f3a3a891e28a
Scott Lahteine <github@thinkyhead.com>
2019-03-30 23:04:40 -0500

    Fix empty HAS_TIMER macros

--- Marlin/src/HAL/HAL_STM32F4/fastio_STM32F4.h
+++ Marlin/src/HAL/HAL_STM32F4/fastio_STM32F4.h
@@ -50,9 +50,9 @@
 
 #define IS_INPUT(IO)
 #define IS_OUTPUT(IO)
-#define HAS_TIMER(IO)
+#define HAS_TIMER(IO)           true
 
-#define PWM_PIN(P)              true
+#define PWM_PIN(P)              HAS_TIMER(P)
 #define USEABLE_HARDWARE_PWM(P) PWM_PIN(P)
 
 // digitalRead/Write wrappers
