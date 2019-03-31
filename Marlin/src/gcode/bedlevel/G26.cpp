a5ddb4813e9be094789ff8dba843aabc23ea2a94
Manuel McLure <manuel@mclure.org>
2019-03-30 19:23:38 -0700

    Minor optimization to ARC_SUPPORT code.

--- Marlin/src/gcode/bedlevel/G26.cpp
+++ Marlin/src/gcode/bedlevel/G26.cpp
@@ -775,8 +775,8 @@ void GcodeSuite::G26() {
 
         // Figure out where to start and end the arc - we always print counterclockwise
         if (xi == 0) {                             // left edge
-          sx = f ? circle_x + INTERSECTION_CIRCLE_RADIUS : circle_x;
-          ex = b ? circle_x + INTERSECTION_CIRCLE_RADIUS : circle_x;
+          sx = f ? sx : circle_x;
+          ex = b ? sx : circle_x;
           sy = f ? circle_y : circle_y - (INTERSECTION_CIRCLE_RADIUS);
           ey = b ? circle_y : circle_y + INTERSECTION_CIRCLE_RADIUS;
           arc_length = (f || b) ? ARC_LENGTH(1) : ARC_LENGTH(2);
@@ -789,15 +789,11 @@ void GcodeSuite::G26() {
           arc_length = (f || b) ? ARC_LENGTH(1) : ARC_LENGTH(2);
         }
         else if (f) {
-          sx = circle_x + INTERSECTION_CIRCLE_RADIUS;
           ex = circle_x - (INTERSECTION_CIRCLE_RADIUS);
-          sy = ey = circle_y;
           arc_length = ARC_LENGTH(2);
         }
         else if (b) {
           sx = circle_x - (INTERSECTION_CIRCLE_RADIUS);
-          ex = circle_x + INTERSECTION_CIRCLE_RADIUS;
-          sy = ey = circle_y;
           arc_length = ARC_LENGTH(2);
         }
         const float arc_offset[2] = {
