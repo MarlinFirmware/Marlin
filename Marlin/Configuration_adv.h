3ed21df32f8c1bc6abd99e627b8f1e758e356b0b
Scott Lahteine <github@thinkyhead.com>
2019-04-03 22:41:36 -0500

    Fix and simplify ENDSTOP_BACKOFF
    - Reduce to a single option
    - Supplant broken ANY() test
    - Only `endstops.enable(false)` (followed by `not_homing`) needed
    - Set default to back off 2mm from all endstops (disabled)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -456,11 +456,7 @@
 #define Z_HOME_BUMP_MM 2
 #define HOMING_BUMP_DIVISOR { 2, 2, 4 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 //#define QUICK_HOME                     // If homing includes X and Y, do a diagonal move initially
-
-// If defined, move away from the endstops after homing.
-//#define ENDSTOP_BACKOFF_X 0
-//#define ENDSTOP_BACKOFF_Y 0
-//#define ENDSTOP_BACKOFF_Z 0
+//#define HOMING_BACKOFF_MM { 2, 2, 2 }  // (mm) Move away from the endstops after homing
 
 // When G28 is called, this option will make Y home before X
 //#define HOME_Y_BEFORE_X
