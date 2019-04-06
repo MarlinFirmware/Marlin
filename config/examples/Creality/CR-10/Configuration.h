5a6caba90e99f16dcf6d54705b097d5f03dfbab5
swilkens <stefanwilkens@gmail.com>
2019-04-06 16:15:06 +0200

    Reduce homing speed of CR-10
    Applying https://github.com/MarlinFirmware/Marlin/issues/12074 to all Creality printers

--- config/examples/Creality/CR-10/Configuration.h
+++ config/examples/Creality/CR-10/Configuration.h
@@ -1290,7 +1290,7 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (50*60)
+#define HOMING_FEEDRATE_XY (20*60)
 #define HOMING_FEEDRATE_Z  (4*60)
 
 // Validate that endstops are triggered on homing moves
