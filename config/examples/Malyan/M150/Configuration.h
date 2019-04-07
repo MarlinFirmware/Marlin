a2b3ad6d0a4e7b238288b01b6f282009a72f7a53
Scott Lahteine <github@thinkyhead.com>
2019-04-07 16:31:34 -0500

    Don't override RGB LED pins, by default

--- config/examples/Malyan/M150/Configuration.h
+++ config/examples/Malyan/M150/Configuration.h
@@ -2078,10 +2078,10 @@
 //#define RGBW_LED
 
 #if EITHER(RGB_LED, RGBW_LED)
-  #define RGB_LED_R_PIN 34
-  #define RGB_LED_G_PIN 43
-  #define RGB_LED_B_PIN 35
-  #define RGB_LED_W_PIN -1
+  //#define RGB_LED_R_PIN 34
+  //#define RGB_LED_G_PIN 43
+  //#define RGB_LED_B_PIN 35
+  //#define RGB_LED_W_PIN -1
 #endif
 
 // Support for Adafruit Neopixel LED driver
