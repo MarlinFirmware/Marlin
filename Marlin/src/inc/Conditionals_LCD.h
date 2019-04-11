9fd3e64ba82790f9389f6f1d2b58455802f4a303
Ludy <Ludy87@users.noreply.github.com>
2019-04-11 20:58:50 +0200

    Fix compile error with EXTRUDER>1 (#13651)

--- Marlin/src/inc/Conditionals_LCD.h
+++ Marlin/src/inc/Conditionals_LCD.h
@@ -425,7 +425,7 @@
  */
 #if ENABLED(DISTINCT_E_FACTORS) && E_STEPPERS > 1
   #define XYZE_N (XYZ + E_STEPPERS)
-  #define E_AXIS_N(E) (E_AXIS + E)
+  #define E_AXIS_N(E) AxisEnum(E_AXIS + E)
 #else
   #undef DISTINCT_E_FACTORS
   #define XYZE_N XYZE
