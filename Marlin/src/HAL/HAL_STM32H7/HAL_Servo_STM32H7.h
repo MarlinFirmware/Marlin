abe760db0dcd3456eebd0159a1cc410efc3c280a
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-04-16 01:37:39 -0500

    Update HAL_Servo_STM32H7.h

--- Marlin/src/HAL/HAL_STM32H7/HAL_Servo_STM32H7.h
+++ Marlin/src/HAL/HAL_STM32H7/HAL_Servo_STM32H7.h
@@ -26,11 +26,11 @@
 // Inherit and expand on the official library
 class libServo : public Servo {
 public:
-    int8_t attach(const int pin);
-    int8_t attach(const int pin, const int min, const int max);
-    void move(const int value);
+  int8_t attach(const int pin);
+  int8_t attach(const int pin, const int min, const int max);
+  void move(const int value);
 private:
-    uint16_t min_ticks;
-    uint16_t max_ticks;
-    uint8_t servoIndex;               // index into the channel data for this servo
+  uint16_t min_ticks;
+  uint16_t max_ticks;
+  uint8_t servoIndex; // index into the channel data for this servo
 };
