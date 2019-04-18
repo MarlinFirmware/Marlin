724ebaa97851d5df272f222cc6640416eb67aec8
Alessandro Corbelli <alessandro.corbelli@guest.it>
2019-04-18 16:57:54 +0200

    Riattivato BACKLASH_COMPENSATION

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -555,7 +555,7 @@
 // Backlash Compensation
 // Adds extra movement to axes on direction-changes to account for backlash.
 //
-//#define BACKLASH_COMPENSATION //costs 48b
+#define BACKLASH_COMPENSATION //costs 48b
 #if ENABLED(BACKLASH_COMPENSATION)
   // Define values for backlash distance and correction.
   // If BACKLASH_GCODE is enabled these values are the defaults.
