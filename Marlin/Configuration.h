608598668626791c44f1d3db53f498a95bcb7625
Alessandro Corbelli <alessandro.corbelli@guest.it>
2019-04-18 16:36:44 +0200

    Disattivata JUNCTION_DEVIATION Riattivata S_CURVE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -715,7 +715,7 @@
 //
 // Use Junction Deviation instead of traditional Jerk Limiting
 //
-#define JUNCTION_DEVIATION
+//#define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
   #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
 #endif
@@ -744,7 +744,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-//#define S_CURVE_ACCELERATION //costs 274b
+#define S_CURVE_ACCELERATION //costs 274b
 
 //===========================================================================
 //============================= Z Probe Options =============================
