6ef18c38001d9d45e45c974ae89dc36a2171eaf9
XtracT <xtract47@gmail.com>
2019-04-22 18:03:57 +0200

    Bchange build upload fw script

--- build_upload_fw.sh
+++ build_upload_fw.sh
@@ -1,1 +1,8 @@
-pio run --target upload
+sudo mount $(ls /dev/sd*1) /mnt/sdcard
+pio run
+sudo cp .pioenvs/LPC1768/firmware.bin /mnt/sdcard/
+sleep 2
+stty speed 115200 < /dev/ttyACM0
+stty speed 115200 < /dev/ttyACM0
+sleep 2
+echo -ne 'M997\n' > /dev/ttyACM0
