1fbdb4e045cd9ba80ad36a340863bfdb67344091
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2019-04-29 08:01:09 +0200

    menus: allow 0.025mm moves on Z axis
    Can be useful to test Z probes.
    
    our Z steps are 400/mm, 0.025mm = 10 steps

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -543,6 +543,9 @@
 #ifndef MSG_HOTEND_TOO_COLD
   #define MSG_HOTEND_TOO_COLD                 _UxGT("Hotend too cold")
 #endif
+#ifndef MSG_MOVE_0025MM
+  #define MSG_MOVE_0025MM                     _UxGT("Move 0.025mm")
+#endif
 #ifndef MSG_MOVE_01MM
   #define MSG_MOVE_01MM                       _UxGT("Move 0.1mm")
 #endif
--- Marlin/src/lcd/language/language_fr.h
+++ Marlin/src/lcd/language/language_fr.h
@@ -195,6 +195,7 @@
 #define MSG_MOVE_Z                          _UxGT("Déplacer Z")
 #define MSG_MOVE_E                          _UxGT("Extrudeur")
 #define MSG_HOTEND_TOO_COLD                 _UxGT("Buse trop froide")
+#define MSG_MOVE_0025MM                     _UxGT("Déplacer 0.025mm")
 #define MSG_MOVE_01MM                       _UxGT("Déplacer 0.1mm")
 #define MSG_MOVE_1MM                        _UxGT("Déplacer 1mm")
 #define MSG_MOVE_10MM                       _UxGT("Déplacer 10mm")
