42f905b4c848f397a6f4d8dc130210f5dcd5826f
Chris Hinger <chris@modis.io>
2019-04-30 11:57:08 +0200

    revert min stepper pulse length change

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1157,7 +1157,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-#define MINIMUM_STEPPER_PULSE 1
+//#define MINIMUM_STEPPER_PULSE 2
 
 /**
  * Maximum stepping rate (in Hz) the stepper driver allows
