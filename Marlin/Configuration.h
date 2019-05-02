baf77f4e0902fbd6bb969a1561e70908d714595b
Alessandro Corbelli <alessandro.corbelli@guest.it>
2019-05-02 18:37:57 +0200

    Settato Z-Offset sensore

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -881,7 +881,7 @@
 /* 3DTouch Supporto custom */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -19    // X offset: -left  +right  [of the nozzle]  -37 = 3dtouch -40 = bltouch
 #define Y_PROBE_OFFSET_FROM_EXTRUDER   1    // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER   0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.40 // Z offset: -below +above  [the nozzle]
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 30 // all insets = min probe edge
 
