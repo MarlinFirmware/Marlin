f36d87ef86338df82a4a15161be924b116242db9
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-05-04 17:50:06 -0500

    Update u8g_com_HAL_LPC1768_sw_spi.cpp

--- Marlin/src/HAL/HAL_LPC1768/u8g/u8g_com_HAL_LPC1768_sw_spi.cpp
+++ Marlin/src/HAL/HAL_LPC1768/u8g/u8g_com_HAL_LPC1768_sw_spi.cpp
@@ -159,14 +159,14 @@ uint8_t u8g_com_HAL_LPC1768_sw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, 
 
     case U8G_COM_MSG_CHIP_SELECT:
       #if ENABLED(FYSETC_MINI_12864)           // LCD SPI is running mode 3 while SD card is running mode 0
-        if (arg_val) {                        //   SCK idle state needs to be set to the proper idle state before
+        if (arg_val) {                         //   SCK idle state needs to be set to the proper idle state before
                                                //   the next chip select goes active
-          u8g_SetPILevel(u8g, U8G_PI_SCK, 1);  //set SCK to mode 3 idle state before CS goes active
+          u8g_SetPILevel(u8g, U8G_PI_SCK, 1);  // Set SCK to mode 3 idle state before CS goes active
           u8g_SetPILevel(u8g, U8G_PI_CS, LOW);
         }
         else {
           u8g_SetPILevel(u8g, U8G_PI_CS, HIGH);
-          u8g_SetPILevel(u8g, U8G_PI_SCK, 0); //set SCK to mode 0 idle state after CS goes inactive
+          u8g_SetPILevel(u8g, U8G_PI_SCK, 0);  // Set SCK to mode 0 idle state after CS goes inactive
         }
       #else
         u8g_SetPILevel(u8g, U8G_PI_CS, !arg_val);
@@ -204,5 +204,4 @@ uint8_t u8g_com_HAL_LPC1768_sw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, 
 }
 
 #endif // HAS_GRAPHICAL_LCD
-
 #endif // TARGET_LPC1768
