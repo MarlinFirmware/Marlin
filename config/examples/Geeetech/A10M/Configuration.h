6b96eaa15bb97c62e5cca52d4fec0c9f083e836c
GUEST.it <3944655+guestisp@users.noreply.github.com>
2019-05-09 05:43:34 +0200

    Fix E-steps/mm for Geeetech A10M/A20M (#13954)

--- config/examples/Geeetech/A10M/Configuration.h
+++ config/examples/Geeetech/A10M/Configuration.h
@@ -682,7 +682,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.8, 400, 340 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.8, 400, 430 }
 
 /**
  * Default Max Feed Rate (mm/s)
