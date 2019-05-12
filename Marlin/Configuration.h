f9788f6ac63db0d8408b5e4d33670cff7179abe0
Alessandro Corbelli <alessandro.corbelli@guest.it>
2019-05-12 14:53:05 +0200

    Calibrazione E-step/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -687,7 +687,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 424 }
 
 /**
  * Default Max Feed Rate (mm/s)
