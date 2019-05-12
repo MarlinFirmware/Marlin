ffac108b47315e09f8865e2982058bfe4177f2b1
Alessandro Corbelli <alessandro.corbelli@guest.it>
2019-05-12 21:48:19 +0200

    Attivato babystepping

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1078,7 +1078,7 @@
  *
  * Warning: Does not respect endstops!
  */
-//#define BABYSTEPPING
+#define BABYSTEPPING
 #if ENABLED(BABYSTEPPING)
   #define BABYSTEP_WITHOUT_HOMING
   #define BABYSTEP_XY                     // Also enable X/Y Babystepping. Not supported on DELTA!
