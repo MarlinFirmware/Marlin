65f5b1410aa939cd3ba0b56e3c564e17d56d3a3e
FanDjango <FanDjango@users.noreply.github.com>
2019-05-15 17:18:55 +0200

    Another little tweak - pulse width report now truthful
    If the measured pulse width is 30ms, the it is 30ms OR MORE (maybe even a lot more)

--- Marlin/src/gcode/config/M43.cpp
+++ Marlin/src/gcode/config/M43.cpp
@@ -211,8 +211,11 @@ inline void servo_probe_test() {
       if (deploy_state != READ(PROBE_TEST_PIN)) {               // probe triggered
         for (probe_counter = 0; probe_counter < 15 && deploy_state != READ(PROBE_TEST_PIN); ++probe_counter) safe_delay(2);
 
-        SERIAL_ECHOLNPAIR(". Pulse width (+/- 4mS): ", probe_counter * 2);
-
+        if (probe_counter = 15)
+          SERIAL_ECHOLNPGM(". Pulse width: 30ms or more");
+        else 
+          SERIAL_ECHOLNPAIR(". Pulse width (+/- 4ms): ", probe_counter * 2);
+          
         if (probe_counter >= 4) {
           if (probe_counter == 15) {
             if (blt) SERIAL_ECHOPGM("= BLTouch V3.1");
