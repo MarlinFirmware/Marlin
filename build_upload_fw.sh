2705a5d21cb77983fd67167b4424587b266382d2
Prod Test <prodtest@ferroamp.sehttps://appsrv.ferroamp.se/gitlab/>
2019-05-22 18:58:25 +0200

    Some changes to upload

--- build_upload_fw.sh
+++ build_upload_fw.sh
@@ -1,5 +1,5 @@
 ssh  pi@192.168.1.132 -t '''sudo mount $(ls /dev/sd*1) /mnt/sdcard -o umask=000'''
 pio run --disable-auto-clean
-scp .pioenvs/LPC1768/firmware.bin pi@192.168.1.132:~/
-ssh  pi@192.168.1.132 -t "cp ~/firmware.bin /mnt/sdcard; stty speed 115200 < /dev/ttyACM0 ; stty speed 115200 < /dev/ttyACM0 ; sleep 2; echo -ne 'M997\n' > /dev/ttyACM0"
+scp .pioenvs/LPC1768/firmware.bin pi@192.168.1.132:/mnt/sdcard
+ssh  pi@192.168.1.132 -t "stty speed 115200 < /dev/ttyACM0 ; stty speed 115200 < /dev/ttyACM0 ; sleep 2; echo -ne 'M997\n' > /dev/ttyACM0"
 
