914cff21a870c8a3565a7f94f08b4938e67ad522
mikeshub <mikesbaker@gmail.com>
2019-05-25 17:20:36 -0500

    Put the LED init call in the right place

--- Marlin/src/feature/leds/leds.cpp
+++ Marlin/src/feature/leds/leds.cpp
@@ -71,11 +71,11 @@ void LEDLights::setup() {
   #if ENABLED(NEOPIXEL_LED)
     setup_neopixel();
   #endif
-  #if ENABLED(LED_USER_PRESET_STARTUP)
-    set_default();
-  #endif
   #if ENABLED(PCA9533)
     RGBinit();
+  #endif
+  #if ENABLED(LED_USER_PRESET_STARTUP)
+    set_default();
   #endif
 }
 
