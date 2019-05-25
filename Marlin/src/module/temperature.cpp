48d1bd29cb51da730b02cec7313889b859bb1ba2
Scott Lahteine <github@thinkyhead.com>
2019-05-25 15:19:47 -0500

    Shift m before compare to 0

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -1191,9 +1191,8 @@ void Temperature::manage_heater() {
 #define SCAN_THERMISTOR_TABLE(TBL,LEN) do{                             \
   uint8_t l = 0, r = LEN, m;                                           \
   for (;;) {                                                           \
-    m = l + r;                                                         \
+    m = (l + r) >> 1;                                                  \
     if (!m) return short(pgm_read_word(&TBL[0][1]));                   \
-    m >>= 1;                                                           \
     if (m == l || m == r) return short(pgm_read_word(&TBL[LEN-1][1])); \
     short v00 = pgm_read_word(&TBL[m-1][0]),                           \
           v10 = pgm_read_word(&TBL[m-0][0]);                           \
