772aa3bd77e111d5c10d42c392d2d36384a5f2f5
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-05-25 23:52:18 -0500

    Update Configuration.h

--- config/examples/FlashForge/CreatorPro/Configuration.h
+++ config/examples/FlashForge/CreatorPro/Configuration.h
@@ -547,8 +547,8 @@
  * details can be tuned in Configuration_adv.h
  */
 
-// #define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
-// #define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
+#define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
+#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
 #define THERMAL_PROTECTION_CHAMBER // Enable thermal protection for the heated chamber
 
 //===========================================================================
