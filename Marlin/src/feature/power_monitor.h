06f58af10ab5b4c68d107b9e2430104b00f48779
doggyfan <49303635+doggyfan@users.noreply.github.com>
2019-06-02 00:17:11 +0100

    Tidy up

--- Marlin/src/feature/power_monitor.h
+++ Marlin/src/feature/power_monitor.h
@@ -26,7 +26,6 @@
 
 template <const float *ADC_SCALE>
 struct lpf_reading_t {
-  static constexpr float adc_scale = *ADC_SCALE;
   float value;
   uint32_t raw_value;
   uint16_t raw_value_filtered;
@@ -36,7 +35,7 @@ struct lpf_reading_t {
     raw_value += uint32_t(sample) << 7;     // Add new ADC reading, scaled by 128
     raw_value_filtered = raw_value >> 10;   // Divide to get to 0-16383 range since we used 1/128 IIR filter approach
   }
-  void capture() { value = raw_value_filtered * adc_scale; }
+  void capture() { value = raw_value_filtered * *ADC_SCALE; }
 };
 
 class PowerMonitor {
@@ -44,11 +43,11 @@ private:
   static uint8_t flags;  // M430, M431 set and clear flags to read/display system current
 
   #if ENABLED(POWER_MONITOR_CURRENT)
-    static constexpr float amps_adc_scale = (float)(POWER_MONITOR_ADC_VREF / (POWER_MONITOR_VOLTS_PER_AMP * 16384));
+    static constexpr float amps_adc_scale = (float)POWER_MONITOR_ADC_VREF / (POWER_MONITOR_VOLTS_PER_AMP * 16384);
     static lpf_reading_t<&amps_adc_scale> amps;
   #endif
   #if ENABLED(POWER_MONITOR_VOLTAGE)
-    static constexpr float volts_adc_scale = (float)(POWER_MONITOR_ADC_VREF / (POWER_MONITOR_VOLTS_PER_VOLT * 16384));
+    static constexpr float volts_adc_scale = (float)POWER_MONITOR_ADC_VREF / (POWER_MONITOR_VOLTS_PER_VOLT * 16384);
     static lpf_reading_t<&volts_adc_scale> volts;
   #endif
 
@@ -59,14 +58,14 @@ public:
 
   PowerMonitor() { reset(); }
 
-  FORCE_INLINE static bool display_enabled() { return (flags & 0x03) != 0x00; }
+  FORCE_INLINE static bool display_enabled() { return (flags & 0x03); }
 
   #if ENABLED(POWER_MONITOR_CURRENT)
     FORCE_INLINE static float getAmps() { return amps.value; }
     FORCE_INLINE static bool current_display_enabled() { return TEST(flags, 0); }
     FORCE_INLINE static void set_current_display_enabled(const bool b) { SET_BIT_TO(flags, 0, b); }
     FORCE_INLINE static void toggle_current_display_enabled() { flags ^= 0x01; }
-    static void add_current_sample(const uint16_t value) { amps.add_sample(value); }
+    void add_current_sample(const uint16_t value) { amps.add_sample(value); }
   #endif
 
   #if ENABLED(POWER_MONITOR_VOLTAGE)
@@ -74,7 +73,7 @@ public:
     FORCE_INLINE static bool voltage_display_enabled() { return TEST(flags, 1); }
     FORCE_INLINE static void set_voltage_display_enabled(const bool b) { SET_BIT_TO(flags, 1, b); }
     FORCE_INLINE static void toggle_voltage_display_enabled() { flags ^= 0x02; }
-    static void add_voltage_sample(const uint16_t value) { volts.add_sample(value); }
+    void add_voltage_sample(const uint16_t value) { volts.add_sample(value); }
   #endif
 
   #if BOTH(POWER_MONITOR_CURRENT, POWER_MONITOR_VOLTAGE)
@@ -82,7 +81,6 @@ public:
   #endif
 
   static void reset() {
-
     flags = 0x00;
 
     #if ENABLED(POWER_MONITOR_CURRENT)
@@ -96,7 +94,7 @@ public:
 
   static void capture_values() {
     #if ENABLED(POWER_MONITOR_CURRENT)
-      amps.capture();
+     amps.capture();
     #endif
     #if ENABLED(POWER_MONITOR_VOLTAGE)
       volts.capture();
