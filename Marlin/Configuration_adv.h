a655cb7559faa56d8c55f9a7fa3d87fd5272d3da
doggyfan <49303635+doggyfan@users.noreply.github.com>
2019-06-02 18:59:03 +0100

    Update config file voltage value and comment

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2086,7 +2086,7 @@
   #define POWER_MONITOR_VOLTS_PER_AMP   0.05000   // Input voltage to the MCU analog pin per amp  - DO NOT exceed the ADC reference voltage!
   #define POWER_MONITOR_VOLTS_PER_VOLT  0.11786   // Input voltage to the MCU analog pin per volt - DO NOT exceed the ADC reference voltage!
 
-  #define POWER_MONITOR_FIXED_VOLTAGE   240       // Used as the voltage measurement if you use a current sensor and not voltage sessnor and want power display
+  #define POWER_MONITOR_FIXED_VOLTAGE   13.6      // used as the voltage if you use a current sensor with no voltage sensor and want power display
 
   // ADC reference voltage appropriate to the board. Suggested values:
   //   3.0 : Possible ADC reference voltage on 32-bit MCUs
