MarlinFirmware/Marlin

commit 123e58d9c3c8cf85236d8c0d6f74427b0f4196a1

Heated Chamber for Graphical LCD (#14190)

Marlin/src/lcd/dogm/dogm_Statusscreen.h
@@ -659,6 +659,63 @@
 
 #endif // !STATUS_BED_WIDTH && !STATUS_COMBINE_HEATERS && HAS_HEATED_BED && HOTENDS < 4
 
+#if HAS_HEATED_CHAMBER
+
+  #define STATUS_CHAMBER_WIDTH 16
+
+  #ifdef STATUS_CHAMBER_ANIM
+
+    const unsigned char status_chamber_bmp[] PROGMEM = {
+      B11111111,B11111111,
+      B10000000,B00000001,
+      B10000000,B00000001,
+      B10000000,B00000001,
+      B10000000,B00000001,
+      B10000000,B00000001,
+      B10000000,B00000001,
+      B10000000,B00000001,
+      B10000000,B00000001,
+      B10000000,B00000001,
+      B11111111,B11111111,
+      B11111111,B11111111
+    };
+
+    const unsigned char status_chamber_on_bmp[] PROGMEM = {
+      B11111111,B11111111,
+      B10000000,B00000001,
+      B10000100,B00100001,
+      B10000010,B00010001,
+      B10000010,B00010001,
+      B10000100,B00100001,
+      B10001000,B01000001,
+      B10001000,B01000001,
+      B10000100,B00100001,
+      B10000000,B00000001,
+      B11111111,B11111111,
+      B11111111,B11111111
+    };
+
+  #else
+
+    const unsigned char status_chamber_bmp[] PROGMEM = {
+      B11111111,B11111111,
+      B10000000,B00000001,
+      B10000100,B00100001,
+      B10000010,B00010001,
+      B10000010,B00010001,
+      B10000100,B00100001,
+      B10001000,B01000001,
+      B10001000,B01000001,
+      B10000100,B00100001,
+      B10000000,B00000001,
+      B11111111,B11111111,
+      B11111111,B11111111
+    };
+
+  #endif
+
+#endif // HAS_HEATED_CHAMBER
+
 // Can also be overridden in Configuration.h
 // If you can afford it, try the 3-frame fan animation!
 // Don't compile in the fan animation with no fan
@@ -1303,6 +1360,50 @@
 #endif
 
 //
+// Chamber Bitmap Properties
+//
+#ifndef STATUS_CHAMBER_WIDTH
+  #define STATUS_CHAMBER_WIDTH 0
+#endif
+#ifndef STATUS_CHAMBER_BYTEWIDTH
+  #define STATUS_CHAMBER_BYTEWIDTH BW(STATUS_CHAMBER_WIDTH)
+#endif
+#if STATUS_CHAMBER_WIDTH && !STATUS_HEATERS_WIDTH
+
+  #ifndef STATUS_CHAMBER_X
+    #define STATUS_CHAMBER_X (128 - (STATUS_FAN_BYTEWIDTH + STATUS_CHAMBER_BYTEWIDTH) * 8)
+  #endif
+
+  #ifndef STATUS_CHAMBER_HEIGHT
+    #ifdef STATUS_CHAMBER_ANIM
+      #define STATUS_CHAMBER_HEIGHT(S) ((S) ? sizeof(status_chamber_on_bmp) / (STATUS_CHAMBER_BYTEWIDTH) : sizeof(status_chamber_bmp) / (STATUS_CHAMBER_BYTEWIDTH))
+    #else
+      #define STATUS_CHAMBER_HEIGHT(S) (sizeof(status_chamber_bmp) / (STATUS_CHAMBER_BYTEWIDTH))
+    #endif
+  #endif
+
+  #ifndef STATUS_CHAMBER_Y
+    #define STATUS_CHAMBER_Y(S) (20 - STATUS_CHAMBER_HEIGHT(S))
+  #endif
+
+  #ifndef STATUS_CHAMBER_TEXT_X
+    #define STATUS_CHAMBER_TEXT_X (STATUS_CHAMBER_X + 7)
+  #endif
+
+  static_assert(
+    sizeof(status_chamber_bmp) == (STATUS_CHAMBER_BYTEWIDTH) * (STATUS_CHAMBER_HEIGHT(0)),
+    "Status chamber bitmap (status_chamber_bmp) dimensions don't match data."
+  );
+  #ifdef STATUS_CHAMBER_ANIM
+    static_assert(
+      sizeof(status_chamber_on_bmp) == (STATUS_CHAMBER_BYTEWIDTH) * (STATUS_CHAMBER_HEIGHT(1)),
+      "Status chamber bitmap (status_chamber_on_bmp) dimensions don't match data."
+    );
+  #endif
+
+#endif
+
+//
 // Bed Bitmap Properties
 //
 #ifndef STATUS_BED_WIDTH
@@ -1314,7 +1415,7 @@
 #if STATUS_BED_WIDTH && !STATUS_HEATERS_WIDTH
 
   #ifndef STATUS_BED_X
-    #define STATUS_BED_X (128 - (STATUS_FAN_BYTEWIDTH + STATUS_BED_BYTEWIDTH) * 8)
+    #define STATUS_BED_X (128 - (STATUS_CHAMBER_BYTEWIDTH + STATUS_FAN_BYTEWIDTH + STATUS_BED_BYTEWIDTH) * 8)
   #endif
 
   #ifndef STATUS_BED_HEIGHT

Marlin/src/lcd/dogm/status_screen_DOGM.cpp
@@ -74,6 +74,7 @@ FORCE_INLINE void _draw_centered_temp(const int16_t temp, const uint8_t tx, cons
 #define DO_DRAW_FAN (HAS_FAN0 && STATUS_FAN_WIDTH && STATUS_FAN_FRAMES)
 #define ANIM_HOTEND (HOTENDS && ENABLED(STATUS_HOTEND_ANIM))
 #define ANIM_BED (DO_DRAW_BED && ENABLED(STATUS_BED_ANIM))
+#define ANIM_CHAMBER (HAS_HEATED_CHAMBER && ENABLED(STATUS_CHAMBER_ANIM))
 
 #if ANIM_HOTEND || ANIM_BED
   uint8_t heat_bits;
@@ -87,6 +88,11 @@ FORCE_INLINE void _draw_centered_temp(const int16_t temp, const uint8_t tx, cons
   #define BED_ALT() TEST(heat_bits, 7)
 #else
   #define BED_ALT() false
+#endif
+#if ANIM_CHAMBER
+  #define CHAMBER_ALT() TEST(heat_bits, 6)
+#else
+  #define CHAMBER_ALT() false
 #endif
 
 #define MAX_HOTEND_DRAW MIN(HOTENDS, ((LCD_PIXEL_WIDTH - (STATUS_LOGO_BYTEWIDTH + STATUS_FAN_BYTEWIDTH) * 8) / (STATUS_HEATERS_XSPACE)))
@@ -124,6 +130,27 @@ FORCE_INLINE void _draw_heater_status(const int8_t heater, const bool blink) {
   #else
     const float temp = IFBED(thermalManager.degBed(), thermalManager.degHotend(heater)),
                 target = IFBED(thermalManager.degTargetBed(), thermalManager.degTargetHotend(heater));
+  #endif
+
+  #if HAS_HEATED_CHAMBER
+    FORCE_INLINE void _draw_chamber_status(const bool blink) {
+      const float temp = thermalManager.degChamber(),
+                  target = thermalManager.degTargetChamber();
+      #if !HEATER_IDLE_HANDLER
+        UNUSED(blink);
+      #endif
+      if (PAGE_UNDER(7)) {
+        #if HEATER_IDLE_HANDLER
+          const bool is_idle = false, // thermalManager.chamber_idle.timed_out,
+                     dodraw = (blink || !is_idle);
+        #else
+          constexpr bool dodraw = true;
+        #endif
+        if (dodraw) _draw_centered_temp(target + 0.5, STATUS_CHAMBER_TEXT_X, 7);
+      }
+      if (PAGE_CONTAINS(28 - INFO_FONT_ASCENT, 28 - 1))
+        _draw_centered_temp(temp + 0.5f, STATUS_CHAMBER_TEXT_X, 28);
+    }
   #endif
 
   #if DISABLED(STATUS_HOTEND_ANIM)
@@ -271,13 +298,16 @@ void MarlinUI::draw_status_screen() {
 
   // At the first page, generate new display values
   if (first_page) {
-    #if ANIM_HOTEND || ANIM_BED
+    #if ANIM_HOTEND || ANIM_BED || ANIM_CHAMBER
       uint8_t new_bits = 0;
       #if ANIM_HOTEND
         HOTEND_LOOP() if (thermalManager.isHeatingHotend(e) ^ SHOW_ON_STATE) SBI(new_bits, e);
       #endif
       #if ANIM_BED
         if (thermalManager.isHeatingBed() ^ SHOW_ON_STATE) SBI(new_bits, 7);
+      #endif
+      #if ANIM_CHAMBER
+        if (thermalManager.isHeatingChamber() ^ SHOW_ON_STATE) SBI(new_bits, 6);
       #endif
       heat_bits = new_bits;
     #endif
@@ -326,6 +356,20 @@ void MarlinUI::draw_status_screen() {
       u8g.drawBitmapP(STATUS_BED_X, bedy, STATUS_BED_BYTEWIDTH, bedh, BED_BITMAP(BED_ALT()));
   #endif
 
+  #if DO_DRAW_CHAMBER
+    #if ANIM_HAMBER
+      #define CHAMBER_BITMAP(S) ((S) ? status_chamber_on_bmp : status_chamber_bmp)
+    #else
+      #define CHAMBER_BITMAP(S) status_chamber_bmp
+    #endif
+    if (PAGE_CONTAINS(STATUS_CHAMBER_Y, STATUS_CHAMBER_Y + STATUS_CHAMBER_HEIGHT - 1))
+      u8g.drawBitmapP(
+        STATUS_CHAMBER_X, STATUS_CHAMBER_Y,
+        STATUS_CHAMBER_BYTEWIDTH, STATUS_CHAMBER_HEIGHT,
+        CHAMBER_BITMAP(CHAMBER_ALT())
+      );
+  #endif
+
   #if DO_DRAW_FAN
     #if STATUS_FAN_FRAMES > 2
       static bool old_blink;
@@ -364,6 +408,10 @@ void MarlinUI::draw_status_screen() {
     // Heated bed
     #if HAS_HEATED_BED && HOTENDS < 4
       _draw_heater_status(-1, blink);
+    #endif
+
+    #if HAS_HEATED_CHAMBER
+      _draw_chamber_status(blink);
     #endif
 
     // Fan, if a bitmap was provided

commit 3cd8c4e14a2037b5cdc34dc28caf94a1a3563b3f

Display chamber temperature
This fix some compile errors introduced by #14190, and also give the possibility to display chamber temperature when chamber is not heated.
Still to decide icon X position

Marlin/src/lcd/dogm/dogm_Statusscreen.h
@@ -659,11 +659,11 @@
 
 #endif // !STATUS_BED_WIDTH && !STATUS_COMBINE_HEATERS && HAS_HEATED_BED && HOTENDS < 4
 
-#if HAS_HEATED_CHAMBER
+#if HAS_TEMP_CHAMBER
 
   #define STATUS_CHAMBER_WIDTH 16
 
-  #ifdef STATUS_CHAMBER_ANIM
+  #if defined(STATUS_CHAMBER_ANIM) || !HAS_HEATED_CHAMBER
 
     const unsigned char status_chamber_bmp[] PROGMEM = {
       B11111111,B11111111,
@@ -680,20 +680,22 @@
       B11111111,B11111111
     };
 
-    const unsigned char status_chamber_on_bmp[] PROGMEM = {
-      B11111111,B11111111,
-      B10000000,B00000001,
-      B10000100,B00100001,
-      B10000010,B00010001,
-      B10000010,B00010001,
-      B10000100,B00100001,
-      B10001000,B01000001,
-      B10001000,B01000001,
-      B10000100,B00100001,
-      B10000000,B00000001,
-      B11111111,B11111111,
-      B11111111,B11111111
-    };
+    #if HAS_HEATED_CHAMBER
+      const unsigned char status_chamber_on_bmp[] PROGMEM = {
+        B11111111,B11111111,
+        B10000000,B00000001,
+        B10000100,B00100001,
+        B10000010,B00010001,
+        B10000010,B00010001,
+        B10000100,B00100001,
+        B10001000,B01000001,
+        B10001000,B01000001,
+        B10000100,B00100001,
+        B10000000,B00000001,
+        B11111111,B11111111,
+        B11111111,B11111111
+      };
+    #endif
 
   #else
 
@@ -1375,15 +1377,11 @@
   #endif
 
   #ifndef STATUS_CHAMBER_HEIGHT
-    #ifdef STATUS_CHAMBER_ANIM
-      #define STATUS_CHAMBER_HEIGHT(S) ((S) ? sizeof(status_chamber_on_bmp) / (STATUS_CHAMBER_BYTEWIDTH) : sizeof(status_chamber_bmp) / (STATUS_CHAMBER_BYTEWIDTH))
-    #else
-      #define STATUS_CHAMBER_HEIGHT(S) (sizeof(status_chamber_bmp) / (STATUS_CHAMBER_BYTEWIDTH))
-    #endif
+    #define STATUS_CHAMBER_HEIGHT (sizeof(status_chamber_bmp) / (STATUS_CHAMBER_BYTEWIDTH))
   #endif
 
   #ifndef STATUS_CHAMBER_Y
-    #define STATUS_CHAMBER_Y(S) (20 - STATUS_CHAMBER_HEIGHT(S))
+    #define STATUS_CHAMBER_Y (20 - STATUS_CHAMBER_HEIGHT)
   #endif
 
   #ifndef STATUS_CHAMBER_TEXT_X
@@ -1391,12 +1389,12 @@
   #endif
 
   static_assert(
-    sizeof(status_chamber_bmp) == (STATUS_CHAMBER_BYTEWIDTH) * (STATUS_CHAMBER_HEIGHT(0)),
+    sizeof(status_chamber_bmp) == (STATUS_CHAMBER_BYTEWIDTH) * (STATUS_CHAMBER_HEIGHT),
     "Status chamber bitmap (status_chamber_bmp) dimensions don't match data."
   );
-  #ifdef STATUS_CHAMBER_ANIM
+  #if HAS_HEATED_CHAMBER && defined(STATUS_CHAMBER_ANIM)
     static_assert(
-      sizeof(status_chamber_on_bmp) == (STATUS_CHAMBER_BYTEWIDTH) * (STATUS_CHAMBER_HEIGHT(1)),
+      sizeof(status_chamber_on_bmp) == (STATUS_CHAMBER_BYTEWIDTH) * (STATUS_CHAMBER_HEIGHT),
       "Status chamber bitmap (status_chamber_on_bmp) dimensions don't match data."
     );
   #endif

Marlin/src/lcd/dogm/status_screen_DOGM.cpp
@@ -72,6 +72,7 @@ FORCE_INLINE void _draw_centered_temp(const int16_t temp, const uint8_t tx, cons
 
 #define DO_DRAW_BED (HAS_HEATED_BED && STATUS_BED_WIDTH && HOTENDS <= 3 && DISABLED(STATUS_COMBINE_HEATERS))
 #define DO_DRAW_FAN (HAS_FAN0 && STATUS_FAN_WIDTH && STATUS_FAN_FRAMES)
+//#define DO_DRAW_CHAMBER (HAS_TEMP_CHAMBER && ((HOTENDS <= 2 && DO_DRAW_BED) || (!DO_DRAW_BED && HOTENDS <= 3)))
 #define ANIM_HOTEND (HOTENDS && ENABLED(STATUS_HOTEND_ANIM))
 #define ANIM_BED (DO_DRAW_BED && ENABLED(STATUS_BED_ANIM))
 #define ANIM_CHAMBER (HAS_HEATED_CHAMBER && ENABLED(STATUS_CHAMBER_ANIM))
@@ -130,27 +131,6 @@ FORCE_INLINE void _draw_heater_status(const int8_t heater, const bool blink) {
   #else
     const float temp = IFBED(thermalManager.degBed(), thermalManager.degHotend(heater)),
                 target = IFBED(thermalManager.degTargetBed(), thermalManager.degTargetHotend(heater));
-  #endif
-
-  #if HAS_HEATED_CHAMBER
-    FORCE_INLINE void _draw_chamber_status(const bool blink) {
-      const float temp = thermalManager.degChamber(),
-                  target = thermalManager.degTargetChamber();
-      #if !HEATER_IDLE_HANDLER
-        UNUSED(blink);
-      #endif
-      if (PAGE_UNDER(7)) {
-        #if HEATER_IDLE_HANDLER
-          const bool is_idle = false, // thermalManager.chamber_idle.timed_out,
-                     dodraw = (blink || !is_idle);
-        #else
-          constexpr bool dodraw = true;
-        #endif
-        if (dodraw) _draw_centered_temp(target + 0.5, STATUS_CHAMBER_TEXT_X, 7);
-      }
-      if (PAGE_CONTAINS(28 - INFO_FONT_ASCENT, 28 - 1))
-        _draw_centered_temp(temp + 0.5f, STATUS_CHAMBER_TEXT_X, 28);
-    }
   #endif
 
   #if DISABLED(STATUS_HOTEND_ANIM)
@@ -253,6 +233,31 @@ FORCE_INLINE void _draw_heater_status(const int8_t heater, const bool blink) {
   }
 
 }
+
+#if DO_DRAW_CHAMBER
+  FORCE_INLINE void _draw_chamber_status(const bool blink) {
+    const float temp = thermalManager.degChamber();
+
+    #if !HEATER_IDLE_HANDLER
+      UNUSED(blink);
+    #endif
+    #if HAS_HEATED_CHAMBER
+      const float target = thermalManager.degTargetChamber();
+
+      if (PAGE_UNDER(7)) {
+        #if HEATER_IDLE_HANDLER
+          const bool is_idle = false, // thermalManager.chamber_idle.timed_out,
+          dodraw = (blink || !is_idle);
+        #else
+          constexpr bool dodraw = true;
+        #endif
+        if (dodraw) _draw_centered_temp(target + 0.5, STATUS_CHAMBER_TEXT_X, 7);
+      }
+    #endif
+    if (PAGE_CONTAINS(28 - INFO_FONT_ASCENT, 28 - 1))
+      _draw_centered_temp(temp + 0.5f, STATUS_CHAMBER_TEXT_X, 28);
+  }
+#endif
 
 //
 // Before homing, blink '123' <-> '???'.
@@ -357,7 +362,7 @@ void MarlinUI::draw_status_screen() {
   #endif
 
   #if DO_DRAW_CHAMBER
-    #if ANIM_HAMBER
+    #if HAS_HEATED_CHAMBER && defined(STATUS_CHAMBER_ANIM)
       #define CHAMBER_BITMAP(S) ((S) ? status_chamber_on_bmp : status_chamber_bmp)
     #else
       #define CHAMBER_BITMAP(S) status_chamber_bmp
@@ -406,11 +411,11 @@ void MarlinUI::draw_status_screen() {
       _draw_heater_status(e, blink);
 
     // Heated bed
-    #if HAS_HEATED_BED && HOTENDS < 4
+    #if DO_DRAW_BED
       _draw_heater_status(-1, blink);
     #endif
 
-    #if HAS_HEATED_CHAMBER
+    #if DO_DRAW_CHAMBER
       _draw_chamber_status(blink);
     #endif
 