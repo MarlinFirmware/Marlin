858b2fa21ad40cfc4ed2e730c4fee060b2e7523b
Scott Lahteine <github@thinkyhead.com>
2019-06-05 21:19:22 -0500

    Fix thermistor table

--- Marlin/src/module/thermistor/thermistor_18.h
+++ Marlin/src/module/thermistor/thermistor_18.h
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (C) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
@@ -19,40 +19,40 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
- 
-// 18 is ATC Semitec 204GT-2 (4.7k pullup) Dagoma.Fr - MKS_Base_DKU001327 - version (measured/tested/approved)
+
+// ATC Semitec 204GT-2 (4.7k pullup) Dagoma.Fr - MKS_Base_DKU001327 - version (measured/tested/approved)
 const short temptable_18[][2] PROGMEM = {
-  {1*OVERSAMPLENR,713},
-  {17*OVERSAMPLENR,284},
-  {20*OVERSAMPLENR,275},
-  {23*OVERSAMPLENR,267},
-  {27*OVERSAMPLENR,257},
-  {31*OVERSAMPLENR,250},
-  {37*OVERSAMPLENR,240},
-  {43*OVERSAMPLENR,232},
-  {51*OVERSAMPLENR,222},
-  {61*OVERSAMPLENR,213},
-  {73*OVERSAMPLENR,204},
-  {87*OVERSAMPLENR,195},
-  {106*OVERSAMPLENR,185},
-  {128*OVERSAMPLENR,175},
-  {155*OVERSAMPLENR,166},
-  {189*OVERSAMPLENR,156},
-  {230*OVERSAMPLENR,146},
-  {278*OVERSAMPLENR,137},
-  {336*OVERSAMPLENR,127},
-  {402*OVERSAMPLENR,117},
-  {476*OVERSAMPLENR,107},
-  {554*OVERSAMPLENR,97},
-  {635*OVERSAMPLENR,87},
-  {713*OVERSAMPLENR,78},
-  {784*OVERSAMPLENR,68},
-  {846*OVERSAMPLENR,58},
-  {897*OVERSAMPLENR,49},
-  {937*OVERSAMPLENR,39},
-  {966*OVERSAMPLENR,30},
-  {986*OVERSAMPLENR,20},
-  {1000*OVERSAMPLENR,10},
-  {1010*OVERSAMPLENR,0},
-  {1024*OVERSAMPLENR,-273} // for safety
+  { OV(   1), 713 },
+  { OV(  17), 284 },
+  { OV(  20), 275 },
+  { OV(  23), 267 },
+  { OV(  27), 257 },
+  { OV(  31), 250 },
+  { OV(  37), 240 },
+  { OV(  43), 232 },
+  { OV(  51), 222 },
+  { OV(  61), 213 },
+  { OV(  73), 204 },
+  { OV(  87), 195 },
+  { OV( 106), 185 },
+  { OV( 128), 175 },
+  { OV( 155), 166 },
+  { OV( 189), 156 },
+  { OV( 230), 146 },
+  { OV( 278), 137 },
+  { OV( 336), 127 },
+  { OV( 402), 117 },
+  { OV( 476), 107 },
+  { OV( 554),  97 },
+  { OV( 635),  87 },
+  { OV( 713),  78 },
+  { OV( 784),  68 },
+  { OV( 846),  58 },
+  { OV( 897),  49 },
+  { OV( 937),  39 },
+  { OV( 966),  30 },
+  { OV( 986),  20 },
+  { OV(1000),  10 },
+  { OV(1010),   0 },
+  { OV(1024),-273 } // for safety
 };
