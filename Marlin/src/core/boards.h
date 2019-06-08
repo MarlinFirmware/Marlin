e0adc4394ccf04eb9823be9aec313c639ce43996
pinchies <pinchies>
2019-06-09 03:41:56 +1000

    Hopefully a merge-friendly build for JGAurora A5S and A1.
    Will NOT compile and work correctly unless (see next commit) changes are made to HAL timers and watchdog files.
    Does NOT include awesome work on touch screen function.
    Does have LCD and sd-eeprom working.

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -245,6 +245,7 @@
 #define BOARD_GTM32_PRO_VB     1805   // STM32f103VET6 controller
 #define BOARD_MORPHEUS         1806   // STM32F103C8/STM32F103CB Libmaple based stm32f1 controller
 #define BOARD_MKS_ROBIN        1808   // MKS Robin / STM32F103ZET6
+#define BOARD_JGAURORA_A5S_A1  1810   // JGAurora A5S A1 / STM32F103ZET6
 
 //
 // STM32 ARM Cortex-M4F
