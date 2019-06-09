012bd03db9936c740c1c814491e0b184acb919bc
InsanityAutomation <d.menzel@insanityautomation.com>
2019-06-09 11:01:08 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -962,13 +962,13 @@
 #if DISABLED(TREX3) || ENABLED(TREX3_UPGRADE)
     #define X_MIN_POS -42
     #define Y_MIN_POS 0
-    #define Z_MIN_POS -3
+    #define Z_MIN_POS 0
     #define X_MAX_POS 450
     #define Y_MAX_POS Y_BED_SIZE
 #else
     #define X_MIN_POS -47
     #define Y_MIN_POS 0
-    #define Z_MIN_POS -3
+    #define Z_MIN_POS 0
     #define X_MAX_POS 460
     #define Y_MAX_POS Y_BED_SIZE
 #endif
@@ -1223,7 +1223,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 //#define MANUAL_X_HOME_POS 0
 //#define MANUAL_Y_HOME_POS 0
-//#define MANUAL_Z_HOME_POS 0
+#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
