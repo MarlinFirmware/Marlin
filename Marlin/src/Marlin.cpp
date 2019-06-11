6affe71f8b26bcfd0807bb859ccb6f21877ec548
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-06-10 23:56:52 -0500

    Update Marlin.cpp

--- Marlin/src/Marlin.cpp
+++ Marlin/src/Marlin.cpp
@@ -1131,7 +1131,9 @@ void loop() {
   for (;;) {
 
     #if ENABLED(SDSUPPORT)
+
       card.checkautostart();
+
       if (card.flag.abort_sd_printing) {
         card.stopSDPrint(
           #if SD_RESORT
@@ -1153,6 +1155,7 @@ void loop() {
           enqueue_and_echo_commands_P(PSTR(EVENT_GCODE_SD_STOP));
         #endif
       }
+
     #endif // SDSUPPORT
 
     if (commands_in_queue < BUFSIZE) get_available_commands();
