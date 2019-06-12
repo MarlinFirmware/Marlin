eee4a26f9c895ae79650e7a355d40edb6a5be1b5
thisiskeithb <thisiskeithb@outlook.com>
2019-06-11 20:58:56 -0700

    Updated Junction Deviation MM Value
    Updated JUNCTION_DEVIATION_MM based on Jerk values of 8 and Accelleration values of 2000: (0.4*8.0^2/2000=0.0128)

--- config/examples/EVNOVO (Artillery)/Sidewinder X1/Configuration.h
+++ config/examples/EVNOVO (Artillery)/Sidewinder X1/Configuration.h
@@ -739,7 +739,7 @@
 //
 //#define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
-  #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.01  // (mm) Distance from real junction edge
 #endif
 
 /**
