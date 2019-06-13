648a91bce886940b2233c9f210d84565c909d6af
Aliaksei Kvitsinski <aleksei.wm@gmail.com>
2019-06-13 11:21:18 +0300

    Support for Z-Bold board (#14024)

--- Marlin/src/pins/pins_EINSTART-S.h
+++ Marlin/src/pins/pins_EINSTART-S.h
@@ -42,28 +42,28 @@
 //
 // Limit Switches
 //
-#define X_STOP_PIN         44   // 2560 PIN 40
-#define Y_STOP_PIN         43   // 2560 PIN 41
-#define Z_STOP_PIN         42   // 2560 PIN 42
+#define X_STOP_PIN         44
+#define Y_STOP_PIN         43
+#define Z_STOP_PIN         42
 
 //
 // Steppers
 //
-#define X_STEP_PIN         76   // 2560 PIN 68
-#define X_DIR_PIN          75   // 2560 PIN 67
-#define X_ENABLE_PIN       73   // 2560 PIN 66
+#define X_STEP_PIN         76
+#define X_DIR_PIN          75
+#define X_ENABLE_PIN       73
 
-#define Y_STEP_PIN         31   // 2560 PIN 59
-#define Y_DIR_PIN          32   // 2560 PIN 58
-#define Y_ENABLE_PIN       72   // 2560 PIN 65
+#define Y_STEP_PIN         31
+#define Y_DIR_PIN          32
+#define Y_ENABLE_PIN       72
 
-#define Z_STEP_PIN         34   // 2560 PIN 56
-#define Z_DIR_PIN          35   // 2560 PIN 55
-#define Z_ENABLE_PIN       33   // 2560 PIN 57
+#define Z_STEP_PIN         34
+#define Z_DIR_PIN          35
+#define Z_ENABLE_PIN       33
 
-#define E0_STEP_PIN        36   // 2560 PIN 54
-#define E0_DIR_PIN         37   // 2560 PIN 53
-#define E0_ENABLE_PIN      30   // 2560 PIN 60
+#define E0_STEP_PIN        36
+#define E0_DIR_PIN         37
+#define E0_ENABLE_PIN      30
 
 //
 // Temperature Sensors
@@ -74,16 +74,16 @@
 //
 // Heaters / Fans
 //
-#define HEATER_0_PIN       83   // 2560 PIN 49
-#define HEATER_BED_PIN     38   // 2560 PIN 50
+#define HEATER_0_PIN       83
+#define HEATER_BED_PIN     38
 
-#define FAN_PIN            82   // 2560 PIN 48
+#define FAN_PIN            82
 
 //
 // Misc. Functions
 //
-#define SDSS               53   // 2560 PIN 19
-#define LED_PIN             4   // 2560 PIN 1
+#define SDSS               53
+#define LED_PIN             4
 
 //////////////////////////
 // LCDs and Controllers //
@@ -97,24 +97,24 @@
 // u8glib constructor
 // U8GLIB_SH1106_128X64 u8g(DOGLCD_SCK, DOGLCD_MOSI, DOGLCD_CS, LCD_PINS_DC, LCD_PINS_RS);
 
-#define LCD_PINS_DC        78   // 2560 PIN 4
-#define LCD_PINS_RS        79   // 2560 PIN 8
+#define LCD_PINS_DC        78
+#define LCD_PINS_RS        79
 // DOGM SPI LCD Support
-#define DOGLCD_CS           3   // 2560 PIN 7
-#define DOGLCD_MOSI         2   // 2560 PIN 6
-#define DOGLCD_SCK          5   // 2560 PIN 5
-#define DOGLCD_A0           2   // 2560 PIN 6
+#define DOGLCD_CS           3
+#define DOGLCD_MOSI         2
+#define DOGLCD_SCK          5
+#define DOGLCD_A0           2
 
 //
 // LCD Display input pins
 //
-#define BTN_UP             25   // 2560 PIN 75
-#define BTN_DWN            26   // 2560 PIN 74
-#define BTN_LFT            27   // 2560 PIN 73
-#define BTN_RT             28   // 2560 PIN 72
+#define BTN_UP             25
+#define BTN_DWN            26
+#define BTN_LFT            27
+#define BTN_RT             28
 
 // 'OK' button
-#define BTN_ENC            29   // 2560 PIN 71
+#define BTN_ENC            29
 
 // Set Kill to right arrow, same as RIGID_PANEL
-#define KILL_PIN           28   // 2560 PIN 72
+#define KILL_PIN           28
