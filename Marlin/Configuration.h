b9a4e3ea44bbcbdddecde7ec529cbda934517866
Joseph Smith <josephsm73@gmail.com>
2019-06-13 09:59:22 -0400

    enable SD Support

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1574,7 +1574,7 @@
  * you must uncomment the following option or it won't work.
  *
  */
-//#define SDSUPPORT
+#define SDSUPPORT
 
 /**
  * SD CARD: SPI SPEED
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -905,7 +905,7 @@
 
   //#define MENU_ADDAUTOSTART               // Add a menu option to run auto#.g files
 
-  #define EVENT_GCODE_SD_STOP "G28XY"       // G-code to run on Stop Print (e.g., "G28XY" or "G27")
+  //#define EVENT_GCODE_SD_STOP "G28XY"       // G-code to run on Stop Print (e.g., "G28XY" or "G27")
 
   /**
    * Continue after Power-Loss (Creality3D)
