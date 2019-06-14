320a614b7442faf28218a4791a87e63f93f22bec
InsanityAutomation <d.menzel@insanityautomation.com>
2019-06-14 13:59:42 -0400

    Rematch oddly changed config

--- config/examples/FlashForge/CreatorPro/Configuration.h
+++ config/examples/FlashForge/CreatorPro/Configuration.h
@@ -707,7 +707,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 5000, 5000, 100, 80000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 5000 }
 
 /**
  * Prevents M201 from writing values over these limits
