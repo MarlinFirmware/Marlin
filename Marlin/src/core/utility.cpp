c21704b066f8e57e94064c3380522818b99e8a6f
Jason Smith <jason.inet@gmail.com>
2019-06-20 17:15:29 -0700

    Include SOLENOID_PROBE in leveling report

--- Marlin/src/core/utility.cpp
+++ Marlin/src/core/utility.cpp
@@ -72,6 +72,8 @@ void safe_delay(millis_t ms) {
         "Z_PROBE_SLED"
       #elif ENABLED(Z_PROBE_ALLEN_KEY)
         "Z_PROBE_ALLEN_KEY"
+      #elif ENABLED(SOLENOID_PROBE)
+        "SOLENOID_PROBE"
       #else
         "NONE"
       #endif
