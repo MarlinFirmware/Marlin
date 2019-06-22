6001cf71061fd5689f7a2a7e16a5c67cd9dcf053
thisiskeithb <thisiskeithb@outlook.com>
2019-06-22 07:24:35 -0700

    MKS Robin Mini Board Support
    Initial MKS Robin Mini (STM32F103VET6) support brought over from Makerbase's Robin Mini repo.

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -247,6 +247,7 @@
 #define BOARD_GTM32_PRO_VB            1805  // STM32f103VET6 controller
 #define BOARD_MORPHEUS                1806  // STM32F103C8/STM32F103CB Libmaple based stm32f1 controller
 #define BOARD_MKS_ROBIN               1808  // MKS Robin / STM32F103ZET6
+#define BOARD_MKS_ROBIN_MINI          1813  // MKS Robin Mini / STM32F103VET6
 #define BOARD_BIGTREE_SKR_MINI_V1_1   1814  // STM32F103RC
 #define BOARD_JGAURORA_A5S_A1         1820  // JGAurora A5S A1 / STM32F103ZET6
 
