a9ac2d806d51b6a632c6975a1f3cc945fb1eb26a
GMagician <GMagician@users.noreply.github.com>
2019-07-03 22:42:56 +0200

    Unused SPI PIN definitions

--- Marlin/src/sd/usb_flashdrive/Sd2Card_FlashDrive.h
+++ Marlin/src/sd/usb_flashdrive/Sd2Card_FlashDrive.h
@@ -46,15 +46,8 @@
 #if DISABLED(SOFTWARE_SPI)
   // hardware pin defs
   #define SD_CHIP_SELECT_PIN  SS_PIN            // The default chip select pin for the SD card is SS.
-  // The following three pins must not be redefined for hardware SPI.
-  #define SPI_MOSI_PIN        MOSI_PIN          // SPI Master Out Slave In pin
-  #define SPI_MISO_PIN        MISO_PIN          // SPI Master In Slave Out pin
-  #define SPI_SCK_PIN         SCK_PIN           // SPI Clock pin
 #else  // SOFTWARE_SPI
   #define SD_CHIP_SELECT_PIN  SOFT_SPI_CS_PIN   // SPI chip select pin
-  #define SPI_MOSI_PIN        SOFT_SPI_MOSI_PIN // SPI Master Out Slave In pin
-  #define SPI_MISO_PIN        SOFT_SPI_MISO_PIN // SPI Master In Slave Out pin
-  #define SPI_SCK_PIN         SOFT_SPI_SCK_PIN  // SPI Clock pin
 #endif // SOFTWARE_SPI
 
 class Sd2Card {
