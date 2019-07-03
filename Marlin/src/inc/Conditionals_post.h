johnathantran/Marlin-Food-3D-Printer-Reconfig

Add E_AXIS_HOMING feature
See default configuration.h file. With #define E_AXIS_HOMING defined in
configuration.h, E-axis can now be controlled similar to XYZ axes:
- support for limit switch,
- support for G-code "G28": auto-home all axes (XYZE) and "G28 E"
  (auto-home E-axis)
- support for enabling software endstops for E-axis by adding
  "#define (MIN|MAX)_SOFTWARE_ENDSTOP_E" to configuration.h

Sanity checks are put into place. Can only be enabled for cartesian
4 axis robots with one limit switch (endstop) per axis. Cannot be
enabled for multiple carriages or multiple stepper-drivers per axis.
yet. Enabling E_AXIS_HOMING is incompatible with LINEAR_ADVANCE.

Marlin/src/inc/Conditionals_post.h
@@ -51,7 +51,9 @@
 #define X_MAX_LENGTH (X_MAX_POS - (X_MIN_POS))
 #define Y_MAX_LENGTH (Y_MAX_POS - (Y_MIN_POS))
 #define Z_MAX_LENGTH (Z_MAX_POS - (Z_MIN_POS))
-
+#if ENABLED(E_AXIS_HOMING)
+  #define E_MAX_LENGTH (E_MAX_POS - (E_MIN_POS))
+#endif
 // Defined only if the sanity-check is bypassed
 #ifndef X_BED_SIZE
   #define X_BED_SIZE X_MAX_LENGTH
@@ -74,7 +76,9 @@
   #define Y_CENTER ((Y_BED_SIZE) / 2)
 #endif
 #define Z_CENTER ((Z_MIN_POS + Z_MAX_POS) / 2)
-
+#if ENABLED(E_AXIS_HOMING)
+  #define E_CENTER ((E_MIN_POS + E_MAX_POS) / 2)
+#endif
 // Get the linear boundaries of the bed
 #define X_MIN_BED (X_CENTER - (X_BED_SIZE) / 2)
 #define X_MAX_BED (X_CENTER + (X_BED_SIZE) / 2)
@@ -177,6 +181,13 @@
   #define Z_HOME_POS (Z_HOME_DIR < 0 ? Z_MIN_POS : Z_MAX_POS)
 #endif
 
+#if ENABLED(E_AXIS_HOMING)
+  #ifdef MANUAL_E_HOME_POS
+    #define E_HOME_POS MANUAL_E_HOME_POS
+  #else
+    #define E_HOME_POS (E_HOME_DIR < 0 ? E_MIN_POS : E_MAX_POS)
+  #endif
+#endif
 /**
  * If DELTA_HEIGHT isn't defined use the old setting
  */
@@ -748,6 +759,9 @@
   #if ENABLED(USE_ZMIN_PLUG)
     #define ENDSTOPPULLUP_ZMIN
   #endif
+  #if ENABLED(USE_EMIN_PLUG)
+    #define ENDSTOPPULLUP_EMIN
+  #endif
 #endif
 
 /**
@@ -888,6 +902,8 @@
 #define HAS_Z3_MIN (PIN_EXISTS(Z3_MIN))
 #define HAS_Z3_MAX (PIN_EXISTS(Z3_MAX))
 #define HAS_Z_MIN_PROBE_PIN (PIN_EXISTS(Z_MIN_PROBE))
+#define HAS_E_MIN HAS_STOP_TEST(E,MIN)
+#define HAS_E_MAX HAS_STOP_TEST(E,MAX)
 
 // ADC Temp Sensors (Thermistor or Thermocouple with amplifier ADC interface)
 #define HAS_ADC_TEST(P) (PIN_EXISTS(TEMP_##P) && TEMP_SENSOR_##P != 0 && DISABLED(HEATER_##P##_USES_MAX6675))

Marlin/src/module/motion.h
@@ -36,7 +36,11 @@
 
 // Axis homed and known-position states
 extern uint8_t axis_homed, axis_known_position;
-constexpr uint8_t xyz_bits = _BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS);
+#if ENABLED(E_AXIS_HOMING)
+  constexpr uint8_t xyz_bits = _BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS) | _BV(E_AXIS);
+#else
+  constexpr uint8_t xyz_bits = _BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS);
+#endif
 FORCE_INLINE bool all_axes_homed() { return (axis_homed & xyz_bits) == xyz_bits; }
 FORCE_INLINE bool all_axes_known() { return (axis_known_position & xyz_bits) == xyz_bits; }
 FORCE_INLINE void set_all_unhomed() { axis_homed = 0; }
@@ -71,7 +75,11 @@ extern float cartes[XYZ];
  * Feed rates are often configured with mm/m
  * but the planner and stepper like mm/s units.
  */
-extern const float homing_feedrate_mm_s[XYZ];
+#if ENABLED(E_AXIS_HOMING)
+  extern const float homing_feedrate_mm_s[XYZE];
+#else
+  extern const float homing_feedrate_mm_s[XYZ];
+#endif
 FORCE_INLINE float homing_feedrate(const AxisEnum a) { return pgm_read_float(&homing_feedrate_mm_s[a]); }
 
 extern float feedrate_mm_s;
@@ -96,11 +104,17 @@ extern int16_t feedrate_percentage;
 FORCE_INLINE float pgm_read_any(const float *p) { return pgm_read_float(p); }
 FORCE_INLINE signed char pgm_read_any(const signed char *p) { return pgm_read_byte(p); }
 
-#define XYZ_DEFS(type, array, CONFIG) \
-  extern const type array##_P[XYZ]; \
-  FORCE_INLINE type array(AxisEnum axis) { return pgm_read_any(&array##_P[axis]); } \
-  typedef void __void_##CONFIG##__
-
+#if ENABLED(E_AXIS_HOMING)
+  #define XYZ_DEFS(type, array, CONFIG) \
+    extern const type array##_P[XYZE]; \
+    FORCE_INLINE type array(AxisEnum axis) { return pgm_read_any(&array##_P[axis]); } \
+    typedef void __void_##CONFIG##__
+#else
+  #define XYZ_DEFS(type, array, CONFIG) \
+    extern const type array##_P[XYZ]; \
+    FORCE_INLINE type array(AxisEnum axis) { return pgm_read_any(&array##_P[axis]); } \
+    typedef void __void_##CONFIG##__
+#endif
 XYZ_DEFS(float, base_min_pos,   MIN_POS);
 XYZ_DEFS(float, base_max_pos,   MAX_POS);
 XYZ_DEFS(float, base_home_pos,  HOME_POS);
@@ -116,13 +130,24 @@ XYZ_DEFS(signed char, home_dir, HOME_DIR);
 
 #if HAS_SOFTWARE_ENDSTOPS
   extern bool soft_endstops_enabled;
-  extern float soft_endstop_min[XYZ], soft_endstop_max[XYZ];
-  void clamp_to_software_endstops(float target[XYZ]);
+  #if ENABLED(E_AXIS_HOMING)
+    extern float soft_endstop_min[XYZE], soft_endstop_max[XYZE];
+    void clamp_to_software_endstops(float target[XYZE]);
+  #else
+    extern float soft_endstop_min[XYZ], soft_endstop_max[XYZ];
+    void clamp_to_software_endstops(float target[XYZ]);
+  #endif
   void update_software_endstops(const AxisEnum axis);
 #else
   constexpr bool soft_endstops_enabled = false;
-  constexpr float soft_endstop_min[XYZ] = { X_MIN_BED, Y_MIN_BED, Z_MIN_POS },
-                  soft_endstop_max[XYZ] = { X_MAX_BED, Y_MAX_BED, Z_MAX_POS };
+  #if ENABLED(E_AXIS_HOMING)
+    constexpr float soft_endstop_min[XYZE] = { X_MIN_BED, Y_MIN_BED, Z_MIN_POS, E_MIN_POS },
+                    soft_endstop_max[XYZE] = { X_MAX_BED, Y_MAX_BED, Z_MAX_POS, E_MAX_POS };
+  #else
+    constexpr float soft_endstop_min[XYZ] = { X_MIN_BED, Y_MIN_BED, Z_MIN_POS },
+                    soft_endstop_max[XYZ] = { X_MAX_BED, Y_MAX_BED, Z_MAX_POS };
+  #endif
+  
   #define clamp_to_software_endstops(x) NOOP
   #define update_software_endstops(x) NOOP
 #endif
@@ -201,7 +226,11 @@ void clean_up_after_endstop_or_probe_move();
     ) || ENABLED(NO_MOTION_BEFORE_HOMING)
 
 #if HAS_AXIS_UNHOMED_ERR
-  bool axis_unhomed_error(const bool x=true, const bool y=true, const bool z=true);
+  #if ENABLED(E_AXIS_HOMING)
+    bool axis_unhomed_error(const bool x=true, const bool y=true, const bool z=true, const bool e=true);
+  #else
+    bool axis_unhomed_error(const bool x=true, const bool y=true, const bool z=true);
+  #endif
 #endif
 
 #if ENABLED(NO_MOTION_BEFORE_HOMING)
@@ -221,13 +250,25 @@ void homeaxis(const AxisEnum axis);
  */
 #if HAS_HOME_OFFSET || HAS_POSITION_SHIFT
   #if HAS_HOME_OFFSET
-    extern float home_offset[XYZ];
+    #if ENABLED(E_AXIS_HOMING)
+      extern float home_offset[XYZE];
+    #else
+      extern float home_offset[XYZ];  
+    #endif
   #endif
   #if HAS_POSITION_SHIFT
-    extern float position_shift[XYZ];
+    #if ENABLED(E_AXIS_HOMING)
+      extern float position_shift[XYZE];
+    #else
+      extern float position_shift[XYZ];  
+    #endif
   #endif
   #if HAS_HOME_OFFSET && HAS_POSITION_SHIFT
-    extern float workspace_offset[XYZ];
+    #if ENABLED(E_AXIS_HOMING)
+      extern float workspace_offset[XYZE];
+    #else
+      extern float workspace_offset[XYZ];
+    #endif
     #define WORKSPACE_OFFSET(AXIS) workspace_offset[AXIS]
   #elif HAS_HOME_OFFSET
     #define WORKSPACE_OFFSET(AXIS) home_offset[AXIS]
@@ -243,10 +284,16 @@ void homeaxis(const AxisEnum axis);
 #define LOGICAL_X_POSITION(POS) NATIVE_TO_LOGICAL(POS, X_AXIS)
 #define LOGICAL_Y_POSITION(POS) NATIVE_TO_LOGICAL(POS, Y_AXIS)
 #define LOGICAL_Z_POSITION(POS) NATIVE_TO_LOGICAL(POS, Z_AXIS)
+#if ENABLED(E_AXIS_HOMING)
+  #define LOGICAL_E_POSITION(POS) NATIVE_TO_LOGICAL(POS, E_AXIS)
+#endif
+
 #define RAW_X_POSITION(POS)     LOGICAL_TO_NATIVE(POS, X_AXIS)
 #define RAW_Y_POSITION(POS)     LOGICAL_TO_NATIVE(POS, Y_AXIS)
 #define RAW_Z_POSITION(POS)     LOGICAL_TO_NATIVE(POS, Z_AXIS)
-
+#if ENABLED(E_AXIS_HOMING)
+  #define RAW_E_POSITION(POS)     LOGICAL_TO_NATIVE(POS, E_AXIS)
+#endif
 /**
  * position_is_reachable family of functions
  */

MarlinFirmware/Marlin

Marlin/src/inc/Conditionals_post.h
@@ -61,7 +61,9 @@
 #define X_MAX_LENGTH (X_MAX_POS - (X_MIN_POS))
 #define Y_MAX_LENGTH (Y_MAX_POS - (Y_MIN_POS))
 #define Z_MAX_LENGTH (Z_MAX_POS - (Z_MIN_POS))
-
+#if ENABLED(E_AXIS_HOMING)
+  #define E_MAX_LENGTH (E_MAX_POS - (E_MIN_POS))
+#endif
 // Defined only if the sanity-check is bypassed
 #ifndef X_BED_SIZE
   #define X_BED_SIZE X_MAX_LENGTH
@@ -84,7 +86,9 @@
   #define Y_CENTER ((Y_BED_SIZE) / 2)
 #endif
 #define Z_CENTER ((Z_MIN_POS + Z_MAX_POS) / 2)
-
+#if ENABLED(E_AXIS_HOMING)
+  #define E_CENTER ((E_MIN_POS + E_MAX_POS) / 2)
+#endif
 // Get the linear boundaries of the bed
 #define X_MIN_BED (X_CENTER - (X_BED_SIZE) / 2)
 #define X_MAX_BED (X_CENTER + (X_BED_SIZE) / 2)
@@ -187,6 +191,13 @@
   #define Z_HOME_POS (Z_HOME_DIR < 0 ? Z_MIN_POS : Z_MAX_POS)
 #endif
 
+#if ENABLED(E_AXIS_HOMING)
+  #ifdef MANUAL_E_HOME_POS
+    #define E_HOME_POS MANUAL_E_HOME_POS
+  #else
+    #define E_HOME_POS (E_HOME_DIR < 0 ? E_MIN_POS : E_MAX_POS)
+  #endif
+#endif
 /**
  * If DELTA_HEIGHT isn't defined use the old setting
  */
@@ -798,6 +809,9 @@
   #if ENABLED(USE_ZMIN_PLUG)
     #define ENDSTOPPULLUP_ZMIN
   #endif
+  #if ENABLED(USE_EMIN_PLUG)
+    #define ENDSTOPPULLUP_EMIN
+  #endif
 #endif
 
 /**
@@ -927,6 +941,8 @@
 #define HAS_Y_MAX _HAS_STOP(Y,MAX)
 #define HAS_Z_MIN _HAS_STOP(Z,MIN)
 #define HAS_Z_MAX _HAS_STOP(Z,MAX)
+#define HAS_E_MIN _HAS_STOP(E,MIN)
+#define HAS_E_MAX _HAS_STOP(E,MAX)
 #define HAS_X2_MIN (PIN_EXISTS(X2_MIN))
 #define HAS_X2_MAX (PIN_EXISTS(X2_MAX))
 #define HAS_Y2_MIN (PIN_EXISTS(Y2_MIN))

Marlin/src/module/motion.h
@@ -36,7 +36,11 @@
 
 // Axis homed and known-position states
 extern uint8_t axis_homed, axis_known_position;
-constexpr uint8_t xyz_bits = _BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS);
+#if ENABLED(E_AXIS_HOMING)
+  constexpr uint8_t xyz_bits = _BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS) | _BV(E_AXIS);
+#else
+  constexpr uint8_t xyz_bits = _BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS);
+#endif
 FORCE_INLINE bool all_axes_homed() { return (axis_homed & xyz_bits) == xyz_bits; }
 FORCE_INLINE bool all_axes_known() { return (axis_known_position & xyz_bits) == xyz_bits; }
 FORCE_INLINE void set_all_unhomed() { axis_homed = 0; }
@@ -81,7 +85,11 @@ extern float cartes[XYZ];
  * Feed rates are often configured with mm/m
  * but the planner and stepper like mm/s units.
  */
-extern const float homing_feedrate_mm_s[XYZ];
+#if ENABLED(E_AXIS_HOMING)
+  extern const float homing_feedrate_mm_s[XYZE];
+#else
+  extern const float homing_feedrate_mm_s[XYZ];
+#endif
 FORCE_INLINE float homing_feedrate(const AxisEnum a) { return pgm_read_float(&homing_feedrate_mm_s[a]); }
 float get_homing_bump_feedrate(const AxisEnum axis);
 
@@ -103,11 +111,17 @@ extern int16_t feedrate_percentage;
 FORCE_INLINE float pgm_read_any(const float *p) { return pgm_read_float(p); }
 FORCE_INLINE signed char pgm_read_any(const signed char *p) { return pgm_read_byte(p); }
 
-#define XYZ_DEFS(type, array, CONFIG) \
-  extern const type array##_P[XYZ]; \
-  FORCE_INLINE type array(AxisEnum axis) { return pgm_read_any(&array##_P[axis]); } \
-  typedef void __void_##CONFIG##__
-
+#if ENABLED(E_AXIS_HOMING)
+  #define XYZ_DEFS(type, array, CONFIG) \
+    extern const type array##_P[XYZE]; \
+    FORCE_INLINE type array(AxisEnum axis) { return pgm_read_any(&array##_P[axis]); } \
+    typedef void __void_##CONFIG##__
+#else
+  #define XYZ_DEFS(type, array, CONFIG) \
+    extern const type array##_P[XYZ]; \
+    FORCE_INLINE type array(AxisEnum axis) { return pgm_read_any(&array##_P[axis]); } \
+    typedef void __void_##CONFIG##__
+#endif
 XYZ_DEFS(float, base_min_pos,   MIN_POS);
 XYZ_DEFS(float, base_max_pos,   MAX_POS);
 XYZ_DEFS(float, base_home_pos,  HOME_POS);
@@ -131,8 +145,13 @@ XYZ_DEFS(signed char, home_dir, HOME_DIR);
 typedef struct { float min, max; } axis_limits_t;
 #if HAS_SOFTWARE_ENDSTOPS
   extern bool soft_endstops_enabled;
-  extern axis_limits_t soft_endstop[XYZ];
-  void apply_motion_limits(float target[XYZ]);
+  #if ENABLED(E_AXIS_HOMING)
+    #define SOFT_AXES XYZE
+  #else
+    #define SOFT_AXES XYZ
+  #endif
+  extern axis_limits_t soft_endstop[SOFT_AXES];
+  void apply_motion_limits(float target[SOFT_AXES]);
   void update_software_endstops(const AxisEnum axis
     #if HAS_HOTEND_OFFSET
       , const uint8_t old_tool_index=0, const uint8_t new_tool_index=0
@@ -140,8 +159,7 @@ typedef struct { float min, max; } axis_limits_t;
   );
 #else
   constexpr bool soft_endstops_enabled = false;
-  //constexpr axis_limits_t soft_endstop[XYZ] = { { X_MIN_POS, X_MAX_POS }, { Y_MIN_POS, Y_MAX_POS }, { Z_MIN_POS, Z_MAX_POS } };
-  #define apply_motion_limits(V)    NOOP
+  #define apply_motion_limits(...) NOOP
   #define update_software_endstops(...) NOOP
 #endif
 
@@ -203,7 +221,11 @@ void clean_up_after_endstop_or_probe_move();
 // Homing
 //
 
-bool axis_unhomed_error(const bool x=true, const bool y=true, const bool z=true);
+bool axis_unhomed_error(const bool x=true, const bool y=true, const bool z=true
+  #if ENABLED(E_AXIS_HOMING)
+    , const bool e=true
+  #endif
+);
 
 #if ENABLED(NO_MOTION_BEFORE_HOMING)
   #define MOTION_CONDITIONS (IsRunning() && !axis_unhomed_error())
@@ -222,13 +244,25 @@ void homeaxis(const AxisEnum axis);
  */
 #if HAS_HOME_OFFSET || HAS_POSITION_SHIFT
   #if HAS_HOME_OFFSET
-    extern float home_offset[XYZ];
+    #if ENABLED(E_AXIS_HOMING)
+      extern float home_offset[XYZE];
+    #else
+      extern float home_offset[XYZ];
+    #endif
   #endif
   #if HAS_POSITION_SHIFT
-    extern float position_shift[XYZ];
+    #if ENABLED(E_AXIS_HOMING)
+      extern float position_shift[XYZE];
+    #else
+      extern float position_shift[XYZ];
+    #endif
   #endif
   #if HAS_HOME_OFFSET && HAS_POSITION_SHIFT
-    extern float workspace_offset[XYZ];
+    #if ENABLED(E_AXIS_HOMING)
+      extern float workspace_offset[XYZE];
+    #else
+      extern float workspace_offset[XYZ];
+    #endif
     #define WORKSPACE_OFFSET(AXIS) workspace_offset[AXIS]
   #elif HAS_HOME_OFFSET
     #define WORKSPACE_OFFSET(AXIS) home_offset[AXIS]
@@ -244,10 +278,16 @@ void homeaxis(const AxisEnum axis);
 #define LOGICAL_X_POSITION(POS) NATIVE_TO_LOGICAL(POS, X_AXIS)
 #define LOGICAL_Y_POSITION(POS) NATIVE_TO_LOGICAL(POS, Y_AXIS)
 #define LOGICAL_Z_POSITION(POS) NATIVE_TO_LOGICAL(POS, Z_AXIS)
+#if ENABLED(E_AXIS_HOMING)
+  #define LOGICAL_E_POSITION(POS) NATIVE_TO_LOGICAL(POS, E_AXIS)
+#endif
+
 #define RAW_X_POSITION(POS)     LOGICAL_TO_NATIVE(POS, X_AXIS)
 #define RAW_Y_POSITION(POS)     LOGICAL_TO_NATIVE(POS, Y_AXIS)
 #define RAW_Z_POSITION(POS)     LOGICAL_TO_NATIVE(POS, Z_AXIS)
-
+#if ENABLED(E_AXIS_HOMING)
+  #define RAW_E_POSITION(POS)     LOGICAL_TO_NATIVE(POS, E_AXIS)
+#endif
 /**
  * position_is_reachable family of functions
  */