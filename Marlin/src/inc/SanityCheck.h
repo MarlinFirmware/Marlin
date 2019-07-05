4c3aa82f591e3f0016b4157368ddba92a2f03f0f
Ludy <1@astra-g.org>
2019-07-05 22:54:25 +0200

    test case light pin with menu

--- Marlin/src/inc/SanityCheck.h
+++ Marlin/src/inc/SanityCheck.h
@@ -1410,8 +1410,14 @@ static_assert(Y_MAX_LENGTH >= Y_BED_SIZE, "Movement bounds (Y_MIN_POS, Y_MAX_POS
 /**
  * Test case light not using the same pin as the fan
  */
-#if ENABLED(CASE_LIGHT_ENABLE) && CASE_LIGHT_PIN == FAN_PIN
+#if ENABLED(CASE_LIGHT_MENU) && CASE_LIGHT_PIN == FAN_PIN
   #error "You cannot set CASE_LIGHT_PIN equal to FAN_PIN."
+
+/**
+ * test whether case light pin has been defined if case light menu is active.
+ */
+#if ENABLED(CASE_LIGHT_MENU) && !ANY_PIN(CASE_LIGHT)
+ #error "CASE_LIGHT_MENU requires CASE_LIGHT_PIN to be defined."
 #endif
 
 /**
