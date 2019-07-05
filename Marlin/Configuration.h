5581aea06b432a208a3424de0cae1ccb29530665
okservice <okservice@users.noreply.github.com>
2019-07-05 14:28:04 +0300

    ok

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -74,7 +74,7 @@
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
 #define STRING_CONFIG_H_AUTHOR "(SHAMAN)" // Who made the changes. OK™
-#define SHOW_BOOTSCREEN
+//#define SHOW_BOOTSCREEN //OK™
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
 
@@ -137,7 +137,7 @@
 
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
-#define CUSTOM_MACHINE_NAME "SUPERPRINTER" //OK™
+#define CUSTOM_MACHINE_NAME "ZAEBIS!" //OK™
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
