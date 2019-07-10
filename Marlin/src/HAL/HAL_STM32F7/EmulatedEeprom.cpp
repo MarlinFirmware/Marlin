c765efddb2bd4820b5c725253745281c9a4c210f
Scott Lahteine <github@thinkyhead.com>
2019-07-09 21:27:30 -0500

    Tweak STM32F4/7 eeprom emulation

--- Marlin/src/HAL/HAL_STM32F7/EmulatedEeprom.cpp
+++ Marlin/src/HAL/HAL_STM32F7/EmulatedEeprom.cpp
@@ -20,7 +20,7 @@
 #ifdef STM32F7
 
 /**
- * Description: functions for I2C connected external EEPROM.
+ * Description: Functions for a Flash emulated EEPROM
  * Not platform dependent.
  */
 
@@ -67,34 +67,32 @@ void eeprom_init() {
 }
 
 void eeprom_write_byte(uint8_t *pos, unsigned char value) {
-  uint16_t eeprom_address = (unsigned) pos;
+  uint16_t eeprom_address = unsigned(pos);
 
   eeprom_init();
 
   HAL_FLASH_Unlock();
   __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR |FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR);
 
-  if (EE_WriteVariable(eeprom_address, (uint16_t) value) != EE_OK)
-      for (;;) HAL_Delay(1); // Spin forever until watchdog reset
+  if (EE_WriteVariable(eeprom_address, uint16_t(value)) != EE_OK)
+    for (;;) HAL_Delay(1); // Spin forever until watchdog reset
 
   HAL_FLASH_Lock();
 }
 
 uint8_t eeprom_read_byte(uint8_t *pos) {
-  uint16_t data = 0xFF;
-  uint16_t eeprom_address = (unsigned)pos;
-
   eeprom_init();
 
-  if (EE_ReadVariable(eeprom_address, &data) != EE_OK) {
-    return (unsigned char)data;
-  }
-  return (unsigned char)data;
+  uint16_t data = 0xFF;
+  uint16_t eeprom_address = unsigned(pos);
+  (void)EE_ReadVariable(eeprom_address, &data); // Data unchanged on error
+
+  return uint8_t(data);
 }
 
 void eeprom_read_block(void *__dst, const void *__src, size_t __n) {
   uint16_t data = 0xFF;
-  uint16_t eeprom_address = (unsigned) __src;
+  uint16_t eeprom_address = unsigned(__src);
 
   eeprom_init();
 
