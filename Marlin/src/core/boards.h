95a5b7f0416c3b33bc87c89c3cef22d551092cf7
neorex <neorex@naver.com>
2019-07-11 14:58:55 +0900

    Oscar board (by Thinknthings of Korea) support
    Am ATmega2560-based board supporting 5 extruders

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -93,6 +93,7 @@
 #define BOARD_VORON                   1139  // VORON Design
 #define BOARD_TRONXY_V3_1_0           1140  // Tronxy TRONXY-V3-1.0
 #define BOARD_Z_BOLT_X_SERIES         1141  // Z-Bolt X Series
+#define BOARD_TT_OSCAR                1142  // TT OSCAR
 
 //
 // RAMBo and derivatives
