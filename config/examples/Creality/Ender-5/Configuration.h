bcbd2ff21c237fd2fcf5cf5fa4661d0b39afdd82
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2019-07-14 07:32:43 -0700

    Fix Ender-5 Z axis settings (#14605)

--- config/examples/Creality/Ender-5/Configuration.h
+++ config/examples/Creality/Ender-5/Configuration.h
@@ -998,7 +998,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
 #define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true
 
 // @section extruder
 
