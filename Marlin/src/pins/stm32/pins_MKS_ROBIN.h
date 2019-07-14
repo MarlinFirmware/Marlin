4e18f5ee71dc84dc74595eca76364530150c66a8
Scott Lahteine <github@thinkyhead.com>
2019-07-14 15:55:21 -0500

    Update similar comments

--- Marlin/src/pins/stm32/pins_MKS_ROBIN.h
+++ Marlin/src/pins/stm32/pins_MKS_ROBIN.h
@@ -109,11 +109,15 @@
 #define LED_PIN            PB2
 
 /**
- * Note: MKS Robin TFT screens may have different TFT controllers
- * If the screen stays white, disable 'LCD_RESET_PIN' to rely on the bootloader to do screen initialization.
+ * Note: MKS Robin TFT screens use various TFT controllers. Supported screens
+ * are based on the ILI9342, ILI9328 and ST7798V. Define init sequences for
+ * other screens in u8g_dev_tft_320x240_upscale_from_128x64.cpp
  *
- * Enabling 'LCD_RESET_PIN' causes flickering when entering the LCD menu due to LCD controller reset.
- * Reset feature was designed to "revive the LCD if static electricity killed it."
+ * If the screen stays white, disable 'LCD_RESET_PIN'
+ * to let the bootloader init the screen.
+ *
+ * Setting an 'LCD_RESET_PIN' may cause a flicker when entering the LCD menu
+ * because Marlin uses the reset as a failsafe to revive a glitchy LCD.
  */
 //#define LCD_RESET_PIN      PF6
 #define LCD_BACKLIGHT_PIN  PG11
