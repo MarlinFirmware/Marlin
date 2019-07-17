6d98321fad9cd7fad4557de82eb20d887514b92c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-07-16 21:34:50 -0500

    strlen probably handles it, but...

--- Marlin/src/gcode/lcd/M117.cpp
+++ Marlin/src/gcode/lcd/M117.cpp
@@ -27,8 +27,10 @@
  * M117: Set LCD Status Message
  */
 void GcodeSuite::M117() {
-  if(strlen(parser.string_arg))
-    ui.set_status(parser.string_arg);
+
+  if (parser.string_arg && parser.string_arg[0])
+    ui.set_status(parser.string_arg)
   else
     ui.reset_status();
+
 }
