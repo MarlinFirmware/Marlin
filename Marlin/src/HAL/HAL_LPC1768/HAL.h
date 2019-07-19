4cc83b6330009d616a8673214fafba2ccc6676e6
Scott Lahteine <github@thinkyhead.com>
2019-07-18 23:37:54 -0500

    Allow THERMAL_PROTECTION_GRACE_PERIOD override
    Co-Authored-By: MarcelMo <marcelmo@users.noreply.github.com>

--- Marlin/src/HAL/HAL_LPC1768/HAL.h
+++ Marlin/src/HAL/HAL_LPC1768/HAL.h
@@ -147,7 +147,9 @@ using FilteredADC = LPC176x::ADC<ADC_LOWPASS_K_VALUE, ADC_MEDIAN_FILTER_SIZE>;
 #define HAL_ADC_READY()        FilteredADC::finished_conversion()
 
 // A grace period to allow ADC readings to stabilize, preventing false alarms
-#define THERMAL_PROTECTION_GRACE_PERIOD 1000
+#ifndef THERMAL_PROTECTION_GRACE_PERIOD
+  #define THERMAL_PROTECTION_GRACE_PERIOD 1000
+#endif
 
 // Parse a G-code word into a pin index
 int16_t PARSED_PIN_INDEX(const char code, const int16_t dval);
