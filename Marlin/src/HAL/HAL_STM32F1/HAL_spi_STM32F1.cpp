75249c4d8f17f9022de0b8b05208a2f56384a273
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-07-20 20:34:13 -0500

    Update HAL_spi_STM32F1.cpp

--- Marlin/src/HAL/HAL_STM32F1/HAL_spi_STM32F1.cpp
+++ Marlin/src/HAL/HAL_STM32F1/HAL_spi_STM32F1.cpp
@@ -97,7 +97,7 @@ void spiInit(uint8_t spiRate) {
     case SPI_EIGHTH_SPEED:  clock = SPI_CLOCK_DIV16; break;
     case SPI_SPEED_5:       clock = SPI_CLOCK_DIV32; break;
     case SPI_SPEED_6:       clock = SPI_CLOCK_DIV64; break;
-    default:                clock = SPI_CLOCK_MAX;  // Default from the SPI library
+    default:                clock = SPI_CLOCK_DIV2;  // Default from the SPI library
   }
   SPI.setModule(SPI_DEVICE);
   SPI.begin();
