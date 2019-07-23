MarlinFirmware/Marlin

commit cf9ac4c84751507d09ccb9379bf366cee897229f

Move vsnprintf_P to Marduino.h

Marlin/src/HAL/shared/Marduino.h
@@ -29,6 +29,7 @@
 #undef M_PI           // Redefined by all
 #undef _BV            // Redefined by some
 #undef sq             // Redefined by teensy3/wiring.h
+#undef vsnprintf_P    // Defined by avr/pgmspace.h in some platforms
 
 #include <Arduino.h>  // NOTE: If included earlier then this line is a NOOP
 
@@ -40,6 +41,10 @@
 
 #undef sq
 #define sq(x) ((x)*(x))
+
+#ifndef vsnprintf_P
+  #define vsnprintf_P vsnprintf
+#endif
 
 #ifndef SBI
   #define SBI(A,B) (A |= (1 << (B)))

Marlin/src/HAL/HAL_DUE/HAL.h
@@ -68,10 +68,6 @@
   #define strncpy_P(dest, src, num) strncpy((dest), (src), (num))
 #endif
 
-#ifndef vsnprintf_P
-  #define vsnprintf_P vsnprintf
-#endif
-
 // Fix bug in pgm_read_ptr
 #undef pgm_read_ptr
 #define pgm_read_ptr(addr) (*((void**)(addr)))

Marlin/src/HAL/HAL_STM32F1/HAL.h
@@ -28,10 +28,6 @@
 
 #define CPU_32_BIT
 
-#ifndef vsnprintf_P
-  #define vsnprintf_P vsnprintf
-#endif
-
 #include "../../core/macros.h"
 #include "../shared/Marduino.h"
 #include "../shared/math_32bit.h"

Marlin/src/HAL/HAL_STM32F4/HAL.h
@@ -24,10 +24,6 @@
 
 #define CPU_32_BIT
 
-#ifndef vsnprintf_P
-  #define vsnprintf_P vsnprintf
-#endif
-
 #include "../shared/Marduino.h"
 #include "../shared/math_32bit.h"
 #include "../shared/HAL_SPI.h"

commit a1ea38cc06808eabeed3d1371660bb3eec9d0192

Fix regression on AVR caused by commit cf9ac4c847
This change caused the machine to lock up while executing GCODE via MENU_ITEM(gcode, _UxGT(...), PSTR(...));

Marlin/src/HAL/shared/Marduino.h
@@ -29,7 +29,6 @@
 #undef M_PI           // Redefined by all
 #undef _BV            // Redefined by some
 #undef sq             // Redefined by teensy3/wiring.h
-#undef vsnprintf_P    // Defined by avr/pgmspace.h in some platforms
 
 #include <Arduino.h>  // NOTE: If included earlier then this line is a NOOP
 
@@ -41,10 +40,6 @@
 
 #undef sq
 #define sq(x) ((x)*(x))
-
-#ifndef vsnprintf_P
-  #define vsnprintf_P vsnprintf
-#endif
 
 #ifndef SBI
   #define SBI(A,B) (A |= (1 << (B)))