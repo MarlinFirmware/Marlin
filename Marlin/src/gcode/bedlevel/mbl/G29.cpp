bbb69d5ce910b47bd5423b1ee63720dc8c47b8f6
Ludy <1@astra-g.org>
2019-07-27 20:49:32 +0200

    Fix 2

--- Marlin/src/gcode/bedlevel/mbl/G29.cpp
+++ Marlin/src/gcode/bedlevel/mbl/G29.cpp
@@ -37,6 +37,10 @@
 #include "../../../lcd/ultralcd.h"
 #include "../../../module/motion.h"
 #include "../../../module/stepper.h"
+  
+#if ENABLED(EXTENSIBLE_UI)
+  #include "../../../lcd/extensible_ui/ui_api.h"
+#endif
 
 // Save 130 bytes with non-duplication of PSTR
 inline void echo_not_entered(const char c) { SERIAL_CHAR(c); SERIAL_ECHOLNPGM(" not entered."); }
