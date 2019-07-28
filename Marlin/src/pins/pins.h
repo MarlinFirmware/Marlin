e8e81d36097187124507596c50f6f140004ff0d6
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2019-07-28 12:56:05 -0700

    Fix MKS Robin Lite pins include (#14755)

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -454,6 +454,8 @@
   #include "stm32/pins_MKS_ROBIN_MINI.h"        // STM32F1                                env:mks_robin_mini
 #elif MB(MKS_ROBIN_NANO)
   #include "stm32/pins_MKS_ROBIN_NANO.h"        // STM32F1                                env:mks_robin_nano
+#elif MB(MKS_ROBIN_LITE)
+  #include "stm32/pins_MKS_ROBIN_LITE.h"        // STM32F1                                env:mks_robin_lite
 #elif MB(BIGTREE_SKR_MINI_V1_1)
   #include "stm32/pins_BIGTREE_SKR_MINI_V1_1.h" // STM32F1                                env:BIGTREE_SKR_MINI
 #elif MB(BIGTREE_SKR_MINI_E3)
