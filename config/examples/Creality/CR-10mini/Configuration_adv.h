f8aa52346f8a514302f962b484dbf2fbe7ce4c63
MarcelMo <mommsen@gmx.de>
2019-07-30 09:16:26 +0200

    Configurable Z move distance (#14593)

--- config/examples/Creality/CR-10mini/Configuration_adv.h
+++ config/examples/Creality/CR-10mini/Configuration_adv.h
@@ -650,6 +650,7 @@
 
 #if EITHER(ULTIPANEL, EXTENSIBLE_UI)
   #define MANUAL_FEEDRATE { 50*60, 50*60, 4*60, 60 } // Feedrates for manual moves along X, Y, Z, E from panel
+  #define SHORT_MANUAL_Z_MOVE 0.025 // (mm) Smallest manual Z move (< 0.1mm)
   #if ENABLED(ULTIPANEL)
     #define MANUAL_E_MOVES_RELATIVE // Display extruder move distance rather than "position"
     #define ULTIPANEL_FEEDMULTIPLY  // Encoder sets the feedrate multiplier on the Status Screen
