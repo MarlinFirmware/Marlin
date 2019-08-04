b6826d7791e5bf79329e79325179d6505bc3e3b8
InsanityAutomation <38436470+InsanityAutomation@users.noreply.github.com>
2019-08-04 12:41:51 -0400

    bump eeprom version

--- Marlin/configuration_store.cpp
+++ Marlin/configuration_store.cpp
@@ -37,7 +37,7 @@
  */
 
 // Change EEPROM version if the structure changes
-#define EEPROM_VERSION "V55"
+#define EEPROM_VERSION "V56"
 #define EEPROM_OFFSET 100
 
 // Check the integrity of data offsets.
