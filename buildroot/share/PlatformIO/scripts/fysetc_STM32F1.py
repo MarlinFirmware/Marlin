b510d868e6c11dac881f943a3c77f8ab79f5ff25
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2019-08-09 10:46:13 +0200

    Revert "Fix FYSETC stm32flash usage (#14703)"
    This reverts commit a6bec539055c21191e49a47b6098b4c21afdafcb.
    
    + Use os.path.join() to handle possible spaces in BUILD_DIR

--- buildroot/share/PlatformIO/scripts/fysetc_STM32F1.py
+++ buildroot/share/PlatformIO/scripts/fysetc_STM32F1.py
@@ -1,8 +1,6 @@
 from os.path import join
 Import("env", "projenv")
 
-platform = env.PioPlatform()
-
 # Relocate firmware from 0x08000000 to 0x08002000
 #env['CPPDEFINES'].remove(("VECT_TAB_ADDR", 134217728))
 #env['CPPDEFINES'].append(("VECT_TAB_ADDR", "0x08010000"))
@@ -29,10 +27,8 @@ env.AddPostAction(
 
 # In-line command with arguments
 env.Replace(
-	UPLOADER=platform.get_package_dir("tool-stm32duino") + '/stm32flash/stm32flash',
-	UPLOADCMD='"${UPLOADER}" -v -i rts,-dtr,dtr,-rts -R -b 115200 -g 0x8000000 -w "${BUILD_DIR}/${PROGNAME}.hex" ${UPLOAD_PORT}'
+	UPLOADCMD="stm32flash -v -i rts,-dtr,dtr " + '$UPLOAD_PORT' + " -R -w " + join("$BUILD_DIR","${PROGNAME}.hex")
 )
-
 
 # Python callback
 #def on_upload(source, target, env):
