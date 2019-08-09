89797a4c26c3b2d7f2a50b5339cf3f80a7095001
thisiskeithb <thisiskeithb@outlook.com>
2019-08-09 02:23:05 -0700

    Increase homing feedrates

--- config/examples/Tevo/Michelangelo/Configuration.h
+++ config/examples/Tevo/Michelangelo/Configuration.h
@@ -1334,8 +1334,8 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_XY (120*60)
+#define HOMING_FEEDRATE_Z  (30*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
