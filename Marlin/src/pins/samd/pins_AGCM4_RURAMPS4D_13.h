3129fd06f0a562217d6faf64a55412140f8d81d8
Giuliano Zaro <3684609+GMagician@users.noreply.github.com>
2019-08-10 07:56:55 +0200

    SAMD51: Update LCD pins (#14907)

--- Marlin/src/pins/samd/pins_AGCM4_RURAMPS4D_13.h
+++ Marlin/src/pins/samd/pins_AGCM4_RURAMPS4D_13.h
@@ -102,7 +102,7 @@
 
 #if HAS_FILAMENT_SENSOR
   #ifndef FIL_RUNOUT_PIN
-    #define FIL_RUNOUT_PIN   Y_MIN_PIN
+    #define FIL_RUNOUT_PIN Y_MIN_PIN
   #endif
 #endif
 
@@ -172,35 +172,38 @@
 
   #if EITHER(RADDS_DISPLAY, REPRAP_DISCOUNT_SMART_CONTROLLER)
 
-    #define BEEPER_PIN     75
+    #error "Pin compatibility check needed!"
+    #define BEEPER_PIN     54
     #define LCD_PINS_D4    48
     #define LCD_PINS_D7    53
     #define SD_DETECT_PIN  -1    // 51 can't be used, it's MOSI
-    #define LCD_PINS_RS    76
-    #define LCD_PINS_ENABLE 77
+    #define LCD_PINS_RS    55
+    #define LCD_PINS_ENABLE 56
 
   #elif ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
 
-    #define BEEPER_PIN     75
+    #define BEEPER_PIN     54
     #define LCD_PINS_D4    48
     #define SD_DETECT_PIN  -1    // 51 can't be used, it's MOSI
-    #define LCD_PINS_RS    76
-    #define LCD_PINS_ENABLE 77
+    #define LCD_PINS_RS    55
+    #define LCD_PINS_ENABLE 56
 
   #elif HAS_SSD1306_OLED_I2C
 
-    #define BEEPER_PIN     75
+    #error "Pin compatibility check needed!"
+    #define BEEPER_PIN     54
     #define LCD_SDSS       10
     #define SD_DETECT_PIN  -1    // 51 can't be used, it's MOSI
 
   #elif ENABLED(FYSETC_MINI_12864)
 
-    #define BEEPER_PIN     75
-    #define DOGLCD_CS      77
-    #define DOGLCD_A0      76
+    #error "Pin compatibility check needed!"
+    #define BEEPER_PIN     54
+    #define DOGLCD_CS      56
+    #define DOGLCD_A0      55
 
     //#define FORCE_SOFT_SPI    // Use this if default of hardware SPI causes display problems
-                                //   results in LCD soft SPI mode 3, SD soft SPI mode 0
+                                // results in LCD soft SPI mode 3, SD soft SPI mode 0
 
     #define LCD_RESET_PIN  48   // Must be high or open for LCD to operate normally.
 
@@ -217,7 +220,7 @@
       #endif
     #elif ENABLED(FYSETC_MINI_12864_2_1)
       #error "Pin compatibility check needed! Grand central M4 pins 50, 51 and 52 are not GPIO pins, they are wired to MISO, MOSI, and SCK."
-      #define NEOPIXEL_PIN    50   // D5
+      #define NEOPIXEL_PIN 50   // D5
     #endif
 
   #elif ENABLED(MKS_MINI_12864)
