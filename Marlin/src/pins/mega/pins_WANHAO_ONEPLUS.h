85be250415bcc566c5de8308ddf098c1e63a2609
thisiskeithb <thisiskeithb@outlook.com>
2019-08-11 05:31:22 -0700

    Fix LCD Contrast Issue
    Since the LCD is attached directly the to board, set the contrast directly.

--- Marlin/src/pins/mega/pins_WANHAO_ONEPLUS.h
+++ Marlin/src/pins/mega/pins_WANHAO_ONEPLUS.h
@@ -92,20 +92,16 @@
 #define KILL_PIN           64
 
 //
-// LCD / Controller
+// LCD / Controller (LCD is non-removable)
 //
 
-#if HAS_SPI_LCD
-  #define DOGLCD_A0        40
-  #define DOGLCD_CS        41
-  #define LCD_BACKLIGHT_PIN 65   // Backlight LED on A11/D65
-  #define LCD_RESET_PIN    27
-
-  #define LCD_CONTRAST    255
-
-  #if ENABLED(NEWPANEL)
-    #define BTN_EN1         2
-    #define BTN_EN2         3
-    #define BTN_ENC         5
-  #endif
-#endif
+#define DOGLCD_A0        40
+#define DOGLCD_CS        41
+#define LCD_BACKLIGHT_PIN 65   // Backlight LED on A11/D65
+#define LCD_RESET_PIN    27
+#define LCD_CONTRAST_MIN 0
+#define LCD_CONTRAST_MAX 255
+#define DEFAULT_LCD_CONTRAST 255
+#define BTN_EN1         2
+#define BTN_EN2         3
+#define BTN_ENC         5
