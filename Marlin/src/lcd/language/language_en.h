4336eed0e32538d34d7dcfdb300aa6192b8a8fd3
Joseph Bozarth <bozarjp@gmail.com>
2019-08-10 22:04:15 -0500

    Added excpected printer check
    Added new command for enforcing that the printer matches the expected printer of the gcode.

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -1164,6 +1164,11 @@
 #ifndef MSG_CASE_LIGHT_BRIGHTNESS
   #define MSG_CASE_LIGHT_BRIGHTNESS           _UxGT("Light Brightness")
 #endif
+
+#ifndef MSG_EXPECTED_PRINTER
+  #define MSG_EXPECTED_PRINTER                 _UxGT("INCORRECT PRINTER")
+#endif
+
 #if LCD_WIDTH >= 20
   #ifndef MSG_INFO_PRINT_COUNT
     #define MSG_INFO_PRINT_COUNT              _UxGT("Print Count")
--- config/default/Configuration.h
+++ config/default/Configuration.h
@@ -137,6 +137,14 @@
 // Name displayed in the LCD "Ready" message and Info menu
 //#define CUSTOM_MACHINE_NAME "3D Printer"
 
+/**
+ * Expected Printer Check
+ * This feature will check a given expected printer name against the printer name set in
+ * the firmware. If they do not match the print will be killed displaying an error message.
+ *   'M16 expected_printer_name'
+ */
+//#define EXPECTED_PRINTER_CHECK
+
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
 //#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
