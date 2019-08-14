b72b30c0143b7f73b94a57cff724b563e4bad242
Scott Lahteine <github@thinkyhead.com>
2019-08-14 16:44:40 -0500

    Revert adjustable contrast conditions

--- Marlin/src/inc/Conditionals_LCD.h
+++ Marlin/src/inc/Conditionals_LCD.h
@@ -384,7 +384,14 @@
 /**
  * Default LCD contrast for Graphical LCD displays
  */
-#define HAS_LCD_CONTRAST (HAS_GRAPHICAL_LCD && defined(DEFAULT_LCD_CONTRAST))
+#define HAS_LCD_CONTRAST (                \
+     ENABLED(MAKRPANEL)                   \
+  || ENABLED(CARTESIO_UI)                 \
+  || ENABLED(VIKI2)                       \
+  || ENABLED(AZSMZ_12864)                 \
+  || ENABLED(miniVIKI)                    \
+  || ENABLED(ELB_FULL_GRAPHIC_CONTROLLER) \
+)
 #if HAS_LCD_CONTRAST
   #ifndef LCD_CONTRAST_MIN
     #define LCD_CONTRAST_MIN 0
