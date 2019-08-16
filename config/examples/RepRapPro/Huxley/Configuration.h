5460a8984e3532f011d95a2768f621593332b8ab
Scott Lahteine <github@thinkyhead.com>
2019-08-16 17:51:50 -0500

    Apply to all configs

--- config/examples/RepRapPro/Huxley/Configuration.h
+++ config/examples/RepRapPro/Huxley/Configuration.h
@@ -2083,6 +2083,12 @@ Black rubber belt(MXL), 18 - tooth aluminium pulley : 87.489 step per mm (Huxley
 //#define MALYAN_LCD
 
 //
+// LulzBot Color Touch UI for FTDI EVE (FT800/FT810) displays
+// See Configuration_adv.h for all configuration options.
+//
+//#defined LULZBOT_TOUCH_UI
+
+//
 // Third-party or vendor-customized controller interfaces.
 // Sources should be installed in 'src/lcd/extensible_ui'.
 //
