8cd2202cc48b7ba379c885db5df54d18b528a8a8
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-08-16 20:01:35 -0500

    Update extui_malyan_lcd.cpp

--- Marlin/src/lcd/extui_malyan_lcd.cpp
+++ Marlin/src/lcd/extui_malyan_lcd.cpp
@@ -420,7 +420,7 @@ namespace ExtUI {
       // If there was a print in progress, we need to emit the final
       // print status as {TQ:100}. Reset last percent done so a new print will
       // issue a percent of 0.
-      const uint8_t percent_done = (ExtUI::isPrinting()||ExtUI::isPrintingFromMediaPaused()) ? ExtUI::getProgress_percent() : last_printing_status ? 100 : 0;
+      const uint8_t percent_done = (ExtUI::isPrinting() || ExtUI::isPrintingFromMediaPaused()) ? ExtUI::getProgress_percent() : last_printing_status ? 100 : 0;
       if (percent_done != last_percent_done) {
         char message_buffer[16];
         sprintf_P(message_buffer, PSTR("{TQ:%03i}"), percent_done);
