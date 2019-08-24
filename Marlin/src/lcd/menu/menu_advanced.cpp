0a280f00c2e1d0a59791fbefeca578ca9ef73f6e
Tanguy Pruvot <tpruvot@users.noreply.github.com>
2019-08-24 05:21:30 +0200

    Flag unused variables without buzzer (#15016)

--- Marlin/src/lcd/menu/menu_advanced.cpp
+++ Marlin/src/lcd/menu/menu_advanced.cpp
@@ -608,6 +608,7 @@ void menu_backlash();
           #if HAS_BUZZER
             ui.completion_feedback(inited);
           #endif
+          UNUSED(inited);
         },
         ui.goto_previous_screen,
         PSTR(MSG_INIT_EEPROM), nullptr, PSTR("?")
