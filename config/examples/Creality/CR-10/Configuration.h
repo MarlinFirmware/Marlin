1769f43226288ed48686a162a1afe6f619576da7
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2019-08-25 02:47:02 -0700

    Fix CR-10/10S/Mini/5S Bed Thermistor Types (#15040)

--- config/examples/Creality/CR-10/Configuration.h
+++ config/examples/Creality/CR-10/Configuration.h
@@ -411,7 +411,7 @@
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_5 0
-#define TEMP_SENSOR_BED 5
+#define TEMP_SENSOR_BED 1
 #define TEMP_SENSOR_CHAMBER 0
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
