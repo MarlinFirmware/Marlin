d439e6fdced33ef4b7d6f2d625d3425eb44ad69a
InsanityAutomation <d.menzel@insanityautomation.com>
2019-08-26 16:54:19 -0400

    Change Estop to Full Shutdown
    Reference to show minimal code changes, but documentation needs updating.

--- Marlin/src/gcode/control/M108_M112_M410.cpp
+++ Marlin/src/gcode/control/M108_M112_M410.cpp
@@ -38,10 +38,10 @@ void GcodeSuite::M108() {
 }
 
 /**
- * M112: Emergency Stop
+ * M112: Full Shutdown
  */
 void GcodeSuite::M112() {
-  kill(PSTR("M112 Estop"));
+  kill(PSTR("M112 Shutdown"));
 }
 
 /**
