12c595c61f4b3e0be2baa6a906718e155a797ea3
Giuliano Zaro <3684609+GMagician@users.noreply.github.com>
2019-09-02 12:45:02 +0200

    SAMD51 LCD support (#15113)

--- Marlin/src/HAL/HAL_SAMD51/HAL.h
+++ Marlin/src/HAL/HAL_SAMD51/HAL.h
@@ -146,3 +146,11 @@ FORCE_INLINE void _delay_ms(const int delay_ms) { delay(delay_ms); }
 #pragma GCC diagnostic ignored "-Wunused-function"
 int freeMemory(void);
 #pragma GCC diagnostic pop
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+char *dtostrf(double __val, signed char __width, unsigned char __prec, char *__s);
+#ifdef __cplusplus
+  }
+#endif
