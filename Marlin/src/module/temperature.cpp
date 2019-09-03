12344085c0b7e51a0f8bdf54955a284dfb2be24f
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-09-02 19:21:47 -0500

    Update temperature.cpp

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -2996,7 +2996,7 @@ void Temperature::isr() {
           SERIAL_EOL();
         }
 
-        manage_inactivity();
+        idle();
         gcode.reset_stepper_timeout(); // Keep steppers powered
 
         const float temp = degHotend(target_extruder);
