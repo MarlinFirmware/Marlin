8c9ed6c5bb4031f208fa32815affd1212bdcd1e2
Scott Lahteine <github@thinkyhead.com>
2019-09-10 17:38:06 -0500

    Fix G90, G91, M82, M83 rel/abs modes

--- Marlin/src/gcode/units/M82_M83.cpp
+++ Marlin/src/gcode/units/M82_M83.cpp
@@ -25,9 +25,9 @@
 /**
  * M82: Set E codes absolute (default)
  */
-void GcodeSuite::M82() { axis_relative_modes[E_AXIS] = false; }
+void GcodeSuite::M82() { set_e_absolute(); }
 
 /**
  * M83: Set E codes relative while in Absolute Coordinates (G90) mode
  */
-void GcodeSuite::M83() { axis_relative_modes[E_AXIS] = true; }
+void GcodeSuite::M83() { set_e_relative(); }
