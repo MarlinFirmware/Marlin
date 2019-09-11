e3278f22f32e2f93663e5fd8d3ef33be958adf82
Scott Lahteine <github@thinkyhead.com>
2019-09-11 03:28:31 -0500

    General formatting

--- Marlin/src/lcd/extensible_ui/lib/lulzbot/ftdi_eve_lib/extended/unicode/western_char_set.cpp
+++ Marlin/src/lcd/extensible_ui/lib/lulzbot/ftdi_eve_lib/extended/unicode/western_char_set.cpp
@@ -228,9 +228,9 @@
     alt_fm.stride = 19;
     alt_fm.width  = 38;
     alt_fm.height = 49;
-    for (uint8_t i = 0; i < 127; i++) {
+    for (uint8_t i = 0; i < 127; i++)
       alt_fm.char_widths[i] = 0;
-    }
+
     // For special characters, copy the character widths from the char tables
     for (uint8_t i = 0; i < NUM_ELEMENTS(char_recipe); i++) {
       uint8_t std_char, alt_char, alt_data;
