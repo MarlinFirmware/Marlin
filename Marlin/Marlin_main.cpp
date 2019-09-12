03383905b9ca879b029ad085a4a6ccf3c410b4fa
Scott Lahteine <github@thinkyhead.com>
2019-09-12 16:45:53 -0500

    Simplify some echos

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2279,12 +2279,15 @@ void clean_up_after_endstop_or_probe_move() {
       #else
         #if ENABLED(DEBUG_LEVELING_FEATURE)
           if (DEBUGGING(LEVELING)) {
-            SERIAL_ECHOLNPAIR("last_written_mode - ", (int)bltouch_last_written_mode);
-            #if ENABLED(BLTOUCH_SET_5V_MODE)
-              SERIAL_ECHOLNPGM("config mode - BLTOUCH_SET_5V_MODE");
-            #else
-              SERIAL_ECHOLNPGM("config mode - OD");
-            #endif
+            SERIAL_ECHOLNPAIR(
+              "last_written_mode - ", int(bltouch_last_written_mode),
+              "config mode - "
+              #if ENABLED(BLTOUCH_SET_5V_MODE)
+                "BLTOUCH_SET_5V_MODE"
+              #else
+                "OD"
+              #endif
+            );
           }
         #endif
 
