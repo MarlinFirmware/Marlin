f2b3a5811dbc2ffa32cbfc5215cb0540da88b2c9
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-09-15 14:18:24 -0500

    Patches to pins

--- Marlin/src/pins/stm32/pins_LERDGE_K.h
+++ Marlin/src/pins/stm32/pins_LERDGE_K.h
@@ -19,84 +19,88 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+#pragma once
 
 #if !defined(STM32F4) && !defined(STM32F4xx)
   #error "Oops! Select an STM32F4 board in 'Tools > Board.'"
+#elif HOTENDS > 2 || E_STEPPERS > 2
+  #error "LERDGE K supports up to 2 hotends / E-steppers."
 #endif
 
+#define BOARD_INFO_NAME      "Lerdge K"
 #define DEFAULT_MACHINE_NAME "LERDGE"
-#define BOARD_NAME "Lerdge K"
 
 #define I2C_EEPROM
 
 // Ignore temp readings during develpment.
 //#define BOGUS_TEMPERATURE_FAILSAFE_OVERRIDE
 
-#if HOTENDS > 2 || E_STEPPERS > 2
-  #error "LERDGE supports up to 2 hotends / E-steppers."
-#endif
+//
+// Servos
+//
+//#define SERVO0_PIN         PD12
 
 //
 // Limit Switches
 //
-#define X_MIN_PIN          PG3
-#define X_MAX_PIN          -1
-#define Y_MIN_PIN          PG4
-#define Y_MAX_PIN          -1
-#define Z_MIN_PIN          PG5
-#define Z_MAX_PIN          -1
+#define X_STOP_PIN         PG3
+#define Y_STOP_PIN         PG4
+#define Z_STOP_PIN         PG5
 
 //
 // Z Probe (when not Z_MIN_PIN)
 //
-
 //#ifndef Z_MIN_PROBE_PIN
 //  #define Z_MIN_PROBE_PIN  PB15
 //#endif
 
 //
+// Filament runout
+//
+#define FIL_RUNOUT_PIN     PE6
+#define FIL_RUNOUT2_PIN    PE7
+
+//
 // Steppers
 //
-
 #define X_STEP_PIN         PG1
 #define X_DIR_PIN          PB10
 #define X_ENABLE_PIN       PG0
 //#ifndef X_CS_PIN
-//  #define X_CS_PIN         PE0-PE4
+//  #define X_CS_PIN       PE0
 //#endif
 
 #define Y_STEP_PIN         PF14
 #define Y_DIR_PIN          PF15
 #define Y_ENABLE_PIN       PF13
 //#ifndef Y_CS_PIN
-//  #define Y_CS_PIN         PE0-PE4
+//  #define Y_CS_PIN       PE1
 //#endif
 
 #define Z_STEP_PIN         PF11
 #define Z_DIR_PIN          PF12
 #define Z_ENABLE_PIN       PC5
 //#ifndef Z_CS_PIN
-//  #define Z_CS_PIN         PE0-PE4
+//  #define Z_CS_PIN       PE2
 //#endif
 
 #define E0_STEP_PIN        PC14
 #define E0_DIR_PIN         PC13
 #define E0_ENABLE_PIN      PC15
 //#ifndef E0_CS_PIN
-//  #define E0_CS_PIN         PE0-PE4
+//  #define E0_CS_PIN      PE3
 //#endif
 
 #define E1_STEP_PIN        PF1
 #define E1_DIR_PIN         PF0
 #define E1_ENABLE_PIN      PF2
 //#ifndef E1_CS_PIN
-//  #define E1_CS_PIN         PE0-PE4
+//  #define E1_CS_PIN      PE4
 //#endif
 
 //
 // Temperature Sensors
 //
-
 #define TEMP_0_PIN         PC1   // Analog Input
 #define TEMP_1_PIN         PC2   // Analog Input
 #define TEMP_BED_PIN       PC0   // Analog Input
@@ -104,7 +108,6 @@
 //
 // Heaters / Fans
 //
-
 #define HEATER_0_PIN       PA1
 #define HEATER_1_PIN       PA0
 #define HEATER_BED_PIN     PA2
@@ -118,48 +121,42 @@
 #define ORIG_E0_AUTO_FAN_PIN  FAN1_PIN // Use this by NOT overriding E0_AUTO_FAN_PIN
 
 //
-// Misc. Functions
+// LED / Lighting
 //
-
 //#define CASE_LIGHT_PIN_CI -1
 //#define CASE_LIGHT_PIN_DO -1
 //#define NEOPIXEL_PIN      -1
 
 //
-// Prusa i3 MK2 Multi Material Multiplexer Support
+// Prusa i3 MK2 Multi-Material Multiplexer Support
 //
-
 //#define E_MUX0_PIN         -1
 //#define E_MUX1_PIN         -1
-
-//
-// Servos
-//
-
-//#define SERVO0_PIN         PD12
-//#define SERVO1_PIN         -1
 
 //
 // SD support
 //
 #define SDIO_SUPPORT
 
+//
+// Misc. Functions
+//
 #define SDSS               PC11
-#define LED_PIN            PC7         //Alive
+#define LED_PIN            PC7   // Alive
 #define PS_ON_PIN          -1
 #define KILL_PIN           -1
-#define PWR_LOSS           -1         //Power loss / nAC_FAULT
+#define POWER_LOSS_PIN     -1    // Power-loss / nAC_FAULT
 
-#define SCK_PIN   PC12
-#define MISO_PIN  PC8
-#define MOSI_PIN  PD2
-#define SS_PIN    PC11
+#define SCK_PIN            PC12
+#define MISO_PIN           PC8
+#define MOSI_PIN           PD2
+#define SS_PIN             PC11
 
 //
 // LCD / Controller
 //
-// These are not right since we need to understand However
-// THE FMC data control works.
+
+// TODO: Replace these with the correct FSMC pins, once known
 #define SD_DETECT_PIN      -1
 #define BEEPER_PIN         PD12
 #define LCD_PINS_RS        -1
@@ -168,16 +165,10 @@
 #define LCD_PINS_D5        -1
 #define LCD_PINS_D6        -1
 #define LCD_PINS_D7        -1
+
 #define BTN_EN1            PE3
 #define BTN_EN2            PE4
 #define BTN_ENC            PE2
-
-//
-// Filament runout
-//
-
-#define FIL_RUNOUT_PIN     PE6
-#define FIL_RUNOUT2_PIN    PE7
 
 //
 // ST7920 Delays
@@ -185,11 +176,9 @@
 #ifndef ST7920_DELAY_1
   #define ST7920_DELAY_1 DELAY_NS(96)
 #endif
-
 #ifndef ST7920_DELAY_2
   #define ST7920_DELAY_2 DELAY_NS(48)
 #endif
-
 #ifndef ST7920_DELAY_3
   #define ST7920_DELAY_3 DELAY_NS(715)
 #endif
