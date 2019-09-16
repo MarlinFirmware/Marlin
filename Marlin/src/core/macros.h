764912b63d4e48bd1e83b83aee70fe8b43683e5f
Marcio Teixeira <marcio@alephobjects.com>
2019-09-16 09:33:25 -0600

    Fix compilation warnings.

--- Marlin/src/core/macros.h
+++ Marlin/src/core/macros.h
@@ -63,7 +63,7 @@
 // Macros for bit masks
 #undef _BV
 #define _BV(n) (1<<(n))
-#define TEST(n,b) !!((n)&_BV(b))
+#define TEST(n,b) (!!((n)&_BV(b)))
 #define SET_BIT_TO(N,B,TF) do{ if (TF) SBI(N,B); else CBI(N,B); }while(0)
 
 #ifndef SBI
