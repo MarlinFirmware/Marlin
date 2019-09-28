b775e826f0b1d02b6439400ba52c871eb878cac4
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-09-28 01:23:49 -0500

    Update babystep.cpp

--- Marlin/src/feature/babystep.cpp
+++ Marlin/src/feature/babystep.cpp
@@ -36,13 +36,10 @@
 Babystep babystep;
 
 volatile int16_t Babystep::steps[BS_TODO_AXIS(Z_AXIS) + 1];
-
-
-int16_t Babystep::accum;
 #if ENABLED(BABYSTEP_DISPLAY_TOTAL)
   int16_t Babystep::axis_total[BS_TOTAL_AXIS(Z_AXIS) + 1];
 #endif
-
+int16_t Babystep::accum;
 
 void Babystep::step_axis(const AxisEnum axis) {
   const int16_t curTodo = steps[BS_TODO_AXIS(axis)]; // get rid of volatile for performance
