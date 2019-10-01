832debe58c6c47f65d7c36dda69602fc7f5b362e
Bill Waldrum <ruredebw@hotmail.com>
2019-10-01 08:31:32 -0700

    fixed merge conflic

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -909,7 +909,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
