d41591cacfed69771f4a24691eb563f45bb902ea
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-10-05 20:38:27 -0500

    Update pins_BIGTREE_SKR_V1.3.h

--- Marlin/src/pins/lpc1768/pins_BIGTREE_SKR_V1.3.h
+++ Marlin/src/pins/lpc1768/pins_BIGTREE_SKR_V1.3.h
@@ -168,22 +168,22 @@
 // Heaters / Fans
 //
 #ifndef HEATER_0_PIN
-  #define HEATER_0_PIN       P2_07
+  #define HEATER_0_PIN     P2_07
 #endif
 #if HOTENDS == 1
   #ifndef FAN1_PIN
-    #define FAN1_PIN         P2_04
+    #define FAN1_PIN       P2_04
   #endif
 #else
   #ifndef HEATER_1_PIN
-    #define HEATER_1_PIN     P2_04
+    #define HEATER_1_PIN   P2_04
   #endif
 #endif
 #ifndef FAN_PIN
-  #define FAN_PIN            P2_03
+  #define FAN_PIN          P2_03
 #endif
 #ifndef HEATER_BED_PIN
-  #define HEATER_BED_PIN     P2_05
+  #define HEATER_BED_PIN   P2_05
 #endif
 
 /**
