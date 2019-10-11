2cf35267c4a9e85e7fecfc58aa7cab3edcabeffc
Roxy-3D <Roxy-3D@users.noreply.github.com>
2019-10-10 19:44:15 -0500

    Set Junction Deviation value from Jerk and Default Acceleration (#15509)
    Formula: `0.4 * sq(min(xjerk, yjerk)) / (default accel)`

--- config/examples/tvrrug/Round2/Configuration.h
+++ config/examples/tvrrug/Round2/Configuration.h
@@ -780,7 +780,7 @@
  *   http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.08  // (mm) Distance from real junction edge
 #endif
 
 /**
