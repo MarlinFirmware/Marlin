6bcd4eb12de93af314765b469fe18525e4849647
milkpirate <milkpirate@users.noreply.github.com>
2019-10-24 19:07:17 +0200

    Fix M420 (UBL) undefined variables (#15621)

--- Marlin/src/gcode/bedlevel/M420.cpp
+++ Marlin/src/gcode/bedlevel/M420.cpp
@@ -64,9 +64,9 @@ void GcodeSuite::M420() {
 
   #if ENABLED(MARLIN_DEV_MODE)
     if (parser.intval('S') == 2) {
+      const float x_min = probe_min_x(), x_max = probe_max_x(),
+                  y_min = probe_min_y(), y_max = probe_max_y();
       #if ENABLED(AUTO_BED_LEVELING_BILINEAR)
-        const float x_min = probe_min_x(), x_max = probe_max_x(),
-                    y_min = probe_min_y(), y_max = probe_max_y();
         bilinear_start.set(x_min, y_min);
         bilinear_grid_spacing.set((x_max - x_min) / (GRID_MAX_POINTS_X - 1),
                                   (y_max - y_min) / (GRID_MAX_POINTS_Y - 1));
