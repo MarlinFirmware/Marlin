12a244f24f063d8f99a1f2341fc83dc38a041fe7
Evgeny Zyatkov <Evg33@users.noreply.github.com>
2019-10-25 20:27:23 +0300

    Fix #endif typo (#15661)

--- Marlin/src/lcd/dogm/status_screen_DOGM.cpp
+++ Marlin/src/lcd/dogm/status_screen_DOGM.cpp
@@ -639,7 +639,7 @@ void MarlinUI::draw_status_screen() {
         #endif
             lcd_put_u8str(elapsed_x_pos, EXTRAS_BASELINE, elapsed_string);
 
-      #else // !DOGM_SD_PERCENT || !SHOW_REMAINING_TIME || !ROTATE_PROGRESS_DISPLAY
+      #endif // !DOGM_SD_PERCENT || !SHOW_REMAINING_TIME || !ROTATE_PROGRESS_DISPLAY
     }
 
   #endif // HAS_PRINT_PROGRESS
