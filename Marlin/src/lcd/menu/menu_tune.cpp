fa5b00e5c0e8f288a23c35c200d68c56554e725e
Marco Munari <mar16+git.yoga3@fastcode.eu>
2019-10-27 14:57:12 +0000

    Don't expect SINGLENOZZLE to have multiple fans E.g. 2-in-1-out head has 1 fan

--- Marlin/src/lcd/menu/menu_tune.cpp
+++ Marlin/src/lcd/menu/menu_tune.cpp
@@ -169,14 +169,14 @@ void menu_tune() {
         EDIT_ITEM_FAST(percent, MSG_FIRST_EXTRA_FAN_SPEED, &thermalManager.new_fan_speed[0], 3, 255);
       #endif
     #endif
-    #if HAS_FAN1 || (ENABLED(SINGLENOZZLE) && EXTRUDERS > 1)
+    #if HAS_FAN1 || (!ENABLED(SINGLENOZZLE) && EXTRUDERS > 1)
       editable.uint8 = thermalManager.fan_speed[1];
       EDIT_ITEM_FAST(percent, MSG_FAN_SPEED_2, &editable.uint8, 0, 255, [](){ thermalManager.set_fan_speed(1, editable.uint8); });
       #if ENABLED(EXTRA_FAN_SPEED)
         EDIT_ITEM_FAST(percent, MSG_EXTRA_FAN_SPEED_2, &thermalManager.new_fan_speed[1], 3, 255);
       #endif
     #endif
-    #if HAS_FAN2 || (ENABLED(SINGLENOZZLE) && EXTRUDERS > 2)
+    #if HAS_FAN2 || (!ENABLED(SINGLENOZZLE) && EXTRUDERS > 2)
       editable.uint8 = thermalManager.fan_speed[2];
       EDIT_ITEM_FAST(percent, MSG_FAN_SPEED_3, &editable.uint8, 0, 255, [](){ thermalManager.set_fan_speed(2, editable.uint8); });
       #if ENABLED(EXTRA_FAN_SPEED)
