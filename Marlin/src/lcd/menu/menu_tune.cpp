26b427009c94725ca341c7dd50459dd31d17dd6c
studiodyne <diabolomix@hotmail.fr>
2019-10-27 02:51:24 +0200

    UP

--- Marlin/src/lcd/menu/menu_tune.cpp
+++ Marlin/src/lcd/menu/menu_tune.cpp
@@ -53,7 +53,7 @@
   void _lcd_babystep(const AxisEnum axis, PGM_P const msg) {
     if (ui.use_click()) return ui.goto_previous_screen_no_defer();
     if (ui.encoderPosition) {
-      const int16_t steps = int16_t(ui.encoderPosition) * (BABYSTEP_MULTIPLICATOR);
+      const int16_t steps = int16_t(ui.encoderPosition) * (axis == Z_AXIS ? BABYSTEP_MULTIPLICATOR_Z : BABYSTEP_MULTIPLICATOR_XY);
       ui.encoderPosition = 0;
       ui.refresh(LCDVIEW_REDRAW_NOW);
       babystep.add_steps(axis, steps);
