b89feeeacf7c09b1a10cd93d5a9c61114e1381b9
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-10-28 16:37:38 -0500

    Tweak formatting

--- Marlin/src/lcd/menu/menu_tune.cpp
+++ Marlin/src/lcd/menu/menu_tune.cpp
@@ -53,12 +53,12 @@
   void _lcd_babystep(const AxisEnum axis, PGM_P const msg) {
     if (ui.use_click()) return ui.goto_previous_screen_no_defer();
     if (ui.encoderPosition) {
-      const int16_t steps = int16_t(ui.encoderPosition) *
+      const int16_t steps = int16_t(ui.encoderPosition) * (
         #if ENABLED(BABYSTEP_XY)
-          (axis == Z_AXIS ? BABYSTEP_MULTIPLICATOR_Z : BABYSTEP_MULTIPLICATOR_XY);
-        #else
-          BABYSTEP_MULTIPLICATOR_Z;
+          axis != Z_AXIS ? BABYSTEP_MULTIPLICATOR_XY :
         #endif
+        BABYSTEP_MULTIPLICATOR_Z
+      );
       ui.encoderPosition = 0;
       ui.refresh(LCDVIEW_REDRAW_NOW);
       babystep.add_steps(axis, steps);
