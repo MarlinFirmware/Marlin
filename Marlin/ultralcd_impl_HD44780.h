75576a60d6565ef6e79d09f24ed4b8d44992136f
Axel <ansepulveda@uc.cl>
2019-10-31 04:16:40 -0300

    viki

--- Marlin/ultralcd_impl_HD44780.h
+++ Marlin/ultralcd_impl_HD44780.h
@@ -1164,9 +1164,14 @@ static void lcd_implementation_status_screen() {
       static uint8_t ledsprev = 0;
       uint8_t leds = 0;
 
+<<<<<<< HEAD
       #if HAS_HEATED_BED
         if (thermalManager.degTargetBed() > 0) leds |= LED_A;
       #endif
+=======
+      if (thermalManager.degTargetBed() > 0) leds |= LED_A;
+
+>>>>>>> parent of f3e7726a8... Compatibility of RAMPS with Viki1 LCD
       if (thermalManager.degTargetHotend(0) > 0) leds |= LED_B;
 
       #if FAN_COUNT > 0
