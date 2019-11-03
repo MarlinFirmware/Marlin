97c5306d010d310ecb15a4896bbd45b7259601e8
ellensp <ellensp@ellensp-HP-ProBook-6470b.fritz.box>
2019-11-04 01:46:53 +1300

    remove env:megaatmega1284 from MKS_GEN_L_V2

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -141,7 +141,7 @@
 #elif MB(RAMPS_ENDER_4)
   #include "pins_RAMPS_ENDER_4.h"     // ATmega2560                                 env:megaatmega2560
 #elif MB(MKS_GEN_L_V2)
-  #include "pins_MKS_GEN_L_V2.h"      // ATmega2560                                 env:megaatmega1280 env:megaatmega2560
+  #include "pins_MKS_GEN_L_V2.h"      // ATmega2560                                 env:megaatmega2560
 
 //
 // Other ATmega1280, ATmega2560
