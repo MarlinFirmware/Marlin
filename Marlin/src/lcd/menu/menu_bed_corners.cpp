51fd226a4dabcb9ecee7173121852aba388bb5c8
Scott Lahteine <github@thinkyhead.com>
2019-11-04 15:40:56 -0600

    Unconfuse intellisense

--- Marlin/src/lcd/menu/menu_bed_corners.cpp
+++ Marlin/src/lcd/menu/menu_bed_corners.cpp
@@ -97,13 +97,13 @@ static inline void _lcd_level_bed_corners_homing() {
         #endif
         ui.goto_previous_screen_no_defer();
       },
-      GET_TEXT(
+      GET_TEXT((
         #if ENABLED(LEVEL_CENTER_TOO)
           MSG_LEVEL_BED_NEXT_POINT
         #else
           MSG_NEXT_CORNER
         #endif
-      ), (PGM_P)nullptr, PSTR("?")
+      )), (PGM_P)nullptr, PSTR("?")
     );
     ui.set_selection(true);
     _lcd_goto_next_corner();
