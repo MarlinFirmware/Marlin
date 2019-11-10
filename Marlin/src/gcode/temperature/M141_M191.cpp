53fb16ef31a1555a2551cd0d81b622b312f63f7b
ellensp <ellensp@ellensp-HP-ProBook-6470b.fritz.box>
2019-11-11 00:22:58 +1300

    Fix issue 15861

--- Marlin/src/gcode/temperature/M141_M191.cpp
+++ Marlin/src/gcode/temperature/M141_M191.cpp
@@ -59,7 +59,7 @@ void GcodeSuite::M191() {
   if (no_wait_for_cooling || parser.seenval('R')) {
     thermalManager.setTargetChamber(parser.value_celsius());
     #if ENABLED(PRINTJOB_TIMER_AUTOSTART)
-      if (parser.value_celsius() > BED_MINTEMP)
+      if (parser.value_celsius() > CHAMBER_MINTEMP)
         startOrResumeJob();
     #endif
   }
@@ -67,7 +67,7 @@ void GcodeSuite::M191() {
 
   const bool is_heating = thermalManager.isHeatingChamber();
   if (is_heating || !no_wait_for_cooling) {
-    lcd_setstatusPGM(is_heating ? GET_TEXT(MSG_CHAMBER_HEATING) : GET_TEXT(MSG_CHAMBER_COOLING));
+    ui.set_status_P(is_heating ? GET_TEXT(MSG_CHAMBER_HEATING) : GET_TEXT(MSG_CHAMBER_COOLING));
     thermalManager.wait_for_chamber(false);
   }
 }
