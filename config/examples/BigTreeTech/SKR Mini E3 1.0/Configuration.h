7cfbcb0412648a0b45a6efac7c5d0625997af70a
thisiskeithb <thisiskeithb@outlook.com>
2019-11-19 15:15:01 -0800

    Enable LCD Bed Leveling

--- config/examples/BigTreeTech/SKR Mini E3 1.0/Configuration.h
+++ config/examples/BigTreeTech/SKR Mini E3 1.0/Configuration.h
@@ -1257,7 +1257,7 @@
  * Add a bed leveling sub-menu for ABL or MBL.
  * Include a guided procedure if manual probing is enabled.
  */
-//#define LCD_BED_LEVELING
+#define LCD_BED_LEVELING
 
 #if ENABLED(LCD_BED_LEVELING)
   #define MESH_EDIT_Z_STEP  0.025 // (mm) Step size while manually probing Z axis.
