bce4d5f373362d25ae7d1a873c55ac9a27ccfa12
thisiskeithb <thisiskeithb@outlook.com>
2019-11-19 15:14:21 -0800

    Disable NOZZLE_* Features

--- config/examples/BigTreeTech/SKR Mini E3 1.2/Configuration.h
+++ config/examples/BigTreeTech/SKR Mini E3 1.2/Configuration.h
@@ -1444,7 +1444,7 @@
  *    P1  Raise the nozzle always to Z-park height.
  *    P2  Raise the nozzle by Z-park amount, limited to Z_MAX_POS.
  */
-#define NOZZLE_PARK_FEATURE
+//#define NOZZLE_PARK_FEATURE
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z_raise }
@@ -1491,7 +1491,7 @@
  * Attention: EXPERIMENTAL. G-code arguments may change.
  *
  */
-#define NOZZLE_CLEAN_FEATURE
+//#define NOZZLE_CLEAN_FEATURE
 
 #if ENABLED(NOZZLE_CLEAN_FEATURE)
   // Default number of pattern repetitions
