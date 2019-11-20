8b295e46ec7ff6b96a32d4b44455117dc8679bb4
Scott Lahteine <github@thinkyhead.com>
2019-11-20 17:02:08 -0600

    Add BACKUP_POWER_SUPPLY to examples

--- config/examples/Formbot/T_Rex_3/Configuration_adv.h
+++ config/examples/Formbot/T_Rex_3/Configuration_adv.h
@@ -964,6 +964,8 @@
    */
   //#define POWER_LOSS_RECOVERY
   #if ENABLED(POWER_LOSS_RECOVERY)
+    //#define BACKUP_POWER_SUPPLY       // Backup power / UPS to move the steppers on power loss
+    //#define POWER_LOSS_ZRAISE       2 // (mm) Z axis raise on resume (on power loss with UPS)
     //#define POWER_LOSS_PIN         44 // Pin to detect power loss
     //#define POWER_LOSS_STATE     HIGH // State of pin indicating power loss
     //#define POWER_LOSS_PULL           // Set pullup / pulldown as appropriate
