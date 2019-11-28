1a6f7a8d27eb8ac4b391e2e3e5f592b7dcdc8b45
Christopher Pepper <p3p@p3psoft.co.uk>
2019-11-28 00:33:04 +0000

    Sanity check ADC pins and FAST_PWM_FAN used with Servos

--- Marlin/src/HAL/HAL_LPC1768/inc/SanityCheck.h
+++ Marlin/src/HAL/HAL_LPC1768/inc/SanityCheck.h
@@ -29,6 +29,27 @@
 #endif
 
 /**
+ *  Try to detect users copying over old pins file by checking if adc pins are defined to old values that are not ADC compatible pins
+ */
+#if TEMP_BED_PIN == 0 ||TEMP_BED_PIN == 1 ||TEMP_BED_PIN == 4 || TEMP_BED_PIN == 5 || TEMP_BED_PIN == 6 || TEMP_BED_PIN == 7
+  #error "Please update your pins file, ADC pin definitions should now use the Pn_nn_An (or Pn_nn) format as used in the default pins files"
+#endif
+#if TEMP_0_PIN == 0 ||TEMP_0_PIN == 1 ||TEMP_0_PIN == 4 || TEMP_0_PIN == 5 || TEMP_0_PIN == 6 || TEMP_0_PIN == 7
+  #error "Please update your pins file, ADC pin definitions should now use the Pn_nn_An (or Pn_nn) format as used in the default pins files"
+#endif
+#if TEMP_1_PIN == 0 ||TEMP_1_PIN == 1 ||TEMP_1_PIN == 4 || TEMP_1_PIN == 5 || TEMP_1_PIN == 6 || TEMP_1_PIN == 7
+  #error "Please update your pins file, ADC pin definitions should now use the Pn_nn_An (or Pn_nn) format as used in the default pins files"
+#endif
+
+/**
+ *  Due to the way the PWM hardware channels all share the same frequency, as do the software fallback
+ *  channels, FAST_PWM_FAN is incompatible with Servos
+ */
+#if NUM_SERVOS > 0 && ENABLED(FAST_PWM_FAN)
+   #error "Servos and BLTouch are incompatible with FAST_PWM_FAN on LPC176x boards"
+#endif
+
+/**
  * Test LPC176x-specific configuration values for errors at compile-time.
  */
 
