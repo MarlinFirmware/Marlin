e585c37d7dfa043ca8707604942184819fc576e4
Chris Pepper <p3p@p3psoft.co.uk>
2019-11-29 16:44:11 +0000

    Fix #16038

--- Marlin/src/HAL/HAL_LPC1768/inc/SanityCheck.h
+++ Marlin/src/HAL/HAL_LPC1768/inc/SanityCheck.h
@@ -31,7 +31,7 @@
 /**
  * Detect an old pins file by checking for old ADC pins values.
  */
-#define _OLD_TEMP_PIN(P) PIN_EXISTS(TEMP_BED) && _CAT(P,_PIN) <= 7 && _CAT(P,_PIN) != 2 && _CAT(P,_PIN) != 3
+#define _OLD_TEMP_PIN(P) PIN_EXISTS(P) && _CAT(P,_PIN) <= 7 && _CAT(P,_PIN) != 2 && _CAT(P,_PIN) != 3
 #if _OLD_TEMP_PIN(TEMP_BED)
   #error "TEMP_BED_PIN must be defined using the Pn_nn or Pn_nn_An format. (See the included pins files)."
 #elif _OLD_TEMP_PIN(TEMP_0)
