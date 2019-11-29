9c6ecaf1ed2f6e3ef8c593095fdfc16b8d6b7933
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-11-29 02:16:22 -0600

    Update SanityCheck.h

--- Marlin/src/HAL/HAL_LPC1768/inc/SanityCheck.h
+++ Marlin/src/HAL/HAL_LPC1768/inc/SanityCheck.h
@@ -29,24 +29,30 @@
 #endif
 
 /**
- *  Try to detect users copying over old pins file by checking if adc pins are defined to old values that are not ADC compatible pins
+ * Detect an old pins file by checking for old ADC pins values.
  */
-#if TEMP_BED_PIN == 0 ||TEMP_BED_PIN == 1 ||TEMP_BED_PIN == 4 || TEMP_BED_PIN == 5 || TEMP_BED_PIN == 6 || TEMP_BED_PIN == 7
-  #error "Please update your pins file, ADC pin definitions should now use the Pn_nn_An (or Pn_nn) format as used in the default pins files"
-#endif
-#if TEMP_0_PIN == 0 ||TEMP_0_PIN == 1 ||TEMP_0_PIN == 4 || TEMP_0_PIN == 5 || TEMP_0_PIN == 6 || TEMP_0_PIN == 7
-  #error "Please update your pins file, ADC pin definitions should now use the Pn_nn_An (or Pn_nn) format as used in the default pins files"
-#endif
-#if TEMP_1_PIN == 0 ||TEMP_1_PIN == 1 ||TEMP_1_PIN == 4 || TEMP_1_PIN == 5 || TEMP_1_PIN == 6 || TEMP_1_PIN == 7
-  #error "Please update your pins file, ADC pin definitions should now use the Pn_nn_An (or Pn_nn) format as used in the default pins files"
+#if PIN_EXISTS(TEMP_BED) && TEMP_BED_PIN <= 7
+  #error "TEMP_BED_PIN must be defined using the Pn_nn or Pn_nn_An format. (See the included pins files)."
+#elif PIN_EXISTS(TEMP_0) && TEMP_0_PIN <= 7
+  #error "TEMP_0_PIN must be defined using the Pn_nn or Pn_nn_An format. (See the included pins files)."
+#elif PIN_EXISTS(TEMP_1) && TEMP_1_PIN <= 7
+  #error "TEMP_1_PIN must be defined using the Pn_nn or Pn_nn_An format. (See the included pins files)."
+#elif PIN_EXISTS(TEMP_2) && TEMP_2_PIN <= 7
+  #error "TEMP_2_PIN must be defined using the Pn_nn or Pn_nn_An format. (See the included pins files)."
+#elif PIN_EXISTS(TEMP_3) && TEMP_3_PIN <= 7
+  #error "TEMP_3_PIN must be defined using the Pn_nn or Pn_nn_An format. (See the included pins files)."
+#elif PIN_EXISTS(TEMP_4) && TEMP_4_PIN <= 7
+  #error "TEMP_4_PIN must be defined using the Pn_nn or Pn_nn_An format. (See the included pins files)."
+#elif PIN_EXISTS(TEMP_5) && TEMP_5_PIN <= 7
+  #error "TEMP_5_PIN must be defined using the Pn_nn or Pn_nn_An format. (See the included pins files)."
 #endif
 
 /**
- *  Due to the way the PWM hardware channels all share the same frequency, as do the software fallback
- *  channels, FAST_PWM_FAN is incompatible with Servos
+ * Because PWM hardware channels all share the same frequency, along with the
+ * fallback software channels, FAST_PWM_FAN is incompatible with Servos.
  */
 #if NUM_SERVOS > 0 && ENABLED(FAST_PWM_FAN)
-   #error "Servos and BLTouch are incompatible with FAST_PWM_FAN on LPC176x boards"
+  #error "Servos and BLTouch are incompatible with FAST_PWM_FAN on LPC176x boards"
 #endif
 
 /**
