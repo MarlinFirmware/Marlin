63d2fae6cc2a7e9ee993d72ea433922c6ae5b0d7
Scott Lahteine <github@thinkyhead.com>
2019-12-01 13:56:18 -0600

    Release version 2.0.0

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "bugfix-2.0.x"
+  #define SHORT_BUILD_VERSION "2.0.x"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2019-11-30"
+  #define STRING_DISTRIBUTION_DATE "2019-12-01"
 #endif
 
 /**
