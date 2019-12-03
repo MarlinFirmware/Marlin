bb1d1b3ec5ec1cb0ba19cf6386b217021965895f
Erkan Colak <erkanc@gmx.de>
2019-12-03 08:29:36 +0100

    v16 Testing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -138,8 +138,8 @@
 
   // define here your custom 4MAX. ATTENTION: ONLY ONE IS TO BE DEFINE!
   //#define ANYCUBIC_4MAX_VG3R
-  #define ANYCUBIC_4MAX_7OF9
-  //#define ANYCUBIC_4MAX_DEFAULT
+  //#define ANYCUBIC_4MAX_7OF9
+  #define ANYCUBIC_4MAX_DEFAULT
 
 // TMC2208 with UART where TX is on SERVO0_PIN and RX is on PS_ON_PIN
 // Info: Just for Testing.
