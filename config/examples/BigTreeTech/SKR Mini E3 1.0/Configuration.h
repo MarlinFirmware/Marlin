5ecce3e6ac338cdc75fd753290ed7bec8b43ee67
Jason Smith <jason.inet@gmail.com>
2019-12-04 13:35:32 -0800

    Disable PRINTCOUNTER in SKR Mini E3 examples.

--- config/examples/BigTreeTech/SKR Mini E3 1.0/Configuration.h
+++ config/examples/BigTreeTech/SKR Mini E3 1.0/Configuration.h
@@ -1549,7 +1549,7 @@
  *
  * View the current statistics with M78.
  */
-#define PRINTCOUNTER
+//#define PRINTCOUNTER
 
 //=============================================================================
 //============================= LCD and SD support ============================
