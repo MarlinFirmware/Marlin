cd30de83b51cda7ec0ce1fb4649a5038d680d359
FNeo31 <37299777+FNeo31@users.noreply.github.com>
2019-12-04 14:55:55 +0000

    add drilling cycles option

--- Marlin/src/gcode/gcode.cpp
+++ Marlin/src/gcode/gcode.cpp
@@ -322,6 +322,12 @@ void GcodeSuite::process_parsed_command(const bool no_ok/*=false*/) {
       #if ENABLED(GCODE_MOTION_MODES)
         case 80: G80(); break;                                    // G80: Reset the current motion mode
       #endif
+        
+      #if ENABLED(DRILLING_CANNED_CYCLES)
+        case 81: G81(); break;
+        case 82: G82(); break;  
+        case 83: G83(); break; 
+      #endif
 
       case 90: set_relative_mode(false); break;                   // G90: Absolute Mode
       case 91: set_relative_mode(true);  break;                   // G91: Relative Mode
@@ -534,6 +540,10 @@ void GcodeSuite::process_parsed_command(const bool no_ok/*=false*/) {
         #if ENABLED(GRADIENT_MIX)
           case 166: M166(); break;                                // M166: Set Gradient Mix
         #endif
+      #endif
+        
+      #if ENABLED(CNC_5X)
+            case 168: M168(); break;                                
       #endif
 
       #if DISABLED(NO_VOLUMETRICS)
