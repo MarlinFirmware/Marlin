5de5eff8656df00f1a12c43987960fa5d4c47263
Scott Lahteine <github@thinkyhead.com>
2019-12-08 00:35:42 -0600

    Update language.h

--- Marlin/language.h
+++ Marlin/language.h
@@ -68,7 +68,6 @@
 // it         Italian
 // kana       Japanese
 // kana_utf8  Japanese (UTF8)
-// ko_kr      Korean
 // nl         Dutch
 // pl         Polish
 // pl_utf8    Polish (UTF8)
@@ -322,7 +321,6 @@
  && DISABLED(DISPLAY_CHARSET_ISO10646_1) \
  && DISABLED(DISPLAY_CHARSET_ISO10646_5) \
  && DISABLED(DISPLAY_CHARSET_ISO10646_KANA) \
- && DISABLED(DISPLAY_CHARSET_ISO10646_KO_KR) \
  && DISABLED(DISPLAY_CHARSET_ISO10646_GREEK) \
  && DISABLED(DISPLAY_CHARSET_ISO10646_CN) \
  && DISABLED(DISPLAY_CHARSET_ISO10646_TR) \
