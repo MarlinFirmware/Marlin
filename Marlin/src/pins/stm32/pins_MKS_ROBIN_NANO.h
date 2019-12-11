7aea1d546330ea6b0e689fc9fb2ee463733ab726
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-12-10 23:58:55 -0600

    Update pins_MKS_ROBIN_NANO.h

--- Marlin/src/pins/stm32/pins_MKS_ROBIN_NANO.h
+++ Marlin/src/pins/stm32/pins_MKS_ROBIN_NANO.h
@@ -123,7 +123,7 @@
   #define FSMC_CS_PIN        PD7    // NE4
   #define FSMC_RS_PIN        PD11   // A0
 
-  #define LCD_RESET_PIN      PC6 //should be PC6
+  #define LCD_RESET_PIN      PC6
   #define NO_LCD_REINIT             // Suppress LCD re-initialization
 
   #define LCD_BACKLIGHT_PIN  PD13
