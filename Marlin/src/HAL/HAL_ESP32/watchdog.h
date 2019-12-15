9d6b2ebf50415ac04ce438a58f62319a6c010b7c
Luc <8822552+luc-github@users.noreply.github.com>
2019-12-15 18:39:39 +0100

    Improve ESP32 HAL (EEPROM, watchdog) (#16228)

--- Marlin/src/HAL/HAL_ESP32/watchdog.h
+++ Marlin/src/HAL/HAL_ESP32/watchdog.h
@@ -21,8 +21,18 @@
  */
 #pragma once
 
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+    esp_err_t esp_task_wdt_reset();
+
+#ifdef __cplusplus
+  }
+#endif
+
 // Initialize watchdog with a 4 second interrupt time
 void watchdog_init();
 
 // Reset watchdog.
-inline void HAL_watchdog_refresh() {}
+inline void HAL_watchdog_refresh() { esp_task_wdt_reset(); }
