4a6f625f86e787ffe9c8c2b3c1365696b3af888c
Scott Lahteine <github@thinkyhead.com>
2019-12-15 20:56:10 -0600

    Tweak E180 config

--- config/examples/Geeetech/E180/Configuration.h
+++ config/examples/Geeetech/E180/Configuration.h
@@ -516,6 +516,12 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
+  //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
+  //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
+
   // FIND YOUR OWN: "M303 E-1 C8 S60"
 #endif // PIDTEMPBED
 
@@ -1083,7 +1089,7 @@
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define NUM_RUNOUT_SENSORS   1     // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
   #define FIL_RUNOUT_PIN      66
-  #define FIL_RUNOUT_INVERTING true  // set to true to invert the logic of the sensors. - some of the geeetech filament sensors are false
+  #define FIL_RUNOUT_INVERTING true  // Set to true to invert the logic of the sensors. (Some Geeetech filament sensors are false)
   #define FIL_RUNOUT_PULLUP          // Use internal pullup for filament runout pins.
   //#define FIL_RUNOUT_PULLDOWN      // Use internal pulldown for filament runout pins.
 
