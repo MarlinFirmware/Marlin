3f5aacb3135954662fe53648aaf994b3ade00809
Vertabreaker <opyrus@hotmail.com>
2019-12-20 07:29:48 -0500

    Add some new configs

--- Marlin/src/lcd/menu/menu_custom.cpp
+++ Marlin/src/lcd/menu/menu_custom.cpp
@@ -65,6 +65,66 @@ void menu_user() {
   #if defined(USER_DESC_5) && defined(USER_GCODE_5)
     ACTION_ITEM_P(PSTR(USER_DESC_5), []{ _lcd_user_gcode(PSTR(USER_GCODE_5 _DONE_SCRIPT)); });
   #endif
+  #if defined(USER_DESC_6) && defined(USER_GCODE_6)
+    ACTION_ITEM_P(PSTR(USER_DESC_6), []{ _lcd_user_gcode(PSTR(USER_GCODE_6 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_7) && defined(USER_GCODE_7)
+    ACTION_ITEM_P(PSTR(USER_DESC_7), []{ _lcd_user_gcode(PSTR(USER_GCODE_7 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_8) && defined(USER_GCODE_8)
+    ACTION_ITEM_P(PSTR(USER_DESC_8), []{ _lcd_user_gcode(PSTR(USER_GCODE_8 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_9) && defined(USER_GCODE_9)
+    ACTION_ITEM_P(PSTR(USER_DESC_9), []{ _lcd_user_gcode(PSTR(USER_GCODE_9 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_10) && defined(USER_GCODE_10)
+    ACTION_ITEM_P(PSTR(USER_DESC_10), []{ _lcd_user_gcode(PSTR(USER_GCODE_10 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_11) && defined(USER_GCODE_11)
+    ACTION_ITEM_P(PSTR(USER_DESC_11), []{ _lcd_user_gcode(PSTR(USER_GCODE_11 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_12) && defined(USER_GCODE_12)
+    ACTION_ITEM_P(PSTR(USER_DESC_12), []{ _lcd_user_gcode(PSTR(USER_GCODE_12 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_13) && defined(USER_GCODE_13)
+    ACTION_ITEM_P(PSTR(USER_DESC_13), []{ _lcd_user_gcode(PSTR(USER_GCODE_13 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_14) && defined(USER_GCODE_14)
+    ACTION_ITEM_P(PSTR(USER_DESC_14), []{ _lcd_user_gcode(PSTR(USER_GCODE_14 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_15) && defined(USER_GCODE_15)
+    ACTION_ITEM_P(PSTR(USER_DESC_15), []{ _lcd_user_gcode(PSTR(USER_GCODE_15 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_16) && defined(USER_GCODE_16)
+    ACTION_ITEM_P(PSTR(USER_DESC_16), []{ _lcd_user_gcode(PSTR(USER_GCODE_16 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_17) && defined(USER_GCODE_17)
+    ACTION_ITEM_P(PSTR(USER_DESC_17), []{ _lcd_user_gcode(PSTR(USER_GCODE_17 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_18) && defined(USER_GCODE_18)
+    ACTION_ITEM_P(PSTR(USER_DESC_18), []{ _lcd_user_gcode(PSTR(USER_GCODE_18 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_19) && defined(USER_GCODE_19)
+    ACTION_ITEM_P(PSTR(USER_DESC_19), []{ _lcd_user_gcode(PSTR(USER_GCODE_19 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_20) && defined(USER_GCODE_20)
+    ACTION_ITEM_P(PSTR(USER_DESC_20), []{ _lcd_user_gcode(PSTR(USER_GCODE_20 _DONE_SCRIPT)); });
+  #endif
+    #if defined(USER_DESC_21) && defined(USER_GCODE_21)
+    ACTION_ITEM_P(PSTR(USER_DESC_21), []{ _lcd_user_gcode(PSTR(USER_GCODE_21 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_22) && defined(USER_GCODE_22)
+    ACTION_ITEM_P(PSTR(USER_DESC_22), []{ _lcd_user_gcode(PSTR(USER_GCODE_22 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_23) && defined(USER_GCODE_23)
+    ACTION_ITEM_P(PSTR(USER_DESC_23), []{ _lcd_user_gcode(PSTR(USER_GCODE_23 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_24) && defined(USER_GCODE_24)
+    ACTION_ITEM_P(PSTR(USER_DESC_24), []{ _lcd_user_gcode(PSTR(USER_GCODE_24 _DONE_SCRIPT)); });
+  #endif
+  #if defined(USER_DESC_25) && defined(USER_GCODE_25)
+    ACTION_ITEM_P(PSTR(USER_DESC_25), []{ _lcd_user_gcode(PSTR(USER_GCODE_25 _DONE_SCRIPT)); });
+  #endif
   END_MENU();
 }
 
