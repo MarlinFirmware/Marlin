a3d88aebed987d76231eecac6e5c72702d499000
Luc <8822552+luc-github@users.noreply.github.com>
2019-12-22 01:58:00 +0100

    Add MRR_ESPA/_ESPE (ESP32) boards (#16238)

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -553,6 +553,10 @@
 #elif MB(ESPRESSIF_ESP32)
 
   #include "esp32/pins_ESP32.h"                 // ESP32                                  env:esp32
+#elif MB(MRR_ESPA)
+  #include "esp32/pins_MRR_ESPA.h"              // ESP32                                  env:esp32
+#elif MB(MRR_ESPE)
+  #include "esp32/pins_MRR_ESPE.h"              // ESP32                                  env:esp32
 
 //
 // Linux Native Debug board
