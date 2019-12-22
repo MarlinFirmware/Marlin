ae71631fb68095dadbfbba222215ca7e163971f2
Scott Lahteine <thinkyhead@users.noreply.github.com>
2019-12-21 18:55:18 -0600

    Update pins_MRR_ESPE.h

--- Marlin/src/pins/esp32/pins_MRR_ESPE.h
+++ Marlin/src/pins/esp32/pins_MRR_ESPE.h
@@ -30,81 +30,82 @@
  */
 
 #ifndef ARDUINO_ARCH_ESP32
-  "Oops! Select an ESP32 board in 'Tools > Board.'"
+  #error "Oops! Select an ESP32 board in 'Tools > Board.'"
+#elif EXTRUDERS > 2 || E_STEPPERS > 2
+  #error "MRR ESPE only supports two E Steppers. Comment out this line to continue."
+#elif HOTENDS > 1
+  #error "MRR ESPE currently supports only one hotend. Comment out this line to continue."
 #endif
 
-#define BOARD_INFO_NAME "MRR ESPE"
-#define BOARD_WEBSITE_URL "github.com/maplerainresearch/MRR_ESPE"
+#define BOARD_INFO_NAME      "MRR ESPE"
+#define BOARD_WEBSITE_URL    "github.com/maplerainresearch/MRR_ESPE"
 #define DEFAULT_MACHINE_NAME BOARD_INFO_NAME
 
 //
 // Limit Switches
 //
-#define X_MIN_PIN          35
-#define Y_MIN_PIN          32
-#define Z_MIN_PIN          33
+#define X_STOP_PIN         35
+#define Y_STOP_PIN         32
+#define Z_STOP_PIN         33
 
 //
 // Enable I2S stepper stream
 //
-#ifndef I2S_STEPPER_STREAM
-  #define I2S_STEPPER_STREAM
-#endif
+#undef I2S_STEPPER_STREAM
+#define I2S_STEPPER_STREAM
 
-#define I2S_WS              26
-#define I2S_BCK             25
-#define I2S_DATA            27
+#undef LIN_ADVANCE  // Currently, I2S stream does not work with linear advance
 
-#ifdef LIN_ADVANCE
-  #undef LIN_ADVANCE  // Currently, I2S stream does not work with linear advance
-#endif
+#define I2S_WS             26
+#define I2S_BCK            25
+#define I2S_DATA           27
 
 //
 // Steppers
 //
-#define X_STEP_PIN         129
-#define X_DIR_PIN          130
-#define X_ENABLE_PIN       128
+#define X_STEP_PIN        129
+#define X_DIR_PIN         130
+#define X_ENABLE_PIN      128
 //#define X_CS_PIN           21
 
-#define Y_STEP_PIN         132
-#define Y_DIR_PIN          133
-#define Y_ENABLE_PIN       131
+#define Y_STEP_PIN        132
+#define Y_DIR_PIN         133
+#define Y_ENABLE_PIN      131
 //#define Y_CS_PIN           22
 
-#define Z_STEP_PIN         135
-#define Z_DIR_PIN          136
-#define Z_ENABLE_PIN       134
+#define Z_STEP_PIN        135
+#define Z_DIR_PIN         136
+#define Z_ENABLE_PIN      134
 //#define Z_CS_PIN            5 // SS_PIN
 
-#define E0_STEP_PIN        138
-#define E0_DIR_PIN         139
-#define E0_ENABLE_PIN      137
+#define E0_STEP_PIN       138
+#define E0_DIR_PIN        139
+#define E0_ENABLE_PIN     137
 //#define E0_CS_PIN          21
 
-#define E1_STEP_PIN        141
-#define E2_DIR_PIN         142
-#define E3_ENABLE_PIN      140
-//#define E0_CS_PIN          22
+#define E1_STEP_PIN       141
+#define E1_DIR_PIN        142
+#define E1_ENABLE_PIN     140
+//#define E1_CS_PIN          22
 
-#define Z2_STEP_PIN        141
-#define Z2_DIR_PIN         142
-#define Z2_ENABLE_PIN      140
+#define Z2_STEP_PIN       141
+#define Z2_DIR_PIN        142
+#define Z2_ENABLE_PIN     140
 //#define Z2_CS_PIN            5
 
 //
 // Temperature Sensors
 //
 #define TEMP_0_PIN         36   // Analog Input
+#define TEMP_1_PIN         34   // Analog Input
 #define TEMP_BED_PIN       39   // Analog Input
-#define TEMP_1_PIN         34
 
 //
 // Heaters / Fans
 //
-#define HEATER_0_PIN     145 // 2
-#define FAN_PIN          146 // 15
-#define HEATER_BED_PIN   144 // 4
+#define HEATER_0_PIN      145 // 2
+#define FAN_PIN           146 // 15
+#define HEATER_BED_PIN    144 // 4
 
 #define CONTROLLER_FAN_PIN 147
 //#define E0_AUTO_FAN_PIN 148 // need to update Configuration_adv.h @section extruder 
@@ -123,38 +124,32 @@
 // LCDs and Controllers //
 //////////////////////////
 
+#if HAS_GRAPHICAL_LCD
 
-//
-// LCD Display output pins
-//
-#if ENABLED(CR10_STOCKDISPLAY)
-  #define LCD_PINS_RS         13
-  #define LCD_PINS_ENABLE     17
-  #define LCD_PINS_D4         16
-  #define BEEPER_PIN          151
+  #define LCD_PINS_RS        13
+  #define LCD_PINS_ENABLE    17
+  #define LCD_PINS_D4        16
 
-#elif ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-  #define LCD_PINS_RS         13
-  #define LCD_PINS_ENABLE     17
-  #define LCD_PINS_D4         16
-  //#define LCD_PINS_D5         150
-  //#define LCD_PINS_D6         151
-  //#define LCD_PINS_D7         153
-  #define BEEPER_PIN          152
+  #if ENABLED(CR10_STOCKDISPLAY)
 
-#endif
+    #define BEEPER_PIN      151
 
-//
-// LCD Display input pins
-//
-#if ENABLED(CR10_STOCKDISPLAY)
+  #elif ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
+
+    #define BEEPER_PIN      152
+
+    //#define LCD_PINS_D5     150
+    //#define LCD_PINS_D6     151
+    //#define LCD_PINS_D7     153
+
+  #else
+
+    #error "Only CR10_STOCKDISPLAY and REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER are currently supported. Comment out this line to continue."
+
+  #endif
+
   #define BTN_EN1             0
-  #define BTN_EN2             12
-  #define BTN_ENC             14
+  #define BTN_EN2            12
+  #define BTN_ENC            14
 
-#elif ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-  #define BTN_EN1             0
-  #define BTN_EN2             12
-  #define BTN_ENC             14
-  
-#endif
+#endif // HAS_GRAPHICAL_LCD
