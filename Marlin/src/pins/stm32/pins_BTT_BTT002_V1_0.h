74e2a37dbd4243d3c6bdb2ec015d87444fe8bfdf
thisiskeithb <thisiskeithb@outlook.com>
2020-01-02 13:48:27 -0800

    Add filament runout, PLR, RGB pins

--- Marlin/src/pins/stm32/pins_BTT_BTT002_V1_0.h
+++ Marlin/src/pins/stm32/pins_BTT_BTT002_V1_0.h
@@ -57,6 +57,20 @@
 #endif
 
 //
+// Filament Runout Sensor
+//
+#ifndef FIL_RUNOUT_PIN
+  #define FIL_RUNOUT_PIN   PA15
+#endif
+
+//
+// Power Loss Detection
+//
+#ifndef POWER_LOSS_PIN
+ #define POWER_LOSS_PIN    PD4
+#endif
+
+//
 // Steppers
 //
 #define X_STEP_PIN         PA9
@@ -226,3 +240,19 @@
   #endif
 
 #endif // HAS_SPI_LCD
+
+//
+// RGB LEDs
+//
+#ifndef RGB_LED_R_PIN
+  #define RGB_LED_R_PIN    PB5
+#endif
+#ifndef RGB_LED_G_PIN
+  #define RGB_LED_G_PIN    PB4
+#endif
+#ifndef RGB_LED_B_PIN
+  #define RGB_LED_B_PIN    PB3
+#endif
+#ifndef RGB_LED_W_PIN
+  #define RGB_LED_W_PIN    -1
+#endif
