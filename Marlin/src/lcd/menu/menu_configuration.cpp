c75b5606044d59d1da041090e91d876a25440aad
randellhodges <rhodges@taxfodder.com>
2020-01-02 17:50:19 -0600

    PWM pin not needed for Neopixel brightness / submenu (#16345)

--- Marlin/src/lcd/menu/menu_configuration.cpp
+++ Marlin/src/lcd/menu/menu_configuration.cpp
@@ -358,7 +358,11 @@ void menu_configuration() {
   //
   #if ENABLED(CASE_LIGHT_MENU)
     #if DISABLED(CASE_LIGHT_NO_BRIGHTNESS)
-      if (PWM_PIN(CASE_LIGHT_PIN))
+      if (true
+        #if DISABLED(CASE_LIGHT_USE_NEOPIXEL)
+          && PWM_PIN(CASE_LIGHT_PIN)
+        #endif
+      )
         SUBMENU(MSG_CASE_LIGHT, menu_case_light);
       else
     #endif
