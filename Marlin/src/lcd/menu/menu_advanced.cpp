bc7657f26fa65a8159000b58d5e763a32a7e6aaa
InsanityAutomation <d.menzel@insanityautomation.com>
2020-01-03 12:03:52 -0500

    Add menu for adjustment of Probe Offsets
    Update menu_advanced.cpp
    
    Update menu_advanced.cpp
    
    Update menu_advanced.cpp

--- Marlin/src/lcd/menu/menu_advanced.cpp
+++ Marlin/src/lcd/menu/menu_advanced.cpp
@@ -509,6 +509,19 @@ void menu_cancelobject();
     END_MENU();
   }
 
+  #if HAS_BED_PROBE
+    void menu_probe_offsets() {
+      START_MENU();
+      BACK_ITEM(MSG_ADVANCED_SETTINGS);
+
+      EDIT_ITEM(float51, MSG_ZPROBE_XOFFSET, &probe_offset.x, -(X_BED_SIZE), X_BED_SIZE);
+      EDIT_ITEM(float51, MSG_ZPROBE_YOFFSET, &probe_offset.y, -(Y_BED_SIZE), Y_BED_SIZE);
+      EDIT_ITEM(LCD_Z_OFFSET_TYPE, MSG_ZPROBE_ZOFFSET, &probe_offset.z, Z_PROBE_OFFSET_RANGE_MIN, Z_PROBE_OFFSET_RANGE_MAX);
+
+      END_MENU();
+    }
+  #endif
+
 #endif // !SLIM_LCD_MENUS
 
 void menu_advanced_settings() {
@@ -539,6 +552,11 @@ void menu_advanced_settings() {
     if (!printer_busy()) {
       // M92 - Steps Per mm
       SUBMENU(MSG_STEPS_PER_MM, menu_advanced_steps_per_mm);
+
+      #if HAS_BED_PROBE
+        // M851 - Z Probe Offsets
+        SUBMENU(MSG_ZPROBE_OFFSETS, menu_probe_offsets);
+      #endif
     }
   #endif // !SLIM_LCD_MENUS
 
