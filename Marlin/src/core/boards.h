595e684658d82a86993167d3451c6a02fa5692c3
Jason Smith <jason.inet@gmail.com>
2020-01-02 22:04:13 -0600

    Add Leapfrog Xeed 2015 support (#16400)

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -138,6 +138,7 @@
 #define BOARD_GT2560_V3_A20           1318  // Geeetech GT2560 Rev B for A20(M/D)
 #define BOARD_EINSTART_S              1319  // Einstart retrofit
 #define BOARD_WANHAO_ONEPLUS          1320  // Wanhao 0ne+ i3 Mini
+#define BOARD_LEAPFROG_XEED2015       1321  // Leapfrog Xeed 2015
 
 //
 // ATmega1281, ATmega2561
