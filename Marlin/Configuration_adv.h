3317ab69d494607e5a7178599d1112621f1ba31d
Jan Šimeček <simecekjann@gmail.com>
2020-01-09 19:59:09 +0100

    Reverted back to 730Vref on extruder

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1772,7 +1772,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT    700
+    #define E0_CURRENT    730
     #define E0_MICROSTEPS  16
     #define E0_RSENSE    0.11
     #define E0_CHAIN_POS    0
