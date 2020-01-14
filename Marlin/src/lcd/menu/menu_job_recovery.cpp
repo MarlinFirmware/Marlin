1457e40208ade67acf4101bfa5ba68c5b319967c
Scott Lahteine <github@thinkyhead.com>
2020-01-13 20:25:06 -0600

    Make lcd_power_loss_recovery_cancel exportable

--- Marlin/src/lcd/menu/menu_job_recovery.cpp
+++ Marlin/src/lcd/menu/menu_job_recovery.cpp
@@ -38,7 +38,7 @@ static void lcd_power_loss_recovery_resume() {
   queue.inject_P(PSTR("M1000"));
 }
 
-static void lcd_power_loss_recovery_cancel() {
+void lcd_power_loss_recovery_cancel() {
   card.removeJobRecoveryFile();
   card.autostart_index = 0;
   ui.return_to_status();
