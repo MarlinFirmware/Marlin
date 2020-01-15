53dfeed6c0584fc6c93d592d164ee5c3af9446e4
Bob Kuhn <bob.kuhn@att.net>
2020-01-14 22:35:25 -0600

    Steval_3DP001V1 timers, analog inputs, etc. (#16565)

--- Marlin/src/HAL/HAL_STM32/timers.h
+++ Marlin/src/HAL/HAL_STM32/timers.h
@@ -61,12 +61,14 @@
 
   #define HAL_TIMER_RATE (F_CPU/2) // frequency of timer peripherals
 
+  // STM32F401 only has timers 1-5 & 9-11 with timers 4 & 5 usually assigned to TIMER_SERVO and TIMER_TONE
+
   #ifndef STEP_TIMER
-    #define STEP_TIMER 9  // STM32F401 has no TIM6, TIM7, or TIM8
+    #define STEP_TIMER 9
   #endif
 
   #ifndef TEMP_TIMER
-    #define TEMP_TIMER 14 // TIM7 is consumed by Software Serial if used.
+    #define TEMP_TIMER 10
   #endif
 
 #endif
