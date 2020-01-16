50faf9d5c5b85a37c1ae4d994ae74b52b32307ca
vivian-ng <vivian@maplerain.com>
2020-01-16 09:49:15 +0900

    Added sanity check for shared SPI

--- Marlin/src/inc/SanityCheck.h
+++ Marlin/src/inc/SanityCheck.h
@@ -2513,3 +2513,15 @@ static_assert(   _ARR_TEST(3,0) && _ARR_TEST(3,1) && _ARR_TEST(3,2)
 #if HAS_ADC_BUTTONS && defined(ADC_BUTTON_DEBOUNCE_DELAY) && !WITHIN(ADC_BUTTON_DEBOUNCE_DELAY, 16, 255)
   #error "ADC_BUTTON_DEBOUNCE_DELAY must be an integer from 16 to 255."
 #endif
+
+/**
+ * Check to make sure MONITOR_DRIVER_STATUS is not enabled
+ * on boards where TMC SPI drivers are sharing the same SPI
+ * as the SD card
+ */
+#if TMC_HAS_SPI && ENABLED(MONITOR_DRIVER_STATUS)
+  #if ENABLED(SDSUPPORT) && defined(HAS_SHARED_SPI)
+    #error "MONITOR_DRIVER_STATUS cannot be enabled together with SDSUPPORT on boards with shared SPI."
+  #endif
+#endif
+
