04d64351a24d3278ef8079c74cc24a55e32a1de2
MaukCC <jos@mauk.cc>
2020-01-21 08:10:31 +0100

    Add HMS434 V15 ATSAM board (#16620)

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -256,6 +256,7 @@
 #define BOARD_ARCHIM1                 3023  // UltiMachine Archim1 (with DRV8825 drivers)
 #define BOARD_ARCHIM2                 3024  // UltiMachine Archim2 (with TMC2130 drivers)
 #define BOARD_ALLIGATOR               3025  // Alligator Board R2
+#define BOARD_CNCONTROLS_15D          3026  // Cartesio CN Controls V15 on DUE
 
 //
 // SAM3X8C ARM Cortex M3
