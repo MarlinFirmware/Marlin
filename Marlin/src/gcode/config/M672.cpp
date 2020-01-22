29981e0d21e8d6d173e551f4e1e3f7d545488662
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-01-21 22:57:28 -0600

    Update M672.cpp

--- Marlin/src/gcode/config/M672.cpp
+++ Marlin/src/gcode/config/M672.cpp
@@ -29,71 +29,71 @@
 #include "../parser.h"
 
 /**
- * M672 - Set/clear Duet Smart Effector sensitivity
+ * M672 - Set/reset Duet Smart Effector sensitivity
  *
  *  One of these is required:
  *    S<sensitivity> - 0-255
- *    R              - Resets sensitivity to default
+ *    R              - Flag to reset sensitivity to default
  */
 
 /**
  * The Marlin format for the M672 command is different than shown in the Duet Smart Effector
- * documantation https://duet3d.dozuki.com/Wiki/Smart_effector_and_carriage_adapters_for_delta_printer#main
+ * documentation https://duet3d.dozuki.com/Wiki/Smart_effector_and_carriage_adapters_for_delta_printer
  *
- * Setting custom sensitivity:
+ * To set custom sensitivity:
  *   Duet:   M672 S105:aaa:bbb
  *   Marlin: M672 Saaa
  *
- *   aaa by the desired sensitivity and bbb is 255 - aaa.
+ *   (where aaa is the desired sensitivity and bbb is 255 - aaa).
  *
  * Revert sensitivity to factory settings:
  *   Duet:   M672 S105:131:131
  *   Marlin: M672 R
- *
  */
 
 #define M672_PROGBYTE    105								// magic byte to start programming custom sensitivity
 #define M672_ERASEBYTE   131								// magic byte to clear custom sensitivity
 
-
-// The protocol for each byte sent is:
-//    0 0 1 0 b7 b6 b5 b4 /b4 b3 b2 b1 b0 /b0
-
+//
+// Smart Effector byte send protocol:
+//
+//  0  0  1  0       ... always 0010
+//  b7 b6 b5 b4 ~b4  ... hi bits, NOT last bit
+//  b3 b2 b1 b0 ~b0  ... lo bits, NOT last bit
+//
 void M672_send(uint8_t b) {    // bit rate requirement: 1KHz +/- 30%
   for (uint8_t bits = 0; bits < 14; bits++) {
-    switch(bits) {
-      case 0:
-      case 1:
-      case 3: {OUT_WRITE(M672_MOD_PIN, LOW); break;}
-      case 2: {OUT_WRITE(M672_MOD_PIN, HIGH); break;}
-      case 7:
-      case 12: {OUT_WRITE(M672_MOD_PIN, b & 0x80); break;}   // don't shift
-      case 8:
-      case 13: {OUT_WRITE(M672_MOD_PIN, !(b & 0x80)); b <<= 1; break;}  // send inverted previous bit
-      default: {OUT_WRITE(M672_MOD_PIN, b & 0x80); b <<= 1; break;}
+    switch (bits) {
+      default: { OUT_WRITE(M672_MOD_PIN, !!(b & 0x80)); b <<= 1; break; } // send bit, shift next into place
+      case  7:
+      case 12: { OUT_WRITE(M672_MOD_PIN, !!(b & 0x80));          break; } // send bit. no shift
+      case  8:
+      case 13: { OUT_WRITE(M672_MOD_PIN,  !(b & 0x80)); b <<= 1; break; } // send inverted previous bit
+      case  0: case  1:                                   // 00
+      case  3: { OUT_WRITE(M672_MOD_PIN, LOW); break; }   // 0010
+      case  2: { OUT_WRITE(M672_MOD_PIN, HIGH); break; }  // 001
     }
     DELAY_US(1000);
   }
 }
 
-
 void GcodeSuite::M672() {
-  SERIAL_ECHO_MSG("M672\n");
   if (parser.seen('R')) {
     M672_send(M672_ERASEBYTE);
     M672_send(M672_ERASEBYTE);
   }
-  else if (parser.seen('S')) {
-    const int8_t M672_sensitivity = parser.byteval('S');
+  else if (parser.seenval('S')) {
+    const int8_t M672_sensitivity = parser.value_byte();
     M672_send(M672_PROGBYTE);
     M672_send(M672_sensitivity);
     M672_send(255 - M672_sensitivity);
   }
   else {
-    SERIAL_ECHO_MSG("'S' or 'R' parameter required\n");
+    SERIAL_ECHO_MSG("!'S' or 'R' parameter required.");
+    return;
   }
 
-  OUT_WRITE(M672_MOD_PIN, LOW);  // keep Smart Effector in normal mode
+  OUT_WRITE(SMART_EFFECTOR_PIN, LOW);  // Keep Smart Effector in NORMAL mode
 }
 
 #endif // SMART_EFFECTOR
