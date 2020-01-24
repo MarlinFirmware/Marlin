f223600f2080fc8756accde79d11398175866bd9
Marek Bel <marek.bel@posta.cz>
2020-01-24 03:51:52 +0100

    Increment EEPROM_VERSION to force updating to new Kp Ki Kd Kc default constants.
    This is hotfix consider doing this for final release in less brutal way.

--- Marlin/src/module/configuration_store.cpp
+++ Marlin/src/module/configuration_store.cpp
@@ -37,7 +37,7 @@
  */
 
 // Change EEPROM version if the structure changes
-#define EEPROM_VERSION "V76"
+#define EEPROM_VERSION "V77"
 #define EEPROM_OFFSET 100
 
 // Check the integrity of data offsets.
