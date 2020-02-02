16e94d5f2c6719e13e0a5af6182fb5b443537cf7
bill-orange <billwebb12@gmail.com>
2020-02-02 08:43:57 -0800

    Update Configuration_adv.h
    updated off measured k values

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1416,7 +1416,7 @@
 #define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.22    // Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 0.03    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
 #endif
 
