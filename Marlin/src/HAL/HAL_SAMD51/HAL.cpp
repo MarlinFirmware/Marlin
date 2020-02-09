834d63f6f1ff94fba7b65c25934bdc66ecb050aa
GMagician <GMagician@users.noreply.github.com>
2020-02-09 10:07:05 +0100

    Limit adc to 10 bits

--- Marlin/src/HAL/HAL_SAMD51/HAL.cpp
+++ Marlin/src/HAL/HAL_SAMD51/HAL.cpp
@@ -495,7 +495,7 @@ void HAL_adc_init() {
       // Preloaded data (fixed for all ADC instances hence not loaded by DMA)
       adc->REFCTRL.bit.REFSEL = ADC_REFCTRL_REFSEL_AREFA_Val;               // VRefA pin
       SYNC(adc->SYNCBUSY.bit.REFCTRL);
-      adc->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_12BIT_Val/*ADC_CTRLB_RESSEL_16BIT_Val*/;
+      adc->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_10BIT_Val/*ADC_CTRLB_RESSEL_16BIT_Val*/;
       SYNC(adc->SYNCBUSY.bit.CTRLB);
       adc->SAMPCTRL.bit.SAMPLEN = (6 - 1);                                  // Sampling clocks
       //adc->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_16 | ADC_AVGCTRL_ADJRES(4);  // 16 Accumulated conversions and shift 4 to get oversampled 12 bits result
