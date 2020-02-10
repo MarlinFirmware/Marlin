354b22fa271feed218513ef7c770c78635d70ccb
bill-orange <billwebb12@gmail.com>
2020-02-09 17:15:17 -0800

    restored to comment

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -821,7 +821,7 @@
  * vibration and surface artifacts. The algorithm adapts to provide the best possible step smoothing at the
  * lowest stepping frequencies.
  */
-#define ADAPTIVE_STEP_SMOOTHING
+//#define ADAPTIVE_STEP_SMOOTHING
 
 /**
  * Custom Microstepping
