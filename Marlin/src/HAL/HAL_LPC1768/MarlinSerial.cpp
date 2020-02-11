85f40f44e9f91388683d348d09db8368fa8e7b08
Robby Candra <robbycandra.mail@gmail.com>
2020-02-11 23:23:57 +0700

    MarlinSerial Constructor

--- Marlin/src/HAL/HAL_LPC1768/MarlinSerial.cpp
+++ Marlin/src/HAL/HAL_LPC1768/MarlinSerial.cpp
@@ -25,28 +25,28 @@
 #include "../../inc/MarlinConfigPre.h"
 #include "MarlinSerial.h"
 
-#if (defined(SERIAL_PORT) && SERIAL_PORT == 0) || (defined(SERIAL_PORT_2) && SERIAL_PORT_2 == 0)
+#if (defined(SERIAL_PORT) && SERIAL_PORT == 0) || (defined(SERIAL_PORT_2) && SERIAL_PORT_2 == 0) || (defined(DGUS_SERIAL_PORT) && DGUS_SERIAL_PORT == 0)
   MarlinSerial MSerial(LPC_UART0);
   extern "C" void UART0_IRQHandler() {
     MSerial.IRQHandler();
   }
 #endif
 
-#if (defined(SERIAL_PORT) && SERIAL_PORT == 1) || (defined(SERIAL_PORT_2) && SERIAL_PORT_2 == 1)
+#if (defined(SERIAL_PORT) && SERIAL_PORT == 1) || (defined(SERIAL_PORT_2) && SERIAL_PORT_2 == 1) || (defined(DGUS_SERIAL_PORT) && DGUS_SERIAL_PORT == 1)
   MarlinSerial MSerial1((LPC_UART_TypeDef *) LPC_UART1);
   extern "C" void UART1_IRQHandler() {
     MSerial1.IRQHandler();
   }
 #endif
 
-#if (defined(SERIAL_PORT) && SERIAL_PORT == 2) || (defined(SERIAL_PORT_2) && SERIAL_PORT_2 == 2)
+#if (defined(SERIAL_PORT) && SERIAL_PORT == 2) || (defined(SERIAL_PORT_2) && SERIAL_PORT_2 == 2) || (defined(DGUS_SERIAL_PORT) && DGUS_SERIAL_PORT == 2)
   MarlinSerial MSerial2(LPC_UART2);
   extern "C" void UART2_IRQHandler() {
     MSerial2.IRQHandler();
   }
 #endif
 
-#if (defined(SERIAL_PORT) && SERIAL_PORT == 3) || (defined(SERIAL_PORT_2) && SERIAL_PORT_2 == 3)
+#if (defined(SERIAL_PORT) && SERIAL_PORT == 3) || (defined(SERIAL_PORT_2) && SERIAL_PORT_2 == 3) || (defined(DGUS_SERIAL_PORT) && DGUS_SERIAL_PORT == 3)
   MarlinSerial MSerial3(LPC_UART3);
   extern "C" void UART3_IRQHandler() {
     MSerial3.IRQHandler();
