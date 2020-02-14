0e17d10bf325afed9ca3f133dd3d2e5cfb3a7724
Giuliano Zaro <3684609+GMagician@users.noreply.github.com>
2020-02-14 01:30:48 +0100

    Fix G-code line parsing (#16840)

--- Marlin/src/gcode/queue.cpp
+++ Marlin/src/gcode/queue.cpp
@@ -363,8 +363,8 @@ inline void process_stream_char(const char c, uint8_t &sis, char (&buff)[MAX_CMD
 
 inline bool process_line_done(uint8_t &sis, char (&buff)[MAX_CMD_SIZE], int &ind) {
   sis = PS_NORMAL;
-  if (!ind) { thermalManager.manage_heater(); return true; }
   buff[ind] = 0;
+  if (!ind) { thermalManager.manage_heater(); return true; }
   ind = 0;
   return false;
 }
@@ -377,7 +377,7 @@ inline bool process_line_done(uint8_t &sis, char (&buff)[MAX_CMD_SIZE], int &ind
 void GCodeQueue::get_serial_commands() {
   static char serial_line_buffer[NUM_SERIAL][MAX_CMD_SIZE];
 
-  static uint8_t serial_input_state[NUM_SERIAL] = { 0 };
+  static uint8_t serial_input_state[NUM_SERIAL] = { PS_NORMAL };
 
   #if ENABLED(BINARY_FILE_TRANSFER)
     if (card.flag.binary_mode) {
