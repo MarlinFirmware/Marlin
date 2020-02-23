5f27f7de47191c5cd2a6f4c2f0b65748ef13c268
Scott Lahteine <github@thinkyhead.com>
2020-02-22 18:49:59 -0600

    Version 2.0.4.2 Release

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.0.4.1"
+//#define SHORT_BUILD_VERSION "2.0.4.2"
 
 /**
  * Verbose version identifier which should contain a reference to the location
