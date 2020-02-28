c6694201346aaa351943d56f183451b76cec0c8d
Anders Sahlman <57940217+AndersSahlman@users.noreply.github.com>
2020-02-28 02:58:35 +0100

    Pass along sanity_check for (G33) probing (#17006)

--- Marlin/src/module/probe.h
+++ Marlin/src/module/probe.h
@@ -85,7 +85,7 @@ public:
     #endif
     static float probe_at_point(const float &rx, const float &ry, const ProbePtRaise raise_after=PROBE_PT_NONE, const uint8_t verbose_level=0, const bool probe_relative=true, const bool sanity_check=true);
     static inline float probe_at_point(const xy_pos_t &pos, const ProbePtRaise raise_after=PROBE_PT_NONE, const uint8_t verbose_level=0, const bool probe_relative=true, const bool sanity_check=true) {
-      return probe_at_point(pos.x, pos.y, raise_after, verbose_level, probe_relative);
+      return probe_at_point(pos.x, pos.y, raise_after, verbose_level, probe_relative, sanity_check);
     }
     #if HAS_HEATED_BED && ENABLED(WAIT_FOR_BED_HEATER)
       static const char msg_wait_for_bed_heating[25];
