1511aaf54ed65428d43ae9985786f9d6877b0381
studiodyne <diabolomix@hotmail.fr>
2020-03-12 22:50:12 +0100

    up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "MT1.5 Vtest2"
+#define CUSTOM_MACHINE_NAME "MT1.5 V3-test"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
