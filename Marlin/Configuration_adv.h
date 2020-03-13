23cad7ca3d1fbbb982ccaae0e0673dc9470e221b
studiodyne <diabolomix@hotmail.fr>
2020-03-13 03:22:51 +0100

    up

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1793,7 +1793,7 @@
  */
 //#define FWRETRACT
 #if ENABLED(FWRETRACT)
-  //#define FWRETRACT_AUTORETRACT           // Override slicer retractions
+//#define FWRETRACT_AUTORETRACT           // Override slicer retractions
   #if ENABLED(FWRETRACT_AUTORETRACT)
     #define MIN_AUTORETRACT 0.1           // (mm) Don't convert E moves under this length
     #define MAX_AUTORETRACT 10.0          // (mm) Don't convert E moves over this length
