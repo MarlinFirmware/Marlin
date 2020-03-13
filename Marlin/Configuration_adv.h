35ef2ce9c067e15526613f8d9554fc07ebbd3b02
studiodyne <diabolomix@hotmail.fr>
2020-03-13 01:04:33 +0100

    up

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1802,9 +1802,10 @@
   #define RETRACT_FEEDRATE 45             // (mm/s) Default feedrate for retracting
   #define RETRACT_ZRAISE 0                // (mm) Default retract Z-raise
   #define RETRACT_RECOVER_LENGTH 0        // (mm) Default additional recover length (added to retract length on recover)
+  #define RETRACT_RECOVER_FEEDRATE 25     // (mm/s) Default feedrate for recovering from retraction
   //#define FWRETRACT_SWAP_ENABLE         // Disable swap (economy of progmem/sram if using TOOLCHANGE_FILAMENT_SWAP )
   #if ENABLED(FWRETRACT_SWAP_ENABLE)
-    #define RETRACT_LENGTH_SWAP 60          // (mm) Default swap retract length (positive value)
+    #define RETRACT_LENGTH_SWAP 13          // (mm) Default swap retract length (positive value)
     #define RETRACT_RECOVER_LENGTH_SWAP 0   // (mm) Default additional swap recover length (added to retract length on recover from toolchange)
     #define RETRACT_RECOVER_FEEDRATE_SWAP RETRACT_RECOVER_FEEDRATE // (mm/s) Default feedrate for recovering from swap retraction
   #endif
