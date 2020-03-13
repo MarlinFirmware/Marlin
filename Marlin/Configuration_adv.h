c9b6f9cc1dca91936fcee311548b0ed885bb3508
studiodyne <diabolomix@hotmail.fr>
2020-03-13 03:21:31 +0100

    up

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1793,25 +1793,25 @@
  */
 //#define FWRETRACT
 #if ENABLED(FWRETRACT)
- //#define FWRETRACT_AUTORETRACT           // Override slicer retractions
- #if ENABLED(FWRETRACT_AUTORETRACT)
-   #define MIN_AUTORETRACT 0.1           // (mm) Don't convert E moves under this length
-   #define MAX_AUTORETRACT 10.0          // (mm) Don't convert E moves over this length
- #endif
- #define RETRACT_LENGTH 3                // (mm) Default retract length (positive value)
- #define RETRACT_FEEDRATE 45             // (mm/s) Default feedrate for retracting
- #define RETRACT_ZRAISE 0                // (mm) Default retract Z-raise
- #define RETRACT_RECOVER_LENGTH 0        // (mm) Default additional recover length (added to retract length on recover)
- #define RETRACT_RECOVER_FEEDRATE 8      // (mm/s) Default feedrate for recovering from retraction
- //#define FWRETRACT_SWAP_ENABLE         // Enable swap (instead of specific tool change )
- #if ENABLED(FWRETRACT_SWAP_ENABLE)
-   #define RETRACT_LENGTH_SWAP 13        // (mm) Default swap retract length (positive value)
-   #define RETRACT_RECOVER_LENGTH_SWAP 0 // (mm) Default additional swap recover length (added to retract length on recover from toolchange)
-   #define RETRACT_RECOVER_FEEDRATE_SWAP 8 // (mm/s) Default feedrate for recovering from swap retraction
- #endif
- #if ENABLED(MIXING_EXTRUDER)
-   //#define RETRACT_SYNC_MIXING         // Retract and restore all mixing steppers simultaneously
- #endif
+  //#define FWRETRACT_AUTORETRACT           // Override slicer retractions
+  #if ENABLED(FWRETRACT_AUTORETRACT)
+    #define MIN_AUTORETRACT 0.1           // (mm) Don't convert E moves under this length
+    #define MAX_AUTORETRACT 10.0          // (mm) Don't convert E moves over this length
+  #endif
+  #define RETRACT_LENGTH 3                // (mm) Default retract length (positive value)
+  #define RETRACT_FEEDRATE 45             // (mm/s) Default feedrate for retracting
+  #define RETRACT_ZRAISE 0                // (mm) Default retract Z-raise
+  #define RETRACT_RECOVER_LENGTH 0        // (mm) Default additional recover length (added to retract length on recover)
+  #define RETRACT_RECOVER_FEEDRATE 8      // (mm/s) Default feedrate for recovering from retraction
+  //#define FWRETRACT_SWAP_ENABLE         // Enable swap (instead of specific tool change )
+  #if ENABLED(FWRETRACT_SWAP_ENABLE)
+    #define RETRACT_LENGTH_SWAP 13        // (mm) Default swap retract length (positive value)
+    #define RETRACT_RECOVER_LENGTH_SWAP 0 // (mm) Default additional swap recover length (added to retract length on recover from toolchange)
+    #define RETRACT_RECOVER_FEEDRATE_SWAP 8 // (mm/s) Default feedrate for recovering from swap retraction
+  #endif
+  #if ENABLED(MIXING_EXTRUDER)
+    //#define RETRACT_SYNC_MIXING         // Retract and restore all mixing steppers simultaneously
+  #endif
 #endif
 
 /**
