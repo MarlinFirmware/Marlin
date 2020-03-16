4f13b9974578757c2f98d73578f9b4ced44fac31
Vert <vert@vertshobbies.com>
2020-03-15 21:23:16 -0400

    set pin based on if bltouch enabled

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -78,21 +78,19 @@
   #else
     #define Z_MIN_PIN      P1_00   // PWRDET
   #endif
-#else
-  #ifndef Z_STOP_PIN
-    #define Z_STOP_PIN     P1_27   // Z-STOP
-  #endif
+#elif ENABLED (BLTOUCH)
+  #define Z_STOP_PIN       P1_27   // Z-STOP
+    #else 
+      #define Z_STOP_PIN       P0_10   // Z-STOP
 #endif
 
 //
 // Z Probe (when not Z_MIN_PIN)
 //
-#ifndef Z_MIN_PROBE_PIN
-  #if Z_STOP_PIN != P1_27
+#if ENABLED (BLTOUCH)
     #define Z_MIN_PROBE_PIN P1_27
   #else
     #define Z_MIN_PROBE_PIN P0_10
-  #endif
 #endif
 
 //
