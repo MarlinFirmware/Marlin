05508053c397300c6ca7c69647f112198c603d6b
Scott Lahteine <github@thinkyhead.com>
2020-03-27 21:00:10 -0500

    Update pins_MKS_ROBIN.h

--- Marlin/src/pins/stm32f1/pins_MKS_ROBIN.h
+++ Marlin/src/pins/stm32f1/pins_MKS_ROBIN.h
@@ -178,14 +178,14 @@
   //#define E4_HARDWARE_SERIAL Serial1
 
   // Unused servo pins may be repurposed with SoftwareSerialM
-  //#define X_SERIAL_TX_PIN  PF8   // SERVO3_PIN
-  //#define Y_SERIAL_TX_PIN  PF9   // SERVO2_PIN
-  //#define Z_SERIAL_TX_PIN  PA1   // SERVO1_PIN
-  //#define E0_SERIAL_TX_PIN PC3   // SERVO0_PIN
-  //#define X_SERIAL_RX_PIN  X_SERIAL_TX_PIN
-  //#define Y_SERIAL_RX_PIN  Y_SERIAL_TX_PIN
-  //#define Z_SERIAL_RX_PIN  Z_SERIAL_TX_PIN
-  //#define E0_SERIAL_RX_PIN E0_SERIAL_TX_PIN
+  //#define X_SERIAL_TX_PIN                 PF8   // SERVO3_PIN
+  //#define Y_SERIAL_TX_PIN                 PF9   // SERVO2_PIN
+  //#define Z_SERIAL_TX_PIN                 PA1   // SERVO1_PIN
+  //#define E0_SERIAL_TX_PIN                PC3   // SERVO0_PIN
+  //#define X_SERIAL_RX_PIN      X_SERIAL_TX_PIN
+  //#define Y_SERIAL_RX_PIN      Y_SERIAL_TX_PIN
+  //#define Z_SERIAL_RX_PIN      Z_SERIAL_TX_PIN
+  //#define E0_SERIAL_RX_PIN    E0_SERIAL_TX_PIN
 
   // Reduce baud rate for software serial reliability
   #if HAS_TMC_SW_SERIAL
