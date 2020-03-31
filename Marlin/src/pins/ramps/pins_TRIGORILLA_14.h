e7f020f3f208db23450c845a6c4b67d784a64058
Scott Lahteine <github@thinkyhead.com>
2020-03-31 14:42:32 -0500

    Shorten some internal pin names

--- Marlin/src/pins/ramps/pins_TRIGORILLA_14.h
+++ Marlin/src/pins/ramps/pins_TRIGORILLA_14.h
@@ -47,43 +47,43 @@
 #endif
 
 // Labeled pins
-#define TRIGORILLA_HEATER_BED_PIN              8
-#define TRIGORILLA_HEATER_0_PIN               10
-#define TRIGORILLA_HEATER_1_PIN               45  // Anycubic Kossel: Unused
+#define TG_HEATER_BED_PIN                      8
+#define TG_HEATER_0_PIN                       10
+#define TG_HEATER_1_PIN                       45  // Anycubic Kossel: Unused
 
-#define TRIGORILLA_FAN0_PIN                    9  // Anycubic Kossel: Usually the part cooling fan
-#define TRIGORILLA_FAN1_PIN                    7  // Anycubic Kossel: Unused
-#define TRIGORILLA_FAN2_PIN                   44  // Anycubic Kossel: Hotend fan
+#define TG_FAN0_PIN                            9  // Anycubic Kossel: Usually the part cooling fan
+#define TG_FAN1_PIN                            7  // Anycubic Kossel: Unused
+#define TG_FAN2_PIN                           44  // Anycubic Kossel: Hotend fan
 
 // Remap MOSFET pins to common usages:
 
-#define RAMPS_D10_PIN    TRIGORILLA_HEATER_0_PIN  // HEATER_0_PIN is always RAMPS_D10_PIN in pins_RAMPS.h
+#define RAMPS_D10_PIN            TG_HEATER_0_PIN  // HEATER_0_PIN is always RAMPS_D10_PIN in pins_RAMPS.h
 
 #if HOTENDS > 1                                   // EEF and EEB
-  #define RAMPS_D9_PIN   TRIGORILLA_HEATER_1_PIN
+  #define RAMPS_D9_PIN           TG_HEATER_1_PIN
   #if !TEMP_SENSOR_BED
     // EEF
-    #define RAMPS_D8_PIN     TRIGORILLA_FAN0_PIN
+    #define RAMPS_D8_PIN             TG_FAN0_PIN
   #else
     // EEB
-    #define RAMPS_D8_PIN     TRIGORILLA_HEATER_BED_PIN
-    #define FAN_PIN          TRIGORILLA_FAN0_PIN  // Override pin 4 in pins_RAMPS.h
+    #define RAMPS_D8_PIN       TG_HEATER_BED_PIN
+    #define FAN_PIN                  TG_FAN0_PIN  // Override pin 4 in pins_RAMPS.h
   #endif
 #elif TEMP_SENSOR_BED
   // EFB (Anycubic Kossel default)
-  #define RAMPS_D9_PIN       TRIGORILLA_FAN0_PIN
-  #define RAMPS_D8_PIN       TRIGORILLA_HEATER_BED_PIN
+  #define RAMPS_D9_PIN               TG_FAN0_PIN
+  #define RAMPS_D8_PIN         TG_HEATER_BED_PIN
 #else
   // EFF
-  #define RAMPS_D9_PIN       TRIGORILLA_FAN1_PIN
-  #define RAMPS_D8_PIN       TRIGORILLA_FAN0_PIN
+  #define RAMPS_D9_PIN               TG_FAN1_PIN
+  #define RAMPS_D8_PIN               TG_FAN0_PIN
 #endif
 
 #if HOTENDS > 1 || TEMP_SENSOR_BED                // EEF, EEB, EFB
-  #define FAN1_PIN           TRIGORILLA_FAN1_PIN
+  #define FAN1_PIN                   TG_FAN1_PIN
 #endif
-#define FAN2_PIN             TRIGORILLA_FAN2_PIN
-#define ORIG_E0_AUTO_FAN_PIN TRIGORILLA_FAN2_PIN   // Used in Anycubic Kossel example config
+#define FAN2_PIN                     TG_FAN2_PIN
+#define ORIG_E0_AUTO_FAN_PIN         TG_FAN2_PIN  // Used in Anycubic Kossel example config
 
 #include "pins_RAMPS.h"
 
