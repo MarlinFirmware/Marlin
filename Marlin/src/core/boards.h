d652931c731253d8acf400811d5ea113f8898029
thisiskeithb <thisiskeithb@outlook.com>
2020-04-02 08:18:16 -0700

    BTT002 V1 Retail Uses STM32F407VGT6
    BigTreeTech's BTT002 MCU was updated from a STM32F407VET6 to STM32F407VGT6 (1024k flash) before launching to retail.

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -319,7 +319,7 @@
 #define BOARD_BLACK_STM32F407ZE       4206  // BLACK_STM32F407ZE
 #define BOARD_STEVAL_3DP001V1         4207  // STEVAL-3DP001V1 3D PRINTER BOARD
 #define BOARD_BTT_SKR_PRO_V1_1        4208  // BigTreeTech SKR Pro v1.1 (STM32F407ZG)
-#define BOARD_BTT_BTT002_V1_0         4209  // BigTreeTech BTT002 v1.0 (STM32F407VE)
+#define BOARD_BTT_BTT002_V1_0         4209  // BigTreeTech BTT002 v1.0 (STM32F407VG)
 #define BOARD_BTT_GTR_V1_0            4210  // BigTreeTech GTR v1.0 (STM32F407IGT)
 #define BOARD_LERDGE_K                4211  // Lerdge K (STM32F407ZG)
 #define BOARD_LERDGE_X                4212  // Lerdge X (STM32F407VE)
