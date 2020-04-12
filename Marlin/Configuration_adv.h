68fb02718b2b50291768f88be9bca5cc42a18030
Scott Lahteine <github@thinkyhead.com>
2020-04-11 21:27:49 -0500

    Fix SD_FINISHED_RELEASECOMMAND bug
    Fixes #17401 bug that was introduced in 8f26c3a

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1035,8 +1035,8 @@
   // Enable this option and set to HIGH if your SD cards are incorrectly detected.
   //#define SD_DETECT_STATE HIGH
 
-  #define SD_FINISHED_STEPPERRELEASE true          // Disable steppers when SD Print is finished
-  #define SD_FINISHED_RELEASECOMMAND "M84 X Y Z E" // You might want to keep the Z enabled so your bed stays in place.
+  #define SD_FINISHED_STEPPERRELEASE true   // Disable steppers when SD Print is finished
+  #define SD_FINISHED_RELEASECOMMAND "M84"  // Use "M84XYE" to keep Z enabled so your bed stays in place
 
   // Reverse SD sort to show "more recent" files first, according to the card's FAT.
   // Since the FAT gets out of order with usage, SDCARD_SORT_ALPHA is recommended.
