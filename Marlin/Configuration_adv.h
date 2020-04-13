e6ccec17a6a0d78983930e1d00822e21eeea2aca
Scott Lahteine <github@thinkyhead.com>
2020-04-13 14:41:57 -0500

    Expose SD_PROCEDURE_DEPTH in config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1035,6 +1035,8 @@
   // Enable this option and set to HIGH if your SD cards are incorrectly detected.
   //#define SD_DETECT_STATE HIGH
 
+  #define SD_PROCEDURE_DEPTH 1              // Increase if you need more nested M32 calls
+
   #define SD_FINISHED_STEPPERRELEASE true   // Disable steppers when SD Print is finished
   #define SD_FINISHED_RELEASECOMMAND "M84"  // Use "M84XYE" to keep Z enabled so your bed stays in place
 
