d5dbd55782b331720e175c757a94b18acb18ef80
mks-viva <1224833100@qq.com>
2020-04-15 11:57:10 +0800

    Update pins.h

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -516,6 +516,8 @@
   #include "stm32f1/pins_MKS_ROBIN_LITE3.h"     // STM32F1                                env:mks_robin_lite3
 #elif MB(MKS_ROBIN_PRO)
   #include "stm32f1/pins_MKS_ROBIN_PRO.h"       // STM32F1                                env:mks_robin_pro
+#elif MB(MKS_ROBIN_E3D)
+  #include "stm32f1/pins_MKS_ROBIN_E3D.h"         // STM32F1                                env:mks_robin_e3d
 
 //
 // ARM Cortex-M4F
