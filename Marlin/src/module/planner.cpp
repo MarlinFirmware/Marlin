0619e417f1467ecb51560fb05e8d791ec932750b
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-04-16 18:16:58 -0500

    Update planner.cpp

--- Marlin/src/module/planner.cpp
+++ Marlin/src/module/planner.cpp
@@ -2978,17 +2978,13 @@ void Planner::set_max_jerk(const AxisEnum axis, float targetValue) {
       const int16_t target = thermalManager.degTargetHotend(active_extruder);
       autotemp_min = target + AUTOTEMP_MIN_P;
       autotemp_max = target + AUTOTEMP_MAX_P;
-      autotemp_enabled = autotemp_factor = AUTOTEMP_FACTOR_P;
+      autotemp_factor = AUTOTEMP_FACTOR_P;
     #endif
 
-    // F0 will disable autotemp
-    if (parser.seenval('F')) {
-      autotemp_factor = parser.value_float();
-      if (!autotemp_factor) autotemp_enabled = false;
-      }
     if (parser.seenval('S')) autotemp_min = parser.value_celsius();
     if (parser.seenval('B')) autotemp_max = parser.value_celsius();
-
+    if (parser.seenval('F')) autotemp_factor = parser.value_float();
+    if (!autotemp_factor) autotemp_enabled = false; // F0 will disable autotemp
   }
 
 #endif
