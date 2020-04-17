e24f97ac66203afa064ab962c46db4d088a4c6da
Scott Lahteine <github@thinkyhead.com>
2020-04-17 07:47:36 -0500

    Finishing touches
    Co-Authored-By: mojocorp <mojocorp@users.noreply.github.com>

--- Marlin/src/pins/stm32f0/pins_MALYAN_M300.h
+++ Marlin/src/pins/stm32f0/pins_MALYAN_M300.h
@@ -85,4 +85,4 @@
 #define HEATER_0_PIN                        PA1   // HOTEND0 MOSFET
 #define HEATER_BED_PIN                      PA5   // BED MOSFET
 
-#define ORIG_E0_AUTO_FAN_PIN                PA8
+#define AUTO_FAN_PIN                        PA8
