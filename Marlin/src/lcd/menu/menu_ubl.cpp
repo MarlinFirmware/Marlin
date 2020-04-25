f907de272aa8bbb5c9d25ae1473d395271e94044
Scott Lahteine <github@thinkyhead.com>
2020-04-25 16:49:53 -0500

    Fix mesh point sign
    Most obvious part of #17670
    
    Co-Authored-By: FilippoR <filippo.rossoni@gmail.com>

--- Marlin/src/lcd/menu/menu_ubl.cpp
+++ Marlin/src/lcd/menu/menu_ubl.cpp
@@ -41,7 +41,8 @@ static int16_t ubl_storage_slot = 0,
                ubl_fillin_amount = 5,
                ubl_height_amount = 1;
 
-static uint8_t n_edit_pts = 1, x_plot = 0, y_plot = 0;
+static uint8_t n_edit_pts = 1;
+static int8_t x_plot = 0, y_plot = 0; // May be negative during move
 
 #if HAS_HEATED_BED
   static int16_t custom_bed_temp = 50;
