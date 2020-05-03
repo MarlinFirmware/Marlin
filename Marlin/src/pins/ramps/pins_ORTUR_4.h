612c486d901c496b5e92b1d29a8c4c5b0ab373c8
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-05-02 22:11:03 -0500

    Clean up and format

--- Marlin/src/pins/ramps/pins_ORTUR_4.h
+++ Marlin/src/pins/ramps/pins_ORTUR_4.h
@@ -25,99 +25,68 @@
  * Ortur 4 Arduino Mega based on RAMPS v1.4 pin assignments
  */
 
-#ifndef BOARD_INFO_NAME
-  #define BOARD_INFO_NAME "Ortur 4.3"
-#endif
+#define BOARD_INFO_NAME "Ortur 4.3"
+#define DEFAULT_MACHINE_NAME BOARD_INFO_NAME
 
-#ifndef DEFAULT_MACHINE_NAME
-  #define DEFAULT_MACHINE_NAME BOARD_INFO_NAME
-#endif
-
-#ifndef DEFAULT_SOURCE_CODE_URL
-  #define DEFAULT_SOURCE_CODE_URL "https://github.com/MarlinFirmware/"
-#endif
-
-
-#include "pins_RAMPS.h"
+//
+// Servos
+//
+#define SERVO0_PIN                            29
 
 //
 // Limit Switches
 //
+#define X_MAX_PIN                             18
+#define Z_MIN_PIN                             63
 
-#undef X_MAX_PIN
-#define X_MAX_PIN         18
-
-#undef Z_MIN_PIN
-#define Z_MIN_PIN          63
+#define Z_MIN_PROBE_PIN                        2
+#define FIL_RUNOUT_PIN                        59
 
 //
 // Steppers
 //
-
-#undef E0_STEP_PIN
-#define E0_STEP_PIN        36
-#undef E0_DIR_PIN
-#define E0_DIR_PIN         34
-#undef E0_ENABLE_PIN
-#define E0_ENABLE_PIN      30
-#undef E0_CS_PIN
-#define E0_CS_PIN        44
-
-#undef E1_STEP_PIN
-#define E1_STEP_PIN        26
-#undef E1_DIR_PIN
-#define E1_DIR_PIN         28
-#undef E1_ENABLE_PIN
-#define E1_ENABLE_PIN      24
-#undef E1_CS_PIN
-#define E1_CS_PIN        42
+#define E0_CS_PIN                             44
+#define E1_CS_PIN                             42
 
 //
 // Temperature Sensors
 //
-
-#undef TEMP_0_PIN
-#define TEMP_0_PIN         15   // Analog Input
-#undef TEMP_1_PIN
-#define TEMP_1_PIN         13   // Analog Input
+#define TEMP_0_PIN                            15  // Analog Input
+#define TEMP_1_PIN                            13  // Analog Input
 
 //
 // Software serial
 //
+#define X_SERIAL_TX_PIN                       59
+#define X_SERIAL_RX_PIN                       63
 
-#undef X_SERIAL_TX_PIN
-#define X_SERIAL_TX_PIN    59
-#undef SERVO0_PIN
-#define X_SERIAL_RX_PIN    63
-#undef X_SERIAL_RX_PIN
+#define Y_SERIAL_TX_PIN                       64
+#define Y_SERIAL_RX_PIN                       40
 
-#undef Y_SERIAL_TX_PIN
-#define Y_SERIAL_TX_PIN    64
-#undef Y_SERIAL_RX_PIN
-#define Y_SERIAL_RX_PIN    40
+#define Z_SERIAL_TX_PIN                       44
+#define Z_SERIAL_RX_PIN                       42
 
-#undef Z_SERIAL_TX_PIN
-#define Z_SERIAL_TX_PIN    44
-#undef Z_SERIAL_RX_PIN
-#define Z_SERIAL_RX_PIN    42
+#define E0_SERIAL_TX_PIN                      66
+#define E0_SERIAL_RX_PIN                      65
 
-#undef E0_SERIAL_TX_PIN
-#define E0_SERIAL_TX_PIN   66
-#undef E0_SERIAL_RX_PIN
-#define E0_SERIAL_RX_PIN   65
+#include "pins_RAMPS.h"
 
 //
-// Misc. Functions
+// Steppers
 //
+#undef E0_STEP_PIN
+#undef E0_DIR_PIN
+#undef E0_ENABLE_PIN
+#define E0_STEP_PIN                           36
+#define E0_DIR_PIN                            34
+#define E0_ENABLE_PIN                         30
 
-#undef SERVO0_PIN
-#define SERVO0_PIN       29
-
-#undef Z_MIN_PROBE_PIN
-#define Z_MIN_PROBE_PIN  2
-
-#undef FIL_RUNOUT_PIN
-#define FIL_RUNOUT_PIN    59
+#undef E1_STEP_PIN
+#undef E1_DIR_PIN
+#undef E1_ENABLE_PIN
+#define E1_STEP_PIN                           26
+#define E1_DIR_PIN                            28
+#define E1_ENABLE_PIN                         24
 
 //
 // LCD / Controller
@@ -126,7 +95,7 @@
 #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
 
   #undef BEEPER_PIN
-  #define BEEPER_PIN                           35
+  #define BEEPER_PIN                          35
 
   #undef LCD_PINS_RS
   #undef LCD_PINS_ENABLE
@@ -137,14 +106,14 @@
 
   #undef LCD_SDSS
   #undef SD_DETECT_PIN
-  #define LCD_SDSS                        53
-  #define SD_DETECT_PIN	                  49
+  #define LCD_SDSS                            53
+  #define SD_DETECT_PIN                       49
 
   #undef BTN_EN1
   #undef BTN_EN2
   #undef BTN_ENC
-  #define BTN_EN1                       29
-  #define BTN_EN2                       25
-  #define BTN_ENC                       16
+  #define BTN_EN1                             29
+  #define BTN_EN2                             25
+  #define BTN_ENC                             16
 
 #endif
