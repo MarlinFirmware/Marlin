087932fda5bc4baa8484d69871872a9ebd069bdd
Scott Lahteine <github@thinkyhead.com>
2020-05-06 01:40:00 -0500

    Format, clean up pins

--- Marlin/src/pins/ramps/pins_K8600.h
+++ Marlin/src/pins/ramps/pins_K8600.h
@@ -24,70 +24,74 @@
  * VERTEX NANO Arduino Mega with RAMPS EFB v1.4 pin assignments.
  */
 
+#if HOTENDS > 1
+  #error "Only 1 hotend is supported for Vertex Nano."
+#endif
 
-#define IS_RAMPS_EFB  // Override autodetection. Bed will be undefined.
-#include "pins_RAMPS.h"
-
-#undef BOARD_INFO_NAME
-#undef DEFAULT_MACHINE_NAME
 #define BOARD_INFO_NAME      "K8600"
 #define DEFAULT_MACHINE_NAME "Vertex Nano"
 
-#define CASE_LIGHT_PIN          7
+//
+// Limit Switches
+//
+#define X_MIN_PIN                              3
+#define Y_MAX_PIN                             14
+#define Z_MAX_PIN                             18
+#define Z_MIN_PIN                             -1
 
+//
+// Heaters / Fans
+//
+#define FAN_PIN                                8
+
+#define IS_RAMPS_EFB                              // Override autodetection. Bed will be undefined.
+#include "pins_RAMPS.h"
+
+//
+// Steppers
+//
 #undef X_STEP_PIN
 #undef X_DIR_PIN
 #undef X_ENABLE_PIN
-#define X_STEP_PIN              54
-#define X_DIR_PIN               55
-#define X_ENABLE_PIN            38
+#define X_STEP_PIN                            54
+#define X_DIR_PIN                             55
+#define X_ENABLE_PIN                          38
 
 #undef Y_STEP_PIN
 #undef Y_DIR_PIN
 #undef Y_ENABLE_PIN
-#define Y_STEP_PIN              60
-#define Y_DIR_PIN               61
-#define Y_ENABLE_PIN            56
+#define Y_STEP_PIN                            60
+#define Y_DIR_PIN                             61
+#define Y_ENABLE_PIN                          56
 
 #undef Z_ENABLE_PIN
-#define Z_ENABLE_PIN            63
+#define Z_ENABLE_PIN                          63
 
 #undef E0_STEP_PIN
 #undef E0_DIR_PIN
 #undef E0_ENABLE_PIN
-#define E0_STEP_PIN             26
-#define E0_DIR_PIN              28
-#define E0_ENABLE_PIN           24
+#define E0_STEP_PIN                           26
+#define E0_DIR_PIN                            28
+#define E0_ENABLE_PIN                         24
 
-#undef X_MIN_PIN
-#undef Y_MAX_PIN
-#undef Z_MAX_PIN
-#undef Z_MIN_PIN
-#define X_MIN_PIN               3
-#define Y_MAX_PIN               14
-#define Z_MAX_PIN               18
-#define Z_MIN_PIN               -1
-
-#undef SDSS
-#define SDSS                    25 //53
-
-#undef FAN_PIN
-#define FAN_PIN                 8
-
-#undef HEATER_0_PIN
-#define HEATER_0_PIN            10
-
-#undef HEATER_1_PIN
-#undef HEATER_2_PIN
+//
+// Heaters / Fans
+//
 #undef HEATER_BED_PIN
-#define HEATER_0_PIN            10
-#define HEATER_1_PIN            -1
-#define HEATER_2_PIN            -1
-#define HEATER_BED_PIN          -1   // BED
 
-#if ENABLED(ULTRA_LCD) && ENABLED(NEWPANEL)
+//
+// Misc. Functions
+//
+#undef SDSS
+#define SDSS                                  25  // 53
+
+#define CASE_LIGHT_PIN                         7
+
+//
+// LCD / Controller
+//
+#if BOTH(ULTRA_LCD, NEWPANEL)
   #undef BEEPER_PIN
-  #define BEEPER_PIN            -1
 
   #undef LCD_PINS_RS
   #undef LCD_PINS_ENABLE
@@ -95,24 +99,23 @@
   #undef LCD_PINS_D5
   #undef LCD_PINS_D6
   #undef LCD_PINS_D7
-  #define LCD_PINS_RS           27
-  #define LCD_PINS_ENABLE       29
-  #define LCD_PINS_D4           37
-  #define LCD_PINS_D5           35
-  #define LCD_PINS_D6           33
-  #define LCD_PINS_D7           31
+  #define LCD_PINS_RS                         27
+  #define LCD_PINS_ENABLE                     29
+  #define LCD_PINS_D4                         37
+  #define LCD_PINS_D5                         35
+  #define LCD_PINS_D6                         33
+  #define LCD_PINS_D7                         31
 
   // Buttons
   #undef BTN_EN1
   #undef BTN_EN2
   #undef BTN_ENC
-  #define BTN_EN1               17
-  #define BTN_EN2               16
-  #define BTN_ENC               23 //the click
+  #define BTN_EN1                             17
+  #define BTN_EN2                             16
+  #define BTN_ENC                             23
 
 #else
 
-  #define BEEPER_PIN            33
+  #define BEEPER_PIN                          33
 
-#endif // ULTRA_LCD && NEWPANEL
-
+#endif
