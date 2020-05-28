e24d0202cd17b7c6c691403f38fab0fc357fc42e
Leo <leo@nutz.de>
2020-05-28 12:30:16 +0200

    Make complete board info name string visible on LCD
    Make sure the complete string is visible on LCD screen and version numbers are not cut off. Change BIGTREETECH to BTT to shorten string and have a consistent convention for all board names.

--- Marlin/src/pins/stm32f1/pins_BTT_SKR_E3_DIP.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_E3_DIP.h
@@ -25,7 +25,7 @@
   #error "Oops! Select an STM32F1 board in 'Tools > Board.'"
 #endif
 
-#define BOARD_INFO_NAME "BIGTREE SKR E3 DIP V1.0"
+#define BOARD_INFO_NAME "BTT SKR E3 DIP V1.0"
 
 // Release PB3/PB4 (TMC_SW Pins) from JTAG pins
 #define DISABLE_JTAG
