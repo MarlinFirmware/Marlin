0069fcf9c3b45447c669c3b74eb456a8446e974d
Scott Lahteine <github@thinkyhead.com>
2020-05-31 01:57:54 -0500

    Format

--- Marlin/src/pins/stm32f1/pins_CCROBOT_MEEB_3DP.h
+++ Marlin/src/pins/stm32f1/pins_CCROBOT_MEEB_3DP.h
@@ -39,46 +39,46 @@
 #define DISABLE_JTAG
 
 // There are 1 neopixel on board and a connector for other neopixels.
-#define NEOPIXEL_PIN                     PC7    // The NEOPIXEL LED driving pin
+#define NEOPIXEL_PIN                        PC7   // The NEOPIXEL LED driving pin
 
 //
 // Limit Switches
 //
-#define X_STOP_PIN                       PC0
-#define Y_STOP_PIN                       PC1
-#define Z_STOP_PIN                       PC2
+#define X_STOP_PIN                          PC0
+#define Y_STOP_PIN                          PC1
+#define Z_STOP_PIN                          PC2
 
 //
 // Z Probe must be this pin
 //
-#define Z_MIN_PROBE_PIN                  PC15  // "PROBE"
+#define Z_MIN_PROBE_PIN                     PC15  // "PROBE"
 
 //
 // TMC2208 stepper drivers
 //
-#define X_ENABLE_PIN                     PB4
-#define X_STEP_PIN                       PC12
-#define X_DIR_PIN                        PC11
-#define X_SERIAL_TX_PIN                  PB3
-#define X_SERIAL_RX_PIN                  PD2
+#define X_ENABLE_PIN                        PB4
+#define X_STEP_PIN                          PC12
+#define X_DIR_PIN                           PC11
+#define X_SERIAL_TX_PIN                     PB3
+#define X_SERIAL_RX_PIN                     PD2
 
-#define Y_ENABLE_PIN                     PC10
-#define Y_STEP_PIN                       PB14
-#define Y_DIR_PIN                        PB13
-#define Y_SERIAL_TX_PIN                  PA15
-#define Y_SERIAL_RX_PIN                  PC6
+#define Y_ENABLE_PIN                        PC10
+#define Y_STEP_PIN                          PB14
+#define Y_DIR_PIN                           PB13
+#define Y_SERIAL_TX_PIN                     PA15
+#define Y_SERIAL_RX_PIN                     PC6
 
-#define Z_ENABLE_PIN                     PB12
-#define Z_STEP_PIN                       PB2
-#define Z_DIR_PIN                        PB1
-#define Z_SERIAL_TX_PIN                  PB11
-#define Z_SERIAL_RX_PIN                  PB10
+#define Z_ENABLE_PIN                        PB12
+#define Z_STEP_PIN                          PB2
+#define Z_DIR_PIN                           PB1
+#define Z_SERIAL_TX_PIN                     PB11
+#define Z_SERIAL_RX_PIN                     PB10
 
-#define E0_ENABLE_PIN                    PB0
-#define E0_STEP_PIN                      PA6
-#define E0_DIR_PIN                       PA5
-#define E0_SERIAL_TX_PIN                 PC5
-#define E0_SERIAL_RX_PIN                 PC4
+#define E0_ENABLE_PIN                       PB0
+#define E0_STEP_PIN                         PA6
+#define E0_DIR_PIN                          PA5
+#define E0_SERIAL_TX_PIN                    PC5
+#define E0_SERIAL_RX_PIN                    PC4
 
 // Reduce baud rate to improve software serial reliability
 #define TMC_BAUD_RATE 19200
@@ -86,18 +86,18 @@
 //
 // Temperature Sensors
 //
-#define TEMP_0_PIN                       PA0   // TH0
-#define TEMP_BED_PIN                     PC3   // THB
+#define TEMP_0_PIN                          PA0   // TH0
+#define TEMP_BED_PIN                        PC3   // THB
 
 //
 // Heaters / Fans
 //
-#define HEATER_0_PIN                     PC8   // HEATER0
-#define HEATER_BED_PIN                   PC9   // HOT BED
+#define HEATER_0_PIN                        PC8   // HEATER0
+#define HEATER_BED_PIN                      PC9   // HOT BED
 
-#define FAN_PIN                          PA7   // FAN  (fan2 on board) model cool fan
-#define FAN1_PIN                         PA8   // FAN  (fan0 on board) e0 cool fan
-#define FAN2_PIN                         PB9   // FAN  (fan1 on board) controller cool fan
+#define FAN_PIN                             PA7   // FAN  (fan2 on board) model cool fan
+#define FAN1_PIN                            PA8   // FAN  (fan0 on board) e0 cool fan
+#define FAN2_PIN                            PB9   // FAN  (fan1 on board) controller cool fan
 
 /**
  *              1 _____ 2   
@@ -114,14 +114,14 @@
 // LCD / Controller
 //
 #if ENABLED(CR10_STOCKDISPLAY)
-  #define BEEPER_PIN                       PB5
-  #define BTN_EN1                          PA2
-  #define BTN_EN2                          PA3
-  #define BTN_ENC                          PB6
+  #define BEEPER_PIN                        PB5
+  #define BTN_EN1                           PA2
+  #define BTN_EN2                           PA3
+  #define BTN_ENC                           PB6
   
-  #define LCD_PINS_RS                      PB7  // CS -- SOFT SPI for ENDER3 LCD
-  #define LCD_PINS_D4                      PB8  // SCLK
-  #define LCD_PINS_ENABLE                  PA4  // DATA MOSI
+  #define LCD_PINS_RS                       PB7   // CS -- SOFT SPI for ENDER3 LCD
+  #define LCD_PINS_D4                       PB8   // SCLK
+  #define LCD_PINS_ENABLE                   PA4   // DATA MOSI
 #endif
 
 // Alter timing for graphical display
@@ -134,12 +134,12 @@
 //
 // Servos
 //
-#define SERVO0_PIN                       PA1
+#define SERVO0_PIN                          PA1
 
 //
 // Camera
 //
-#define CHDK_PIN                         PB15
+#define CHDK_PIN                            PB15
 
 //
 // EEPROM
@@ -150,7 +150,7 @@
   #define EEPROM_PAGE_SIZE     (0x800U) // 2KB
   #define EEPROM_START_ADDRESS (0x8000000UL + (STM32_FLASH_SIZE) * 1024UL - (EEPROM_PAGE_SIZE) * 2UL)
   #undef E2END
-  #define E2END                0xFFF    // EEPROM end address (4kB)
+  #define E2END 0xFFF                             // EEPROM end address (4kB)
 #endif
 
 /* //
@@ -167,5 +167,3 @@
 
 #define ON_BOARD_SPI_DEVICE 1                     // SPI1
 #define ONBOARD_SD_CS_PIN                   PA4   // Chip select for SD-NAND */
-
-
