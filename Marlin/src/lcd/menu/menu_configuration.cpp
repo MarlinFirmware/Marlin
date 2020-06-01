ba5a62a838bbe5284f691393a80455656db6996e
borland1 <barryorlando@hotmail.com>
2020-06-01 16:48:51 -0700

    Update menu_configuration.cpp
    Add custom menus to 'Configuration Menu'.

--- Marlin/src/lcd/menu/menu_configuration.cpp
+++ Marlin/src/lcd/menu/menu_configuration.cpp
@@ -349,6 +349,10 @@ void menu_configuration() {
   START_MENU();
   BACK_ITEM(MSG_MAIN);
 
+  #if ENABLED(CUSTOM_MENUS_CONFIGURATION)
+    void custom_menus_configuration();
+  #endif
+  
   //
   // Debug Menu when certain options are enabled
   //
@@ -356,6 +360,14 @@ void menu_configuration() {
     SUBMENU(MSG_DEBUG_MENU, menu_debug);
   #endif
 
+  #if ENABLED(CUSTOM_MENUS_CONFIGURATION)
+    #ifdef CUSTOM_MENUS_CONFIGURATION_TITLE
+      SUBMENU_P(PSTR(CUSTOM_MENUS_CONFIGURATION_TITLE), custom_menus_configuration);
+    #else
+      SUBMENU(MSG_CUSTOM_MENUS_CONFIGURATION, custom_menus_configuration);
+    #endif
+  #endif
+  
   SUBMENU(MSG_ADVANCED_SETTINGS, menu_advanced_settings);
 
   #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
