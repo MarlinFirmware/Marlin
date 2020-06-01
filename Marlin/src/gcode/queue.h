ac2c83e3148857fa322c19deab23570bacf454c9
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-06-01 18:42:05 -0500

    Update queue.h

--- Marlin/src/gcode/queue.h
+++ Marlin/src/gcode/queue.h
@@ -52,19 +52,20 @@ public:
 
   static char command_buffer[BUFSIZE][MAX_CMD_SIZE];
 
-  /*
+  /**
    * The port that the command was received on
    */
   #if NUM_SERIAL > 1
     static int16_t port[BUFSIZE];
-    static int16_t command_port() {
-      return (0
-        #if NUM_SERIAL > 1
-          + port[index_r]
-        #endif
-      );
-    }
   #endif
+
+  static int16_t command_port() {
+    return (0
+      #if NUM_SERIAL > 1
+        + port[index_r]
+      #endif
+    );
+  }
 
   GCodeQueue();
 
