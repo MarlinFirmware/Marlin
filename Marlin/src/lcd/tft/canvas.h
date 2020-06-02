1badfff25c5862dddf74fb0ed7272acbcb2d251a
Scott Lahteine <github@thinkyhead.com>
2020-06-01 23:13:52 -0500

    Update (c), indentation

--- Marlin/src/lcd/tft/canvas.h
+++ Marlin/src/lcd/tft/canvas.h
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -19,14 +19,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
 #pragma once
 
-#include "../../inc/MarlinConfig.h"
 #include "tft_color.h"
 #include "tft_string.h"
 #include "tft_image.h"
 #include "tft.h"
+
+#include "../../inc/MarlinConfig.h"
 
 class CANVAS {
   private:
--- Marlin/src/lcd/tft/ui_320x240.cpp
+++ Marlin/src/lcd/tft/ui_320x240.cpp
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -46,7 +46,7 @@
 #endif
 
 #if !HAS_LCD_MENU
-  #error Seriously? High resolution TFT screen without menu?
+  #error "Seriously? High resolution TFT screen without menu?"
 #endif
 
 bool draw_menu_navigation = false;
