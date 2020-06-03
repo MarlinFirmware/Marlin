ea47689d7da561fc365f712ed74c570b7aa5ec2a
ellensp <ellensp@ellensp-HP-ProBook-6470b.fritz.box>
2020-06-03 19:48:57 +1200

    keep PERCENT and PWM at 3 digits, while RPM is 5 digits

--- Marlin/src/lcd/dogm/status_screen_DOGM.cpp
+++ Marlin/src/lcd/dogm/status_screen_DOGM.cpp
@@ -542,10 +542,11 @@ void MarlinUI::draw_status_screen() {
     // Laser / Spindle
     #if DO_DRAW_CUTTER
       if (cutter.power && PAGE_CONTAINS(STATUS_CUTTER_TEXT_Y - INFO_FONT_ASCENT, STATUS_CUTTER_TEXT_Y - 1)) {
-        lcd_put_u8str(STATUS_CUTTER_TEXT_X, STATUS_CUTTER_TEXT_Y, i16tostr5rj(cutter.power));
-        #if CUTTER_DISPLAY_IS(PERCENT)
+        #if CUTTER_DISPLAY_IS(PERCENT) || CUTTER_DISPLAY_IS(PWM)
+          lcd_put_u8str(STATUS_CUTTER_TEXT_X, STATUS_CUTTER_TEXT_Y, i16tostr3rj(cutter.power));
           lcd_put_wchar('%');
         #elif CUTTER_DISPLAY_IS(RPM)
+          lcd_put_u8str(STATUS_CUTTER_TEXT_X, STATUS_CUTTER_TEXT_Y, i16tostr5rj(cutter.power));
           lcd_put_wchar('K');
         #endif
       }
