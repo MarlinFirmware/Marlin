3d3b4c392ff464c87475e47aa7f7aac126a06f51
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-06-04 21:32:37 -0500

    Update pins_RAMPS_144.h

--- Marlin/src/pins/samd/pins_RAMPS_144.h
+++ Marlin/src/pins/samd/pins_RAMPS_144.h
@@ -473,7 +473,7 @@
       //  #define KILL_PIN         41
       //#endif
 
-      #if EITHER(MKS_MINI_12864, MKS_LCD12864)                 // Added in Marlin 1.1.6
+      #if EITHER(MKS_MINI_12864, MKS_LCD12864)
 
         // TO TEST
         //#define DOGLCD_A0                   27
