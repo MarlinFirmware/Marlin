a0ed8038c0c4aa9e40bf3e2523f657a5659b9922
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-06-07 23:29:44 -0500

    Update endstops.h

--- Marlin/src/module/endstops.h
+++ Marlin/src/module/endstops.h
@@ -144,7 +144,7 @@ class Endstops {
     #if ENABLED(VALIDATE_HOMING_ENDSTOPS)
       // If the last move failed to trigger an endstop, call kill
       static void validate_homing_move();
-      FORCE_INLINE static bool checkEndStop() { return trigger_state() != 0; }
+      FORCE_INLINE static bool any() { return trigger_state() != 0; }
     #else
       FORCE_INLINE static void validate_homing_move() { hit_on_purpose(); }
     #endif
