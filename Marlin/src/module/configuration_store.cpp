6eda5355c4cce87756048ae799744f1e16723fe0
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-06-08 03:24:10 -0500

    Update configuration_store.cpp

--- Marlin/src/module/configuration_store.cpp
+++ Marlin/src/module/configuration_store.cpp
@@ -37,7 +37,7 @@
  */
 
 // Change EEPROM version if the structure changes
-#define EEPROM_VERSION "V79"
+#define EEPROM_VERSION "V80"
 #define EEPROM_OFFSET 100
 
 // Check the integrity of data offsets.
