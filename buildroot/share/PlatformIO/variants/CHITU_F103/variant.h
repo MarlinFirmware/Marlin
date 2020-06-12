c68343249c432432e8eafb2e183a40f21e2aa5b3
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-06-11 19:35:17 -0500

    wax on

--- buildroot/share/PlatformIO/variants/CHITU_F103/variant.h
+++ buildroot/share/PlatformIO/variants/CHITU_F103/variant.h
@@ -1,12 +1,11 @@
-#ifndef _VARIANT_ARDUINO_STM32_
-#define _VARIANT_ARDUINO_STM32_
+#pragma once
 
 #define digitalPinToPort(P)        ( PIN_MAP[P].gpio_device )
 #define digitalPinToBitMask(P)     ( BIT(PIN_MAP[P].gpio_bit) )
 #define portOutputRegister(port)   ( &(port->regs->ODR) )
 #define portInputRegister(port)    ( &(port->regs->IDR) )
 
-#define portSetRegister(pin)		( &(PIN_MAP[pin].gpio_device->regs->BSRR) )
+#define portSetRegister(pin)		  ( &(PIN_MAP[pin].gpio_device->regs->BSRR) )
 #define portClearRegister(pin)		( &(PIN_MAP[pin].gpio_device->regs->BRR) )
 
 #define portConfigRegister(pin)		( &(PIN_MAP[pin].gpio_device->regs->CRL) )
@@ -16,5 +15,3 @@ static const uint8_t SS1  = BOARD_SPI2_NSS_PIN;
 static const uint8_t MOSI = BOARD_SPI1_MOSI_PIN;
 static const uint8_t MISO = BOARD_SPI1_MISO_PIN;
 static const uint8_t SCK  = BOARD_SPI1_SCK_PIN;
-
-#endif /* _VARIANT_ARDUINO_STM32_ */
