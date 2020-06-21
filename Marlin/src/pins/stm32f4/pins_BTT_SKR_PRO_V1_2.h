e424dfc8854378ae9ea4448770a5510b070654b4
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-06-21 01:46:31 -0700

    BigTreeTech SKR Pro 1.2 (#18366)

--- Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_V1_2.h
+++ Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_V1_2.h
@@ -22,9 +22,9 @@
 #pragma once
 
 #if HOTENDS > 3 || E_STEPPERS > 3
-  #error "BIGTREE SKR Pro V1.1 supports up to 3 hotends / E-steppers."
+  #error "BIGTREE SKR Pro V1.2 supports up to 3 hotends / E-steppers."
 #endif
 
-#define BOARD_INFO_NAME "BTT SKR Pro V1.1"
+#define BOARD_INFO_NAME "BTT SKR Pro V1.2"
 
 #include "pins_BTT_SKR_PRO_common.h"
