aeb8b0262bded6ed9409e67a8118ff833395c8a1
Gazooma <66881168+Gazooma@users.noreply.github.com>
2020-06-22 08:15:20 +0100

    v4

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -489,9 +489,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  #define DEFAULT_Kp 22.27
+  #define DEFAULT_Ki 1.98
+  #define DEFAULT_Kd 62.47
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -522,7 +522,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -540,9 +540,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 82.19
+  #define DEFAULT_bedKi 16.18
+  #define DEFAULT_bedKd 278.28
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
