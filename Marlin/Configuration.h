b15c131e06900a92571c5c9d03da542395cfedd9
Gazooma <66881168+Gazooma@users.noreply.github.com>
2020-06-23 10:17:06 +0100

    v7

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1708,7 +1708,7 @@
 // This option overrides the default number of encoder pulses needed to
 // produce one step. Should be increased for high-resolution encoders.
 //
-//#define ENCODER_PULSES_PER_STEP 4
+#define ENCODER_PULSES_PER_STEP 4
 
 //
 // Use this option to override the number of step signals required to
