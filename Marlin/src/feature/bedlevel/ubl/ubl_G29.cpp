25c7c43a829e3bbc581aae20d24b724a7ad8fbdb
Fabio Santos <fabiosan@live.com>
2020-06-22 19:12:45 -0700

    Add SERIAL_FLOAT_PRECISION option (#18367)

--- Marlin/src/feature/bedlevel/ubl/ubl_G29.cpp
+++ Marlin/src/feature/bedlevel/ubl/ubl_G29.cpp
@@ -433,7 +433,7 @@
             if (g29_verbose_level > 1) {
               SERIAL_ECHOPAIR("Probing around (", g29_pos.x);
               SERIAL_CHAR(',');
-              SERIAL_ECHO(g29_pos.y);
+              SERIAL_DECIMAL(g29_pos.y);
               SERIAL_ECHOLNPGM(").\n");
             }
             const xy_pos_t near_probe_xy = g29_pos + probe.offset_xy;
