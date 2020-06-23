5fe247acd2f6e12cb995d94c1af9888383160598
meltonpieman <51961825+meltonpieman@users.noreply.github.com>
2020-06-23 16:58:47 +0100

    extra meshes added to menu

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2851,8 +2851,8 @@
   #define USER_DESC_1 "Home & show UBL Info" // UBL
   #define USER_GCODE_1 "G28\nG29 W" // UBL
 
-  #define USER_DESC_2 "Probe UBL Slot 0" //Use probe to setup UBL. // UBL
-  #define USER_GCODE_2 "M190 S60\nG28\nG29 P1\nG29 P3\nG29 S0\nG29 A\nG29 F 10.0\nM500\nM140 S0\nM0 Mesh Saved Slot 0" // heat bed, home, UBL, fade // UBL 
+  #define USER_DESC_2 "Probe UBL Slot 0 PEI p coated" //Use probe to setup UBL. // 20/06/23
+  #define USER_GCODE_2 "M190 S60\nG28\nG29 P1\nG29 P3\nG29 S0\nG29 A\nG29 F 10.0\nM500\nM140 S0\nM0 Mesh Saved Slot 0 PEI p coated" // heat bed, home, UBL, fade // 20/06/23
 
   #define USER_DESC_3 "UBL Z Offset Step 1" // UBL
   #define USER_GCODE_3 "G28\nG1 X117.5 Y117.5 Z0.00\nM211 S0\nM0 Lower Z Offset" // home, centre bed, software endstops off, wait to do offset // UBL
@@ -2866,6 +2866,12 @@
   #define USER_DESC_6 "Mesh Tilt 3 point level" //Tilt mesh to account for changes of knobs under the bed // UBL
   #define USER_GCODE_6 "G29 J2\nM500" // 3 point level, save eprom // UBL
 
+  #define USER_DESC_7 "Probe UBL Slot 1 PEI smooth " //Use probe to setup UBL. // 20/06/23
+  #define USER_GCODE_7 "M190 S60\nG28\nG29 P1\nG29 P3\nG29 S1\nG29 A\nG29 F 10.0\nM500\nM140 S0\nM0 Mesh Saved Slot 1 PEI smooth" // heat bed, home, UBL, fade // 20/06/23
+
+  #define USER_DESC_8 "Probe UBL Slot 2 Ender bed" //Use probe to setup UBL. // 20/06/23
+  #define USER_GCODE_8 "M190 S60\nG28\nG29 P1\nG29 P3\nG29 S2\nG29 A\nG29 F 10.0\nM500\nM140 S0\nM0 Mesh Saved Slot 2 Ender bed" // heat bed, home, UBL, fade // 20/06/23
+  
   //#define USER_DESC_2 "Preheat for " PREHEAT_1_LABEL
   //#define USER_GCODE_2 "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
 
