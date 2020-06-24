c3d2ffc09c8a15398894bb2050397b9653878923
Scott Lahteine <github@thinkyhead.com>
2020-06-23 20:37:19 -0500

    Board-based ST7920 delays

--- Marlin/src/pins/stm32f1/pins_GTM32_REV_B.h
+++ Marlin/src/pins/stm32f1/pins_GTM32_REV_B.h
@@ -169,14 +169,14 @@
   #endif
 
   #if HAS_GRAPHICAL_LCD
-    #ifndef ST7920_DELAY_1
-      #define ST7920_DELAY_1        DELAY_NS(96)
+    #ifndef BOARD_ST7920_DELAY_1
+      #define BOARD_ST7920_DELAY_1  DELAY_NS(96)
     #endif
-    #ifndef ST7920_DELAY_2
-      #define ST7920_DELAY_2        DELAY_NS(48)
+    #ifndef BOARD_ST7920_DELAY_2
+      #define BOARD_ST7920_DELAY_2  DELAY_NS(48)
     #endif
-    #ifndef ST7920_DELAY_3
-      #define ST7920_DELAY_3       DELAY_NS(715)
+    #ifndef BOARD_ST7920_DELAY_3
+      #define BOARD_ST7920_DELAY_3 DELAY_NS(715)
     #endif
   #endif
 
