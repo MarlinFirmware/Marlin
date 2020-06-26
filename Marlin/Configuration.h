c01b7f21c041bc47bfd6c8cefec5b3cfe4fee792
Gazooma <66881168+Gazooma@users.noreply.github.com>
2020-06-26 21:05:19 +0100

    v10

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1374,8 +1374,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT 50  // X point for Z homing when homing all axes (G28).
-  #define Z_SAFE_HOMING_Y_POINT 10  // Y point for Z homing when homing all axes (G28).
+  #define Z_SAFE_HOMING_X_POINT 65  // X point for Z homing when homing all axes (G28).
+  #define Z_SAFE_HOMING_Y_POINT 15  // Y point for Z homing when homing all axes (G28).
 #endif
 
 // Homing speeds (mm/m)
