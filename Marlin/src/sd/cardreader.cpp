b42a8589ee736db825f01efc7a9d8a5f7f2d3c11
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-07-01 01:11:15 -0500

    Update cardreader.cpp

--- Marlin/src/sd/cardreader.cpp
+++ Marlin/src/sd/cardreader.cpp
@@ -403,7 +403,7 @@ void CardReader::manage_media() {
     if (stat) {                       // Media Inserted
       safe_delay(500);                // Some boards need a delay to get settled
       mount();                        // Try to mount the media
-      #if MB(FYSETC_CHEETAH,FYSETC_CHEETAH_V12, FYSETC_AIO_II)
+      #if MB(FYSETC_CHEETAH, FYSETC_CHEETAH_V12, FYSETC_AIO_II)
         reset_stepper_drivers();      // Workaround for Cheetah bug
       #endif
       if (!isMounted()) stat = 0;     // Not mounted?
