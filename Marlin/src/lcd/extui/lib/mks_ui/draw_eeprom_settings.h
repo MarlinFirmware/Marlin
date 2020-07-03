084b192019e18d4517e3dd32bf9d27d39584f63b
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-07-03 10:49:29 -0500

    Update draw_eeprom_settings.h

--- Marlin/src/lcd/extui/lib/mks_ui/draw_eeprom_settings.h
+++ Marlin/src/lcd/extui/lib/mks_ui/draw_eeprom_settings.h
@@ -28,9 +28,6 @@ extern "C" { /* C-declarations for C++ */
 extern void lv_draw_eeprom_settings(void);
 extern void lv_clear_eeprom_settings();
 
-
 #ifdef __cplusplus
 } /* C-declarations for C++ */
 #endif
-
-
