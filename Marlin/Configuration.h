93628b15d980fef92755ebe2e2dd7cb4d875b42b
Gazooma <66881168+Gazooma@users.noreply.github.com>
2020-07-03 19:32:41 +0100

    v12

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -972,7 +972,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -5, -0.8 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -5, -2.2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1299,7 +1299,7 @@
 
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 0              // Set Mesh bounds as an inset region of the bed
+  #define MESH_INSET 10              // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
