02526eab9459c63c27990423ea6a4f09b3b872e1
Jeff <jeff@j.com>
2020-07-04 14:33:22 -0400

    Update fastio.h

--- Marlin/src/HAL/STM32/fastio.h
+++ Marlin/src/HAL/STM32/fastio.h
@@ -80,7 +80,7 @@ void FastIO_init(); // Must be called before using fast io macros
 #define IS_INPUT(IO)
 #define IS_OUTPUT(IO)
 
-#define PWM_PIN(P)              digitalPinHasPWM(P)
+#define PWM_PIN(P)              true //digitalPinHasPWM(P) this has to be changed to pass sanity checks for PWM on Extruder Auto Fans
 
 // digitalRead/Write wrappers
 #define extDigitalRead(IO)    digitalRead(IO)
