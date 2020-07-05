d0a563cfe37b0656fb61ecf1eef7313057d88a8f
bill-orange <billwebb12@gmail.com>
2020-07-04 21:26:10 -0700

    BABYSTEP_MILLIMETER_UNITS

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1553,6 +1553,7 @@
   #define INTEGRATED_BABYSTEPPING         // EXPERIMENTAL integration of babystepping into the Stepper ISR
   //#define BABYSTEP_WITHOUT_HOMING
   //#define BABYSTEP_XY                     // Also enable X/Y Babystepping. Not supported on DELTA!
+  #define BABYSTEP_MILLIMETER_UNITS         // Use millimeters directly for babystepping
   #define BABYSTEP_INVERT_Z false           // Change if Z babysteps should go the other way
   #define BABYSTEP_MULTIPLICATOR_Z  8       // Babysteps are very small. Increase for faster motion.
   #define BABYSTEP_MULTIPLICATOR_XY 1
