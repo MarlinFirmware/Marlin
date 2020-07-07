588d40aedf4d0f148186280cf6f387f5436302c2
notabucketofspam <43456540+notabucketofspam@users.noreply.github.com>
2020-07-07 07:55:01 -0400

    Fix typo in Configuration_adv.h (#18570)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1829,7 +1829,7 @@
 
 // @section motion
 
-// The number of lineear moves that can be in the planner at once.
+// The number of linear moves that can be in the planner at once.
 // The value of BLOCK_BUFFER_SIZE must be a power of 2 (e.g. 8, 16, 32)
 #if BOTH(SDSUPPORT, DIRECT_STEPPING)
   #define BLOCK_BUFFER_SIZE  8
