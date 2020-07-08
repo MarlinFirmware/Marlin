9e01fa5cd22a46932deb6edae77e591f1c64f614
GMagician <GMagician@users.noreply.github.com>
2020-07-08 22:28:47 +0200

    Remove no more valid remark

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1162,7 +1162,6 @@
  *
  * RAMPS-based boards use SERVO3_PIN for the first runout sensor.
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
- * By default the firmware assumes HIGH=FILAMENT PRESENT.
  */
 //#define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
