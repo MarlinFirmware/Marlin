babcd8c26f0e18511dfe21da9f98b1107dea0029
bill-orange <billwebb12@gmail.com>
2020-07-08 18:42:15 -0700

    added Print Counter

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1634,7 +1634,7 @@
  *
  * View the current statistics with M78.
  */
-//#define PRINTCOUNTER
+#define PRINTCOUNTER
 
 //=============================================================================
 //============================= LCD and SD support ============================
