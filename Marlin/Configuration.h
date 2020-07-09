8ea82a380c59ee6c708e39fb095cc6e07ed2b4cc
Gazooma <66881168+Gazooma@users.noreply.github.com>
2020-07-09 22:35:16 +0100

    v15

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1079,7 +1079,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
