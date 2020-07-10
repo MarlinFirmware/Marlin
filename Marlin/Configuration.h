db49c11dd2bfda39d8e78474e94141dece042d85
John BouAntoun <jbouantoun@deloitte.com.au>
2020-07-10 13:32:38 +1000

    Max feedrate on extruder is way too low

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -762,7 +762,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 60 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
