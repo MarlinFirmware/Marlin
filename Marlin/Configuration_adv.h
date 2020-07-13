15eee3622c36b52e0c60d3009198be8b37a41851
bill-orange <billwebb12@gmail.com>
2020-07-12 21:11:59 -0700

    correct increment for mm babystepping

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1555,7 +1555,7 @@
   //#define BABYSTEP_XY                     // Also enable X/Y Babystepping. Not supported on DELTA!
   #define BABYSTEP_MILLIMETER_UNITS         // Use millimeters directly for babystepping
   #define BABYSTEP_INVERT_Z false           // Change if Z babysteps should go the other way
-  #define BABYSTEP_MULTIPLICATOR_Z  8       // Babysteps are very small. Increase for faster motion.
+  #define BABYSTEP_MULTIPLICATOR_Z  0.02       // Babysteps are very small. Increase for faster motion.
   #define BABYSTEP_MULTIPLICATOR_XY 1
 
   //#define DOUBLECLICK_FOR_Z_BABYSTEPPING  // Double-click on the Status Screen for Z Babystepping.
