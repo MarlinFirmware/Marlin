b6f3c65eb33cab0200b324cf12e4767c7f701e5c
Gazooma <66881168+Gazooma@users.noreply.github.com>
2020-07-15 19:23:09 +0100

    v21

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "3D Printer"
+#define CUSTOM_MACHINE_NAME "Geeetech A10"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
