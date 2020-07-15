9f5fc31dbebe05ef2913edd369fc25c8d8daa5b1
Gazooma <66881168+Gazooma@users.noreply.github.com>
2020-07-16 00:01:23 +0100

    v22

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2369,7 +2369,7 @@
    */
   #define STEALTHCHOP_XY
   #define STEALTHCHOP_Z
-  #define STEALTHCHOP_E
+  //#define STEALTHCHOP_E
 
   /**
    * Optimize spreadCycle chopper parameters by using predefined parameter sets
