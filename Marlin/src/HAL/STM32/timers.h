a7bd94cc67edf5e2623ec8988358f879391767f9
Jason Smith <jason.inet@gmail.com>
2020-07-19 00:48:15 -0700

    Fix BLTouch PWM reliability in HAL/STM32

--- Marlin/src/HAL/STM32/timers.h
+++ Marlin/src/HAL/STM32/timers.h
@@ -86,8 +86,9 @@ void HAL_timer_enable_interrupt(const uint8_t timer_num);
 void HAL_timer_disable_interrupt(const uint8_t timer_num);
 bool HAL_timer_interrupt_enabled(const uint8_t timer_num);
 
+// Configure timer priorities for peripherals such as Software Serial or Servos.
 // Exposed here to allow all timer priority information to reside in timers.cpp
-void SetSoftwareSerialTimerInterruptPriority();
+void SetTimerInterruptPriorities();
 
 //TIM_TypeDef* HAL_timer_device(const uint8_t timer_num); no need to be public for now. not public = not used externally
 
