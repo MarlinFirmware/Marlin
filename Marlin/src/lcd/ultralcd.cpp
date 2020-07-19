434673167a7b141c314b8be3bca68890c88ded43
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-07-19 15:56:44 -0500

    Tweaks while reviewing

--- Marlin/src/lcd/ultralcd.cpp
+++ Marlin/src/lcd/ultralcd.cpp
@@ -902,20 +902,21 @@ void MarlinUI::update() {
         // When reversing the encoder direction, a movement step can be missed because
         // encoderDiff has a non-zero residual value, making the controller unresponsive.
         // The fix clears the residual value when the encoder is reversed.
-        // Also check if passed half the threshold. This will compensate for missed single steps.
+        // Also check if past half the threshold to compensate for missed single steps.
         static int8_t lastEncoderDiff;
+        int8_t prevDiff = lastEncoderDiff;
+        lastEncoderDiff = encoderDiff;  // Store before updating encoderDiff to save actual steps
+
         // When not past threshold, and reversing... or past half the threshold
-        if ((encoderDiff != 0 && abs_diff < (ENCODER_PULSES_PER_STEP)) && // Not past threshold
-            (
-              abs_diff > (ENCODER_PULSES_PER_STEP) / 2 || // Passed half the threshold
-              (ABS(encoderDiff - lastEncoderDiff) >= (ENCODER_PULSES_PER_STEP) && ABS(lastEncoderDiff) < (ENCODER_PULSES_PER_STEP)) // Or direction change through zero
-            )) {
-            lastEncoderDiff = encoderDiff; // Store before updating encoderDiff, we want actual steps
-            encoderDiff = (encoderDiff < 0 ? -1 : 1) * (ENCODER_PULSES_PER_STEP); // Treat as full step
-            abs_diff = ENCODER_PULSES_PER_STEP;
-        }
-        else {
-          lastEncoderDiff = encoderDiff;
+        if (WITHIN(abs_diff, 1, (ENCODER_PULSES_PER_STEP) - 1)  // Not past threshold
+          && (abs_diff > (ENCODER_PULSES_PER_STEP) / 2          // Passed half the threshold?
+            || (ABS(encoderDiff - prevDiff) >= (ENCODER_PULSES_PER_STEP)  // Or a large change...
+                && ABS(prevDiff) < (ENCODER_PULSES_PER_STEP)    // ...starting from a partial step?
+               )
+             )
+        ) {
+          encoderDiff = (encoderDiff < 0 ? -1 : 1) * (ENCODER_PULSES_PER_STEP); // Treat as full step
+          abs_diff = ENCODER_PULSES_PER_STEP;
         }
       #endif
 
