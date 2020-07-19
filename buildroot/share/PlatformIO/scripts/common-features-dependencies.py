ee4c9a288eff691d97981877df01aea95344fa07
Scott Lahteine <github@thinkyhead.com>
2020-07-19 04:19:51 -0500

    Remaining common lib_deps

--- buildroot/share/PlatformIO/scripts/common-features-dependencies.py
+++ buildroot/share/PlatformIO/scripts/common-features-dependencies.py
@@ -11,25 +11,67 @@ except ImportError:
 
 Import("env")
 
+TMCSTEPPER        = 'TMCStepper@~0.7.1'
+SAILFISHLCD       = 'SailfishLCD=https://github.com/mikeshub/SailfishLCD/archive/master.zip'
+SLOWSOFTI2CMASTER = 'SlowSoftI2CMaster'
+TMC26XSTEPPER     = 'TMC26XStepper=https://github.com/trinamic/TMC26XStepper/archive/master.zip'
+L64XX             = 'Arduino-L6470@0.8.0'
+NEOPIXEL          = 'Adafruit NeoPixel@1.5.0'
+MAX31865          = 'Adafruit MAX31865 library@~1.1.0'
+U8GLIB            = 'U8glib-HAL@0.4.1'
+LIQUIDCRYSTAL     = 'LiquidCrystal@1.5.0'
+LIQUIDTWI2        = 'LiquidTWI2@1.2.7'
+LITTLEVGL         = 'MKS-LittlevGL=https://github.com/makerbase-mks/MKS-LittlevGL/archive/master.zip'
+
 FEATURE_DEPENDENCIES = {
 	'TFT_LVGL_UI': {
-		'lib_deps': ['MKS-LittlevGL=https://github.com/makerbase-mks/MKS-LittlevGL/archive/master.zip'],
+		'lib_deps': [LITTLEVGL],
 		# not supported yet
 		#'src_filter': '+<src/lcd/extui/lib/mks_ui>',
 		# script in another PR
-		#'extra_scripts': 'buildroot/share/PlatformIO/scripts/dowload_mks_assets.py', 
+		#'extra_scripts': 'buildroot/share/PlatformIO/scripts/dowload_mks_assets.py',
 	},
 	'HAS_TRINAMIC': {
-		'lib_deps': ['TMCStepper@~0.7.1']
+		'lib_deps': [TMCSTEPPER]
+	},
+	'SR_LCD_2W_NL': {
+		'lib_deps': [SAILFISHLCD]
+	},
+	'SR_LCD_3W_NL': {
+		'lib_deps': [SAILFISHLCD]
+	},
+	'DIGIPOT_MCP4018': {
+		'lib_deps': [SLOWSOFTI2CMASTER]
+	},
+	'DIGIPOT_MCP4451': {
+		'lib_deps': [SLOWSOFTI2CMASTER]
+	},
+	'HAS_TMC26X': {
+		'lib_deps': [TMC26XSTEPPER]
+	},
+	'HAS_L64XX': {
+		'lib_deps': [L64XX]
+	},
+	'NEOPIXEL_LED': {
+		'lib_deps': [NEOPIXEL]
+	},
+	'MAX6675_IS_MAX31865': {
+		'lib_deps': [MAX31865]
+	},
+	'HAS_GRAPHICAL_LCD': {
+		'lib_deps': [U8GLIB]
+	},
+	'HAS_CHARACTER_LCD': {
+		'lib_deps': [LIQUIDCRYSTAL, LIQUIDTWI2]
 	}
 }
 
 def install_features_dependencies():
 	for feature in FEATURE_DEPENDENCIES:
-		if env.MarlinFeatureIsEnabled(feature) == False:
+		if not env.MarlinFeatureIsEnabled(feature):
 			continue
 
-		print("Feature enabled: %s" % feature)
+		#print("Feature enabled: %s" % feature)
 		if 'lib_deps' in FEATURE_DEPENDENCIES[feature]:
 			print("Adding lib_deps for %s... " % feature)
 			proj = env.GetProjectConfig()
@@ -45,7 +87,7 @@ def install_features_dependencies():
 def load_marlin_features():
 	if "MARLIN_FEATURES" in env:
 		return
-	
+
 	# procces defines
 	build_flags = env.get('BUILD_FLAGS')
 	build_flags = env.ParseFlagsExtended(build_flags)
