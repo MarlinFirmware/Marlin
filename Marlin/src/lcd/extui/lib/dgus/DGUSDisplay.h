e448e8161c825fcbd25a750f7352ec9cad01d6cd
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-07-21 03:18:01 -0500

    Update DGUSDisplay.h

--- Marlin/src/lcd/extui/lib/dgus/DGUSDisplay.h
+++ Marlin/src/lcd/extui/lib/dgus/DGUSDisplay.h
@@ -102,8 +102,8 @@ private:
   static bool Initialized, no_reentrance;
 };
 
-#define GET_VARIABLE(f, t, ...) (&DGUSDisplay::GetVariable<decltype(t), f, t, ##__VA_ARGS__>)
-#define SET_VARIABLE(f, t, ...) (&DGUSDisplay::SetVariable<decltype(t), f, t, ##__VA_ARGS__>)
+#define GET_VARIABLE(f, t, V...) (&DGUSDisplay::GetVariable<decltype(t), f, t, ##V>)
+#define SET_VARIABLE(f, t, V...) (&DGUSDisplay::SetVariable<decltype(t), f, t, ##V>)
 
 extern DGUSDisplay dgusdisplay;
 
