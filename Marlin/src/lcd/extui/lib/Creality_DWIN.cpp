638bac84cf7f49e6ebefa37240c48f7c78553bb8
InsanityAutomation <d.menzel@insanityautomation.com>
2020-07-22 21:31:39 -0400

    Update Creality_DWIN.cpp

--- Marlin/src/lcd/extui/lib/Creality_DWIN.cpp
+++ Marlin/src/lcd/extui/lib/Creality_DWIN.cpp
@@ -1511,12 +1511,9 @@ SERIAL_ECHOLN(PSTR("BeginSwitch"));
       if (recdat.data[0] == 1) //Filament is out, resume / resume selected on screen
       {
         if(
-<<<<<<< HEAD
           #if (FIL_RUNOUT_STATE == LOW)
             #define FIL_RUNOUT_INVERTING true
           #endif
-=======
->>>>>>> Rebase and catchup
         #if DISABLED(FILAMENT_RUNOUT_SENSOR) || ENABLED(FILAMENT_MOTION_SENSOR)
           true
         #elif NUM_RUNOUT_SENSORS > 1
