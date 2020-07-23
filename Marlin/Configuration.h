ad7131ec8ba2fa67fc23fae32d1eeaf8da4bfe01
miferkolb <61791039+miferkolb@users.noreply.github.com>
2020-07-23 11:07:08 +0200

    Cambio de nombre en línea 74

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(bq Witbox)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(MFK bq Witbox)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
