18a63809321c54f807de73b5f0005ccd98fb151d
Frank <flodejr@gmail.com>
2020-07-24 21:44:30 +0800

    Forcefully inject G28 before G29 on Bed Level Selection.  Bug #18733

--- Marlin/src/lcd/dwin/dwin.cpp
+++ Marlin/src/lcd/dwin/dwin.cpp
@@ -2374,7 +2374,7 @@ void HMI_Control(void) {
 void HMI_Leveling(void) {
   Popup_Window_Leveling();
   DWIN_UpdateLCD();
-  queue.inject_P(PSTR("G29"));
+  queue.inject_P(PSTR("G28\nG29"));
 }
 
 /* Axis Move */
