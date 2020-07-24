1d07c274a7d59bd3249204793db82beb5d427c19
Matt Waddilove <mattheww@unity3d.com>
2020-07-24 10:14:16 +0100

    set up basics for the GTR & TMC2209

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2500,7 +2500,7 @@
    * IMPROVE_HOMING_RELIABILITY tunes acceleration and jerk when
    * homing and adds a guard period for endstop triggering.
    */
-  //#define SENSORLESS_HOMING // StallGuard capable drivers only
+  #define SENSORLESS_HOMING // StallGuard capable drivers only
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
