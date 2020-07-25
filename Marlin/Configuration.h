bdf07c32a91b06636c5216ac284e37935809c8cb
Scott Lahteine <github@thinkyhead.com>
2020-07-25 00:04:04 -0500

    Update some hyperlinks

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -135,7 +135,7 @@
 //#define CUSTOM_MACHINE_NAME "3D Printer"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
-// Choose your own or use a service like http://www.uuidgenerator.net/version4
+// Choose your own or use a service like https://www.uuidgenerator.net/version4
 //#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
 
 // @section extruder
