3a8a6f3a272668338395d896b43fee75e75a2328
Matt Waddilove <mattheww@unity3d.com>
2020-07-26 10:41:02 +0100

    PWM_PIN is not necessarily available at compile time.

--- Marlin/src/inc/SanityCheck.h
+++ Marlin/src/inc/SanityCheck.h
@@ -2945,8 +2945,10 @@ static_assert(   _ARR_TEST(3,0) && _ARR_TEST(3,1) && _ARR_TEST(3,2)
   #elif ENABLED(SPINDLE_LASER_PWM)
     #if !defined(SPINDLE_LASER_PWM_PIN) || SPINDLE_LASER_PWM_PIN < 0
       #error "SPINDLE_LASER_PWM_PIN is required for SPINDLE_LASER_PWM."
-    #elif !PWM_PIN(SPINDLE_LASER_PWM_PIN)
-      #error "SPINDLE_LASER_PWM_PIN not assigned to a PWM pin."
+    #elif DISABLED(NO_COMPILE_TIME_PWM)
+      #if !PWM_PIN(SPINDLE_LASER_PWM_PIN)
+        #error "SPINDLE_LASER_PWM_PIN not assigned to a PWM pin."
+      #endif
     #elif !defined(SPINDLE_LASER_PWM_INVERT)
       #error "SPINDLE_LASER_PWM_INVERT is required for (SPINDLE|LASER)_FEATURE."
     #elif !(defined(SPEED_POWER_INTERCEPT) && defined(SPEED_POWER_MIN) && defined(SPEED_POWER_MAX) && defined(SPEED_POWER_STARTUP))
