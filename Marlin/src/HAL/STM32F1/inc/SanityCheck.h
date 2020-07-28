592e7c3c5f8333f08f51d6ca93b9bacbf3e0dc62
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2020-07-28 15:42:25 -0300

    Adafruit NeoPixel isnt support in stm32f1

--- Marlin/src/HAL/STM32F1/inc/SanityCheck.h
+++ Marlin/src/HAL/STM32F1/inc/SanityCheck.h
@@ -51,3 +51,7 @@
 #elif ENABLED(SERIAL_STATS_DROPPED_RX)
   #error "SERIAL_STATS_DROPPED_RX is not supported on this platform."
 #endif
+
+#if ENABLED(NEOPIXEL_LED)
+  #error "NEOPIXEL_LED (Adafruit NeoPixel) is not supported for this platform."
+#endif
