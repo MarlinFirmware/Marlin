2c99c1c448d39e8917611733e97d53c5d7c7e53e
miferkolb <61791039+miferkolb@users.noreply.github.com>
2020-07-30 10:32:55 +0200

    Changed Witbox to 4 printheads and activated temp_sensor 0 to 3 and bed temp_sensor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -142,7 +142,7 @@
 
 // This defines the number of extruders
 // :[1, 2, 3, 4, 5, 6, 7, 8]
-#define EXTRUDERS 3 //Witbox
+#define EXTRUDERS 4 //Witbox
 
 // Generally expected filament diameter (1.75, 2.85, 3.0, ...). Used for Volumetric, Filament Width Sensor, etc.
 #define DEFAULT_NOMINAL_FILAMENT_DIA 3.0
@@ -409,12 +409,12 @@
 #define TEMP_SENSOR_0 1
 #define TEMP_SENSOR_1 1
 #define TEMP_SENSOR_2 1
-#define TEMP_SENSOR_3 0
+#define TEMP_SENSOR_3 1
 #define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_5 0
 #define TEMP_SENSOR_6 0
 #define TEMP_SENSOR_7 0
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_BED 1
 #define TEMP_SENSOR_PROBE 0
 #define TEMP_SENSOR_CHAMBER 0
 
