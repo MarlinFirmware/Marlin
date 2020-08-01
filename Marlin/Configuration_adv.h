132e3d8149ca91ef2b70ecb2e92c8ca0b532e78f
dspearing <david@spearing.net>
2020-08-01 17:10:57 -0400

    Configured BLT for 5v and removed SW Mode

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -668,7 +668,7 @@
 
   // Feature: Switch into SW mode after a deploy. It makes the output pulse longer. Can be useful
   //          in special cases, like noisy or filtered input configurations.
-  #define BLTOUCH_FORCE_SW_MODE
+//#define BLTOUCH_FORCE_SW_MODE
 
   /**
    * Settings for BLTouch Smart 3.0 and 3.1
@@ -686,7 +686,7 @@
    * differs, a mode set eeprom write will be completed at initialization.
    * Use the option below to force an eeprom write to a V3.1 probe regardless.
    */
-//#define BLTOUCH_SET_5V_MODE
+#define BLTOUCH_SET_5V_MODE
 
   /**
    * Safety: Activate if connecting a probe with an unknown voltage mode.
