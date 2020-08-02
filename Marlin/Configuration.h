6ae1ac364bd61c328cb35992da5435b28ef9a414
Steve Hooker <thewitchphotographer@gmail.com>
2020-08-02 18:14:10 +0100

    increase rez

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,8 +71,9 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-//steps per unit and x_min_pos, manual_x_hom_pos, nozzle_to_probe_offset ###### Tue Jul 14 11:16:35 BST 2020
-//change to TMC2209 ###### Sun Jul 19 16:48:45 BST 2020
+//steps per unit and x_min_pos, manual_x_hom_pos, nozzle_to_probe_offset  Tue Jul 14 11:16:35 BST 2020
+//change to TMC2209  Sun Jul 19 16:48:45 BST 2020
+//add resolution with microsteps  Sun Aug 2 18:06:40 BST 2020
 #define STRING_CONFIG_H_AUTHOR "(Steve, SKR 1.4 Turbo, TMC2209)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
@@ -745,14 +746,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 409.13 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 400, 409.13 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2210,7 +2210,7 @@
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
-    #define X_MICROSTEPS     16    // 0..256
+    #define X_MICROSTEPS     64    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
   #endif
@@ -2226,7 +2226,7 @@
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       800
     #define Y_CURRENT_HOME  Y_CURRENT
-    #define Y_MICROSTEPS     16
+    #define Y_MICROSTEPS     64
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
   #endif
