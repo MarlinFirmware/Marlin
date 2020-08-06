2d753b8c90ccf3736d8bb8359459631f4c95bacd
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-08-06 17:40:22 -0500

    Move similar things closer

--- Marlin/src/lcd/dogm/ultralcd_DOGM.h
+++ Marlin/src/lcd/dogm/ultralcd_DOGM.h
@@ -103,30 +103,47 @@
 #elif ENABLED(U8GLIB_SSD1306)
 
   #if ENABLED(ZONESTAR_12864OLED)
-    // Generic SSD1306 OLED SPI LCD  
-    #define FORCE_SOFT_SPI                                        // SW-SPI
+    // Generic SSD1306 OLED SPI LCD
+    #define FORCE_SOFT_SPI                                      // SW-SPI
     #if ENABLED(ALTERNATIVE_LCD)
-      #define U8G_CLASS U8GLIB_SH1106_128X64_2X                   // 4 stripes
+      #define U8G_CLASS U8GLIB_SH1106_128X64_2X                 // 4 stripes
     #else
-      #define U8G_CLASS U8GLIB_SH1106_128X64                      // 8 stripes
+      #define U8G_CLASS U8GLIB_SH1106_128X64                    // 8 stripes
     #endif
   #else
-
     // Generic SSD1306 OLED I2C LCD
     #if ENABLED(ALTERNATIVE_LCD)
-      #define U8G_CLASS U8GLIB_SSD1306_128X64_2X_I2C_2_WIRE       // 4 stripes
+      #define U8G_CLASS U8GLIB_SSD1306_128X64_2X_I2C_2_WIRE     // 4 stripes
     #else
-      #define U8G_CLASS U8GLIB_SSD1306_128X64_2X                  // 4 stripes
+      #define U8G_CLASS U8GLIB_SSD1306_128X64_2X                // 4 stripes
     #endif
     #define U8G_PARAM (U8G_I2C_OPT_NONE | U8G_I2C_OPT_FAST)
   #endif  
 
+#elif ENABLED(U8GLIB_SH1106)
+
+  #if ENABLED(ZONESTAR_12864OLED)
+    // Generic SH1106 OLED SPI LCD
+    #define FORCE_SOFT_SPI                                      // SW-SPI
+    #if ENABLED(ALTERNATIVE_LCD)
+      #define U8G_CLASS U8GLIB_SH1106_128X64_2X                 // 4 stripes
+    #else
+      #define U8G_CLASS U8GLIB_SH1106_128X64                    // 8 stripes
+    #endif
+  #else
+    // Generic SH1106 OLED I2C LCD
+    #if ENABLED(ALTERNATIVE_LCD)
+      #define U8G_CLASS U8GLIB_SH1106_128X64_2X_I2C_2_WIRE      // 4 stripes
+    #else
+      #define U8G_CLASS U8GLIB_SH1106_128X64_2X                 // 4 stripes
+    #endif
+    #define U8G_PARAM (U8G_I2C_OPT_NONE | U8G_I2C_OPT_FAST)     // I2C
+  #endif
+
 #elif ENABLED(MKS_12864OLED)
 
   // MKS 128x64 (SH1106) OLED I2C LCD
-
   #define FORCE_SOFT_SPI                                        // SW-SPI
-
   #if ENABLED(ALTERNATIVE_LCD)
     #define U8G_CLASS U8GLIB_SH1106_128X64_2X                   // 4 stripes
   #else
@@ -136,35 +153,13 @@
 #elif ENABLED(FYSETC_242_OLED_12864)
 
   // FYSETC OLED 2.42" 128 × 64 FULL GRAPHICS CONTROLLER
-
   #define FORCE_SOFT_SPI                                        // SW-SPI
-
   #if ENABLED(ALTERNATIVE_LCD)
     #define U8G_CLASS U8GLIB_SSD1306_128X64_2X                  // 4 stripes
   #else
     #define U8G_CLASS U8GLIB_SSD1306_128X64                     // 8 stripes
   #endif
 
-#elif ENABLED(U8GLIB_SH1106)
-
-  #if ENABLED(ZONESTAR_12864OLED)
-    // Generic SH1106 OLED SPI LCD   
-    #define FORCE_SOFT_SPI                                        // SW-SPI
-    #if ENABLED(ALTERNATIVE_LCD)
-      #define U8G_CLASS U8GLIB_SH1106_128X64_2X                   // 4 stripes
-    #else
-      #define U8G_CLASS U8GLIB_SH1106_128X64                      // 8 stripes
-    #endif
-  #else
-    // Generic SH1106 OLED I2C LCD
-    #if ENABLED(ALTERNATIVE_LCD)
-      #define U8G_CLASS U8GLIB_SH1106_128X64_2X_I2C_2_WIRE        // 4 stripes
-    #else
-      #define U8G_CLASS U8GLIB_SH1106_128X64_2X                   // 4 stripes
-    #endif
-    #define U8G_PARAM (U8G_I2C_OPT_NONE | U8G_I2C_OPT_FAST)       // I2C
-  #endif
-  
 #elif ENABLED(U8GLIB_SSD1309)
 
   // Generic support for SSD1309 OLED I2C LCDs
