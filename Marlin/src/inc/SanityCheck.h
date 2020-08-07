385c9c17686ab278dd399cc58d4c1501e769e937
ellensp <ellensp@ellensp-HP-ProBook-6470b.fritz.box>
2020-08-07 23:26:31 +1200

    Add sanity check also, to try and cover all the bases

--- Marlin/src/inc/SanityCheck.h
+++ Marlin/src/inc/SanityCheck.h
@@ -3080,5 +3080,13 @@ static_assert(   _ARR_TEST(3,0) && _ARR_TEST(3,1) && _ARR_TEST(3,2)
   #error "ESP3D_WIFISUPPORT or WIFISUPPORT requires an ESP32 controller."
 #endif
 
+/**
+ * Sanity check for SPINDLE_LASER_FREQUENCY
+ */
+#if SPINDLE_LASER_FREQUENCY && !(__AVR_ATmega2560__||__AVR_ATmega1280__||TARGET_LPC1768)
+  #error "SPINDLE_LASER_FREQUENCY requires a 8 bit AVR or a LPC176x."
+#endif
+
+
 // Misc. Cleanup
 #undef _TEST_PWM
