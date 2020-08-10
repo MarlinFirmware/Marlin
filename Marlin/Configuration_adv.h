05507913c3eebc28800129c5a376ad8f2e6e61ca
BobaVanBoba <63214071+BobaVanBoba@users.noreply.github.com>
2020-08-10 17:31:20 +0100

    bltouch confiadv h
    changed to 500ms
    turned off sw mode

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -660,7 +660,7 @@
 
   // Safety: The probe needs time to recognize the command.
   //         Minimum command delay (ms). Enable and increase if needed.
-  #define BLTOUCH_DELAY 300
+  #define BLTOUCH_DELAY 500
 
   /**
    * Settings for BLTOUCH Classic 1.2, 1.3 or BLTouch Smart 1.0, 2.0, 2.2, 3.0, 3.1, and most clones:
@@ -668,7 +668,7 @@
 
   // Feature: Switch into SW mode after a deploy. It makes the output pulse longer. Can be useful
   //          in special cases, like noisy or filtered input configurations.
-  #define BLTOUCH_FORCE_SW_MODE
+  //#define BLTOUCH_FORCE_SW_MODE
 
   /**
    * Settings for BLTouch Smart 3.0 and 3.1
