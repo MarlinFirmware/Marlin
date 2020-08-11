b256a00f735c4c1892da6da79f16eadd8d828cb6
qwewer0 <57561110+qwewer0@users.noreply.github.com>
2020-08-11 21:18:01 +0200

    Change XY_PROBE_SPEED to (x*60) format
    Changing XY_PROBE_SPEED value format to match the other mm/m formats in the configurations.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1000,7 +1000,7 @@
 #define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 8000
+#define XY_PROBE_SPEED (133*60)
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
