739c13a95871f6abb279353705080f210654362e
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2020-08-14 12:54:12 -0300

    Marlin/src/lcd/touch/xpt2046 become TouchButtons. It only do touch logic now, and it uses the XPT IO from HAL.

--- Marlin/src/lcd/touch/xpt2046.cpp
+++ Marlin/src/lcd/touch/xpt2046.cpp
@@ -24,9 +24,8 @@
 #include "xpt2046.h"
 #include "../scaled_tft.h"
 
-#ifndef XPT2046_Z1_THRESHOLD
-  #define XPT2046_Z1_THRESHOLD 10
-#endif
+#include HAL_PATH(../../HAL, tft/xpt2046.h)
+XPT2046 touchIO;
 
 /**
  * Draw and Touch processing
@@ -71,59 +70,22 @@
 #define TOUCHABLE_X_WIDTH  SCREEN_WIDTH
 #define TOUCHABLE_Y_HEIGHT SCREEN_HEIGHT
 
-#ifndef TOUCH_INT_PIN
-  #define TOUCH_INT_PIN  -1
-#endif
-#ifndef TOUCH_MISO_PIN
-  #define TOUCH_MISO_PIN MISO_PIN
-#endif
-#ifndef TOUCH_MOSI_PIN
-  #define TOUCH_MOSI_PIN MOSI_PIN
-#endif
-#ifndef TOUCH_SCK_PIN
-  #define TOUCH_SCK_PIN  SCK_PIN
-#endif
-#ifndef TOUCH_CS_PIN
-  #define TOUCH_CS_PIN   CS_PIN
-#endif
+TouchButtons touch;
 
-XPT2046 touch;
-
-void XPT2046::init() {
-  SET_INPUT(TOUCH_MISO_PIN);
-  SET_OUTPUT(TOUCH_MOSI_PIN);
-  SET_OUTPUT(TOUCH_SCK_PIN);
-  OUT_WRITE(TOUCH_CS_PIN, HIGH);
-
-  #if PIN_EXISTS(TOUCH_INT)
-    // Optional Pendrive interrupt pin
-    SET_INPUT(TOUCH_INT_PIN);
-  #endif
-
-  // Read once to enable pendrive status pin
-  getInTouch(XPT2046_X);
+void TouchButtons::init() {
+  touchIO.Init();
 }
 
 #include "../../lcd/ultralcd.h" // For EN_C bit mask
 
-uint8_t XPT2046::read_buttons() {
+uint8_t TouchButtons::read_buttons() {
   #ifdef HAS_SPI_LCD
-    int16_t tsoffsets[4] = { 0 };
+    int16_t x, y;
 
-    if (tsoffsets[0] + tsoffsets[1] == 0) {
-      // Not yet set, so use defines as fallback...
-      tsoffsets[0] = XPT2046_X_CALIBRATION;
-      tsoffsets[1] = XPT2046_X_OFFSET;
-      tsoffsets[2] = XPT2046_Y_CALIBRATION;
-      tsoffsets[3] = XPT2046_Y_OFFSET;
-    }
+    if (!touchIO.getRawPoint(&x, &y)) return 0;
 
-    // We rely on XPT2046 compatible mode to ADS7843, hence no Z1 and Z2 measurements possible.
-
-    if (!isTouched()) return 0;
-    uint16_t x = uint16_t(((uint32_t(getInTouch(XPT2046_X))) * tsoffsets[0]) >> 16) + tsoffsets[1],
-             y = uint16_t(((uint32_t(getInTouch(XPT2046_Y))) * tsoffsets[2]) >> 16) + tsoffsets[3];
-    if (!isTouched()) return 0; // Fingers must still be on the TS for a valid read.
+    x = uint16_t(((uint32_t(x)) * XPT2046_X_CALIBRATION) >> 16) + XPT2046_X_OFFSET;
+	  y = uint16_t(((uint32_t(y)) * XPT2046_Y_CALIBRATION) >> 16) + XPT2046_Y_OFFSET;
 
     #if ENABLED(GRAPHICAL_TFT_ROTATE_180)
       x = TOUCH_SENSOR_WIDTH - x;
@@ -150,102 +112,6 @@ uint8_t XPT2046::read_buttons() {
     MarlinUI::screen_click(row, col, x, y);
   #endif
   return 0;
-}
-
-bool XPT2046::isTouched() {
-  return (
-    #if PIN_EXISTS(TOUCH_INT)
-      READ(TOUCH_INT_PIN) != HIGH
-    #else
-      getInTouch(XPT2046_Z1) >= XPT2046_Z1_THRESHOLD
-    #endif
-  );
-}
-
-#if ENABLED(TOUCH_BUTTONS_HW_SPI)
-
-  #include <SPI.h>
-
-  static void touch_spi_init(uint8_t spiRate) {
-    /**
-     * STM32F1 APB2 = 72MHz, APB1 = 36MHz, max SPI speed of this MCU if 18Mhz
-     * STM32F1 has 3 SPI ports, SPI1 in APB2, SPI2/SPI3 in APB1
-     * so the minimum prescale of SPI1 is DIV4, SPI2/SPI3 is DIV2
-     */
-    uint8_t clock;
-    switch (spiRate) {
-      case SPI_FULL_SPEED:    clock = SPI_CLOCK_DIV4;  break;
-      case SPI_HALF_SPEED:    clock = SPI_CLOCK_DIV4; break;
-      case SPI_QUARTER_SPEED: clock = SPI_CLOCK_DIV8; break;
-      case SPI_EIGHTH_SPEED:  clock = SPI_CLOCK_DIV16; break;
-      case SPI_SPEED_5:       clock = SPI_CLOCK_DIV32; break;
-      case SPI_SPEED_6:       clock = SPI_CLOCK_DIV64; break;
-      default:                clock = SPI_CLOCK_DIV2;        // Default from the SPI library
-    }
-    SPI.setModule(TOUCH_BUTTONS_HW_SPI_DEVICE);
-    SPI.begin();
-    SPI.setClockDivider(clock);
-    SPI.setBitOrder(MSBFIRST);
-    SPI.setDataMode(SPI_MODE0);
-  }
-#endif // TOUCH_BUTTONS_HW_SPI
-
-uint16_t XPT2046::getInTouch(const XPTCoordinate coordinate) {
-  uint16_t data[3];
-  const uint8_t coord = uint8_t(coordinate) | XPT2046_CONTROL | XPT2046_DFR_MODE;
-
-  #if ENABLED(TOUCH_BUTTONS_HW_SPI)
-
-    touch_spi_init(SPI_SPEED_6);
-    for (uint16_t i = 0; i < 3; i++) {
-      OUT_WRITE(TOUCH_CS_PIN, LOW);
-      SPI.transfer(coord);
-      data[i] = (((SPI.transfer(0xFF) << 8) | SPI.transfer(0xFF)) >> 3) & 0x0FFF;
-      WRITE(TOUCH_CS_PIN, HIGH);
-    }
-
-  #else // !TOUCH_BUTTONS_HW_SPI
-
-    OUT_WRITE(TOUCH_CS_PIN, LOW);
-    for (uint16_t i = 0; i < 3; i++) {
-      for (uint8_t j = 0x80; j; j >>= 1) {
-        WRITE(TOUCH_SCK_PIN, LOW);
-        WRITE(TOUCH_MOSI_PIN, bool(coord & j));
-        WRITE(TOUCH_SCK_PIN, HIGH);
-      }
-
-      data[i] = 0;
-      for (uint16_t j = 0x8000; j; j >>= 1) {
-        WRITE(TOUCH_SCK_PIN, LOW);
-        if (READ(TOUCH_MISO_PIN)) data[i] |= j;
-        WRITE(TOUCH_SCK_PIN, HIGH);
-      }
-      WRITE(TOUCH_SCK_PIN, LOW);
-      data[i] >>= 4;
-    }
-    WRITE(TOUCH_CS_PIN, HIGH);
-
-  #endif // !TOUCH_BUTTONS_HW_SPI
-
-  uint16_t delta01 = _MAX(data[0], data[1]) - _MIN(data[0], data[1]),
-           delta02 = _MAX(data[0], data[2]) - _MIN(data[0], data[2]),
-           delta12 = _MAX(data[1], data[2]) - _MIN(data[1], data[2]);
-
-  if (delta01 <= delta02 && delta01 <= delta12)
-    return (data[0] + data[1]) >> 1;
-
-  if (delta02 <= delta12)
-    return (data[0] + data[2]) >> 1;
-
-  return (data[1] + data[2]) >> 1;
-}
-
-bool XPT2046::getTouchPoint(uint16_t &x, uint16_t &y) {
-  if (isTouched()) {
-    x = getInTouch(XPT2046_X);
-    y = getInTouch(XPT2046_Y);
-  }
-  return isTouched();
 }
 
 #endif // HAS_TOUCH_XPT2046
