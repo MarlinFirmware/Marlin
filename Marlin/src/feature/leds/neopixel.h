957413cb173520ddc52b960eeb89b711716bb4e1
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-08-14 08:23:30 -0500

    ibid.

--- Marlin/src/feature/leds/neopixel.h
+++ Marlin/src/feature/leds/neopixel.h
@@ -91,7 +91,7 @@ public:
 
   static inline void set_pixel_color(const uint16_t n, const uint32_t c) {
     #if ENABLED(NEOPIXEL2_INSERIES)
-      if (n >= NEOPIXEL_PIXELS) adaneo2.setPixelColor(n-NEOPIXEL_PIXELS, c);
+      if (n >= NEOPIXEL_PIXELS) adaneo2.setPixelColor(n - (NEOPIXEL_PIXELS), c);
       else adaneo1.setPixelColor(n, c);
     #else
       adaneo1.setPixelColor(n, c);
@@ -126,7 +126,7 @@ public:
   #endif
 
   // Accessors
-  static inline uint16_t pixels() { TERN(NEOPIXEL2_INSERIES, return adaneo1.numPixels()*2, return adaneo1.numPixels()); }
+  static inline uint16_t pixels() { TERN(NEOPIXEL2_INSERIES, return adaneo1.numPixels() * 2, return adaneo1.numPixels()); }
   static inline uint8_t brightness() { return adaneo1.getBrightness(); }
   static inline uint32_t Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
     return adaneo1.Color(r, g, b, w);
