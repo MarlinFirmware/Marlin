3b0fe0f1ec9606ee7a785062b0285c4093a246a6
Mathew Winters <mathew@winters.org.nz>
2020-08-15 13:02:57 +1200

    Improve Homing message, always homes first. Adds Z position as extra line information

--- Marlin/src/lcd/menu/menu_probe_offset.cpp
+++ Marlin/src/lcd/menu/menu_probe_offset.cpp
@@ -91,67 +91,67 @@ void _goto_manual_move_z(const float scale) {
 }
 
 static void _lcd_probe_offset_move_z() {
-  _lcd_draw_homing();
-  if (all_axes_homed()) {
-     ui.goto_screen([]{
-      ui.defer_status_screen();
-      START_MENU();
-      calculated_z_offset = probe.offset.z + current_position.z;
+  ui.defer_status_screen();
+  START_MENU();
+  ui.defer_status_screen();
+  calculated_z_offset = probe.offset.z + current_position.z;
 
-      if (LCD_HEIGHT >= 4) {
-        STATIC_ITEM(MSG_MOVE_Z, SS_CENTER|SS_INVERT); 
-      }
-
-      VALUE_ITEM(MSG_ZPROBE_ZOFFSET, ftostr42_52(calculated_z_offset), SS_LEFT);
-
-      SUBMENU(MSG_MOVE_1MM,  []{ _goto_manual_move_z( 1);    });
-      SUBMENU(MSG_MOVE_01MM, []{ _goto_manual_move_z( 0.1f); });
-      char tmp[20], numstr[10];
-      
-      if ((SHORT_MANUAL_Z_MOVE) > 0.0f && (SHORT_MANUAL_Z_MOVE) < 0.1f) {
-        extern const char NUL_STR[];
-        SUBMENU_P(NUL_STR, []{ _goto_manual_move_z(float(SHORT_MANUAL_Z_MOVE)); });
-        MENU_ITEM_ADDON_START(0 + ENABLED(HAS_CHARACTER_LCD));
-          // Determine digits needed right of decimal
-          const uint8_t digs = !UNEAR_ZERO((SHORT_MANUAL_Z_MOVE) * 1000 - int((SHORT_MANUAL_Z_MOVE) * 1000)) ? 4 :
-                              !UNEAR_ZERO((SHORT_MANUAL_Z_MOVE) *  100 - int((SHORT_MANUAL_Z_MOVE) *  100)) ? 3 : 2;
-          sprintf_P(tmp, GET_TEXT(MSG_MOVE_Z_DIST), dtostrf(SHORT_MANUAL_Z_MOVE, 1, digs, numstr));
-          lcd_put_u8str(tmp);
-        MENU_ITEM_ADDON_END();
-      }
-
-      ACTION_ITEM(MSG_BUTTON_DONE,   _lcd_probe_offset_move_done);
-      ACTION_ITEM(MSG_BUTTON_CANCEL, _lcd_probe_offset_move_undo);
-
-      END_MENU();
-    });
+  if (LCD_HEIGHT >= 4) {
+    STATIC_ITEM(MSG_MOVE_Z, SS_CENTER|SS_INVERT); 
   }
+
+  VALUE_ITEM(MSG_MESH_EDIT_Z, ftostr42_52(current_position.z), SS_LEFT);
+  VALUE_ITEM(MSG_ZPROBE_ZOFFSET, ftostr42_52(calculated_z_offset), SS_LEFT);
+
+  SUBMENU(MSG_MOVE_1MM,  []{ _goto_manual_move_z( 1);    });
+  SUBMENU(MSG_MOVE_01MM, []{ _goto_manual_move_z( 0.1f); });
+  char tmp[20], numstr[10];
+  
+  if ((SHORT_MANUAL_Z_MOVE) > 0.0f && (SHORT_MANUAL_Z_MOVE) < 0.1f) {
+    extern const char NUL_STR[];
+    SUBMENU_P(NUL_STR, []{ _goto_manual_move_z(float(SHORT_MANUAL_Z_MOVE)); });
+    MENU_ITEM_ADDON_START(0 + ENABLED(HAS_CHARACTER_LCD));
+      // Determine digits needed right of decimal
+      const uint8_t digs = !UNEAR_ZERO((SHORT_MANUAL_Z_MOVE) * 1000 - int((SHORT_MANUAL_Z_MOVE) * 1000)) ? 4 :
+                          !UNEAR_ZERO((SHORT_MANUAL_Z_MOVE) *  100 - int((SHORT_MANUAL_Z_MOVE) *  100)) ? 3 : 2;
+      sprintf_P(tmp, GET_TEXT(MSG_MOVE_Z_DIST), dtostrf(SHORT_MANUAL_Z_MOVE, 1, digs, numstr));
+      lcd_put_u8str(tmp);
+    MENU_ITEM_ADDON_END();
+  }
+
+  ACTION_ITEM(MSG_BUTTON_DONE,   _lcd_probe_offset_move_done);
+  ACTION_ITEM(MSG_BUTTON_CANCEL, _lcd_probe_offset_move_undo);
+
+  END_MENU();
 }
 
 void _lcd_probe_offset() {
   ui.defer_status_screen();
 
-  if (!all_axes_known()) {
-    set_all_unhomed();
-    queue.inject_P(G28_STR);
-  }
+  set_all_unhomed();
+  queue.inject_P(G28_STR);
 
   store_z_offset = probe.offset.z;
   probe.offset.z = PROBE_OFFSET_START;
 
 
-  // Disable leveling so the planner won't mess with us
-  #if HAS_LEVELING
-    leveling_was_active = planner.leveling_active;
-    set_bed_leveling_enabled(false);
-  #endif
+// Disable leveling so the planner won't mess with us
+#if HAS_LEVELING
+  leveling_was_active = planner.leveling_active;
+  set_bed_leveling_enabled(false);
+#endif
 
-  #if HAS_SOFTWARE_ENDSTOPS
-    store_soft_endstops_enabled = soft_endstops_enabled;
-    soft_endstops_enabled = false;
-  #endif    
+#if HAS_SOFTWARE_ENDSTOPS
+  store_soft_endstops_enabled = soft_endstops_enabled;
+  soft_endstops_enabled = false;
+#endif    
 
-  ui.goto_screen(_lcd_probe_offset_move_z);
+  ui.goto_screen([]{
+      _lcd_draw_homing();
+      if (all_axes_homed()) {
+        ui.goto_screen(_lcd_probe_offset_move_z);
+      }
+  });
 
 }
 
