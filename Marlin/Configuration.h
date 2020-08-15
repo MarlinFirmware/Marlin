5c6ae04561936edad5d80df4fb58e6e77c39d667
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-08-15 17:33:41 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2232,9 +2232,9 @@
 
 // @section extras
 
-// Reduce installed fans (default to number of defined fan pins).
+// Set number of user-controlled fans. Disable to use all board-defined fans.
 // :[1,2,3,4,5,6,7,8]
-//#define FANS 1
+//#define NUM_M106_FANS 1
 
 // Increase the FAN PWM frequency. Removes the PWM noise but increases heating in the FET/Arduino
 //#define FAST_PWM_FAN
