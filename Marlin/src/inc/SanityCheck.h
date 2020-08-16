e09aaa36fa075acc54f1ed947213e608fae02d7c
GMagician <GMagician@users.noreply.github.com>
2020-08-16 17:04:08 +0200

    Add sanity check

--- Marlin/src/inc/SanityCheck.h
+++ Marlin/src/inc/SanityCheck.h
@@ -1107,6 +1107,15 @@ static_assert(Y_MAX_LENGTH >= Y_BED_SIZE, "Movement bounds (Y_MIN_POS, Y_MAX_POS
 #endif
 
 /**
+ * Limited user-controlled fans
+ */
+#ifdef NUM_M106_FANS          // FAN_COUNT may be 0 but if 'user-controlled fans' is not defined let it compile
+  #if NUM_M106_FANS > FAN_COUNT
+    #error "The selected board doesn't support enough user-controlled fans. Reduce NUM_M106_FANS."
+  #endif
+#endif
+
+/**
  * Limited number of servos
  */
 #if NUM_SERVOS > NUM_SERVO_PLUGS
