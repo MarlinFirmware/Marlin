5183a146f12c99dee3dd1f7efca99284d30330df
Matt Waddilove <mattheww@unity3d.com>
2020-08-18 07:18:01 +0100

    Add NO_SD_HOST_DRIVE so Marlin can read the SD

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1301,7 +1301,7 @@
  * By default an onboard SD card reader may be shared as a USB mass-
  * storage device. This option hides the SD card from the host PC.
  */
-//#define NO_SD_HOST_DRIVE   // Disable SD Card access over USB (for security).
+#define NO_SD_HOST_DRIVE   // Disable SD Card access over USB (for security).
 
 /**
  * Additional options for Graphical Displays
