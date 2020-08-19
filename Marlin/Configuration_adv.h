d1f86ab9e968b3940493299d55c5d4062f4f5cd9
meltonpieman <51961825+meltonpieman@users.noreply.github.com>
2020-08-19 19:50:49 +0100

    Mesh updated
    min & max offsets y 5  & 15
    max offset X 260 can be achieved, 255 used

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1561,10 +1561,10 @@
 
 #if EITHER(MESH_BED_LEVELING, AUTO_BED_LEVELING_UBL)
   // Override the mesh area if the automatic (max) area is too large
-  #define MESH_MIN_X MESH_INSET // 20/08/19
-  //#define MESH_MIN_Y MESH_INSET
-  //#define MESH_MAX_X 255 // was X_BED_SIZE - (MESH_INSET) // 20/08/19
-  //#define MESH_MAX_Y Y_BED_SIZE - 15 // was (MESH_INSET) // 20/08/19
+  #define MESH_MIN_X 5 // was MESH_INSET // 20/08/19
+  #define MESH_MIN_Y 5 // was MESH_INSET // 20/08/19
+  #define MESH_MAX_X 255 // was X_BED_SIZE - (MESH_INSET) // 20/08/19
+  #define MESH_MAX_Y Y_BED_SIZE - 15 // was (MESH_INSET) // 20/08/19
 #endif
 
 /**
