9cc22742928d2f3a35ab79b6b765f0915a0e55cf
Tim Hoogland <tim@th3dstudio.com>
2020-08-19 17:55:39 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -17,7 +17,7 @@
 //===========================================================================
 // Creality Ender 3 V2 Options
 //===========================================================================
-#define ENDER3_V2
+//#define ENDER3_V2
 
 // If you are using our EZOut V1/V2 (connected to LCD header) filament sensor kit please follow the install guide
 // and then uncomment the #define EZOUT_ENABLE line below.
@@ -25,7 +25,7 @@
 //#define EZOUT_ENABLE
 
 // EZABL Probe Mounts
-#define ENDER3_V2_OEM
+//#define ENDER3_V2_OEM
 //#define CUSTOM_PROBE
 
 //===========================================================================
@@ -188,6 +188,10 @@
   #define DEFAULT_Kp 28.72
   #define DEFAULT_Ki 2.62
   #define DEFAULT_Kd 78.81
+  
+  #define DEFAULT_bedKp 462.10
+  #define DEFAULT_bedKi 85.47
+  #define DEFAULT_bedKd 624.59
 
   #define ENDSTOPPULLUPS
 
@@ -248,7 +252,6 @@
 
 #define DUMMY_THERMISTOR_998_VALUE 25
 #define DUMMY_THERMISTOR_999_VALUE 100
-
 #define MAX_REDUNDANT_TEMP_SENSOR_DIFF 10
 
 #define HEATER_0_MINTEMP   0
@@ -279,29 +282,41 @@
 #define TEMP_BED_WINDOW          1
 #define TEMP_BED_HYSTERESIS      3
 
+// PID Settings
 #define PIDTEMP
 #define BANG_MAX 255
 #define PID_MAX BANG_MAX
 #define PID_K1 0.95
 
+#ifndef DEFAULT_Kp
+  #define DEFAULT_Kp 22.2 // Define Marlin default PID if no machine specific PID is defined.
+  #define DEFAULT_Ki 1.08
+  #define DEFAULT_Kd 114
+#endif
+
 #define PIDTEMPBED
 #define MAX_BED_POWER 255
 #if ENABLED(PIDTEMPBED)
-  #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi 85.47
-  #define DEFAULT_bedKd 624.59
+  #ifndef DEFAULT_bedKp
+    #define DEFAULT_bedKp 10.00 // Define Marlin default bed PID if no machine specific PID is defined.
+    #define DEFAULT_bedKi .023
+    #define DEFAULT_bedKd 305.4
+  #endif
 #endif
 
 #if EITHER(PIDTEMP, PIDTEMPBED)
   #define PID_FUNCTIONAL_RANGE 10
 #endif
+// End PID Settings
 
-//#define PREVENT_COLD_EXTRUSION
-#define EXTRUDE_MINTEMP 180
+// Misc User Protections
+#define PREVENT_COLD_EXTRUSION // Enable for release. If you want to disable cold extrusion prevention, disable or remove this line.
+#define EXTRUDE_MINTEMP 170
 
-#define PREVENT_LENGTHY_EXTRUDE
+#define PREVENT_LENGTHY_EXTRUDE // Enable for release. If you want to disable lengthy extrude prevention, disable or remove this line.
 #define EXTRUDE_MAXLENGTH 1000
 
+// Enable Thermal Protection for Bed and Hotends
 #define THERMAL_PROTECTION_HOTENDS
 #define THERMAL_PROTECTION_BED
 
@@ -310,23 +325,22 @@
  */
 
 #if ENABLED(ABL_ENABLE)
-  //#define ENABLE_LEVELING_FADE_HEIGHT
+  //#define ENABLE_LEVELING_FADE_HEIGHT // Disable for release. Can cause issues.
   #define SEGMENT_LEVELED_MOVES
   #define LEVELED_SEGMENT_LENGTH 5.0
-
-  #undef Z_PROBE_OFFSET_RANGE_MIN
+  
+  #undef Z_PROBE_OFFSET_RANGE_MIN //Some machines define these even with no probe. Override when a probe is used to ensure correct settings.
   #define Z_PROBE_OFFSET_RANGE_MIN    -5
-  #undef Z_PROBE_OFFSET_RANGE_MAX
+  #undef Z_PROBE_OFFSET_RANGE_MAX //Some machines define these even with no probe. Override when a probe is used to ensure correct settings.
   #define Z_PROBE_OFFSET_RANGE_MAX     1
+  
   #define Z_MIN_PROBE_REPEATABILITY_TEST
   #define Z_AFTER_PROBING              5
   #define Z_PROBE_LOW_POINT           -2
-
   #if DISABLED(BLTOUCH)
     #define FIX_MOUNTED_PROBE
   #endif
-
-  #if ENABLED(SLOWER_PROBE_MOVES) || ENABLED(TH3D_EZ300) || ENABLED(TIM_AM8)
+  #if ENABLED(SLOWER_PROBE_MOVES)
     #define XY_PROBE_SPEED 8000
   #else
     #if ENABLED(EZABL_SUPERFASTPROBE)
@@ -335,35 +349,28 @@
       #define XY_PROBE_SPEED 12000
     #endif
   #endif
-
   #define PROBING_MARGIN EZABL_PROBE_EDGE
-
   #if ENABLED(FIX_MOUNTED_PROBE) && DISABLED(HEATERS_ON_DURING_PROBING)
     #define PROBING_HEATERS_OFF   
   #endif
-
   #define MULTIPLE_PROBING 2
-
   #define AUTO_BED_LEVELING_BILINEAR
-  
   #define EXTRAPOLATE_BEYOND_GRID
-
   #define GRID_MAX_POINTS_X EZABL_POINTS
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
-
   #define Z_SAFE_HOMING
-
   #if ENABLED(Z_SAFE_HOMING)
     #define Z_SAFE_HOMING_X_POINT ((X_BED_SIZE) / 2)    // X point for Z homing
     #define Z_SAFE_HOMING_Y_POINT ((Y_BED_SIZE) / 2)    // Y point for Z homing
   #endif
   
-  #if ENABLED(BLTOUCH)
+  // ABL Probe Logic Settings
+  #if ENABLED(BLTOUCH) // BLTouch uses false
     #undef Z_MIN_PROBE_ENDSTOP_INVERTING
     #define Z_MIN_PROBE_ENDSTOP_INVERTING false
     #undef Z_MIN_ENDSTOP_INVERTING
     #define Z_MIN_ENDSTOP_INVERTING false
-  #else
+  #else                // EZABL uses true
     #undef Z_MIN_PROBE_ENDSTOP_INVERTING
     #define Z_MIN_PROBE_ENDSTOP_INVERTING true
     #undef Z_MIN_ENDSTOP_INVERTING
@@ -377,7 +384,30 @@
  */
 
 #define STRING_CONFIG_H_AUTHOR "TH3D Studio"
-#define CUSTOM_VERSION_FILE Version.h
+#define UNIFIED_VERSION "TH3D UF2.00"
+#define S_CURVE_ACCELERATION
+#define DEFAULT_NOMINAL_FILAMENT_DIA 1.75
+#define HOMING_FEEDRATE_XY (50*60)
+#define Z_MIN_POS 0
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
+#define VALIDATE_HOMING_ENDSTOPS
+#define PRINTJOB_TIMER_AUTOSTART
+
+#if DISABLED(NO_SDCARD)
+  #define SDSUPPORT
+#endif
+
+#if DISABLED(SPEAKER_KILL)
+  #define SPEAKER
+#endif
+
+#if ENABLED(FAN_FIX)
+  #define FAN_SOFT_PWM
+  #define SOFT_PWM_SCALE 1
+#else
+  #define SOFT_PWM_SCALE 0
+#endif
 
 #if ENABLED(EZABL_SUPERFASTPROBE) && ENABLED(ABL_ENABLE) && DISABLED(BLTOUCH)
   #define HOMING_FEEDRATE_Z  (15*60)
@@ -386,8 +416,6 @@
 #else
   #define HOMING_FEEDRATE_Z  (4*60)
 #endif
-
-#define S_CURVE_ACCELERATION
 
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
 #define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
@@ -408,21 +436,11 @@
   #define Z_CLEARANCE_MULTI_PROBE    3
 #endif
 
-#define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-
-#define HOMING_FEEDRATE_XY (50*60)
-
-#define VALIDATE_HOMING_ENDSTOPS
-
 #define HOST_KEEPALIVE_FEATURE
 #define DEFAULT_KEEPALIVE_INTERVAL 2
 #if DISABLED(SPACE_SAVER)
   #define BUSY_WHILE_HEATING
 #endif
-
-#define DEFAULT_NOMINAL_FILAMENT_DIA 1.75
 
 #define EEPROM_SETTINGS
 #if ENABLED(SPACE_SAVER)
@@ -439,7 +457,7 @@
   #define MIN_SOFTWARE_ENDSTOP_X
   #define MIN_SOFTWARE_ENDSTOP_Y
   #if DISABLED(ABL_ENABLE)
-    #define MIN_SOFTWARE_ENDSTOP_Z
+    #define MIN_SOFTWARE_ENDSTOP_Z // Disable when any ABL is being used.
   #endif
 #endif
 
@@ -458,11 +476,11 @@
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED    100
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_2_FAN_SPEED     0
 
 #define NOZZLE_PARK_FEATURE
 #if ENABLED(NOZZLE_PARK_FEATURE)
-  #define NOZZLE_PARK_POINT { 15, 15, 10 }
+  #define NOZZLE_PARK_POINT { 10, 10, 10 }
   #define NOZZLE_PARK_XY_FEEDRATE 100  
   #define NOZZLE_PARK_Z_FEEDRATE 5
 #endif
@@ -480,40 +498,19 @@
 #define DISPLAY_CHARSET_HD44780 JAPANESE
 #define LCD_INFO_SCREEN_STYLE 0
 #define DISABLE_REDUCED_ACCURACY_WARNING
+#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 2
+#define LCD_FEEDBACK_FREQUENCY_HZ 5000
+
 #if DISABLED(DWIN_CREALITY_LCD) && DISABLED(SPACE_SAVER)
   #define INDIVIDUAL_AXIS_HOMING_MENU
 #endif
 
-#define LEVEL_BED_CORNERS
-#if ENABLED(LEVEL_BED_CORNERS)
-  #define LEVEL_CORNERS_INSET_LFRB { 30, 30, 30, 30 }
-  #define LEVEL_CORNERS_HEIGHT      0.0
-  #define LEVEL_CORNERS_Z_HOP       4.0
-  #define LEVEL_CENTER_TOO
+#if DISABLED(SPACE_SAVER)
+  #define LEVEL_BED_CORNERS
+  #if ENABLED(LEVEL_BED_CORNERS)
+    #define LEVEL_CORNERS_INSET_LFRB { 30, 30, 30, 30 }
+    #define LEVEL_CORNERS_HEIGHT      0.0
+    #define LEVEL_CORNERS_Z_HOP       4.0
+    #define LEVEL_CENTER_TOO
+  #endif
 #endif
-
-#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 2
-#define LCD_FEEDBACK_FREQUENCY_HZ 5000
-
-/**
- * Misc features and settings
- */
-
-#define PRINTJOB_TIMER_AUTOSTART
-
-#if DISABLED(NO_SDCARD)
-  #define SDSUPPORT
-#endif
-
-#if DISABLED(SPEAKER_KILL)
-  #define SPEAKER
-#endif
-
-#if ENABLED(FAN_FIX)
-  #define FAN_SOFT_PWM
-  #define SOFT_PWM_SCALE 1
-#else
-  #define SOFT_PWM_SCALE 0
-#endif
-
-#define UNIFIED_VERSION "TH3D UF2.00"
