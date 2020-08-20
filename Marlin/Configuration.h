894591f8de076245dc34ba3b686f4a3b1fcc86ef
miferkolb <61791039+miferkolb@users.noreply.github.com>
2020-08-20 11:50:07 +0200

    Comented in configuration //#define FIX_MOUNTED_PROBE because of compilation error

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -861,7 +861,7 @@
  * A Fix-Mounted Probe either doesn't deploy or needs manual deployment.
  *   (e.g., an inductive probe or a nozzle-based probe-switch.)
  */
-#define FIX_MOUNTED_PROBE
+//#define FIX_MOUNTED_PROBE
 
 /**
  * Use the nozzle as the probe, as with a conductive
