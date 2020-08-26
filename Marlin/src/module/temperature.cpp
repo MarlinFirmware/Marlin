f5e94cd7f1fcf380c6f286e1ae9183754937c794
thordarsen <50306469+thordarsen@users.noreply.github.com>
2020-08-26 06:23:56 -0400

    Fix SINGLENOZZLE fan speed bug (#19152)

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -210,8 +210,9 @@ const char str_t_thermal_runaway[] PROGMEM = STR_T_THERMAL_RUNAWAY,
         if (target < EXTRUDERS) singlenozzle_fan_speed[target] = speed;
         return;
       }
-      target = 0; // Always use fan index 0 with SINGLENOZZLE
     #endif
+
+    TERN_(SINGLENOZZLE, target = 0); // Always use fan index 0 with SINGLENOZZLE
 
     if (target >= FAN_COUNT) return;
 
