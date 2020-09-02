f760c4d8d55e365c096b87c7fc4b60b433188a10
quas7 <quastus@gmail.com>
2020-09-03 01:13:14 +0200

    XON/XOFF Flow Control enabled

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1879,12 +1879,12 @@
 // Without XON/XOFF flow control (see SERIAL_XON_XOFF below) 32 bytes should be enough.
 // To use flow control, set this buffer size to at least 1024 bytes.
 // :[0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
-//#define RX_BUFFER_SIZE 1024
+//#define RX_BUFFER_SIZE 2048
 
 #if RX_BUFFER_SIZE >= 1024
   // Enable to have the controller send XON/XOFF control characters to
   // the host to signal the RX buffer is becoming full.
-  //#define SERIAL_XON_XOFF
+  #define SERIAL_XON_XOFF
 #endif
 
 // Add M575 G-code to change the baud rate
