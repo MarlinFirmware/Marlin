58c64b779858414107522a28d38c67adf68f4a4c
Foxies <Foxies-CSTL@users.noreply.github.com>
2020-09-05 17:39:00 +0200

    Customized display TFT LCD screen (FSMC)

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2020-08-12"
+#define STRING_DISTRIBUTION_DATE "2020-09-05"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
@@ -54,23 +54,23 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-//#define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "https://github.com/Foxies-CSTL/Marlin2.0.x-FLSun_QQ-S"
 
 /**
  * Default generic printer UUID.
  */
-//#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
+#define DEFAULT_MACHINE_UUID "8959c708-a02c-45fb-bb2e-a9bae09dbc02"
 
 /**
  * The WEBSITE_URL is the location where users can get more information such as
  * documentation about a specific Marlin release.
  */
-//#define WEBSITE_URL "https://marlinfw.org"
+#define WEBSITE_URL "https://marlinfw.org"
 
 /**
  * Set the vendor info the serial USB interface, if changable
  * Currently only supported by DUE platform
  */
-//#define USB_DEVICE_VENDOR_ID           0x0000
-//#define USB_DEVICE_PRODUCT_ID          0x0000
+#define USB_DEVICE_VENDOR_ID           0x1a86
+#define USB_DEVICE_PRODUCT_ID          0x7523
 //#define USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
---
+++ Marlin/_Statusscreen.h
@@ -0,0 +1,79 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+/**
+ * Custom Status Screen bitmap
+ *
+ * Place this file in the root with your configuration files
+ * and enable CUSTOM_STATUS_SCREEN_IMAGE in Configuration.h.
+ *
+ * Use the Marlin Bitmap Converter to make your own:
+ * http://marlinfw.org/tools/u8glib/converter.html
+ */
+
+//
+// Status Screen Logo bitmap
+//
+#define STATUS_LOGO_X            0
+#define STATUS_LOGO_Y            0
+#define STATUS_LOGO_WIDTH 40
+
+const unsigned char status_logo_bmp[] PROGMEM = {
+  B00000011,B00000000,B00011000,B00000000,B00000000,
+  B00000011,B10000000,B00111000,B00000000,B00000000,
+  B00000001,B11000000,B01100000,B00000000,B00000000,
+  B00000001,B11111111,B11100000,B00000000,B00000000,
+  B00000111,B11111111,B11111000,B00000000,B00000000,
+  B00001111,B11111111,B11111100,B00000000,B00000000,
+  B00001111,B11111111,B11111100,B00000000,B00000000,
+  B01100111,B11111111,B11111001,B10000000,B00000000,
+  B00110111,B11000000,B01111011,B00000000,B00000000,
+  B00011000,B01111111,B11000110,B00000000,B00000000,
+  B00001111,B00000000,B00111100,B00000000,B00000000,
+  B00000011,B11111111,B11110000,B00000000,B00000000,
+  B00000000,B00000000,B00000000,B00000000,B00000000,
+  B00000000,B01111111,B10000000,B00000000,B00000000,
+  B00000000,B01111111,B10000000,B00000000,B00000000,
+  B00000000,B01111111,B10000000,B00000000,B00000000,
+  B00000000,B01111111,B10000000,B00000000,B00000000,
+  B00000000,B01111111,B10000000,B00000000,B00000000,
+  B00000000,B00000000,B00000000,B00000000,B00000000,
+  B00000000,B01111111,B11000000,B00000000,B00000000,
+  B00000000,B11111111,B11000000,B00000000,B00000000,
+  B00000000,B11111111,B11000000,B00000000,B00000000,
+  B00000000,B11111111,B11000000,B00000000,B00000000,
+  B00000000,B00111111,B10000000,B00000000,B00000000,
+  B00000000,B00111111,B10000000,B00000000,B00000000,
+  B00000000,B00111111,B00000000,B00000000,B00000000,
+  B00000000,B00011110,B00000000,B00000000,B00000000,
+  B00000000,B00001100,B00000000,B00000000,B00000000
+};
+
+//
+// Use default bitmaps
+//
+#define STATUS_HOTEND_ANIM
+#define STATUS_BED_ANIM
+// 
+#define STATUS_HEATERS_X      40  //48
+#define STATUS_BED_X          68 //74
