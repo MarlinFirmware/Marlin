81a656c6d75973ac1a584c9b7d99261c7a932144
Ashton <ashton.mitchell@gmail.com>
2020-09-07 02:34:47 -0600

    bed changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1128,7 +1128,7 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -5
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
