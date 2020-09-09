76ac3bd35d35168d01044820c94025b263d8a789
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-09-09 17:55:18 -0500

    whitespace

--- Marlin/src/HAL/TEENSY40_41/HAL.cpp
+++ Marlin/src/HAL/TEENSY40_41/HAL.cpp
@@ -35,64 +35,64 @@
 uint16_t HAL_adc_result, HAL_adc_select;
 
 static const uint8_t pin2sc1a[] = {
-	7,	// 0/A0  AD_B1_02
-	8,	// 1/A1  AD_B1_03
-	12,	// 2/A2  AD_B1_07
-	11,	// 3/A3  AD_B1_06
-	6,	// 4/A4  AD_B1_01
-	5,	// 5/A5  AD_B1_00
-	15,	// 6/A6  AD_B1_10
-	0,	// 7/A7  AD_B1_11
-	13,	// 8/A8  AD_B1_08
-	14,	// 9/A9  AD_B1_09
-	1,	// 24/A10 AD_B0_12
-	2,	// 25/A11 AD_B0_13
-	128+3,	// 26/A12 AD_B1_14 - only on ADC2, 3
-	128+4,	// 27/A13 AD_B1_15 - only on ADC2, 4
-	7,	// 14/A0  AD_B1_02
-	8,	// 15/A1  AD_B1_03
-	12,	// 16/A2  AD_B1_07
-	11,	// 17/A3  AD_B1_06
-	6,	// 18/A4  AD_B1_01
-	5,	// 19/A5  AD_B1_00
-	15,	// 20/A6  AD_B1_10
-	0,	// 21/A7  AD_B1_11
-	13,	// 22/A8  AD_B1_08
-	14,	// 23/A9  AD_B1_09
-	1,	// 24/A10 AD_B0_12
-	2,	// 25/A11 AD_B0_13
-	128+3,	// 26/A12 AD_B1_14 - only on ADC2, 3
-	128+4,	// 27/A13 AD_B1_15 - only on ADC2, 4
-#ifdef ARDUINO_TEENSY41
-	255,	// 28
-	255,	// 29
-	255,	// 30
-	255,	// 31
-	255,	// 32
-	255,	// 33
-	255,	// 34
-	255,	// 35
-	255,	// 36
-	255,	// 37
-	128+1,	// 38/A14 AD_B1_12 - only on ADC2, 1
-	128+2,	// 39/A15 AD_B1_13 - only on ADC2, 2
-	9,	// 40/A16 AD_B1_04
-	10,	// 41/A17 AD_B1_05
-#endif
+  0x07,  // 0/A0  AD_B1_02
+  0x08,  // 1/A1  AD_B1_03
+  0x0C,  // 2/A2  AD_B1_07
+  0x0B,  // 3/A3  AD_B1_06
+  0x06,  // 4/A4  AD_B1_01
+  0x05,  // 5/A5  AD_B1_00
+  0x0F,  // 6/A6  AD_B1_10
+  0x00,  // 7/A7  AD_B1_11
+  0x0D,  // 8/A8  AD_B1_08
+  0x0E,  // 9/A9  AD_B1_09
+  0x01,  // 24/A10 AD_B0_12
+  0x02,  // 25/A11 AD_B0_13
+  0x83,  // 26/A12 AD_B1_14 - only on ADC2, 3
+  0x84,  // 27/A13 AD_B1_15 - only on ADC2, 4
+  0x07,  // 14/A0  AD_B1_02
+  0x08,  // 15/A1  AD_B1_03
+  0x0C,  // 16/A2  AD_B1_07
+  0x0B,  // 17/A3  AD_B1_06
+  0x06,  // 18/A4  AD_B1_01
+  0x05,  // 19/A5  AD_B1_00
+  0x0F,  // 20/A6  AD_B1_10
+  0x00,  // 21/A7  AD_B1_11
+  0x0D,  // 22/A8  AD_B1_08
+  0x0E,  // 23/A9  AD_B1_09
+  0x01,  // 24/A10 AD_B0_12
+  0x02,  // 25/A11 AD_B0_13
+  0x83,  // 26/A12 AD_B1_14 - only on ADC2, 3
+  0x84,  // 27/A13 AD_B1_15 - only on ADC2, 4
+  #ifdef ARDUINO_TEENSY41
+    0xFF,  // 28
+    0xFF,  // 29
+    0xFF,  // 30
+    0xFF,  // 31
+    0xFF,  // 32
+    0xFF,  // 33
+    0xFF,  // 34
+    0xFF,  // 35
+    0xFF,  // 36
+    0xFF,  // 37
+    0x81,  // 38/A14 AD_B1_12 - only on ADC2, 1
+    0x82,  // 39/A15 AD_B1_13 - only on ADC2, 2
+    0x09,  // 40/A16 AD_B1_04
+    0x0A,  // 41/A17 AD_B1_05
+  #endif
 };
 
 /*
-  // disable interrupts
-  void cli() { noInterrupts(); }
+// disable interrupts
+void cli() { noInterrupts(); }
 
-  // enable interrupts
-  void sei() { interrupts(); }
+// enable interrupts
+void sei() { interrupts(); }
 */
 
 void HAL_adc_init() {
   analog_init();
-	while (ADC1_GC & ADC_GC_CAL) ;
-	while (ADC2_GC & ADC_GC_CAL) ;
+  while (ADC1_GC & ADC_GC_CAL) ;
+  while (ADC2_GC & ADC_GC_CAL) ;
 }
 
 void HAL_clear_reset_source() {
@@ -120,7 +120,7 @@ extern "C" {
   extern char __heap_start;
   extern void* __brkval;
 
-// doesn't wowrk on Teensy 4.x
+  // Doesn't work on Teensy 4.x
   uint32_t freeMemory() {
     uint32_t free_memory;
     if ((uint32_t)__brkval == 0)
@@ -134,8 +134,7 @@ extern "C" {
 void HAL_adc_start_conversion(const uint8_t adc_pin) {
   const uint16_t pin = pin2sc1a[adc_pin];
   if (pin == 0xFF) {
-    // Digital only
-    HAL_adc_select = -1;
+    HAL_adc_select = -1; // Digital only
   }
   else if (pin & 0x80) {
     HAL_adc_select = 1;
@@ -150,19 +149,19 @@ void HAL_adc_start_conversion(const uint8_t adc_pin) {
 uint16_t HAL_adc_get_result() {
   switch (HAL_adc_select) {
     case 0:
-		  while (!(ADC1_HS & ADC_HS_COCO0)) ; // wait
+      while (!(ADC1_HS & ADC_HS_COCO0)) ; // wait
       return ADC1_R0;
     case 1:
-		  while (!(ADC2_HS & ADC_HS_COCO0)) ; // wait
+      while (!(ADC2_HS & ADC_HS_COCO0)) ; // wait
       return ADC2_R0;
   }
   return 0;
 }
 
 bool is_output(uint8_t pin) {
-	const struct digital_pin_bitband_and_config_table_struct *p;
-	p = digital_pin_to_info_PGM + pin;
-	return (*(p->reg + 1) & p->mask);
+  const struct digital_pin_bitband_and_config_table_struct *p;
+  p = digital_pin_to_info_PGM + pin;
+  return (*(p->reg + 1) & p->mask);
 }
 
 #endif // __IMXRT1062__
