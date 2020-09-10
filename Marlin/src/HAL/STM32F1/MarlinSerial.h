8393c6a63d030bd9b3ae9578a74086678186e42a
Victor Oliveira <rhapsodyv@gmail.com>
2020-09-10 00:46:50 -0300

    Raise STM32F1 UART IRQ Priority, add error handling (#19301)
    (Error handling for Overrun, Framing and Parity.)

--- Marlin/src/HAL/STM32F1/MarlinSerial.h
+++ Marlin/src/HAL/STM32F1/MarlinSerial.h
@@ -22,12 +22,15 @@
 #pragma once
 
 #include <HardwareSerial.h>
+#include <libmaple/usart.h>
 #include <WString.h>
 
 #include "../../inc/MarlinConfigPre.h"
 #if ENABLED(EMERGENCY_PARSER)
   #include "../../feature/e_parser.h"
 #endif
+
+#define UART_IRQ_PRIO 1
 
 class MarlinSerial : public HardwareSerial {
 public:
@@ -37,6 +40,18 @@ public:
       , emergency_state(EmergencyParser::State::EP_RESET)
     #endif
     { }
+
+  #ifdef UART_IRQ_PRIO
+    // shadow the parent methods to set irq priority after the begin
+    void begin(uint32 baud) {
+      MarlinSerial::begin(baud, SERIAL_8N1);
+    }
+
+    void begin(uint32 baud, uint8_t config) {
+      HardwareSerial::begin(baud, config);
+      nvic_irq_set_priority(c_dev()->irq_num, UART_IRQ_PRIO);
+    }
+  #endif
 
   #if ENABLED(EMERGENCY_PARSER)
     EmergencyParser::State emergency_state;
