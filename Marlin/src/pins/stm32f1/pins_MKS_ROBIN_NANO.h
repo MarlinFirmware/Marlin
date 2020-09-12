c13be14e58491956fa545e18a0aba51e1c92c042
John Doe <johndoe@example.com>
2020-09-12 11:13:44 +0200

    Allow compiling for generic HAL STM32 library too for Robin Nano board

--- Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO.h
+++ Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO.h
@@ -25,7 +25,7 @@
  * MKS Robin nano (STM32F130VET6) board pin assignments
  */
 
-#ifndef __STM32F1__
+#if !defined(STM32F1) && !defined(STM32F1xx)
   #error "Oops! Select an STM32F1 board in 'Tools > Board.'"
 #elif HOTENDS > 2 || E_STEPPERS > 2
   #error "MKS Robin nano supports up to 2 hotends / E-steppers. Comment out this line to continue."
