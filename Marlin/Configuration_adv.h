db5788a78abe9582b91c0d6f39e7562053269e38
Scope666 <cscoppa@gmail.com>
2020-09-13 14:44:46 -0400

    Fix SD card init issue

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1292,7 +1292,7 @@
 
   // Add an optimized binary file transfer mode, initiated with 'M28 B1'
   //#define BINARY_FILE_TRANSFER
-
+  #define HAS_SDCARD_CONNECTION 1
   #if HAS_SDCARD_CONNECTION
     /**
      * Set this option to one of the following (or the board's defaults apply):
