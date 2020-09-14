2c021040b3277a1be267b0f43e416852a1e840e3
Tim Hoogland <tim@th3dstudio.com>
2020-09-14 13:19:49 -0500

    Update version number for next release
    Update version number for next release

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.04"
+#define UNIFIED_VERSION "TH3D UFW 2.05"
 
 /**
  * Temp Settings
