5249fd72358f418e57f7d072bedf9c129e23478b
Tim Hoogland <tim@th3dstudio.com>
2020-09-14 09:42:27 -0500

    Update Configuration_backend.h
    Update version

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.03"
+#define UNIFIED_VERSION "TH3D UFW 2.04"
 
 /**
  * Temp Settings
