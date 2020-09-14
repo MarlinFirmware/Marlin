6726ef9ca3b087754ac8fdc981e28c03aa57095e
Tim Hoogland <tim@th3dstudio.com>
2020-09-14 17:43:50 -0500

    Update UpdatePackages.bat

--- Internal Tools/UpdatePackages.bat
+++ Internal Tools/UpdatePackages.bat
@@ -1,14 +1,36 @@
 @echo off
-set /p ufwver="Enter Unified Version Number: "
+set /p newufwver="Enter New Unified Version Number: "
+set /p ufwver="Enter Old Unified Version Number: "
 
-"C:\Program Files\7-Zip\7z.exe" a -tZip D:\Work\UF2Packages\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware
-"C:\Program Files\7-Zip\7z.exe" a -tZip D:\Work\UF2Packages\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\CrealityV422\Firmware"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\LCD Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\CrealityV422\Firmware"
+rename "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV422-UFW_%ufwver%.zip" TH3D_Unified2_CrealityV422-UFW_%newufwver%.zip
 
-"C:\Program Files\7-Zip\7z.exe" a -tZip D:\Work\UF2Packages\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware
-"C:\Program Files\7-Zip\7z.exe" a -tZip D:\Work\UF2Packages\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\CrealityV427\Firmware"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\CrealityV427\Firmware"
+rename "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV427-UFW_%ufwver%.zip" TH3D_Unified2_CrealityV427-UFW_%newufwver%.zip
 
-"C:\Program Files\7-Zip\7z.exe" a -tZip D:\Work\UF2Packages\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware
-"C:\Program Files\7-Zip\7z.exe" a -tZip D:\Work\UF2Packages\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\SKR_E3_Mini\Firmware"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\SKR_E3_Mini\Firmware"
+rename "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_%ufwver%.zip" TH3D_Unified2_SKR_E3_Mini-UFW_%newufwver%.zip
 
-"C:\Program Files\7-Zip\7z.exe" a -tZip D:\Work\UF2Packages\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware
-"C:\Program Files\7-Zip\7z.exe" a -tZip D:\Work\UF2Packages\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\TH3D_EZBoardLite\Firmware"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\TH3D_EZBoardLite\Firmware"
+rename "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" TH3D_Unified2_TH3D_EZBoardLite-UFW_%newufwver%.zip
