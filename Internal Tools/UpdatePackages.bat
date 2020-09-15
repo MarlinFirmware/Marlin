a98df2981d0ca56e8a6b6b5cc7479e7c63570194
Tim Hoogland <tim@th3dstudio.com>
2020-09-15 18:05:53 -0500

    EZBoard 206 Update Files
    EZBoard 206 Update Files

--- Internal Tools/UpdatePackages.bat
+++ Internal Tools/UpdatePackages.bat
@@ -54,6 +54,10 @@ ping localhost -n 5 >NUL
 ping localhost -n 5 >NUL
 "C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
 ping localhost -n 5 >NUL
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\EZBoard Installation Guide"
+ping localhost -n 5 >NUL
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Pre-Compiled Binaries\EZBoard Pre-Compiled Firmware"
+ping localhost -n 5 >NUL
 "C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
 ping localhost -n 5 >NUL
 "C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_%ufwver%.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\TH3D_EZBoardLite\Firmware"
