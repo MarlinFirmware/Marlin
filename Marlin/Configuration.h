6a08bc6bea69a031ee1244a63269dd4659dd98b5
ladismrkolj <ladismrkolj@gmail.com>
2020-09-17 12:51:30 +0200

    changed board type in platformio.ini, updated configuration_adv to newest comands, edited softwaare limits and added a temp_chamber_pin instead of temp_2_pin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1143,7 +1143,7 @@
 #define Y_MIN_POS 10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE+10
 #define Z_MAX_POS 300
 
 /**
--- Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
@@ -220,7 +220,7 @@
 //
 #define TEMP_0_PIN                          PF4   // T1 <-> E0
 #define TEMP_1_PIN                          PF5   // T2 <-> E1
-#define TEMP_2_PIN                          PF6   // T3 <-> E2
+#define TEMP_CHAMBER_PIN                          PF6   // T3 <-> E2
 #define TEMP_BED_PIN                        PF3   // T0 <-> Bed
 
 //
