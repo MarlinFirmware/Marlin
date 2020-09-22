d1eeb692c108b387342ab653ce29eb4a3cf56487
Jason Smith <jason.inet@gmail.com>
2020-09-22 15:36:59 -0700

    Update board/teensy comment (#19456)

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -359,8 +359,8 @@
 
 #define BOARD_THE_BORG                5000  // THE-BORG (Power outputs: Hotend0, Hotend1, Bed, Fan)
 #define BOARD_REMRAM_V1               5001  // RemRam v1
-#define BOARD_TEENSY41                5002  // Teensy4.0 and Teensy4.1
-#define BOARD_T41U5XBB                5003  // T41U5XBB Teensy4.1 breakout board
+#define BOARD_TEENSY41                5002  // Teensy 4.1
+#define BOARD_T41U5XBB                5003  // T41U5XBB Teensy 4.1 breakout board
 
 //
 // Espressif ESP32 WiFi
