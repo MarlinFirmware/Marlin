dec5d4b7ac5661a791350e722c29928cffb46a5e
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-09-23 16:44:32 +0200

    Fix auto-homing attempt

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -849,7 +849,7 @@
 #define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
-//#define USE_PROBE_FOR_Z_HOMING
+#define USE_PROBE_FOR_Z_HOMING
 
 /**
  * Z_MIN_PROBE_PIN
