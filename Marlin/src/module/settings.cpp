ce6b2d199f73c170471982117431eae809afdcbf
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-09-23 15:39:17 -0500

    Check 'validating' flag

--- Marlin/src/module/settings.cpp
+++ Marlin/src/module/settings.cpp
@@ -1804,13 +1804,11 @@ void MarlinSettings::postprocess() {
       //
       {
         _FIELD_TEST(lcd_contrast);
-        #if HAS_LCD_CONTRAST
-          int16_t &lcd_contrast = ui.contrast;
-        #else
-          int16_t lcd_contrast;
-        #endif
+        int16_t lcd_contrast;
         EEPROM_READ(lcd_contrast);
-        TERN_(HAS_LCD_CONTRAST, ui.set_contrast(lcd_contrast));
+        if (!validating) {
+          TERN_(HAS_LCD_CONTRAST, ui.set_contrast(lcd_contrast));
+        }
       }
 
       //
