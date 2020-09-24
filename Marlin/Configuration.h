51dcbe1a34e1623e8fff3524ac15276be9c99afb
miferkolb <61791039+miferkolb@users.noreply.github.com>
2020-09-24 15:37:01 +0200

    changed DEFAULT_AXIS_STEPS_PER_UNIT to 160 in Z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -724,7 +724,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 102.073 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 160, 102.073 }
 
 /**
  * Default Max Feed Rate (mm/s)
