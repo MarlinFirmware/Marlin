9ce86d58b56ecc9ea08a86aee5a99c4cdf7e632a
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-09-24 11:54:51 +0200

    Use interruptable endstops (experiment)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -702,7 +702,7 @@
 
 // Enable this feature if all enabled endstop pins are interrupt-capable.
 // This will remove the need to poll the interrupt pins, saving many CPU cycles.
-//#define ENDSTOP_INTERRUPTS_FEATURE
+#define ENDSTOP_INTERRUPTS_FEATURE
 
 /**
  * Endstop Noise Threshold
