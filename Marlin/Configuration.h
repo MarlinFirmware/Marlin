549932363156c34bfa2915face988854f3b0f5fa
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-09-24 17:14:29 +0200

    Enable custom version .h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -72,7 +72,7 @@
 
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "CR-6 SE" // Who made the changes.
-#//define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
+#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 #define	MACVERSION		STRING_CONFIG_H_AUTHOR
 #define	SOFTVERSION		SHORT_BUILD_VERSION
 #define MAC_LENGTH              235
