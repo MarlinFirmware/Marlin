1bf131bdb9389c09cc66311f4d4e2a45536ce647
Pavel Roman <233407@LAW-352575-NB.staff.ucn.muni.cz>
2020-09-24 10:49:08 +0200

    Delta_PR_MPMD z Thing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Delta PR_1_bot"
+#define CUSTOM_MACHINE_NAME "Delta_PR_MPMD z Thing"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
