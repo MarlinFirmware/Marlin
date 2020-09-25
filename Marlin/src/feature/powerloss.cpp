445667bb58230e3cdbf88adbd2fc0dd3473332e3
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-09-25 16:22:41 -0500

    Add POWER_LOSS_RECOVER_ZHOME

--- Marlin/src/feature/powerloss.cpp
+++ Marlin/src/feature/powerloss.cpp
@@ -359,7 +359,9 @@ void PrintJobRecovery::resume() {
     gcode.process_subcommands_now_P(PSTR(
       "G28R0"                               // No raise during G28
       TERN_(MARLIN_DEV_MODE, "S")           // Simulated Homing
-      TERN_(IS_CARTESIAN, "XY")             // Don't home Z on Cartesian
+      #if IS_CARTESIAN && DISABLED(POWER_LOSS_RECOVER_ZHOME)
+        "XY"                                // Don't home Z on Cartesian unless overridden
+      #endif
     ));
 
   #endif
