a11ba513899e8c4ed0400657635f18a37d1c650b
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-09-28 01:01:35 -0500

    Menu tweak

--- Marlin/src/lcd/menu/menu.cpp
+++ Marlin/src/lcd/menu/menu.cpp
@@ -343,8 +343,10 @@ void scroll_screen(const uint8_t limit, const bool is_menu) {
 #endif // BABYSTEP_ZPROBE_OFFSET
 
 void _lcd_draw_homing() {
-  constexpr uint8_t line = (LCD_HEIGHT - 1) / 2;
-  if (ui.should_draw()) MenuItem_static::draw(line, GET_TEXT(MSG_LEVEL_BED_HOMING));
+  if (ui.should_draw()) {
+    constexpr uint8_t line = (LCD_HEIGHT - 1) / 2;
+    MenuItem_static::draw(line, GET_TEXT(MSG_LEVEL_BED_HOMING));
+  }
 }
 
 #if ENABLED(LCD_BED_LEVELING) || (HAS_LEVELING && DISABLED(SLIM_LCD_MENUS))
