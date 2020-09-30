e1047dbb6a7544a512d7410e90c04d5b88c00e61
Tim Hoogland <tim@th3dstudio.com>
2020-09-30 10:22:13 -0500

    Fix Home Adjust Feature variables
    Fix Home Adjust Feature variables for certain board configs that were set incorrectly.

--- Board Configuration Files/Blank_Template/Marlin/Configuration.h
+++ Board Configuration Files/Blank_Template/Marlin/Configuration.h
@@ -232,8 +232,8 @@
   #define Z_MAX_POS 250
   
   #if ENABLED(HOME_ADJUST)
-    #define X_MIN_POS X_HOME_ADJUST_LOCATION
-    #define Y_MIN_POS Y_HOME_ADJUST_LOCATION
+    #define X_MIN_POS X_HOME_LOCATION
+    #define Y_MIN_POS Y_HOME_LOCATION
   #else
     #define X_MIN_POS 0
     #define Y_MIN_POS 0
