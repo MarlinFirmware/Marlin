6002dc634e56b50a47b272169ba009018de2f4f5
Scott Lahteine <github@thinkyhead.com>
2020-10-05 01:09:32 -0500

    Trailing whitespace

--- buildroot/share/PlatformIO/variants/archim/build_gcc/release.mk
+++ buildroot/share/PlatformIO/variants/archim/build_gcc/release.mk
@@ -8,7 +8,7 @@
 #
 #  This library is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #  See the GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public
