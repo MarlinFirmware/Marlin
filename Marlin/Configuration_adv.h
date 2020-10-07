7860e39170cee67d1fd8c9a59b8144cefa668c04
miferkolb <61791039+miferkolb@users.noreply.github.com>
2020-10-07 11:36:32 +0200

    Changd probing points in config_adv

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1526,12 +1526,12 @@
  * Override if the automatically selected points are inadequate.
  */
 #if EITHER(AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_UBL)
-  //#define PROBE_PT_1_X 15
-  //#define PROBE_PT_1_Y 180
-  //#define PROBE_PT_2_X 15
-  //#define PROBE_PT_2_Y 20
-  //#define PROBE_PT_3_X 170
-  //#define PROBE_PT_3_Y 20
+  #define PROBE_PT_1_X 20
+  #define PROBE_PT_1_Y 10
+  #define PROBE_PT_2_X 260
+  #define PROBE_PT_2_Y 10
+  #define PROBE_PT_3_X 130
+  #define PROBE_PT_3_Y 140
 #endif
 
 /**
