fe918e7709fed32bc5dc6f17d8c42ca99e6432a2
Foxies <Foxies-CSTL@users.noreply.github.com>
2020-10-08 09:57:00 +0200

    Update QQS_Config.h

--- Marlin/QQS_Config.h
+++ Marlin/QQS_Config.h
@@ -12,6 +12,7 @@
 //#define STOCK
 
 /*------Drivers-(1 CHOICE)-----*/
+<<<<<<< Updated upstream
 #define QQS               //(S) 4xA4988
 //#define QQS_TMC           //(8) 4xTMC220x For 2208 or 2209
 
@@ -24,6 +25,19 @@
 
 // Set Software Serial UART for TMC 2208 / TMC 2209
 //#define TMC_SOFTWARE_SERIAL                             
+=======
+//#define QQS               //(S) 4xA4988
+//#define QQS_TMC           //(8) 4xTMC220x For 2208 or 2209
+
+/* MODE UART XYZ*/
+#define QQS_UART8           //(U8) 4xTMC2208 (Remove module ESP12)
+//#define QQS_UART9         //(U9) 4xTMC2209 (Remove module ESP12)
+
+/*------- Choice Other driver for EXTRUDER-------*/
+#define DRIVER_EXT  TMC2208_STANDALONE
+
+// Set Software Serial UART for TMC 2208 / TMC 2209
+>>>>>>> Stashed changes
 #ifdef QQS_UART8
     #define QQS_UART
     #define SOFTWARE_SERIAL
@@ -34,7 +48,10 @@
 #endif
 
 // Set Hardware Serial UART only TMC 2209
+<<<<<<< Updated upstream
 //#define TMC_HARDWARE_SERIAL                             
+=======
+>>>>>>> Stashed changes
 #ifdef QQS_UART9
     #define QQS_UART
     #define HARDWARE_SERIAL
@@ -46,10 +63,17 @@
 
 
 /*----  Modules -----*/
+<<<<<<< Updated upstream
 #define ESP_WIFI               //(W) Module ESP8266/ESP12
 //#define ESP3D_WIFISUPPORT      //(W)
 //#define BMG                    //(B) Extruder
 //#define NEOPIXEL_LED           //(N) Use port GPIO Wifi module (PA10/PA9/PA8/PC7)
+=======
+//#define ESP_WIFI               //(W) Module ESP8266/ESP12
+//#define ESP3D_WIFISUPPORT      //(W)
+//#define BMG                    //(B) Extruder
+#define NEOPIXEL_LED             //(N) Use port GPIO Wifi module (PA10/PA9/PA8/PC7)
+>>>>>>> Stashed changes
 
 
 //Many options for Modules: 
@@ -60,17 +84,29 @@
 
 /*-------Screen Mks_Robin_TFT_v2---(FSMC)-----*/
 //Choice UI TFT 
+<<<<<<< Updated upstream
 //#define FSMC_GRAPHICAL_TFT      //(F) UI STANDARD 
 #define TFT_320x240             //(C) UI MARLIN (too big with mode UART+UBL=ok with nanolib)
 //#define TFT_LVGL_UI_FSMC        //(I) UI MKS  => (Bug)
+=======
+//#define FSMC_GRAPHICAL_TFT    //(F) UI STANDARD 
+#define TFT_320x240             //(C) UI MARLIN (too big with mode UART+UBL=ok with nanolib)
+//#define TFT_LVGL_UI_FSMC      //(I) UI MKS  => (Bug)
+>>>>>>> Stashed changes
 
 //============= End_Hardware ===============//
 
 //Choice menu: (OPT)
 #define DELTA_CALIBRATION_MENU        //NC LVGL
+<<<<<<< Updated upstream
 #define PID_EDIT_MENU                 //
 #define PID_AUTOTUNE_MENU             //
 //#define PAUSE_BEFORE_DEPLOY_STOW      //Message Stow/remove Probe (bug Octoprint & UI Marlin)
+=======
+#define PID_EDIT_MENU              //
+#define PID_AUTOTUNE_MENU          //
+#define PAUSE_BEFORE_DEPLOY_STOW      //Message Stow/remove Probe (bug Octoprint & UI Marlin)
+>>>>>>> Stashed changes
 #define LCD_INFO_MENU                 //
 //#define LED_CONTROL_MENU              // For LedStrip
 
