a14e26c095c5d13835597cb5112c6c1d8ce05cb4
Serhiy-K <52166448+Serhiy-K@users.noreply.github.com>
2020-10-10 13:01:46 +0300

    Fixes for TFTGLCD Panel, FastIO (#19614)

--- Marlin/src/inc/Conditionals_LCD.h
+++ Marlin/src/inc/Conditionals_LCD.h
@@ -226,9 +226,9 @@
     #define LCD_PROGRESS_BAR
   #endif
   #if ENABLED(TFTGLCD_PANEL_I2C)
-    #define LCD_USE_I2C_BUZZER              // Enable buzzer on LCD for I2C and SPI buses (LiquidTWI2 not required)
     #define LCD_I2C_ADDRESS           0x27  // Must be equal to panel's I2C slave addres
   #endif
+  #define LCD_USE_I2C_BUZZER                // Enable buzzer on LCD, used for both I2C and SPI buses (LiquidTWI2 not required)
   #define STD_ENCODER_PULSES_PER_STEP 2
   #define STD_ENCODER_STEPS_PER_MENU_ITEM 1
   #define LCD_WIDTH                   20    // 20 or 24 chars in line
