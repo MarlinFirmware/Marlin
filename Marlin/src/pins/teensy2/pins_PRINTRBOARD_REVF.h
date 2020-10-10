9304d194087662ecfd105dfb27a242ffe4d4b737
Jason Smith <jason.inet@gmail.com>
2020-10-09 23:18:57 -0700

    Remove check for USBCON for printrboard, since this file is processed prior to its definition for dependency detection

--- Marlin/src/pins/teensy2/pins_PRINTRBOARD_REVF.h
+++ Marlin/src/pins/teensy2/pins_PRINTRBOARD_REVF.h
@@ -67,10 +67,6 @@
   #error "Oops! Select 'Teensy++ 2.0' or 'Printrboard' in 'Tools > Board.'"
 #endif
 
-#ifndef USBCON
-  #error "USBCON should be defined by the platform for this board."
-#endif
-
 #define BOARD_INFO_NAME "Printrboard Rev.F"
 
 // Disable JTAG pins so EXP1 pins work correctly
