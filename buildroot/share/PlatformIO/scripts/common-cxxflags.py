25e115fcb997744c29fa58a6215cdf3e58bb2f71
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2020-10-10 15:53:31 -0300

    allow build flags per folder/files/any match. Added to remove LiquidCrystal multi line comment warning

--- buildroot/share/PlatformIO/scripts/common-cxxflags.py
+++ buildroot/share/PlatformIO/scripts/common-cxxflags.py
@@ -2,6 +2,9 @@
 # common-cxxflags.py
 # Convenience script to apply customizations to CPP flags
 #
+
+import re
+
 Import("env")
 env.Append(CXXFLAGS=[
   "-Wno-register"
@@ -10,3 +13,23 @@ env.Append(CXXFLAGS=[
   #"-Wno-maybe-uninitialized",
   #"-Wno-sign-compare"
 ])
+
+
+# We can use it to apply custom compilation flags for individual folders or files
+def ignore_lcd_warnings(node):
+	"""
+	`node.name` - a name of File System Node
+	`node.get_path()` - a relative path
+	`node.get_abspath()` - an absolute path
+	"""
+	mat = re.match(r'.*\/LiquidCrystal\/.*', node.get_path())
+	if not mat:
+		return node
+
+	#print(node.get_path())
+	return env.Object(
+        node,
+        CCFLAGS=env["CCFLAGS"] + ["-Wno-comment"]
+    )
+
+env.AddBuildMiddleware(ignore_lcd_warnings)
