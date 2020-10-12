18137ee77c961ef3ab9c037c4b83f4c090c01889
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-10-11 19:45:51 -0500

    Update G35.cpp

--- Marlin/src/gcode/bedlevel/G35.cpp
+++ Marlin/src/gcode/bedlevel/G35.cpp
@@ -160,7 +160,7 @@ void GcodeSuite::G35() {
              " ", (screw_thread & 1) == (adjust > 0) ? "CCW" : "CW",
              " by ", abs(full_turns), " turns");
       if (minutes) SERIAL_ECHOPAIR(" and ", abs(minutes), " minutes");
-      if ENABLED(TRAMMING_POINT_Z_DISTANCE) SERIAL_ECHOPAIR(", Diff: ", (diff * -1));
+      if (ENABLED(TRAMMING_POINT_Z_DISTANCE)) SERIAL_ECHOPAIR(" (", -diff, "mm)");
       SERIAL_EOL();
     }
   }
