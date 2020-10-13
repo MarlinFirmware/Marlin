33b2f0700e47eaaed5eab23fa06c86a42d2184c8
Jason Smith <jason.inet@gmail.com>
2020-10-13 16:03:09 -0700

    Add NUCLEO-F767ZI dev board (#19373)
    Co-authored-by: Lorenzo Delana <lorenzo.delana@gmail.com>

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -363,6 +363,7 @@
 #define BOARD_REMRAM_V1               5001  // RemRam v1
 #define BOARD_TEENSY41                5002  // Teensy 4.1
 #define BOARD_T41U5XBB                5003  // T41U5XBB Teensy 4.1 breakout board
+#define BOARD_NUCLEO_F767ZI           5004  // ST NUCLEO-F767ZI Dev Board
 
 //
 // Espressif ESP32 WiFi
