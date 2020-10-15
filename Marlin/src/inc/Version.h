a33250fabb232bc0149494e36094da19ecfe7d00
Scott Lahteine <github@thinkyhead.com>
2020-10-14 23:55:20 -0500

    Marlin 2.0.7.2

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.0.7.1"
+  #define SHORT_BUILD_VERSION "2.0.7.2"
 #endif
 
 /**
