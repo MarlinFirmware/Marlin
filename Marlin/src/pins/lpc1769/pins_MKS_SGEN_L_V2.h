b7ae0f9a96046124d579d9c3735ab08a5c35dd4a
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-10-16 02:15:12 -0500

    Update pins_MKS_SGEN_L_V2.h

--- Marlin/src/pins/lpc1769/pins_MKS_SGEN_L_V2.h
+++ Marlin/src/pins/lpc1769/pins_MKS_SGEN_L_V2.h
@@ -227,17 +227,24 @@
 // Misc. Functions
 //
 #define LED_PIN                            P1_18  // Used as a status indicator
-//#define LED2_PIN                         P1_19
-//#define LED3_PIN                         P1_20
-//#define LED4_PIN                         P1_21
 
 //
 // RGB LED
 //
 #if ENABLED(RGB_LED)
-  #define RGB_LED_R_PIN                    P1_19
-  #define RGB_LED_G_PIN                    P1_20
-  #define RGB_LED_B_PIN                    P1_21
+  #ifndef RGB_LED_R_PIN
+    #define RGB_LED_R_PIN                  P1_19
+  #endif
+  #ifndef RGB_LED_G_PIN
+    #define RGB_LED_G_PIN                  P1_20
+  #endif
+  #ifndef RGB_LED_B_PIN
+    #define RGB_LED_B_PIN                  P1_21
+  #endif
+#else
+  #define LED2_PIN                         P1_19  // Initialized by HAL/LPC1768/main.cpp
+  #define LED3_PIN                         P1_20
+  #define LED4_PIN                         P1_21
 #endif
 
 /**
