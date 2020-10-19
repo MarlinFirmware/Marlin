3fa73d57c3c4fc4409b5fcc872cb264d311fff78
Jaykkin <51738128+Jaykkin@users.noreply.github.com>
2020-10-19 12:09:33 -0500

    Change BLTouch Nozzle Orientation
    Since I reversed the bed orientation, I needed to change where the BLTouch was in relation to the nozzle.  With the old orientation, the machine thought it was behind the nozzle, but with the new orientation, it is in front of the nozzle.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 36.5, -.55 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -36.5, -.55 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
