529c951c8b6b869fd2947526a09ff2ebb1cbf815
Jaykkin <51738128+Jaykkin@users.noreply.github.com>
2020-10-19 12:18:29 -0500

    Update Configuration.h
    Updated Author

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Kory Olson" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
