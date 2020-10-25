fbb8a716abdb971e4385e78f6b959ae829ba7552
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-10-25 14:29:48 +0100

    Enable RESTORE_LEVELING_AFTER_G28

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1248,7 +1248,7 @@
  * Normally G28 leaves leveling disabled on completion. Enable
  * this option to have G28 restore the prior leveling state.
  */
-//#define RESTORE_LEVELING_AFTER_G28
+#define RESTORE_LEVELING_AFTER_G28
 
 /**
  * Enable detailed logging of G28, G29, M48, etc.
