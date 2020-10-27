70cf1b0918b2867fff38ca07a01b0a1d42fd64f2
mickcomp <49050081+mickcomp@users.noreply.github.com>
2020-10-28 10:58:19 +1100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,11 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
+<<<<<<< Updated upstream
 #define STRING_CONFIG_H_AUTHOR "(MickComp, CoreXY)" // Who made the changes.
+=======
+#define STRING_CONFIG_H_AUTHOR "(MickCOMP, CoreXYv2)" // Who made the changes.
+>>>>>>> Stashed changes
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -132,6 +136,7 @@
 
 // Name displayed in the LCD "Ready" message and Info menu
 //#define CUSTOM_MACHINE_NAME "3D Printer"
+#define CUSTOM_MACHINE_NAME "ISO Cube"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
