b2584dbc913fa71d7c085f952dc9b2815eec6ebb
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-10-27 17:16:36 -0500

    Use char for single char

--- Marlin/src/gcode/bedlevel/G35.cpp
+++ Marlin/src/gcode/bedlevel/G35.cpp
@@ -131,7 +131,7 @@ void GcodeSuite::G35() {
     if (isnan(z_probed_height)) {
       SERIAL_ECHOPAIR("G35 failed at point ", int(i), " (");
       SERIAL_ECHOPGM_P((char *)pgm_read_ptr(&tramming_point_name[i]));
-      SERIAL_ECHOPGM(")");
+      SERIAL_CHAR(')');
       SERIAL_ECHOLNPAIR_P(SP_X_STR, screws_tilt_adjust_pos[i].x, SP_Y_STR, screws_tilt_adjust_pos[i].y);
       err_break = true;
       break;
@@ -140,8 +140,8 @@ void GcodeSuite::G35() {
     if (DEBUGGING(LEVELING)) {
       DEBUG_ECHOPAIR("Probing point ", int(i), " (");
       DEBUG_PRINT_P((char *)pgm_read_ptr(&tramming_point_name[i]));
-      DEBUG_ECHOPGM(")");
-      SERIAL_ECHOLNPAIR_P(SP_X_STR, screws_tilt_adjust_pos[i].x, SP_Y_STR, screws_tilt_adjust_pos[i].y, SP_Z_STR, z_probed_height);
+      DEBUG_CHAR(')');
+      DEBUG_ECHOLNPAIR_P(SP_X_STR, screws_tilt_adjust_pos[i].x, SP_Y_STR, screws_tilt_adjust_pos[i].y, SP_Z_STR, z_probed_height);
     }
 
     z_measured[i] = z_probed_height;
@@ -161,8 +161,7 @@ void GcodeSuite::G35() {
 
       SERIAL_ECHOPGM("Turn ");
       SERIAL_ECHOPGM_P((char *)pgm_read_ptr(&tramming_point_name[i]));
-      SERIAL_ECHOPAIR(" ", (screw_thread & 1) == (adjust > 0) ? "CCW" : "CW",
-             " by ", abs(full_turns), " turns");
+      SERIAL_ECHOPAIR(" ", (screw_thread & 1) == (adjust > 0) ? "CCW" : "CW", " by ", abs(full_turns), " turns");
       if (minutes) SERIAL_ECHOPAIR(" and ", abs(minutes), " minutes");
       if (ENABLED(REPORT_TRAMMING_MM)) SERIAL_ECHOPAIR(" (", -diff, "mm)");
       SERIAL_EOL();
