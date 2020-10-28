58e1bcf39edf9743d268a3b4bb50839725d92a53
Giuliano Zaro <3684609+GMagician@users.noreply.github.com>
2020-10-28 11:40:59 +0100

    Remove senseless #undef

--- Marlin/src/inc/SanityCheck.h
+++ Marlin/src/inc/SanityCheck.h
@@ -879,7 +879,6 @@ static_assert(Y_MAX_LENGTH >= Y_BED_SIZE, "Movement bounds (Y_MIN_POS, Y_MAX_POS
  */
 #if HAS_PRUSA_MMU2
   #if EXTRUDERS != 5
-    #undef SINGLENOZZLE
     #error "PRUSA_MMU2(S) requires exactly 5 EXTRUDERS. Please update your Configuration."
   #elif DISABLED(NOZZLE_PARK_FEATURE)
     #error "PRUSA_MMU2(S) requires NOZZLE_PARK_FEATURE. Enable it to continue."
