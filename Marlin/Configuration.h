fa0614516a51d06bfddbc9c3a142a67fe16d74e5
Gurmeet Athwal <gurmeet.athwal@gmail.com>
2020-10-28 08:25:49 +0530

    Update ExtUI path in config comment (#19908)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2196,7 +2196,7 @@
 
 //
 // Third-party or vendor-customized controller interfaces.
-// Sources should be installed in 'src/lcd/extensible_ui'.
+// Sources should be installed in 'src/lcd/extui'.
 //
 //#define EXTENSIBLE_UI
 
