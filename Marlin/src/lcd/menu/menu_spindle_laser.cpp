3dbe4ac59395048c98f6cd76bdaada7fac807f64
Victor <victorrar.ru@gmail.com>
2020-10-30 00:17:04 +0200

    FAST_PWM for STM32F1 (#19952)

--- Marlin/src/lcd/menu/menu_spindle_laser.cpp
+++ Marlin/src/lcd/menu/menu_spindle_laser.cpp
@@ -55,10 +55,8 @@
       #endif
     }
 
-    #if ENABLED(MARLIN_DEV_MODE)
-      #if ENABLED(HAL_CAN_SET_PWM_FREQ) && defined(SPINDLE_LASER_FREQUENCY)
-        EDIT_ITEM_FAST(CUTTER_MENU_FREQUENCY_TYPE, MSG_CUTTER_FREQUENCY, &cutter.frequency, 2000, 50000, cutter.refresh_frequency);
-      #endif
+    #if BOTH(MARLIN_DEV_MODE, HAL_CAN_SET_PWM_FREQ) && defined(SPINDLE_LASER_FREQUENCY)
+      EDIT_ITEM_FAST(CUTTER_MENU_FREQUENCY_TYPE, MSG_CUTTER_FREQUENCY, &cutter.frequency, 2000, 50000, cutter.refresh_frequency);
     #endif
     END_MENU();
   }
