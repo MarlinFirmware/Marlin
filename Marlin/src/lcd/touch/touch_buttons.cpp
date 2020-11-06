ac2e3f8cddc3f92c8652d781dc48de9a7835a4d2
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2020-11-05 22:58:15 -0300

    using saved calibration values

--- Marlin/src/lcd/touch/touch_buttons.cpp
+++ Marlin/src/lcd/touch/touch_buttons.cpp
@@ -59,8 +59,8 @@ uint8_t TouchButtons::read_buttons() {
       }
     #endif
 
-    x = uint16_t((uint32_t(x) * XPT2046_X_CALIBRATION) >> 16) + XPT2046_X_OFFSET;
-    y = uint16_t((uint32_t(y) * XPT2046_Y_CALIBRATION) >> 16) + XPT2046_Y_OFFSET;
+    x = int16_t((int32_t(x) * touch_calibration.calibration.x) >> 16) + touch_calibration.calibration.offset_x;
+    y = int16_t((int32_t(y) * touch_calibration.calibration.y) >> 16) + touch_calibration.calibration.offset_y;
 
     // Touch within the button area simulates an encoder button
     if (y > BUTTON_AREA_TOP && y < BUTTON_AREA_BOT)
