4785b04aa13470ee8f1b8f2db07058d473616b06
George Fu <nailao_5918@163.com>
2020-11-07 13:48:52 +0800

    FYSETC E4 board support (#20032)

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -665,6 +665,8 @@
   #include "esp32/pins_MRR_ESPE.h"              // ESP32                                  env:esp32
 #elif MB(E4D_BOX)
   #include "esp32/pins_E4D.h"                   // ESP32                                  env:esp32
+#elif MB(FYSETC_E4)
+  #include "esp32/pins_FYSETC_E4.h"             // ESP32                                  env:FYSETC_E4
 
 //
 // Adafruit Grand Central M4 (SAMD51 ARM Cortex-M4)
