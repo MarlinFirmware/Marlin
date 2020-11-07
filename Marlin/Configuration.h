10fd9ed896bc2eebc4f4d81e02d7f7502e207039
Darren Horrocks <killallthehumans@gmail.com>
2020-11-07 08:42:14 +0000

    Anet 1.0 alternative graphical LCD wiring (#20022)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2095,9 +2095,10 @@
 //
 // Anet 128x64 full graphics lcd with rotary encoder as used on Anet A6
 // A clone of the RepRapDiscount full graphics display but with
-// different pins/wiring (see pins_ANET_10.h).
+// different pins/wiring (see pins_ANET_10.h). Enable one of these.
 //
 //#define ANET_FULL_GRAPHICS_LCD
+//#define ANET_FULL_GRAPHICS_LCD_ALT_WIRING
 
 //
 // AZSMZ 12864 LCD with SD
