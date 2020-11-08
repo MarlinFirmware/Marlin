8fd37402a3055f4ce7bcc2e39d225ae215b0c5ab
InsanityAutomation <d.menzel@insanityautomation.com>
2020-11-08 14:14:07 -0500

    Bump rev, invert flag

--- Marlin/src/lcd/extui/lib/Creality/Creality_DWIN.cpp
+++ Marlin/src/lcd/extui/lib/Creality/Creality_DWIN.cpp
@@ -1512,7 +1512,7 @@ SERIAL_ECHOLN(PSTR("BeginSwitch"));
       {
         if(
           #if (FIL_RUNOUT_STATE == LOW)
-            #define FIL_RUNOUT_INVERTING true
+            #define FIL_RUNOUT_INVERTING false
           #endif
         #if DISABLED(FILAMENT_RUNOUT_SENSOR) || ENABLED(FILAMENT_MOTION_SENSOR)
           true
