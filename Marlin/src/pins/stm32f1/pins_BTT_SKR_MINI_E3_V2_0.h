1677a9d81190c760c73c1c781ba4f3734498fd91
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-11-08 02:18:57 -0600

    Move POWER_LOSS_PIN

--- Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
@@ -43,10 +43,6 @@
 
 #define PS_ON_PIN                          PC13  // Power Supply Control
 
-#ifndef POWER_LOSS_PIN
-  #define POWER_LOSS_PIN                   PC12  // Power Loss Detection: PWR-DET
-#endif
-
 #define FAN1_PIN                           PC7
 
 #ifndef CONTROLLER_FAN_PIN
