66a3cf3349791878e87c4da0dcc6de259e1be46b
vitaliy172 <68148475+vitaliy172@users.noreply.github.com>
2020-11-11 11:16:38 +0200

    Fixed name of Ukrainian language

--- Marlin/src/lcd/language/language_ua.h
+++ Marlin/src/lcd/language/language_ua.h
@@ -30,7 +30,7 @@
 
 #define DISPLAY_CHARSET_ISO10646_5
 
-namespace Language_uk {
+namespace Language_ua {
   using namespace Language_en; // Inherit undefined strings from English
 
   constexpr uint8_t    CHARSIZE                            = 2;
