c6ed827466b991af3a2f604d66d8572822266205
phcay <58492957+phcay@users.noreply.github.com>
2020-11-13 11:58:12 +0100

    Update stepper.cpp

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -2266,72 +2266,62 @@ uint32_t Stepper::block_phase_isr() {
     else
       interval = LA_ADV_NEVER;
 
-    DIR_WAIT_BEFORE();
-
-    #if ENABLED(MIXING_EXTRUDER)
-      // We don't know which steppers will be stepped because LA loop follows,
-      // with potentially multiple steps. Set all.
-      if (LA_steps > 0)
-        MIXER_STEPPER_LOOP(j) NORM_E_DIR(j);
-      else if (LA_steps < 0)
-        MIXER_STEPPER_LOOP(j) REV_E_DIR(j);
-    #else
-      if (LA_steps > 0)
-        NORM_E_DIR(stepper_extruder);
-      else if (LA_steps < 0)
-        REV_E_DIR(stepper_extruder);
-    #endif
-
-    DIR_WAIT_AFTER();
-
-    //const hal_timer_t added_step_ticks = hal_timer_t(ADDED_STEP_TICKS);
-
-    // Step E stepper if we have steps
-    #if ISR_MULTI_STEPS
-      bool firstStep = true;
-      USING_TIMED_PULSE();
-    #endif
-
-    while (LA_steps) {
+    if (LA_steps) { // LA_steps != 0
+      DIR_WAIT_BEFORE();
+      #if ENABLED(MIXING_EXTRUDER)
+        // We don't know which steppers will be stepped because LA loop follows,
+        // with potentially multiple steps. Set all.
+        if (LA_steps > 0)
+          MIXER_STEPPER_LOOP(j) NORM_E_DIR(j);
+        else if (LA_steps < 0)
+          MIXER_STEPPER_LOOP(j) REV_E_DIR(j);
+      #else
+        if (LA_steps > 0)
+          NORM_E_DIR(stepper_extruder);
+        else if (LA_steps < 0)
+          REV_E_DIR(stepper_extruder);
+      #endif
+      DIR_WAIT_AFTER();
+      //const hal_timer_t added_step_ticks = hal_timer_t(ADDED_STEP_TICKS);
+      // Step E stepper if we have steps
       #if ISR_MULTI_STEPS
-        if (firstStep)
-          firstStep = false;
-        else
-          AWAIT_LOW_PULSE();
+        bool firstStep = true;
+        USING_TIMED_PULSE();
       #endif
-
-      // Set the STEP pulse ON
-      #if ENABLED(MIXING_EXTRUDER)
-        E_STEP_WRITE(mixer.get_next_stepper(), !INVERT_E_STEP_PIN);
-      #else
-        E_STEP_WRITE(stepper_extruder, !INVERT_E_STEP_PIN);
-      #endif
-
-      // Enforce a minimum duration for STEP pulse ON
-      #if ISR_PULSE_CONTROL
-        START_HIGH_PULSE();
-      #endif
-
-      LA_steps < 0 ? ++LA_steps : --LA_steps;
-
-      #if ISR_PULSE_CONTROL
-        AWAIT_HIGH_PULSE();
-      #endif
-
-      // Set the STEP pulse OFF
-      #if ENABLED(MIXING_EXTRUDER)
-        E_STEP_WRITE(mixer.get_stepper(), INVERT_E_STEP_PIN);
-      #else
-        E_STEP_WRITE(stepper_extruder, INVERT_E_STEP_PIN);
-      #endif
-
-      // For minimum pulse time wait before looping
-      // Just wait for the requested pulse duration
-      #if ISR_PULSE_CONTROL
-        if (LA_steps) START_LOW_PULSE();
-      #endif
-    } // LA_steps
-
+      while (LA_steps) {
+        #if ISR_MULTI_STEPS
+          if (firstStep)
+            firstStep = false;
+          else
+            AWAIT_LOW_PULSE();
+        #endif
+        // Set the STEP pulse ON
+        #if ENABLED(MIXING_EXTRUDER)
+          E_STEP_WRITE(mixer.get_next_stepper(), !INVERT_E_STEP_PIN);
+        #else
+          E_STEP_WRITE(stepper_extruder, !INVERT_E_STEP_PIN);
+        #endif
+        // Enforce a minimum duration for STEP pulse ON
+        #if ISR_PULSE_CONTROL
+          START_HIGH_PULSE();
+        #endif
+        LA_steps < 0 ? ++LA_steps : --LA_steps;
+        #if ISR_PULSE_CONTROL
+          AWAIT_HIGH_PULSE();
+        #endif
+        // Set the STEP pulse OFF
+        #if ENABLED(MIXING_EXTRUDER)
+          E_STEP_WRITE(mixer.get_stepper(), INVERT_E_STEP_PIN);
+        #else
+          E_STEP_WRITE(stepper_extruder, INVERT_E_STEP_PIN);
+        #endif
+        // For minimum pulse time wait before looping
+        // Just wait for the requested pulse duration
+        #if ISR_PULSE_CONTROL
+          if (LA_steps) START_LOW_PULSE();
+        #endif
+      } // LA_steps
+    } // LA_steps != 0
     return interval;
   }
 
