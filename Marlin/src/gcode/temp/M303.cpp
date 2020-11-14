6b5ddfe235c4bbbc454a2c52dcf154e774bd0f4c
Luu Lac <45380455+shitcreek@users.noreply.github.com>
2020-11-13 18:48:59 -0600

    Fix "autotune" LCD message (#20127)

--- Marlin/src/gcode/temp/M303.cpp
+++ Marlin/src/gcode/temp/M303.cpp
@@ -77,7 +77,7 @@ void GcodeSuite::M303() {
     KEEPALIVE_STATE(NOT_BUSY);
   #endif
 
-  ui.set_status(GET_TEXT(MSG_PID_AUTOTUNE));
+  ui.set_status_P(GET_TEXT(MSG_PID_AUTOTUNE));
   thermalManager.PID_autotune(temp, e, c, u);
   ui.reset_status();
 }
