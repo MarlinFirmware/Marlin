65a5eb47b3fdf93643181a29ec4098d4817976fe
phcay <58492957+phcay@users.noreply.github.com>
2020-11-14 02:24:08 +0100

    Fix extraneous Linear Advance DIR change (#20131)

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -2255,7 +2255,6 @@ uint32_t Stepper::block_phase_isr() {
         interval = LA_isr_rate;
       }
       else if (step_events_completed < decelerate_after && LA_current_adv_steps < LA_max_adv_steps) {
-             //step_events_completed <= (uint32_t)accelerate_until) {
         LA_steps++;
         LA_current_adv_steps++;
         interval = LA_isr_rate;
@@ -2265,6 +2264,8 @@ uint32_t Stepper::block_phase_isr() {
     }
     else
       interval = LA_ADV_NEVER;
+
+    if (!LA_steps) return interval; // Leave pins alone if there are no steps!
 
     DIR_WAIT_BEFORE();
 
