0fcc3ad91fbdf13a020c940b2664615e26274855
Jason Smith <jason.inet@gmail.com>
2020-11-15 15:42:07 -0800

    Fix LERDGE env names in pins.h

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -622,11 +622,11 @@
 #elif MB(BTT_BTT002_V1_0)
   #include "stm32f4/pins_BTT_BTT002_V1_0.h"     // STM32F4                                env:BIGTREE_BTT002
 #elif MB(LERDGE_K)
-  #include "stm32f4/pins_LERDGE_K.h"            // STM32F4                                env:STM32F4
+  #include "stm32f4/pins_LERDGE_K.h"            // STM32F4                                env:LERDGEK
 #elif MB(LERDGE_S)
-  #include "stm32f4/pins_LERDGE_S.h"            // STM32F4                                env:LERDGE_S
+  #include "stm32f4/pins_LERDGE_S.h"            // STM32F4                                env:LERDGES
 #elif MB(LERDGE_X)
-  #include "stm32f4/pins_LERDGE_X.h"            // STM32F4                                env:LERDGE_X
+  #include "stm32f4/pins_LERDGE_X.h"            // STM32F4                                env:LERDGEX
 #elif MB(VAKE403D)
   #include "stm32f4/pins_VAKE403D.h"            // STM32F4                                env:STM32F4
 #elif MB(FYSETC_S6)
