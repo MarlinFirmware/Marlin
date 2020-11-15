c4bcd4be75356add96158c55071ba356c5ab8245
Luke Harrison <looxonline@gmail.com>
2020-11-15 13:35:01 +0200

    Fixed z_min

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1124,7 +1124,7 @@
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
-#define Z_MIN_POS -5
+#define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE + 5
 #define Z_MAX_POS 270
