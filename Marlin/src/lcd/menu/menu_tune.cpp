ef51bd16dc8e4b28ea048b333e6de65d506cc273
Philippe Cayrol <58492957+phcay@users.noreply.github.com>
2020-11-18 06:56:51 +0100

    Better edit range for Linear Advance K (#20155)

--- Marlin/src/lcd/menu/menu_tune.cpp
+++ Marlin/src/lcd/menu/menu_tune.cpp
@@ -202,10 +202,10 @@ void menu_tune() {
   //
   #if ENABLED(LIN_ADVANCE) && DISABLED(SLIM_LCD_MENUS)
     #if EXTRUDERS == 1
-      EDIT_ITEM(float42_52, MSG_ADVANCE_K, &planner.extruder_advance_K[0], 0, 999);
+      EDIT_ITEM(float42_52, MSG_ADVANCE_K, &planner.extruder_advance_K[0], 0, 10);
     #elif HAS_MULTI_EXTRUDER
       LOOP_L_N(n, EXTRUDERS)
-        EDIT_ITEM_N(float42_52, n, MSG_ADVANCE_K_E, &planner.extruder_advance_K[n], 0, 999);
+        EDIT_ITEM_N(float42_52, n, MSG_ADVANCE_K_E, &planner.extruder_advance_K[n], 0, 10);
     #endif
   #endif
 
