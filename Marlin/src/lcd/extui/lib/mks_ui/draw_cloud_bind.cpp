344eeeb99246febea12ee0ee64ea621a16d16258
Scott Lahteine <github@thinkyhead.com>
2020-11-19 00:18:48 -0600

    Clean up formatting

--- Marlin/src/lcd/extui/lib/mks_ui/draw_cloud_bind.cpp
+++ Marlin/src/lcd/extui/lib/mks_ui/draw_cloud_bind.cpp
@@ -37,17 +37,17 @@
 
 extern lv_group_t * g;
 static lv_obj_t * scr;
-// static lv_obj_t * qr;
+//static lv_obj_t * qr;
 static lv_obj_t *button_bind_or_not = NULL, *label_bind_or_not = NULL;
 static lv_obj_t *buttonReleaseBind = NULL, *label_ReleaseBind = NULL;
 static lv_obj_t * text_id;
 
 static uint8_t unbinding_flag = 0;
-static uint8_t id_mark = 0;
+static uint8_t id_mark        = 0;
 
-#define ID_CLOUD_BIND_RETURN    1
-#define ID_CLOUD_BIND_OR_NOT    2
-#define ID_CLOUD_RELEASE_BIND   3
+#define ID_CLOUD_BIND_RETURN  1
+#define ID_CLOUD_BIND_OR_NOT  2
+#define ID_CLOUD_RELEASE_BIND 3
 
 static void event_handler(lv_obj_t * obj, lv_event_t event) {
   switch (obj->mks_obj_id) {
@@ -56,30 +56,30 @@ static void event_handler(lv_obj_t * obj, lv_event_t event) {
         // do nothing
       }
       else if (event == LV_EVENT_RELEASED) {
-		// lv_qrcode_delete(qr);
+        //lv_qrcode_delete(qr);
         clear_cur_ui();
         draw_return_ui();
       }
       break;
-	case ID_CLOUD_RELEASE_BIND:
+    case ID_CLOUD_RELEASE_BIND:
       if (event == LV_EVENT_CLICKED) {
         // do nothing
       }
       else if (event == LV_EVENT_RELEASED) {
-		if(cloud_para.state == 0x12) {			
-			clear_cur_ui();
-			lv_draw_dialog(DIALOG_TYPE_UNBIND);
-		}
+        if (cloud_para.state == 0x12) {
+          clear_cur_ui();
+          lv_draw_dialog(DIALOG_TYPE_UNBIND);
+        }
       }
       break;
   }
 }
 
 void lv_draw_cloud_bind(void) {
-  lv_obj_t *buttonBack=NULL,*label_Back=NULL;
+  lv_obj_t *buttonBack = NULL, *label_Back = NULL;
   scr = lv_screen_create(BIND_UI);
 
-  //   buttonBack = lv_imgbtn_create(scr, NULL);
+  //buttonBack = lv_imgbtn_create(scr, NULL);
 
   button_bind_or_not = lv_btn_create(scr, NULL);
   lv_obj_set_pos(button_bind_or_not, TFT_WIDTH - 130, TFT_HEIGHT - 80 * 3);
@@ -97,21 +97,21 @@ void lv_draw_cloud_bind(void) {
   lv_label_set_text(label_ReleaseBind, cloud_menu.unbind);
   lv_obj_align(label_ReleaseBind, buttonReleaseBind, LV_ALIGN_CENTER, 0, 0);
 
-//   lv_obj_set_event_cb_mks(buttonBack, event_handler,ID_CLOUD_BIND_RETURN, NULL,0);	
-//   lv_imgbtn_set_src(buttonBack, LV_BTN_STATE_REL, "F:/bmp_return.bin");
-//   lv_imgbtn_set_src(buttonBack, LV_BTN_STATE_PR, "F:/bmp_return.bin");	
-//   lv_imgbtn_set_style(buttonBack, LV_BTN_STATE_PR, &tft_style_label_pre);
-//   lv_imgbtn_set_style(buttonBack, LV_BTN_STATE_REL, &tft_style_label_rel);
+  //lv_obj_set_event_cb_mks(buttonBack, event_handler,ID_CLOUD_BIND_RETURN, NULL,0);
+  //lv_imgbtn_set_src(buttonBack, LV_BTN_STATE_REL, "F:/bmp_return.bin");
+  //lv_imgbtn_set_src(buttonBack, LV_BTN_STATE_PR, "F:/bmp_return.bin");
+  //lv_imgbtn_set_style(buttonBack, LV_BTN_STATE_PR, &tft_style_label_pre);
+  //lv_imgbtn_set_style(buttonBack, LV_BTN_STATE_REL, &tft_style_label_rel);
 
-//   lv_obj_set_pos(buttonBack,BTN_X_PIXEL*3+INTERVAL_V*4,  BTN_Y_PIXEL+INTERVAL_H+titleHeight);
-//   lv_btn_set_layout(buttonBack, LV_LAYOUT_OFF);
+  //lv_obj_set_pos(buttonBack,BTN_X_PIXEL*3+INTERVAL_V*4,  BTN_Y_PIXEL+INTERVAL_H+titleHeight);
+  //lv_btn_set_layout(buttonBack, LV_LAYOUT_OFF);
 
-//   label_Back = lv_label_create(buttonBack, NULL);
+  //label_Back = lv_label_create(buttonBack, NULL);
 
-//   if(gCfgItems.multiple_language !=0) {
-// 	lv_label_set_text(label_Back, common_menu.text_back);
-// 	lv_obj_align(label_Back, buttonBack, LV_ALIGN_IN_BOTTOM_MID,0, BUTTON_TEXT_Y_OFFSET);
-//   }
+  //if (gCfgItems.multiple_language !=0) {
+  //  lv_label_set_text(label_Back, common_menu.text_back);
+  //  lv_obj_align(label_Back, buttonBack, LV_ALIGN_IN_BOTTOM_MID,0, BUTTON_TEXT_Y_OFFSET);
+  //}
 
   buttonBack = lv_btn_create(scr, NULL);
   lv_obj_set_pos(buttonBack, TFT_WIDTH - 130, TFT_HEIGHT - 80);
@@ -124,138 +124,132 @@ void lv_draw_cloud_bind(void) {
   lv_obj_align(label_Back, buttonBack, LV_ALIGN_CENTER, 0, 0);
 
   #if BUTTONS_EXIST(EN1, EN2, ENC)
-	if (gCfgItems.encoder_enable == true) {
-      	lv_group_add_obj(g, buttonReleaseBind);
-		lv_group_add_obj(g, buttonBack);
-	}
-  #endif // BUTTONS_EXIST(EN1, EN2, ENC)
+    if (gCfgItems.encoder_enable) {
+      lv_group_add_obj(g, buttonReleaseBind);
+      lv_group_add_obj(g, buttonBack);
+    }
+  #endif
 
   //lv_task_handler();
 
-//   lv_fill_rect(0, titleHeight + 30, TFT_HEIGHT - (titleHeight + 30), TFT_HEIGHT - (titleHeight + 30), LV_COLOR_WHITE);
+  //lv_fill_rect(0, titleHeight + 30, TFT_HEIGHT - (titleHeight + 30), TFT_HEIGHT - (titleHeight + 30), LV_COLOR_WHITE);
 
-//   /*Create a 100x100 QR code*/
-//   qr = lv_qrcode_create(scr, 200, lv_color_hex3(0xffffff), lv_color_hex3(0x000000));
+  ///*Create a 100x100 QR code*/
+  //qr = lv_qrcode_create(scr, 200, lv_color_hex3(0xffffff), lv_color_hex3(0x000000));
 
-//   lv_obj_set_pos(qr, (TFT_HEIGHT - (titleHeight + 30)) / 2 - 100, (TFT_HEIGHT - (titleHeight + 30)) / 2 - 100 + titleHeight + 30);
+  //lv_obj_set_pos(qr, (TFT_HEIGHT - (titleHeight + 30)) / 2 - 100, (TFT_HEIGHT - (titleHeight + 30)) / 2 - 100 + titleHeight + 30);
 
-//   /*Set data*/
-//   lv_qrcode_update(qr, (char *)cloud_para.id, strlen((char *)cloud_para.id));
+  ///*Set data*/
+  //lv_qrcode_update(qr, (char *)cloud_para.id, strlen((char *)cloud_para.id));
 
-//   if(id_mark == 0) id_mark = 1;
+  //if (!id_mark) id_mark = 1;
 
   //display_qrcode((uint8_t *)cloud_para.id);
 
   text_id = lv_label_create_empty(scr);
-  lv_obj_set_pos(text_id, 50, 60 + 200 +20);
+  lv_obj_set_pos(text_id, 50, 60 + 200 + 20);
   lv_obj_set_style(text_id, &tft_style_label_rel);
   lv_label_set_text(text_id, (char *)cloud_para.id);
 
   id_mark = 0;
 
-  disp_bind_state(); 
+  disp_bind_state();
 }
 
 void disp_bind_state() {
-	if(cloud_para.state != 0x12) {
-		unbinding_flag = 0;
-	}
-	if(unbinding_flag) {
-    	lv_label_set_text(label_bind_or_not, cloud_menu.unbinding);
-    	lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
-		lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_REL, &style_para_value);
-    	lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_PR, &style_para_value);
-	}
-	else {
-		if(cloud_para.state == 0x10) {
-      		lv_label_set_text(label_bind_or_not, cloud_menu.disconnected);
-      		lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
-		}
-		else if(cloud_para.state == 0x11) {
-      		lv_label_set_text(label_bind_or_not, cloud_menu.unbinded);
-      		lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
-		}
-		else if(cloud_para.state == 0x12) {
-      		lv_label_set_text(label_bind_or_not, cloud_menu.binded);
-      		lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
-		}
-		else {
-      		lv_label_set_text(label_bind_or_not, cloud_menu.disable);
-      		lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
-		}
-	}
+  if (cloud_para.state != 0x12)
+    unbinding_flag = 0;
 
-	if((cloud_para.state == 0x12) && (unbinding_flag == 0)) {
-    	lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_REL, &style_para_back);
-    	lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_PR, &style_para_back);	
-	}
-	else {
-		lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_REL, &style_para_value);
-    	lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_PR, &style_para_value);
-	}
+  if (unbinding_flag) {
+    lv_label_set_text(label_bind_or_not, cloud_menu.unbinding);
+    lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
+    lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_REL, &style_para_value);
+    lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_PR, &style_para_value);
+  }
+  else {
+    if (cloud_para.state == 0x10) {
+      lv_label_set_text(label_bind_or_not, cloud_menu.disconnected);
+      lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
+    }
+    else if (cloud_para.state == 0x11) {
+      lv_label_set_text(label_bind_or_not, cloud_menu.unbinded);
+      lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
+    }
+    else if (cloud_para.state == 0x12) {
+      lv_label_set_text(label_bind_or_not, cloud_menu.binded);
+      lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
+    }
+    else {
+      lv_label_set_text(label_bind_or_not, cloud_menu.disable);
+      lv_obj_align(label_bind_or_not, button_bind_or_not, LV_ALIGN_CENTER, 0, 0);
+    }
+  }
+
+  if (cloud_para.state == 0x12 && !unbinding_flag) {
+    lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_REL, &style_para_back);
+    lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_PR, &style_para_back);
+  }
+  else {
+    lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_REL, &style_para_value);
+    lv_btn_set_style(buttonReleaseBind, LV_BTN_STYLE_PR, &style_para_value);
+  }
 }
 
 static char last_cloud_state = 0;
 void refresh_bind_ui() {
-	if((last_cloud_state != cloud_para.state) || unbinding_flag) {
-		disp_bind_state();
-		last_cloud_state = cloud_para.state;
-	}
-	if(cloud_para.id[0] != 0) {		
-		if(id_mark == 0) {
-			// lv_qrcode_update(qr, (char *)cloud_para.id, strlen((char *)cloud_para.id));
-			display_qrcode((uint8_t *)cloud_para.id);
-			lv_label_set_text(text_id, (char *)cloud_para.id);
-		}
-	}
-	else
-		id_mark = 0;
+  if ((last_cloud_state != cloud_para.state) || unbinding_flag) {
+    disp_bind_state();
+    last_cloud_state = cloud_para.state;
+  }
+  if (cloud_para.id[0]) {
+    if (!id_mark) {
+      //lv_qrcode_update(qr, (char *)cloud_para.id, strlen((char *)cloud_para.id));
+      display_qrcode((uint8_t *)cloud_para.id);
+      lv_label_set_text(text_id, (char *)cloud_para.id);
+    }
+  }
+  else
+    id_mark = 0;
 }
 
-void display_qrcode(uint8_t *qrcode_data)
-{
-	uint8_t i,j;
-	uint16_t x,y,p;
+void display_qrcode(uint8_t *qrcode_data) {
+  uint8_t i, j;
+  uint16_t x, y, p;
 
-	if(id_mark ==0) {
-		EncodeData((char *)qrcode_data);
-		id_mark = 1;
-	}
-	lv_fill_rect(10, QRCODE_Y, 300, QRCODE_Y + 300, LV_COLOR_WHITE);
+  if (!id_mark) {
+    EncodeData((char *)qrcode_data);
+    id_mark = 1;
+  }
 
-	if(m_nSymbleSize*2 > QRCODE_WIDTH) {
-		return;
-	}
-	
-	for(i=0;i<40;i++) {
-		if((m_nSymbleSize*i*2) > QRCODE_WIDTH)	break;
-	}
-	
-	p = (i - 1) * 2;
-	
-	x = QRCODE_X + 70;
-	y = QRCODE_Y + 70;
-	
-	for(i = 0; i < m_nSymbleSize; i++) {
-		for(j = 0; j < m_nSymbleSize; j++) {
-			if(m_byModuleData[i][j]==1)
-				lv_fill_rect(x + p * i, y + p * j, x + p * (i + 1) - 1, y + p * (j + 1) - 1, LV_COLOR_BACKGROUND);
-		}	
-	}
+  lv_fill_rect(10, QRCODE_Y, 300, QRCODE_Y + 300, LV_COLOR_WHITE);
+
+  if (m_nSymbleSize * 2 > QRCODE_WIDTH) return;
+
+  for (i = 0; i < 40; i++)
+    if ((m_nSymbleSize * i * 2) > QRCODE_WIDTH) break;
+
+  p = (i - 1) * 2;
+
+  x = QRCODE_X + 70;
+  y = QRCODE_Y + 70;
+
+  for (i = 0; i < m_nSymbleSize; i++)
+    for (j = 0; j < m_nSymbleSize; j++)
+      if (m_byModuleData[i][j] == 1)
+        lv_fill_rect(x + p * i, y + p * j, x + p * (i + 1) - 1, y + p * (j + 1) - 1, LV_COLOR_BACKGROUND);
 }
 
 void cloud_unbind() {
-	package_to_wifi(WIFI_CLOUD_UNBIND, (char *)0, 0);
-	unbinding_flag = 1;
+  package_to_wifi(WIFI_CLOUD_UNBIND, (char *)0, 0);
+  unbinding_flag = 1;
 }
 
 void lv_clear_cloud_bind() {
-	#if BUTTONS_EXIST(EN1, EN2, ENC)
-		if (gCfgItems.encoder_enable == true) {
-			lv_group_remove_all_objs(g);
-		}
-  	#endif // BUTTONS_EXIST(EN1, EN2, ENC)
-	lv_obj_del(scr); 
+  #if BUTTONS_EXIST(EN1, EN2, ENC)
+    if (gCfgItems.encoder_enable)
+      lv_group_remove_all_objs(g);
+  #endif
+  lv_obj_del(scr);
 }
 
 #endif // HAS_TFT_LVGL_UI
