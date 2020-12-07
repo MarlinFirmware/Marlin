f8721117df7c2877b5e09f7efb12b163854a7cce
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-12-07 05:53:15 -0600

    Fix auto#.g file handling, add NO_SD_AUTOSTART (#20071)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1197,6 +1197,7 @@
 
   #define SD_MENU_CONFIRM_START             // Confirm the selected SD file before printing
 
+  //#define NO_SD_AUTOSTART                 // Remove auto#.g file support completely to save some Flash, SRAM
   //#define MENU_ADDAUTOSTART               // Add a menu option to run auto#.g files
 
   //#define BROWSE_MEDIA_ON_INSERT          // Open the file browser when media is inserted
