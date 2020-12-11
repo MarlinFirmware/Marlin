86b3f34eeb6b1f1814d6432945e19fd5d9103bd9
qwewer0 <57561110+qwewer0@users.noreply.github.com>
2020-12-11 10:00:38 +0100

    Update menu_tramming.cpp

--- Marlin/src/lcd/menu/menu_tramming.cpp
+++ Marlin/src/lcd/menu/menu_tramming.cpp
@@ -43,10 +43,8 @@ float z_measured[G35_PROBE_COUNT] = { 0 };
 static uint8_t tram_index = 0;
 
 bool probe_single_point() {
-  // In BLTOUCH HS mode, the probe travels in a deployed state.
-  // Users of Tramming Wizard might have a badly misaligned bed, so raise Z by the
-  // length of the deployed pin (BLTOUCH stroke < 7mm)
-  do_blocking_move_to_z((Z_CLEARANCE_BETWEEN_PROBES) + TERN0(BLTOUCH_HS_MODE, 7));
+  do_blocking_move_to_z(Z_CLEARANCE_BETWEEN_PROBES);
+  //Stow after each point with BLTouch "HIGH SPEED" mode for push-pin safety
   const float z_probed_height = probe.probe_at_point(screws_tilt_adjust_pos[tram_index], TERN(BLTOUCH_HS_MODE, PROBE_PT_STOW, PROBE_PT_RAISE), 0, true);
   DEBUG_ECHOLNPAIR("probe_single_point: ", z_probed_height, "mm");
   z_measured[tram_index] = z_probed_height;
