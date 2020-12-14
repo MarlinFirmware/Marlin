c64b56ead690567f39561dc613f0f82a2fcd1330
Vi B-P <violetbp@eecs.umich.edu>
2020-12-14 16:32:14 -0500

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3443,10 +3443,9 @@
   #define JOY_X_LIMITS { 5600, 8190-100, 8190+100, 10800 } // min, deadzone start, deadzone end, max
   #define JOY_Y_LIMITS { 5600, 8250-100, 8250+100, 11000 }
   #define JOY_Z_LIMITS { 4800, 8080-100, 8080+100, 11550 }
-  //#define JOYSTICK_DEBUG
+  //#define JOYSTICK_DEBUG 
   
-  //ensure joystick axis are homed
-  #define JOY_NO_MOVE_BEFORE_HOME
+  #define JOY_NO_MOVE_BEFORE_HOME  //ensure joystick axis are homed
 #endif
 
 /**
