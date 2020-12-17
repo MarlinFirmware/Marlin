c3a1cd10abc8ec0336f52abf2822f2707b92f3a8
Jason Smith <jason.inet@gmail.com>
2020-12-17 04:18:07 -0800

    Balance CI tests (#20485)

--- Marlin/src/pins/ramps/pins_FYSETC_F6_13.h
+++ Marlin/src/pins/ramps/pins_FYSETC_F6_13.h
@@ -226,7 +226,7 @@
   #define DOGLCD_SCK                          17
   #define DOGLCD_A0                  LCD_PINS_DC
 
-  #define KILL_PIN                            -1  // NC
+  #undef KILL_PIN
   #define NEOPIXEL_PIN                        27
 
 #else
@@ -243,7 +243,6 @@
       #define LCD_BACKLIGHT_PIN               27
     #endif
 
-    #define KILL_PIN                          41
     #define LCD_RESET_PIN                     23  // Must be high or open for LCD to operate normally.
                                   // Seems to work best if left open.
 
