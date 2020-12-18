255644e7a82d540f5b19a1681ba65774415f745e
Victor Oliveira <rhapsodyv@gmail.com>
2020-12-18 19:18:04 -0300

    MKS Robin Nano V3 and STM32F4x0Vx Variant (#20430)

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -592,6 +592,8 @@
   #include "stm32f4/pins_MKS_ROBIN2.h"          // STM32F4                                env:MKS_ROBIN2
 #elif MB(MKS_ROBIN_PRO_V2)
   #include "stm32f4/pins_MKS_ROBIN_PRO_V2.h"    // STM32F4                                env:mks_robin_pro2
+#elif MB(MKS_ROBIN_NANO_V3)
+  #include "stm32f4/pins_MKS_ROBIN_NANO_V3.h"   // STM32F4                                env:mks_robin_nano_v3
 
 //
 // ARM Cortex M7
