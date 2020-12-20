2db71c338224cb2e043023223129e378af852c4b
Jason Smith <jason.inet@gmail.com>
2020-12-20 08:20:03 -0800

    Remove more overrides

--- Marlin/src/pins/stm32f4/pins_LERDGE_S.h
+++ Marlin/src/pins/stm32f4/pins_LERDGE_S.h
@@ -27,9 +27,6 @@
 #define BOARD_INFO_NAME      "Lerdge S"
 #define DEFAULT_MACHINE_NAME "LERDGE"
 
-#define STEP_TIMER                             4
-#define TEMP_TIMER                             2
-
 //#define I2C_EEPROM
 
 //
