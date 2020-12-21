023ef753b9c6cb04ecad32436ddf93b1cc14fc71
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-12-21 17:36:15 -0600

    Clean up some pins

--- Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_common.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_common.h
@@ -41,25 +41,25 @@
 //
 // Servos
 //
-#define SERVO0_PIN                          PA1   // "SERVOS"
+#define SERVO0_PIN                          PA1   // SERVOS
 
 //
 // Limit Switches
 //
-#define X_STOP_PIN                          PC0   // "X-STOP"
-#define Y_STOP_PIN                          PC1   // "Y-STOP"
-#define Z_STOP_PIN                          PC2   // "Z-STOP"
+#define X_STOP_PIN                          PC0   // X-STOP
+#define Y_STOP_PIN                          PC1   // Y-STOP
+#define Z_STOP_PIN                          PC2   // Z-STOP
 
 //
 // Z Probe must be this pin
 //
-#define Z_MIN_PROBE_PIN                     PC14  // "PROBE"
+#define Z_MIN_PROBE_PIN                     PC14  // PROBE
 
 //
 // Filament Runout Sensor
 //
 #ifndef FIL_RUNOUT_PIN
-  #define FIL_RUNOUT_PIN                    PC15  // "E0-STOP"
+  #define FIL_RUNOUT_PIN                    PC15  // E0-STOP
 #endif
 
 //
