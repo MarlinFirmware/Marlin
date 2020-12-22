cfad5cb4356f90179eef636ec6c27db242227fff
Victor Oliveira <rhapsodyv@gmail.com>
2020-12-22 09:57:11 -0300

    Unify FYSETC F6 1.3 / 1.4 (#20507)
    Co-authored-by: Scott Lahteine <github@thinkyhead.com>

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -163,9 +163,9 @@
 #elif MB(DAGOMA_F5)
   #include "ramps/pins_DAGOMA_F5.h"             // ATmega2560                             env:mega2560
 #elif MB(FYSETC_F6_13)
-  #include "ramps/pins_FYSETC_F6_13.h"          // ATmega2560                             env:FYSETC_F6_13
+  #include "ramps/pins_FYSETC_F6_13.h"          // ATmega2560                             env:FYSETC_F6
 #elif MB(FYSETC_F6_14)
-  #include "ramps/pins_FYSETC_F6_14.h"          // ATmega2560                             env:FYSETC_F6_14
+  #include "ramps/pins_FYSETC_F6_14.h"          // ATmega2560                             env:FYSETC_F6
 #elif MB(DUPLICATOR_I3_PLUS)
   #include "ramps/pins_DUPLICATOR_I3_PLUS.h"    // ATmega2560                             env:mega2560
 #elif MB(VORON)
