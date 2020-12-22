6f11d481b91f8347f684f6efd6231a0c834086fa
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2020-12-22 04:51:29 -0800

    Anet ET4 / ET4P and Anet TFT28 / TFT35 (#20280)

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -370,6 +370,8 @@
 #define BOARD_MKS_ROBIN2              4218  // MKS_ROBIN2 (STM32F407ZE)
 #define BOARD_MKS_ROBIN_PRO_V2        4219  // MKS Robin Pro V2 (STM32F407VE)
 #define BOARD_MKS_ROBIN_NANO_V3       4220  // MKS Robin Nano V3 (STM32F407VG)
+#define BOARD_ANET_ET4                4221  // ANET ET4 V1.x (STM32F407VGT6)
+#define BOARD_ANET_ET4P               4222  // ANET ET4P V1.x (STM32F407VGT6)
 
 //
 // ARM Cortex M7
