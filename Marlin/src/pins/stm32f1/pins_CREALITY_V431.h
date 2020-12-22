08dcd1f680423139b19d4a6f0bb9438a9aeb5fe2
Sean McGroty <sean@darkon.net>
2020-12-22 07:25:12 -0500

    Creality v4.3.1 (Ender 6) board (#20512)

--- Marlin/src/pins/stm32f1/pins_CREALITY_V431.h
+++ Marlin/src/pins/stm32f1/pins_CREALITY_V431.h
@@ -21,24 +21,19 @@
  */
 
 /**
- * CREALITY v4.2.7 (STM32F103) board pin assignments
+ * CREALITY v4.3.1 (STM32F103) board pin assignments
  */
 
-#define BOARD_INFO_NAME      "Creality v4.2.7"
+#define BOARD_INFO_NAME      "Creality v4.3.1"
 #define DEFAULT_MACHINE_NAME "Creality3D"
 
 //
 // Steppers
 //
-#define X_STEP_PIN                          PB9
-#define X_DIR_PIN                           PC2
-#define Y_STEP_PIN                          PB7
+#define X_STEP_PIN                        PB8
+#define X_DIR_PIN                         PB7
 
-#define Y_DIR_PIN                           PB8
-#define Z_STEP_PIN                          PB5
-#define Z_DIR_PIN                           PB6
-
-#define E0_STEP_PIN                         PB3
-#define E0_DIR_PIN                          PB4
+#define Y_STEP_PIN                        PC2
+#define Y_DIR_PIN                         PB9
 
 #include "pins_CREALITY_V4.h"
