203daea8054009d3c0c2efe9da7fc9318fa577fd
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-12-23 20:32:39 +0100

    Enable custom version file

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "(Sebastiaan Dammann, CR-6 SE with stock motherboard)" // Who made the changes.
-//#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
+#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
  * *** VENDORS PLEASE READ ***
