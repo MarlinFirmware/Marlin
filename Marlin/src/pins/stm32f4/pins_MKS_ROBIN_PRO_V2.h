91dcf1ad342d30972ff92c2dc717d9b0032ed38f
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2020-12-23 17:24:34 -0300

    duplicated

--- Marlin/src/pins/stm32f4/pins_MKS_ROBIN_PRO_V2.h
+++ Marlin/src/pins/stm32f4/pins_MKS_ROBIN_PRO_V2.h
@@ -361,24 +361,4 @@
     #endif
 
   #endif // !MKS_MINI_12864
-
-#elif ENABLED(SPI_GRAPHICAL_TFT)
-  #define SPI_TFT_CS_PIN                    PD11
-  #define SPI_TFT_SCK_PIN                   PA5
-  #define SPI_TFT_MISO_PIN                  PA6
-  #define SPI_TFT_MOSI_PIN                  PA7
-  #define SPI_TFT_DC_PIN                    PD10
-  #define SPI_TFT_RST_PIN                   PC6
-
-  #define LCD_BACKLIGHT_PIN                 PD13
-
-  #define TOUCH_CS_PIN                      PE14  // SPI1_NSS
-  #define TOUCH_SCK_PIN                     PA5   // SPI1_SCK
-  #define TOUCH_MISO_PIN                    PA6   // SPI1_MISO
-  #define TOUCH_MOSI_PIN                    PA7   // SPI1_MOSI
-
-  #define BTN_EN1                           PE8
-  #define BTN_EN2                           PE11
-  #define BEEPER_PIN                        PC5
-  #define BTN_ENC                           PE13
 #endif // HAS_SPI_LCD
