7afd27c2c841935425e3527b7b9efc999f8dbde9
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-12-23 21:44:58 +0100

    Improve filament change message

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -268,7 +268,7 @@ namespace Language_en {
   PROGMEM Language_Str MSG_BED_Z                           = _UxGT("Bed Z");
   PROGMEM Language_Str MSG_NOZZLE                          = _UxGT("Nozzle");
   PROGMEM Language_Str MSG_NOZZLE_N                        = _UxGT("Nozzle ~");
-  PROGMEM Language_Str MSG_NOZZLE_PARKED                   = _UxGT("Nozzle Parked");
+  PROGMEM Language_Str MSG_NOZZLE_PARKED                   = _UxGT("Load/unload filament");
   PROGMEM Language_Str MSG_NOZZLE_STANDBY                  = _UxGT("Nozzle Standby");
   PROGMEM Language_Str MSG_BED                             = _UxGT("Bed");
   PROGMEM Language_Str MSG_CHAMBER                         = _UxGT("Enclosure");
