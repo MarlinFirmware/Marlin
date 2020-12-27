f423edd938dcb593b58844cd7dc3b9164d792686
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-12-27 16:21:01 -0600

    Add probe_switch_activated

--- Marlin/src/module/probe.cpp
+++ Marlin/src/module/probe.cpp
@@ -519,7 +519,7 @@ bool Probe::probe_down_to_z(const float z, const feedRate_t fr_mm_s) {
    */
   bool Probe::tare() {
     #if BOTH(PROBE_ACTIVATION_SWITCH, PROBE_TARE_ONLY_WHILE_INACTIVE)
-      if (READ(PROBE_ACTIVATION_SWITCH_PIN) == PROBE_ACTIVATION_SWITCH_STATE) {
+      if (endstops.probe_switch_activated()) {
         SERIAL_ECHOLNPGM("Cannot tare an active probe");
         return true;
       }
