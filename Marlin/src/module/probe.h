f6ef20f3647ba1fd2971dca3ac8847e9811cc784
Scott Lahteine <github@thinkyhead.com>
2020-12-26 19:55:36 -0600

    tern limits

--- Marlin/src/module/probe.h
+++ Marlin/src/module/probe.h
@@ -183,7 +183,13 @@ public:
 
     // constexpr helpers used in build-time static_asserts, relying on default probe offsets.
     class build_time {
-      static constexpr xyz_pos_t default_probe_xyz_offset = TERN(HAS_BED_PROBE, NOZZLE_TO_PROBE_OFFSET, {0});
+      static constexpr xyz_pos_t default_probe_xyz_offset =
+        #if HAS_BED_PROBE
+          NOZZLE_TO_PROBE_OFFSET
+        #else
+          { 0 }
+        #endif
+      ;
       static constexpr xy_pos_t default_probe_xy_offset = { default_probe_xyz_offset.x,  default_probe_xyz_offset.y };
 
     public:
