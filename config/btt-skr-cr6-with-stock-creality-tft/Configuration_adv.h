ef18b4a33b36547ce5baf85d08a49cdfd6101a5f
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-12-28 22:12:36 +0100

    Update config examples

--- config/btt-skr-cr6-with-stock-creality-tft/Configuration_adv.h
+++ config/btt-skr-cr6-with-stock-creality-tft/Configuration_adv.h
@@ -375,8 +375,8 @@
 #if ENABLED(HOTEND_IDLE_TIMEOUT)
   #define HOTEND_IDLE_TIMEOUT_SEC (5*60)    // (seconds) Time without extruder movement to trigger protection
   #define HOTEND_IDLE_MIN_TRIGGER   180     // (°C) Minimum temperature to enable hotend protection
-  #define HOTEND_IDLE_NOZZLE_TARGET   0     // (°C) Safe temperature for the nozzle after timeout
-  #define HOTEND_IDLE_BED_TARGET      0     // (°C) Safe temperature for the bed after timeout
+  #define HOTEND_IDLE_NOZZLE_TARGET 120     // (°C) Safe temperature for the nozzle after timeout
+  #define HOTEND_IDLE_BED_TARGET    50     // (°C) Safe temperature for the bed after timeout
 #endif
 
 // @section temperature
