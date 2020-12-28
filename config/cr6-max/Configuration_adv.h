abbe090220045ff67f2ccfa612a8246003293554
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-12-28 17:14:28 +0100

    Repair LCD serial port

--- config/cr6-max/Configuration_adv.h
+++ config/cr6-max/Configuration_adv.h
@@ -1463,7 +1463,7 @@
 // Additional options for DGUS / DWIN displays
 //
 #if HAS_DGUS_LCD
-  #define LCD_SERIAL_PORT 2
+  #define LCD_SERIAL_PORT 3
   #define LCD_BAUDRATE 115200
 
   #define DGUS_RX_BUFFER_SIZE 128
