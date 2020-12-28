44342c9a8e01feb48db772ea39f3ea78880bb71c
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-12-28 06:40:46 -0800

    Small / Large Boot Screen option for TFT_COLOR_UI follow-up

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1140,7 +1140,7 @@
 
   #if ENABLED(SHOW_BOOTSCREEN)
     #define BOOTSCREEN_TIMEOUT 4000      // (ms) Total Duration to display the boot screen(s)
-    #if EITHER(HAS_MARLINUI_U8GLIB, TFT_COLOR_UI)
+    #if ANY(HAS_MARLINUI_U8GLIB, TFT_SCALED_DOGLCD, TFT_COLOR_UI)
       #define BOOT_MARLIN_LOGO_SMALL     // Show a smaller Marlin logo on the Boot Screen (saving lots of flash)
     #endif
   #endif
