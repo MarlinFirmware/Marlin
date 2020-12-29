efea4548364280529e49b2151dcdbd4c52d6d1fd
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-12-29 20:51:33 +0100

    Select different offset

--- Marlin/src/lcd/extui/lib/dgus_creality/DGUSScreenHandler.cpp
+++ Marlin/src/lcd/extui/lib/dgus_creality/DGUSScreenHandler.cpp
@@ -69,9 +69,10 @@ float DGUSScreenHandler::feed_amount = 100;
 constexpr uint8_t DGUS_GRID_VISUALIZATION_START_ID = GRID_MAX_POINTS > (4*4) ? 30 : 1;
 
 static_assert(
-  (GRID_MAX_POINTS == 16 && DGUS_GRID_VISUALIZATION_START_ID ==1)||  // CR-6 SE
-  (GRID_MAX_POINTS == 49 && DGUS_GRID_VISUALIZATION_START_ID ==30) || // CR-6 MAX
-  (GRID_MAX_POINTS != 16 && GRID_MAX_POINTS != 49)                    // Custom Leveling
+  (GRID_MAX_POINTS == 16 && DGUS_GRID_VISUALIZATION_START_ID == 1)||  // CR-6 SE
+  (GRID_MAX_POINTS == 49 && DGUS_GRID_VISUALIZATION_START_ID == 30) || // CR-6 MAX
+  (GRID_MAX_POINTS != 16 && GRID_MAX_POINTS != 49),                    // Custom Leveling
+  "Incorrect offset selected for leveling config"
 );
 
 // endianness swap
