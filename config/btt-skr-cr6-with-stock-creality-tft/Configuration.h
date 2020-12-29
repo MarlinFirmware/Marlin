8e1177b7b012feab435ab61f36601327955f0252
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-12-29 22:00:21 +0100

    Final example config update

--- config/btt-skr-cr6-with-stock-creality-tft/Configuration.h
+++ config/btt-skr-cr6-with-stock-creality-tft/Configuration.h
@@ -1088,6 +1088,7 @@
  */
 #define PROBING_HEATERS_OFF       // Turn heaters off when probing
 #if ENABLED(PROBING_HEATERS_OFF)
+  #define PROBING_KEEP_BED_HEATER
   //#define WAIT_FOR_BED_HEATER     // Wait for bed to heat back up between probes (to improve accuracy)
 #endif
 #define PROBING_FANS_OFF          // Turn fans off when probing
