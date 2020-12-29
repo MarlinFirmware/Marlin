0c268e4c2ef51f02fd14c062fb185d41fc3e326a
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-12-29 13:16:12 +0100

    Set HYBRID_THRESHOLD by default to high values so it configurable but not enabled by default

--- config/cr6-max/Configuration_adv.h
+++ config/cr6-max/Configuration_adv.h
@@ -2568,15 +2568,15 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
+  #define X_HYBRID_THRESHOLD     5000  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
+  #define Y_HYBRID_THRESHOLD     5000
   #define Y2_HYBRID_THRESHOLD    100
   #define Z_HYBRID_THRESHOLD       3
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
-  #define E0_HYBRID_THRESHOLD     30
+  #define E0_HYBRID_THRESHOLD     7000
   #define E1_HYBRID_THRESHOLD     30
   #define E2_HYBRID_THRESHOLD     30
   #define E3_HYBRID_THRESHOLD     30
