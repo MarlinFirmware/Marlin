ce15cfc0a7b2926454975c8a80f6edf83cded587
jbuck2005 <59450931+jbuck2005@users.noreply.github.com>
2020-12-30 12:59:12 -0500

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2020-12-28 11:00" // 20201228 ---------------------------------------------------------------------------------------------------------------
+#define STRING_DISTRIBUTION_DATE "2020-12-30 13:00" // 20201228 ---------------------------------------------------------------------------------------------------------------
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
