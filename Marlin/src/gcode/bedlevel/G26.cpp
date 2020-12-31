be819a7c6318bd91fbd40cc696c89963c7a2c17f
ScrewThisBanana <dominik.steffan@gmx.de>
2020-12-31 17:47:30 +0100

    Adding custom move feedrate for G26
    This commit adds an additional configuration parameter that can be used to specify the movement speed during the G26 validation pattern command during moves without extrusion.
    
    Closes MarlinFirmware/Marlin#20615

--- Marlin/src/gcode/bedlevel/G26.cpp
+++ Marlin/src/gcode/bedlevel/G26.cpp
@@ -213,7 +213,8 @@ void move_to(const float &rx, const float &ry, const float &z, const float &e_de
 
   const xy_pos_t dest = { rx, ry };
 
-  const bool has_xy_component = dest != current_position; // Check if X or Y is involved in the movement.
+  const bool has_xy_component = dest != current_position; // Check if X or Y is involved in the movement.  
+  const bool has_e_component = e_delta != 0.0;
 
   destination = current_position;
 
@@ -227,7 +228,10 @@ void move_to(const float &rx, const float &ry, const float &z, const float &e_de
   // If X or Y is involved do a 'normal' move. Otherwise retract/recover/hop.
   destination = dest;
   destination.e += e_delta;
-  const feedRate_t feed_value = has_xy_component ? feedRate_t(G26_XY_FEEDRATE) : planner.settings.max_feedrate_mm_s[E_AXIS] * 0.666f;
+  const feedRate_t feed_value = 
+    has_xy_component 
+    ? (has_e_component ? feedRate_t(G26_XY_FEEDRATE) : feedRate_t(G26_XY_FEEDRATE_MOVE)) 
+    : planner.settings.max_feedrate_mm_s[E_AXIS] * 0.666f;
   prepare_internal_move_to_destination(feed_value);
   destination = current_position;
 }
