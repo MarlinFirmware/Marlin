34b2f678e9062be9344fe4ff5a0fcefbd4ef31ad
Koarl <sb0nschbob@web.de>
2021-01-01 21:55:18 +0100

    always allow z homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1486,7 +1486,7 @@
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing.
 // - Prevent Z homing when the Z probe is outside bed area.
 //
-#define Z_SAFE_HOMING
+//#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
