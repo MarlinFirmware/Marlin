05e66a15791bccb5165073493807076dbd336424
James Buck <jbuck2005@hotmail.com>
2021-01-02 10:01:36 -0500

    version increment and moveit.bat update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Replicator v0.4a" // 20210102 does not appear to work if defined in Version.h ------------------------------------------------------------------------
+#define CUSTOM_MACHINE_NAME "Replicator v0.4b" // 20210102 does not appear to work if defined in Version.h ------------------------------------------------------------------------
 
 // Printer's unique ID, used by some programs to dffferentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
--- moveit.bat
+++ moveit.bat
@@ -1,5 +1,6 @@
 del C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.elf
 move C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin C:\Users\james\Documents\3d\firmware\james
+del D:\*.bin
 copy C:\Users\james\Documents\3d\firmware\james\*.bin D:\
-move C:\Users\james\Documents\3d\firmware\james\*.bin C:\Users\james\Documents\3d\firmware\james\firmware archive
+move C:\Users\james\Documents\3d\firmware\james\*.bin "C:\Users\james\Documents\3d\firmware\james\firmware archive"
 fsutil volume dismount D:\
