dfee7cc72fa5bf4f5a2bd79cf308dfc11d990c8f
jbuck2005 <59450931+jbuck2005@users.noreply.github.com>
2021-01-02 09:39:46 -0500

    Update moveit.bat
    added auto dismount

--- moveit.bat
+++ moveit.bat
@@ -2,3 +2,4 @@ del C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_c
 move C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin C:\Users\james\Documents\3d\firmware\james
 copy C:\Users\james\Documents\3d\firmware\james\*.bin D:\
 move C:\Users\james\Documents\3d\firmware\james\*.bin C:\Users\james\Documents\3d\firmware\james\firmware archive
+fsutil volume dismount D:\
