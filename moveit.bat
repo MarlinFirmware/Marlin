ce2ca493f337d70959ada337cad03d669ee0bf27
jbuck2005 <59450931+jbuck2005@users.noreply.github.com>
2021-01-02 08:41:46 -0500

    Update moveit.bat

--- moveit.bat
+++ moveit.bat
@@ -1,2 +1,4 @@
+move C:\Users\james\Documents\3d\firmware\james\*.bin C:\Users\james\Documents\3d\firmware\james\firmware archive
 move C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin C:\Users\james\Documents\3d\firmware\james
 copy C:\Users\james\Documents\3d\firmware\james\*.bin D:\
+del C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.elf
