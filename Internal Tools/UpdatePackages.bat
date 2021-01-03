23baae4ea5868ce48d2c860c751f31dd7c5410e2
Tim Hoogland <tim@th3dstudio.com>
2021-01-03 16:37:32 -0600

    Update UpdatePackages.bat

--- Internal Tools/UpdatePackages.bat
+++ Internal Tools/UpdatePackages.bat
@@ -1,78 +1,78 @@
 @echo off
-"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X-UFW_LATEST.zip"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X.zip"
 ping localhost -n 10 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\LCD Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\LCD Firmware"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\CrealityV42X\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_CrealityV42X.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\CrealityV42X\Firmware"
 
-"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_LATEST.zip"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini.zip"
 ping localhost -n 10 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\SKR_E3_Mini\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_SKR_E3_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\SKR_E3_Mini\Firmware"
 
-"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_LATEST.zip"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite.zip"
 ping localhost -n 10 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\EZBoard Installation Guide"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\EZBoard Installation Guide"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Pre-Compiled Binaries\EZBoard Pre-Compiled Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Pre-Compiled Binaries\EZBoard Pre-Compiled Firmware"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\TH3D_EZBoardLite\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_TH3D_EZBoardLite.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\TH3D_EZBoardLite\Firmware"
 
-"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini-UFW_LATEST.zip"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini.zip"
 ping localhost -n 10 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\MKS_Robin_Mini\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_MKS_Robin_Mini.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\MKS_Robin_Mini\Firmware"
 
-"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards-UFW_LATEST.zip"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards.zip"
 ping localhost -n 10 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\Melzi_Boards\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Melzi_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\Melzi_Boards\Firmware"
 
-"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards-UFW_LATEST.zip"
+"C:\Program Files\7-Zip\7z.exe" d -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards.zip"
 ping localhost -n 10 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Firmware"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\STL Files"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\TH3D Installation Guides"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\README.md"
 ping localhost -n 5 >NUL
-"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards-UFW_LATEST.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\Ramps_Boards\Firmware"
+"C:\Program Files\7-Zip\7z.exe" a -tZip "D:\Work\TH3D Studio LLC\TH3D Internal Site - Shared Folders\Firmware\Web Downloads\Unified2\TH3D_Unified2_Ramps_Boards.zip" "D:\Personal\Documents\GitHub\UnifiedFirmware\Board Configuration Files\Ramps_Boards\Firmware"
