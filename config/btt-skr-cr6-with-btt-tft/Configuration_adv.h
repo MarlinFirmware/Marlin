d1cca71b0fd80ad4336d1108c7c2d2650b264c38
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-01-03 22:22:29 +0100

    Tune hot-end idle timeout

--- config/btt-skr-cr6-with-btt-tft/Configuration_adv.h
+++ config/btt-skr-cr6-with-btt-tft/Configuration_adv.h
@@ -373,8 +373,8 @@
  */
 #define HOTEND_IDLE_TIMEOUT
 #if ENABLED(HOTEND_IDLE_TIMEOUT)
-  #define HOTEND_IDLE_TIMEOUT_SEC (5*60)    // (seconds) Time without extruder movement to trigger protection
-  #define HOTEND_IDLE_MIN_TRIGGER   180     // (°C) Minimum temperature to enable hotend protection
+  #define HOTEND_IDLE_TIMEOUT_SEC (15*60)    // (seconds) Time without extruder movement to trigger protection
+  #define HOTEND_IDLE_MIN_TRIGGER   150     // (°C) Minimum temperature to enable hotend protection
   #define HOTEND_IDLE_NOZZLE_TARGET 120     // (°C) Safe temperature for the nozzle after timeout
   #define HOTEND_IDLE_BED_TARGET    50     // (°C) Safe temperature for the bed after timeout
 #endif
