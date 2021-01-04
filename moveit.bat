8884b5a6e39e39df3c34a3b614a0fca8813d5feb
James Buck <jbuck2005@hotmail.com>
2021-01-04 11:59:47 -0500

    move built firmware to new directory

--- moveit.bat
+++ moveit.bat
@@ -1,5 +1,5 @@
 del C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.elf
-copy C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin C:\Users\james\Documents\GitHub\Marlin
+copy C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin "C:\Users\james\Documents\GitHub\Marlin\firmware archive"
 move C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin C:\Users\james\Documents\3d\firmware\james
 del D:\*.bin
 copy C:\Users\james\Documents\3d\firmware\james\*.bin D:\
