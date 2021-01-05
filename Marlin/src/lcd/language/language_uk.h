a562091d016947510de342e796584c43ddb938c1
Scott Lahteine <github@thinkyhead.com>
2021-01-04 18:17:30 -0600

    No PID needed

--- Marlin/src/lcd/language/language_uk.h
+++ Marlin/src/lcd/language/language_uk.h
@@ -356,17 +356,6 @@ namespace Language_uk {
   PROGMEM Language_Str MSG_PID_TEMP_TOO_HIGH               = _UxGT("Збій автопідбору. Температура завищена.");
   PROGMEM Language_Str MSG_PID_TIMEOUT                     = _UxGT("Збій автопідбору! Час вийшов.");
 
-  PROGMEM Language_Str MSG_PID_P                           = _UxGT("PID-P");
-  PROGMEM Language_Str MSG_PID_P_E                         = _UxGT("PID-P *");
-  PROGMEM Language_Str MSG_PID_I                           = _UxGT("PID-I");
-  PROGMEM Language_Str MSG_PID_I_E                         = _UxGT("PID-I *");
-  PROGMEM Language_Str MSG_PID_D                           = _UxGT("PID-D");
-  PROGMEM Language_Str MSG_PID_D_E                         = _UxGT("PID-D *");
-  PROGMEM Language_Str MSG_PID_C                           = _UxGT("PID-C");
-  PROGMEM Language_Str MSG_PID_C_E                         = _UxGT("PID-C *");
-  PROGMEM Language_Str MSG_PID_F                           = _UxGT("PID-F");
-  PROGMEM Language_Str MSG_PID_F_E                         = _UxGT("PID-F *");
-
   PROGMEM Language_Str MSG_SELECT                          = _UxGT("Вибрати");
   PROGMEM Language_Str MSG_SELECT_E                        = _UxGT("Вибрати *");
   PROGMEM Language_Str MSG_ACC                             = _UxGT("Прискорорення");
@@ -835,7 +824,6 @@ namespace Language_uk {
   PROGMEM Language_Str MSG_CALIBRATION_COMPLETED           = _UxGT("Калібрування успішне");
   PROGMEM Language_Str MSG_CALIBRATION_FAILED              = _UxGT("Збій калібрування");
 }
-
 
 #if FAN_COUNT == 1
   #define MSG_FIRST_FAN_SPEED       MSG_FAN_SPEED
