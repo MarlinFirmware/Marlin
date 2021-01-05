d10c7347300e08a0961f6cc79128585e0b19cda7
ellensp <ellensp@hotmail.com>
2021-01-06 01:21:08 +1300

    No BTN_ENC_EN on Anet 10 (#20684)

--- Marlin/src/pins/sanguino/pins_ANET_10.h
+++ Marlin/src/pins/sanguino/pins_ANET_10.h
@@ -198,14 +198,8 @@
 
   #endif
 
-  #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-    #define BTN_ENC_EN               LCD_PINS_D7  // Detect the presence of the encoder
-  #endif
-
 #else
-
   #define SERVO0_PIN                          27
-
 #endif
 
 #ifndef FIL_RUNOUT_PIN
