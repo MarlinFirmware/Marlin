a3be80fc1b85ed52d632203e6365a107ea860f67
Cilliers9 <tclouwrens@gmail.com>
2021-01-07 06:15:34 +0200

    Update board define

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -127,7 +127,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BIGTREE_SKR_V1_3
+  #define MOTHERBOARD BOARD_BTT_SKR_V1_3
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
