bec3b1540c95a43ad44ecd0e6a70b2d72da7cca6
Kevin Stampe <kevinstampe@gmail.com>
2021-01-08 18:31:18 +0100

    adjusted bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1160,11 +1160,11 @@
 #define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -25
-#define Y_MIN_POS -5
+#define X_MIN_POS -28
+#define Y_MIN_POS -18
 #define Z_MIN_POS 0
-#define X_MAX_POS 300
-#define Y_MAX_POS 297
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 350
 
 /**
