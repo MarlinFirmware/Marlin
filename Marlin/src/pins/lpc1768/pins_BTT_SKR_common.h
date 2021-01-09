6146ef5e7e4e1d8002fba541f5d10bdf51c34744
Cilliers9 <tclouwrens@gmail.com>
2021-01-09 14:24:05 +0200

    Update pins_BTT_SKR_common.h

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_common.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_common.h
@@ -50,10 +50,10 @@
 //  3.3V max when defined as an analog input
 //
 #ifndef TEMP_0_PIN
-  #define TEMP_0_PIN                    P0_25_A2  // A1 (T1) - (68) - TEMP_0_PIN
+  #define TEMP_0_PIN                    P0_24_A1  // A1 (T1) - (68) - TEMP_0_PIN
 #endif
 #ifndef TEMP_1_PIN
-  #define TEMP_1_PIN                    P0_24_A1  // A2 (T2) - (69) - TEMP_1_PIN
+  #define TEMP_1_PIN                    P0_25_A2  // A2 (T2) - (69) - TEMP_1_PIN
 #endif
 #ifndef TEMP_BED_PIN
   #define TEMP_BED_PIN                  P0_23_A0  // A0 (T0) - (67) - TEMP_BED_PIN
