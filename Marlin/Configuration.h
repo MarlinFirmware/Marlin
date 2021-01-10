b683b7b77f7144b03aa016cdfd93fe977c343fac
Kevin Stampe <kevinstampe@gmail.com>
2021-01-10 23:02:27 +0100

    reverted step calibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.5, 80, 413.3, 98 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -24, 0, -1.9 }
+#define NOZZLE_TO_PROBE_OFFSET { -24, 0, -1.7 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
