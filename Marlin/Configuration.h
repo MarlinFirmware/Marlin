43cbad9ca3b81d09996be5bcc6ca8e30a32ba5ec
Kevin Stampe <kevinstampe@gmail.com>
2021-01-10 14:28:58 +0100

    changed junction deviation

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -807,7 +807,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.013 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.10 // (mm) Distance from real junction edge
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
