ae1190f8eb88d49b21529e8069d34bbfda377e8f
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-01-11 01:38:46 -0600

    blerf

--- Marlin/src/lcd/extui/lib/mks_ui/draw_move_motor.cpp
+++ Marlin/src/lcd/extui/lib/mks_ui/draw_move_motor.cpp
@@ -53,33 +53,12 @@ enum {
 
 static void event_handler(lv_obj_t *obj, lv_event_t event) {
   if (event != LV_EVENT_RELEASED) return;
-  float dist = 0;
   if (queue.length <= (BUFSIZE - 3)) {
+    float dist = uiCfg.move_dist;
     switch (obj->mks_obj_id) {
-      case ID_M_X_P:
-        dist = uiCfg.move_dist;
-        cur_label = 'X';
-        break;
-      case ID_M_X_N:
-        dist = -uiCfg.move_dist;
-        cur_label = 'X';
-        break;
-      case ID_M_Y_P:
-        dist = uiCfg.move_dist;
-        cur_label = 'Y';
-        break;
-      case ID_M_Y_N:
-        dist = -uiCfg.move_dist;
-        cur_label = 'Y';
-        break;
-      case ID_M_Z_P:
-        dist = uiCfg.move_dist;
-        cur_label = 'Z';
-        break;
-      case ID_M_Z_N:
-        dist = -uiCfg.move_dist;
-        cur_label = 'Z';
-        break;
+      case ID_M_X_N: dist *= -1; case ID_M_X_P: cur_label = 'X'; break;
+      case ID_M_Y_N: dist *= -1; case ID_M_Y_P: cur_label = 'Y'; break;
+      case ID_M_Z_N: dist *= -1; case ID_M_Z_P: cur_label = 'Z'; break;
     }
     sprintf_P(public_buf_l, PSTR("G91\nG1 %c%3.1f F%d\nG90"), cur_label, dist, uiCfg.moveSpeed);
     queue.inject(public_buf_l);
