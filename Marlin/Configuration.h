a6e4a2ee6bcc32dda0f2b93ff12201ab3d3ce2c6
Chris Jones <cmsj@tenshu.net>
2021-01-12 12:28:46 +0000

    Enable bed temperature sensor (using the sensor supplied with the EasyThreeD heated bed upgrade

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -149,7 +149,7 @@ Here are some standard links for getting your machine calibrated:
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_BED 4
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
