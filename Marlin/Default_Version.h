50ea9c05309ff425ed981982c89029bca0444a4b
Chris Jones <cmsj@tenshu.net>
2021-01-12 20:07:49 +0000

    Update version strings

--- Marlin/Default_Version.h
+++ Marlin/Default_Version.h
@@ -6,8 +6,8 @@
 // #error "You must specify the following parameters related to your distribution"
 
 #if true
-#define SHORT_BUILD_VERSION "1.1.0-RC3-cmsj"
-#define DETAILED_BUILD_VERSION "1.1.0-RC3-cmsj Hotbed Mod"
+#define SHORT_BUILD_VERSION "1.1.0-RC3-cmsj-hotbed"
+#define DETAILED_BUILD_VERSION "1.1.0-RC3-cmsj-hotbed Official Hotbed Mod"
 #define STRING_DISTRIBUTION_DATE "2021-01-12 12:00"
 // It might also be appropriate to define a location where additional information can be found
 #define SOURCE_CODE_URL  "https://github.com/cmsj/Marlin/tree/1.1.0-RC3-EasyThreeD-X1-cmsj"
