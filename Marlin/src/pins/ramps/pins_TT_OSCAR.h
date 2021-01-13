4a897310252341f203371cb2e99f93bf9c035143
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-01-12 20:43:52 -0600

    Rotary encoder cleanup (#20753)

--- Marlin/src/pins/ramps/pins_TT_OSCAR.h
+++ Marlin/src/pins/ramps/pins_TT_OSCAR.h
@@ -342,10 +342,10 @@
     #if !IS_NEWPANEL
       // Buttons attached to a shift register
       // Not wired yet
-      //#define SHIFT_CLK                     38
-      //#define SHIFT_LD                      42
-      //#define SHIFT_OUT                     40
-      //#define SHIFT_EN                      17
+      //#define SHIFT_CLK_PIN                 38
+      //#define SHIFT_LD_PIN                  42
+      //#define SHIFT_OUT_PIN                 40
+      //#define SHIFT_EN_PIN                  17
     #endif
 
   #endif
@@ -491,9 +491,9 @@
 
       // Buttons are directly attached to AUX-2
       #if IS_RRW_KEYPAD
-        #define SHIFT_OUT                     40
-        #define SHIFT_CLK                     44
-        #define SHIFT_LD                      42
+        #define SHIFT_OUT_PIN                 40
+        #define SHIFT_CLK_PIN                 44
+        #define SHIFT_LD_PIN                  42
         #define BTN_EN1                       64
         #define BTN_EN2                       59
         #define BTN_ENC                       63
