2218838945b43f4b0300bcf217f459afc61e6a9b
Tim Hoogland <tim@th3dstudio.com>
2021-01-13 14:11:38 -0600

    Add Alfawise U10 Support
    Add Alfawise U10 Support

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.16b"
+#define UNIFIED_VERSION "TH3D UFW 2.17"
 
 /**
  * TH3D Sanity Checks
