MarlinFirmware/Marlin

commit 4472ba2b6bc0835d0fcc039ad66d9dab40e13010

Ok to use C++11 'auto'

Marlin/src/lcd/dwin/e3v2/dwin.cpp
@@ -497,21 +497,13 @@ inline void Draw_Back_First(const bool is_sel=true) {
   if (is_sel) Draw_Menu_Cursor(0);
 }
 
-#define APPLY_ENCODER_F \
-  if (encoder_diffState == ENCODER_DIFF_CW)          \
-    valref += EncoderRate.encoderMoveValue;          \
-  else if (encoder_diffState == ENCODER_DIFF_CCW)    \
-    valref -= EncoderRate.encoderMoveValue;          \
-  else if (encoder_diffState == ENCODER_DIFF_ENTER)  \
-    return true;                                     \
-  return false;
-
-inline bool Apply_Encoder(const ENCODER_DiffState &encoder_diffState, int16_t &valref) {
-  APPLY_ENCODER_F
-}
-
-inline bool Apply_Encoder(const ENCODER_DiffState &encoder_diffState, float &valref) {
-  APPLY_ENCODER_F
+inline bool Apply_Encoder(const ENCODER_DiffState &encoder_diffState, auto &valref) {
+  if (encoder_diffState == ENCODER_DIFF_CW)
+    valref += EncoderRate.encoderMoveValue;
+  else if (encoder_diffState == ENCODER_DIFF_CCW)
+    valref -= EncoderRate.encoderMoveValue;
+  else if (encoder_diffState == ENCODER_DIFF_ENTER)
+    return true;
 }
 
 //

commit 6d604fe039ec1f74e464eac71728e294b3a401ff

Fix bug where encoder acts also as enter
Commit https://github.com/MarlinFirmware/Marlin/commit/4472ba2b6bc0835d0fcc039ad66d9dab40e13010 introduced an issue where rotating the encoder also acts as enter.

This issue fixes bug https://github.com/MarlinFirmware/Marlin/issues/20774

Marlin/src/lcd/dwin/e3v2/dwin.cpp
@@ -504,6 +504,7 @@ inline bool Apply_Encoder(const ENCODER_DiffState &encoder_diffState, auto &valr
     valref -= EncoderRate.encoderMoveValue;
   else if (encoder_diffState == ENCODER_DIFF_ENTER)
     return true;
+  return false;
 }
 
 //