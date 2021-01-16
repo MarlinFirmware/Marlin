8374cd9718a6d38c177e4d020afb5a32595642bd
zeleps <39417467+zeleps@users.noreply.github.com>
2021-01-16 19:56:58 +0200

    Temporarily disabled leveling during tramming

--- Marlin/src/lcd/menu/menu_tramming.cpp
+++ Marlin/src/lcd/menu/menu_tramming.cpp
@@ -42,6 +42,13 @@
 float z_measured[G35_PROBE_COUNT] = { 0 };
 static uint8_t tram_index = 0;
 
+#if HAS_LEVELING
+  #include "../../module/planner.h"
+  #include "../../feature/bedlevel/bedlevel.h"
+  
+  bool prev_leveling_state = false;
+#endif
+
 static bool probe_single_point() {
   do_blocking_move_to_z(TERN(BLTOUCH, Z_CLEARANCE_DEPLOY_PROBE, Z_CLEARANCE_BETWEEN_PROBES));
   // Stow after each point with BLTouch "HIGH SPEED" mode for push-pin safety
@@ -66,6 +73,12 @@ static void _menu_single_probe(const uint8_t point) {
 
 static void tramming_wizard_menu() {
   DEBUG_ECHOLNPAIR("Screen: tramming_wizard_menu");
+
+  #if HAS_LEVELING
+    prev_leveling_state = planner.leveling_active;
+    set_bed_leveling_enabled(false); // temporarily disable leveling; tramming requires physical coords
+  #endif 
+
   START_MENU();
   STATIC_ITEM(MSG_SELECT_ORIGIN);
 
@@ -75,6 +88,9 @@ static void tramming_wizard_menu() {
 
   ACTION_ITEM(MSG_BUTTON_DONE, []{
     probe.stow(); // Stow before exiting Tramming Wizard
+
+    TERN_(HAS_LEVELING, set_bed_leveling_enabled(prev_leveling_state)); // restore bed leveling on exit
+
     ui.goto_previous_screen_no_defer();
   });
   END_MENU();
