62e1366061bdebbe3e8b0d5a34261edb6a144dd1
Martin <34376785+G4Cab@users.noreply.github.com>
2021-01-18 23:06:49 +0100

    use case of dual Z
    redefine E1 for Z2 if Z2 driver is defined

--- Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO.h
+++ Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO.h
@@ -88,9 +88,17 @@
 #define E0_STEP_PIN                         PD6
 #define E0_DIR_PIN                          PD3
 
-#define E1_ENABLE_PIN                       PA3
-#define E1_STEP_PIN                         PA6
-#define E1_DIR_PIN                          PA1
+#ifndef Z2_DRIVER_TYPE 
+  #define E1_ENABLE_PIN                       PA3
+  #define E1_STEP_PIN                         PA6
+  #define E1_DIR_PIN                          PA1
+#endif
+
+#if DEFINED(Z2_DRIVER_TYPE)
+  #define Z2_ENABLE_PIN                       PA3
+  #define Z2_STEP_PIN                         PA6
+  #define Z2_DIR_PIN                          PA1
+#endif
 
 //
 // Temperature Sensors
