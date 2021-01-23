59cf8756ba04ae41d9f32758cde2ce8ed0d66639
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-01-23 15:55:25 +0100

    Somehow the bed heat was commented out

--- config/btt-skr-cr6-with-stock-creality-tft/Configuration.h
+++ config/btt-skr-cr6-with-stock-creality-tft/Configuration.h
@@ -1101,7 +1101,7 @@
 #define PREHEAT_BEFORE_PROBING
 #if ENABLED(PREHEAT_BEFORE_PROBING)
   #define PROBING_NOZZLE_TEMP 145   // (°C) Only applies to E0 at this time
-  //#define PROBING_BED_TEMP     50
+  #define PROBING_BED_TEMP     50
 #endif
 
 // For Inverting Stepper Enable Pins (Active Low) use 0, Non Inverting (Active High) use 1
