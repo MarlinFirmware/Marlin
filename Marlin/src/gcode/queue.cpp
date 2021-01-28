0ebf3093beded17e18941887026367fc940c0f67
ellensp <ellensp@hotmsil.com>
2021-01-28 15:32:46 +1300

    make report_state reply on initiating serial port.

--- Marlin/src/gcode/queue.cpp
+++ Marlin/src/gcode/queue.cpp
@@ -479,7 +479,7 @@ void GCodeQueue::get_serial_commands() {
       if (c < 0) continue;
 
       #if ENABLED(MEATPACK)
-        meatpack.handle_rx_char(uint8_t(c));
+        meatpack.handle_rx_char(uint8_t(c),i);
         char c_res[2] = { 0, 0 };
         const uint8_t char_count = meatpack.get_result_char(c_res);
       #else
