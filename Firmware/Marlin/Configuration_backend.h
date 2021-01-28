83eb16c9874cf576271b4ce1e465619478e2ed4c
Tim Hoogland <tim@th3dstudio.com>
2021-01-28 08:40:57 -0600

    Bump Version Number

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.17"
+#define UNIFIED_VERSION "TH3D UFW 2.18"
 
 /**
  * TH3D Sanity Checks
