fd0ae2c5c6e9ee3a36de26f15911ee4870355df3
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-01-28 22:45:32 +0100

    Optimize for H2

--- config/btt-skr-cr6-with-btt-tft-biqu-h2/Configuration.h
+++ config/btt-skr-cr6-with-btt-tft-biqu-h2/Configuration.h
@@ -1044,7 +1044,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 2
+//#define MULTIPLE_PROBING 2
 //#define EXTRA_PROBING    1
 
 /**
