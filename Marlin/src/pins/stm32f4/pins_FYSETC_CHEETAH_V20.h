90bc046e186d94998a40f0efea77f34c5df98ee5
Scott Lahteine <github@thinkyhead.com>
2021-01-28 19:50:46 -0600

    Update pins_FYSETC_CHEETAH_V20.h

--- Marlin/src/pins/stm32f4/pins_FYSETC_CHEETAH_V20.h
+++ Marlin/src/pins/stm32f4/pins_FYSETC_CHEETAH_V20.h
@@ -242,7 +242,7 @@
       #define LCD_PINS_D7           EXPA1_03_PIN
 
       #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-        #define BTN_ENC_EN          LCD_PINS_D7  // Detect the presence of the encoder
+        #define BTN_ENC_EN           LCD_PINS_D7  // Detect the presence of the encoder
       #endif
 
     #endif
