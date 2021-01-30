9f6872c695d109f6c73a25f8ba07e4f7b3152066
Malderin <grechishkin_aa@mil.ru>
2021-01-31 01:44:13 +0300

    Add "more" menu in TFT_LVGL_UI interface

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3344,25 +3344,30 @@
 /**
  * User-defined menu items that execute custom GCode
  */
-//#define CUSTOM_USER_MENUS
+#define CUSTOM_USER_MENUS
 #if ENABLED(CUSTOM_USER_MENUS)
   //#define CUSTOM_USER_MENU_TITLE "Custom Commands"
   #define USER_SCRIPT_DONE "M117 User Script Done"
   #define USER_SCRIPT_AUDIBLE_FEEDBACK
   //#define USER_SCRIPT_RETURN  // Return to status screen after a script
 
-  #define USER_DESC_1 "Home & UBL Info"
+  #define USER_CMD_1_ENABLE
+  #define USER_DESC_1 "Home &\nUBL Info"
   #define USER_GCODE_1 "G28\nG29W"
 
-  #define USER_DESC_2 "Preheat for " PREHEAT_1_LABEL
+  #define USER_CMD_2_ENABLE
+  #define USER_DESC_2 "Preheat for\n " PREHEAT_1_LABEL
   #define USER_GCODE_2 "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
 
-  #define USER_DESC_3 "Preheat for " PREHEAT_2_LABEL
+  #define USER_CMD_3_ENABLE
+  #define USER_DESC_3 "Preheat for\n " PREHEAT_2_LABEL
   #define USER_GCODE_3 "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_2_TEMP_HOTEND)
 
-  #define USER_DESC_4 "Heat Bed/Home/Level"
+  #define USER_CMD_4_ENABLE
+  #define USER_DESC_4 "Heat Bed/\nHome/Level"
   #define USER_GCODE_4 "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nG28\nG29"
 
+  #define USER_CMD_5_ENABLE
   #define USER_DESC_5 "Home & Info"
   #define USER_GCODE_5 "G28\nM503"
 #endif
