01b99738061b726aed800e4789720bca9ee4b251
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-02-02 14:47:35 -0600

    Update msc_sd.h

--- Marlin/src/HAL/STM32/msc_sd.h
+++ Marlin/src/HAL/STM32/msc_sd.h
@@ -1,7 +1,7 @@
 /**
  * Marlin 3D Printer Firmware
  *
- * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2021 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  * Copyright (c) 2019 BigTreeTech [https://github.com/bigtreetech]
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,7 +14,5 @@
  *
  */
 #pragma once
-
-#include "../../inc/MarlinConfigPre.h"
 
 void MSC_SD_init();
