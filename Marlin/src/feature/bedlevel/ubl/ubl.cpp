032152c41d4a6671ba7ef661a02e5d7a7630e67f
Giuliano Zaro <3684609+GMagician@users.noreply.github.com>
2021-02-02 21:57:30 +0100

    Update a UBL comment (#20931)

--- Marlin/src/feature/bedlevel/ubl/ubl.cpp
+++ Marlin/src/feature/bedlevel/ubl/ubl.cpp
@@ -214,7 +214,7 @@
         else if (isnan(f))
           serialprintPGM(human ? PSTR("  .   ") : PSTR("NAN"));
         else if (human || csv) {
-          if (human && f >= 0.0) SERIAL_CHAR(f > 0 ? '+' : ' ');  // Space for positive ('-' for negative)
+          if (human && f >= 0.0) SERIAL_CHAR(f > 0 ? '+' : ' ');  // Display sign also for positive numbers (' ' for 0)
           SERIAL_ECHO_F(f, 3);                                    // Positive: 5 digits, Negative: 6 digits
         }
         if (csv && i < GRID_MAX_POINTS_X - 1) SERIAL_CHAR('\t');
