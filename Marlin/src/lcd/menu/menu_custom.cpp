97c7d625365d37eda594584ec2fb179804b1c3f7
Scott Lahteine <github@thinkyhead.com>
2021-02-02 15:20:38 -0600

    => HAS_USER_ITEM

--- Marlin/src/lcd/menu/menu_custom.cpp
+++ Marlin/src/lcd/menu/menu_custom.cpp
@@ -31,8 +31,6 @@
 #include "menu_item.h"
 #include "../../gcode/queue.h"
 
-#define HAS_CUSTOM_USER_MENU_ITEM(N) (ENABLED(CUSTOM_USER_MENUS) && defined(USER_DESC_##N) && defined(USER_GCODE_##N))
-
 #ifdef USER_SCRIPT_DONE
   #define _DONE_SCRIPT "\n" USER_SCRIPT_DONE
 #else
@@ -48,80 +46,81 @@ void _lcd_user_gcode(PGM_P const cmd) {
 void menu_user() {
   START_MENU();
   BACK_ITEM(MSG_MAIN);
+  #define HAS_USER_ITEM(N) (defined(USER_DESC_##N) && defined(USER_GCODE_##N))
   #define USER_ITEM(N) ACTION_ITEM_P(PSTR(USER_DESC_##N), []{ _lcd_user_gcode(PSTR(USER_GCODE_##N _DONE_SCRIPT)); });
-  #if HAS_CUSTOM_USER_MENU_ITEM(1)
+  #if HAS_USER_ITEM(1)
     USER_ITEM(1);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(2)
+  #if HAS_USER_ITEM(2)
     USER_ITEM(2);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(3)
+  #if HAS_USER_ITEM(3)
     USER_ITEM(3);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(4)
+  #if HAS_USER_ITEM(4)
     USER_ITEM(4);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(5)
+  #if HAS_USER_ITEM(5)
     USER_ITEM(5);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(6)
+  #if HAS_USER_ITEM(6)
     USER_ITEM(6);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(7)
+  #if HAS_USER_ITEM(7)
     USER_ITEM(7);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(8)
+  #if HAS_USER_ITEM(8)
     USER_ITEM(8);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(9)
+  #if HAS_USER_ITEM(9)
     USER_ITEM(9);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(10)
+  #if HAS_USER_ITEM(10)
     USER_ITEM(10);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(11)
+  #if HAS_USER_ITEM(11)
     USER_ITEM(11);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(12)
+  #if HAS_USER_ITEM(12)
     USER_ITEM(12);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(13)
+  #if HAS_USER_ITEM(13)
     USER_ITEM(13);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(14)
+  #if HAS_USER_ITEM(14)
     USER_ITEM(14);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(15)
+  #if HAS_USER_ITEM(15)
     USER_ITEM(15);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(16)
+  #if HAS_USER_ITEM(16)
     USER_ITEM(16);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(17)
+  #if HAS_USER_ITEM(17)
     USER_ITEM(17);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(18)
+  #if HAS_USER_ITEM(18)
     USER_ITEM(18);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(19)
+  #if HAS_USER_ITEM(19)
     USER_ITEM(19);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(20)
+  #if HAS_USER_ITEM(20)
     USER_ITEM(20);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(21)
+  #if HAS_USER_ITEM(21)
     USER_ITEM(21);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(22)
+  #if HAS_USER_ITEM(22)
     USER_ITEM(22);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(23)
+  #if HAS_USER_ITEM(23)
     USER_ITEM(23);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(24)
+  #if HAS_USER_ITEM(24)
     USER_ITEM(24);
   #endif
-  #if HAS_CUSTOM_USER_MENU_ITEM(25)
+  #if HAS_USER_ITEM(25)
     USER_ITEM(25);
   #endif
   #undef USER_ITEM
