8f045fb73a5aa678db5fb4bd03ac768d12ba800a
X-Ryl669 <boite.pour.spam@gmail.com>
2021-02-05 19:03:31 +0100

    Fix building by deducing the environment. Thank you Scott for the tip.

--- buildroot/ci-check/build_config.sh
+++ buildroot/ci-check/build_config.sh
@@ -5,6 +5,8 @@ if [ "$1" != "internal" ]; then
   exit 1
 fi
 
+SED=$(which gsed || which sed)
+
 shopt -s nullglob
 for sub in $3/*/; do
   [ -d "${sub}" ] || continue
@@ -18,10 +20,21 @@ for sub in $3/*/; do
   echo "Copying configuration file from $2 to current working directory"
   cp $sub/Configuration.h Marlin/
   cp $sub/Configuration_adv.h Marlin/
-  env=`cat $sub/platformio.env`
 
+  # Shamelessly copied from mftest script, skimmed the dependencies
+  MB=$( grep -E "^\s*#define MOTHERBOARD" Marlin/Configuration.h | awk '{ print $3 }' | $SED 's/BOARD_//' )
+  [[ -z $MB ]] && { echo "Error - Can't read MOTHERBOARD setting." ; exit 2 ; }
+  BLINE=$( grep -E "define\s+BOARD_$MB\b" Marlin/src/core/boards.h )
+  BNUM=$( $SED -E 's/^.+BOARD_[^ ]+ +([0-9]+).+$/\1/' <<<"$BLINE" )
+  BDESC=$( $SED -E 's/^.+\/\/ *(.+)$/\1/' <<<"$BLINE" )
+  [[ -z $BNUM ]] && { echo "Error - Can't find $MB in boards list." ; exit 2 ; }
+  ENVS=( $( grep -EA1 "MB\(.*\b$MB\b.*\)" Marlin/src/pins/pins.h | grep -E "#include.+//.+(env|$SYS):[^ ]+" | grep -oE "(env|$SYS):[^ ]+" | $SED -E "s/(env|$SYS)://" ) )
+  [[ -z $ENVS ]] && { errout "Error - Can't find target(s) for $MB ($BNUM)." ; exit 2 ; }
+  ECOUNT=${#ENVS[*]}
+
+  echo "Deduced env: $ENVS ($ECOUNT)"
   echo "Building the firmware now..."
-  platformio run --environment $env || { echo "Failed"; exit 3; }
+  platformio run --environment $ENVS || { echo "Failed"; exit 3; }
 done
 
 echo "Success"
