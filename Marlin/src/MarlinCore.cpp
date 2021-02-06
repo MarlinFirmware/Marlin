1c19af2c8fd95ff6da86c9f8eb18166805ea0097
X-Ryl669 <boite.pour.spam@gmail.com>
2021-02-06 05:43:51 +0100

    Fix ARM delay function (#20901)

--- Marlin/src/MarlinCore.cpp
+++ Marlin/src/MarlinCore.cpp
@@ -1005,6 +1005,9 @@ void setup() {
   SERIAL_ECHO_MSG("Compiled: " __DATE__);
   SERIAL_ECHO_MSG(STR_FREE_MEMORY, freeMemory(), STR_PLANNER_BUFFER_BYTES, (int)sizeof(block_t) * (BLOCK_BUFFER_SIZE));
 
+  // Some HAL need precise delay adjustment
+  calibrate_delay_loop();
+
   // Init buzzer pin(s)
   #if USE_BEEPER
     SETUP_RUN(buzzer.init());
