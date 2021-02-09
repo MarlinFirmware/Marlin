a257a61102fc9a5d41f22a1ab02dcbe38e8f4b48
Foxies <Foxies-CSTL@users.noreply.github.com>
2021-02-09 02:47:49 +0100

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -46,7 +46,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "FLSUN_Qx"
+#define MACHINE_NAME "FLSun_Qx"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
