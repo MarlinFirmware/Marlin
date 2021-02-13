e51fdd2c6d7d48d7e2053e66bb1ade098e58f064
Cilliers9 <tclouwrens@gmail.com>
2021-02-13 06:54:27 +0200

    Chamber heater Watchdog
    1C at 120sec

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -235,8 +235,8 @@
   /**
    * Heated chamber watch settings (M141/M191).
    */
-  #define WATCH_CHAMBER_TEMP_PERIOD            60 // Seconds
-  #define WATCH_CHAMBER_TEMP_INCREASE           2 // Degrees Celsius
+  #define WATCH_CHAMBER_TEMP_PERIOD           120 // Seconds
+  #define WATCH_CHAMBER_TEMP_INCREASE           1 // Degrees Celsius
 #endif
 
 #if ENABLED(PIDTEMP)
