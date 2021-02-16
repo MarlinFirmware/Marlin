921a8132c8a869b47deeafc759ca97009603dca8
ellensp <ellensp@hotmsil.com>
2021-02-16 13:33:30 +1300

    swap Z_MIN and Z_MAX pins

--- Marlin/src/pins/stm32f1/pins_TRIGORILLA_PRO.h
+++ Marlin/src/pins/stm32f1/pins_TRIGORILLA_PRO.h
@@ -58,8 +58,8 @@
 //
 #define X_STOP_PIN                          PG10
 #define Y_STOP_PIN                          PA12
-#define Z_MAX_PIN                           PA14
-#define Z_MIN_PIN                           PA13
+#define Z_MIN_PIN                           PA14
+#define Z_MAX_PIN                           PA13
 
 //
 // Steppers
