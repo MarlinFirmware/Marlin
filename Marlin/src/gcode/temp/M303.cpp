a232720cc4e060867b8b14aeae471bcd5107a564
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-02-22 19:18:12 -0600

    use switch

--- Marlin/src/gcode/temp/M303.cpp
+++ Marlin/src/gcode/temp/M303.cpp
@@ -62,24 +62,25 @@ void GcodeSuite::M303() {
 
   const heater_id_t hid = (heater_id_t)parser.intval('E');
   int16_t default_temp;
-  do {
+  switch (hid) {
     #if ENABLED(PIDTEMP)
-      if (WITHIN(hid, 0, HOTENDS - 1)) { default_temp = PREHEAT_1_TEMP_HOTEND; break; }
+      case 0 ... HOTENDS - 1: default_temp = PREHEAT_1_TEMP_HOTEND; break;
     #endif
     #if ENABLED(PIDTEMPBED)
-      if (hid == H_BED) { default_temp = PREHEAT_1_TEMP_BED; break; }
+      case H_BED: default_temp = PREHEAT_1_TEMP_BED; break;
     #endif
     #if ENABLED(PIDTEMPCHAMBER)
-      if (hid == H_CHAMBER) { default_temp = PREHEAT_1_TEMP_CHAMBER; break; }
+      case H_CHAMBER: default_temp = PREHEAT_1_TEMP_CHAMBER; break;
     #endif
-    SERIAL_ECHOLNPGM(STR_PID_BAD_HEATER_ID);
-    TERN_(EXTENSIBLE_UI, ExtUI::onPidTuning(ExtUI::result_t::PID_BAD_EXTRUDER_NUM));
-    return;
-  } while(0);
+    default:
+      SERIAL_ECHOLNPGM(STR_PID_BAD_HEATER_ID);
+      TERN_(EXTENSIBLE_UI, ExtUI::onPidTuning(ExtUI::result_t::PID_BAD_EXTRUDER_NUM));
+      return;
+  }
 
+  const int16_t temp = parser.celsiusval('S', default_temp);
   const int c = parser.intval('C', 5);
   const bool u = parser.boolval('U');
-  const int16_t temp = parser.celsiusval('S', default_temp);
 
   #if DISABLED(BUSY_WHILE_HEATING)
     KEEPALIVE_STATE(NOT_BUSY);
