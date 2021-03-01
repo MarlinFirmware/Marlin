be8b5472615daad6a53694eb215e46c7a9377303
Scott Lahteine <github@thinkyhead.com>
2021-03-01 07:10:00 -0600

    Fix some config builds

--- Marlin/src/module/motion.h
+++ Marlin/src/module/motion.h
@@ -65,7 +65,9 @@ extern xyz_pos_t cartes;
   #define XY_PROBE_FEEDRATE_MM_S PLANNER_XY_FEEDRATE()
 #endif
 
-constexpr feedRate_t z_probe_fast_mm_s = MMM_TO_MMS(Z_PROBE_FEEDRATE_FAST);
+#if HAS_BED_PROBE
+  constexpr feedRate_t z_probe_fast_mm_s = MMM_TO_MMS(Z_PROBE_FEEDRATE_FAST);
+#endif
 
 /**
  * Feed rates are often configured with mm/m
