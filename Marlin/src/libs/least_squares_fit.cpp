c882715cd1df26e2c961ca93037d0eb836c2b3c8
Ramiro Polla <ramiro.polla@gmail.com>
2021-03-03 13:37:10 +0100

    Optimize least_squares_fit
    Remove the z2bar variable (which is unused) and don't bother storing
    intermediate values back into the linear_fit_data struct in function
    finish_incremental_LSF().
    
    Saves 232 bytes of program memory on AVR.
    
    This function could be optimized for speed by calculating (1/N) once
    and using multiplications instead of divisions, but this code is not
    speed-critical and that would increase program memory by a few bytes.

--- Marlin/src/libs/least_squares_fit.cpp
+++ Marlin/src/libs/least_squares_fit.cpp
@@ -46,23 +46,22 @@ int finish_incremental_LSF(struct linear_fit_data *lsf) {
   if (N == 0.0)
     return 1;
 
-  lsf->xbar /= N;
-  lsf->ybar /= N;
-  lsf->zbar /= N;
-  lsf->x2bar = lsf->x2bar / N - sq(lsf->xbar);
-  lsf->y2bar = lsf->y2bar / N - sq(lsf->ybar);
-  lsf->z2bar = lsf->z2bar / N - sq(lsf->zbar);
-  lsf->xybar = lsf->xybar / N - lsf->xbar * lsf->ybar;
-  lsf->yzbar = lsf->yzbar / N - lsf->ybar * lsf->zbar;
-  lsf->xzbar = lsf->xzbar / N - lsf->xbar * lsf->zbar;
-  const float DD = lsf->x2bar * lsf->y2bar - sq(lsf->xybar);
+  const float xbar = lsf->xbar / N;
+  const float ybar = lsf->ybar / N;
+  const float zbar = lsf->zbar / N;
+  const float x2bar = lsf->x2bar / N - sq(xbar);
+  const float y2bar = lsf->y2bar / N - sq(ybar);
+  const float xybar = lsf->xybar / N - xbar * ybar;
+  const float yzbar = lsf->yzbar / N - ybar * zbar;
+  const float xzbar = lsf->xzbar / N - xbar * zbar;
+  const float DD = x2bar * y2bar - sq(xybar);
 
   if (ABS(DD) <= 1e-10 * (lsf->max_absx + lsf->max_absy))
     return 1;
 
-  lsf->A = (lsf->yzbar * lsf->xybar - lsf->xzbar * lsf->y2bar) / DD;
-  lsf->B = (lsf->xzbar * lsf->xybar - lsf->yzbar * lsf->x2bar) / DD;
-  lsf->D = -(lsf->zbar + lsf->A * lsf->xbar + lsf->B * lsf->ybar);
+  lsf->A = (yzbar * xybar - xzbar * y2bar) / DD;
+  lsf->B = (xzbar * xybar - yzbar * x2bar) / DD;
+  lsf->D = -(zbar + lsf->A * xbar + lsf->B * ybar);
   return 0;
 }
 
