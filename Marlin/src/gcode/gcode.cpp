41cdac5d72a4d1fb3a76bfe7d6e761d9dbae40e7
Tegeon <szafran.sebastian@gmail.com>
2021-03-04 20:37:25 +0100

    Update gcode.cpp
    G42 defined twice.

--- Marlin/src/gcode/gcode.cpp
+++ Marlin/src/gcode/gcode.cpp
@@ -396,9 +396,11 @@ void GcodeSuite::process_parsed_command(const bool no_ok/*=false*/) {
 
       case 92: G92(); break;                                      // G92: Set current axis position(s)
 
-      #if HAS_MESH
+ /* Already defined in lines 367-369 above
+       #if HAS_MESH
         case 42: G42(); break;                                    // G42: Coordinated move to a mesh point
       #endif
+ */
 
       #if ENABLED(CALIBRATION_GCODE)
         case 425: G425(); break;                                  // G425: Perform calibration with calibration cube
