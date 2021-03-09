dd7d135971e0d4e50b66857cdddf4e20f2337064
Jyers <jyers.m@gmail.com>
2021-03-08 20:43:43 -0500

    Fixed My Offsets

--- Configuration Files/Jyers's Config/Configuration.h
+++ Configuration Files/Jyers's Config/Configuration.h
@@ -992,7 +992,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET {  -44, -7,- 3.2 }
+#define NOZZLE_TO_PROBE_OFFSET {  -45.5, -8,- 3.2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
