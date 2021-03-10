a2c453727d61ca2cdf2df0900ed0b3607edd52ca
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-03-10 19:47:25 +0100

    Back to nightly version

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "CR6Comm-Rel6-Pre5"
+#define SHORT_BUILD_VERSION "CR6Comm-Rel6-Nightly-Pre6"
 
 /**
  * Verbose version identifier which should contain a reference to the location
