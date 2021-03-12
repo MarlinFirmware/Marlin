66dca344bf6cb1fb916ee843ecaa00b5b5be79f0
Tim Hoogland <tim@th3dstudio.com>
2021-03-11 22:44:16 -0600

    Move Z Probe Offset Wizard to Community Features
    Move Z Probe Offset Wizard to Community Features

--- Firmware/Marlin/Configuration_adv.h
+++ Firmware/Marlin/Configuration_adv.h
@@ -1126,7 +1126,7 @@
 
   // Add Probe Z Offset calibration to the Z Probe Offsets menu
   #if HAS_BED_PROBE
-    #define PROBE_OFFSET_WIZARD
+    //#define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
       #define PROBE_OFFSET_START -5.0   // Estimated nozzle-to-probe Z offset, plus a little extra
     #endif
