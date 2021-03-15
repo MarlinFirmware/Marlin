e9aaac190e2e43626fd136e89a9acdd65476ff8c
X-Ryl669 <boite.pour.spam@gmail.com>
2021-03-15 10:28:32 +0100

    Continue removing useless macro anymore

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1444,9 +1444,6 @@
     #define SD_FIRMWARE_UPDATE_INACTIVE_VALUE 0xFF
   #endif
 
-  // Add an optimized binary file transfer mode, initiated with 'M28 B1'
-  //#define BINARY_FILE_TRANSFER
-
   /**
    * Set this option to one of the following (or the board's defaults apply):
    *
@@ -3372,10 +3369,6 @@
 #if ENABLED(FASTER_GCODE_PARSER)
   //#define GCODE_QUOTED_STRINGS  // Support for quoted string parameters
 #endif
-
-// Support for MeatPack G-code compression (https://github.com/scottmudge/OctoPrint-MeatPack)
-//#define MEATPACK_ON_SERIAL_PORT_1
-//#define MEATPACK_ON_SERIAL_PORT_2
 
 //#define GCODE_CASE_INSENSITIVE  // Accept G-code sent to the firmware in lowercase
 
