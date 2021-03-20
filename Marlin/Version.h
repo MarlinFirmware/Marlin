a05137dc728e0a386a60a42a2d5839e3703de292
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-03-20 18:18:01 +0100

    Bump version for pre6 - last stability tests

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "CR6Comm-Rel6-Nightly-Pre6"
+#define SHORT_BUILD_VERSION "CR6Comm-Rel6-Pre6"
 
 /**
  * Verbose version identifier which should contain a reference to the location
