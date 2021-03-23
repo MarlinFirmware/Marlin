e806c3376a2a2cbd863d6ae67fef45f32734762a
Marcio T <mlt4356-github@yahoo.com>
2021-03-23 00:45:51 -0600

    UBL - Hilbert space-filling curve probing sequence (#21387)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1478,6 +1478,8 @@
   #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
+  //#define UBL_HILBERT_CURVE       // Use Hilbert distribution for less travel when probing multiple points
+
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
   #define UBL_SAVE_ACTIVE_ON_M500   // Save the currently active mesh in the current slot on M500
 
