7379868416fa35f07800dfdfca866c2977e1e906
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-03-24 19:51:07 +0100

    Fix typo in Hemera config

--- config/btt-skr-cr6-with-btt-tft-e3d-hemera/Configuration.h
+++ config/btt-skr-cr6-with-btt-tft-e3d-hemera/Configuration.h
@@ -1177,7 +1177,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS (250 - LOST_PRINT_SIZE_Z
+#define Z_MAX_POS (250 - LOST_PRINT_SIZE_Z)
 /**
  * Software Endstops
  *
