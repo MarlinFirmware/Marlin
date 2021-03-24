e9618f1ba25c0560f490ce831e10d1906fa686b1
ldursw <37294448+ldursw@users.noreply.github.com>
2021-03-24 11:30:19 -0300

    Fix SDIO buffer alignment (#21396)

--- Marlin/src/feature/binary_stream.h
+++ Marlin/src/feature/binary_stream.h
@@ -51,7 +51,12 @@ inline int bs_read_serial(const uint8_t index) {
 
 #if ENABLED(BINARY_STREAM_COMPRESSION)
   static heatshrink_decoder hsd;
-  static uint8_t decode_buffer[512] = {};
+  #ifdef BOTH(ARDUINO_ARCH_STM32F1, SDIO_SUPPORT)
+    // STM32 requires a word-aligned buffer for SD card transfers via DMA
+    static __attribute__((aligned(sizeof(size_t)))) uint8_t decode_buffer[512] = {};
+  #else
+    static uint8_t decode_buffer[512] = {};
+  #endif
 #endif
 
 class SDFileTransferProtocol  {
