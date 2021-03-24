1709e252efbb37db443cb7ac63f83e2060a07055
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-03-24 21:40:25 +0100

    Enable SOFT_ENDSTOPS_MENU_ITEM for BTT TFT

--- config/btt-skr-cr6-with-btt-tft-e3d-hemera/Configuration.h
+++ config/btt-skr-cr6-with-btt-tft-e3d-hemera/Configuration.h
@@ -1204,7 +1204,7 @@
 #endif
 
 #if EITHER(MIN_SOFTWARE_ENDSTOPS, MAX_SOFTWARE_ENDSTOPS)
-  //#define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
+  #define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
 #endif
 
 /**
