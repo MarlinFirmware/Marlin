627331aa684cae1ea15f0c38f67d4eeaf51fbb93
Malderin <52313714+Malderin@users.noreply.github.com>
2021-03-29 11:19:38 +0300

    Fix MKS LVGL UI Main screen / print buttons (#21468)

--- Marlin/src/lcd/extui/lib/mks_ui/draw_dialog.cpp
+++ Marlin/src/lcd/extui/lib/mks_ui/draw_dialog.cpp
@@ -486,7 +486,7 @@ void lv_draw_dialog(uint8_t type) {
 
 void filament_sprayer_temp() {
   char buf[20] = {0};
-  sprintf(buf, preheat_menu.value_state, (int)thermalManager.degHotend(uiCfg.extruderIndex), (int)thermalManager.degTargetHotend(uiCfg.extruderIndex));
+  sprintf(buf, preheat_menu.value_state, thermalManager.degHotend(uiCfg.extruderIndex), thermalManager.degTargetHotend(uiCfg.extruderIndex));
 
   strcpy(public_buf_l, uiCfg.extruderIndex < 1 ? extrude_menu.ext1 : extrude_menu.ext2);
   strcat_P(public_buf_l, PSTR(": "));
