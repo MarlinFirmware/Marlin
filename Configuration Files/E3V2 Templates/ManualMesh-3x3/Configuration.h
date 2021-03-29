4f2791eb25149fddaa41b5d08c4c39a108f63698
Jyers <jyers.m@gmail.com>
2021-03-29 01:03:13 -0400

    Probing Margin Adjustment

--- Configuration Files/E3V2 Templates/ManualMesh-3x3/Configuration.h
+++ Configuration Files/E3V2 Templates/ManualMesh-3x3/Configuration.h
@@ -1060,7 +1060,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 15
+#define PROBING_MARGIN 20
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (133*60)
