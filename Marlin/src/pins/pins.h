abc046824128c3015246396ee12c4f7323449e21
vert <vert@vert.com>
2021-04-01 04:22:11 -0400

    fix typo

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -250,7 +250,7 @@
   #include "mega/pins_GT2560_V3_MC2.h"          // ATmega2560                             env:mega2560
 #elif MB(GT2560_V3_A20)
   #include "mega/pins_GT2560_V3_A20.h"          // ATmega2560                             env:mega2560
-#elif MB(GT2560_V3)
+#elif MB(GT2560_V4)
   #include "mega/pins_GT2560_V4.h"              // ATmega2560                             env:mega2560  
 #elif MB(EINSTART_S)
   #include "mega/pins_EINSTART-S.h"             // ATmega2560, ATmega1280                 env:mega2560ext env:mega1280
