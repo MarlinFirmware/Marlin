290fac7eddc4a985810adb772b9f76ef1f4bacd6
Scott Lahteine <github@thinkyhead.com>
2021-04-02 18:32:27 -0500

    improve macros for strict types

--- Marlin/src/core/macros.h
+++ Marlin/src/core/macros.h
@@ -117,14 +117,16 @@
 
   // C++11 solution that is standards compliant.
   template <class V, class N> static inline constexpr void NOLESS(V& v, const N n) {
-    if (n > v) v = n;
+    __typeof__(v) _n = n;
+    if (v < _n) v = _n;
   }
   template <class V, class N> static inline constexpr void NOMORE(V& v, const N n) {
-    if (n < v) v = n;
+    __typeof__(v) _n = n;
+    if (v > _n) v = _n;
   }
   template <class V, class N1, class N2> static inline constexpr void LIMIT(V& v, const N1 n1, const N2 n2) {
-    if (n1 > v) v = n1;
-    else if (n2 < v) v = n2;
+    __typeof__(v) _n1 = n1, _n2 = n2;
+    if (v < _n1) v = _n1; else if (v > _n2) v = _n2;
   }
 
 #else
@@ -132,21 +134,19 @@
   #define NOLESS(v, n) \
     do{ \
       __typeof__(v) _n = (n); \
-      if (_n > v) v = _n; \
+      if (v < _n) v = _n; \
     }while(0)
 
   #define NOMORE(v, n) \
     do{ \
       __typeof__(v) _n = (n); \
-      if (_n < v) v = _n; \
+      if (v > _n) v = _n; \
     }while(0)
 
   #define LIMIT(v, n1, n2) \
     do{ \
-      __typeof__(v) _n1 = (n1); \
-      __typeof__(v) _n2 = (n2); \
-      if (_n1 > v) v = _n1; \
-      else if (_n2 < v) v = _n2; \
+      __typeof__(v) _n1 = (n1), _n2 = (n2); \
+     if (v < _n1) v = _n1; else if (v > _n2) v = _n2; \
     }while(0)
 
 #endif
