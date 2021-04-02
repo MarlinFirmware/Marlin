901d8b0ec3a61b05a0913350e98ffdc20c74f3d0
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2021-04-02 15:33:48 -0300

    SD SPI pins for STM32

--- Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_common.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_common.h
@@ -279,5 +279,14 @@
   #error "SD CUSTOM_CABLE is not compatible with SKR Mini E3."
 #endif
 
-#define ONBOARD_SPI_DEVICE                     1  // SPI1
+#define ONBOARD_SPI_DEVICE                     1  // SPI1 -> used only by STM32F1...
 #define ONBOARD_SD_CS_PIN                   PA4   // Chip select for "System" SD card
+
+#define CUSTOM_SPI_PINS                         // TODO: needed because is the only way to set SPI for SD on STM32 (by now)
+#if ENABLED(CUSTOM_SPI_PINS)
+  #define ENABLE_SPI1
+  #define SDSS                              ONBOARD_SD_CS_PIN
+  #define SD_SCK_PIN                        PA5
+  #define SD_MISO_PIN                       PA6
+  #define SD_MOSI_PIN                       PA7
+#endif
