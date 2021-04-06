4e272ae4a71112ffeeb17f0177a704b835ac028b
vert <vert@vert.com>
2021-04-06 15:37:39 -0400

    ubl wizard follow up fix compile error

--- Marlin/src/lcd/menu/menu_ubl.cpp
+++ Marlin/src/lcd/menu/menu_ubl.cpp
@@ -642,11 +642,15 @@ void _lcd_ubl_level_bed() {
  * UBL Mesh Wizard - simple 1 click mesh creation with or without a probe.
  */
 void _lcd_ubl_mesh_wizard() {
-  char ubl_lcd_gcode[64];
-  #if HAS_BED_probe
+  char ubl_lcd_gcode[128];
+  #if HAS_BED_probe && HAS_HEATED_BED
     sprintf_P(ubl_lcd_gcode, PSTR("G28\nM190 S%i\nM109 S%i\nG29 P1\nG29 P3\nG29 S0\nG29 A\nG29 F 10.0\nM500\nM140 S0\nM0 Mesh Saved in Slot 0"), custom_bed_temp, custom_hotend_temp);
-  #else
+  #elif HAS_HEATED_BED
     sprintf_P(ubl_lcd_gcode, PSTR("G28\nM190 S%i\nM109 S%i\nG29 P4 R255\nG29 S0\nG29 A\nG29 F 10.0\nM500\nM0 Mesh Saved Slot 0"), custom_bed_temp, custom_hotend_temp);
+  #elif HAS_BED_probe
+    sprintf_P(ubl_lcd_gcode, PSTR("G28\nM109 S%i\nG29 P1\nG29 P3\nG29 S0\nG29 A\nG29 F 10.0\nM500\nM140 S0\nM0 Mesh Saved in Slot 0"), custom_hotend_temp);
+  #else
+    sprintf_P(ubl_lcd_gcode, PSTR("G28\nM109 S%i\nG29 P4 R255\nG29 S0\nG29 A\nG29 F 10.0\nM500\nM0 Mesh Saved Slot 0"), custom_hotend_temp);
   #endif
   queue.inject(ubl_lcd_gcode);
 }
