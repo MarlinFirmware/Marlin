779f704a75ef5dd89eaa9c728ed17a1bc0ee4711
Jyers <jyers.m@gmail.com>
2021-04-06 15:42:58 -0400

    Fixed X Max Position for Probe Configs

--- Configuration Files/Jyers's Config/Configuration.h
+++ Configuration Files/Jyers's Config/Configuration.h
@@ -1240,7 +1240,7 @@
 #define X_MIN_POS 4
 #define Y_MIN_POS 7
 #define Z_MIN_POS 0
-#define X_MAX_POS (X_BED_SIZE + X_MIN_POS)
+#define X_MAX_POS (X_BED_SIZE + X_MIN_POS) + 15 // Extended max to allow the probe to reach more of the bed.
 #define Y_MAX_POS (Y_BED_SIZE + Y_MIN_POS)
 #define Z_MAX_POS 250
 
