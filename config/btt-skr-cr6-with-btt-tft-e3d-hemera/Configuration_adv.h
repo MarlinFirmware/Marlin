52d9a55e3bf5fce9cd5e44301384070df38d1aeb
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-04-07 22:05:18 +0200

    Decrease Hemera current

--- config/btt-skr-cr6-with-btt-tft-e3d-hemera/Configuration_adv.h
+++ config/btt-skr-cr6-with-btt-tft-e3d-hemera/Configuration_adv.h
@@ -2381,7 +2381,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      1.197
+    #define E0_CURRENT      700
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
