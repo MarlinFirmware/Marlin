129ccb76b8932459cd2e87aaf0833674ad86f106
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-04-11 17:04:27 +0200

    Set resume purge length to zero #236

--- config/btt-skr-cr6-with-stock-creality-tft-biqu-h2/Configuration_adv.h
+++ config/btt-skr-cr6-with-stock-creality-tft-biqu-h2/Configuration_adv.h
@@ -2159,7 +2159,7 @@
                                                   //   Set to 0 for manual extrusion.
                                                   //   Filament can be extruded repeatedly from the Filament Change menu
                                                   //   until extrusion is consistent, and to purge old filament.
-  #define ADVANCED_PAUSE_RESUME_PRIME          5  // (mm) Extra distance to prime nozzle after returning from park.
+  #define ADVANCED_PAUSE_RESUME_PRIME          0  // (mm) Extra distance to prime nozzle after returning from park.
   //#define ADVANCED_PAUSE_FANS_PAUSE             // Turn off print-cooling fans while the machine is paused.
 
                                                   // Filament Unload does a Retract, Delay, and Purge first:
