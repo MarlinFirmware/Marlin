e7764eacceb9107b9ab24f0c867412180cdd2792
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-04-12 16:49:37 +0200

    Fix G/B color VPs #231

--- Marlin/src/lcd/extui/lib/dgus_creality/creality_touch/DGUSDisplayDef.h
+++ Marlin/src/lcd/extui/lib/dgus_creality/creality_touch/DGUSDisplayDef.h
@@ -505,8 +505,8 @@ constexpr uint16_t VP_ESTEPS_BACK_BUTTON = 0x22D7;
 constexpr uint16_t VP_RGB_NAV_BUTTON = 0x2390;
 
 constexpr uint16_t VP_RGB_CONTROL_R = 0x2392;
-constexpr uint16_t VP_RGB_CONTROL_B = 0x2394;
-constexpr uint16_t VP_RGB_CONTROL_G = 0x2396;
+constexpr uint16_t VP_RGB_CONTROL_G = 0x2394;
+constexpr uint16_t VP_RGB_CONTROL_B = 0x2396;
 constexpr uint16_t VP_RGB_CONTROL_W = 0x2398;
 constexpr uint16_t VP_RGB_CONTROL_I = 0x239a; // brightness
 
