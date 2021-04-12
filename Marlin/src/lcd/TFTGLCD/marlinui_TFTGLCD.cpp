800260801aecc1a62ef808ff1c0de8989809de93
Scott Lahteine <github@thinkyhead.com>
2021-04-12 16:44:57 -0500

    Reduce size of float math code

--- Marlin/src/lcd/TFTGLCD/marlinui_TFTGLCD.cpp
+++ Marlin/src/lcd/TFTGLCD/marlinui_TFTGLCD.cpp
@@ -941,7 +941,7 @@ void MarlinUI::draw_status_screen() {
       // Show the location value
       lcd.setCursor(_LCD_W_POS, 3); lcd_put_u8str_P(PSTR("Z:"));
 
-      if (!isnan(ubl.z_values[x_plot][y_plot]))
+      if (!ISNAN(ubl.z_values[x_plot][y_plot]))
         lcd.print(ftostr43sign(ubl.z_values[x_plot][y_plot]));
       else
         lcd_put_u8str_P(PSTR(" -----"));
