4e630a302a62189ca41181e7d201911a5f47f322
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-04-13 19:12:55 +0200

    Bump version

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "CR6Comm-Rel6.1-Nightly"
+#define SHORT_BUILD_VERSION "CR6Comm-Rel6.1-Pre1"
 
 /**
  * Verbose version identifier which should contain a reference to the location
