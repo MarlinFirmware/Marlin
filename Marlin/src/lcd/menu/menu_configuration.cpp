9f5c044f9e500592ebe695f77c8f80d84adb94c7
Lucas Oshiro <lucasseikioshiro@gmail.com>
2021-04-16 22:49:53 -0300

    Disable Z axis after change probe offset

--- Marlin/src/lcd/menu/menu_configuration.cpp
+++ Marlin/src/lcd/menu/menu_configuration.cpp
@@ -56,6 +56,11 @@ void menu_advanced_settings();
   void menu_delta_calibrate();
 #endif
 
+#if HAS_BED_PROBE
+  #include "../../module/stepper/indirection.h"
+  void disable_axis_z() { DISABLE_AXIS_Z(); }
+#endif
+
 #if ENABLED(LCD_PROGRESS_BAR_TEST)
 
   #include "../lcdprint.h"
@@ -509,7 +514,7 @@ void menu_configuration() {
   #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
     SUBMENU(MSG_ZPROBE_ZOFFSET, lcd_babystep_zoffset);
   #elif HAS_BED_PROBE
-    EDIT_ITEM(LCD_Z_OFFSET_TYPE, MSG_ZPROBE_ZOFFSET, &probe.offset.z, Z_PROBE_OFFSET_RANGE_MIN, Z_PROBE_OFFSET_RANGE_MAX);
+    EDIT_ITEM(LCD_Z_OFFSET_TYPE, MSG_ZPROBE_ZOFFSET, &probe.offset.z, Z_PROBE_OFFSET_RANGE_MIN, Z_PROBE_OFFSET_RANGE_MAX, disable_axis_z);
   #endif
 
   //
