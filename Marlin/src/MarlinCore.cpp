781f8fb23ee4ea463dcf2c6d9a4dfc23d95dab80
vert <vert@vert.com>
2021-04-21 19:01:26 -0400

    follow up

--- Marlin/src/MarlinCore.cpp
+++ Marlin/src/MarlinCore.cpp
@@ -1456,26 +1456,28 @@ void setup() {
     queue.inject_P(PSTR(STARTUP_COMMANDS));
   #endif
 
-  #if MIXING_STEPPERS == 2 //Vtool presets for 2 steppers
-     queue.inject_P(PSTR("M163 S0 P1\nM163 S1 P0\nM164 S0"));
-     queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P1\nM164 S1"));
-     queue.inject_P(PSTR("M163 S0 P0.50\nM163 S1 P0.50\nM164 S2"));
-     queue.inject_P(PSTR("M163 S0 P0.75\nM163 S1 P0.25\nM164 S3"));
-     queue.inject_P(PSTR("M163 S0 P0.25\nM163 S1 P0.75\nM164 S4"));
-     queue.inject_P(PSTR("M163 S0 P0.33\nM163 S1 P0.67\nM164 S5"));
-     queue.inject_P(PSTR("M163 S0 P0.67\nM163 S1 P0.33\nM164 S6"));
-     queue.inject_P(PSTR("M163 S0 P0.60\nM163 S1 P0.40\nM164 S7"));
-  #elif MIXING_STEPPERS == 3 // Vtool presets for 3 steppers
-    queue.inject_P(PSTR("M163 S0 P1\nM163 S1 P0\nM163 S2 P0\nM164 S0")); 
-    queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P1\nM163 S2 P0\nM164 S1"));  
-    queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P0\nM163 S2 P1\nM164 S2"));
-    queue.inject_P(PSTR("M163 S0 P0.33\nM163 S1 P0.33\nM163 S2 P0.34\nM164 S3"));
-    queue.inject_P(PSTR("M163 S0 P0.25\nM163 S1 P0.75\nM163 S2 P0\nM164 S4"));
-    queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P0.75\nM163 S2 P0.25\nM164 S5"));
-    queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P0.50\nM163 S2 P0.50\nM164 S6"));
-    queue.inject_P(PSTR("M163 S0 P0.50\nM163 S1 P0.50\nM163 S2 P0\nM164 S7")); 
+#if ENABLED(MIXING_EXTRUDER)
+  #if MIXING_STEPPERS == 2 // Vtool presets for 2 steppers (Overriden by slicer)
+     queue.inject_P(PSTR("M163 S0 P1\nM163 S1 P0\nM164 S0")); //Vtool0
+     queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P1\nM164 S1")); //Vtool1
+     queue.inject_P(PSTR("M163 S0 P0.50\nM163 S1 P0.50\nM164 S2")); //Vtool2
+     queue.inject_P(PSTR("M163 S0 P0.75\nM163 S1 P0.25\nM164 S3")); //Vtool3
+     queue.inject_P(PSTR("M163 S0 P0.25\nM163 S1 P0.75\nM164 S4")); //Vtool4
+     queue.inject_P(PSTR("M163 S0 P0.33\nM163 S1 P0.67\nM164 S5")); //Vtool5
+     queue.inject_P(PSTR("M163 S0 P0.67\nM163 S1 P0.33\nM164 S6")); //Vtool6
+     queue.inject_P(PSTR("M163 S0 P0.60\nM163 S1 P0.40\nM164 S7")); //Vtool7
+  #elif MIXING_STEPPERS == 3 // Vtool presets for 3 steppers (Overriden by slicer)
+    queue.inject_P(PSTR("M163 S0 P1\nM163 S1 P0\nM163 S2 P0\nM164 S0")); //Vtool0
+    queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P1\nM163 S2 P0\nM164 S1")); //Vtool1 
+    queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P0\nM163 S2 P1\nM164 S2")); //Vtool2
+    queue.inject_P(PSTR("M163 S0 P0.33\nM163 S1 P0.33\nM163 S2 P0.34\nM164 S3")); //Vtool3
+    queue.inject_P(PSTR("M163 S0 P0.25\nM163 S1 P0.75\nM163 S2 P0\nM164 S4")); //Vtool4
+    queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P0.75\nM163 S2 P0.25\nM164 S5")); //Vtool5
+    queue.inject_P(PSTR("M163 S0 P0\nM163 S1 P0.50\nM163 S2 P0.50\nM164 S6")); //Vtool6
+    queue.inject_P(PSTR("M163 S0 P0.50\nM163 S1 P0.50\nM163 S2 P0\nM164 S7")); //Vtool7 
   #endif
-  
+#endif  
+
   #if ENABLED(HOST_PROMPT_SUPPORT)
     SETUP_RUN(host_action_prompt_end());
   #endif
