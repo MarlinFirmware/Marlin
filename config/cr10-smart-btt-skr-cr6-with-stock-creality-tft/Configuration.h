b57b3ce250a464d1869502f89714e1a3e38d3d9e
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-04-23 22:39:20 +0200

    A little less probing speed doesn't hurt on this printer

--- config/cr10-smart-btt-skr-cr6-with-stock-creality-tft/Configuration.h
+++ config/cr10-smart-btt-skr-cr6-with-stock-creality-tft/Configuration.h
@@ -1058,7 +1058,7 @@
 #define XY_PROBE_FEEDRATE (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST ((4 * 60) / 2)
+#define Z_PROBE_FEEDRATE_FAST ((4 * 60) / 3)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
