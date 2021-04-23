a88cbce70b5fab9bd2c5013d1a2ffdf69a93eeb3
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-04-23 22:37:21 +0200

    95 is a better default for Creality single gear stock extruders

--- config/cr6-se-v4.5.2-mb-no-watchdog/Configuration.h
+++ config/cr6-se-v4.5.2-mb-no-watchdog/Configuration.h
@@ -798,7 +798,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 95}
 
 /**
  * Default Max Feed Rate (mm/s)
