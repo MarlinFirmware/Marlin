482a03437558b3adc8a1b1e9b44d9bf9f10a1f84
Scott Lahteine <github@thinkyhead.com>
2021-04-24 03:15:35 -0500

    and it was

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -972,8 +972,7 @@
   #define BACKLASH_DISTANCE_MM { 0, 0, 0 } // (mm)
   #define BACKLASH_CORRECTION    0.0       // 0.0 = no correction; 1.0 = full correction
 
-  // If CORE_BACKLASH enabled, backlash correction will made by addition steps 
-  // when motor changed direction on CORE kinematics
+  // Add steps for motor direction changes on CORE kinematics
   //#define CORE_BACKLASH
 
   // Set BACKLASH_SMOOTHING_MM to spread backlash correction over multiple segments
