3ae05572582272780f78a8e5083c2268a1cb88da
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-04-24 08:01:17 +0200

    Prevent extrusion on M600 un-park (#21670)
    Fixes #21669

--- Marlin/src/feature/pause.cpp
+++ Marlin/src/feature/pause.cpp
@@ -602,7 +602,7 @@ void resume_print(const_float_t slow_load_length/*=0*/, const_float_t fast_load_
 
   if (!axes_should_home()) {
     // Move XY back to saved position
-    destination.set(resume_position.x, resume_position.y, current_position.z);
+    destination.set(resume_position.x, resume_position.y, current_position.z, current_position.e);
     prepare_internal_move_to_destination(NOZZLE_PARK_XY_FEEDRATE);
 
     // Move Z back to saved position
