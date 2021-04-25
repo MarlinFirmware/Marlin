06a43356b349412d8a2df819d95bffd06e01c443
Scott Lahteine <github@thinkyhead.com>
2021-04-25 03:35:13 -0500

    document

--- Marlin/src/feature/bedlevel/bedlevel.cpp
+++ Marlin/src/feature/bedlevel/bedlevel.cpp
@@ -213,31 +213,27 @@ void reset_bed_level() {
 
   void _manual_goto_xy(const xy_pos_t &pos) {
 
-    /**
-     * Get the Z final position for the point measurement
-     * MBL will use the current Z position (again) if there's any raise or
-     * a Z safe homing height, otherwise it will move across without any raise.
-     */
+    // Get the resting Z position for after the XY move
     #ifdef MANUAL_PROBE_START_Z
-      constexpr float finalz = _MAX(0, MANUAL_PROBE_START_Z);
+      constexpr float finalz = _MAX(0, MANUAL_PROBE_START_Z); // If a MANUAL_PROBE_START_Z value is set, always respect it
     #else
       #warning "It's recommended to set some MANUAL_PROBE_START_Z value for manual leveling."
     #endif
-    #if Z_CLEARANCE_BETWEEN_MANUAL_PROBES > 0 // Move up, over, and back down
+    #if Z_CLEARANCE_BETWEEN_MANUAL_PROBES > 0     // A probe/obstacle clearance exists so there is a raise:
       #ifndef MANUAL_PROBE_START_Z
-        const float finalz = current_position.z;
+        const float finalz = current_position.z;  // - Use the current Z for starting-Z if no MANUAL_PROBE_START_Z was provided
       #endif
-      do_blocking_move_to_xy_z(pos, Z_CLEARANCE_BETWEEN_MANUAL_PROBES);
-      do_blocking_move_to_z(finalz);
-    #elif defined(MANUAL_PROBE_START_Z)       // Move over and back down
-      do_blocking_move_to_xy_z(pos, finalz);
-    #else                                     // Move over
-      do_blocking_move_to_xy(pos);
+      do_blocking_move_to_xy_z(pos, Z_CLEARANCE_BETWEEN_MANUAL_PROBES); // - Raise Z, then move to the new XY
+      do_blocking_move_to_z(finalz);              // - Lower down to the starting Z height, ready for adjustment!
+    #elif defined(MANUAL_PROBE_START_Z)           // A starting-Z was provided, but there's no raise:
+      do_blocking_move_to_xy_z(pos, finalz);      // - Move in XY then down to the starting Z height, ready for adjustment!
+    #else                                         // Zero raise and no starting Z height either:
+      do_blocking_move_to_xy(pos);                // - Move over with no raise, ready for adjustment!
     #endif
 
     TERN_(LCD_BED_LEVELING, ui.wait_for_move = false);
   }
 
-#endif
+#endif // MESH_BED_LEVELING || PROBE_MANUALLY
 
 #endif // HAS_LEVELING
