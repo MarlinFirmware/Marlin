625ec1e8dc5588ebad34a24850d721a249252816
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2021-04-26 13:43:40 -0500

    Fix Beta in Version.h

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "bugfix-2.0.x Development"
+  #define SHORT_BUILD_VERSION "bugfix-2.0.x Beta"
 #endif
 
 /**
