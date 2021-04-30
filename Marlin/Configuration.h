c82c2e137c98be7fbd9b3879373c059fb309c67e
Plarkass <olivier@breizh.ovh>
2021-04-30 14:35:04 +0200

    V4

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -76,7 +76,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Olivier V3, Ender-5)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Olivier V4, Ender-5)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
