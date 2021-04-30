196f145d74b5be35f4566886ff8af78036d93ae4
Alex <alexqm@icloud.com>
2021-04-30 15:16:52 -0500

    Disabled M503 to save memory

--- Configuration Files/Aquila Templates/BLTouch-9x9/Configuration.h
+++ Configuration Files/Aquila Templates/BLTouch-9x9/Configuration.h
@@ -1450,7 +1450,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
+  #define GRID_MAX_POINTS_X 9
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
