888e9cb60bc98b740b01a54ad07238d217915931
hartmannathan <59230071+hartmannathan@users.noreply.github.com>
2021-05-01 04:43:32 -0400

    Comment correction (#21729)

--- Marlin/src/libs/duration_t.h
+++ Marlin/src/libs/duration_t.h
@@ -115,7 +115,8 @@ struct duration_t {
    * @brief Formats the duration as a string
    * @details String will be formated using a "full" representation of duration
    *
-   * @param buffer The array pointed to must be able to accommodate 21 bytes
+   * @param buffer The array pointed to must be able to accommodate 22 bytes
+   *               (21 for the string, 1 more for the terminating nul)
    *
    * Output examples:
    *  123456789012345678901 (strlen)
