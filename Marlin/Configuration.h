bf0c7493831f6c378e742a89b1792c550e216b14
John-Nathan Hill <john.nat.hill@gmail.com>
2021-05-03 17:57:03 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -495,9 +495,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  26.34
+    #define DEFAULT_Ki   2.55
+    #define DEFAULT_Kd  68.10
   #endif
 #endif // PIDTEMP
 
