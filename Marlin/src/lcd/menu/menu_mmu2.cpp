8245f921debc895ded22d78dbce8994e52af8cb0
ellensp <ellensp@hotmsil.com>
2021-05-05 16:55:05 +1200

    Add missing quote mark

--- Marlin/src/lcd/menu/menu_mmu2.cpp
+++ Marlin/src/lcd/menu/menu_mmu2.cpp
@@ -24,7 +24,7 @@
 
 #if BOTH(HAS_LCD_MENU, MMU2_MENUS)
 
-#include "../../MarlinCore.h
+#include "../../MarlinCore.h"
 #include "../../feature/mmu/mmu2.h"
 #include "menu_mmu2.h"
 #include "menu_item.h"
