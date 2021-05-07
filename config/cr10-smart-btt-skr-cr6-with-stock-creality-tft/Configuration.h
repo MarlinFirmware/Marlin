0341ebdedbad0f3d4a55201b382934dd940c6804
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-05-07 23:26:06 +0200

    Increase PID functional range to 50 #248

--- config/cr10-smart-btt-skr-cr6-with-stock-creality-tft/Configuration.h
+++ config/cr10-smart-btt-skr-cr6-with-stock-creality-tft/Configuration.h
@@ -606,7 +606,7 @@
   //#define PID_DEBUG             // Sends debug data to the serial port. Use 'M303 D' to toggle activation.
   //#define PID_OPENLOOP          // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
   //#define SLOW_PWM_HEATERS      // PWM with very low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
-  #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
+  #define PID_FUNCTIONAL_RANGE 50 // If the temperature difference between the target temperature and the actual temperature
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
 #endif
 
