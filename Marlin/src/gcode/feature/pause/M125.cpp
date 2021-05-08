90c4f13320e68a5a261fe747c281c5629264f914
Scott Lahteine <github@thinkyhead.com>
2021-05-08 06:13:21 -0500

    rephrase

--- Marlin/src/gcode/feature/pause/M125.cpp
+++ Marlin/src/gcode/feature/pause/M125.cpp
@@ -81,7 +81,7 @@ void GcodeSuite::M125() {
   TERN_(POWER_LOSS_RECOVERY, if (recovery.enabled) recovery.save(true));
 
   if (pause_print(retract, park_point, show_lcd, 0)) {
-    if (ENABLED(EXTENSIBLE_UI) || !sd_printing || show_lcd || ENABLED(M600_PURGE_MORE_RESUMABLE)) {
+    if (EITHER(EXTENSIBLE_UI, M600_PURGE_MORE_RESUMABLE) || !sd_printing || show_lcd) {
       wait_for_confirmation(false, 0);
       resume_print(0, 0, -retract, 0);
     }
