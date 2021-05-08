45f968572fd4fe47b4d8244bbf4b771713e06ca8
tiagofreire-pt <41837236+tiagofreire-pt@users.noreply.github.com>
2021-05-08 15:17:24 +0100

    Fixing E0 current value

--- Configuration Files/Jyers's Config/Configuration_adv.h
+++ Configuration Files/Jyers's Config/Configuration_adv.h
@@ -2479,7 +2479,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT      990
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
@@ -2487,7 +2487,7 @@
   #endif
 
   #if AXIS_IS_TMC(E1)
-    #define E1_CURRENT      990
+    #define E1_CURRENT      800
     #define E1_MICROSTEPS    E0_MICROSTEPS
     #define E1_RSENSE         0.11
     #define E1_CHAIN_POS     -1
