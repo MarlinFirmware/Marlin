85156fd4d50287868274f375051ea96d2af11db5
Jyers <jyers.m@gmail.com>
2021-05-08 02:30:17 -0400

    Organized Display Firmware

--- Display firmware/dwin-ico-tools/run.sh
+++
@@ -1,6 +0,0 @@
-cp ../icons-new/*.jpg new
-cp ../icons-new/*.jpg new+denoised
-rm 9.new.ICO
-rm 9.new+denoised.ICO
-python makeIco.py new 9.new.ICO
-python makeIco.py new+denoised 9.new+denoised.ICO
