05bb3bde7a761a3a3087ec29d0b45814cb2174bb
Scott Lahteine <github@thinkyhead.com>
2021-05-15 15:21:02 -0500

    Update Hex Version to 02000801

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -35,7 +35,7 @@
  *
  * Advanced settings can be found in Configuration_adv.h
  */
-#define CONFIGURATION_H_VERSION 020008
+#define CONFIGURATION_H_VERSION 02000801
 
 //===========================================================================
 //============================= Getting Started =============================
