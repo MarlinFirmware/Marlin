1d7edc1c1333b692cdb109d1f35aafe151e89b91
ldursw <37294448+ldursw@users.noreply.github.com>
2021-05-17 09:20:36 -0300

    Update stm32_serialbuffer.py

--- buildroot/share/PlatformIO/scripts/stm32_serialbuffer.py
+++ buildroot/share/PlatformIO/scripts/stm32_serialbuffer.py
@@ -3,7 +3,16 @@
 #
 Import("env")
 
-# Set the buffer size according to the configuration file or 64 if too small or absent.
+# Marlin has `RX_BUFFER_SIZE` and `TX_BUFFER_SIZE` to configure the
+# buffer size for receiving and transmitting data respectively.
+# Stm32duino uses another set of defines for the same purpose,
+# so we get the values from the Marlin configuration and set
+# them in `SERIAL_RX_BUFFER_SIZE` and `SERIAL_TX_BUFFER_SIZE`.
+# It is not possible to change the values at runtime, they must
+# be set with build flags.
+#
+# The script will set the value as the default one (64 bytes)
+# or the user-configured one, whichever is higher.
 mf = env["MARLIN_FEATURES"]
 rxBuf = str(max(64, int(mf["RX_BUFFER_SIZE"]) if "RX_BUFFER_SIZE" in mf else 0))
 txBuf = str(max(64, int(mf["TX_BUFFER_SIZE"]) if "TX_BUFFER_SIZE" in mf else 0))
