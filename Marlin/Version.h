058e7d43ad943a1824ac7d0a2ddc8c412f9f9915
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-05-18 23:29:23 +0200

    Version

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "CR6Comm-Rel6.1-PreXS"
+#define SHORT_BUILD_VERSION "CR6Comm-Rel6.1-Pre4"
 
 /**
  * Verbose version identifier which should contain a reference to the location
