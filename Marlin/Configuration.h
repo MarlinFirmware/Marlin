26757aa412d836411695caf873f2b75501eea2fb
PeteG <peteg@hotmail.co.nz>
2021-05-22 21:05:19 +1200

    ??

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1473,7 +1473,7 @@
 
     // Beyond the probed grid, continue the implied tilt?
     // Default is to maintain the height of the nearest edge.
-    //#define EXTRAPOLATE_BEYOND_GRID
+    #define EXTRAPOLATE_BEYOND_GRID
 
     //
     // Experimental Subdivision of the grid by Catmull-Rom method.
