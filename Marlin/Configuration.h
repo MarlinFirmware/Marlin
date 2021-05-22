19008dcb41d1ddb7c0d2005c215dbd546e69da59
PeteG <peteg@hotmail.co.nz>
2021-05-23 09:38:06 +1200

    set min z-stop to -4 so we can move lower - eg respect a neg zoffset?

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1247,7 +1247,7 @@
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -5
 #define Y_MIN_POS -10
-#define Z_MIN_POS 0
+#define Z_MIN_POS -4
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
