dac19ea2c147aafa8d48307270ad1de9c059a076
PeteG <peteg@hotmail.co.nz>
2021-05-23 21:02:49 +1200

    set correct nozzle z-offset and correct z feed rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -815,7 +815,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 136.3 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1064,7 +1064,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -14, -3.6 }
+#define NOZZLE_TO_PROBE_OFFSET { -42.5, -14, -3.65 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
