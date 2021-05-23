71273cda203566b423823c366b88949192ae7e2e
PeteG <peteg@hotmail.co.nz>
2021-05-23 15:10:41 +1200

    my ender3 with the MS extruder has -3.6mm z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1064,7 +1064,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -14, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -14, -3.6 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
