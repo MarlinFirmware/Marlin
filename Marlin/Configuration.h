e7bb624ce8f63bd161d1bb68526370594f706e9c
PeteG <peteg@hotmail.co.nz>
2021-05-23 15:11:26 +1200

    copied this from someone else online
    not sure this has any effect to my z-offset issues

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1227,10 +1227,10 @@
  */
 //#define Z_IDLE_HEIGHT Z_HOME_POS
 
-#define Z_HOMING_HEIGHT  20      // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
+#define Z_HOMING_HEIGHT  10      // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
                                   // Be sure to have this much clearance over your Z_MAX_POS to prevent grinding.
 
-#define Z_AFTER_HOMING  10      // (mm) Height to move to after homing Z
+//#define Z_AFTER_HOMING  10      // (mm) Height to move to after homing Z
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
