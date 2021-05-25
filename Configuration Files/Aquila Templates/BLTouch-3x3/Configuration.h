f2201270cbd657374410f011b7d6ed94f0abaeed
Alex <alexqm@icloud.com>
2021-05-25 15:25:42 -0500

    Dialed X_MAX_POS back a little

--- Configuration Files/Aquila Templates/BLTouch-3x3/Configuration.h
+++ Configuration Files/Aquila Templates/BLTouch-3x3/Configuration.h
@@ -1246,7 +1246,7 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE + 15
+#define X_MAX_POS X_BED_SIZE + 10
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 
