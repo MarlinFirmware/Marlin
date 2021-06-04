5c1c2daca38fdee797aff4b78fb6109ec23f92d6
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2021-06-04 01:42:58 -0700

    Update Hex Version to 02000802

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -35,7 +35,7 @@
  *
  * Advanced settings can be found in Configuration_adv.h
  */
-#define CONFIGURATION_H_VERSION 02000801
+#define CONFIGURATION_H_VERSION 02000802
 
 //===========================================================================
 //============================= Getting Started =============================
