b28979bacfb9df4c767037296f420fa46f155480
Cilliers9 <tclouwrens@gmail.com>
2021-06-06 07:35:34 +0200

    Update Configuration.h
    Bed size lower too 225

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1122,7 +1122,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 230
+#define Y_BED_SIZE 225
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
