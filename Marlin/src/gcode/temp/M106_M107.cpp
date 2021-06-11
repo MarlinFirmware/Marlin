20666849200ecac8e2296c746c654da9c97751a5
Scott Lahteine <github@thinkyhead.com>
2021-06-11 18:18:27 -0500

    no direct control of redundant fan

--- Marlin/src/gcode/temp/M106_M107.cpp
+++ Marlin/src/gcode/temp/M106_M107.cpp
@@ -61,6 +61,9 @@
 void GcodeSuite::M106() {
   const uint8_t pfan = parser.byteval('P', _ALT_P);
   if (pfan >= _CNT_P) return;
+  #if REDUNDANT_COOLING_FAN
+    if (pfan == REDUNDANT_COOLING_FAN) return;
+  #endif
 
   #if ENABLED(EXTRA_FAN_SPEED)
     const uint16_t t = parser.intval('T');
@@ -97,6 +100,9 @@ void GcodeSuite::M106() {
 void GcodeSuite::M107() {
   const uint8_t pfan = parser.byteval('P', _ALT_P);
   if (pfan >= _CNT_P) return;
+  #if REDUNDANT_COOLING_FAN
+    if (pfan == REDUNDANT_COOLING_FAN) return;
+  #endif
 
   thermalManager.set_fan_speed(pfan, 0);
 
