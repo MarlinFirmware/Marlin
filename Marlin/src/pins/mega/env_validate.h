70854c6586284a3c0c6cf3834cd610fb879544fa
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2021-06-16 20:49:40 -0300

    fix ALLOW_MEGA1280 if

--- Marlin/src/pins/mega/env_validate.h
+++ Marlin/src/pins/mega/env_validate.h
@@ -23,7 +23,7 @@
 
 #if ENABLED(ALLOW_MEGA1280) && NOT_TARGET(__AVR_ATmega1280__, __AVR_ATmega2560__)
   #error "Oops! Select 'Arduino/Genuino Mega or Mega 2560 or 1280' in 'Tools > Board.'"
-#elif NOT_TARGET(__AVR_ATmega2560__)
+#elif DISABLED(ALLOW_MEGA1280) && NOT_TARGET(__AVR_ATmega2560__)
   #error "Oops! Select 'Arduino/Genuino Mega or Mega 2560' in 'Tools > Board.'"
 #endif
 
