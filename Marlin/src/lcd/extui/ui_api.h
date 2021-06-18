34dbfe7e1e96f692d060cce946486100c0427f0f
InsanityAutomation <d.menzel@insanityautomation.com>
2021-06-18 00:28:18 -0400

    Fix to celsius_t in header for PID tune

--- Marlin/src/lcd/extui/ui_api.h
+++ Marlin/src/lcd/extui/ui_api.h
@@ -312,7 +312,7 @@ namespace ExtUI {
     float getPIDValues_Ki(const extruder_t);
     float getPIDValues_Kd(const extruder_t);
     void setPIDValues(const_float_t, const_float_t , const_float_t , extruder_t);
-    void startPIDTune(const_float_t, extruder_t);
+    void startPIDTune(celsius_t, extruder_t);
   #endif
 
   #if ENABLED(PIDTEMPBED)
@@ -320,7 +320,7 @@ namespace ExtUI {
     float getBedPIDValues_Ki();
     float getBedPIDValues_Kd();
     void setBedPIDValues(const_float_t, const_float_t , const_float_t );
-    void startBedPIDTune(const_float_t );
+    void startBedPIDTune(celsius_t );
   #endif
 
   /**
