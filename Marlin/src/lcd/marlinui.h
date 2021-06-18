870c7074ec90a0f960e11e91cdb54ec4a9e484f8
InsanityAutomation <d.menzel@insanityautomation.com>
2021-06-18 00:10:55 -0400

    Add Pause State to ExtUI

--- Marlin/src/lcd/marlinui.h
+++ Marlin/src/lcd/marlinui.h
@@ -55,7 +55,7 @@
   #include "../module/printcounter.h"
 #endif
 
-#if BOTH(HAS_LCD_MENU, ADVANCED_PAUSE_FEATURE)
+#if ANY(HAS_LCD_MENU, EXTENSIBLE_UI) && ENABLED(ADVANCED_PAUSE_FEATURE)
   #include "../feature/pause.h"
   #include "../module/motion.h" // for active_extruder
 #endif
@@ -541,7 +541,7 @@ public:
     static inline bool use_click() { return false; }
   #endif
 
-  #if BOTH(HAS_LCD_MENU, ADVANCED_PAUSE_FEATURE)
+  #if ANY(HAS_LCD_MENU, EXTENSIBLE_UI) && ENABLED(ADVANCED_PAUSE_FEATURE)
     static void pause_show_message(const PauseMessage message, const PauseMode mode=PAUSE_MODE_SAME, const uint8_t extruder=active_extruder);
   #else
     static inline void _pause_show_message() {}
