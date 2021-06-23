3563f97e91599fe2887c550875eda24367491f46
Jyers <jyers.m@gmail.com>
2021-06-23 15:42:41 -0400

    Format Fix

--- Configuration Files/E3V2 Templates/UBL-NoProbe-3x3/Configuration.h
+++ Configuration Files/E3V2 Templates/UBL-NoProbe-3x3/Configuration.h
@@ -1700,7 +1700,7 @@
 //#define PREHEAT_5_LABEL       "TPU"
 //#define PREHEAT_5_TEMP_HOTEND 230
 //#define PREHEAT_5_TEMP_BED     60
-///#define PREHEAT_5_FAN_SPEED   255 // Value from 0 to 255
+//#define PREHEAT_5_FAN_SPEED   255 // Value from 0 to 255
 
 /**
  * Nozzle Park
