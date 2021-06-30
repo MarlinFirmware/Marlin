220e15d667247e975db892b440f68fe4aa6cfd5f
Marcio Teixeira <mlt4356-github@yahoo.com>
2021-06-30 17:56:21 -0600

    Remove M75/M77 as it can cause issues.

--- Marlin/src/lcd/extui/ftdi_eve_touch_ui/generic/bed_mesh_view_screen.cpp
+++ Marlin/src/lcd/extui/ftdi_eve_touch_ui/generic/bed_mesh_view_screen.cpp
@@ -160,7 +160,7 @@ void BedMeshViewScreen::doProbe() {
 void BedMeshViewScreen::doMeshValidation() {
   mydata.count = 0;
   GOTO_SCREEN(StatusScreen);
-  injectCommands_P(PSTR("M75\nG28\nM117 Heating...\nG26 R X0 Y0\nG27\nM77"));
+  injectCommands_P(PSTR("G28\nM117 Heating...\nG26 R X0 Y0\nG27"));
 }
 
 void BedMeshViewScreen::show() {
