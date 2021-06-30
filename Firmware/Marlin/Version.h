5f8d30c9ca66ae5f12845a09ae817fb74c31032b
Tim Hoogland <tim@th3dstudio.com>
2021-06-30 10:48:15 -0500

    More updates for 2.0.9 Update.

--- Firmware/Marlin/Version.h
+++ Firmware/Marlin/Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "06-25-2021"
+#define STRING_DISTRIBUTION_DATE "06-30-2021"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
