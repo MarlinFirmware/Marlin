54946ec1480ddf9bdb8856205549f8e50dbba5c1
mks-viva <1224833100@qq.com>
2021-07-02 18:07:52 +0800

    MKS Robin E3/E3D has supported MKS MINI12864 V3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2819,7 +2819,7 @@
 #endif
 
 // Support for Adafruit NeoPixel LED driver
-#define NEOPIXEL_LED
+//#define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
   #define NEOPIXEL_TYPE   NEO_GRBW // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
   //#define NEOPIXEL_PIN     4     // LED driving pin
