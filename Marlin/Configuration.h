9f0a6e21769a8fbed72c886ef101f21d70cc80c2
Ihor M <erwaht@gmail.com>
2021-07-04 10:46:55 +0200

    my printer id update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -147,7 +147,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "c8c479a4-dca3-11eb-ba80-0242ac130004"
 
 /**
  * Define the number of coordinated linear axes.
