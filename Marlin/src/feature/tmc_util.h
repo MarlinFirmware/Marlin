40bda0c0603d6928787056d93297702cee223b60
lujios <83166168+lujios@users.noreply.github.com>
2021-07-10 21:54:16 +0200

    Update tmc_util.h

--- Marlin/src/feature/tmc_util.h
+++ Marlin/src/feature/tmc_util.h
@@ -361,21 +361,12 @@ void test_tmc_connection(LOGICAL_AXIS_DECL(const bool, true));
     extern millis_t sg_guard_period;
     constexpr uint16_t default_sg_guard_duration = 400;
 
-    #if ENABLED(DELTA)
-      struct motion_state_t {
-        xyz_ulong_t acceleration;
-        #if HAS_CLASSIC_JERK
-          xyz_float_t jerk_state;
-        #endif
-      };
-    #else
-      struct motion_state_t {
-        xy_ulong_t acceleration;
-        #if HAS_CLASSIC_JERK
-          xy_float_t jerk_state;
-        #endif
-      };
-    #endif
+    struct motion_state_t {
+      xyz_ulong_t acceleration;
+      #if HAS_CLASSIC_JERK
+        xyz_float_t jerk_state;
+      #endif
+    };
   #endif
 
   bool tmc_enable_stallguard(TMC2130Stepper &st);
