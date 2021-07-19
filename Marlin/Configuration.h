b5da4c464285808dacc2d564339d1233f4f4ba26
Ihor M <erwaht@gmail.com>
2021-07-19 21:56:21 +0200

    probe offset wizard

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1571,7 +1571,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 4
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1190,7 +1190,7 @@
 
   // Add Probe Z Offset calibration to the Z Probe Offsets menu
   #if HAS_BED_PROBE
-    //#define PROBE_OFFSET_WIZARD
+    #define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
       //
       // Enable to init the Probe Z-Offset when starting the Wizard.
