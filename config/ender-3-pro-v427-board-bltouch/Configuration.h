18b5ecfa5607749348e6ff1e10c9299f01d86b69
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-07-21 22:51:00 +0200

    Update Ender 3 config

--- config/ender-3-pro-v427-board-bltouch/Configuration.h
+++ config/ender-3-pro-v427-board-bltouch/Configuration.h
@@ -1150,7 +1150,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -7, -2.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1339,8 +1339,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
