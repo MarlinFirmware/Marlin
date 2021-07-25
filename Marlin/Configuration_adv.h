2ceb4b93a08e8f7243e83e4e4aaee117b2371618
Ihor M <erwaht@gmail.com>
2021-07-25 13:00:09 +0200

    z-offset-wizard

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1238,7 +1238,7 @@
       // Use a height slightly above the estimated nozzle-to-probe Z offset.
       // For example, with an offset of -5, consider a starting height of -4.
       //
-      //#define PROBE_OFFSET_WIZARD_START_Z -4.0
+      #define PROBE_OFFSET_WIZARD_START_Z -4.0
 
       // Set a convenient position to do the calibration (probing point and nozzle/bed-distance)
       //#define PROBE_OFFSET_WIZARD_XY_POS { X_CENTER, Y_CENTER }
