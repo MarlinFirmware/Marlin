301eb492c9bbf1ec17cde6b568162718abcb1e73
Justin Nesselrotte <admin@jnesselr.org>
2021-07-27 00:49:52 -0600

    Updated most pins for Mobo Rev03 beta

--- Marlin/src/pins/stm32f4/pins_INDEX_REV03.h
+++ Marlin/src/pins/stm32f4/pins_INDEX_REV03.h
@@ -22,23 +22,18 @@
 #pragma once
 
 /**
- * STM32F407VET6 with RAMPS-like shield
- * 'Black' STM32F407VET6 board - https://www.stm32duino.com/viewtopic.php?t=485
- * Shield - https://github.com/jmz52/Hardware
+ * STM32F407VET6 on Index PnP Mobo Rev03
+ * Website - https://indexmachines.io/
  */
 
 #define ALLOW_STM32DUINO
 #include "env_validate.h"
 
-#if HOTENDS > 2 || E_STEPPERS > 2
-  #error "Black STM32F4VET6 supports up to 2 hotends / E-steppers."
-#endif
-
 #ifndef BOARD_INFO_NAME
-  #define BOARD_INFO_NAME "Black STM32F4VET6"
+  #define BOARD_INFO_NAME "Index Mobo Rev03"
 #endif
 
-#define DEFAULT_MACHINE_NAME "STM32F407VET6"
+#define DEFAULT_MACHINE_NAME "Index Pick and Place"
 
 //#define I2C_EEPROM
 #define SRAM_EEPROM_EMULATION
@@ -47,118 +42,75 @@
 //
 // Servos
 //
-#define SERVO0_PIN                          PC6
-#define SERVO1_PIN                          PC7
+#define SERVO0_PIN                          PB6
+#define SERVO1_PIN                          PB7
 
 //
 // Limit Switches
 //
-#define X_MIN_PIN                           PC13
-#define X_MAX_PIN                           PA15
-#define Y_MIN_PIN                           PA5
-#define Y_MAX_PIN                           PD12
+#define X_MIN_PIN                           PC6
+#define X_MAX_PIN                           -1
+#define Y_MIN_PIN                           PD15
+#define Y_MAX_PIN                           -1
 #define Z_MIN_PIN                           PD14
-#define Z_MAX_PIN                           PD15
+#define Z_MAX_PIN                           -1
+#define I_MIN_PIN                           -1
+#define I_MAX_PIN                           -1
+#define J_MIN_PIN                           -1
+#define J_MAX_PIN                           -1
+#define K_MIN_PIN                           -1
+#define K_MAX_PIN                           -1
 
 //
 // Steppers
 //
-#define X_STEP_PIN                          PC4
-#define X_DIR_PIN                           PA4
-#define X_ENABLE_PIN                        PE7
+#define X_STEP_PIN                          PB15
+#define X_DIR_PIN                           PB14
+#define X_ENABLE_PIN                        PD9
 
-#define Y_STEP_PIN                          PE5
-#define Y_DIR_PIN                           PE2
-#define Y_ENABLE_PIN                        PE6
+#define Y_STEP_PIN                          PE15
+#define Y_DIR_PIN                           PE14
+#define Y_ENABLE_PIN                        PB13
 
-#define Z_STEP_PIN                          PD5
-#define Z_DIR_PIN                           PD3
-#define Z_ENABLE_PIN                        PD6
+#define Z_STEP_PIN                          PE7
+#define Z_DIR_PIN                           PB1
+#define Z_ENABLE_PIN                        PE9
 
-#define E0_STEP_PIN                         PD7
-#define E0_DIR_PIN                          PD0
-#define E0_ENABLE_PIN                       PB9
+#define I_STEP_PIN                         PC4
+#define I_DIR_PIN                          PA4
+#define I_ENABLE_PIN                       PB0
 
-#define E1_STEP_PIN                         PE0
-#define E1_DIR_PIN                          PE1
-#define E1_ENABLE_PIN                       PB8
+#define J_STEP_PIN                         PE11
+#define J_DIR_PIN                          PE10
+#define J_ENABLE_PIN                       PE13
 
-//
-// Temperature Sensors
-//
-#define TEMP_0_PIN                          PC0   // T0
-#define TEMP_1_PIN                          PC1   // T1
-#define TEMP_BED_PIN                        PC2   // TB
+#define K_STEP_PIN                         PC_15  // Not an Arduino pin, might need a new board config
+#define K_DIR_PIN                          PC_14  // Not an Arduino pin, might need a new board config
+#define K_ENABLE_PIN                       PA3
 
-#ifndef TEMP_CHAMBER_PIN
-  #define TEMP_CHAMBER_PIN                  PC3   // TC
-#endif
+#define X_SERIAL_TX_PIN                    PD8
+#define X_SERIAL_RX_PIN                    PD8
+#define Y_SERIAL_TX_PIN                    PB12
+#define Y_SERIAL_RX_PIN                    PB12
+#define Z_SERIAL_TX_PIN                    PE8
+#define Z_SERIAL_RX_PIN                    PE8
+#define I_SERIAL_TX_PIN                    PC5
+#define I_SERIAL_RX_PIN                    PC5
+#define J_SERIAL_TX_PIN                    PE12
+#define J_SERIAL_RX_PIN                    PE12
+#define K_SERIAL_TX_PIN                    PA2
+#define K_SERIAL_RX_PIN                    PA2
 
-//
-// Heaters / Fans
-//
-#define HEATER_0_PIN                        PA2   // Heater0
-#define HEATER_1_PIN                        PA3   // Heater1
-#define HEATER_BED_PIN                      PA1   // Hotbed
+// Reduce baud rate to improve software serial reliability
+#define TMC_BAUD_RATE                    19200
 
-#define FAN_PIN                             PE9   // Fan0
-#define FAN1_PIN                            PE11  // Fan1
-#define FAN2_PIN                            PE13  // Fan2
-#define FAN3_PIN                            PE14  // Fan3
+#define TEMP_0_PIN  PD0 // Not required for this board, really
 
-//
-// Misc. Functions
-//
-#define LED_PIN                             PA6
-//#define LED_PIN                           PA7
-#define KILL_PIN                            PB1
+#define MOSFET_A_PIN                       PE2
+#define MOSFET_B_PIN                       PE3
+#define MOSFET_C_PIN                       PE4
+#define MOSFET_D_PIN                       PE5
 
-//
-// LCD / Controller
-//
-//#define SD_DETECT_PIN                     PC5
-//#define SD_DETECT_PIN                     PA8   // SDIO SD_DETECT_PIN, external SDIO card reader only
-
-#define BEEPER_PIN                          PD10
-#define LCD_PINS_RS                         PE15
-#define LCD_PINS_ENABLE                     PD8
-#define LCD_PINS_D4                         PE10
-#define LCD_PINS_D5                         PE12
-#define LCD_PINS_D6                         PD1
-#define LCD_PINS_D7                         PE8
-#define BTN_ENC                             PD9
-#define BTN_EN1                             PD4
-#define BTN_EN2                             PD13
-
-#define DOGLCD_CS                    LCD_PINS_D5
-#define DOGLCD_A0                    LCD_PINS_D6
-
-#if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-  #define BTN_ENC_EN                 LCD_PINS_D7  // Detect the presence of the encoder
-#endif
-
-//
-// Onboard SD support
-//
-#define SDIO_D0_PIN                         PC8
-#define SDIO_D1_PIN                         PC9
-#define SDIO_D2_PIN                         PC10
-#define SDIO_D3_PIN                         PC11
-#define SDIO_CK_PIN                         PC12
-#define SDIO_CMD_PIN                        PD2
-
-#ifndef SDCARD_CONNECTION
-  #define SDCARD_CONNECTION              ONBOARD
-#endif
-
-#if SD_CONNECTION_IS(ONBOARD)
-  #define SDIO_SUPPORT                            // Use SDIO for onboard SD
-
-  #ifndef SDIO_SUPPORT
-    #define SOFTWARE_SPI                          // Use soft SPI for onboard SD
-    #define SDSS                     SDIO_D3_PIN
-    #define SD_SCK_PIN               SDIO_CK_PIN
-    #define SD_MISO_PIN              SDIO_D0_PIN
-    #define SD_MOSI_PIN             SDIO_CMD_PIN
-  #endif
-#endif
+// Neopixel Rings
+#define NEOPIXEL_PIN                        PC7
+#define NEOPIXEL2_PIN                       PC8
