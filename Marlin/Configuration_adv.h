e62486a6109f0ff9c22140d717c6a73e484d19e2
charlespick <17918019+charlespick@users.noreply.github.com>
2021-07-28 14:09:33 -0700

    ✨ M76 Host Pause Feature (#21738)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3822,6 +3822,7 @@
  */
 //#define HOST_ACTION_COMMANDS
 #if ENABLED(HOST_ACTION_COMMANDS)
+  //#define HOST_PAUSE_M76
   //#define HOST_PROMPT_SUPPORT
   //#define HOST_START_MENU_ITEM  // Add a menu item that tells the host to start
 #endif
