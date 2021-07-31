3b15a27772b53f682d1c2f3866dbd6a9c204d9e6
ldursw <37294448+ldursw@users.noreply.github.com>
2021-07-30 22:38:26 -0300

    Update stm32_serialbuffer.py

--- buildroot/share/PlatformIO/scripts/stm32_serialbuffer.py
+++ buildroot/share/PlatformIO/scripts/stm32_serialbuffer.py
@@ -15,14 +15,44 @@ Import("env")
 # or the user-configured one, whichever is higher.
 #
 # Marlin has 128 and 32 as default values for RX_BUFFER_SIZE and
-# TX_BUFFER_SIZE respectively. We use the highest value.
+# TX_BUFFER_SIZE respectively. We use the highest value (128/64).
+#
+# If the specific flags SERIAL_*X_BUFFER_SIZE, USART_*X_BUF_SIZE,
+# or MF_*X_BUFFER_SIZE are set, then the script will use these
+# values as minimum.
+build_flags = env.ParseFlags(env.get('BUILD_FLAGS'))["CPPDEFINES"]
 mf = env["MARLIN_FEATURES"]
-rxBuf = str(max(128, int(mf["RX_BUFFER_SIZE"]) if "RX_BUFFER_SIZE" in mf else 0))
-txBuf = str(max(64, int(mf["TX_BUFFER_SIZE"]) if "TX_BUFFER_SIZE" in mf else 0))
 
-build_flags = env.get('BUILD_FLAGS')
-build_flags.append("-DSERIAL_RX_BUFFER_SIZE=" + rxBuf)
-build_flags.append("-DSERIAL_TX_BUFFER_SIZE=" + txBuf)
-build_flags.append("-DUSART_RX_BUF_SIZE=" + rxBuf)
-build_flags.append("-DUSART_TX_BUF_SIZE=" + txBuf)
-env.Replace(BUILD_FLAGS=build_flags)
+def getBuildFlagValue(name):
+	for flag in build_flags:
+		if isinstance(flag, list) and flag[0] == name:
+			return flag[1]
+
+	return None
+
+def getInternalSize(side):
+	return getBuildFlagValue(f"MF_{side}_BUFFER_SIZE") or \
+		getBuildFlagValue(f"SERIAL_{side}_BUFFER_SIZE") or \
+		getBuildFlagValue(f"USART_{side}_BUF_SIZE")
+
+def getBufferSize(side, default):
+	internal = int(getInternalSize(side) or default)
+	flag = side + "_BUFFER_SIZE"
+	if flag in mf:
+		return max(int(mf[flag]), internal)
+	else:
+		return internal
+
+def tryAddFlag(name, value):
+	if getBuildFlagValue(name) is not None:
+		return
+
+	env.Append(BUILD_FLAGS=[f"-D{name}={value}"])
+
+rxBuf = getBufferSize("RX", 128)
+txBuf = getBufferSize("TX", 64)
+
+tryAddFlag("SERIAL_RX_BUFFER_SIZE", rxBuf)
+tryAddFlag("SERIAL_TX_BUFFER_SIZE", txBuf)
+tryAddFlag("USART_RX_BUF_SIZE", rxBuf)
+tryAddFlag("USART_TX_BUF_SIZE", txBuf)
