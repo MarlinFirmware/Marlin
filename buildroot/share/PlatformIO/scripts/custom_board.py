b4710dc296c4812d9f3fad45e44928e467cfc0b3
Scott Lahteine <github@thinkyhead.com>
2021-08-01 02:24:08 -0500

    try this trick and spin it, yeah

--- buildroot/share/PlatformIO/scripts/custom_board.py
+++ buildroot/share/PlatformIO/scripts/custom_board.py
@@ -3,7 +3,6 @@
 #
 # - For build.address replace VECT_TAB_ADDR to relocate the firmware
 # - For build.ldscript use one of the linker scripts in buildroot/share/PlatformIO/ldscripts
-# - For build.rename simply rename the firmware file.
 #
 import marlin
 board = marlin.env.BoardConfig()
@@ -15,12 +14,3 @@ if address:
 ldscript = board.get("build.ldscript", "")
 if ldscript:
 	marlin.custom_ld_script(ldscript)
-
-rename_to = board.get("build.rename", "")
-if rename_to:
-	def rename_target(source, target, env):
-		import os, shutil
-		firmware = os.path.join(target[0].dir.path, rename_to)
-		shutil.copy(target[0].path, firmware)
-	marlin.add_post_action(rename_target)
-
