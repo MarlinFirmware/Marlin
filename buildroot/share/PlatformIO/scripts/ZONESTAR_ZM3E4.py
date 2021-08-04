313e577847397c5b471b4e8dddfb1f0c34325b93
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-08-03 19:22:31 -0500

    Simplification - level 1

--- buildroot/share/PlatformIO/scripts/ZONESTAR_ZM3E4.py
+++ buildroot/share/PlatformIO/scripts/ZONESTAR_ZM3E4.py
@@ -1,17 +1,4 @@
-import os
-Import("env")
+import marlin
 
-# Relocate firmware from 0x08000000 to 0x08005000
-for define in env['CPPDEFINES']:
-    if define[0] == "VECT_TAB_ADDR":
-        env['CPPDEFINES'].remove(define)
-env['CPPDEFINES'].append(("VECT_TAB_ADDR", "0x08005000"))
-
-custom_ld_script = os.path.abspath("buildroot/share/PlatformIO/ldscripts/ZONESTAR_ZM3E4.ld")
-for i, flag in enumerate(env["LINKFLAGS"]):
-    if "-Wl,-T" in flag:
-        env["LINKFLAGS"][i] = "-Wl,-T" + custom_ld_script
-    elif flag == "-T":
-        env["LINKFLAGS"][i + 1] = custom_ld_script
-
-
+marlin.relocate_firmware("0x08005000")
+marlin.custom_ld_script("ZONESTAR_ZM3E4.ld")
