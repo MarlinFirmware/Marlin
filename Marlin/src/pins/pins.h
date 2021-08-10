6ee20ab18fe2ae9e13877461f5e91f4b14b9280a
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2021-08-10 08:23:45 +0100

    Specify env in pins.h file (new Marlin requirement)

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -644,7 +644,7 @@
 #elif MB(TH3D_EZBOARD_LITE_V2)
   #include "stm32f4/pins_TH3D_EZBOARD_LITE_V2.h" // STM32F4                               env:TH3D_EZBoard_Lite_V2
 #elif MB(INDEX_REV03)
-  #include "stm32f4/pins_INDEX_REV03.h"         // STM32F4
+  #include "stm32f4/pins_INDEX_REV03.h"         // STM32F4                                env:Index_Mobo_Rev03
 
 //
 // ARM Cortex M7
