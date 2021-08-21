e0d40530a1f52c2e152eea7c1fd49f320076d3ab
Scott Lahteine <github@thinkyhead.com>
2021-08-21 14:59:59 -0500

    Format

--- Marlin/src/pins/stm32f4/pins_RUMBA32_BTT.h
+++ Marlin/src/pins/stm32f4/pins_RUMBA32_BTT.h
@@ -27,7 +27,7 @@
 
 #pragma once
 
-#define BOARD_INFO_NAME "RUMBA32"
+#define BOARD_INFO_NAME "RUMBA32 (BTT)"
 
 #if NO_EEPROM_SELECTED
   #define I2C_EEPROM
@@ -42,23 +42,30 @@
 
 #include "pins_RUMBA32_common.h"
 
-#define SERVO0_PIN                        PA15 // Pin is not broken out, is a test point only.
+#define X_MIN_PIN                           PB12
+
+#define SERVO0_PIN                          PA15  // Pin is not broken out, is a test point only.
 #undef BTN_PIN
 
 #if HAS_TMC_UART
   //
-  // TMC2208/TMC2209 stepper drivers - Software Serial is used according to below pins
+  // TMC2208/TMC2209 Software Serial
   //
-  #define X_SERIAL_TX_PIN                 PC14 // BTT Rumba32 only uses 1 pin for uart
-  #define X_SERIAL_RX_PIN                 PC14
-  #define Y_SERIAL_TX_PIN                 PE4
-  #define Y_SERIAL_RX_PIN                 PE4
-  #define Z_SERIAL_TX_PIN                 PE0
-  #define Z_SERIAL_RX_PIN                 PE0
-  #define E0_SERIAL_TX_PIN                PC13
-  #define E0_SERIAL_RX_PIN                PC13
-  #define E1_SERIAL_TX_PIN                PD5
-  #define E1_SERIAL_RX_PIN                PD5
-  #define E2_SERIAL_TX_PIN                PD1
-  #define E2_SERIAL_RX_PIN                PD1
+  #define X_SERIAL_TX_PIN                   PC14  // BTT Rumba32 only uses 1 pin for UART
+  #define X_SERIAL_RX_PIN        X_SERIAL_TX_PIN
+
+  #define Y_SERIAL_TX_PIN                   PE4
+  #define Y_SERIAL_RX_PIN        Y_SERIAL_TX_PIN
+
+  #define Z_SERIAL_TX_PIN                   PE0
+  #define Z_SERIAL_RX_PIN        Z_SERIAL_TX_PIN
+
+  #define E0_SERIAL_TX_PIN                  PC13
+  #define E0_SERIAL_RX_PIN      E0_SERIAL_TX_PIN
+
+  #define E1_SERIAL_TX_PIN                  PD5
+  #define E1_SERIAL_RX_PIN      E1_SERIAL_TX_PIN
+
+  #define E2_SERIAL_TX_PIN                  PD1
+  #define E2_SERIAL_RX_PIN      E2_SERIAL_TX_PIN
 #endif
