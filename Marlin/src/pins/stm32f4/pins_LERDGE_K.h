97c2fe4389e1b1a44b311d2418ff3be606d1b453
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2021-08-29 17:46:58 +0200

    LerdgeK: Fix commented CS pins, same ones as UART
    on v1.0.1, but i think v2 just use .1" connectors instead of SH1.0 (1mm)

--- Marlin/src/pins/stm32f4/pins_LERDGE_K.h
+++ Marlin/src/pins/stm32f4/pins_LERDGE_K.h
@@ -72,42 +72,42 @@
 #define X_DIR_PIN                           PB10
 #define X_ENABLE_PIN                        PG0
 //#ifndef X_CS_PIN
-//  #define X_CS_PIN                        PE0
+//  #define X_CS_PIN                        PB2
 //#endif
 
 #define Y_STEP_PIN                          PF14
 #define Y_DIR_PIN                           PF15
 #define Y_ENABLE_PIN                        PF13
 //#ifndef Y_CS_PIN
-//  #define Y_CS_PIN                        PE1
+//  #define Y_CS_PIN                        PE2
 //#endif
 
 #define Z_STEP_PIN                          PF11
 #define Z_DIR_PIN                           PF12
 #define Z_ENABLE_PIN                        PC5
 //#ifndef Z_CS_PIN
-//  #define Z_CS_PIN                        PE2
+//  #define Z_CS_PIN                        PE3
 //#endif
 
 #define E0_STEP_PIN                         PC14
 #define E0_DIR_PIN                          PC13
 #define E0_ENABLE_PIN                       PC15
 //#ifndef E0_CS_PIN
-//  #define E0_CS_PIN                       PE3
+//  #define E0_CS_PIN                       PE4
 //#endif
 
 #define E1_STEP_PIN                         PF1
 #define E1_DIR_PIN                          PF0
 #define E1_ENABLE_PIN                       PF2
 //#ifndef E1_CS_PIN
-//  #define E1_CS_PIN                       PE4
+//  #define E1_CS_PIN                       PE1
 //#endif
 
 //#define E2_STEP_PIN                       PF4  // best guess
 //#define E2_DIR_PIN                        PF3  // best guess
 //#define E2_ENABLE_PIN                     PF5  // best guess
 //#ifndef E2_CS_PIN
-//  #define E2_CS_PIN                       PB2  // best guess
+//  #define E2_CS_PIN                       PE0
 //#endif
 
 #if HAS_TMC_UART
