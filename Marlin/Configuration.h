9e18a543fa02bb103fd2ec35e7a26085227ce720
Thomas White <TomW1605@users.noreply.github.com>
2021-09-03 12:30:24 +0800

    ✨ Homing submenu option (#22692)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2159,6 +2159,7 @@
 // Add individual axis homing items (Home X, Home Y, and Home Z) to the LCD menu.
 //
 //#define INDIVIDUAL_AXIS_HOMING_MENU
+//#define INDIVIDUAL_AXIS_HOMING_SUBMENU
 
 //
 // SPEAKER/BUZZER
