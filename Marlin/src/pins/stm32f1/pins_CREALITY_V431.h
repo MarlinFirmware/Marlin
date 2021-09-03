da090648c38b8708599656e3ad684cf6b9a043e8
Chico <jjjm6000@gmail.com>
2021-09-03 14:32:22 +0900

    🐛 Fix Creality v4.3.1 pins

--- Marlin/src/pins/stm32f1/pins_CREALITY_V431.h
+++ Marlin/src/pins/stm32f1/pins_CREALITY_V431.h
@@ -31,10 +31,7 @@
 //
 // Steppers
 //
-#define X_STEP_PIN                        PB8
-#define X_DIR_PIN                         PB7
-
-#define Y_STEP_PIN                        PC2
-#define Y_DIR_PIN                         PB9
+#define E0_STEP_PIN                         PB3
+#define E0_DIR_PIN                          PB4
 
 #include "pins_CREALITY_V4.h"
