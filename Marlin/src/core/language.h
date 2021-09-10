6b5eec3d89c01e1bef254ac43f6dc311d240a521
Eryone <40707672+Eryone@users.noreply.github.com>
2021-09-10 11:58:16 +0800

    Update language.h

--- Marlin/src/core/language.h
+++ Marlin/src/core/language.h
@@ -92,7 +92,7 @@
   #define MACHINE_UUID DEFAULT_MACHINE_UUID
 #endif
 
-#define MARLIN_WEBSITE_URL "marlinfw.org"
+#define MARLIN_WEBSITE_URL "www.eryone.com"
 
 //#if !defined(STRING_SPLASH_LINE3) && defined(WEBSITE_URL)
 //  #define STRING_SPLASH_LINE3 WEBSITE_URL
