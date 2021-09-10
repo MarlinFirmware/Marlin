bb8e4d6d9392ba37d6981064eeff940c0a69e570
Eryone <40707672+Eryone@users.noreply.github.com>
2021-09-10 11:58:38 +0800

    Update Version.h

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -97,7 +97,7 @@
    * documentation about a specific Marlin release. Displayed in the Info Menu.
    */
 #ifndef WEBSITE_URL
-  #define WEBSITE_URL "marlinfw.org"
+  #define WEBSITE_URL "www.eryone.com"
 #endif
 
 /**
