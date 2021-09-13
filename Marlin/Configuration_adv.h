033043218e6a55da72631aee6f2fc28f000f261e
Scott Lahteine <github@thinkyhead.com>
2021-09-13 18:46:30 -0500

    🔖 Configurations version 02000902

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -30,7 +30,7 @@
  *
  * Basic settings can be found in Configuration.h
  */
-#define CONFIGURATION_ADV_H_VERSION 02000901
+#define CONFIGURATION_ADV_H_VERSION 02000902
 
 //===========================================================================
 //============================= Thermal Settings ============================
