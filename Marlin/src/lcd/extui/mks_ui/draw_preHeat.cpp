27d48cdc64f759b6c0f6dfe8da095eddab18a14e
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-09-28 20:18:36 -0500

    Update draw_preHeat.cpp

--- Marlin/src/lcd/extui/mks_ui/draw_preHeat.cpp
+++ Marlin/src/lcd/extui/mks_ui/draw_preHeat.cpp
@@ -158,20 +158,16 @@ static void event_handler(lv_obj_t *obj, lv_event_t event) {
       draw_return_ui();
       break;
     case ID_P_ABS:
-      if (uiCfg.curTempType == 0) {
+      if (uiCfg.curTempType == 0)
         thermalManager.setTargetHotend(PREHEAT_2_TEMP_HOTEND, 0);
-      }
-      else if (uiCfg.curTempType == 1) {
+      else if (uiCfg.curTempType == 1)
         thermalManager.setTargetBed(PREHEAT_2_TEMP_BED);
-      }
       break;
     case ID_P_PLA:
-      if (uiCfg.curTempType == 0) {
+      if (uiCfg.curTempType == 0)
         thermalManager.setTargetHotend(PREHEAT_1_TEMP_HOTEND, 0);
-      }
-      else if (uiCfg.curTempType == 1) {
+      else if (uiCfg.curTempType == 1)
         thermalManager.setTargetBed(PREHEAT_1_TEMP_BED);
-      }
       break;
   }
 }
