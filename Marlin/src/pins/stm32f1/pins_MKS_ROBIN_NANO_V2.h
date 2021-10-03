2a4ee1a482278abb830c0f5180bfa7571c00c9f7
Scott Lahteine <github@thinkyhead.com>
2021-10-02 21:54:07 -0500

    MKS Robin pins updates

--- Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_V2.h
+++ Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_V2.h
@@ -336,19 +336,6 @@
     #ifndef BEEPER_PIN
       #define BEEPER_PIN                    -1
     #endif
-  
-  #elif ENABLED(MKS_MINI_12864_V3)
-    #define DOGLCD_CS                       PD13
-    #define DOGLCD_A0                       PC6
-    #define LCD_PINS_DC                DOGLCD_A0
-    #define LCD_BACKLIGHT_PIN               -1
-    #define LCD_RESET_PIN                   PE14
-    #define NEOPIXEL_PIN                    PE15
-    #define DOGLCD_MOSI                     PA7
-    #define DOGLCD_SCK                      PA5
-    #if SD_CONNECTION_IS(ONBOARD)
-      #define FORCE_SOFT_SPI
-    #endif
 
   #elif ENABLED(MKS_MINI_12864_V3)
     #define DOGLCD_CS                       PD13
