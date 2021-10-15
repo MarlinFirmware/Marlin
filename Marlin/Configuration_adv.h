50c2c9e0ac1ce5fefbdadc89050d3348b1839eb5
aalku <aalku7@gmail.com>
2021-10-15 12:30:12 +0200

    Default HOST_SHUTDOWN disabled

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3838,7 +3838,7 @@
   //#define HOST_PAUSE_M76
   //#define HOST_PROMPT_SUPPORT
   //#define HOST_START_MENU_ITEM  // Add a menu item that tells the host to start
-  #define HOST_SHUTDOWN // Action menu item to tell the host to shutdown
+  //#define HOST_SHUTDOWN // Action menu item to tell the host to shutdown
 #endif
 
 /**
