bc567e7b4939f3abb75a72c313824e6abf0fb07c
Scott Lahteine <github@thinkyhead.com>
2021-10-17 16:56:01 -0500

    🔧 Safety feature warnings

--- Marlin/src/inc/Warnings.cpp
+++ Marlin/src/inc/Warnings.cpp
@@ -35,6 +35,23 @@
   #warning "WARNING! Disable MARLIN_DEV_MODE for the final build!"
 #endif
 
+// Safety Features
+#if DISABLED(USE_WATCHDOG)
+  #warning "Safety Alert! Enable USE_WATCHDOG for the final build!"
+#endif
+#if HAS_HOTEND && DISABLED(THERMAL_PROTECTION_HOTENDS)
+  #warning "Safety Alert! Enable THERMAL_PROTECTION_HOTENDS for the final build!"
+#endif
+#if HAS_HEATED_BED && DISABLED(THERMAL_PROTECTION_BED)
+  #warning "Safety Alert! Enable THERMAL_PROTECTION_BED for the final build!"
+#endif
+#if HAS_HEATED_CHAMBER && DISABLED(THERMAL_PROTECTION_CHAMBER)
+  #warning "Safety Alert! Enable THERMAL_PROTECTION_CHAMBER for the final build!"
+#endif
+#if HAS_COOLER && DISABLED(THERMAL_PROTECTION_COOLER)
+  #warning "Safety Alert! Enable THERMAL_PROTECTION_COOLER for the final build!"
+#endif
+
 #if NONE(HAS_RESUME_CONTINUE, HOST_PROMPT_SUPPORT)
   #warning "Your Configuration provides no method to acquire user feedback!"
 #endif
