af08f16efc8b31f2ae66672ac0df8dedbabdc163
Scott Lahteine <github@thinkyhead.com>
2021-10-17 20:24:41 -0500

    🚸 Tweak MKS UI G-code console

--- Marlin/src/lcd/extui/mks_ui/draw_keyboard.cpp
+++ Marlin/src/lcd/extui/mks_ui/draw_keyboard.cpp
@@ -164,13 +164,13 @@ static void lv_kb_event_cb(lv_obj_t *kb, lv_event_t event) {
           draw_return_ui();
           break;
         case GCodeCommand:
-          if (!queue.ring_buffer.full(3)) {
-            // Hook anything that goes to the serial port
+          if (ret_ta_txt[0] && !queue.ring_buffer.full(3)) {
+            // Hook for the next bytes to arrive from the serial port
             MYSERIAL1.setHook(lv_serial_capt_hook, lv_eom_hook, 0);
-            queue.enqueue_one_now(ret_ta_txt);
+            // Run the command as soon as possible
+            queue.inject(ret_ta_txt);
           }
-          lv_clear_keyboard();
-          // draw_return_ui is called in the end of message hook
+          goto_previous_ui();
           break;
         default: break;
       }
