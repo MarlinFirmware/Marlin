c95e14d41847342cecc88944e514952d3edd51b6
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-10-25 00:37:35 -0500

    Update pins_CREALITY_V4.h

--- Marlin/src/pins/stm32f1/pins_CREALITY_V4.h
+++ Marlin/src/pins/stm32f1/pins_CREALITY_V4.h
@@ -135,10 +135,10 @@
 // Heaters / Fans
 //
 #ifndef HEATER_0_PIN
-  #define HEATER_0_PIN                        PA1   // HEATER1
+  #define HEATER_0_PIN                      PA1   // HEATER1
 #endif
 #ifndef HEATER_BED_PIN
-  #define HEATER_BED_PIN                      PA2   // HOT BED
+  #define HEATER_BED_PIN                    PA2   // HOT BED
 #endif
 #ifndef FAN_PIN
   #define FAN_PIN                           PA0   // FAN
