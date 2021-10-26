9b1a5c5435f5acf913c64da30cc6c7b05c40d317
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2021-10-26 14:08:20 -0700

    Add Artillery Ruby (STM32F401RCT6)

---
+++ buildroot/share/PlatformIO/scripts/artillery_ruby.py
@@ -0,0 +1,10 @@
+Import("env")
+from os.path import join
+
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(" ".join([
+        "$OBJCOPY", "-O", "ihex", "-R", ".eeprom",
+        "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.hex"
+    ]), "Building $BUILD_DIR/${PROGNAME}.hex")
+)
