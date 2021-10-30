7b9e01eb2bd03564ad667746637d8f33899ad5b5
aalku <aalku7@gmail.com>
2021-10-30 07:17:20 +0200

    ✨ Shutdown Host Action (#22908)
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -745,6 +745,8 @@ namespace Language_en {
   LSTR MSG_SD_CARD                        = _UxGT("SD Card");
   LSTR MSG_USB_DISK                       = _UxGT("USB Disk");
 
+  LSTR MSG_HOST_SHUTDOWN                  = _UxGT("Host Shutdown");
+
   // These strings can be the same in all languages
   LSTR MSG_MARLIN                         = _UxGT("Marlin");
   LSTR MSG_SHORT_DAY                      = _UxGT("d"); // One character only
