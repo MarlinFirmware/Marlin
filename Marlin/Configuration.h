6f59d8171f701cbeacf687937de1b0d6a68f6711
Scott Lahteine <github@thinkyhead.com>
2021-10-29 20:42:52 -0500

    🔧 Configuration version 02000903

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -35,7 +35,7 @@
  *
  * Advanced settings can be found in Configuration_adv.h
  */
-#define CONFIGURATION_H_VERSION 02000902
+#define CONFIGURATION_H_VERSION 02000903
 
 //===========================================================================
 //============================= Getting Started =============================
