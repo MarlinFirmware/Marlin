296a6137cd06ce06f062767ab1b59f99ee95a094
tombrazier <68918209+tombrazier@users.noreply.github.com>
2021-11-01 23:03:50 +0000

    🚸 More flexible Probe Temperature Compensation (#23033)

--- Marlin/src/inc/Conditionals_adv.h
+++ Marlin/src/inc/Conditionals_adv.h
@@ -550,6 +550,20 @@
   #endif
 #endif
 
+// Probe Temperature Compensation
+#if !TEMP_SENSOR_PROBE
+  #undef PTC_PROBE
+#endif
+#if !TEMP_SENSOR_BED
+  #undef PTC_BED
+#endif
+#if !HAS_EXTRUDERS
+  #undef PTC_HOTEND
+#endif
+#if ANY(PTC_PROBE, PTC_BED, PTC_HOTEND)
+  #define HAS_PTC 1
+#endif
+
 // Let SD_FINISHED_RELEASECOMMAND stand in for SD_FINISHED_STEPPERRELEASE
 #if ENABLED(SD_FINISHED_STEPPERRELEASE)
   #ifndef SD_FINISHED_RELEASECOMMAND
