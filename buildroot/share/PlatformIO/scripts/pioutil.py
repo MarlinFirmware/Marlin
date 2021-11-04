66a6865820765ce086e0c8064106c637ac1c2fa1
Scott Lahteine <github@thinkyhead.com>
2021-11-04 04:42:37 -0500

    etc

--- buildroot/share/PlatformIO/scripts/pioutil.py
+++ buildroot/share/PlatformIO/scripts/pioutil.py
@@ -4,6 +4,5 @@
 
 # Detect that 'vscode init' is running
 def is_vscode_init():
-	#print("is_vscode_init")
 	from SCons.Script import COMMAND_LINE_TARGETS
 	return "idedata" in COMMAND_LINE_TARGETS or "_idedata" in COMMAND_LINE_TARGETS
