fc2020c6ecc7d731448509012a41d6ff499419bd
Robby Candra <robbycandra.mail@gmail.com>
2021-11-04 17:28:42 +0700

    🔨 Fix IntelliSense / PIO conflicts (#23058)
    Co-authored-by: Scott Lahteine <github@thinkyhead.com>

--- buildroot/share/PlatformIO/scripts/lerdge.py
+++ buildroot/share/PlatformIO/scripts/lerdge.py
@@ -1,47 +1,49 @@
 #
-# buildroot/share/PlatformIO/scripts/lerdge.py
+# lerdge.py
 # Customizations for Lerdge build environments:
 #   env:LERDGEX  env:LERDGEX_usb_flash_drive
 #   env:LERDGES  env:LERDGES_usb_flash_drive
 #   env:LERDGEK  env:LERDGEK_usb_flash_drive
 #
-import os,marlin
-Import("env")
+import pioutil
+if pioutil.is_pio_build():
+	import os,marlin
+	Import("env")
 
-from SCons.Script import DefaultEnvironment
-board = DefaultEnvironment().BoardConfig()
+	from SCons.Script import DefaultEnvironment
+	board = DefaultEnvironment().BoardConfig()
 
-def encryptByte(byte):
-	byte = 0xFF & ((byte << 6) | (byte >> 2))
-	i = 0x58 + byte
-	j = 0x05 + byte + (i >> 8)
-	byte = (0xF8 & i) | (0x07 & j)
-	return byte
+	def encryptByte(byte):
+		byte = 0xFF & ((byte << 6) | (byte >> 2))
+		i = 0x58 + byte
+		j = 0x05 + byte + (i >> 8)
+		byte = (0xF8 & i) | (0x07 & j)
+		return byte
 
-def encrypt_file(input, output_file, file_length):
-	input_file = bytearray(input.read())
-	for i in range(len(input_file)):
-		input_file[i] = encryptByte(input_file[i])
-	output_file.write(input_file)
+	def encrypt_file(input, output_file, file_length):
+		input_file = bytearray(input.read())
+		for i in range(len(input_file)):
+			input_file[i] = encryptByte(input_file[i])
+		output_file.write(input_file)
 
-# Encrypt ${PROGNAME}.bin and save it with the name given in build.encrypt
-def encrypt(source, target, env):
-	fwpath = target[0].path
-	enname = board.get("build.encrypt")
-	print("Encrypting %s to %s" % (fwpath, enname))
-	fwfile = open(fwpath, "rb")
-	enfile = open(target[0].dir.path + "/" + enname, "wb")
-	length = os.path.getsize(fwpath)
+	# Encrypt ${PROGNAME}.bin and save it with the name given in build.encrypt
+	def encrypt(source, target, env):
+		fwpath = target[0].path
+		enname = board.get("build.encrypt")
+		print("Encrypting %s to %s" % (fwpath, enname))
+		fwfile = open(fwpath, "rb")
+		enfile = open(target[0].dir.path + "/" + enname, "wb")
+		length = os.path.getsize(fwpath)
 
-	encrypt_file(fwfile, enfile, length)
+		encrypt_file(fwfile, enfile, length)
 
-	fwfile.close()
-	enfile.close()
-	os.remove(fwpath)
+		fwfile.close()
+		enfile.close()
+		os.remove(fwpath)
 
-if 'encrypt' in board.get("build").keys():
-	if board.get("build.encrypt") != "":
-		marlin.add_post_action(encrypt)
-else:
-	print("LERDGE builds require output file via board_build.encrypt = 'filename' parameter")
-	exit(1)
+	if 'encrypt' in board.get("build").keys():
+		if board.get("build.encrypt") != "":
+			marlin.add_post_action(encrypt)
+	else:
+		print("LERDGE builds require output file via board_build.encrypt = 'filename' parameter")
+		exit(1)
--- buildroot/share/PlatformIO/scripts/robin.py
+++ buildroot/share/PlatformIO/scripts/robin.py
@@ -1,12 +1,14 @@
 #
-# buildroot/share/PlatformIO/scripts/robin.py
+# robin.py
 #
-import marlin
 
 # Apply customizations for a MKS Robin
 def prepare(address, ldname, fwname):
-	def encrypt(source, target, env):
-		marlin.encrypt_mks(source, target, env, fwname)
-	marlin.relocate_firmware(address)
-	marlin.custom_ld_script(ldname)
-	marlin.add_post_action(encrypt);
+	import pioutil
+	if pioutil.is_pio_build():
+		import marlin
+		def encrypt(source, target, env):
+			marlin.encrypt_mks(source, target, env, fwname)
+		marlin.relocate_firmware(address)
+		marlin.custom_ld_script(ldname)
+		marlin.add_post_action(encrypt);
