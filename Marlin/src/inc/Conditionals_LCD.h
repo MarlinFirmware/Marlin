aea556354f0e264d5923e07566384e56968804db
JohnR <john@cirtech.co.uk>
2021-11-16 18:55:43 +0000

    Add support for Markforged YX kinematics

--- Marlin/src/inc/Conditionals_LCD.h
+++ Marlin/src/inc/Conditionals_LCD.h
@@ -1079,7 +1079,7 @@
     #define CORE_AXIS_2 C_AXIS
   #endif
   #define CORESIGN(n) (ANY(COREYX, COREZX, COREZY) ? (-(n)) : (n))
-#elif ENABLED(MARKFORGED_XY)
+#elif EITHER(MARKFORGED_XY, MARKFORGED_YX)
   // Markforged kinematics
   #define CORE_AXIS_1 A_AXIS
   #define CORE_AXIS_2 B_AXIS
