0e60c8b7e04a6cd2758108bcc80f2ab57deec23c
John Robertson <john@cirtech.co.uk>
2021-11-23 21:24:24 +0000

    ✨ MarkForged YX kinematics (#23163)

--- Marlin/src/inc/Conditionals_post.h
+++ Marlin/src/inc/Conditionals_post.h
@@ -193,7 +193,7 @@
 
 // Calibration codes only for non-core axes
 #if EITHER(BACKLASH_GCODE, CALIBRATION_GCODE)
-  #if EITHER(IS_CORE, MARKFORGED_XY)
+  #if ANY(IS_CORE, MARKFORGED_XY, MARKFORGED_YX)
     #define CAN_CALIBRATE(A,B) (_AXIS(A) == B)
   #else
     #define CAN_CALIBRATE(A,B) true
