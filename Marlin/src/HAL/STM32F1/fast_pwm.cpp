a1edf3e946dca7e34b647bf4c9cba33b601c5c2c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-11-29 15:41:41 -0600

    shared timer_and_index_for_pin

--- Marlin/src/HAL/STM32F1/fast_pwm.cpp
+++ Marlin/src/HAL/STM32F1/fast_pwm.cpp
@@ -29,16 +29,22 @@
 
 static uint16_t timer_freq[NR_TIMERS];
 
+inline uint8_t timer_and_index_for_pin(const pin_t pin, timer_dev **timer_dev_ptr) {
+  *timer = PIN_MAP[pin].timer_device;
+  for (uint8_t i = 0; i < NR_TIMERS; i++) if (timer == HAL_get_timer_dev(i))
+    return i;
+  return 0;
+}
+
 void set_pwm_duty(const pin_t pin, const uint16_t v, const uint16_t v_size/*=255*/, const bool invert/*=false*/) {
   if (!PWM_PIN(pin)) return;
-  uint16_t duty = v;
-  if (invert) duty = v_size - duty;
-  timer_dev * const timer = PIN_MAP[pin].timer_device;
-  const uint8_t channel = PIN_MAP[pin].timer_channel;
-  uint8_t timer_index = 0;
-  for (uint8_t i = 0; i < NR_TIMERS; i++) if (timer == HAL_get_timer_dev(i)) { timer_index = i; break; }
-  if (timer_freq[timer_index] == 0) set_pwm_frequency(pin, PWM_FREQUENCY);
 
+  timer_dev *timer;
+  if (timer_freq[timer_and_index_for_pin(pin, &timer)] == 0)
+    set_pwm_frequency(pin, PWM_FREQUENCY);
+
+  const uint8_t channel = PIN_MAP[pin].timer_channel;
+  const uint16_t duty = invert ? v_size - v : v;
   timer_set_compare(timer, channel, duty);
   timer_set_mode(timer, channel, TIMER_PWM); // PWM Output Mode
 }
@@ -46,12 +52,8 @@ void set_pwm_duty(const pin_t pin, const uint16_t v, const uint16_t v_size/*=255
 void set_pwm_frequency(const pin_t pin, int f_desired) {
   if (!PWM_PIN(pin)) return;                    // Don't proceed if no hardware timer
 
-  timer_dev *timer = PIN_MAP[pin].timer_device;
-  uint8_t channel = PIN_MAP[pin].timer_channel;
-
-  uint8_t timer_index = 0;
-  for (uint8_t i = 0; i < NR_TIMERS; i++) if (timer == HAL_get_timer_dev(i)) { timer_index = i; break; }
-  timer_freq[timer_index] = f_desired;
+  timer_dev *timer;
+  timer_freq[timer_and_index_for_pin(pin, &timer)] = f_desired;
 
   // Protect used timers
   if (timer == HAL_get_timer_dev(MF_TIMER_TEMP)) return;
@@ -63,6 +65,7 @@ void set_pwm_frequency(const pin_t pin, int f_desired) {
   if (!(timer->regs.bas->SR & TIMER_CR1_CEN))   // Ensure the timer is enabled
     timer_init(timer);
 
+  const uint8_t channel = PIN_MAP[pin].timer_channel;
   timer_set_mode(timer, channel, TIMER_PWM);
   // Preload (resolution) cannot be equal to duty of 255 otherwise it may not result in digital off or on.
   uint16_t preload = 254;
