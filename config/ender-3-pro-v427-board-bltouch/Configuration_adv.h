857f36e082e1e7d85eba7f354b4a449ec08ebb7e
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-12-19 20:48:26 +0100

    Fan kickstart time for E3 as well #274

--- config/ender-3-pro-v427-board-bltouch/Configuration_adv.h
+++ config/ender-3-pro-v427-board-bltouch/Configuration_adv.h
@@ -498,7 +498,7 @@
 // When first starting the main fan, run it at full speed for the
 // given number of milliseconds.  This gets the fan spinning reliably
 // before setting a PWM value. (Does not work with software PWM for fan on Sanguinololu)
-//#define FAN_KICKSTART_TIME 100
+#define FAN_KICKSTART_TIME 100
 
 // Some coolers may require a non-zero "off" state.
 //#define FAN_OFF_PWM  1
