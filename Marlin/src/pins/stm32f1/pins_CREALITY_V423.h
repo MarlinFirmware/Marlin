fdae9db5db28cd47eaa1ef09dfc36f73d84eb3d4
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-12-18 18:43:02 -0600

    Update pins_CREALITY_V423.h

--- Marlin/src/pins/stm32f1/pins_CREALITY_V423.h
+++ Marlin/src/pins/stm32f1/pins_CREALITY_V423.h
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2021 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -32,6 +32,6 @@
 // Heaters
 //
 #define HEATER_BED_PIN                      PB10  // HOT BED
-#define BTN_EN1                             PA2  // Rotary Encoder
+#define BTN_EN1                             PA2   // Rotary Encoder
 
 #include "pins_CREALITY_V4.h"
