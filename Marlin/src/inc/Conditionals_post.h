1db84be66aee65ca120b6f9d3203ac0e19699c30
Mike La Spina <mike.laspina@shaw.ca>
2021-12-21 01:26:31 -0600

    🚑️ FAST_PWM_FAN default 1KHz base freq. (#23326)
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/src/inc/Conditionals_post.h
+++ Marlin/src/inc/Conditionals_post.h
@@ -2636,10 +2636,14 @@
 #endif
 
 /**
- * FAST PWM FAN Settings
+ * FAST PWM FAN default PWM frequency
  */
-#if ENABLED(FAST_PWM_FAN) && !defined(FAST_PWM_FAN_FREQUENCY)
-  #define FAST_PWM_FAN_FREQUENCY ((F_CPU) / (2 * 255 * 1)) // Fan frequency default
+#if !defined(FAST_PWM_FAN_FREQUENCY) && ENABLED(FAST_PWM_FAN)
+  #ifdef __AVR__
+    #define FAST_PWM_FAN_FREQUENCY ((F_CPU) / (2 * 255 * 1))
+  #else
+    #define FAST_PWM_FAN_FREQUENCY 1000U
+  #endif
 #endif
 
 /**
