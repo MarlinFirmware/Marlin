5abc941edb0600ddf966bc7f8a9b62f55c25a800
Attila BODY <attila.body@gmail.com>
2021-12-24 00:53:17 +0100

    Fix: Robin Nano v3 MT_DET_#_PINs do not depend on LVGL_UI

--- Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3_common.h
+++ Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3_common.h
@@ -138,11 +138,9 @@
 //
 // Misc. Functions
 //
-#if HAS_TFT_LVGL_UI
-  #define MT_DET_1_PIN                      PA4   // MT_DET
-  #define MT_DET_2_PIN                      PE6
-  #define MT_DET_PIN_STATE                  LOW
-#endif
+#define MT_DET_1_PIN                      PA4   // MT_DET
+#define MT_DET_2_PIN                      PE6
+#define MT_DET_PIN_STATE                  LOW
 
 #ifndef FIL_RUNOUT_PIN
   #define FIL_RUNOUT_PIN            MT_DET_1_PIN
