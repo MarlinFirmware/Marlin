2c12171f46488a31cb5d4d78868892ad2918e298
Attila BODY <attila.body@gmail.com>
2021-12-24 06:57:20 +0100

    🐛 Fix Robin Nano v3 filament runout pins (#23344)
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3_common.h
+++ Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3_common.h
@@ -145,10 +145,10 @@
 #endif
 
 #ifndef FIL_RUNOUT_PIN
-  #define FIL_RUNOUT_PIN            MT_DET_1_PIN
+  #define FIL_RUNOUT_PIN                    PA4
 #endif
 #ifndef FIL_RUNOUT2_PIN
-  #define FIL_RUNOUT2_PIN           MT_DET_2_PIN
+  #define FIL_RUNOUT2_PIN                   PE6
 #endif
 
 #ifndef POWER_LOSS_PIN
