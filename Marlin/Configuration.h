2ce46a258a2392bccac149c77a34edab2a8a5703
Patxi Bretos <franciscojavier.bretos@bq.com>
2021-12-27 12:01:21 +0100

    vuelta atras baudrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -115,7 +115,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 250000
+#define BAUDRATE 115200
 //#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
 
 /**
