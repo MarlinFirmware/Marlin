67c17ecb0abdd20c1cb4e7883c86ed30636a0631
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2022-01-08 20:15:37 +0100

    Longer3D: Keep FAN_SOFT_PWM as common default
    and reduce the fan config restrictions...

--- Marlin/src/pins/stm32f1/pins_LONGER3D_LK.h
+++ Marlin/src/pins/stm32f1/pins_LONGER3D_LK.h
@@ -91,18 +91,18 @@
 #define FAN_PIN                             PA15  // pin 77 (4cm Fan)
 #ifdef MAPLE_STM32F1
   #define FAN_SOFT_PWM_REQUIRED
-  #if ENABLED(FAN_SOFT_PWM) && FAN_MIN_PWM < 35   // Required to avoid issues with heating or STLink
-    #error "FAN_MIN_PWM must be 35 or higher."    // Fan will not start in 1-30 range
+  #if ENABLED(FAN_SOFT_PWM) && FAN_MIN_PWM < 5    // Required to avoid issues with heating or STLink
+    #error "FAN_MIN_PWM must be 5 or higher."     // Fan will not start in 1-30 range
   #endif
-#elif ENABLED(FAST_PWM_FAN)
-  #if FAST_PWM_FAN_FREQUENCY != 31400             // Default 1000 is noisy, max 65K (uint16)
+#elif ENABLED(FAST_PWM_FAN)                       // STM32 HAL required to allow TIMER2 Hardware PWM
+  #if FAST_PWM_FAN_FREQUENCY == 1000              // Default 1000 is noisy, max 65K (uint16)
     #error "FAST_PWM_FAN_FREQUENCY must be set to 31400."
   #endif
   #if FAN_MIN_PWM < 5
     #error "FAN_MIN_PWM must be 5 or higher."
   #endif
 #else
-  #error "FAST_PWM_FAN required to allow TIMER2 Hardware PWM."
+  #define FAN_SOFT_PWM_REQUIRED
 #endif
 
 //#define BEEPER_PIN                        PD13  // pin 60 (Servo PWM output 5V/GND on Board V0G+) made for BL-Touch sensor
