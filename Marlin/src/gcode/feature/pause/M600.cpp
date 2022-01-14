71deee82c3d6f67f69be1a4909f51d2281c6395c
Scott Lahteine <github@thinkyhead.com>
2022-01-14 03:43:14 -0600

    🩹 Followup to extra axes

--- Marlin/src/gcode/feature/pause/M600.cpp
+++ Marlin/src/gcode/feature/pause/M600.cpp
@@ -116,17 +116,14 @@ void GcodeSuite::M600() {
 
   xyz_pos_t park_point NOZZLE_PARK_POINT;
 
-  // Lift Z axis first
-  if (parser.seenval('Z')) park_point.z = parser.linearval('Z');
-
   // Move XY axes to filament change position or given position
   LINEAR_AXIS_CODE(
     if (parser.seenval('X')) park_point.x = parser.linearval('X'),
     if (parser.seenval('Y')) park_point.y = parser.linearval('Y'),
-    NOOP,
-    if (parser.seenval(AXIS4_NAME)) park_point.i = parser.linearval(AXIS4_NAME);
-    if (parser.seenval(AXIS5_NAME)) park_point.j = parser.linearval(AXIS5_NAME);
-    if (parser.seenval(AXIS6_NAME)) park_point.k = parser.linearval(AXIS6_NAME);
+    if (parser.seenval('Z')) park_point.z = parser.linearval('Z'),    // Lift Z axis
+    if (parser.seenval(AXIS4_NAME)) park_point.i = parser.linearval(AXIS4_NAME),
+    if (parser.seenval(AXIS5_NAME)) park_point.j = parser.linearval(AXIS5_NAME),
+    if (parser.seenval(AXIS6_NAME)) park_point.k = parser.linearval(AXIS6_NAME)
   );
 
   #if HAS_HOTEND_OFFSET && NONE(DUAL_X_CARRIAGE, DELTA)
