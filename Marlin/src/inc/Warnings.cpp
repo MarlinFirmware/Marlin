7734aeb0a652749b6abfdb1328b7599b1332a6dc
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2022-01-18 00:30:52 -0800

    🔨 Suppressible CONFIGURATION_EMBEDDING warning (#23545)

--- Marlin/src/inc/Warnings.cpp
+++ Marlin/src/inc/Warnings.cpp
@@ -548,11 +548,11 @@
 //
 // Warn users of potential endstop/DIAG pin conflicts to prevent homing issues when not using sensorless homing
 //
-#if NONE(USE_SENSORLESS, DIAG_JUMPERS_REMOVED)
-  #if ENABLED(USES_DIAG_JUMPERS)
+#if !USE_SENSORLESS
+  #if ENABLED(USES_DIAG_JUMPERS) && DISABLED(DIAG_JUMPERS_REMOVED)
     #warning "Motherboard DIAG jumpers must be removed when SENSORLESS_HOMING is disabled. (Define DIAG_JUMPERS_REMOVED to suppress this warning.)"
-  #elif ENABLED(USES_DIAG_PINS)
-    #warning "Driver DIAG pins must be physically removed unless SENSORLESS_HOMING is enabled. (See https://bit.ly/2ZPRlt0) (Define DIAG_JUMPERS_REMOVED to suppress this warning.)"
+  #elif ENABLED(USES_DIAG_PINS) && DISABLED(DIAG_PINS_REMOVED)
+    #warning "Driver DIAG pins must be physically removed unless SENSORLESS_HOMING is enabled. (See https://bit.ly/2ZPRlt0) (Define DIAG_PINS_REMOVED to suppress this warning.)"
   #endif
 #endif
 
