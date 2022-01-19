496c88db50bd3e0933eaf20e24c716000aab615d
ellensp <530024+ellensp@users.noreply.github.com>
2022-01-20 01:43:25 +1300

    clean up spacing, add banner

--- Marlin/src/HAL/ESP32/u8g_esp32_spi.cpp
+++ Marlin/src/HAL/ESP32/u8g_esp32_spi.cpp
@@ -1,3 +1,26 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2022 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * Copypaste of SAMD51 HAL developed by Giuliano Zaro (AKA GMagician)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 #ifdef ARDUINO_ARCH_ESP32
 
 #include "../../inc/MarlinConfigPre.h"
@@ -8,16 +31,15 @@
 #include "../shared/HAL_SPI.h"
 #include "SPI.h"
 
-
 static SPISettings spiConfig;
 static uint8_t msgInitCount = 2; // Ignore all messages until 2nd U8G_COM_MSG_INIT
 
-  #define MDOGLCD_MOSI                     23
-  #define MDOGLCD_SCK                      18
-  #define MLCD_RESET_PIN                   0
-  #define MLCD_PINS_DC                     4
-  #define MDOGLCD_CS                       21
-  #define MDOGLCD_A0                       4
+#define MDOGLCD_MOSI                     23
+#define MDOGLCD_SCK                      18
+#define MLCD_RESET_PIN                   0
+#define MLCD_PINS_DC                     4
+#define MDOGLCD_CS                       21
+#define MDOGLCD_A0                       4
 
 #ifndef LCD_SPI_SPEED
   #ifdef SD_SPI_SPEED
@@ -27,19 +49,17 @@ static uint8_t msgInitCount = 2; // Ignore all messages until 2nd U8G_COM_MSG_IN
   #endif
 #endif
 
-uint8_t u8g_eps_hw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
-{
+uint8_t u8g_eps_hw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr){
   if (msgInitCount) {
     if (msg == U8G_COM_MSG_INIT) msgInitCount --;
     if (msgInitCount) return -1;
   }
 
-  switch(msg)
-  {
+  switch(msg){
     case U8G_COM_MSG_STOP:
       break;
-    case U8G_COM_MSG_INIT:
 
+    case U8G_COM_MSG_INIT:
       pinMode(MDOGLCD_CS, OUTPUT);
       pinMode(MDOGLCD_A0, OUTPUT);
       pinMode(MLCD_RESET_PIN, OUTPUT);
@@ -50,39 +70,35 @@ uint8_t u8g_eps_hw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_pt
       u8g_Delay(5);
       spiBegin();
       spiInit(LCD_SPI_SPEED);
-
       break;
 
     case U8G_COM_MSG_ADDRESS:           /* define cmd (arg_val = 0) or data mode (arg_val = 1) */
-        if(arg_val)
-            digitalWrite(MDOGLCD_A0, HIGH);
-        else
-            digitalWrite(MDOGLCD_A0, LOW);
+      if(arg_val) digitalWrite(MDOGLCD_A0, HIGH);
+      else digitalWrite(MDOGLCD_A0, LOW);
       break;
 
     case U8G_COM_MSG_CHIP_SELECT:       /* arg_val == 0 means HIGH level of U8G_PI_CS */
-        digitalWrite(MDOGLCD_CS, arg_val ? LOW : HIGH);
+      digitalWrite(MDOGLCD_CS, arg_val ? LOW : HIGH);
       break;
 
     case U8G_COM_MSG_RESET:
-        digitalWrite(MLCD_RESET_PIN, arg_val);
+      digitalWrite(MLCD_RESET_PIN, arg_val);
       break;
 
     case U8G_COM_MSG_WRITE_BYTE:
       spiSend((uint8_t)arg_val);
-    break;
+      break;
 
     case U8G_COM_MSG_WRITE_SEQ:
       uint8_t *ptr = (uint8_t*) arg_ptr;
-        while (arg_val > 0) {
-          spiSend(*ptr++);
-          arg_val--;
-        }
+      while (arg_val > 0) {
+        spiSend(*ptr++);
+        arg_val--;
+      }
       break;
   }
   return 1;
 }
 
 #endif // MKS_MINI_12864_V3
-
 #endif // ARDUINO_ARCH_ESP32
