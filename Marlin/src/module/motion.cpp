24bd11df4c81506e691ea446e3c6879324364d79
Bob-the-Kuhn <bob.kuhn@att.net>
2022-01-19 12:04:36 -0600

    disable leveling when only moving E

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -409,8 +409,19 @@ void line_to_current_position(const_feedRate_t fr_mm_s/*=feedrate_mm_s*/) {
 #if HAS_EXTRUDERS
   void unscaled_e_move(const_float_t length, const_feedRate_t fr_mm_s) {
     TERN_(HAS_FILAMENT_SENSOR, runout.reset());
+
+    #if HAS_LEVELING
+      bool leveling_local = planner.leveling_active; // save leveling state
+      set_bed_leveling_enabled(false);  // turn off leveling
+    #endif
+
     current_position.e += length / planner.e_factor[active_extruder];
     line_to_current_position(fr_mm_s);
+
+    #if HAS_LEVELING
+      set_bed_leveling_enabled(leveling_local); // restore leveling
+    #endif
+
     planner.synchronize();
   }
 #endif
