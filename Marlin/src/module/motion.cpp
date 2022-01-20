b612a1700eac38c6d9a70abf677b042680ed7284
Scott Lahteine <github@thinkyhead.com>
2022-01-20 06:06:40 -0600

    tweak style

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -411,16 +411,14 @@ void line_to_current_position(const_feedRate_t fr_mm_s/*=feedrate_mm_s*/) {
     TERN_(HAS_FILAMENT_SENSOR, runout.reset());
 
     #if HAS_LEVELING
-      bool leveling_local = planner.leveling_active; // save leveling state
-      set_bed_leveling_enabled(false);  // turn off leveling
+      const bool leveling_was_active = planner.leveling_active;
+      set_bed_leveling_enabled(false);
     #endif
 
     current_position.e += length / planner.e_factor[active_extruder];
     line_to_current_position(fr_mm_s);
 
-    #if HAS_LEVELING
-      set_bed_leveling_enabled(leveling_local); // restore leveling
-    #endif
+    TERN_(HAS_LEVELING, set_bed_leveling_enabled(leveling_was_active));
 
     planner.synchronize();
   }
