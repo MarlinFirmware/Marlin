6ad88612f128a8d27baccb2619ff447d5dd34b2e
GHGiampy <tisgiampy@tiscali.it>
2022-01-21 04:29:00 +0100

    Change leds behavior when CASE_LIGHT_USE_NEOPIXEL

--- Marlin/src/feature/leds/leds.cpp
+++ Marlin/src/feature/leds/leds.cpp
@@ -42,7 +42,7 @@
   #include "pca9533.h"
 #endif
 
-#if ENABLED(CASE_LIGHT_USE_RGB_LED)
+#if EITHER(CASE_LIGHT_USE_RGB_LED, CASE_LIGHT_USE_NEOPIXEL)
   #include "../../feature/caselight.h"
 #endif
 
@@ -95,6 +95,10 @@ void LEDLights::set_color(const LEDColor &incol
       #endif
     #endif
 
+    #if BOTH(CASE_LIGHT_MENU, CASE_LIGHT_USE_NEOPIXEL)
+      // Update brightness only if caselight is ON or switching leds off
+      if (caselight.on || incol.is_off())
+    #endif
     neo.set_brightness(incol.i);
 
     #if ENABLED(NEOPIXEL_IS_SEQUENTIAL)
@@ -106,6 +110,10 @@ void LEDLights::set_color(const LEDColor &incol
       }
     #endif
 
+    #if BOTH(CASE_LIGHT_MENU, CASE_LIGHT_USE_NEOPIXEL)
+      // Update color only if caselight is ON or switching leds off
+      if (caselight.on || incol.is_off())
+    #endif
     neo.set_color(neocolor);
 
   #endif
