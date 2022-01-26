361dab93b387d3b1b5b62da016da0614d457db3d
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-01-25 18:33:30 -0600

    ✨ BOARD_CREALITY_V24S1_301 (#23620)
    Co-Authored-By: Miguel Risco-Castillo <mriscoc@users.noreply.github.com>

--- Marlin/src/inc/Conditionals_LCD.h
+++ Marlin/src/inc/Conditionals_LCD.h
@@ -511,6 +511,8 @@
   #ifndef LCD_SERIAL_PORT
     #if MB(BTT_SKR_MINI_E3_V1_0, BTT_SKR_MINI_E3_V1_2, BTT_SKR_MINI_E3_V2_0, BTT_SKR_MINI_E3_V3_0, BTT_SKR_E3_TURBO)
       #define LCD_SERIAL_PORT 1
+    #elif MB(CREALITY_V24S1_301)
+      #define LCD_SERIAL_PORT 2 // Creality Ender3S1 board
     #else
       #define LCD_SERIAL_PORT 3 // Creality 4.x board
     #endif
