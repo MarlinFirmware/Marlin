064efc68e51f3ef8810bb01f545ab77aa4443dc1
AndyBig <andybig@gmail.com>
2022-01-29 09:02:39 +0300

    Fix building scripts

--- buildroot/share/PlatformIO/scripts/offset_and_rename.py
+++ buildroot/share/PlatformIO/scripts/offset_and_rename.py
@@ -57,9 +57,11 @@ if pioutil.is_pio_build():
 
 		def rename_target(source, target, env):
 			firmware = os.path.join(target[0].dir.path, board.get("build.rename"))
-#			os.rename(target[0].path, firmware)
+			if os.path.exists(firmware):
+			    os.remove(firmware)
+			os.rename(target[0].path, firmware)
 
-			import shutil
-			shutil.copy(target[0].path, firmware)
+#			import shutil
+#			shutil.copy(target[0].path, firmware)
 
 		marlin.add_post_action(rename_target)
