816754ffb222d23a016205335a0bab2c57af3e20
Mike La Spina <mike.laspina@shaw.ca>
2022-02-04 13:33:52 -0600

    🍻 STM32 set_pwm_duty "on/off" for digital pins (#23665)

--- Marlin/src/HAL/STM32F1/fast_pwm.cpp
+++ Marlin/src/HAL/STM32F1/fast_pwm.cpp
@@ -39,16 +39,19 @@ inline uint8_t timer_and_index_for_pin(const pin_t pin, timer_dev **timer_ptr) {
 }
 
 void set_pwm_duty(const pin_t pin, const uint16_t v, const uint16_t v_size/*=255*/, const bool invert/*=false*/) {
-  if (!PWM_PIN(pin)) return;
-
-  timer_dev *timer; UNUSED(timer);
-  if (timer_freq[timer_and_index_for_pin(pin, &timer)] == 0)
-    set_pwm_frequency(pin, PWM_FREQUENCY);
-
-  const uint8_t channel = PIN_MAP[pin].timer_channel;
   const uint16_t duty = invert ? v_size - v : v;
-  timer_set_compare(timer, channel, duty);
-  timer_set_mode(timer, channel, TIMER_PWM); // PWM Output Mode
+  if (PWM_PIN(pin)) {
+    timer_dev *timer; UNUSED(timer);
+    if (timer_freq[timer_and_index_for_pin(pin, &timer)] == 0)
+      set_pwm_frequency(pin, PWM_FREQUENCY);
+    const uint8_t channel = PIN_MAP[pin].timer_channel;
+    timer_set_compare(timer, channel, duty);
+    timer_set_mode(timer, channel, TIMER_PWM); // PWM Output Mode
+  }
+  else {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, duty < v_size / 2 ? LOW : HIGH);
+  }
 }
 
 void set_pwm_frequency(const pin_t pin, const uint16_t f_desired) {
