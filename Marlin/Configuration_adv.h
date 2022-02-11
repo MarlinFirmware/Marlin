baf9f910340d429e2c5d65d7965a05d6a73ddd74
Eric <erowinsky@gmail.com>
2022-02-11 12:56:01 -0700

    disabled PINS_DEEBUG - problem with HAL STM32

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4309,7 +4309,7 @@
 //
 // M43 - display pin status, toggle pins, watch pins, watch endstops & toggle LED, test servo probe
 //
-#define PINS_DEBUGGING
+//#define PINS_DEBUGGING
 
 // Enable Marlin dev mode which adds some special commands
 //#define MARLIN_DEV_MODE
