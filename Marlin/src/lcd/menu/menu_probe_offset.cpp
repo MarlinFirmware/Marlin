36e593f11216f8ab6a594ada1609420e27b6d710
Tom Brazier <tom_github@firstsolo.net>
2022-02-19 16:20:38 +0000

    Fix display of Z offset in Z probe wizard (= is a special character)

--- Marlin/src/lcd/menu/menu_probe_offset.cpp
+++ Marlin/src/lcd/menu/menu_probe_offset.cpp
@@ -62,7 +62,7 @@ void probe_offset_wizard_menu() {
   if (LCD_HEIGHT >= 4)
     STATIC_ITEM(MSG_MOVE_NOZZLE_TO_BED, SS_CENTER|SS_INVERT);
 
-  STATIC_ITEM_P(PSTR("Z="), SS_CENTER, ftostr42_52(current_position.z));
+  STATIC_ITEM_P(PSTR("Z"), SS_CENTER, ftostr42_52(current_position.z));
   STATIC_ITEM(MSG_ZPROBE_ZOFFSET, SS_LEFT, ftostr42_52(calculated_z_offset));
 
   SUBMENU(MSG_MOVE_1MM,  []{ _goto_manual_move_z( 1);    });
