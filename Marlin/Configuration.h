809c62afdcfe95e562aa0975fedb0a11411279a7
Tom Brazier <tom_github@firstsolo.net>
2022-03-01 19:52:45 +0000

    Added G26 mesh validation

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1662,7 +1662,7 @@
   /**
    * Enable the G26 Mesh Validation Pattern tool.
    */
-  //#define G26_MESH_VALIDATION
+  #define G26_MESH_VALIDATION
   #if ENABLED(G26_MESH_VALIDATION)
     #define MESH_TEST_NOZZLE_SIZE    0.4  // (mm) Diameter of primary nozzle.
     #define MESH_TEST_LAYER_HEIGHT   0.2  // (mm) Default layer height for G26.
