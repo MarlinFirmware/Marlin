26eefabf5708a094b526386af20d6f07b190bf4b
Jyers <jyers.m@gmail.com>
2022-03-08 14:55:22 -0500

    Icon File Fix

--- Marlin/src/lcd/e3v2/common/dwin_set.h
+++ Marlin/src/lcd/e3v2/common/dwin_set.h
@@ -26,10 +26,11 @@
 #define Language_Chinese    2
 
 //#define USE_STOCK_DWIN_SET  // Use the Creality stock DWIN_SET instead of Marlin's unified DWIN_SET by The-EG & thinkyhead
+// Yeah no thanks, the stock should be the default
 #ifdef USE_STOCK_DWIN_SET
   #define ICON 9 // 9.ICO
 #else
-  #define ICON 7 // 7.ICO
+  #define ICON 9
 #endif
 
 #ifndef CORP_WEBSITE
