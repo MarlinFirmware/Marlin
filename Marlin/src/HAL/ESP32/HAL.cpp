8bccc11d474c1cd099ba8b870bb7018b1a5b3f01
JohnR <john@cirtech.co.uk>
2022-03-14 17:49:01 +0000

    Revert "Corrects ADC Vref divisor for MKS TinyBee"
    This reverts commit 8259ee9884645f9de550ee20da9bc99ad4de1b9b.

--- Marlin/src/HAL/ESP32/HAL.cpp
+++ Marlin/src/HAL/ESP32/HAL.cpp
@@ -240,12 +240,8 @@ void MarlinHAL::adc_start(const pin_t pin) {
   const adc1_channel_t chan = get_channel(pin);
   uint32_t mv;
   esp_adc_cal_get_voltage((adc_channel_t)chan, &characteristics[attenuations[chan]], &mv);
-  #if MB(MKS_TINYBEE)
-  adc_result = mv * 1023.0 / 2500.0; // TinyBee uses 2.5v VDDA ref
-  #else
-  adc_result = mv * 1023.0 / 3300.0;
-  #endif
-  
+  adc_result = mv * 1023.0 / TERN(MB(MKS_TINYBEE), 2500.0, 3300.0); // TinyBee uses 2.5v VDDA ref
+
   // Change the attenuation level based on the new reading
   adc_atten_t atten;
   if (mv < thresholds[ADC_ATTEN_DB_0] - 100)
