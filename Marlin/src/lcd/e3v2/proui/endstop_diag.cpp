5b7052cd4502affcc0fb12b16bccb2a33a18624d
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-03-24 18:05:42 -0500

    Update endstop_diag.cpp

--- Marlin/src/lcd/e3v2/proui/endstop_diag.cpp
+++ Marlin/src/lcd/e3v2/proui/endstop_diag.cpp
@@ -21,14 +21,14 @@
  */
 
 /**
- * DWIN End Stops diagnostic page for PRO UI
+ * DWIN Endstops diagnostic page for PRO UI
  * Author: Miguel A. Risco-Castillo (MRISCOC)
  * Version: 1.2.2
  * Date: 2022/02/24
  */
 
-
 #include "../../../inc/MarlinConfigPre.h"
+
 #if ENABLED(DWIN_LCD_PROUI)
 
 #include "dwin_defines.h"
