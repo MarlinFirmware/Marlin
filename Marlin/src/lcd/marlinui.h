9324132a401cf0ab3781c9992d607bd4f88ba086
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-03-25 17:09:55 -0500

    🎨 Format, use status macros

--- Marlin/src/lcd/marlinui.h
+++ Marlin/src/lcd/marlinui.h
@@ -39,10 +39,6 @@
   #define HAS_ENCODER_ACTION 1
 #endif
 
-#if HAS_STATUS_MESSAGE
-  #define START_OF_UTF8_CHAR(C) (((C) & 0xC0u) != 0x80U)
-#endif
-
 #if E_MANUAL > 1
   #define MULTI_E_MANUAL 1
 #endif
