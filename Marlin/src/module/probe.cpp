6b55eec9bbc72db2777d3ff3577f2bf1ef13a613
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-03-25 17:04:06 -0500

    🩹 Print English to serial out

--- Marlin/src/module/probe.cpp
+++ Marlin/src/module/probe.cpp
@@ -311,7 +311,7 @@ FORCE_INLINE void probe_specific_action(const bool deploy) {
       FSTR_P const ds_str = deploy ? GET_TEXT_F(MSG_MANUAL_DEPLOY) : GET_TEXT_F(MSG_MANUAL_STOW);
       ui.return_to_status();       // To display the new status message
       ui.set_status(ds_str, 99);
-      SERIAL_ECHOLNF(ds_str);
+      SERIAL_ECHOLNF(deploy ? GET_EN_TEXT_F(MSG_MANUAL_DEPLOY) : GET_EN_TEXT_F(MSG_MANUAL_STOW));
 
       TERN_(HOST_PROMPT_SUPPORT, hostui.prompt_do(PROMPT_USER_CONTINUE, F("Stow Probe"), FPSTR(CONTINUE_STR)));
       TERN_(EXTENSIBLE_UI, ExtUI::onUserConfirmRequired(F("Stow Probe")));
