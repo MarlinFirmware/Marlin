e99104a0040f58477809c9e2b9506fe2bcd94331
ellensp <530024+ellensp@users.noreply.github.com>
2022-03-26 16:44:22 +1300

    ✨ Configurable FREEZE pin state (#23944, #23948)
    Co-Authored-By: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4116,6 +4116,7 @@
 //#define FREEZE_FEATURE
 #if ENABLED(FREEZE_FEATURE)
   //#define FREEZE_PIN 41   // Override the default (KILL) pin here
+  #define FREEZE_STATE LOW  // State of pin indicating freeze
 #endif
 
 /**
