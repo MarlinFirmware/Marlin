b8a1eff963a364f250b4041f97b9def7b08159b9
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-03-25 19:00:42 -0500

    Init FREEZE based on active state

--- Marlin/src/MarlinCore.cpp
+++ Marlin/src/MarlinCore.cpp
@@ -1168,7 +1168,11 @@ void setup() {
 
   #if ENABLED(FREEZE_FEATURE)
     SETUP_LOG("FREEZE_PIN");
-    SET_INPUT_PULLUP(FREEZE_PIN);
+    #if FREEZE_STATE
+      SET_INPUT_PULLDOWN(FREEZE_PIN);
+    #else
+      SET_INPUT_PULLUP(FREEZE_PIN);
+    #endif
   #endif
 
   #if HAS_SUICIDE
