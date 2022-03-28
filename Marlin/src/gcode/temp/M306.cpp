1bd53ca441704dfde680d59cb56a9b20e06fca4c
Tom Brazier <tom_github@firstsolo.net>
2022-03-28 13:42:19 +0100

    Added functionality to M306: report and set constants for MPC

--- Marlin/src/gcode/temp/M306.cpp
+++ Marlin/src/gcode/temp/M306.cpp
@@ -28,13 +28,52 @@
 #include "../../module/temperature.h"
 
 /**
- * M306: MPC autotune
+ * M306: MPC settings and autotune
  *
- * Autotune the active extruder
+ *  A<watts/kelvin>           Ambient heat transfer coefficient (no fan).
+ *  C<joules/kelvin>          Block heat capacity.
+ *  E<extruder>               Extruder number to set. (Default: E0)
+ *  F<watts/kelvin>           Ambient heat transfer coefficient (fan on full).
+ *  P<watts>                  Heater power.
+ *  R<kelvin/second/kelvin>   Sensor responsiveness (= transfer coefficient / heat capcity).
+ *  T                         Autotune the active extruder.
  */
 
 void GcodeSuite::M306() {
-  thermalManager.MPC_autotune();
+  if (parser.seen_test('T')) {
+    thermalManager.MPC_autotune();
+    return;
+  }
+
+  else if (parser.seen("ACFPR")) {
+    const heater_id_t hid = (heater_id_t)parser.intval('E', 0);
+    MPC_t& constants = thermalManager.temp_hotend[hid].constants;
+    constants.heater_power = parser.floatval('P', constants.heater_power);
+    constants.block_heat_capacity = parser.floatval('C', constants.block_heat_capacity);
+    constants.sensor_responsiveness = parser.floatval('R', constants.sensor_responsiveness);
+    constants.ambient_xfer_coeff_fan0 = parser.floatval('A', constants.ambient_xfer_coeff_fan0);
+    #if ENABLED(MPC_INCLUDE_FAN)
+      constants.fan255_adjustment = parser.floatval('F', constants.ambient_xfer_coeff_fan0 + constants.fan255_adjustment) - constants.ambient_xfer_coeff_fan0;
+    #endif
+  }
+
+  HOTEND_LOOP() {
+    SERIAL_ECHOLNPGM("MPC constants for hotend ", e);
+    MPC_t& constants = thermalManager.temp_hotend[e].constants;
+    SERIAL_ECHOLNPGM("Heater power: ", constants.heater_power);
+    SERIAL_ECHOLNPGM("Heatblock heat capacity: ", constants.block_heat_capacity);
+    SERIAL_ECHOPGM("Sensor responsivness: ");
+    SERIAL_PRINT(constants.sensor_responsiveness, 4);
+    SERIAL_EOL();
+    SERIAL_ECHOPGM("Ambient heat transfer coefficient (no fan): ");
+    SERIAL_PRINT(constants.ambient_xfer_coeff_fan0, 4);
+    SERIAL_EOL();
+    #if ENABLED(MPC_INCLUDE_FAN)
+      SERIAL_ECHOPGM("Ambient heat transfer coefficient (full fan): ");
+      SERIAL_PRINT(constants.ambient_xfer_coeff_fan0 + constants.fan255_adjustment, 4);
+      SERIAL_EOL();
+    #endif
+  }
 }
 
 #endif // MPCTEMP
