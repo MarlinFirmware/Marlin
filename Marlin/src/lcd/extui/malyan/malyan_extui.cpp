4ec9af42b8e24cf6ce9f238c9342bc1c11120429
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-03-29 03:38:25 -0500

    💥 Rename ExtUI settings methods

--- Marlin/src/lcd/extui/malyan/malyan_extui.cpp
+++ Marlin/src/lcd/extui/malyan/malyan_extui.cpp
@@ -147,8 +147,8 @@ namespace ExtUI {
   void onStoreSettings(char*) {}
   void onLoadSettings(const char*) {}
   void onPostprocessSettings() {}
-  void onConfigurationStoreWritten(bool) {}
-  void onConfigurationStoreRead(bool) {}
+  void onSettingsStored(bool) {}
+  void onSettingsLoaded(bool) {}
 
   #if HAS_MESH
     void onLevelingStart() {}
