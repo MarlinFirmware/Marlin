4185c931c7bcd413603235a3f63ed03a09df7594
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2022-04-02 14:08:04 +0200

    M906 with UVW axes

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2866,6 +2866,7 @@
     #define U_RSENSE         0.11
     #define U_CHAIN_POS     -1
     //#define U_INTERPOLATE  true
+    //#define U_HOLD_MULTIPLIER 0.5
   #endif
 
   #if AXIS_IS_TMC(V)
@@ -2875,6 +2876,7 @@
     #define V_RSENSE         0.11
     #define V_CHAIN_POS     -1
     //#define V_INTERPOLATE  true
+    //#define V_HOLD_MULTIPLIER 0.5
   #endif
 
   #if AXIS_IS_TMC(W)
@@ -2884,6 +2886,7 @@
     #define W_RSENSE         0.11
     #define W_CHAIN_POS     -1
     //#define W_INTERPOLATE  true
+    //#define W_HOLD_MULTIPLIER 0.5
   #endif
 
   #if AXIS_IS_TMC(E0)
