02dce401ff8b941257bda0a2a5113c047c35d11a
lujios <83166168+lujios@users.noreply.github.com>
2022-04-06 20:01:01 +0200

    Add files via upload

--- Marlin/src/pins/ramps/pins_RAMPS.h
+++ Marlin/src/pins/ramps/pins_RAMPS.h
@@ -83,26 +83,26 @@
 //
 #ifndef X_STOP_PIN
   #ifndef X_MIN_PIN
-  #define X_MIN_PIN                            2 //3  //Lujpins  Debe coincidir con _MAX_PIN al activar SENSORLESS_PROBING 
+    #define X_MIN_PIN                          3
   #endif
   #ifndef X_MAX_PIN
-    #define X_MAX_PIN                          2      //Lujpins  Asociado a diag1 stepper X al activar SENSORLESS_HOMING
+    #define X_MAX_PIN                          2
   #endif
 #endif
 #ifndef Y_STOP_PIN
   #ifndef Y_MIN_PIN
-  #define Y_MIN_PIN                            3 //14 //Lujpins  Debe coincidir con _MAX_PIN al activar SENSORLESS_PROBING  
+    #define Y_MIN_PIN                         14
   #endif
   #ifndef Y_MAX_PIN
-    #define Y_MAX_PIN                          3 //15 //Lujpins  Asociado a diag1 stepper X al activar SENSORLESS_HOMING
+    #define Y_MAX_PIN                         15
   #endif
 #endif
 #ifndef Z_STOP_PIN
   #ifndef Z_MIN_PIN
-    #define Z_MIN_PIN                         19      //Lujpins  Debe coincidir con _MAX_PIN al activar SENSORLESS_PROBING  
+    #define Z_MIN_PIN                         18
   #endif
   #ifndef Z_MAX_PIN
-    #define Z_MAX_PIN                         19      //Lujpins  Asociado a diag1 stepper X al activar SENSORLESS_HOMING
+    #define Z_MAX_PIN                         19
   #endif
 #endif
 
@@ -308,11 +308,11 @@
    * Serial2 -- AUX-4 Pin 18 (D16 TX2) and AUX-4 Pin 17 (D17 RX2)
    * Serial1 -- Pins D18 and D19 are used for Z-MIN and Z-MAX
    */
-  #define X_HARDWARE_SERIAL Serial3
+  //#define X_HARDWARE_SERIAL Serial1
   //#define X2_HARDWARE_SERIAL Serial1
-  #define Y_HARDWARE_SERIAL Serial3
+  //#define Y_HARDWARE_SERIAL Serial1
   //#define Y2_HARDWARE_SERIAL Serial1
-  #define Z_HARDWARE_SERIAL Serial3
+  //#define Z_HARDWARE_SERIAL Serial1
   //#define Z2_HARDWARE_SERIAL Serial1
   //#define E0_HARDWARE_SERIAL Serial1
   //#define E1_HARDWARE_SERIAL Serial1
@@ -321,12 +321,10 @@
   //#define E4_HARDWARE_SERIAL Serial1
 
   #ifndef X_SERIAL_TX_PIN
-    //#define X_SERIAL_TX_PIN                   40   //Lujpins
-    #define X_SERIAL_TX_PIN                   14   //Lujpins
+    #define X_SERIAL_TX_PIN                   40
   #endif
   #ifndef X_SERIAL_RX_PIN
-    //#define X_SERIAL_RX_PIN                   63   //Lujpins   
-    #define X_SERIAL_RX_PIN                   15   //   
+    #define X_SERIAL_RX_PIN                   63
   #endif
   #ifndef X2_SERIAL_TX_PIN
     #define X2_SERIAL_TX_PIN                  -1
@@ -336,12 +334,10 @@
   #endif
 
   #ifndef Y_SERIAL_TX_PIN
-    //#define Y_SERIAL_TX_PIN                   59  //Lujpins
-    #define Y_SERIAL_TX_PIN                   14  //
+    #define Y_SERIAL_TX_PIN                   59
   #endif
   #ifndef Y_SERIAL_RX_PIN
-    //#define Y_SERIAL_RX_PIN                   64  //Lujpins
-    #define Y_SERIAL_RX_PIN                   15  //
+    #define Y_SERIAL_RX_PIN                   64
   #endif
   #ifndef Y2_SERIAL_TX_PIN
     #define Y2_SERIAL_TX_PIN                  -1
@@ -351,12 +347,10 @@
   #endif
 
   #ifndef Z_SERIAL_TX_PIN
-    //#define Z_SERIAL_TX_PIN                   42  //Lujpins
-    #define Z_SERIAL_TX_PIN                   14
+    #define Z_SERIAL_TX_PIN                   42
   #endif
   #ifndef Z_SERIAL_RX_PIN
-    //#define Z_SERIAL_RX_PIN                   65  //Lujpins
-    #define Z_SERIAL_RX_PIN                   15
+    #define Z_SERIAL_RX_PIN                   65
   #endif
   #ifndef Z2_SERIAL_TX_PIN
     #define Z2_SERIAL_TX_PIN                  -1
@@ -366,12 +360,10 @@
   #endif
 
   #ifndef E0_SERIAL_TX_PIN
-    #define E0_SERIAL_TX_PIN                  44  //LUj
-    //#define E0_SERIAL_TX_PIN                  14 //44  LUj    
+    #define E0_SERIAL_TX_PIN                  44
   #endif
   #ifndef E0_SERIAL_RX_PIN
-    #define E0_SERIAL_RX_PIN                  66  //Luj
-    //#define E0_SERIAL_RX_PIN                  15 //66  Luj    
+    #define E0_SERIAL_RX_PIN                  66
   #endif
   #ifndef E1_SERIAL_TX_PIN
     #define E1_SERIAL_TX_PIN                  -1
@@ -415,8 +407,6 @@
   #ifndef E7_SERIAL_RX_PIN
     #define E7_SERIAL_RX_PIN                  -1
   #endif
-  // Reduce baud rate to improve software serial reliability    //LujTMC2209   copiado de pins_BTT_SKR_V1_4.h
-  #define TMC_BAUD_RATE                    115200               //19200   Al final 115200 resolvio el problema de carga inicial de configuracion M913.
 #endif
 
 //
