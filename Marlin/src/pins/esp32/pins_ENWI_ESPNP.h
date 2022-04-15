1b17ec6bf7fe015aab4309ca5a57a009e8a71c97
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-04-14 21:41:02 -0500

    misc. cleanup

--- Marlin/src/pins/esp32/pins_ENWI_ESPNP.h
+++ Marlin/src/pins/esp32/pins_ENWI_ESPNP.h
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2022 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -94,8 +94,8 @@
 //
 // Temperature Sensors
 //
-// #define TEMP_0_PIN                            36  // Analog Input
-// #define TEMP_BED_PIN                          39  // Analog Input
+//#define TEMP_0_PIN                          36  // Analog Input
+//#define TEMP_BED_PIN                        39  // Analog Input
 
 // General use mosfets, useful for things like pumps and solenoids
 // Shift register pins 128, 129, 130 and 131 are broken out and can be used
