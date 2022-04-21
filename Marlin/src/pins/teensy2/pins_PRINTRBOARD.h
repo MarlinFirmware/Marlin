cd6d4bbc8ecdf450770e3e88bcc2e41343905d80
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-04-21 07:16:52 -0500

    🧑‍💻 IS_ULTRA_LCD => HAS_WIRED_LCD

--- Marlin/src/pins/teensy2/pins_PRINTRBOARD.h
+++ Marlin/src/pins/teensy2/pins_PRINTRBOARD.h
@@ -121,7 +121,7 @@
 //
 // LCD / Controller
 //
-#if IS_ULTRA_LCD && IS_NEWPANEL
+#if HAS_WIRED_LCD && IS_NEWPANEL
 
   #define LCD_PINS_RS                          9  // E1       JP11-11
   #define LCD_PINS_ENABLE                      8  // E0       JP11-10
@@ -163,7 +163,7 @@
 
   #endif
 
-#endif // IS_ULTRA_LCD && IS_NEWPANEL
+#endif // HAS_WIRED_LCD && IS_NEWPANEL
 
 #ifndef SDSS
   #define SDSS                                26  // B6 SDCS
