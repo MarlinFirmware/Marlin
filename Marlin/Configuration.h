40d888e5e122149bc0df5857d02d430d43d9bbed
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2022-04-29 12:52:25 -0700

    Update NeoPixel Type Comment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3051,7 +3051,7 @@
 // Support for Adafruit NeoPixel LED driver
 //#define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
-  #define NEOPIXEL_TYPE          NEO_GRBW // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
+  #define NEOPIXEL_TYPE          NEO_GRBW // NEO_GRBW, NEO_RGBW, etc. / NEO_GRB, NEO_RBG, etc. - four/three channel driver type (defined in https://github.com/adafruit/Adafruit_NeoPixel/blob/master/Adafruit_NeoPixel.h)
   //#define NEOPIXEL_PIN                4 // LED driving pin
   //#define NEOPIXEL2_TYPE  NEOPIXEL_TYPE
   //#define NEOPIXEL2_PIN               5
