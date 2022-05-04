4c8c02f66ff501fdf84082f9b89a9cc04be626d3
Scott Lahteine <github@thinkyhead.com>
2022-05-04 12:47:15 -0500

    📝 Replace KB with K in pins

--- Marlin/src/pins/stm32f4/pins_MKS_ROBIN_PRO_V2.h
+++ Marlin/src/pins/stm32f4/pins_MKS_ROBIN_PRO_V2.h
@@ -37,7 +37,7 @@
 //#define SRAM_EEPROM_EMULATION                   // Use BackSRAM-based EEPROM emulation
 //#define FLASH_EEPROM_EMULATION                  // Use Flash-based EEPROM emulation
 #define I2C_EEPROM
-#define MARLIN_EEPROM_SIZE                0x1000  // 4KB
+#define MARLIN_EEPROM_SIZE                0x1000  // 4K
 
 #define HAS_OTG_USB_HOST_SUPPORT                  // USB Flash Drive support
 
