959a6c4f7037345dd6f2bf5abe3c376981988aed
Scott Lahteine <github@thinkyhead.com>
2022-05-31 18:44:57 -0500

    🔖 Marlin 2.1

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.0.9.4"
+//#define SHORT_BUILD_VERSION "2.1"
 
 /**
  * Verbose version identifier which should contain a reference to the location
