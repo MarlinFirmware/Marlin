fb5fe6786941aa5c712812c82aa60ffdb006289d
soysnorre <soysmorre@gmail.com>
2022-06-02 16:37:43 +0200

    Errores finales corregidos

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2035,7 +2035,7 @@
  *
  * View the current statistics with M78.
  */
-#define PRINTCOUNTER
+//#define PRINTCOUNTER
 #if ENABLED(PRINTCOUNTER)
   #define PRINTCOUNTER_SAVE_INTERVAL 60 // (minutes) EEPROM save interval during print
 #endif
