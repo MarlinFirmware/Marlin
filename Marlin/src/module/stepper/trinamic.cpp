cd9a23c4d0b4b7e4a2a8d9dd020cdc787300102e
Scott Lahteine <github@thinkyhead.com>
2022-06-20 21:15:57 -0500

    🎨 Fix comments, formatting

--- Marlin/src/module/stepper/trinamic.cpp
+++ Marlin/src/module/stepper/trinamic.cpp
@@ -942,9 +942,7 @@ void reset_trinamic_drivers() {
   struct SanityHwSerialDetails { const char port[20]; uint32_t address; };
   #define TMC_HW_DETAIL_ARGS(A) TERN(A##_HAS_HW_SERIAL, STRINGIFY(A##_HARDWARE_SERIAL), ""), TERN0(A##_HAS_HW_SERIAL, A##_SLAVE_ADDRESS)
   #define TMC_HW_DETAIL(A) { TMC_HW_DETAIL_ARGS(A) }
-  constexpr SanityHwSerialDetails sanity_tmc_hw_details[] = {
-    MAPLIST(TMC_HW_DETAIL, ALL_AXIS_NAMES)
-  };
+  constexpr SanityHwSerialDetails sanity_tmc_hw_details[] = { MAPLIST(TMC_HW_DETAIL, ALL_AXIS_NAMES) };
 
   // constexpr compatible string comparison
   constexpr bool str_eq_ce(const char * a, const char * b) {
@@ -969,9 +967,7 @@ void reset_trinamic_drivers() {
   struct SanitySwSerialDetails { int32_t txpin; int32_t rxpin; uint32_t address; };
   #define TMC_SW_DETAIL_ARGS(A) TERN(A##_HAS_SW_SERIAL, A##_SERIAL_TX_PIN, -1), TERN(A##_HAS_SW_SERIAL, A##_SERIAL_RX_PIN, -1), TERN0(A##_HAS_SW_SERIAL, A##_SLAVE_ADDRESS)
   #define TMC_SW_DETAIL(A) { TMC_SW_DETAIL_ARGS(A) }
-  constexpr SanitySwSerialDetails sanity_tmc_sw_details[] = {
-    MAPLIST(TMC_SW_DETAIL, ALL_AXIS_NAMES)
-  };
+  constexpr SanitySwSerialDetails sanity_tmc_sw_details[] = { MAPLIST(TMC_SW_DETAIL, ALL_AXIS_NAMES) };
 
   constexpr bool sc_sw_done(size_t start, size_t end) { return start == end; }
   constexpr bool sc_sw_skip(int32_t txpin) { return txpin < 0; }
