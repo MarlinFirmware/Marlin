5de051df19e769c5de2effeae15beb2b5931e4df
Victor Oliveira <rhapsodyv@gmail.com>
2022-06-22 05:33:03 -0300

    🚑️ Fix BIQU BX touch freeze (#24383)

--- Marlin/src/HAL/STM32/tft/gt911.h
+++ Marlin/src/HAL/STM32/tft/gt911.h
@@ -23,7 +23,7 @@
 
 #include "../../../inc/MarlinConfig.h"
 
-#define GT911_SLAVE_ADDRESS   0xBA
+#define GT911_SLAVE_ADDRESS   0x28
 
 #if !PIN_EXISTS(GT911_RST)
   #error "GT911_RST_PIN is not defined."
