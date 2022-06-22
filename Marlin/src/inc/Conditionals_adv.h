b7e1b6b8933f3a10a5bf1ec76ca7797aa5af54b4
ellensp <530024+ellensp@users.noreply.github.com>
2022-06-22 22:08:24 +1200

    🩹 Fix DGUS (MKS) compile (#24378)

--- Marlin/src/inc/Conditionals_adv.h
+++ Marlin/src/inc/Conditionals_adv.h
@@ -1018,13 +1018,13 @@
  * LCD_SERIAL_PORT must be defined ahead of HAL.h
  */
 #ifndef LCD_SERIAL_PORT
-  #if HAS_DWIN_E3V2 || IS_DWIN_MARLINUI
+  #if HAS_DWIN_E3V2 || IS_DWIN_MARLINUI || HAS_DGUS_LCD
     #if MB(BTT_SKR_MINI_E3_V1_0, BTT_SKR_MINI_E3_V1_2, BTT_SKR_MINI_E3_V2_0, BTT_SKR_MINI_E3_V3_0, BTT_SKR_E3_TURBO)
       #define LCD_SERIAL_PORT 1
-    #elif MB(CREALITY_V24S1_301, CREALITY_V24S1_301F4, CREALITY_V423)
-      #define LCD_SERIAL_PORT 2 // Creality Ender3S1 board
+    #elif MB(CREALITY_V24S1_301, CREALITY_V24S1_301F4, CREALITY_V423, MKS_ROBIN)
+      #define LCD_SERIAL_PORT 2 // Creality Ender3S1, MKS Robin
     #else
-      #define LCD_SERIAL_PORT 3 // Creality 4.x board
+      #define LCD_SERIAL_PORT 3 // Other boards
     #endif
   #endif
   #ifdef LCD_SERIAL_PORT
