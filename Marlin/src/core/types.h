4dad5871888fd99fea0ec918c04f1c134f49ab8a
InsanityAutomation <38436470+InsanityAutomation@users.noreply.github.com>
2022-06-21 22:09:24 -0400

    🐛 Resolve DUE Servo pulse issue (#24305)
    Co-authored-by: sjasonsmith <20053467+sjasonsmith@users.noreply.github.com>
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/src/core/types.h
+++ Marlin/src/core/types.h
@@ -93,9 +93,9 @@ struct Flags {
   void set(const int n)                    { b |=  (bits_t)_BV(n); }
   void clear(const int n)                  { b &= ~(bits_t)_BV(n); }
   bool test(const int n) const             { return TEST(b, n); }
-        bool operator[](const int n)       { return test(n); }
+  const bool operator[](const int n)       { return test(n); }
   const bool operator[](const int n) const { return test(n); }
-  const int size() const                   { return sizeof(b); }
+  int size() const                         { return sizeof(b); }
 };
 
 // Specialization for a single bool flag
@@ -107,9 +107,9 @@ struct Flags<1> {
   void set(const int)                     { b = true; }
   void clear(const int)                   { b = false; }
   bool test(const int) const              { return b; }
-        bool operator[](const int)        { return b; }
-  const bool operator[](const int) const  { return b; }
-  const int size() const                  { return sizeof(b); }
+  bool& operator[](const int)             { return b; }
+  bool  operator[](const int) const       { return b; }
+  int size() const                        { return sizeof(b); }
 };
 
 typedef Flags<8> flags_8_t;
