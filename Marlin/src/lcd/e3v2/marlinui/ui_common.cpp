0435b2220a695f40aaba29a6965f7406d9a30db9
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-06-26 05:47:18 -0500

    🐛 Fix Manual Move axis selection (#24404)

--- Marlin/src/lcd/e3v2/marlinui/ui_common.cpp
+++ Marlin/src/lcd/e3v2/marlinui/ui_common.cpp
@@ -410,8 +410,7 @@ void MarlinUI::draw_status_message(const bool blink) {
       const dwin_coord_t by = (row * MENU_LINE_HEIGHT) + MENU_FONT_HEIGHT + EXTRA_ROW_HEIGHT / 2;
       DWIN_Draw_String(true, font16x32, Color_Yellow, Color_Bg_Black, (LCD_PIXEL_WIDTH - vallen * 16) / 2, by, S(dwin_string.string()));
 
-      extern screenFunc_t _manual_move_func_ptr;
-      if (ui.currentScreen != _manual_move_func_ptr && !ui.external_control) {
+      if (ui.can_show_slider()) {
 
         const dwin_coord_t slider_length = LCD_PIXEL_WIDTH - TERN(DWIN_MARLINUI_LANDSCAPE, 120, 20),
                            slider_height = 16,
