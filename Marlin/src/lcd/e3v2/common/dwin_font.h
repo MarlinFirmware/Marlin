9bd39749d75b6ecd5cbe598fc72feaf285945eab
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2022-07-14 00:00:33 -0500

    🚸 Fix and update ProUI (#24477)

--- Marlin/src/lcd/e3v2/common/dwin_font.h
+++ Marlin/src/lcd/e3v2/common/dwin_font.h
@@ -21,6 +21,8 @@
  */
 #pragma once
 
+typedef uint8_t fontid_t;
+
 /**
  * 3-.0：The font size, 0x00-0x09, corresponds to the font size below:
  * 0x00=6*12   0x01=8*16   0x02=10*20  0x03=12*24  0x04=14*28
