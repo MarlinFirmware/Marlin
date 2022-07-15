a17b0f635f63b5fa6f3b5d981d1f2581d9a8ea38
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-07-15 17:44:59 -0500

    run pins formatter

--- Marlin/src/pins/lpc1768/pins_EMOTRONIC.h
+++ Marlin/src/pins/lpc1768/pins_EMOTRONIC.h
@@ -33,7 +33,6 @@
 #define BOARD_INFO_NAME   "eMotronic"
 #define BOARD_WEBSITE_URL "www.reprap-france.com/article/lemotronic-quesaco"
 
-
 //
 // Limit Switches
 //
@@ -43,11 +42,10 @@
 #define _S3_PIN                            P0_26  // (S3)
 #define _S4_PIN                            P0_26  // (S4: optocoupled)
 
-#define X_MAX_PIN                          _S0_PIN
-#define Y_MAX_PIN                          _S1_PIN
-#define Z_MAX_PIN                          _S2_PIN
-#define Z_MIN_PIN                          _S4_PIN // manual probe
-
+#define X_MAX_PIN                        _S0_PIN
+#define Y_MAX_PIN                        _S1_PIN
+#define Z_MAX_PIN                        _S2_PIN
+#define Z_MIN_PIN                        _S4_PIN  // manual probe
 
 //
 // Steppers
@@ -66,46 +64,43 @@
 
 #define _M4_STEP_PIN                       P2_03
 #define _M4_DIR_PIN                        P0_04
-#define _M4_ENABLE_PIN                     P0_05 // the doc wrongly says 0.3
+#define _M4_ENABLE_PIN                     P0_05  // the doc wrongly says 0.3
 
 #define _M5_STEP_PIN                       P2_08
 #define _M5_DIR_PIN                        P0_20
 #define _M5_ENABLE_PIN                     P0_19
 
+#define X_STEP_PIN                  _M3_STEP_PIN
+#define X_DIR_PIN                    _M3_DIR_PIN
+#define X_ENABLE_PIN              _M3_ENABLE_PIN
 
-#define X_STEP_PIN                         _M3_STEP_PIN
-#define X_DIR_PIN                          _M3_DIR_PIN
-#define X_ENABLE_PIN                       _M3_ENABLE_PIN
+#define Y_STEP_PIN                  _M2_STEP_PIN
+#define Y_DIR_PIN                    _M2_DIR_PIN
+#define Y_ENABLE_PIN              _M2_ENABLE_PIN
 
-#define Y_STEP_PIN                         _M2_STEP_PIN
-#define Y_DIR_PIN                          _M2_DIR_PIN
-#define Y_ENABLE_PIN                       _M2_ENABLE_PIN
+#define Z_STEP_PIN                  _M1_STEP_PIN
+#define Z_DIR_PIN                    _M1_DIR_PIN
+#define Z_ENABLE_PIN              _M1_ENABLE_PIN
 
-#define Z_STEP_PIN                         _M1_STEP_PIN
-#define Z_DIR_PIN                          _M1_DIR_PIN
-#define Z_ENABLE_PIN                       _M1_ENABLE_PIN
+#define E0_STEP_PIN                 _M4_STEP_PIN
+#define E0_DIR_PIN                   _M4_DIR_PIN
+#define E0_ENABLE_PIN             _M4_ENABLE_PIN
 
-#define E0_STEP_PIN                        _M4_STEP_PIN
-#define E0_DIR_PIN                         _M4_DIR_PIN
-#define E0_ENABLE_PIN                      _M4_ENABLE_PIN
-
-#define E1_STEP_PIN                        _M5_STEP_PIN
-#define E1_DIR_PIN                         _M5_DIR_PIN
-#define E1_ENABLE_PIN                      _M5_ENABLE_PIN
-
+#define E1_STEP_PIN                 _M5_STEP_PIN
+#define E1_DIR_PIN                   _M5_DIR_PIN
+#define E1_ENABLE_PIN             _M5_ENABLE_PIN
 
 //
 // Temperature Sensors
 // 3.3V max when defined as an analog input
 //
-#define _TH0_PIN                           P0_23_A0  // (TH0)
-#define _TH1_PIN                           P0_24_A1  // (TH1)
-#define _TH2_PIN                           P0_25_A2  // (TH2)
+#define _TH0_PIN                        P0_23_A0  // (TH0)
+#define _TH1_PIN                        P0_24_A1  // (TH1)
+#define _TH2_PIN                        P0_25_A2  // (TH2)
 
-#define TEMP_0_PIN                         _TH1_PIN
-#define TEMP_1_PIN                         _TH0_PIN
-#define TEMP_BED_PIN                       _TH2_PIN
-
+#define TEMP_0_PIN                      _TH1_PIN
+#define TEMP_1_PIN                      _TH0_PIN
+#define TEMP_BED_PIN                    _TH2_PIN
 
 //
 // Heaters / Fans
@@ -118,13 +113,12 @@
 #define _FAN0_PIN                          P2_11  // (FAN0: 1A)
 #define _FAN1_PIN                          P2_13  // (FAN1: 1A)
 
-#define HEATER_BED_PIN                     _H1_PIN
-#define HEATER_0_PIN                       _H2_PIN
-#define HEATER_1_PIN                       _H3_PIN
+#define HEATER_BED_PIN                   _H1_PIN
+#define HEATER_0_PIN                     _H2_PIN
+#define HEATER_1_PIN                     _H3_PIN
 
-#define FAN_PIN                            _FAN0_PIN  // part cooling on Micro-Delta Rework
-#define FAN1_PIN                           _FAN1_PIN  // extruder fan on Micro-Delta Rework
-
+#define FAN_PIN                        _FAN0_PIN  // part cooling on Micro-Delta Rework
+#define FAN1_PIN                       _FAN1_PIN  // extruder fan on Micro-Delta Rework
 
 //
 // Extension ports
@@ -151,7 +145,6 @@
 #define _EXT2_09                           -1 /* GND */
 #define _EXT2_10                           -1 /* 5V */
 
-
 //
 // SD Support
 //
@@ -165,27 +158,26 @@
   #define SD_MOSI_PIN                      P0_09
   #define SD_SS_PIN                        P0_06
 #elif SD_CONNECTION_IS(LCD)
-  #define SD_SCK_PIN                       _EXT1_07
-  #define SD_MISO_PIN                      _EXT1_05
-  #define SD_MOSI_PIN                      _EXT1_06
-  #define SD_SS_PIN                        _EXT1_08
-  #define SD_DETECT_PIN                    _EXT1_04
+  #define SD_SCK_PIN                    _EXT1_07
+  #define SD_MISO_PIN                   _EXT1_05
+  #define SD_MOSI_PIN                   _EXT1_06
+  #define SD_SS_PIN                     _EXT1_08
+  #define SD_DETECT_PIN                 _EXT1_04
 #elif SD_CONNECTION_IS(CUSTOM_CABLE)
   #error "No custom SD drive cable defined for this board."
 #endif
-
 
 //
 // LCD / Controller
 //
 #if ENABLED(EMOTION_TECH_LCD)
-  #define BEEPER_PIN                       _EXT2_01
-  #define DOGLCD_A0                        _EXT2_06
-  #define DOGLCD_CS                        _EXT2_04
-  #define DOGLCD_SCK                       _EXT1_07
-  #define DOGLCD_MOSI                      _EXT1_06
+  #define BEEPER_PIN                    _EXT2_01
+  #define DOGLCD_A0                     _EXT2_06
+  #define DOGLCD_CS                     _EXT2_04
+  #define DOGLCD_SCK                    _EXT1_07
+  #define DOGLCD_MOSI                   _EXT1_06
 
-  #define BTN_EN1                          _EXT2_07
-  #define BTN_EN2                          _EXT2_05
-  #define BTN_ENC                          _EXT2_02
+  #define BTN_EN1                       _EXT2_07
+  #define BTN_EN2                       _EXT2_05
+  #define BTN_ENC                       _EXT2_02
 #endif
