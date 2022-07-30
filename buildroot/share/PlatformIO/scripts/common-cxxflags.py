f259f303de9dcd520fdd09878539c2a98f60ff63
Ivan Kravets <me@ikravets.com>
2022-07-30 12:59:52 +0300

    Fix issue with debugging when using PlatformIO

--- buildroot/share/PlatformIO/scripts/common-cxxflags.py
+++ buildroot/share/PlatformIO/scripts/common-cxxflags.py
@@ -29,8 +29,9 @@ if pioutil.is_pio_build():
 	# It useful to keep two live versions: a debug version for debugging and another for
 	# release, for flashing when upload is not done automatically by jlink/stlink.
 	# Without this, PIO needs to recompile everything twice for any small change.
-	if env.GetBuildType() == "debug" and env.get('UPLOAD_PROTOCOL') not in ['jlink', 'stlink', 'custom']:
-		env['BUILD_DIR'] = '$PROJECT_BUILD_DIR/$PIOENV/debug'
+	#
+	# 	if env.GetBuildType() == "debug" and env.get('UPLOAD_PROTOCOL') not in ['jlink', 'stlink', 'custom']:
+	# 		env['BUILD_DIR'] = '$PROJECT_BUILD_DIR/$PIOENV/debug'
 
 	# On some platform, F_CPU is a runtime variable. Since it's used to convert from ns
 	# to CPU cycles, this adds overhead preventing small delay (in the order of less than
