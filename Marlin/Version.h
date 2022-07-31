4ba35d3284284d99de757483c38dafa392a0b84c
thinkyhead <thinkyhead@users.noreply.github.com>
2022-07-31 06:05:45 +0000

    [cron] Bump distribution date (2022-07-31)

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2022-07-30"
+//#define STRING_DISTRIBUTION_DATE "2022-07-31"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
