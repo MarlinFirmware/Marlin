6fc783e379fe23a30cd21b6162b555c720887ba1
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-08-06 01:44:04 -0500

    tweak name

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1590,7 +1590,7 @@
    */
   //#define SD_ABORT_ON_ENDSTOP_HIT
   #if ENABLED(SD_ABORT_ON_ENDSTOP_HIT)
-    //#define SD_ABORT_ON_ENDSTOP_HIT_CUSTOM_GCODE "G28XY" // G-code to run on endstop hit (e.g., "G28XY" or "G27")
+    //#define SD_ABORT_ON_ENDSTOP_HIT_GCODE "G28XY" // G-code to run on endstop hit (e.g., "G28XY" or "G27")
   #endif
 
   //#define SD_REPRINT_LAST_SELECTED_FILE // On print completion open the LCD Menu and select the same file
