53b202cf9de385b039a4c929f4aa590cc0888865
ExtNeon <33217029+ExtNeon@users.noreply.github.com>
2022-08-06 23:37:03 +0000

    ✨ SD Endstop Abort G-Code (#24461)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1589,6 +1589,9 @@
    * Endstops must be activated for this option to work.
    */
   //#define SD_ABORT_ON_ENDSTOP_HIT
+  #if ENABLED(SD_ABORT_ON_ENDSTOP_HIT)
+    //#define SD_ABORT_ON_ENDSTOP_HIT_GCODE "G28XY" // G-code to run on endstop hit (e.g., "G28XY" or "G27")
+  #endif
 
   //#define SD_REPRINT_LAST_SELECTED_FILE // On print completion open the LCD Menu and select the same file
 
