b0d17ecb314bba95c021b30c1bd8cc23e1850b80
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-08-06 01:41:22 -0500

    cleanup

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1555,9 +1555,9 @@
    */
   //#define SD_ABORT_ON_ENDSTOP_HIT
   #if ENABLED(SD_ABORT_ON_ENDSTOP_HIT)
-    //#define SD_ABORT_ON_ENDSTOP_HIT_CUSTOM_GCODE "G28XY" //G-code to run on endstop hit (e.g., "G28XY" or "G27")
+    //#define SD_ABORT_ON_ENDSTOP_HIT_CUSTOM_GCODE "G28XY" // G-code to run on endstop hit (e.g., "G28XY" or "G27")
   #endif
-  
+
   //#define SD_REPRINT_LAST_SELECTED_FILE // On print completion open the LCD Menu and select the same file
 
   //#define AUTO_REPORT_SD_STATUS         // Auto-report media status with 'M27 S<seconds>'
