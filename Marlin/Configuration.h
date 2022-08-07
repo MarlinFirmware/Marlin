f2299194c2081f2d00339274b39ecab9de0f6b5c
MasterofNull <cjlnorcal@gmail.com>
2022-08-06 22:13:04 -0700

    Update Configuration.h
    adding #endif to close out #if ENABLED(CLASSIC_JERK)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1222,6 +1222,8 @@
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
 
   //#define LIMITED_JERK_EDITING        // Limit edit via M205 or LCD to DEFAULT_aJERK * 2
+  #endif
+
   #if ENABLED(LIMITED_JERK_EDITING)
     #define MAX_JERK_EDIT_VALUES { 20, 20, 0.6, 10 } // ...or, set your own edit limits
   #endif
