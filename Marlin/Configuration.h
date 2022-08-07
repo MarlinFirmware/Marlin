308710ecf56464010ffaf6a89ff8b4680a1128bb
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2022-08-07 11:45:06 -0700

    Rework Skew Correction Configuration Options

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2129,9 +2129,9 @@
   #define XY_DIAG_BD 282.8427124746
   #define XY_SIDE_AD 200
 
-  // Or, set the default skew factors directly here
+  // Or, set the XY skew factor directly here
   // to override the above measurements:
-  #define XY_SKEW_FACTOR 0.0
+  //#define XY_SKEW_FACTOR 0.0
 
   //#define SKEW_CORRECTION_FOR_Z
   #if ENABLED(SKEW_CORRECTION_FOR_Z)
@@ -2140,8 +2140,11 @@
     #define YZ_DIAG_AC 282.8427124746
     #define YZ_DIAG_BD 282.8427124746
     #define YZ_SIDE_AD 200
-    #define XZ_SKEW_FACTOR 0.0
-    #define YZ_SKEW_FACTOR 0.0
+
+    // Or, set the Z skew factors directly here
+    // to override the above measurements:
+    //#define XZ_SKEW_FACTOR 0.0
+    //#define YZ_SKEW_FACTOR 0.0
   #endif
 
   // Enable this option for M852 to set skew at runtime
