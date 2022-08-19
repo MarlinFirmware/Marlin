ff09ea13a4ef86d810ebb69eb08933c448c9e32c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-08-19 11:00:52 -0500

    🧑‍💻 Use spaces indent for Python

--- buildroot/share/PlatformIO/scripts/preprocessor.py
+++ buildroot/share/PlatformIO/scripts/preprocessor.py
@@ -7,8 +7,8 @@ nocache = 1
 verbose = 0
 
 def blab(str):
-	if verbose:
-		print(str)
+    if verbose:
+        print(str)
 
 ################################################################################
 #
@@ -16,36 +16,36 @@ def blab(str):
 #
 preprocessor_cache = {}
 def run_preprocessor(env, fn=None):
-	filename = fn or 'buildroot/share/PlatformIO/scripts/common-dependencies.h'
-	if filename in preprocessor_cache:
-		return preprocessor_cache[filename]
+    filename = fn or 'buildroot/share/PlatformIO/scripts/common-dependencies.h'
+    if filename in preprocessor_cache:
+        return preprocessor_cache[filename]
 
-	# Process defines
-	build_flags = env.get('BUILD_FLAGS')
-	build_flags = env.ParseFlagsExtended(build_flags)
+    # Process defines
+    build_flags = env.get('BUILD_FLAGS')
+    build_flags = env.ParseFlagsExtended(build_flags)
 
-	cxx = search_compiler(env)
-	cmd = ['"' + cxx + '"']
+    cxx = search_compiler(env)
+    cmd = ['"' + cxx + '"']
 
-	# Build flags from board.json
-	#if 'BOARD' in env:
-	#	cmd += [env.BoardConfig().get("build.extra_flags")]
-	for s in build_flags['CPPDEFINES']:
-		if isinstance(s, tuple):
-			cmd += ['-D' + s[0] + '=' + str(s[1])]
-		else:
-			cmd += ['-D' + s]
+    # Build flags from board.json
+    #if 'BOARD' in env:
+    #   cmd += [env.BoardConfig().get("build.extra_flags")]
+    for s in build_flags['CPPDEFINES']:
+        if isinstance(s, tuple):
+            cmd += ['-D' + s[0] + '=' + str(s[1])]
+        else:
+            cmd += ['-D' + s]
 
-	cmd += ['-D__MARLIN_DEPS__ -w -dM -E -x c++']
-	depcmd = cmd + [ filename ]
-	cmd = ' '.join(depcmd)
-	blab(cmd)
-	try:
-		define_list = subprocess.check_output(cmd, shell=True).splitlines()
-	except:
-		define_list = {}
-	preprocessor_cache[filename] = define_list
-	return define_list
+    cmd += ['-D__MARLIN_DEPS__ -w -dM -E -x c++']
+    depcmd = cmd + [ filename ]
+    cmd = ' '.join(depcmd)
+    blab(cmd)
+    try:
+        define_list = subprocess.check_output(cmd, shell=True).splitlines()
+    except:
+        define_list = {}
+    preprocessor_cache[filename] = define_list
+    return define_list
 
 
 ################################################################################
@@ -54,41 +54,41 @@ def run_preprocessor(env, fn=None):
 #
 def search_compiler(env):
 
-	from pathlib import Path, PurePath
+    from pathlib import Path, PurePath
 
-	ENV_BUILD_PATH = Path(env['PROJECT_BUILD_DIR'], env['PIOENV'])
-	GCC_PATH_CACHE = ENV_BUILD_PATH / ".gcc_path"
+    ENV_BUILD_PATH = Path(env['PROJECT_BUILD_DIR'], env['PIOENV'])
+    GCC_PATH_CACHE = ENV_BUILD_PATH / ".gcc_path"
 
-	try:
-		gccpath = env.GetProjectOption('custom_gcc')
-		blab("Getting compiler from env")
-		return gccpath
-	except:
-		pass
+    try:
+        gccpath = env.GetProjectOption('custom_gcc')
+        blab("Getting compiler from env")
+        return gccpath
+    except:
+        pass
 
-	# Warning: The cached .gcc_path will obscure a newly-installed toolkit
-	if not nocache and GCC_PATH_CACHE.exists():
-		blab("Getting g++ path from cache")
-		return GCC_PATH_CACHE.read_text()
+    # Warning: The cached .gcc_path will obscure a newly-installed toolkit
+    if not nocache and GCC_PATH_CACHE.exists():
+        blab("Getting g++ path from cache")
+        return GCC_PATH_CACHE.read_text()
 
-	# Use any item in $PATH corresponding to a platformio toolchain bin folder
-	path_separator = ':'
-	gcc_exe = '*g++'
-	if env['PLATFORM'] == 'win32':
-		path_separator = ';'
-		gcc_exe += ".exe"
+    # Use any item in $PATH corresponding to a platformio toolchain bin folder
+    path_separator = ':'
+    gcc_exe = '*g++'
+    if env['PLATFORM'] == 'win32':
+        path_separator = ';'
+        gcc_exe += ".exe"
 
-	# Search for the compiler in PATH
-	for ppath in map(Path, env['ENV']['PATH'].split(path_separator)):
-		if ppath.match(env['PROJECT_PACKAGES_DIR'] + "/**/bin"):
-			for gpath in ppath.glob(gcc_exe):
-				gccpath = str(gpath.resolve())
-				# Cache the g++ path to no search always
-				if not nocache and ENV_BUILD_PATH.exists():
-					blab("Caching g++ for current env")
-					GCC_PATH_CACHE.write_text(gccpath)
-				return gccpath
+    # Search for the compiler in PATH
+    for ppath in map(Path, env['ENV']['PATH'].split(path_separator)):
+        if ppath.match(env['PROJECT_PACKAGES_DIR'] + "/**/bin"):
+            for gpath in ppath.glob(gcc_exe):
+                gccpath = str(gpath.resolve())
+                # Cache the g++ path to no search always
+                if not nocache and ENV_BUILD_PATH.exists():
+                    blab("Caching g++ for current env")
+                    GCC_PATH_CACHE.write_text(gccpath)
+                return gccpath
 
-	gccpath = env.get('CXX')
-	blab("Couldn't find a compiler! Fallback to %s" % gccpath)
-	return gccpath
+    gccpath = env.get('CXX')
+    blab("Couldn't find a compiler! Fallback to %s" % gccpath)
+    return gccpath
