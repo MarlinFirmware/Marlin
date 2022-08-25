719c07f6af701ae8c8cbad86127c9c42b2623924
Paweł Jędrak <jedrak.pawel@gmail.com>
2022-08-25 23:46:19 +0200

    tmc2209 working on software UART

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3045,9 +3045,9 @@
    * Set *_SERIAL_TX_PIN and *_SERIAL_RX_PIN to match for all drivers
    * on the same serial port, either here or in your board's pins file.
    */
-  //#define  X_SLAVE_ADDRESS 0
-  //#define  Y_SLAVE_ADDRESS 0
-  //#define  Z_SLAVE_ADDRESS 0
+  #define  X_SLAVE_ADDRESS 0
+  #define  Y_SLAVE_ADDRESS 1
+  #define  Z_SLAVE_ADDRESS 2
   //#define X2_SLAVE_ADDRESS 0
   //#define Y2_SLAVE_ADDRESS 0
   //#define Z2_SLAVE_ADDRESS 0
@@ -3141,7 +3141,7 @@
    * M912 - Clear stepper driver overtemperature pre-warn condition flag.
    * M122 - Report driver parameters (Requires TMC_DEBUG)
    */
-  //#define MONITOR_DRIVER_STATUS
+  #define MONITOR_DRIVER_STATUS
 
   #if ENABLED(MONITOR_DRIVER_STATUS)
     #define CURRENT_STEP_DOWN     50  // [mA]
@@ -3250,7 +3250,7 @@
    * Enable M122 debugging command for TMC stepper drivers.
    * M122 S0/1 will enable continuous reporting.
    */
-  //#define TMC_DEBUG
+  #define TMC_DEBUG
 
   /**
    * You can set your own advanced settings by filling in predefined functions.
--- Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO.h
+++ Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO.h
@@ -61,8 +61,45 @@
   //
   // Software serial
   //
-
   #define TMC_BAUD_RATE 19200
+
+  #if HAS_TMC220x
+  /**
+   * TMC2208/TMC2209 stepper drivers
+   *
+   * Hardware serial communication ports.
+   * If undefined software serial is used according to the pins below
+   */
+  //#define X_HARDWARE_SERIAL  Serial1
+  //#define Y_HARDWARE_SERIAL  Serial1
+  //#define Z_HARDWARE_SERIAL  Serial1
+  //#define E0_HARDWARE_SERIAL Serial1
+  //#define E1_HARDWARE_SERIAL Serial1
+
+  //
+  // Software serial
+  //
+
+  // #ifndef X_SERIAL_TX_PIN
+  //   #define X_SERIAL_TX_PIN  PA6 //E1_STEP_PIN
+  // #endif
+  // #ifndef X_SERIAL_RX_PIN
+  //   #define X_SERIAL_RX_PIN  PA1 //E1_DIR_PIN
+  // #endif
+
+  // #ifndef Y_SERIAL_TX_PIN
+  //   #define Y_SERIAL_TX_PIN  PA6 //E1_STEP_PIN
+  // #endif
+  // #ifndef Y_SERIAL_RX_PIN
+  //   #define Y_SERIAL_RX_PIN  PA1 //E1_DIR_PIN
+  // #endif
+
+  // #ifndef Z_SERIAL_TX_PIN
+  //   #define Z_SERIAL_TX_PIN  PA6 //E1_STEP_PIN
+  // #endif
+  // #ifndef Z_SERIAL_RX_PIN
+  //   #define Z_SERIAL_RX_PIN  PA1 //E1_DIR_PIN
+  // #endif
 
   #ifndef E0_SERIAL_TX_PIN
     #define E0_SERIAL_TX_PIN PA6 //E1_STEP_PIN
@@ -70,4 +107,6 @@
   #ifndef E0_SERIAL_RX_PIN
     #define E0_SERIAL_RX_PIN PA1 //E1_DIR_PIN
   #endif
+#endif
+
 #endif
