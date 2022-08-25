e57d156fcc2f305b80945f924ba876355800b517
Lefteris Garyfalakis <46350667+lefterisgar@users.noreply.github.com>
2022-08-25 19:30:39 +0300

    🔨 Suppressible Creality 4.2.2 stepper warning

--- Marlin/src/inc/Warnings.cpp
+++ Marlin/src/inc/Warnings.cpp
@@ -708,8 +708,10 @@
 #endif
 
 // Ender 3 Pro (but, apparently all Creality 4.2.2 boards)
-#if ENABLED(EMIT_CREALITY_422_WARNING) || MB(CREALITY_V4)
-  #warning "Creality 4.2.2 boards come with a variety of stepper drivers. Check the board label and set the correct *_DRIVER_TYPE! (C=HR4988, E=A4988, A=TMC2208, B=TMC2209, H=TMC2225)."
+#ifndef NO_CREALITY_STEPPER_WARNING
+  #if ENABLED(EMIT_CREALITY_422_WARNING) || MB(CREALITY_V4)
+    #warning "Creality 4.2.2 boards come with a variety of stepper drivers. Check the board label and set the correct *_DRIVER_TYPE! (C=HR4988, E=A4988, A=TMC2208, B=TMC2209, H=TMC2225). (Define NO_CREALITY_STEPPER_WARNING to suppress this warning.)"
+  #endif
 #endif
 
 #if PRINTCOUNTER_SYNC
