27ca0ab5b7052481459cb9f2cdd413977e73cb82
studiodyne <diabolomix@hotmail.fr>
2022-08-28 01:02:19 +0200

    2

--- Marlin/src/module/tool_change.cpp
+++ Marlin/src/module/tool_change.cpp
@@ -1153,7 +1153,6 @@ void tool_change(const uint8_t new_tool, bool no_move/*=false*/) {
     #endif
 
     const uint8_t old_tool = active_extruder;
-    const bool can_move_away = !no_move && !idex_full_control;
 
     #if HAS_LEVELING
       // Set current position to the physical position
