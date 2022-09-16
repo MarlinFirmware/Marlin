d25a71e2018035317ad4b556ba77635ba07e1a20
EvilGremlin <22657714+EvilGremlin@users.noreply.github.com>
2022-09-16 17:19:05 +0300

    cleanup

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4150,8 +4150,8 @@
 //#define ESP3D_WIFISUPPORT   // ESP3D Library WiFi management (https://github.com/luc-github/ESP3DLib)
 
 #if EITHER(WIFISUPPORT, ESP3D_WIFISUPPORT)
-  #define WEBSUPPORT          // Start a webserver (which may include auto-discovery)
-  #define OTASUPPORT          // Support over-the-air firmware updates
+  //#define WEBSUPPORT          // Start a webserver (which may include auto-discovery)
+  //#define OTASUPPORT          // Support over-the-air firmware updates
   //#define WIFI_CUSTOM_COMMAND // Accept feature config commands (e.g., WiFi ESP3D) from the host
 
   /**
