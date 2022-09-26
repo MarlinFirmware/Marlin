3e16476e0c823548db899b6090506f63c57d34f3
Kurtis Potier <kurts.potier@geofabrica.com>
2022-09-26 13:28:56 -0400

    multi endstop enable

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -831,7 +831,7 @@
 #ifdef Z2_DRIVER_TYPE
   //#define INVERT_Z2_VS_Z_DIR        // Z2 direction signal is the opposite of Z
 
-  //#define Z_MULTI_ENDSTOPS          // Other Z axes have their own endstops
+  #define Z_MULTI_ENDSTOPS          // Other Z axes have their own endstops
   #if ENABLED(Z_MULTI_ENDSTOPS)
     #define Z2_USE_ENDSTOP   _XMAX_   // Z2 endstop board plug. Don't forget to enable USE_*_PLUG.
     #define Z2_ENDSTOP_ADJUSTMENT 0   // Z2 offset relative to Y endstop
