bfcb8c704a8ddbdd86d145151e3a305a780034f1
adam3654 <adam3654@gmail.com>
2022-10-12 19:03:32 -0400

    ✨ DOGM Display Sleep (#24829)

--- Marlin/src/gcode/gcode.cpp
+++ Marlin/src/gcode/gcode.cpp
@@ -793,6 +793,10 @@ void GcodeSuite::process_parsed_command(const bool no_ok/*=false*/) {
         case 250: M250(); break;                                  // M250: Set LCD contrast
       #endif
 
+      #if HAS_GCODE_M255
+        case 255: M255(); break;                                  // M255: Set LCD Sleep/Backlight Timeout (Minutes)
+      #endif
+
       #if HAS_LCD_BRIGHTNESS
         case 256: M256(); break;                                  // M256: Set LCD brightness
       #endif
