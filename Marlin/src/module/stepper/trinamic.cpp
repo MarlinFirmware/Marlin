f9fb193f9784df949d436f5390a2fd2f05646225
Martin Turski <turningtides@outlook.de>
2022-10-21 02:59:36 +0200

    - added support for boards whose SPI pins stay anonymous/defaults

--- Marlin/src/module/stepper/trinamic.cpp
+++ Marlin/src/module/stepper/trinamic.cpp
@@ -48,7 +48,12 @@ enum StealthIndex : uint8_t {
 #if ENABLED(TMC_USE_SW_SPI)
   #define __TMC_SPI_DEFINE(IC, ST, L, AI) TMCMarlin<IC##Stepper, L, AI> stepper##ST(ST##_CS_PIN, float(ST##_RSENSE), TMC_SPI_MOSI, TMC_SPI_MISO, TMC_SPI_SCK, ST##_CHAIN_POS, true)
 #else
+#if !defined(TMC_SPI_MISO) || !defined(TMC_SPI_MOSI) || !defined(TMC_SPI_SCK)
+  // define in case the pins are unknown/not-definable/fixed.
+  #define __TMC_SPI_DEFINE(IC, ST, L, AI) TMCMarlin<IC##Stepper, L, AI> stepper##ST(ST##_CS_PIN, float(ST##_RSENSE), ST##_CHAIN_POS)
+#else
   #define __TMC_SPI_DEFINE(IC, ST, L, AI) TMCMarlin<IC##Stepper, L, AI> stepper##ST(ST##_CS_PIN, float(ST##_RSENSE), TMC_SPI_MOSI, TMC_SPI_MISO, TMC_SPI_SCK, ST##_CHAIN_POS, false)
+#endif
 #endif
 
 #if ENABLED(TMC_SERIAL_MULTIPLEXER)
