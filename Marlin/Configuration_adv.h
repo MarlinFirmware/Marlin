3fad9f04c8d89ad7ed0b778ac17b2e90648cc1d0
EvilGremlin <evilgremlin8@gmail.com>
2022-10-25 01:28:21 +0300

    right config defaults

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1434,11 +1434,11 @@
 
 #endif // HAS_DISPLAY || DWIN_LCD_PROUI
 
-// Add 'M73' to set print job progress, overriding Marlin's built-in estimate
+// Add 'M73' to set print job progress, overrides Marlin's built-in estimate
 //#define SET_PROGRESS_MANUALLY
 #if ENABLED(SET_PROGRESS_MANUALLY)
   #define SET_PROGRESS_PERCENT            // Add 'P' parameter to set percentage done
-  //#define SET_REMAINING_TIME            // Add 'R' parameter to set remaining time
+  #define SET_REMAINING_TIME              // Add 'R' parameter to set remaining time
   //#define SET_INTERACTION_TIME          // Add 'C' parameter to set time until next filament change or other user interaction
   //#define M73_REPORT                    // Report M73 values to host
   #if BOTH(M73_REPORT, SDDUPPORT)
