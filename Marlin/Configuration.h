4b61de95751cc573dc3df9e9dfbd96858510238b
Kyle Conquergood <kyleconquergood@gmail.com>
2022-10-31 00:20:20 -0700

    Set stepper motor directions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1637,7 +1637,7 @@
 // @section motion
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
+#define INVERT_X_DIR true
 #define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 //#define INVERT_I_DIR false
