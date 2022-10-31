8411371d0984ab0d7525919d6d2ec40be16a305c
Kyle Conquergood <kyleconquergood@gmail.com>
2022-10-31 02:47:03 -0700

    Reversed rotary encoder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2501,7 +2501,7 @@
 //
 //  Set this option if CLOCKWISE causes values to DECREASE
 //
-//#define REVERSE_ENCODER_DIRECTION
+#define REVERSE_ENCODER_DIRECTION
 
 //
 // This option reverses the encoder direction for navigating LCD menus.
