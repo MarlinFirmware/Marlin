cd19051e1cfcaf5571f5b8472e9044e40c8dcdd4
Kyle Conquergood <kyleconquergood@gmail.com>
2022-10-31 04:48:20 -0700

    Changed thermistor to reflect ED3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -535,7 +535,7 @@
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  *
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 5
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
