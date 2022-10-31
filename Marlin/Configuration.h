9e6ab2b546724f848f91bbb2ce28d2ef2074b468
Kyle Conquergood <kyleconquergood@gmail.com>
2022-10-30 23:44:11 -0700

    Dissabled probe reference for z homeing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1271,7 +1271,7 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
 //#define USE_PROBE_FOR_Z_HOMING
