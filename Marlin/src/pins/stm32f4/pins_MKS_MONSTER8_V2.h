84e90fa4ebfa7a15d41bac1b35d11e6938916f4a
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-11-13 22:43:50 -0600

    Update pins_MKS_MONSTER8_V2.h

--- Marlin/src/pins/stm32f4/pins_MKS_MONSTER8_V2.h
+++ Marlin/src/pins/stm32f4/pins_MKS_MONSTER8_V2.h
@@ -58,6 +58,6 @@
   #define WIFI_RESET_PIN PD14 // MKS ESP WIFI RESET PIN
 #endif
 
-#define NEOPIXEL_PIN                        PC5
+#define NEOPIXEL_PIN                       PC5
 
 #include "pins_MKS_MONSTER8_common.h"
