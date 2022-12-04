6d1d5e4e6a13b055e78aebbae98eda1868051522
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-12-04 15:35:45 -0600

    🔨 Fix CI Test clean step (2)

--- buildroot/share/PlatformIO/scripts/pioutil.py
+++ buildroot/share/PlatformIO/scripts/pioutil.py
@@ -6,7 +6,8 @@
 def is_pio_build():
     from SCons.Script import DefaultEnvironment
     env = DefaultEnvironment()
-    return not env.IsIntegrationDump() and not env.IsCleanTarget()
+    if "IsCleanTarget" in dir(env) and env.IsCleanTarget(): return False
+    return not env.IsIntegrationDump()
 
 def get_pio_version():
     from platformio import util
