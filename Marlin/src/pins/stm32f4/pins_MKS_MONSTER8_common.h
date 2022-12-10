4e89f8a40cbcff256447ca32627052b75223b2fc
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-12-10 11:03:20 -0600

    * Always define TMC SPI

--- Marlin/src/pins/stm32f4/pins_MKS_MONSTER8_common.h
+++ Marlin/src/pins/stm32f4/pins_MKS_MONSTER8_common.h
@@ -132,16 +132,17 @@
 #if HAS_TMC_SPI
   #define TMC_USE_SW_SPI
 #endif
-#if ENABLED(TMC_USE_SW_SPI)
-  #if !defined(TMC_SPI_MOSI) || TMC_SPI_MOSI == -1
-    #define TMC_SPI_MOSI                    PE14
-  #endif
-  #if !defined(TMC_SPI_MISO) || TMC_SPI_MISO == -1
-    #define TMC_SPI_MISO                    PE13
-  #endif
-  #if !defined(TMC_SPI_SCK) || TMC_SPI_SCK == -1
-    #define TMC_SPI_SCK                     PE12
-  #endif
+#if !defined(TMC_SPI_MOSI) || TMC_SPI_MOSI == -1
+  #undef TMC_SPI_MOSI
+  #define TMC_SPI_MOSI                      PE14
+#endif
+#if !defined(TMC_SPI_MISO) || TMC_SPI_MISO == -1
+  #undef TMC_SPI_MISO
+  #define TMC_SPI_MISO                      PE13
+#endif
+#if !defined(TMC_SPI_SCK) || TMC_SPI_SCK == -1
+  #undef TMC_SPI_SCK
+  #define TMC_SPI_SCK                       PE12
 #endif
 
 #if HAS_TMC_UART
