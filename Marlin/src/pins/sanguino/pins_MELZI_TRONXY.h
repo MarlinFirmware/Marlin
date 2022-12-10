9949672f1d303d786e20cd053701fcd5c61de736
ellensp <530024+ellensp@users.noreply.github.com>
2022-12-11 04:00:51 +1300

    📌 Pins updates for Longer LK5, etc. (#25012)
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/src/pins/sanguino/pins_MELZI_TRONXY.h
+++ Marlin/src/pins/sanguino/pins_MELZI_TRONXY.h
@@ -27,6 +27,24 @@
 
 #define BOARD_INFO_NAME "Melzi (Tronxy)"
 
+#define Z_ENABLE_PIN                          14
+
+#define LCD_SDSS                              -1
+
+#if ENABLED(CR10_STOCKDISPLAY)
+  #define LCD_PINS_RS                         30
+  #define LCD_PINS_ENABLE                     28
+  #define LCD_PINS_D4                         16
+  #define LCD_PINS_D5                         17
+  #define LCD_PINS_D6                         27
+  #define LCD_PINS_D7                         29
+  #define BTN_EN1                             10
+  #define BTN_EN2                             11
+  #define BTN_ENC                             26
+
+  #define LCD_PINS_DEFINED
+#endif
+
 // Alter timing for graphical display
 #if IS_U8GLIB_ST7920
   #define BOARD_ST7920_DELAY_1                 0
@@ -35,26 +53,3 @@
 #endif
 
 #include "pins_MELZI.h" // ... SANGUINOLOLU_12 ... SANGUINOLOLU_11
-
-#undef Z_ENABLE_PIN
-#undef LCD_PINS_RS
-#undef LCD_PINS_ENABLE
-#undef LCD_PINS_D4
-#undef LCD_PINS_D5
-#undef LCD_PINS_D6
-#undef LCD_PINS_D7
-#undef BTN_EN1
-#undef BTN_EN2
-#undef BTN_ENC
-#undef LCD_SDSS
-
-#define Z_ENABLE_PIN                          14
-#define LCD_PINS_RS                           30
-#define LCD_PINS_ENABLE                       28
-#define LCD_PINS_D4                           16
-#define LCD_PINS_D5                           17
-#define LCD_PINS_D6                           27
-#define LCD_PINS_D7                           29
-#define BTN_EN1                               10
-#define BTN_EN2                               11
-#define BTN_ENC                               26
