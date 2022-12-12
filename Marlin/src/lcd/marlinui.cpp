02a247ac20579c21aae6746319fb34eca29a15df
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-12-12 15:53:12 -0600

    etc

--- Marlin/src/lcd/marlinui.cpp
+++ Marlin/src/lcd/marlinui.cpp
@@ -269,12 +269,8 @@ void MarlinUI::init() {
     slow_buttons = 0;
   #endif
 
-  #if HAS_U8GLIB_I2C_OLED && DISABLED(SOFT_I2C_EEPROM)
-    Wire.begin(
-      #if PINS_EXIST(I2C_SCL, I2C_SDA)
-        int(I2C_SDA_PIN), int(I2C_SCL_PIN)
-      #endif
-    );
+  #if HAS_U8GLIB_I2C_OLED && PINS_EXIST(I2C_SCL, I2C_SDA) && DISABLED(SOFT_I2C_EEPROM)
+    Wire.begin(int(I2C_SDA_PIN), int(I2C_SCL_PIN));
   #endif
 
   update_buttons();
