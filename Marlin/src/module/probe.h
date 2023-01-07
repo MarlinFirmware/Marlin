9efef17099a8575cabeb08434020206665a831a3
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-01-06 22:11:53 -0600

    allow Z_AFTER_HOMING decimal point

--- Marlin/src/module/probe.h
+++ Marlin/src/module/probe.h
@@ -163,7 +163,7 @@ public:
   static void move_z_after_homing() {
     #if ALL(DWIN_LCD_PROUI, INDIVIDUAL_AXIS_HOMING_SUBMENU, MESH_BED_LEVELING)
       do_z_clearance(HMI_data.z_after_homing, true);
-    #elif Z_AFTER_HOMING
+    #elif defined(Z_AFTER_HOMING)
       do_z_clearance(Z_AFTER_HOMING, true);
     #elif BOTH(Z_AFTER_PROBING, HAS_BED_PROBE)
       move_z_after_probing();
