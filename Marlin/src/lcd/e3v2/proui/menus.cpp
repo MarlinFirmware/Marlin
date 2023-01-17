f8c7ec39df7e4a714a62206262e12a842c3b651c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-01-16 22:02:52 -0600

    NO YOU CANNOT RELICENSE GPLv3

--- Marlin/src/lcd/e3v2/proui/menus.cpp
+++ Marlin/src/lcd/e3v2/proui/menus.cpp
@@ -38,7 +38,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  */
 
 #include "../../../inc/MarlinConfigPre.h"
--- Marlin/src/lcd/e3v2/proui/plot.h
+++ Marlin/src/lcd/e3v2/proui/plot.h
@@ -38,8 +38,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * For commercial applications additional licenses can be requested
  */
 #pragma once
 
