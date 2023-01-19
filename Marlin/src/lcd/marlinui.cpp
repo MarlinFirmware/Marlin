f7f5611c6b03ffd732ec54105451de24e17c5d23
ellensp <530024+ellensp@users.noreply.github.com>
2023-01-19 20:24:33 +1300

    patch 25249 also

--- Marlin/src/lcd/marlinui.cpp
+++ Marlin/src/lcd/marlinui.cpp
@@ -1746,19 +1746,21 @@ void MarlinUI::init() {
   #if LCD_WITH_BLINK && DISABLED(HAS_GRAPHICAL_TFT)
     typedef void (*PrintProgress_t)();
     void MarlinUI::rotate_progress() { // Renew and redraw all enabled progress strings
-      const PrintProgress_t progFunc[] = {
-        OPTITEM(SHOW_PROGRESS_PERCENT, drawPercent)
-        OPTITEM(SHOW_ELAPSED_TIME, drawElapsed)
-        OPTITEM(SHOW_REMAINING_TIME, drawRemain)
-        OPTITEM(SHOW_INTERACTION_TIME, drawInter)
-      };
-      static bool prev_blink;
-      static uint8_t i;
-      if (prev_blink != get_blink()) {
-        prev_blink = get_blink();
-        if (++i >= COUNT(progFunc)) i = 0;
-        (*progFunc[i])();
-      }
+      #if ANY(SHOW_PROGRESS_PERCENT,SHOW_ELAPSED_TIME,SHOW_REMAINING_TIME,SHOW_INTERACTION_TIME)
+        const PrintProgress_t progFunc[] = {
+          OPTITEM(SHOW_PROGRESS_PERCENT, drawPercent)
+          OPTITEM(SHOW_ELAPSED_TIME, drawElapsed)
+          OPTITEM(SHOW_REMAINING_TIME, drawRemain)
+          OPTITEM(SHOW_INTERACTION_TIME, drawInter)
+        };
+        static bool prev_blink;
+        static uint8_t i;
+        if (prev_blink != get_blink()) {
+          prev_blink = get_blink();
+          if (++i >= COUNT(progFunc)) i = 0;
+          (*progFunc[i])();
+        }
+      #endif
     }
   #endif
 
