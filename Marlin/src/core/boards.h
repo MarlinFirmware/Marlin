509a9926805a2067f8f7821c208893e299f94195
Michael Hill <mhill@hillsoftware.com>
2023-01-22 14:05:49 -0500

    Added Sovol Defintion
    Added the definition for the Sovol 1.3.1 mainboard used in the SV06

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -372,6 +372,7 @@
 #define BOARD_ZONESTAR_ZM3E4V2        4067  // Zonestar ZM3E4 V2 (STM32F103VC)
 #define BOARD_ERYONE_ERY32_MINI       4068  // Eryone Ery32 mini (STM32F103VE)
 #define BOARD_PANDA_PI_V29            4069  // Panda Pi V2.9 - Standalone (STM32F103RC)
+#define BOARD_SOVOL_V131              4070  // Sovol V1.3.1 (GD32F103RET6)
 
 //
 // ARM Cortex-M4F
