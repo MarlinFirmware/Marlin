d2327202ea797386adaa14df365c8f43a4c5fb66
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-01-21 23:31:41 -0600

    ENDER2_STOCKDISPLAY is like MKS_MINI_12864

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -358,11 +358,11 @@
     #define BEEPER_PIN               EXP1_01_PIN
 
   #elif ENABLED(CR10_STOCKDISPLAY)
-    #define BTN_ENC                  EXP1_02_PIN  // (58) open-drain
     #define LCD_PINS_RS              EXP1_07_PIN
 
     #define BTN_EN1                  EXP1_03_PIN
     #define BTN_EN2                  EXP1_05_PIN
+    #define BTN_ENC                  EXP1_02_PIN
 
     #define LCD_PINS_ENABLE          EXP1_08_PIN
     #define LCD_PINS_D4              EXP1_06_PIN
