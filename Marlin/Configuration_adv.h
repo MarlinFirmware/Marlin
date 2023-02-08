3ef5466f9acb836c859e040ac0f1ac44b2034aa5
Alex Muthmann <deveth0@users.noreply.github.com>
2023-02-08 08:15:33 +0100

    Enable progress / remaining time

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1716,13 +1716,13 @@
 #endif // HAS_DISPLAY
 
 // Add 'M73' to set print job progress, overrides Marlin's built-in estimate
-//#define SET_PROGRESS_MANUALLY
+#define SET_PROGRESS_MANUALLY
 #if ENABLED(SET_PROGRESS_MANUALLY)
 #define SET_PROGRESS_PERCENT // Add 'P' parameter to set percentage done
 #define SET_REMAINING_TIME   // Add 'R' parameter to set remaining time
-//#define SET_INTERACTION_TIME          // Add 'C' parameter to set time until
+#define SET_INTERACTION_TIME          // Add 'C' parameter to set time until
 // next filament change or other user interaction
-//#define M73_REPORT                    // Report M73 values to host
+#define M73_REPORT                    // Report M73 values to host
 #if BOTH(M73_REPORT, SDSUPPORT)
 #define M73_REPORT_SD_ONLY // Report only when printing from SD
 #endif
@@ -1733,7 +1733,7 @@
 #define SHOW_PROGRESS_PERCENT // Show print progress percentage (doesn't affect
                               // progress bar)
 #define SHOW_ELAPSED_TIME     // Display elapsed printing time (prefix 'E')
-//#define SHOW_REMAINING_TIME           // Display estimated time to completion
+#define SHOW_REMAINING_TIME           // Display estimated time to completion
 //(prefix 'R')
 #if ENABLED(SET_INTERACTION_TIME)
 #define SHOW_INTERACTION_TIME // Display time until next user interaction ('C' =
