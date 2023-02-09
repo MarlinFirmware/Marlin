ee4c4f7f40a181dd679a154e75ea853ba2b9bd0e
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2023-02-08 23:23:46 -0800

    Fix TOUCH_SCREEN_CALIBRATION Invalid First Point
    Avoid registering first touch point as top-left corner when initiating touch calibration since user is touching TFT

--- Marlin/src/lcd/tft_io/touch_calibration.cpp
+++ Marlin/src/lcd/tft_io/touch_calibration.cpp
@@ -92,6 +92,13 @@ void TouchCalibration::validate_calibration() {
 bool TouchCalibration::handleTouch(uint16_t x, uint16_t y) {
   static millis_t next_button_update_ms = 0;
   const millis_t now = millis();
+
+  // Avoid registering first touch point as top-left corner when initiating touch calibration since user is touching TFT
+  if (next_button_update_ms == 0) {
+    next_button_update_ms = now + BUTTON_DELAY_MENU;
+    return true;
+  }
+
   if (PENDING(now, next_button_update_ms)) return false;
   next_button_update_ms = now + BUTTON_DELAY_MENU;
 
