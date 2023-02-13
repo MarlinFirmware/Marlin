dc318343791d652f0cfc3570781d4eac9cf3171a
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-02-12 18:19:36 -0600

    localize some defines

--- Marlin/src/sd/Sd2Card.cpp
+++ Marlin/src/sd/Sd2Card.cpp
@@ -40,6 +40,29 @@
 
 #include "../MarlinCore.h"
 
+#if DISABLED(SD_NO_DEFAULT_TIMEOUT)
+  #ifndef SD_INIT_TIMEOUT
+    #define SD_INIT_TIMEOUT 2000u   // (ms) Init timeout
+  #elif SD_INIT_TIMEOUT < 0
+    #error "SD_INIT_TIMEOUT must be greater than or equal to 0."
+  #endif
+  #ifndef SD_ERASE_TIMEOUT
+    #define SD_ERASE_TIMEOUT 10000u // (ms) Erase timeout
+  #elif SD_ERASE_TIMEOUT < 0
+    #error "SD_ERASE_TIMEOUT must be greater than or equal to 0."
+  #endif
+  #ifndef SD_READ_TIMEOUT
+    #define SD_READ_TIMEOUT 300u    // (ms) Read timeout
+  #elif SD_READ_TIMEOUT < 0
+    #error "SD_READ_TIMEOUT must be greater than or equal to 0."
+  #endif
+  #ifndef SD_WRITE_TIMEOUT
+    #define SD_WRITE_TIMEOUT 600u   // (ms) Write timeout
+  #elif SD_WRITE_TIMEOUT < 0
+    #error "SD_WRITE_TIMEOUT must be greater than or equal to 0."
+  #endif
+#endif // SD_NO_DEFAULT_TIMEOUT
+
 #if ENABLED(SD_CHECK_AND_RETRY)
   #ifndef SD_RETRY_COUNT
     #define SD_RETRY_COUNT 3
