66d17072a3434b295a96cbccce8d304c656f76e4
mo0ony68 <124028815+mo0ony68@users.noreply.github.com>
2023-02-13 17:32:40 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -135,7 +135,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "RI3 V2.1.2 SKR BLT"
+//#define CUSTOM_MACHINE_NAME "RI3 SKR BLT"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
