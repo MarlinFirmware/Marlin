a700d46f78c94f49e63c0c6b266869f0f7f7acb4
Shaneh125 <126294819+Shaneh125@users.noreply.github.com>
2023-02-26 13:54:35 +0000

    Changes to be committed: 	modified:   Marlin/Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2922,7 +2922,7 @@
 // This is RAMPS-compatible using a single 10-pin connector.
 // (For CR-10 owners who want to replace the Melzi Creality board but retain the display)
 //
-#define CR10_STOCKDISPLAY
+//#define CR10_STOCKDISPLAY
 
 //
 // Ender-2 OEM display, a variant of the MKS_MINI_12864
