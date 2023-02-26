a8f28342a57cd62f5cfe06f189ee2406273d84ac
Shaneh125 <126294819+Shaneh125@users.noreply.github.com>
2023-02-26 13:48:33 +0000

    Changes to be committed: 	modified:   Marlin/Configuration.h 	modified:   Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_common.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -98,7 +98,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 2
+#define SERIAL_PORT -1
 
 /**
  * Serial Port Baud Rate
@@ -120,7 +120,7 @@
  * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT_2 -1
+//#define SERIAL_PORT_2 -1
 //#define BAUDRATE_2 250000   // :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000] Enable to override BAUDRATE
 
 /**
