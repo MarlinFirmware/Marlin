284cc8f4c001d8bacc9437741778e5b097a25993
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-03-25 20:40:44 -0500

    🧑‍💻 Update Python indentation

--- buildroot/share/PlatformIO/scripts/preprocessor.py
+++ buildroot/share/PlatformIO/scripts/preprocessor.py
@@ -7,8 +7,8 @@ nocache = 1
 verbose = 0
 
 def blab(str):
-	if verbose:
-		print(str)
+    if verbose:
+        print(str)
 
 ################################################################################
 #
@@ -16,36 +16,36 @@ def blab(str):
 #
 preprocessor_cache = {}
 def run_preprocessor(env, fn=None):
-	filename = fn or 'buildroot/share/PlatformIO/scripts/common-dependencies.h'
-	if filename in preprocessor_cache:
-		return preprocessor_cache[filename]
+    filename = fn or 'buildroot/share/PlatformIO/scripts/common-dependencies.h'
+    if filename in preprocessor_cache:
+        return preprocessor_cache[filename]
 
-	# Process defines
-	build_flags = env.get('BUILD_FLAGS')
-	build_flags = env.ParseFlagsExtended(build_flags)
+    # Process defines
+    build_flags = env.get('BUILD_FLAGS')
+    build_flags = env.ParseFlagsExtended(build_flags)
 
-	cxx = search_compiler(env)
-	cmd = ['"' + cxx + '"']
+    cxx = search_compiler(env)
+    cmd = ['"' + cxx + '"']
 
-	# Build flags from board.json
-	#if 'BOARD' in env:
-	#	cmd += [env.BoardConfig().get("build.extra_flags")]
-	for s in build_flags['CPPDEFINES']:
-		if isinstance(s, tuple):
-			cmd += ['-D' + s[0] + '=' + str(s[1])]
-		else:
-			cmd += ['-D' + s]
+    # Build flags from board.json
+    #if 'BOARD' in env:
+    #   cmd += [env.BoardConfig().get("build.extra_flags")]
+    for s in build_flags['CPPDEFINES']:
+        if isinstance(s, tuple):
+            cmd += ['-D' + s[0] + '=' + str(s[1])]
+        else:
+            cmd += ['-D' + s]
 
-	cmd += ['-D__MARLIN_DEPS__ -w -dM -E -x c++']
-	depcmd = cmd + [ filename ]
-	cmd = ' '.join(depcmd)
-	blab(cmd)
-	try:
-		define_list = subprocess.check_output(cmd, shell=True).splitlines()
-	except:
-		define_list = {}
-	preprocessor_cache[filename] = define_list
-	return define_list
+    cmd += ['-D__MARLIN_DEPS__ -w -dM -E -x c++']
+    depcmd = cmd + [ filename ]
+    cmd = ' '.join(depcmd)
+    blab(cmd)
+    try:
+        define_list = subprocess.check_output(cmd, shell=True).splitlines()
+    except:
+        define_list = {}
+    preprocessor_cache[filename] = define_list
+    return define_list
 
 
 ################################################################################
@@ -54,51 +54,51 @@ def run_preprocessor(env, fn=None):
 #
 def search_compiler(env):
 
-	ENV_BUILD_PATH = os.path.join(env['PROJECT_BUILD_DIR'], env['PIOENV'])
-	GCC_PATH_CACHE = os.path.join(ENV_BUILD_PATH, ".gcc_path")
+    ENV_BUILD_PATH = os.path.join(env['PROJECT_BUILD_DIR'], env['PIOENV'])
+    GCC_PATH_CACHE = os.path.join(ENV_BUILD_PATH, ".gcc_path")
 
-	try:
-		filepath = env.GetProjectOption('custom_gcc')
-		blab("Getting compiler from env")
-		return filepath
-	except:
-		pass
+    try:
+        filepath = env.GetProjectOption('custom_gcc')
+        blab("Getting compiler from env")
+        return filepath
+    except:
+        pass
 
-	# Warning: The cached .gcc_path will obscure a newly-installed toolkit
-	if not nocache and os.path.exists(GCC_PATH_CACHE):
-		blab("Getting g++ path from cache")
-		with open(GCC_PATH_CACHE, 'r') as f:
-			return f.read()
+    # Warning: The cached .gcc_path will obscure a newly-installed toolkit
+    if not nocache and os.path.exists(GCC_PATH_CACHE):
+        blab("Getting g++ path from cache")
+        with open(GCC_PATH_CACHE, 'r') as f:
+            return f.read()
 
-	# Find the current platform compiler by searching the $PATH
-	# which will be in a platformio toolchain bin folder
-	path_regex = re.escape(env['PROJECT_PACKAGES_DIR'])
-	gcc = "g++"
-	if env['PLATFORM'] == 'win32':
-		path_separator = ';'
-		path_regex += r'.*\\bin'
-		gcc += ".exe"
-	else:
-		path_separator = ':'
-		path_regex += r'/.+/bin'
+    # Find the current platform compiler by searching the $PATH
+    # which will be in a platformio toolchain bin folder
+    path_regex = re.escape(env['PROJECT_PACKAGES_DIR'])
+    gcc = "g++"
+    if env['PLATFORM'] == 'win32':
+        path_separator = ';'
+        path_regex += r'.*\\bin'
+        gcc += ".exe"
+    else:
+        path_separator = ':'
+        path_regex += r'/.+/bin'
 
-	# Search for the compiler
-	for pathdir in env['ENV']['PATH'].split(path_separator):
-		if not re.search(path_regex, pathdir, re.IGNORECASE):
-			continue
-		for filepath in os.listdir(pathdir):
-			if not filepath.endswith(gcc):
-				continue
-			# Use entire path to not rely on env PATH
-			filepath = os.path.sep.join([pathdir, filepath])
-			# Cache the g++ path to no search always
-			if not nocache and os.path.exists(ENV_BUILD_PATH):
-				blab("Caching g++ for current env")
-				with open(GCC_PATH_CACHE, 'w+') as f:
-					f.write(filepath)
+    # Search for the compiler
+    for pathdir in env['ENV']['PATH'].split(path_separator):
+        if not re.search(path_regex, pathdir, re.IGNORECASE):
+            continue
+        for filepath in os.listdir(pathdir):
+            if not filepath.endswith(gcc):
+                continue
+            # Use entire path to not rely on env PATH
+            filepath = os.path.sep.join([pathdir, filepath])
+            # Cache the g++ path to no search always
+            if not nocache and os.path.exists(ENV_BUILD_PATH):
+                blab("Caching g++ for current env")
+                with open(GCC_PATH_CACHE, 'w+') as f:
+                    f.write(filepath)
 
-			return filepath
+            return filepath
 
-	filepath = env.get('CXX')
-	blab("Couldn't find a compiler! Fallback to %s" % filepath)
-	return filepath
+    filepath = env.get('CXX')
+    blab("Couldn't find a compiler! Fallback to %s" % filepath)
+    return filepath
--- buildroot/share/scripts/config-labels.py
+++ buildroot/share/scripts/config-labels.py
@@ -47,152 +47,152 @@ different_out_dir = not (output_examples_dir == input_examples_dir)
 #----------------------------------------------
 def process_file(subdir: str, filename: str):
 #----------------------------------------------
-	global filenum
-	filenum += 1
+    global filenum
+    filenum += 1
 
-	print(str(filenum) + '  ' + filename + ':  ' + subdir)
+    print(str(filenum) + '  ' + filename + ':  ' + subdir)
 
-	def_line = (def_macro_name + ' "'  + subdir.replace('\\', '/')  + '"')
+    def_line = (def_macro_name + ' "'  + subdir.replace('\\', '/')  + '"')
 
-	#------------------------
-	# Read file
-	#------------------------
-	lines = []
-	infilepath = os.path.join(input_examples_dir, subdir, filename)
-	try:
-		# UTF-8 because some files contain unicode chars
-		with open(infilepath, 'rt', encoding="utf-8") as infile:
-			lines = infile.readlines()
+    #------------------------
+    # Read file
+    #------------------------
+    lines = []
+    infilepath = os.path.join(input_examples_dir, subdir, filename)
+    try:
+        # UTF-8 because some files contain unicode chars
+        with open(infilepath, 'rt', encoding="utf-8") as infile:
+            lines = infile.readlines()
 
-	except Exception as e:
-		print('Failed to read file: ' + str(e) )
-		raise Exception
+    except Exception as e:
+        print('Failed to read file: ' + str(e) )
+        raise Exception
 
-	lines = [line.rstrip('\r\n') for line in lines]
+    lines = [line.rstrip('\r\n') for line in lines]
 
-	#------------------------
-	# Process lines
-	#------------------------
-	file_modified    = False
+    #------------------------
+    # Process lines
+    #------------------------
+    file_modified    = False
 
-	# region state machine
-	# -1 = before pragma once;
-	# 0 = region to place define;
-	# 1 = past region to place define
-	region      = -1
+    # region state machine
+    # -1 = before pragma once;
+    # 0 = region to place define;
+    # 1 = past region to place define
+    region      = -1
 
-	outlines = []
-	for line in lines:
-		outline = line
+    outlines = []
+    for line in lines:
+        outline = line
 
-		if (region == -1) and (def_macro_name in line):
-			outline       = None
-			file_modified = True
+        if (region == -1) and (def_macro_name in line):
+            outline       = None
+            file_modified = True
 
-		elif (region == -1) and ('pragma once' in line):
-			region = 0
+        elif (region == -1) and ('pragma once' in line):
+            region = 0
 
-		elif (region == 0):
-			if (line.strip() == ''):
-				pass
-			elif (def_macro_name in line):
-				region = 1
-				if line == def_line:   # leave it as is
-					pass
-				else:
-					outline       = def_line
-					file_modified = True
-			else: # some other string
-				outlines.append(def_line)
-				outlines.append('')
-				region = 1
-				file_modified = True
+        elif (region == 0):
+            if (line.strip() == ''):
+                pass
+            elif (def_macro_name in line):
+                region = 1
+                if line == def_line:   # leave it as is
+                    pass
+                else:
+                    outline       = def_line
+                    file_modified = True
+            else: # some other string
+                outlines.append(def_line)
+                outlines.append('')
+                region = 1
+                file_modified = True
 
-		elif (region == 1):
-			if (def_macro_name in line):
-				outline       = None
-				file_modified = True
-			else:
-				pass
+        elif (region == 1):
+            if (def_macro_name in line):
+                outline       = None
+                file_modified = True
+            else:
+                pass
 
-		# end if
-		if outline is not None:
-			outlines.append(outline)
-	# end for
+        # end if
+        if outline is not None:
+            outlines.append(outline)
+    # end for
 
-	#-------------------------
-	#     Output file
-	#-------------------------
-	outdir      = os.path.join(output_examples_dir, subdir)
-	outfilepath = os.path.join(outdir, filename)
+    #-------------------------
+    #     Output file
+    #-------------------------
+    outdir      = os.path.join(output_examples_dir, subdir)
+    outfilepath = os.path.join(outdir, filename)
 
-	if file_modified:
-		# Note: no need to create output dirs, as the initial copy_tree
-		# will do that.
+    if file_modified:
+        # Note: no need to create output dirs, as the initial copy_tree
+        # will do that.
 
-		print('  writing ' + str(outfilepath))
-		try:
-			# Preserve unicode chars; Avoid CR-LF on Windows.
-			with open(outfilepath, "w", encoding="utf-8", newline='\n') as outfile:
-				outfile.write("\n".join(outlines))
-				outfile.write("\n")
+        print('  writing ' + str(outfilepath))
+        try:
+            # Preserve unicode chars; Avoid CR-LF on Windows.
+            with open(outfilepath, "w", encoding="utf-8", newline='\n') as outfile:
+                outfile.write("\n".join(outlines))
+                outfile.write("\n")
 
-		except Exception as e:
-			print('Failed to write file: ' + str(e) )
-			raise Exception
-	else:
-		print('  no change for ' + str(outfilepath))
+        except Exception as e:
+            print('Failed to write file: ' + str(e) )
+            raise Exception
+    else:
+        print('  no change for ' + str(outfilepath))
 
 #----------
 def main():
 #----------
-	global filenum
-	global input_examples_dir
-	global output_examples_dir
-	filenum = 0
+    global filenum
+    global input_examples_dir
+    global output_examples_dir
+    filenum = 0
 
-	#--------------------------------
-	# Check for requirements
-	#--------------------------------
-	input_examples_dir  = input_examples_dir.strip()
-	input_examples_dir  = input_examples_dir.rstrip('\\/')
-	output_examples_dir = output_examples_dir.strip()
-	output_examples_dir = output_examples_dir.rstrip('\\/')
+    #--------------------------------
+    # Check for requirements
+    #--------------------------------
+    input_examples_dir  = input_examples_dir.strip()
+    input_examples_dir  = input_examples_dir.rstrip('\\/')
+    output_examples_dir = output_examples_dir.strip()
+    output_examples_dir = output_examples_dir.rstrip('\\/')
 
-	for dir in [input_examples_dir, output_examples_dir]:
-		if not (os.path.exists(dir)):
-			print('Directory not found: ' + dir)
-			sys.exit(1)
+    for dir in [input_examples_dir, output_examples_dir]:
+        if not (os.path.exists(dir)):
+            print('Directory not found: ' + dir)
+            sys.exit(1)
 
-	#--------------------------------
-	# Copy tree if necessary.
-	#--------------------------------
-	# This includes files that are not otherwise included in the
-	# insertion of the define statement.
-	#
-	if different_out_dir:
-		print('Copying files to new directory: ' + output_examples_dir)
-		try:
-			copy_tree(input_examples_dir, output_examples_dir)
-		except Exception as e:
-			print('Failed to copy directory: ' + str(e) )
-			raise Exception
+    #--------------------------------
+    # Copy tree if necessary.
+    #--------------------------------
+    # This includes files that are not otherwise included in the
+    # insertion of the define statement.
+    #
+    if different_out_dir:
+        print('Copying files to new directory: ' + output_examples_dir)
+        try:
+            copy_tree(input_examples_dir, output_examples_dir)
+        except Exception as e:
+            print('Failed to copy directory: ' + str(e) )
+            raise Exception
 
-	#-----------------------------
-	# Find and process files
-	#-----------------------------
-	len_input_examples_dir = len(input_examples_dir);
-	len_input_examples_dir += 1
+    #-----------------------------
+    # Find and process files
+    #-----------------------------
+    len_input_examples_dir = len(input_examples_dir);
+    len_input_examples_dir += 1
 
-	for filename in files_to_mod:
-		input_path = Path(input_examples_dir)
-		filepathlist = input_path.rglob(filename)
+    for filename in files_to_mod:
+        input_path = Path(input_examples_dir)
+        filepathlist = input_path.rglob(filename)
 
-		for filepath in filepathlist:
-			fulldirpath = str(filepath.parent)
-			subdir      = fulldirpath[len_input_examples_dir:]
+        for filepath in filepathlist:
+            fulldirpath = str(filepath.parent)
+            subdir      = fulldirpath[len_input_examples_dir:]
 
-			process_file(subdir, filename)
+            process_file(subdir, filename)
 
 #==============
 print('--- Starting config-labels ---')
