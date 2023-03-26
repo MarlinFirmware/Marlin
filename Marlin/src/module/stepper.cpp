2f18457e04dcd0d70b527500079c0435770a8912
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-03-25 20:18:31 -0500

    style tweaks

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -2094,10 +2094,8 @@ hal_timer_t Stepper::calc_timer_interval(uint32_t step_rate) {
 
   #ifdef CPU_32_BIT
 
-    if (step_rate != 0)
-      return uint32_t(STEPPER_TIMER_RATE) / step_rate; // A fast processor can just do integer division
-
-    return HAL_TIMER_TYPE_MAX;
+    // A fast processor can just do integer division
+    return step_rate ? uint32_t(STEPPER_TIMER_RATE) / step_rate : HAL_TIMER_TYPE_MAX;
 
   #else
 
