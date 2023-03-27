90fe147433f24a2d676b51c71b4b10d296175064
John Unland <unlandj2012@gmail.com>
2023-03-27 08:51:11 -0500

    simplify python script

--- scripts/get_test_targets.py
+++ scripts/get_test_targets.py
@@ -2,15 +2,10 @@
 """
 Extract the builds used in Github CI, so that we can run them locally
 """
-import sys, yaml
+import yaml
 
-# Handle the case where the user doesn't provide a yaml file
-if len(sys.argv) != 2:
-    print('Usage: get_test_targets.py <path to yaml file>')
-    sys.exit(1)
-
-# Get the yaml file from the command line
-yaml_file = sys.argv[1]
+# Set the yaml file to parse
+yaml_file = '.github/workflows/test-builds.yml'
 
 # Parse the yaml file, and load it into a dictionary (github_configuration)
 with open(yaml_file) as f:
@@ -18,6 +13,3 @@ with open(yaml_file) as f:
 
 # Print out the test platforms
 print(' '.join(github_configuration['jobs']['test_builds']['strategy']['matrix']['test-platform']))
-
-# Exit with success
-sys.exit(0)
