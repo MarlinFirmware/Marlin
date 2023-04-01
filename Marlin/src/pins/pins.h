19e32a74b62e25cf10f2a28fb2bb8e4bc17cbb00
I3DBeeTech <129617321+I3DBeeTech@users.noreply.github.com>
2023-04-02 00:27:07 +0530

    New Board I3DBEEZ9
    stm32f407zgt6 Based 9 Drive supported board.

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -731,6 +731,8 @@
   #include "stm32f4/pins_MKS_SKIPR_V1_0.h"      // STM32F4                                env:mks_skipr_v1 env:mks_skipr_v1_nobootloader
 #elif MB(TRONXY_V10)
   #include "stm32f4/pins_TRONXY_V10.h"          // STM32F4                                env:STM32F446_tronxy
+#elif MB(I3DBEEZ9_V1)
+  #include "stm32f4/pins_I3DBEEZ9.h"    		// STM32F4                                env:I3DBEEZ9_V1
 
 //
 // ARM Cortex M7
