98ec0be92141b34729a2d6d43c038b7b9442924b
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-04-05 17:18:12 -0500

    BLTOUCH HS clearance 7mm

--- Marlin/src/feature/bltouch.h
+++ Marlin/src/feature/bltouch.h
@@ -22,7 +22,6 @@
 #pragma once
 
 #include "../inc/MarlinConfigPre.h"
-#include "../module/probe.h"
 
 // BLTouch commands are sent as servo angles
 typedef unsigned char BLTCommand;
@@ -77,7 +76,7 @@ public:
     static constexpr bool high_speed_mode = false;
   #endif
 
-  static float z_extra_clearance() { return high_speed_mode ? -probe.offset.z : 0; }
+  static float z_extra_clearance() { return high_speed_mode ? 7 : 0; }
 
   // DEPLOY and STOW are wrapped for error handling - these are used by homing and by probing
   static bool deploy()              { return deploy_proc(); }
