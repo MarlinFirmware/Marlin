989ec9d906bd6e169976fe516960c3ee00b77065
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-04-07 21:08:01 -0500

    rename

--- Marlin/src/HAL/STM32/HAL.h
+++ Marlin/src/HAL/STM32/HAL.h
@@ -28,7 +28,7 @@
 #include "../shared/Marduino.h"
 #include "../shared/math_32bit.h"
 #include "../shared/HAL_SPI.h"
-#include "SoC_temp.h"
+#include "temp_soc.h"
 #include "fastio.h"
 #include "Servo.h"
 #include "MarlinSerial.h"
