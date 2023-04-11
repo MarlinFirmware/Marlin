dbd019f066fee44a2dfa8d41fdbc2f3d90205a69
Ramon de Ruiter <rderuiter@slashwhy.de>
2023-04-11 08:14:24 +0200

    Updated e-steps.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1173,7 +1173,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 96.875 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 97.25 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.1.2-snor-0.6"
+  #define SHORT_BUILD_VERSION "2.1.2-snor-0.7"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2023-04-01"
+  #define STRING_DISTRIBUTION_DATE "2023-04-02"
 #endif
 
 /**
