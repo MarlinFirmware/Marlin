78bdf34b7bc56a0a8aa193cfd824ad2841ed25dc
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-04-14 18:06:56 -0500

    🧑‍💻 HostUI::continue_prompt method

--- Marlin/src/lcd/menu/menu_delta_calibrate.cpp
+++ Marlin/src/lcd/menu/menu_delta_calibrate.cpp
@@ -68,7 +68,7 @@ void _man_probe_pt(const xy_pos_t &xy) {
   float lcd_probe_pt(const xy_pos_t &xy) {
     _man_probe_pt(xy);
     ui.defer_status_screen();
-    TERN_(HOST_PROMPT_SUPPORT, hostui.prompt_do(PROMPT_USER_CONTINUE, GET_TEXT_F(MSG_DELTA_CALIBRATION_IN_PROGRESS), FPSTR(CONTINUE_STR)));
+    TERN_(HOST_PROMPT_SUPPORT, hostui.continue_prompt(GET_TEXT_F(MSG_DELTA_CALIBRATION_IN_PROGRESS)));
     TERN_(EXTENSIBLE_UI, ExtUI::onUserConfirmRequired(GET_TEXT_F(MSG_DELTA_CALIBRATION_IN_PROGRESS)));
     TERN_(HAS_RESUME_CONTINUE, wait_for_user_response());
     ui.goto_previous_screen_no_defer();
