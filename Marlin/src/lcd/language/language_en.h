fe744582507aaa5196981ab15008e4a7b2ea4e7b
Anson Liu <ansonl@users.noreply.github.com>
2023-04-14 22:00:59 -0400

    Update language_en.h

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -849,6 +849,7 @@ namespace LanguageNarrow_en {
   LSTR MSG_DRIVER_BACKWARD                = _UxGT(" driver backward");
 
   LSTR MSG_SD_CARD                        = _UxGT("SD Card");
+  LSTR MSG_SDIO_CARD                      = _UxGT("SDIO Card");
   LSTR MSG_USB_DISK                       = _UxGT("USB Disk");
 
   LSTR MSG_HOST_SHUTDOWN                  = _UxGT("Host Shutdown");
