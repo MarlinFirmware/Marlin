ded9ffa1e7cc154518fe94b5c7da4f4332eb8a27
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-04-20 17:35:20 -0500

    🧑‍💻 Apply INVERT_DIR to SET_STEP_DIR

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -591,14 +591,14 @@ void Stepper::disable_all_steppers() {
   TERN_(EXTENSIBLE_UI, ExtUI::onSteppersDisabled());
 }
 
-#define SET_STEP_DIR(A)                       \
-  if (motor_direction(_AXIS(A))) {            \
-    A##_APPLY_DIR(INVERT_##A##_DIR, false);   \
-    count_direction[_AXIS(A)] = -1;           \
-  }                                           \
-  else {                                      \
-    A##_APPLY_DIR(!INVERT_##A##_DIR, false);  \
-    count_direction[_AXIS(A)] = 1;            \
+#define SET_STEP_DIR(A)                         \
+  if (motor_direction(_AXIS(A))) {              \
+    A##_APPLY_DIR(INVERT_DIR(A, LOW), false);   \
+    count_direction[_AXIS(A)] = -1;             \
+  }                                             \
+  else {                                        \
+    A##_APPLY_DIR(INVERT_DIR(A, HIGH), false);  \
+    count_direction[_AXIS(A)] = 1;              \
   }
 
 /**
