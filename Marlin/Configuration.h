a10b4c08a165120b11ef390f098d2559be440370
Mirco Verona <mircoverona@protonmail.ch>
2023-04-23 16:40:49 +0200

    temp changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -841,7 +841,7 @@
  */
 
 #define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
-#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
+//#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
 #define THERMAL_PROTECTION_CHAMBER // Enable thermal protection for the heated chamber
 #define THERMAL_PROTECTION_COOLER  // Enable thermal protection for the laser cooling
 
