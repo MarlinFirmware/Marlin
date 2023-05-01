006a9e514b68bc10ae069f0715bf5d7a44e5b6a7
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-04-30 23:10:44 -0500

    🩹 NOZZLE_CLEAN_PATTERN_* Followup
    Followup to #25666

--- Marlin/src/gcode/feature/clean/G12.cpp
+++ Marlin/src/gcode/feature/clean/G12.cpp
@@ -64,7 +64,7 @@ void GcodeSuite::G12() {
       NOZZLE_CLEAN_DEFAULT_PATTERN
     #endif
   );
-  const uint8_t strokes = TERN0(NOZZLE_CLEAN_PATTERN_LINEAR, parser.ushortval('S', NOZZLE_CLEAN_STROKES)),
+  const uint8_t strokes = TERN0(NOZZLE_CLEAN_PATTERN_LINE, parser.ushortval('S', NOZZLE_CLEAN_STROKES)),
                 objects = TERN0(NOZZLE_CLEAN_PATTERN_ZIGZAG, parser.ushortval('T', NOZZLE_CLEAN_TRIANGLES));
   const float radius = TERN0(NOZZLE_CLEAN_PATTERN_CIRCLE, parser.linearval('R', NOZZLE_CLEAN_CIRCLE_RADIUS));
 
