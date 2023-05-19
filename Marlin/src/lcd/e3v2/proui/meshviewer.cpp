9669eadaf9524aec66af8996df6324fcc4b33ca6
classicrocker883 <andrewleduc88@yahoo.com>
2023-05-19 17:14:20 -0400

    USE_UBL_VIEWER enabled for bedlevel_tools.h

--- Marlin/src/lcd/e3v2/proui/meshviewer.cpp
+++ Marlin/src/lcd/e3v2/proui/meshviewer.cpp
@@ -40,7 +40,7 @@
 #include "../../../feature/bedlevel/bedlevel.h"
 #include "meshviewer.h"
 
-#if ENABLED(AUTO_BED_LEVELING_UBL)
+#if ENABLED(USE_UBL_VIEWER)
   #include "bedlevel_tools.h"
 #endif
 
