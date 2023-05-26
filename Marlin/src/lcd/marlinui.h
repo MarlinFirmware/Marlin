1e593e5a3f1297bd6e61bd78639575f6a495a247
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-05-25 22:08:06 -0500

    notes and a wrapper

--- Marlin/src/lcd/marlinui.h
+++ Marlin/src/lcd/marlinui.h
@@ -410,7 +410,10 @@ public:
     static void abort_print();
     static void pause_print();
     static void resume_print();
-    static void flow_fault();
+
+    #if ENABLED(FLOWMETER_SAFETY)
+      static void flow_fault();
+    #endif
 
     #if BOTH(HAS_MARLINUI_MENU, PSU_CONTROL)
       static void poweroff();
