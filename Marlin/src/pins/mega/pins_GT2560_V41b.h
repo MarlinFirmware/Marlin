efd0fd37c4b2ca80fb05cf7c708bcf80cfc0b9c5
Pagliarulo Onofrio <oponyx@hotmail.it>
2023-05-27 22:39:13 +0200

    Add board support and fix YHCB2004

---
+++ Marlin/src/pins/mega/pins_GT2560_V41b.h
@@ -0,0 +1,43 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2021 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+/**
+ * Geeetech GT2560 V4.X Pins
+ * Schematic (4.0): https://green-candy.osdn.jp/external/MarlinFW/board_schematics/Geeetech%20GT2560%20V4.x%20+%20A20/GT2560V4.0SCHA20T.pdf
+ * Schematic (4.1B): https://green-candy.osdn.jp/external/MarlinFW/board_schematics/Geeetech%20GT2560%20V4.x%20+%20A20/GT2560V4.1BSCHA20T.pdf
+ * Origin: https://www.geeetech.com/download.html?spm=a2g0s.imconversation.0.0.22d23e5fXlQBWv&download_id=45
+ */
+
+#define BOARD_INFO_NAME "GT2560 4.1b"
+/*
+#define YHCB2004_SS_PIN                     SS
+#define YHCB2004_SCK_PIN                     5
+#define YHCB2004_MOSI_PIN                   21
+#define YHCB2004_MISO_PIN                   36
+*/
+#define YHCB2004_SS_PIN                     5
+#define YHCB2004_SCK_PIN                    21
+#define YHCB2004_MOSI_PIN                   36
+#define YHCB2004_MISO_PIN                   20 // unused pin on V4.1b board
+
+#include "pins_GT2560_V3.h"
