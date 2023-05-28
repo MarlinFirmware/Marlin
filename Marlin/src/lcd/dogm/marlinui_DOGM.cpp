0fccaa500ee32acb147e36534caf610ee99b9bbb
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-05-28 16:22:44 -0500

    FYSETC_MINI_12864_2_1 has no contrast control

--- Marlin/src/lcd/dogm/marlinui_DOGM.cpp
+++ Marlin/src/lcd/dogm/marlinui_DOGM.cpp
@@ -92,11 +92,7 @@ U8G_CLASS u8g;
 #endif
 
 #if HAS_LCD_CONTRAST
-  #if EITHER (NEOPIXEL_LED, FYSETC_MINI_12864_2_1)
-	void MarlinUI::_set_contrast() { u8g.setContrast(255); }
-  #else
-	void MarlinUI::_set_contrast() { u8g.setContrast(contrast); }
-  #endif
+  void MarlinUI::_set_contrast() { u8g.setContrast(contrast); }
 #endif
 
 void MarlinUI::set_font(const MarlinFont font_nr) {
