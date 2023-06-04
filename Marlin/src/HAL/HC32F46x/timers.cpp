bec7dedf887149412a624513b1007b92b3e59f9f
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-06-04 19:07:24 +0200

    minor adjustments to timer HAL

--- Marlin/src/HAL/HC32F46x/timers.cpp
+++ Marlin/src/HAL/HC32F46x/timers.cpp
@@ -3,7 +3,6 @@
 
 /**
  * HAL only uses Timer0 Unit 2 for Temperature and Step interrupts
- * (automatic start() only supports Timer0 Unit 2)
  */
 Timer0 timer0(&TIMER02_config);
 
@@ -12,13 +11,13 @@ void HAL_timer_start(const timer_channel_t timer_num, const uint32_t frequency)
   switch(timer_num)
   {
     case TEMP_TIMER_NUM:
-      CORE_DEBUG_PRINTF("HAL_timer_start: temp timer, f=%ld\n", frequency);
+      CORE_DEBUG_PRINTF("HAL_timer_start: temp timer, f=%ld\n", long(frequency));
       timer0.start(TEMP_TIMER_NUM, frequency, TEMP_TIMER_PRESCALE);
       timer0.setCallback(TEMP_TIMER_NUM, &Temp_Handler);
       timer0.setCallbackPriority(TEMP_TIMER_NUM, TEMP_TIMER_PRIORITY);
       break;
     case STEP_TIMER_NUM:
-      CORE_DEBUG_PRINTF("HAL_timer_start: step timer, f=%ld\n", frequency);
+      CORE_DEBUG_PRINTF("HAL_timer_start: step timer, f=%ld\n", long(frequency));
       timer0.start(STEP_TIMER_NUM, frequency, STEPPER_TIMER_PRESCALE);
       timer0.setCallback(STEP_TIMER_NUM, &Step_Handler);
       timer0.setCallbackPriority(STEP_TIMER_NUM, STEP_TIMER_PRIORITY);
@@ -40,7 +39,6 @@ bool HAL_timer_interrupt_enabled(const timer_channel_t timer_num)
 {
   return !timer0.isPaused(timer_num);
 }
-
 
 void HAL_timer_set_compare(const timer_channel_t timer_num, const hal_timer_t compare)
 {
--- Marlin/src/HAL/HC32F46x/timers.h
+++ Marlin/src/HAL/HC32F46x/timers.h
@@ -33,20 +33,25 @@ typedef uint16_t hal_timer_t;
 //
 // Timer Configurations
 //
-#define HAL_TIMER_RATE uint32_t(SYSTEM_CLOCK_FREQUENCIES.pclk1)
+
+// TODO: some calculations (step irq min_step_rate) require the timer rate to be known at compile time
+//       this is not possible with the HC32F46x, as the timer rate depends on PCLK1
+//       as a workaround, PCLK1 = 50MHz is assumed (check with clock dump in MarlinHAL::init())
+#define HAL_TIMER_RATE 50000000 // 50MHz
+//#define HAL_TIMER_RATE TIMER0_BASE_FREQUENCY
 
 // temperature timer (Timer0 Unit1 Channel A)
 #define TEMP_TIMER_NUM Timer0Channel::A
 #define TEMP_TIMER_PRIORITY DDL_IRQ_PRIORITY_02
-#define TEMP_TIMER_RATE 1000 // 1kHz
 #define TEMP_TIMER_PRESCALE 16
+#define TEMP_TIMER_RATE 1000 // 1kHz
 #define TEMP_TIMER_FREQUENCY TEMP_TIMER_RATE // alias for Marlin
 
 // stepper timer (Timer0 Unit1 Channel B)
 #define STEP_TIMER_NUM Timer0Channel::B
 #define STEP_TIMER_PRIORITY DDL_IRQ_PRIORITY_01
-#define STEPPER_TIMER_RATE 2000000 // 2MHz
-#define STEPPER_TIMER_PRESCALE 16
+#define STEPPER_TIMER_PRESCALE 8 
+#define STEPPER_TIMER_RATE (HAL_TIMER_RATE / STEPPER_TIMER_PRESCALE) // 50MHz / 16 = 3.125MHz
 #define STEPPER_TIMER_TICKS_PER_US (STEPPER_TIMER_RATE / 1000000)
 
 // pulse timer (== stepper timer)
