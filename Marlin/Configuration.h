bd9cda0ba4b46095cb8ce5aa1dddcda3b5a0d6e2
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2023-06-15 10:19:33 -0700

    Update MPC Autotune Description
    Since MPC_AUTOTUNE is disabled by default now, remind users to enable it so M306 T works.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -688,7 +688,8 @@
  *
  * Use a physical model of the hotend to control temperature. When configured correctly
  * this gives better responsiveness and stability than PID and it also removes the need
- * for PID_EXTRUSION_SCALING and PID_FAN_SCALING. Use M306 T to autotune the model.
+ * for PID_EXTRUSION_SCALING and PID_FAN_SCALING. Enable MPC_AUTOTUNE and use M306 T to
+ * autotune the model.
  * @section mpctemp
  */
 #if ENABLED(MPCTEMP)
