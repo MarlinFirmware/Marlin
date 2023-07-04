27450de51b5b26f638ae118a41c0bbf98ebe9d79
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2023-07-04 09:29:22 -0700

    Spacing

--- Marlin/src/lcd/tft/tft_color.h
+++ Marlin/src/lcd/tft/tft_color.h
@@ -33,40 +33,40 @@
 // RGB565 color picker: https://embeddednotepad.com/page/rgb565-color-picker
 // Hex code to color name: https://www.color-name.com/
 
-#define COLOR_BLACK           0x0000  // #000000
-#define COLOR_WHITE           0xFFFF  // #FFFFFF
-#define COLOR_SILVER          0xC618  // #C0C0C0
-#define COLOR_GREY            0x7BEF  // #808080
-#define COLOR_DARKGREY        0x4208  // #404040
-#define COLOR_DARKGREY2       0x39E7  // #303030
-#define COLOR_DARK            0x0003  // #000019
+#define COLOR_BLACK            0x0000  // #000000
+#define COLOR_WHITE            0xFFFF  // #FFFFFF
+#define COLOR_SILVER           0xC618  // #C0C0C0
+#define COLOR_GREY             0x7BEF  // #808080
+#define COLOR_DARKGREY         0x4208  // #404040
+#define COLOR_DARKGREY2        0x39E7  // #303030
+#define COLOR_DARK             0x0003  // #000019
 
-#define COLOR_RED             0xF800  // #FF0000
-#define COLOR_SCARLET         0xF904  // #FF2020
-#define COLOR_LIME            0x7E00  // #00FF00
-#define COLOR_MIDNIGHT_BLUE   0x20AC  // #1E156E
-#define COLOR_BLUE            0x001F  // #0000FF
-#define COLOR_OCEAN_BOAT_BLUE 0x03B7  // #0075BD
-#define COLOR_LIGHT_BLUE      0x061F  // #00C3FF
-#define COLOR_YELLOW          0xFFE0  // #FFFF00
-#define COLOR_MAGENTA         0xF81F  // #FF00FF
-#define COLOR_CYAN            0x07FF  // #00FFFF
-#define COLOR_DODGER_BLUE     0x041F  // #0080FF
-#define COLOR_VIVID_VIOLET    0x7933  // #772399
+#define COLOR_RED              0xF800  // #FF0000
+#define COLOR_SCARLET          0xF904  // #FF2020
+#define COLOR_LIME             0x7E00  // #00FF00
+#define COLOR_MIDNIGHT_BLUE    0x20AC  // #1E156E
+#define COLOR_BLUE             0x001F  // #0000FF
+#define COLOR_OCEAN_BOAT_BLUE  0x03B7  // #0075BD
+#define COLOR_LIGHT_BLUE       0x061F  // #00C3FF
+#define COLOR_YELLOW           0xFFE0  // #FFFF00
+#define COLOR_MAGENTA          0xF81F  // #FF00FF
+#define COLOR_CYAN             0x07FF  // #00FFFF
+#define COLOR_DODGER_BLUE      0x041F  // #0080FF
+#define COLOR_VIVID_VIOLET     0x7933  // #772399
 
-#define COLOR_DARK_PURPLE     0x9930  // #992380
+#define COLOR_DARK_PURPLE      0x9930  // #992380
 
-#define COLOR_MAROON          0x7800  // #800000
-#define COLOR_GREEN           0x03E0  // #008000
-#define COLOR_NAVY            0x000F  // #000080
-#define COLOR_OLIVE           0x8400  // #808000
-#define COLOR_PURPLE          0x8010  // #800080
-#define COLOR_TEAL            0x0410  // #008080
+#define COLOR_MAROON           0x7800  // #800000
+#define COLOR_GREEN            0x03E0  // #008000
+#define COLOR_NAVY             0x000F  // #000080
+#define COLOR_OLIVE            0x8400  // #808000
+#define COLOR_PURPLE           0x8010  // #800080
+#define COLOR_TEAL             0x0410  // #008080
 
-#define COLOR_ORANGE          0xFC00  // #FF7F00
-#define COLOR_VIVID_GREEN     0x7FE0  // #7FFF00
-#define COLOR_DARK_ORANGE     0xFC40  // #FF8C00
-#define COLOR_CORAL_RED       0xF9E7  // #FF3F3F
+#define COLOR_ORANGE           0xFC00  // #FF7F00
+#define COLOR_VIVID_GREEN      0x7FE0  // #7FFF00
+#define COLOR_DARK_ORANGE      0xFC40  // #FF8C00
+#define COLOR_CORAL_RED        0xF9E7  // #FF3F3F
 
 #ifndef COLOR_BACKGROUND
   #define COLOR_BACKGROUND        COLOR_MIDNIGHT_BLUE
