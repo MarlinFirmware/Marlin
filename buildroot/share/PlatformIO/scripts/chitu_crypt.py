743d3100fa7b7cb00d2456652e2604f0c63c16f3
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-07-04 00:19:45 -0500

    🔨 [2.0.9.7] Use older chitu_crypt.py

--- buildroot/share/PlatformIO/scripts/chitu_crypt.py
+++ buildroot/share/PlatformIO/scripts/chitu_crypt.py
@@ -4,9 +4,9 @@
 #
 import pioutil
 if pioutil.is_pio_build():
-    import struct,uuid,marlin
-
-    board = marlin.env.BoardConfig()
+    import os,random,struct,uuid,marlin
+    # Relocate firmware from 0x08000000 to 0x08008800
+    marlin.relocate_firmware("0x08008800")
 
     def calculate_crc(contents, seed):
         accumulating_xor_value = seed;
@@ -105,22 +105,13 @@ if pioutil.is_pio_build():
 
     # Encrypt ${PROGNAME}.bin and save it as 'update.cbd'
     def encrypt(source, target, env):
-        from pathlib import Path
+        firmware = open(target[0].path, "rb")
+        update = open(target[0].dir.path + '/update.cbd', "wb")
+        length = os.path.getsize(target[0].path)
 
-        fwpath = Path(target[0].path)
-        fwsize = fwpath.stat().st_size
+        encrypt_file(firmware, update, length)
 
-        enname = board.get("build.crypt_chitu")
-        enpath = Path(target[0].dir.path)
+        firmware.close()
+        update.close()
 
-        fwfile = fwpath.open("rb")
-        enfile = (enpath / enname).open("wb")
-
-        print(f"Encrypting {fwpath} to {enname}")
-        encrypt_file(fwfile, enfile, fwsize)
-        fwfile.close()
-        enfile.close()
-        fwpath.unlink()
-
-    marlin.relocate_firmware("0x08008800")
     marlin.add_post_action(encrypt);
