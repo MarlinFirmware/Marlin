e48bc01dbcf0730427dff56fe2d0c1aa82197d8c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-07-18 18:30:21 -0500

    🔨 PlatformIO 6 compatibility

--- buildroot/share/PlatformIO/scripts/mks_robin_lite3.py
+++ buildroot/share/PlatformIO/scripts/mks_robin_lite3.py
@@ -1,11 +1,8 @@
-import os
+import os, marlin
 Import("env")
 
 # Relocate firmware from 0x08000000 to 0x08005000
-for define in env['CPPDEFINES']:
-    if define[0] == "VECT_TAB_ADDR":
-        env['CPPDEFINES'].remove(define)
-env['CPPDEFINES'].append(("VECT_TAB_ADDR", "0x08005000"))
+marlin.relocate_firmware("0x08005000")
 
 custom_ld_script = os.path.abspath("buildroot/share/PlatformIO/ldscripts/mks_robin_lite.ld")
 for i, flag in enumerate(env["LINKFLAGS"]):
