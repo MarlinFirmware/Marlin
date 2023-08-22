a0557c55c27f83fcf2bf0b10f8ca45564cc5ae33
classicrocker883 <andrewleduc88@yahoo.com>
2023-08-22 09:01:45 -0400

    small tweaks

--- Marlin/src/lcd/e3v2/proui/bedlevel_tools.cpp
+++ Marlin/src/lcd/e3v2/proui/bedlevel_tools.cpp
@@ -277,8 +277,6 @@ bool BedLevelTools::meshValidate() {
 
   void BedLevelTools::setMeshViewerStatus() { // TODO: draw gradient with values as a legend instead
     float v_max = abs(getMaxValue()), v_min = abs(getMinValue()), range = _MAX(v_min, v_max);
-    if (v_min > 3e+10f) v_min = 0.0000001;
-    if (v_max > 3e+10f) v_max = 0.0000001;
     if (range > 3e+10f) range = 0.0000001;
     ui.set_status(&MString<45>(F("Red "),  p_float_t(-range, 3), F("..0.."), p_float_t(range, 3), F(" Green")));
     drawing_mesh = false;
--- Marlin/src/lcd/e3v2/proui/dwinui.cpp
+++ Marlin/src/lcd/e3v2/proui/dwinui.cpp
@@ -181,7 +181,7 @@ void DWINUI::drawString(uint16_t color, const char * const string, uint16_t rlim
 //  iNum: Number of digits
 //  x/y: Upper-left coordinate
 //  value: Integer value
-void DWINUI::drawInt(uint8_t bShow, bool signedMode, fontid_t fid, uint16_t color, uint16_t bColor, uint8_t iNum, uint16_t x, uint16_t y, int32_t value) {
+void DWINUI::drawInt(uint8_t bShow, bool signedMode, fontid_t fid, uint16_t color, uint16_t bColor, uint8_t iNum, uint16_t x, uint16_t y, long value) {
   char nstr[10];
   sprintf_P(nstr, PSTR("%*li"), (signedMode ? iNum + 1 : iNum), value);
   dwinDrawString(bShow, fid, color, bColor, x, y, nstr);
