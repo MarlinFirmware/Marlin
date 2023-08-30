6f64be9cff59b893e182f48e9699c075968ac225
narno2202 <130909513+narno2202@users.noreply.github.com>
2023-08-30 21:03:32 +0200

    Commit changes suggested by @ulendoalex for CoreXY printers

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1128,28 +1128,49 @@
   #define FTM_WINDOW_SIZE           200                 // Window size for trajectory generation.
   #define FTM_FS                   1000                 // (Hz) Frequency for trajectory generation. (1 / FTM_TS)
   #define FTM_TS                      0.001f            // (s) Time step for trajectory generation. (1 / FTM_FS)
-  #define FTM_STEPPER_FS          20000                 // (Hz) Frequency for stepper I/O update.
-  #define FTM_MIN_TICKS ((STEPPER_TIMER_RATE) / (FTM_STEPPER_FS)) // Minimum stepper ticks between steps.
-  #define FTM_MIN_SHAPE_FREQ         10                 // Minimum shaping frequency.
-  #define FTM_ZMAX                  100                 // Maximum delays for shaping functions (even numbers only!).
-                                                        // Calculate as:
-                                                        //    1/2 * (FTM_FS / FTM_MIN_SHAPE_FREQ) for ZV.
-                                                        //    (FTM_FS / FTM_MIN_SHAPE_FREQ) for ZVD, MZV.
-                                                        //    3/2 * (FTM_FS / FTM_MIN_SHAPE_FREQ) for 2HEI.
-                                                        //    2 * (FTM_FS / FTM_MIN_SHAPE_FREQ) for 3HEI.
-  #define FTM_STEPS_PER_UNIT_TIME    20                 // Interpolated stepper commands per unit time.
-                                                        // Calculate as (FTM_STEPPER_FS / FTM_FS).
-  #define FTM_CTS_COMPARE_VAL        10                 // Comparison value used in interpolation algorithm.
-                                                        // Calculate as (FTM_STEPS_PER_UNIT_TIME / 2).
-  // These values may be configured to adjust duration of loop().
-  #define FTM_STEPS_PER_LOOP         60                 // Number of stepper commands to generate each loop().
-  #define FTM_POINTS_PER_LOOP       100                 // Number of trajectory points to generate each loop().
+  /**
+ * Advanced configuration
+ */
+  #define FTM_BATCH_SIZE 100                                      // Batch size for trajectory generation;
+  #define FTM_WINDOW_SIZE 200                                     // Window size for trajectory generation.
+  #define FTM_FS 1000                                             // (Hz) Frequency for trajectory generation. (1 / FTM_TS)
+  #define FTM_TS 0.001f                                           // (s) Time step for trajectory generation. (1 / FTM_FS)
+  #if ENABLED(COREXY)
+    #define FTM_STEPPER_FS 40000                              // (Hz) Frequency for stepper I/O update.
+    #define FTM_STEPS_PER_UNIT_TIME (FTM_STEPPER_FS / FTM_FS) // Interpolated stepper commands per unit time.
 
-  // This value may be configured to adjust duration to consume the command buffer.
-  // Try increasing this value if stepper motion is not smooth.
-  #define FTM_STEPPERCMD_BUFF_SIZE 3000                 // Size of the stepper command buffers.
-                                                        // (FTM_STEPS_PER_LOOP * FTM_POINTS_PER_LOOP)/2, give the best results.
-                                                        // If you run out of memory, fallback to 1000
+    #define FTM_CTS_COMPARE_VAL (FTM_STEPS_PER_UNIT_TIME / 2) // Comparison value used in interpolation algorithm.
+
+    // This value may be configured to adjust duration to consume the command buffer.
+    // Try increasing this value if stepper motion is not smooth.
+    #define FTM_STEPPERCMD_BUFF_SIZE 6000                     // Size of the stepper command buffers.
+                                                              // (FTM_STEPS_PER_LOOP * FTM_POINTS_PER_LOOP) is a good start
+                                                              // If you run out of memory, fallback to 3000 and increase progressively
+  #else
+    #define FTM_STEPPER_FS 20000                              // (Hz) Frequency for stepper I/O update.
+    #define FTM_STEPS_PER_UNIT_TIME (FTM_STEPPER_FS / FTM_FS) // Interpolated stepper commands per unit time.
+
+    #define FTM_CTS_COMPARE_VAL (FTM_STEPS_PER_UNIT_TIME / 2) // Comparison value used in interpolation algorithm.
+
+    // This value may be configured to adjust duration to consume the command buffer.
+    // Try increasing this value if stepper motion is not smooth.
+    #define FTM_STEPPERCMD_BUFF_SIZE 3000                     // Size of the stepper command buffers.
+                                                              // (FTM_STEPS_PER_LOOP * FTM_POINTS_PER_LOOP)/2, give the best results.
+                                                              // If you run out of memory, fallback to 1000 and increase progressively
+  #endif
+
+#define FTM_MIN_TICKS ((STEPPER_TIMER_RATE) / (FTM_STEPPER_FS)) // Minimum stepper ticks between steps.
+#define FTM_MIN_SHAPE_FREQ 10                                   // Minimum shaping frequency.
+#define FTM_ZMAX 100                                            // Maximum delays for shaping functions (even numbers only!).
+                                                                // Calculate as:
+                                                                //    1/2 * (FTM_FS / FTM_MIN_SHAPE_FREQ) for ZV.
+                                                                //    (FTM_FS / FTM_MIN_SHAPE_FREQ) for ZVD, MZV.
+                                                                //    3/2 * (FTM_FS / FTM_MIN_SHAPE_FREQ) for 2HEI.
+                                                                //    2 * (FTM_FS / FTM_MIN_SHAPE_FREQ) for 3HEI.
+
+// These values may be configured to adjust duration of loop().
+#define FTM_STEPS_PER_LOOP 60   // Number of stepper commands to generate each loop().
+#define FTM_POINTS_PER_LOOP 100 // Number of trajectory points to generate each loop().
 
   //#define FT_MOTION_MENU                              // Provide a MarlinUI menu to set M493 parameters.
 #endif
