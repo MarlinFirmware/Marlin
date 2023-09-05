18101702609ed8ea5f70c7368a160dae597f6b64
studiodyne <diabolomix@hotmail.fr>
2023-09-06 01:22:41 +0200

    report fan speed

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -4263,6 +4263,12 @@ void Temperature::isr() {
       HOTEND_LOOP() s.append(F(" @"), e, ':', getHeaterPower((heater_id_t)e));
     #endif
     s.echo();
+    #if FAN_COUNT > 0 && ENABLED(REPORT_FAN_CHANGE)
+      FANS_LOOP(i) {
+        thermalManager.report_fan_speed(i);
+        SERIAL_EOL();
+      }
+    #endif
   }
 
   #if ENABLED(AUTO_REPORT_TEMPERATURES)
