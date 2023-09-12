9dadbc48b2ff7a4b9eaa098ebe6ad5933fd36c81
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-09-12 06:27:10 +0300

    Use warning for backward compatibility

--- Marlin/src/inc/Conditionals_LCD.h
+++ Marlin/src/inc/Conditionals_LCD.h
@@ -1507,9 +1507,11 @@
 #endif
 #if ENABLED(PROBE_MANUALLY)
   #if ANY(MESH_BED_LEVELING, AUTO_BED_LEVELING_UBL)
-    #error MESH_BED_LEVELING and AUTO_BED_LEVELING_UBL have their own manual probing procedures, so PROBE_MANUALLY is not needed for them. Please remove PROBE_MANUALLY, or use a different leveling type like AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_LINEAR, or AUTO_BED_LEVELING_BILINEAR.
+    #undef PROBE_MANUALLY
+    #warning MESH_BED_LEVELING and AUTO_BED_LEVELING_UBL have their own manual probing procedures, so PROBE_MANUALLY is not needed for them. Please remove PROBE_MANUALLY, or use a different leveling type like AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_LINEAR, or AUTO_BED_LEVELING_BILINEAR.
   #elif !HAS_LEVELING
-    #error No leveling type is selected, so PROBE_MANUALLY has no use. Please select leveling type, for example AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_LINEAR, or AUTO_BED_LEVELING_BILINEAR
+    #undef PROBE_MANUALLY
+    #warning No leveling type is selected, so PROBE_MANUALLY has no use. Please select leveling type, for example AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_LINEAR, or AUTO_BED_LEVELING_BILINEAR
   #endif
 #endif
 #if ANY(HAS_BED_PROBE, PROBE_MANUALLY, MESH_BED_LEVELING)
