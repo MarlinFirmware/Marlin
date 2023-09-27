MarlinFirmware/Marlin

commit 381515721481d09c4f30ca8a526e90fc39602fd9

🐛 Ensure BLTouch deploy in HS Mode (#26033)

Marlin/src/module/probe.cpp
@@ -596,10 +596,8 @@ bool Probe::probe_down_to_z(const_float_t z, const_feedRate_t fr_mm_s) {
     thermalManager.wait_for_hotend_heating(active_extruder);
   #endif
 
-  #if ENABLED(BLTOUCH)
-    if (!bltouch.high_speed_mode && bltouch.deploy())
-      return true; // Deploy in LOW SPEED MODE on every probe action
-  #endif
+  // Ensure the BLTouch is deployed. Does nothing if already deployed.
+  if (TERN0(BLTOUCH, bltouch.deploy())) return true;
 
   #if HAS_Z_SERVO_PROBE && (ENABLED(Z_SERVO_INTERMEDIATE_STOW) || defined(Z_SERVO_MEASURE_ANGLE))
     probe_specific_action(true);  //  Always re-deploy in this case

commit de31571dc557bfdc6d36bc59e523013f5b22b476

Fixed BLTouch HSMode broken after #26033

Marlin/src/module/probe.cpp
@@ -596,8 +596,16 @@ bool Probe::probe_down_to_z(const_float_t z, const_feedRate_t fr_mm_s) {
     thermalManager.wait_for_hotend_heating(active_extruder);
   #endif
 
-  // Ensure the BLTouch is deployed. Does nothing if already deployed.
-  if (TERN0(BLTOUCH, bltouch.deploy())) return true;
+  #if ENABLED(BLTOUCH)
+    #if ENABLED(MEASURE_BACKLASH_WHEN_PROBING)
+      // Ensure the BLTouch is deployed. Does nothing if already deployed.
+      if (bltouch.deploy())
+        return true; // Deploy in LOW SPEED MODE on every probe action
+    #else
+      if (!bltouch.high_speed_mode && bltouch.deploy())
+        return true; // Deploy in LOW SPEED MODE on every probe action
+    #endif
+  #endif
 
   #if HAS_Z_SERVO_PROBE && (ENABLED(Z_SERVO_INTERMEDIATE_STOW) || defined(Z_SERVO_MEASURE_ANGLE))
     probe_specific_action(true);  //  Always re-deploy in this case