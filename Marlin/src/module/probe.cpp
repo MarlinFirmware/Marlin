1ec6ad56ac3bee9e2a6e8fa2b2312124c99d980b
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-10-12 11:44:36 -0500

    comment

--- Marlin/src/module/probe.cpp
+++ Marlin/src/module/probe.cpp
@@ -735,7 +735,7 @@ bool Probe::probe_down_to_z(const_float_t z, const_feedRate_t fr_mm_s) {
  *
  * @param sanity_check Flag to compare the probe result with the expected result
  *                     based on the probe Z offset. If the result is too far away
- *                     (more than 2mm too early) then consider it an error.
+ *                     (more than Z_PROBE_ERROR_TOLERANCE too early) then throw an error.
  * @param z_min_point Override the minimum probing height (-2mm), to allow deeper probing.
  * @param z_clearance Z clearance to apply on probe failure.
  *
