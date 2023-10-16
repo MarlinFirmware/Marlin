2e587a72d584e6916081ab3af82e3305040f59fd
Bradford1040 <bradfordaldenadams@gmail.com>
2023-10-16 06:29:10 -0400

    Update Configuration.h
    Move z_probe_end_script Y axis to front left instead of back left (changing from Y330 to Y35)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2188,7 +2188,7 @@
  * Commands to execute at the end of G29 probing.
  * Useful to retract or move the Z probe out of the way.
  */
-#define Z_PROBE_END_SCRIPT "G1 Z10 F12000\nG1 X15 Y330\nG1 Z0.5\nG1 Z10"
+#define Z_PROBE_END_SCRIPT "G1 Z10 F12000\nG1 X15 Y35\nG1 Z0.5\nG1 Z10"
 
 // @section homing
 
