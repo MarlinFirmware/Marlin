b5b310ee9eaa46df3e2b8bf2eb0dad979bc9bfec
Bradford1040 <bradfordaldenadams@gmail.com>
2023-10-18 02:16:24 -0400

    Update Configuration.h
    enable baud rate gcode

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -95,7 +95,7 @@
  */
 #define BAUDRATE 250000
 
-//#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
+#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
 
 /**
  * Select a secondary serial port on the board to use for communication with the host.
