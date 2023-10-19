fdbeb53136a92d693974f5305f8a079b8a42a1ce
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-10-19 17:27:21 -0500

    adjust desc

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2782,19 +2782,20 @@
     #define TOOLCHANGE_MIGRATION_FEATURE
     #if ENABLED(TOOLCHANGE_MIGRATION_FEATURE)
       // Override toolchange settings
-      // Migration use toolchange settings, when prime tower used, toolchange swapping and priming are made inside the bed
-      // Migrate to a new tool not primed, can now have MIGRATION_SETTINGS without modifying current toolchange settings
+      // By default tool migration uses regular toolchange settings.
+      // With a prime tower, tool-change swapping/priming occur inside the bed.
+      // When migrating to a new unprimed tool you can instead use the values below.
       //#define MIGRATION_SETTINGS
       #if ENABLED(MIGRATION_SETTINGS)
-        //#define MIGRATION_ZRAISE                  0 // (mm)
+        //#define MIGRATION_ZRAISE            0 // (mm)
 
         // Longer prime to clean out
-        //#define MIGRATION_FS_EXTRA_PRIME          0 // (mm) Extra priming length
-        //#define MIGRATION_FS_WIPE_RETRACT         0 // (mm) Retract before cooling for less stringing, better wipe, etc.
+        //#define MIGRATION_FS_EXTRA_PRIME    0 // (mm) Extra priming length
+        //#define MIGRATION_FS_WIPE_RETRACT   0 // (mm) Retract before cooling for less stringing, better wipe, etc.
 
         // Cool after prime to reduce stringing
-        //#define MIGRATION_FS_FAN_SPEED          255 // 0-255
-        //#define MIGRATION_FS_FAN_TIME             0 // (seconds)
+        //#define MIGRATION_FS_FAN_SPEED    255 // 0-255
+        //#define MIGRATION_FS_FAN_TIME       0 // (seconds)
 
       #endif
     #endif
