484704ddd6c41e46ce9fdb68a3a479da9df2f4b5
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-10-19 21:00:33 -0500

    as string

--- Marlin/src/lcd/menu/menu_configuration.cpp
+++ Marlin/src/lcd/menu/menu_configuration.cpp
@@ -111,8 +111,8 @@ void menu_advanced_settings();
   #else
     #define S1_SPACE(I)
   #endif
-  #define STOP_ITEM(A,I,M,L) TERN(HAS_##A##I##_##M##_STATE, _STOP_ITEM, _IF_1_ELSE)(STRINGIFY(A) STRINGIFY(I) S1_SPACE(I) " " STRINGIFY(L), A##I##_##M)
-  #define STOP_MINMAX(A,I) STOP_ITEM(A,I,MIN,Min) STOP_ITEM(A,I,MAX,Max)
+  #define STOP_ITEM(A,I,M,L) TERN(HAS_##A##I##_##M##_STATE, _STOP_ITEM, _IF_1_ELSE)(STRINGIFY(A) STRINGIFY(I) S1_SPACE(I) " " L, A##I##_##M)
+  #define STOP_MINMAX(A,I) STOP_ITEM(A,I,MIN,"Min") STOP_ITEM(A,I,MAX,"Max")
   #define FIL_ITEM(N) PSTRING_ITEM_N_P(N-1, MSG_FILAMENT_EN, (READ(FIL_RUNOUT##N##_PIN) != FIL_RUNOUT##N##_STATE) ? PSTR("PRESENT ") : PSTR("out "), SS_FULL);
 
   static void endstop_test() {
