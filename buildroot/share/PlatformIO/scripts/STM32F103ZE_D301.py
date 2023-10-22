5c295d061b48b9790c74211727d9532c0861bf28
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-10-22 17:50:46 -0500

    env cleanup

--- buildroot/share/PlatformIO/scripts/STM32F103ZE_D301.py
+++
@@ -1,9 +0,0 @@
-
-import pioutil
-if pioutil.is_pio_build():
-    import marlin
-
-    STM32_FLASH_SIZE = 512
-
-    # Relocate firmware from 0x08000000 to 0x08007000
-    marlin.relocate_firmware("0x08007000")
