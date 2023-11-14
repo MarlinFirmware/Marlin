a8c4dd014d4aab4704d06e12c35b030e9d9f6fea
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-11-14 02:45:03 -0600

    cleanup

--- Marlin/src/pins/stm32f4/pins_BLACKBEEZMINI.h
+++ Marlin/src/pins/stm32f4/pins_BLACKBEEZMINI.h
@@ -32,19 +32,26 @@
 #ifndef DEFAULT_MACHINE_NAME
   #define DEFAULT_MACHINE_NAME "I3DBEE BP_01"
 #endif
+
 #define TEMP_TIMER 5
-//#define DISABLE_DEBUG                      false  // DISABLE_(DEBUG|JTAG) is not supported for STM32F4.
+
+//#define DISABLE_DEBUG                         // DISABLE_(DEBUG|JTAG) is not supported for STM32F4.
+//#define DISABLE_JTAG
 //#define ALLOW_STM32F4
 //#define BOARD_NO_NATIVE_USB
 
 //
 // EEPROM
 //
+#if NO_EEPROM_SELECTED
   #define FLASH_EEPROM_EMULATION
   #define FLASH_EEPROM_LEVELING
-  #define EEPROM_SIZE           0x4000                                           // 16kB
   #define FLASH_SECTOR          (FLASH_SECTOR_TOTAL - 1)
   #define FLASH_UNIT_SIZE       0x4000                                           // 16kB
+  #define MARLIN_EEPROM_SIZE    FLASH_UNIT_SIZE
+  #undef NO_EEPROM_SELECTED
+#endif
+
 /*
 #if NO_EEPROM_SELECTED
   #define IIC_BL24CXX_EEPROM                      // EEPROM on I2C-0
@@ -55,35 +62,13 @@
 //
 // Limit Switches
 //
-#ifndef X_STOP_PIN
-  #ifndef X_MIN_PIN
-    #define X_MIN_PIN                       PB9
-  #endif
-  #ifndef X_MAX_PIN
-    #define X_MAX_PIN                       PB9
-  #endif
-#endif
-#ifndef Y_STOP_PIN
-  #ifndef Y_MIN_PIN
-    #define Y_MIN_PIN                       PB3
-  #endif
-  #ifndef Y_MAX_PIN
-    #define Y_MAX_PIN                       PB3
-  #endif
-#endif
-#ifndef Z_STOP_PIN
-  #ifndef Z_MIN_PIN
-    #define Z_MIN_PIN                       PA9
-  #endif
-  #ifndef Z_MAX_PIN
-    #define Z_MAX_PIN                       PA9
-  #endif
-#endif
+#define X_STOP_PIN                          PB9
+#define Y_STOP_PIN                          PB3
+#define Z_STOP_PIN                          PA9
 
 //
 // Steppers
 //
-// X & Y enable are the same
 #define X_STEP_PIN                          PB7
 #define X_DIR_PIN                           PB6
 #define X_ENABLE_PIN                        PB8
@@ -114,50 +99,49 @@
 
 #define FAN1_PIN                            PA1   // FAN1 header on board - PRINT FAN
 
-//Encoder
-#define BTN_EN1                             PC15
-#define BTN_EN2                             PC14
-#define BTN_ENC                             PC13
-
-// SDCard
+//
+// SD Card
+//
 #define SDSS                                PA4
 #define SD_DETECT_PIN                       -1
-#define BEEPER_PIN                          PB10
-#define KILL_PIN                            -1
-
-//
-// SPI
-//
 #define SD_SCK_PIN                          PA5
 #define SD_MISO_PIN                         PA6
 #define SD_MOSI_PIN                         PA7
 
-//LCD REPRAP_DISCOUNT_SMART_CONTROLLER
 //
- #define LCD_PINS_RS                        PB12
- #define LCD_PINS_EN                        PB13
- #define LCD_PINS_D4                        PB14
- #define LCD_PINS_D5                        PA3
+// LCD / Controller
+//
+#if HAS_WIRED_LCD
+  #define BEEPER_PIN                        PB10
 
+  #define BTN_EN1                           PC15
+  #define BTN_EN2                           PC14
+  #define BTN_ENC                           PC13
 
-    #if ENABLED(FYSETC_MINI_12864)
-      #define DOGLCD_CS                     PB13
-      #define DOGLCD_A0                     PB12
-      //#define LCD_BACKLIGHT_PIN           -1
-      #define LCD_RESET_PIN                 PB14  // Must be high or open for LCD to operate normally.
-      #if ENABLED(FYSETC_MINI_12864_2_1)
-        #define NEOPIXEL_PIN                PA3
-      #endif
-    #endif // !FYSETC_MINI_12864
- // GLCD features
- //
- //#define LCD_CONTRAST                      190
+  #define LCD_PINS_RS                       PB12
+  #define LCD_PINS_EN                       PB13
+  #define LCD_PINS_D4                       PB14
+  #define LCD_PINS_D5                       PA3
 
- // Uncomment screen orientation
- //
- //#define LCD_SCREEN_ROT_90
- //#define LCD_SCREEN_ROT_180
- //#define LCD_SCREEN_ROT_270
+  #if ENABLED(FYSETC_MINI_12864)
+    #define DOGLCD_CS                       PB13
+    #define DOGLCD_A0                       PB12
+    //#define LCD_BACKLIGHT_PIN             -1
+    #define LCD_RESET_PIN                   PB14  // Must be high or open for LCD to operate normally.
+    #if ENABLED(FYSETC_MINI_12864_2_1)
+      #define NEOPIXEL_PIN                  PA3
+    #endif
+  #endif
 
-//#define DISABLE_JTAG
-//#define FAN_SOFT_PWM
+  //
+  // GLCD features
+  //
+  //#define LCD_CONTRAST                     190
+
+  //
+  // Dcreen orientation
+  //
+  //#define LCD_SCREEN_ROT_90
+  //#define LCD_SCREEN_ROT_180
+  //#define LCD_SCREEN_ROT_270
+#endif
