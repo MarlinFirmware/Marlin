55a109e7fe78088334f94ef2cf2470281f29a56a
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2023-11-20 23:53:16 -0800

    (c) 2023, use env_validate.h, final new line

--- Marlin/src/pins/stm32f4/pins_MELLOW_FLY_E3_PRO_V3.h
+++ Marlin/src/pins/stm32f4/pins_MELLOW_FLY_E3_PRO_V3.h
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2021 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2023 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -21,9 +21,7 @@
  */
 #pragma once
 
-#if NOT_TARGET(STM32F4)
-  #error "Oops! Select an STM32F4 board in 'Tools > Board.'"
-#endif
+#include "env_validate.h"
 
 #ifndef BOARD_INFO_NAME
   #define BOARD_INFO_NAME "Mellow Fly E3 Pro V3"
