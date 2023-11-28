75f7b41fe98c90e1b0e309369bf565878f30dbc8
kdobrev <kdobrev@mail.bg>
2023-11-28 11:49:50 +0100

    SV06PLus bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1793,8 +1793,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 300
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1802,7 +1802,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 240
+#define Z_MAX_POS 330
 // #define I_MIN_POS 0
 // #define I_MAX_POS 50
 // #define J_MIN_POS 0
