666297eec138ea7062710aeb0c0082a6c4a70437
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-11-28 18:16:28 -0600

    Closer match to STM32F1

--- Marlin/src/HAL/STM32F1/HAL_N32.cpp
+++ Marlin/src/HAL/STM32F1/HAL_N32.cpp
@@ -78,226 +78,10 @@
 #define SCB_AIRCR_PRIGROUP_Pos              8                                             /*!< SCB AIRCR: PRIGROUP Position */
 #define SCB_AIRCR_PRIGROUP_Msk             (7UL << SCB_AIRCR_PRIGROUP_Pos)                /*!< SCB AIRCR: PRIGROUP Mask */
 
-// ------------------------
-// Public Variables
-// ------------------------
 
-#if (defined(SERIAL_USB) && !defined(USE_USB_COMPOSITE))
-  USBSerial SerialUSB;
-#endif
-
-// ------------------------
-// Watchdog Timer
-// ------------------------
-
-#if ENABLED(USE_WATCHDOG)
-
-  #include <libmaple/iwdg.h>
-
-  void watchdogSetup() {
-    // do whatever. don't remove this function.
-  }
-
-  /**
-   *  The watchdog clock is 40Khz. So for a 4s or 8s interval use a /256 preescaler and 625 or 1250 reload value (counts down to 0).
-   */
-  #define STM32F1_WD_RELOAD TERN(WATCHDOG_DURATION_8S, 1250, 625) // 4 or 8 second timeout
-
-  /**
-   * @brief  Initialize the independent hardware watchdog.
-   *
-   * @return No return
-   *
-   * @details The watchdog clock is 40Khz. So for a 4s or 8s interval use a /256 preescaler and 625 or 1250 reload value (counts down to 0).
-   */
-  void MarlinHAL::watchdog_init() {
-    #if DISABLED(DISABLE_WATCHDOG_INIT)
-      iwdg_init(IWDG_PRE_256, STM32F1_WD_RELOAD);
-    #endif
-  }
-
-  // Reset watchdog. MUST be called every 4 or 8 seconds after the
-  // first watchdog_init or the STM32F1 will reset.
-  void MarlinHAL::watchdog_refresh() {
-    #if DISABLED(PINS_DEBUGGING) && PIN_EXISTS(LED)
-      TOGGLE(LED_PIN);  // heartbeat indicator
-    #endif
-    iwdg_feed();
-  }
-
-#endif // USE_WATCHDOG
-
-uint16_t MarlinHAL::adc_result;
-
-// ------------------------
-// Private Variables
-// ------------------------
-STM32ADC adc(ADC1);
-
-const uint8_t adc_pins[] = {
-  OPTITEM(HAS_TEMP_ADC_0,        TEMP_0_PIN)
-  OPTITEM(HAS_TEMP_ADC_PROBE,    TEMP_PROBE_PIN)
-  OPTITEM(HAS_HEATED_BED,        TEMP_BED_PIN)
-  OPTITEM(HAS_TEMP_CHAMBER,      TEMP_CHAMBER_PIN)
-  OPTITEM(HAS_TEMP_COOLER,       TEMP_COOLER_PIN)
-  OPTITEM(HAS_TEMP_ADC_1,        TEMP_1_PIN)
-  OPTITEM(HAS_TEMP_ADC_2,        TEMP_2_PIN)
-  OPTITEM(HAS_TEMP_ADC_3,        TEMP_3_PIN)
-  OPTITEM(HAS_TEMP_ADC_4,        TEMP_4_PIN)
-  OPTITEM(HAS_TEMP_ADC_5,        TEMP_5_PIN)
-  OPTITEM(HAS_TEMP_ADC_6,        TEMP_6_PIN)
-  OPTITEM(HAS_TEMP_ADC_7,        TEMP_7_PIN)
-  OPTITEM(HAS_TEMP_BOARD,        TEMP_BOARD_PIN)
-  OPTITEM(HAS_TEMP_SOC,          TEMP_SOC_PIN)
-  OPTITEM(FILAMENT_WIDTH_SENSOR, FILWIDTH_PIN)
-  OPTITEM(ADC_KEYPAD,            ADC_KEYPAD_PIN)
-  OPTITEM(HAS_JOY_ADC_X,         JOY_X_PIN)
-  OPTITEM(HAS_JOY_ADC_Y,         JOY_Y_PIN)
-  OPTITEM(HAS_JOY_ADC_Z,         JOY_Z_PIN)
-  OPTITEM(POWER_MONITOR_CURRENT, POWER_MONITOR_CURRENT_PIN)
-  OPTITEM(POWER_MONITOR_VOLTAGE, POWER_MONITOR_VOLTAGE_PIN)
-};
-
-enum ADCIndex : uint8_t {
-  OPTITEM(HAS_TEMP_ADC_0, TEMP_0)
-  OPTITEM(HAS_TEMP_ADC_1, TEMP_1)
-  OPTITEM(HAS_TEMP_ADC_2, TEMP_2)
-  OPTITEM(HAS_TEMP_ADC_3, TEMP_3)
-  OPTITEM(HAS_TEMP_ADC_4, TEMP_4)
-  OPTITEM(HAS_TEMP_ADC_5, TEMP_5)
-  OPTITEM(HAS_TEMP_ADC_6, TEMP_6)
-  OPTITEM(HAS_TEMP_ADC_7, TEMP_7)
-  OPTITEM(HAS_HEATED_BED, TEMP_BED)
-  OPTITEM(HAS_TEMP_CHAMBER, TEMP_CHAMBER)
-  OPTITEM(HAS_TEMP_ADC_PROBE, TEMP_PROBE)
-  OPTITEM(HAS_TEMP_COOLER, TEMP_COOLER)
-  OPTITEM(HAS_TEMP_BOARD, TEMP_BOARD)
-  OPTITEM(HAS_TEMP_SOC, TEMP_SOC)
-  OPTITEM(FILAMENT_WIDTH_SENSOR, FILWIDTH)
-  OPTITEM(HAS_ADC_BUTTONS, ADC_KEY)
-  OPTITEM(HAS_JOY_ADC_X, JOY_X)
-  OPTITEM(HAS_JOY_ADC_Y, JOY_Y)
-  OPTITEM(HAS_JOY_ADC_Z, JOY_Z)
-  OPTITEM(POWER_MONITOR_CURRENT, POWERMON_CURRENT)
-  OPTITEM(POWER_MONITOR_VOLTAGE, POWERMON_VOLTS)
-  ADC_COUNT
-};
-
-static uint16_t adc_results[ADC_COUNT];
-
-// ------------------------
-// Private functions
-// ------------------------
-static void NVIC_SetPriorityGrouping(uint32_t PriorityGroup) {
-  uint32_t reg_value;
-  uint32_t PriorityGroupTmp = (PriorityGroup & (uint32_t)0x07);               /* only values 0..7 are used          */
-
-  reg_value  =  SCB->AIRCR;                                                   /* read old register configuration    */
-  reg_value &= ~(SCB_AIRCR_VECTKEY_Msk | SCB_AIRCR_PRIGROUP_Msk);             /* clear bits to change               */
-  reg_value  =  (reg_value                                 |
-                ((uint32_t)0x5FA << SCB_AIRCR_VECTKEY_Pos) |
-                (PriorityGroupTmp << 8));                                     /* Insert write key and priorty group */
-  SCB->AIRCR =  reg_value;
-}
-
-// ------------------------
-// Public functions
-// ------------------------
-
-//
-// Leave PA11/PA12 intact if USBSerial is not used
-//
-#if SERIAL_USB
-  namespace wirish { namespace priv {
-    #if SERIAL_PORT > 0
-      #if SERIAL_PORT2
-        #if SERIAL_PORT2 > 0
-          void board_setup_usb() {}
-        #endif
-      #else
-        void board_setup_usb() {}
-      #endif
-    #endif
-  } }
-#endif
-
-void MarlinHAL::init() {
-  NVIC_SetPriorityGrouping(0x3);
-  #if PIN_EXISTS(LED)
-    OUT_WRITE(LED_PIN, LOW);
-  #endif
-  #ifdef USE_USB_COMPOSITE
-    MSC_SD_init();
-  #endif
-  #if PIN_EXISTS(USB_CONNECT)
-    OUT_WRITE(USB_CONNECT_PIN, !USB_CONNECT_INVERTING);  // USB clear connection
-    delay(1000);                                         // Give OS time to notice
-    OUT_WRITE(USB_CONNECT_PIN, USB_CONNECT_INVERTING);
-  #endif
-}
-
-// HAL idle task
-void MarlinHAL::idletask() {
-  #ifdef USE_USB_COMPOSITE
-    #if HAS_SHARED_MEDIA
-      // If Marlin is using the SD card we need to lock it to prevent access from
-      // a PC via USB.
-      // Other HALs use IS_SD_PRINTING() and IS_SD_FILE_OPEN() to check for access but
-      // this will not reliably detect delete operations. To be safe we will lock
-      // the disk if Marlin has it mounted. Unfortunately there is currently no way
-      // to unmount the disk from the LCD menu.
-      // if (IS_SD_PRINTING() || IS_SD_FILE_OPEN())
-      /* copy from lpc1768 framework, should be fixed later for process HAS_SHARED_MEDIA*/
-    #endif
-    // process USB mass storage device class loop
-    MarlinMSC.loop();
-  #endif
-}
-
-void HAL_clear_reset_source() { }
-
-/**
- * TODO: Check this and change or remove.
- */
-uint8_t HAL_get_reset_source() { return RST_POWER_ON; }
-
-void _delay_ms(const int delay_ms) { delay(delay_ms); }
-
-extern "C" {
-  extern unsigned int _ebss; // end of bss section
-}
-
-/**
- * TODO: Change this to correct it for libmaple
- */
-
-#if 0
-// Return free memory between end of heap (or end bss) and whatever is current
-
-#include <wirish/syscalls.c>
-//extern caddr_t _sbrk(int incr);
-#ifndef CONFIG_HEAP_END
-extern char _lm_heap_end;
-#define CONFIG_HEAP_END ((caddr_t)&_lm_heap_end)
-#endif
-
-extern "C" {
-  static int freeMemory() {
-    char top = 't';
-    return &top - reinterpret_cast<char*>(sbrk(0));
-  }
-  int freeMemory() {
-    int free_memory;
-    int heap_end = (int)_sbrk(0);
-    free_memory = ((int)&free_memory) - ((int)heap_end);
-    return free_memory;
-  }
-}
-#endif
-
-/**====================================================================================================================================================================
+/**=============================================================================
  *        n32g452
-====================================================================================================================================================================*/
+==============================================================================*/
 typedef struct {
   uint32_t WorkMode;
   uint32_t MultiChEn;
@@ -1044,9 +828,8 @@ typedef struct {
 #define RCC_AHB_PERIPH_ADC3   ((uint32_t)0x00004000)
 #define RCC_AHB_PERIPH_ADC4   ((uint32_t)0x00008000)
 
-/**================================================================
- *        库函数: ADC初始化
-================================================================*/
+static uint16_t adc_results[ADC_COUNT];
+
 void ADC_Init(ADC_Module* NS_ADCx, ADC_InitType* ADC_InitStruct) {
   uint32_t tmpreg1 = 0;
   uint8_t tmpreg2  = 0;
@@ -1372,7 +1155,7 @@ typedef struct {
 #define RCC                 ((RCC_TypeDef *) ADC_RCC_BASE)
 
 /**================================================================
- *        Function function: initialize ADC clock
+ *        Initialize ADC clock
 ================================================================*/
 
 void enable_adc_clk(uint8_t cmd) {
@@ -1433,7 +1216,7 @@ void enable_adc_clk(uint8_t cmd) {
 }
 
 /**================================================================
- *        Function function: initialize ADC peripheral parameters
+ *        Initialize ADC peripheral parameters
 ================================================================*/
 void ADC_Initial(ADC_Module* NS_ADCx) {
   ADC_InitType ADC_InitStructure;
@@ -1467,7 +1250,7 @@ void ADC_Initial(ADC_Module* NS_ADCx) {
 }
 
 /**================================================================
- *        Function function: single independent sampling
+ *        Single independent sampling
 ================================================================*/
 uint16_t ADC_GetData(ADC_Module* NS_ADCx, uint8_t ADC_Channel) {
   uint16_t dat;
@@ -1606,7 +1389,6 @@ void DMA_Init(DMA_ChannelType* DMAyChx, DMA_InitType* DMA_InitParam) {
   DMAyChx->MADDR = DMA_InitParam->MemAddr;
 }
 
-
 void DMA_EnableChannel(DMA_ChannelType* DMAyChx, uint32_t Cmd) {
   if (Cmd != 0) {
     /* Enable the selected DMAy Channelx */
@@ -1622,7 +1404,7 @@ void DMA_EnableChannel(DMA_ChannelType* DMAyChx, uint32_t Cmd) {
 #define USE_DMA_CH    DMA1_CH8
 
 /**================================================================
- *        Function: initialize the DMA of ADC
+ *        Initialize the DMA of ADC
 ================================================================*/
 void ADC_DMA_init(void) {
   DMA_InitType DMA_InitStructure;
@@ -1655,18 +1437,250 @@ void ADC_DMA_init(void) {
 
 /**=============================================================================
  *        n32g452 - end
-================================================================================*/
+==============================================================================*/
+
+// ------------------------
+// Serial ports
+// ------------------------
+
+#if defined(SERIAL_USB) && !defined(USE_USB_COMPOSITE)
+
+  USBSerial SerialUSB;
+  DefaultSerial1 MSerial0(true, SerialUSB);
+
+  #if ENABLED(EMERGENCY_PARSER)
+    #include "../libmaple/usb/stm32f1/usb_reg_map.h"
+    #include "libmaple/usb_cdcacm.h"
+    // The original callback is not called (no way to retrieve address).
+    // That callback detects a special STM32 reset sequence: this functionality is not essential
+    // as M997 achieves the same.
+    void my_rx_callback(unsigned int, void*) {
+      // max length of 16 is enough to contain all emergency commands
+      uint8 buf[16];
+
+      //rx is usbSerialPart.endpoints[2]
+      uint16 len = usb_get_ep_rx_count(USB_CDCACM_RX_ENDP);
+      uint32 total = usb_cdcacm_data_available();
+
+      if (len == 0 || total == 0 || !WITHIN(total, len, COUNT(buf)))
+        return;
+
+      // cannot get character by character due to bug in composite_cdcacm_peek_ex
+      len = usb_cdcacm_peek(buf, total);
+
+      for (uint32 i = 0; i < len; i++)
+        emergency_parser.update(MSerial0.emergency_state, buf[i + total - len]);
+    }
+  #endif
+#endif
+
+// ------------------------
+// Watchdog Timer
+// ------------------------
+
+#if ENABLED(USE_WATCHDOG)
+
+  #include <libmaple/iwdg.h>
+
+  void watchdogSetup() {
+    // do whatever. don't remove this function.
+  }
+
+  /**
+   *  The watchdog clock is 40Khz. So for a 4s or 8s interval use a /256 preescaler and 625 or 1250 reload value (counts down to 0).
+   */
+  #define STM32F1_WD_RELOAD TERN(WATCHDOG_DURATION_8S, 1250, 625) // 4 or 8 second timeout
+
+  /**
+   * @brief  Initialize the independent hardware watchdog.
+   *
+   * @return No return
+   *
+   * @details The watchdog clock is 40Khz. So for a 4s or 8s interval use a /256 preescaler and 625 or 1250 reload value (counts down to 0).
+   */
+  void MarlinHAL::watchdog_init() {
+    #if DISABLED(DISABLE_WATCHDOG_INIT)
+      iwdg_init(IWDG_PRE_256, STM32F1_WD_RELOAD);
+    #endif
+  }
+
+  // Reset watchdog. MUST be called every 4 or 8 seconds after the
+  // first watchdog_init or the STM32F1 will reset.
+  void MarlinHAL::watchdog_refresh() {
+    #if DISABLED(PINS_DEBUGGING) && PIN_EXISTS(LED)
+      TOGGLE(LED_PIN);  // heartbeat indicator
+    #endif
+    iwdg_feed();
+  }
+
+#endif // USE_WATCHDOG
 
 // ------------------------
 // ADC
 // ------------------------
-// Init the AD in continuous capture mode
+
+// Watch out for recursion here! Our pin_t is signed, so pass through to Arduino -> analogRead(uint8_t)
+
+uint16_t analogRead(const pin_t pin) {
+  const bool is_analog = _GET_MODE(pin) == GPIO_INPUT_ANALOG;
+  return is_analog ? analogRead(uint8_t(pin)) : 0;
+}
+
+// Wrapper to maple unprotected analogWrite
+void analogWrite(const pin_t pin, int pwm_val8) {
+  if (PWM_PIN(pin)) analogWrite(uint8_t(pin), pwm_val8);
+}
+
+uint16_t MarlinHAL::adc_result;
+
+// ------------------------
+// Private functions
+// ------------------------
+
+static void NVIC_SetPriorityGrouping(uint32_t PriorityGroup) {
+  uint32_t reg_value;
+  uint32_t PriorityGroupTmp = (PriorityGroup & (uint32_t)0x07);               // only values 0..7 are used
+
+  reg_value  =  SCB->AIRCR;                                                   // read old register configuration
+  reg_value &= ~(SCB_AIRCR_VECTKEY_Msk | SCB_AIRCR_PRIGROUP_Msk);             // clear bits to change
+  reg_value  =  (reg_value                                 |
+                ((uint32_t)0x5FA << SCB_AIRCR_VECTKEY_Pos) |
+                (PriorityGroupTmp << 8));                                     // Insert write key & priority group
+  SCB->AIRCR =  reg_value;
+}
+
+// ------------------------
+// Public functions
+// ------------------------
+
+void flashFirmware(const int16_t) { hal.reboot(); }
+
+//
+// Leave PA11/PA12 intact if USBSerial is not used
+//
+#if SERIAL_USB
+  namespace wirish { namespace priv {
+    #if SERIAL_PORT > 0
+      #if SERIAL_PORT2
+        #if SERIAL_PORT2 > 0
+          void board_setup_usb() {}
+        #endif
+      #else
+        void board_setup_usb() {}
+      #endif
+    #endif
+  } }
+#endif
+
+TERN_(POSTMORTEM_DEBUGGING, extern void install_min_serial());
+
+// ------------------------
+// MarlinHAL class
+// ------------------------
+
+void MarlinHAL::init() {
+  NVIC_SetPriorityGrouping(0x3);
+  #if PIN_EXISTS(LED)
+    OUT_WRITE(LED_PIN, LOW);
+  #endif
+  #ifdef USE_USB_COMPOSITE
+    MSC_SD_init();
+  #endif
+  #if PIN_EXISTS(USB_CONNECT)
+    OUT_WRITE(USB_CONNECT_PIN, !USB_CONNECT_INVERTING);  // USB clear connection
+    delay(1000);                                         // Give OS time to notice
+    WRITE(USB_CONNECT_PIN, USB_CONNECT_INVERTING);
+  #endif
+  TERN_(POSTMORTEM_DEBUGGING, install_min_serial());    // Install the minimal serial handler
+}
+
+// HAL idle task
+void MarlinHAL::idletask() {
+  #ifdef USE_USB_COMPOSITE
+    #if HAS_SHARED_MEDIA
+      // If Marlin is using the SD card we need to lock it to prevent access from
+      // a PC via USB.
+      // Other HALs use IS_SD_PRINTING() and IS_SD_FILE_OPEN() to check for access but
+      // this will not reliably detect delete operations. To be safe we will lock
+      // the disk if Marlin has it mounted. Unfortunately there is currently no way
+      // to unmount the disk from the LCD menu.
+      // if (IS_SD_PRINTING() || IS_SD_FILE_OPEN())
+      /* copy from lpc1768 framework, should be fixed later for process HAS_SHARED_MEDIA*/
+    #endif
+    // process USB mass storage device class loop
+    MarlinMSC.loop();
+  #endif
+}
+
+void MarlinHAL::reboot() { nvic_sys_reset(); }
+
+// ------------------------
+// Free Memory Accessor
+// ------------------------
+
+extern "C" {
+  extern unsigned int _ebss; // end of bss section
+}
+
+/**
+ * TODO: Change this to correct it for libmaple
+ */
+
+/*
+// Return free memory between end of heap (or end bss) and whatever is current
+
+#include <wirish/syscalls.c>
+//extern caddr_t _sbrk(int incr);
+#ifndef CONFIG_HEAP_END
+extern char _lm_heap_end;
+#define CONFIG_HEAP_END ((caddr_t)&_lm_heap_end)
+#endif
+
+extern "C" {
+  static int freeMemory() {
+    char top = 't';
+    return &top - reinterpret_cast<char*>(sbrk(0));
+  }
+  int freeMemory() {
+    int free_memory;
+    int heap_end = (int)_sbrk(0);
+    free_memory = ((int)&free_memory) - ((int)heap_end);
+    return free_memory;
+  }
+}
+*/
+
+// ------------------------
+// ADC
+// ------------------------
+
+enum ADCIndex : uint8_t {
+  OPTITEM(HAS_TEMP_ADC_0, TEMP_0)
+  OPTITEM(HAS_TEMP_ADC_1, TEMP_1)
+  OPTITEM(HAS_TEMP_ADC_2, TEMP_2)
+  OPTITEM(HAS_TEMP_ADC_3, TEMP_3)
+  OPTITEM(HAS_TEMP_ADC_4, TEMP_4)
+  OPTITEM(HAS_TEMP_ADC_5, TEMP_5)
+  OPTITEM(HAS_TEMP_ADC_6, TEMP_6)
+  OPTITEM(HAS_TEMP_ADC_7, TEMP_7)
+  OPTITEM(HAS_HEATED_BED, TEMP_BED)
+  OPTITEM(HAS_TEMP_CHAMBER, TEMP_CHAMBER)
+  OPTITEM(HAS_TEMP_ADC_PROBE, TEMP_PROBE)
+  OPTITEM(HAS_TEMP_COOLER, TEMP_COOLER)
+  OPTITEM(HAS_TEMP_BOARD, TEMP_BOARD)
+  OPTITEM(HAS_TEMP_SOC, TEMP_SOC)
+  OPTITEM(FILAMENT_WIDTH_SENSOR, FILWIDTH)
+  OPTITEM(HAS_ADC_BUTTONS, ADC_KEY)
+  OPTITEM(HAS_JOY_ADC_X, JOY_X)
+  OPTITEM(HAS_JOY_ADC_Y, JOY_Y)
+  OPTITEM(HAS_JOY_ADC_Z, JOY_Z)
+  OPTITEM(POWER_MONITOR_CURRENT, POWERMON_CURRENT)
+  OPTITEM(POWER_MONITOR_VOLTAGE, POWERMON_VOLTS)
+  ADC_COUNT
+};
 
 #define NS_PINRT(V...)       do{ SERIAL_ECHO_START(); SERIAL_ECHOLNPAIR(V); }while(0)
 
-// ------------------------
-// ADC
-// ------------------------
 // Init the AD in continuous capture mode
 void MarlinHAL::adc_init() {
   uint32_t reg_temp;
@@ -1686,30 +1700,10 @@ void MarlinHAL::adc_init() {
   ADC_DMA_init();                   // DMA initialization
   ADC_Initial(NS_ADC2);             // ADC initialization
 
-  _delay_ms(2);
-  //NS_PINRT("get adc1 = ", HAL_adc_results[0], "\r\n");
-  //NS_PINRT("get adc2 = ", HAL_adc_results[1], "\r\n");
+  delay(2);
+  //NS_PINRT("get adc1 = ", adc_results[0], "\r\n");
+  //NS_PINRT("get adc2 = ", adc_results[1], "\r\n");
 }
-
-#if 0
-void HAL_adc_init() {
-
-  SERIAL_ECHO_MSG("\r\n n32g45x luke \r\n");
-
-  // configure the ADC
-  adc.calibrate();
-  #if F_CPU > 72000000
-    adc.setSampleRate(ADC_SMPR_71_5); // 71.5 ADC cycles
-  #else
-    adc.setSampleRate(ADC_SMPR_41_5); // 41.5 ADC cycles
-  #endif
-  adc.setPins((uint8_t *)adc_pins, ADC_PIN_COUNT);
-  adc.setDMA(HAL_adc_results, (uint16_t)ADC_PIN_COUNT, (uint32_t)(DMA_MINC_MODE | DMA_CIRC_MODE), nullptr);
-  adc.setScanMode();
-  adc.setContinuous();
-  adc.startConversion();
-}
-#endif
 
 void MarlinHAL::adc_start(const pin_t pin) {
   #define __TCASE(N,I) case N: pin_index = I; break;
@@ -1741,20 +1735,5 @@ void MarlinHAL::adc_start(const pin_t pin) {
   }
   adc_result = (adc_results[(int)pin_index] & 0xFFF) >> (12 - HAL_ADC_RESOLUTION); // shift out unused bits
 }
-
-uint16_t analogRead(pin_t pin) {
-  const bool is_analog = _GET_MODE(pin) == GPIO_INPUT_ANALOG;
-  return is_analog ? analogRead(uint8_t(pin)) : 0;
-}
-
-// Wrapper to maple unprotected analogWrite
-void analogWrite(pin_t pin, int pwm_val8) {
-  if (PWM_PIN(pin))
-    analogWrite(uint8_t(pin), pwm_val8);
-}
-
-void MarlinHAL::reboot() { nvic_sys_reset(); }
-
-void flashFirmware(const int16_t) { nvic_sys_reset(); }
 
 #endif // __STM32F1__ && VOXELAB_N32
