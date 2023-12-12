f50dc400585b84fc0cb05e74eba0a19ba1ae676c
classicrocker883 <andrewleduc88@yahoo.com>
2023-12-12 11:44:47 -0500

    update abl/bbl.cpp, line up comment

--- Marlin/src/feature/bedlevel/abl/bbl.cpp
+++ Marlin/src/feature/bedlevel/abl/bbl.cpp
@@ -120,16 +120,16 @@ void LevelingBilinear::extrapolate_unprobed_bed_level() {
   #ifdef HALF_IN_X
     constexpr uint8_t ctrx2 = 0, xend = GRID_MAX_POINTS_X - 1;
   #else
-    constexpr uint8_t ctrx1 = (GRID_MAX_CELLS_X) / 2, // left-of-center
-                      ctrx2 = (GRID_MAX_POINTS_X) / 2,  // right-of-center
+    constexpr uint8_t ctrx1 = (GRID_MAX_CELLS_X) / 2,  // left-of-center
+                      ctrx2 = (GRID_MAX_POINTS_X) / 2, // right-of-center
                       xend = ctrx1;
   #endif
 
   #ifdef HALF_IN_Y
     constexpr uint8_t ctry2 = 0, yend = GRID_MAX_POINTS_Y - 1;
   #else
-    constexpr uint8_t ctry1 = (GRID_MAX_CELLS_Y) / 2, // top-of-center
-                      ctry2 = (GRID_MAX_POINTS_Y) / 2,  // bottom-of-center
+    constexpr uint8_t ctry1 = (GRID_MAX_CELLS_Y) / 2,  // top-of-center
+                      ctry2 = (GRID_MAX_POINTS_Y) / 2, // bottom-of-center
                       yend = ctry1;
   #endif
 
