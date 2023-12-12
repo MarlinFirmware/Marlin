5119dd38f947e1fe0565a9917615f91fb832fe92
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-12-11 21:27:46 -0600

    more flags

--- Marlin/src/lcd/dogm/u8g/HAL_LCD_class_defines.h
+++ Marlin/src/lcd/dogm/u8g/HAL_LCD_class_defines.h
@@ -25,54 +25,56 @@
 
 // Use this file to create the public interface for device drivers that are NOT in the U8G library
 
-extern u8g_dev_t u8g_dev_st7565_64128n_HAL_2x_sw_spi;
-extern u8g_dev_t u8g_dev_st7565_64128n_HAL_2x_hw_spi;
+#if IS_U8GLIB_ST7565_64128N
 
-class U8GLIB_64128N_2X_HAL : public U8GLIB {
-public:
-  U8GLIB_64128N_2X_HAL() : U8GLIB() { }
-  U8GLIB_64128N_2X_HAL(pin_t sck, pin_t mosi, pin_t cs, pin_t a0, pin_t reset=U8G_PIN_NONE) { init(sck, mosi, cs, a0, reset); }
-  U8GLIB_64128N_2X_HAL(pin_t cs, pin_t a0, pin_t reset=U8G_PIN_NONE) { init(cs, a0, reset); }
-  void init(pin_t sck, pin_t mosi, pin_t cs, pin_t a0, pin_t reset=U8G_PIN_NONE) {
-    U8GLIB::init(&u8g_dev_st7565_64128n_HAL_2x_sw_spi, (uint8_t)sck, (uint8_t)mosi, (uint8_t)cs, (uint8_t)a0, (uint8_t)reset);
-  }
-  void init(pin_t cs, pin_t a0, pin_t reset=U8G_PIN_NONE) {
-    U8GLIB::init(&u8g_dev_st7565_64128n_HAL_2x_hw_spi, (uint8_t)cs, (uint8_t)a0, (uint8_t)reset);
-  }
-};
+  extern u8g_dev_t u8g_dev_st7565_64128n_HAL_2x_sw_spi, u8g_dev_st7565_64128n_HAL_2x_hw_spi;
 
-extern u8g_dev_t u8g_dev_st7920_128x64_HAL_4x_sw_spi;
-extern u8g_dev_t u8g_dev_st7920_128x64_HAL_4x_hw_spi;
+  class U8GLIB_64128N_2X_HAL : public U8GLIB {
+  public:
+    U8GLIB_64128N_2X_HAL() : U8GLIB() { }
+    U8GLIB_64128N_2X_HAL(pin_t sck, pin_t mosi, pin_t cs, pin_t a0, pin_t reset=U8G_PIN_NONE) { init(sck, mosi, cs, a0, reset); }
+    U8GLIB_64128N_2X_HAL(pin_t cs, pin_t a0, pin_t reset=U8G_PIN_NONE) { init(cs, a0, reset); }
+    void init(pin_t sck, pin_t mosi, pin_t cs, pin_t a0, pin_t reset=U8G_PIN_NONE) {
+      U8GLIB::init(&u8g_dev_st7565_64128n_HAL_2x_sw_spi, (uint8_t)sck, (uint8_t)mosi, (uint8_t)cs, (uint8_t)a0, (uint8_t)reset);
+    }
+    void init(pin_t cs, pin_t a0, pin_t reset=U8G_PIN_NONE) {
+      U8GLIB::init(&u8g_dev_st7565_64128n_HAL_2x_hw_spi, (uint8_t)cs, (uint8_t)a0, (uint8_t)reset);
+    }
+  };
 
-class U8GLIB_ST7920_128X64_4X_HAL : public U8GLIB {
-public:
-  U8GLIB_ST7920_128X64_4X_HAL() : U8GLIB() { }
-  U8GLIB_ST7920_128X64_4X_HAL(pin_t sck, pin_t mosi, pin_t cs, pin_t reset=U8G_PIN_NONE) { init(sck, mosi, cs, reset); }
-  U8GLIB_ST7920_128X64_4X_HAL(pin_t cs, pin_t reset=U8G_PIN_NONE) { init(cs, reset); }
-  void init(pin_t sck, pin_t mosi, pin_t cs, pin_t reset=U8G_PIN_NONE) {
-    U8GLIB::init(&u8g_dev_st7920_128x64_HAL_4x_sw_spi, (uint8_t)sck, (uint8_t)mosi, (uint8_t)cs, U8G_PIN_NONE, (uint8_t)reset); // a0 = U8G_PIN_NONE
-  }
-  void init(pin_t cs, pin_t reset=U8G_PIN_NONE) {
-    U8GLIB::init(&u8g_dev_st7920_128x64_HAL_4x_hw_spi, (uint8_t)cs, U8G_PIN_NONE, (uint8_t)reset); // a0 = U8G_PIN_NONE
-  }
-};
+#elif IS_U8GLIB_ST7920
 
-//
-// AVR version uses ultralcd_st7920_u8glib_rrd_AVR.cpp
-// HAL version uses u8g_dev_st7920_128x64_HAL.cpp
-//
-extern u8g_dev_t u8g_dev_st7920_128x64_rrd_sw_spi;
+  extern u8g_dev_t u8g_dev_st7920_128x64_HAL_4x_sw_spi, u8g_dev_st7920_128x64_HAL_4x_hw_spi;
 
-class U8GLIB_ST7920_128X64_RRD : public U8GLIB {
-public:
-  U8GLIB_ST7920_128X64_RRD() : U8GLIB() { }
-  U8GLIB_ST7920_128X64_RRD(pin_t sck, pin_t mosi, pin_t cs, pin_t reset=U8G_PIN_NONE) { init(sck, mosi, cs, reset); }
-  void init(pin_t sck, pin_t mosi, pin_t cs, pin_t reset=U8G_PIN_NONE) {
-    U8GLIB::init(&u8g_dev_st7920_128x64_rrd_sw_spi, (uint8_t)sck, (uint8_t)mosi, (uint8_t)cs, U8G_PIN_NONE, (uint8_t)reset);   // a0 = U8G_PIN_NONE
-  }
-};
+  class U8GLIB_ST7920_128X64_4X_HAL : public U8GLIB {
+  public:
+    U8GLIB_ST7920_128X64_4X_HAL() : U8GLIB() { }
+    U8GLIB_ST7920_128X64_4X_HAL(pin_t sck, pin_t mosi, pin_t cs, pin_t reset=U8G_PIN_NONE) { init(sck, mosi, cs, reset); }
+    U8GLIB_ST7920_128X64_4X_HAL(pin_t cs, pin_t reset=U8G_PIN_NONE) { init(cs, reset); }
+    void init(pin_t sck, pin_t mosi, pin_t cs, pin_t reset=U8G_PIN_NONE) {
+      U8GLIB::init(&u8g_dev_st7920_128x64_HAL_4x_sw_spi, (uint8_t)sck, (uint8_t)mosi, (uint8_t)cs, U8G_PIN_NONE, (uint8_t)reset); // a0 = U8G_PIN_NONE
+    }
+    void init(pin_t cs, pin_t reset=U8G_PIN_NONE) {
+      U8GLIB::init(&u8g_dev_st7920_128x64_HAL_4x_hw_spi, (uint8_t)cs, U8G_PIN_NONE, (uint8_t)reset); // a0 = U8G_PIN_NONE
+    }
+  };
 
-#if ENABLED(U8GLIB_SH1106)
+  //
+  // AVR version uses ultralcd_st7920_u8glib_rrd_AVR.cpp
+  // HAL version uses u8g_dev_st7920_128x64_HAL.cpp
+  //
+  extern u8g_dev_t u8g_dev_st7920_128x64_rrd_sw_spi;
+
+  class U8GLIB_ST7920_128X64_RRD : public U8GLIB {
+  public:
+    U8GLIB_ST7920_128X64_RRD() : U8GLIB() { }
+    U8GLIB_ST7920_128X64_RRD(pin_t sck, pin_t mosi, pin_t cs, pin_t reset=U8G_PIN_NONE) { init(sck, mosi, cs, reset); }
+    void init(pin_t sck, pin_t mosi, pin_t cs, pin_t reset=U8G_PIN_NONE) {
+      U8GLIB::init(&u8g_dev_st7920_128x64_rrd_sw_spi, (uint8_t)sck, (uint8_t)mosi, (uint8_t)cs, U8G_PIN_NONE, (uint8_t)reset);   // a0 = U8G_PIN_NONE
+    }
+  };
+
+#elif ENABLED(U8GLIB_SH1106)
 
   extern u8g_dev_t u8g_dev_sh1106_128x64_2x_i2c_2_wire;
 
@@ -83,9 +85,7 @@ public:
     void init(uint8_t options = U8G_I2C_OPT_NONE) { U8GLIB::init(&u8g_dev_sh1106_128x64_2x_i2c_2_wire, options); }
   };
 
-#endif
-
-#if ENABLED(U8GLIB_SSD1306)
+#elif ENABLED(U8GLIB_SSD1306)
 
   extern u8g_dev_t u8g_dev_ssd1306_128x64_2x_i2c_2_wire;
 
@@ -96,41 +96,41 @@ public:
     void init(uint8_t options = U8G_I2C_OPT_NONE) { U8GLIB::init(&u8g_dev_ssd1306_128x64_2x_i2c_2_wire, options); }
   };
 
-#endif
+#elif TFT_SCALED_DOGLCD
 
-//
-// Very basic support for 320x240 TFT screen
-// Tested on MKS Robin TFT_V2.0 with ST7789V controller
-//
-extern u8g_dev_t u8g_dev_tft_320x240_upscale_from_128x64;
+  //
+  // Very basic support for 320x240 TFT screen
+  // Tested on MKS Robin TFT_V2.0 with ST7789V controller
+  //
+  extern u8g_dev_t u8g_dev_tft_320x240_upscale_from_128x64;
 
-class U8GLIB_TFT_320X240_UPSCALE_FROM_128X64 : public U8GLIB {
-public:
-  U8GLIB_TFT_320X240_UPSCALE_FROM_128X64() : U8GLIB() { }
-  U8GLIB_TFT_320X240_UPSCALE_FROM_128X64(uint8_t cs, uint8_t rs, uint8_t reset=U8G_PIN_NONE) { init(cs, rs, reset); }
-  void init(uint8_t cs, uint8_t rs, uint8_t reset=U8G_PIN_NONE) { U8GLIB::init(&u8g_dev_tft_320x240_upscale_from_128x64, cs, rs, reset); }
-};
+  class U8GLIB_TFT_320X240_UPSCALE_FROM_128X64 : public U8GLIB {
+  public:
+    U8GLIB_TFT_320X240_UPSCALE_FROM_128X64() : U8GLIB() { }
+    U8GLIB_TFT_320X240_UPSCALE_FROM_128X64(uint8_t cs, uint8_t rs, uint8_t reset=U8G_PIN_NONE) { init(cs, rs, reset); }
+    void init(uint8_t cs, uint8_t rs, uint8_t reset=U8G_PIN_NONE) { U8GLIB::init(&u8g_dev_tft_320x240_upscale_from_128x64, cs, rs, reset); }
+  };
 
+  extern u8g_dev_t u8g_dev_uc1701_mini12864_HAL_2x_sw_spi, u8g_dev_uc1701_mini12864_HAL_2x_hw_spi;
 
-extern u8g_dev_t u8g_dev_uc1701_mini12864_HAL_2x_sw_spi, u8g_dev_uc1701_mini12864_HAL_2x_hw_spi;
+#elif ENABLED(U8GLIB_MINI12864)
 
-class U8GLIB_MINI12864_2X_HAL : public U8GLIB {
-public:
-  U8GLIB_MINI12864_2X_HAL() : U8GLIB() { }
-  U8GLIB_MINI12864_2X_HAL(uint8_t sck, uint8_t mosi, uint8_t cs, uint8_t a0, uint8_t reset=U8G_PIN_NONE) { init(sck, mosi, cs, a0, reset); }
-  U8GLIB_MINI12864_2X_HAL(uint8_t cs, uint8_t a0, uint8_t reset=U8G_PIN_NONE) { init(cs, a0, reset); }
-  void init(uint8_t sck, uint8_t mosi, uint8_t cs, uint8_t a0, uint8_t reset=U8G_PIN_NONE) {
-    U8GLIB::init(&u8g_dev_uc1701_mini12864_HAL_2x_sw_spi, sck, mosi, cs, a0, reset);
-  }
-  void init(uint8_t cs, uint8_t a0, uint8_t reset=U8G_PIN_NONE) {
-    U8GLIB::init(&u8g_dev_uc1701_mini12864_HAL_2x_hw_spi, cs, a0, reset);
-  }
-};
+  class U8GLIB_MINI12864_2X_HAL : public U8GLIB {
+  public:
+    U8GLIB_MINI12864_2X_HAL() : U8GLIB() { }
+    U8GLIB_MINI12864_2X_HAL(uint8_t sck, uint8_t mosi, uint8_t cs, uint8_t a0, uint8_t reset=U8G_PIN_NONE) { init(sck, mosi, cs, a0, reset); }
+    U8GLIB_MINI12864_2X_HAL(uint8_t cs, uint8_t a0, uint8_t reset=U8G_PIN_NONE) { init(cs, a0, reset); }
+    void init(uint8_t sck, uint8_t mosi, uint8_t cs, uint8_t a0, uint8_t reset=U8G_PIN_NONE) {
+      U8GLIB::init(&u8g_dev_uc1701_mini12864_HAL_2x_sw_spi, sck, mosi, cs, a0, reset);
+    }
+    void init(uint8_t cs, uint8_t a0, uint8_t reset=U8G_PIN_NONE) {
+      U8GLIB::init(&u8g_dev_uc1701_mini12864_HAL_2x_hw_spi, cs, a0, reset);
+    }
+  };
 
-#if ENABLED(U8GLIB_SSD1309)
+#elif ENABLED(U8GLIB_SSD1309)
 
-  extern u8g_dev_t u8g_dev_ssd1309_sw_spi;
-  extern u8g_dev_t u8g_dev_ssd1309_hw_spi;
+  extern u8g_dev_t u8g_dev_ssd1309_sw_spi, u8g_dev_ssd1309_hw_spi;
 
   class U8GLIB_SSD1309_128X64_HAL : public U8GLIB {
   public:
