b76db0ec29331466b37cc14334d77caf3ff2c7d8
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-12-31 14:48:21 -0600

    coffee

--- Marlin/src/module/ft_motion.cpp
+++ Marlin/src/module/ft_motion.cpp
@@ -503,8 +503,10 @@ void FTMotion::loadBlockData(block_t * const current_block) {
 
   /* Keep for comprehension
   const float spm = totalLength / current_block->step_event_count;  // (steps/mm) Distance for each step
-              f_s = spm * current_block->initial_rate,  // (steps/s) Start feedrate
-              f_e = spm * current_block->final_rate;    // (steps/s) End feedrate
+
+  f_s = spm * current_block->initial_rate;              // (steps/s) Start feedrate
+
+  const float f_e = spm * current_block->final_rate;    // (steps/s) End feedrate
 
   const float a = current_block->acceleration,          // (mm/s^2) Same magnitude for acceleration or deceleration
               oneby2a = 1.0f / (2.0f * a),              // (s/mm) Time to accelerate or decelerate one mm (i.e., oneby2a * 2
@@ -528,9 +530,9 @@ void FTMotion::loadBlockData(block_t * const current_block) {
   */
 
   const float spm = totalLength / current_block->step_event_count;
-              f_s = spm * current_block->initial_rate;
 
-  // NOTE: Keep this declaration separate to get the compiler to behave
+  f_s = spm * current_block->initial_rate;
+
   const float f_e = spm * current_block->final_rate;
 
   const float accel = current_block->acceleration,
