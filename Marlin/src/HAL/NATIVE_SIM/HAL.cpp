b4132ab0435cb36a978c8569ab993e55b6d9ed7e
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-01-01 00:04:47 -0600

    docs, license

--- Marlin/src/HAL/NATIVE_SIM/HAL.cpp
+++ Marlin/src/HAL/NATIVE_SIM/HAL.cpp
@@ -20,11 +20,33 @@
  *
  */
 
+/**
+ * Copyright (c) 1998, 2015 Todd C. Miller <Todd.Miller@courtesan.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #ifdef __PLAT_NATIVE_SIM__
 
 #ifndef HAS_LIBBSD
 
   #include "HAL.h"
+
+  /**
+   * Copy string src to buffer dst of size dsize.  At most dsize-1
+   * chars will be copied.  Always NUL terminates (unless dsize == 0).
+   * Returns strlen(src); if retval >= dsize, truncation occurred.
+   */
 
   /**
    * Redirect missing strlcpy here
