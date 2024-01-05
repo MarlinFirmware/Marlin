8bfb4eceaa6469aeecae6f32defa6ba73d1c8010
classicrocker883 <andrewleduc88@yahoo.com>
2024-01-05 10:50:43 -0500

    move NOTE @section servos Up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3620,8 +3620,9 @@
  * For some servo-related options NUM_SERVOS will be set automatically.
  * Set this manually if there are extra servos needing manual control.
  * Set to 0 to turn off servo support.
+ * NOTE: Servo index starts with 0 for M280-M282 commands
  */
-//#define NUM_SERVOS 3 // NOTE: Servo index starts with 0 for M280-M282 commands
+//#define NUM_SERVOS 3
 
 // (ms) Delay before the next move will start, to give the servo time to reach its target angle.
 // 300ms is a good value but you can try less delay.
