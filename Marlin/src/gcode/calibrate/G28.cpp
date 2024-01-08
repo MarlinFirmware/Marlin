3aa77acf932bd5de6e9448cf10f7ed53926b0378
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-01-07 21:52:59 -0600

    also

--- Marlin/src/gcode/calibrate/G28.cpp
+++ Marlin/src/gcode/calibrate/G28.cpp
@@ -639,10 +639,8 @@ void GcodeSuite::G28() {
     #endif
 
     #ifdef XY_AFTER_HOMING
-    if (!(homing_needed_error(_BV(X_AXIS) | _BV(Y_AXIS)))){
-      constexpr xy_pos_t xy_after XY_AFTER_HOMING;
-      do_blocking_move_to(xy_after);
-    }
+      if (!(homing_needed_error(_BV(X_AXIS) | _BV(Y_AXIS))))
+        do_blocking_move_to(xy_pos_t(XY_AFTER_HOMING));
     #endif
 
     restore_feedrate_and_scaling();
