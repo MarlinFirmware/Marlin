e4cd45176aa21a85e1da82926e7b5b629e099256
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-01-10 00:23:25 -0600

    only some

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1237,7 +1237,7 @@
 #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
 
 /**
- * Enable support for M92. Disable to save ~530 bytes of flash (1400 more if using a display)
+ * Enable support for M92. Disable to save at least ~530 bytes of flash.
  */
 #define EDITABLE_STEPS_PER_UNIT
 
