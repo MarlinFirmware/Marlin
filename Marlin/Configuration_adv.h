64319c8cfdf473e9f2a04c2bb34c7cf85a3a55a1
sargonphin <ieuropa@outlook.fr>
2024-01-14 01:15:32 +0100

    Added preprocessor checks for Input Shaping

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1208,8 +1208,13 @@
     #define SHAPING_ZETA_X  0.15f       // Damping ratio of the X axis (range: 0.0 = no damping to 1.0 = critical damping).
   #endif
   #if ENABLED(INPUT_SHAPING_Y)
-    #define SHAPING_FREQ_Y  40          // (Hz) The default dominant resonant frequency on the Y axis.
-    #define SHAPING_ZETA_Y  0.15f       // Damping ratio of the Y axis (range: 0.0 = no damping to 1.0 = critical damping).
+    #if ANY(COREXY, COREYX, MARKFORGED_XY, MARKFORGED_YX)
+      #define SHAPING_FREQ_Y  SHAPING_FREQ_X          // (Hz) The default dominant resonant frequency on the Y axis.
+      #define SHAPING_ZETA_Y  SHAPING_ZETA_X       // Damping ratio of the Y axis (range: 0.0 = no damping to 1.0 = critical damping).
+    #else
+      #define SHAPING_FREQ_Y  40
+      #define SHAPING_ZETA_Y  0.15f
+    #endif
   #endif
   //#define SHAPING_MIN_FREQ  20        // By default the minimum of the shaping frequencies. Override to affect SRAM usage.
   //#define SHAPING_MAX_STEPRATE 10000  // By default the maximum total step rate of the shaped axes. Override to affect SRAM usage.
