b2ddf104f1b37ab1de7a505af594f6046b18e1b2
classicrocker883 <andrewleduc88@yahoo.com>
2024-01-18 06:41:09 -0500

    add to pinsDebug_list.h

--- Marlin/src/HAL/STM32/inc/SanityCheck.h
+++ Marlin/src/HAL/STM32/inc/SanityCheck.h
@@ -60,7 +60,7 @@
  */
 #define _CHECK_SERIAL_PIN(N) (( \
     BTN_EN1 == N || DOGLCD_CS == N || HEATER_BED_PIN == N || FAN0_PIN == N || \
-    BOARD_SDIO_D2 == N || BOARD_SDIO_D3 == N || BOARD_SDIO_CLK == N || BOARD_SDIO_CMD == N \
+    SDIO_D2_PIN == N || SDIO_D3_PIN == N || SDIO_CK_PIN == N || SDIO_CMD_PIN == N \
   ))
 #define CHECK_SERIAL_PIN(T,N) defined(UART##N##_##T##_PIN) && _CHECK_SERIAL_PIN(UART##N##_##T##_PIN)
 #if SERIAL_IN_USE(1)
--- Marlin/src/HAL/STM32/sdio.h
+++ Marlin/src/HAL/STM32/sdio.h
@@ -21,9 +21,9 @@
  */
 #pragma once
 
-#define BOARD_SDIO_D0   PC8
-#define BOARD_SDIO_D1   PC9
-#define BOARD_SDIO_D2   PC10
-#define BOARD_SDIO_D3   PC11
-#define BOARD_SDIO_CLK  PC12
-#define BOARD_SDIO_CMD  PD2
+#define SDIO_D0_PIN   PC8
+#define SDIO_D1_PIN   PC9
+#define SDIO_D2_PIN   PC10
+#define SDIO_D3_PIN   PC11
+#define SDIO_CK_PIN   PC12
+#define SDIO_CMD_PIN  PD2
