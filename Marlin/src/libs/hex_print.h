2fa77a7b92ee5b674aee9784c6f6fe445c8b82f5
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-01-21 03:52:11 -0600

    templatize

--- Marlin/src/libs/hex_print.h
+++ Marlin/src/libs/hex_print.h
@@ -37,8 +37,8 @@ char* hex_address(const void * const w);
   char* hex_long(const uintptr_t l);
 #endif
 
-inline char* hex_long(const unsigned int w) { return hex_long(w); }
-inline char* hex_word(const unsigned short w) { return hex_word(w); }
+template<typename T> char* hex_long(T w) { return hex_long(w); }
+template<typename T> char* hex_word(T w) { return hex_word(w); }
 
 void print_hex_nybble(const uint8_t n);
 void print_hex_byte(const uint8_t b);
