16edc203c084fdaf67bbdee235493f2c7b258523
classicrocker883 <andrewleduc88@yahoo.com>
2024-01-22 11:08:35 -0500

    update runout string

--- Marlin/src/lcd/e3v2/proui/endstop_diag.cpp
+++ Marlin/src/lcd/e3v2/proui/endstop_diag.cpp
@@ -61,7 +61,7 @@ void draw_es_state(const bool is_hit, const bool is_fil) {
   const uint8_t LM = 130;
   DWINUI::cursor.x = LM;
   dwinDrawRectangle(1, hmiData.colorPopupBg, LM, DWINUI::cursor.y, LM + 120, DWINUI::cursor.y + 20);
-  is_fil ? (is_hit ? DWINUI::drawString(RGB(16, 63, 16), F("PRESENT")) : DWINUI::drawString(RGB(31, 31, 16), F("Runout Detected"))) :
+  is_fil ? (is_hit ? DWINUI::drawString(RGB(16, 63, 16), GET_TEXT_F(MSG_FILAMENT)) : DWINUI::drawString(RGB(31, 31, 16), GET_TEXT_F(MSG_RUNOUT_SENSOR))) :
   (is_hit ? DWINUI::drawString(RGB(31, 31, 16), F(STR_ENDSTOP_HIT)) : DWINUI::drawString(RGB(16, 63, 16), F(STR_ENDSTOP_OPEN)));
   DWINUI::moveBy(0, 25);
 }
