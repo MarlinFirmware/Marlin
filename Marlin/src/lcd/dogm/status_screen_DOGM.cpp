8eb378840adf9672582cf98af5e9d51fb6a3fc4f
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-01-22 23:10:07 -0600

    merge followup

--- Marlin/src/lcd/dogm/status_screen_DOGM.cpp
+++ Marlin/src/lcd/dogm/status_screen_DOGM.cpp
@@ -323,10 +323,9 @@ FORCE_INLINE void _draw_centered_temp(const celsius_t temp, const uint8_t tx, co
 
       } // PAGE_CONTAINS
 
-      #if HAS_MULTI_EXTRUDER
-        if (active_extruder == heater_id) {
-          u8g.drawBitmapP(STATUS_HOTEND_X(heater_id)-6, STATUS_HEATERS_Y+3, 1, 5, status_active_extruder_indicator_bmp);
-        } 
+      #if HAS_MULTI_EXTRUDER && DISABLED(SLIM_LCD_MENUS)
+        if (active_extruder == heater_id)
+          u8g.drawBitmapP(STATUS_HOTEND_X(heater_id) - 6, STATUS_HEATERS_Y + 3, 1, 5, status_active_extruder_indicator_bmp);
       #endif
 
     #endif // !STATUS_COMBINE_HEATERS
