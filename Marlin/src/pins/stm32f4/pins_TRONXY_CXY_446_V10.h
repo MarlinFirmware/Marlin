a28a97fdd28ba4cf936bf58b5bbd5c0dc2877dde
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-02-01 15:12:06 -0600

    run pinsformat.js

--- Marlin/src/pins/stm32f4/pins_TRONXY_CXY_446_V10.h
+++ Marlin/src/pins/stm32f4/pins_TRONXY_CXY_446_V10.h
@@ -60,13 +60,13 @@
 //
 // SPI Flash
 //
-#define SPI_FLASH  // W25Q16
+#define SPI_FLASH                                 // W25Q16
 #if ENABLED(SPI_FLASH)
   #define SPI_FLASH_SIZE                      0x1000000  // 16MB
-  #define SPI_FLASH_CS_PIN                    PG15
-  #define SPI_FLASH_MOSI_PIN                  PB5
-  #define SPI_FLASH_MISO_PIN                  PB4
-  #define SPI_FLASH_SCK_PIN                   PB3
+  #define SPI_FLASH_CS_PIN                  PG15
+  #define SPI_FLASH_MOSI_PIN                PB5
+  #define SPI_FLASH_MISO_PIN                PB4
+  #define SPI_FLASH_SCK_PIN                 PB3
 #endif
 
 //
@@ -78,90 +78,90 @@
 // SD Card
 //
 #define ONBOARD_SDIO
-#define SD_DETECT_PIN                         -1
+#define SD_DETECT_PIN                       -1
 #define SDIO_CLOCK                            4500000
 #define SDIO_READ_RETRIES                     16
 
-#define SDIO_D0_PIN                           PC8
-#define SDIO_D1_PIN                           PC9
-#define SDIO_D2_PIN                           PC10
-#define SDIO_D3_PIN                           PC11
-#define SDIO_CK_PIN                           PC12
-#define SDIO_CMD_PIN                          PD2
+#define SDIO_D0_PIN                         PC8
+#define SDIO_D1_PIN                         PC9
+#define SDIO_D2_PIN                         PC10
+#define SDIO_D3_PIN                         PC11
+#define SDIO_CK_PIN                         PC12
+#define SDIO_CMD_PIN                        PD2
 
 //
 // Limit Switches
 //
-#define X_STOP_PIN                            PC15
-#define Y_STOP_PIN                            PC14
+#define X_STOP_PIN                          PC15
+#define Y_STOP_PIN                          PC14
 
 #if ENABLED(FIX_MOUNTED_PROBE)
-  #define Z_STOP_PIN                          PE3
+  #define Z_STOP_PIN                        PE3
 #else
-  #define Z_STOP_PIN                          PC13
+  #define Z_STOP_PIN                        PC13
 #endif
 
 //
 // Filament Sensors
 //
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
-  #define FIL_RUNOUT_PIN                      PE6
-  #define FIL_RUNOUT2_PIN                     PF12
+  #define FIL_RUNOUT_PIN                    PE6
+  #define FIL_RUNOUT2_PIN                   PF12
 #endif
 
 //
 // Steppers
 //
-#define X_ENABLE_PIN                          PF0 
-#define X_STEP_PIN                            PE5 
-#define X_DIR_PIN                             PF1 
+#define X_ENABLE_PIN                        PF0
+#define X_STEP_PIN                          PE5
+#define X_DIR_PIN                           PF1
 
-#define Y_ENABLE_PIN                          PF5 
-#define Y_STEP_PIN                            PF9 
-#define Y_DIR_PIN                             PF3 
+#define Y_ENABLE_PIN                        PF5
+#define Y_STEP_PIN                          PF9
+#define Y_DIR_PIN                           PF3
 
-#define Z_ENABLE_PIN                          PA5
-#define Z_STEP_PIN                            PA6 
-#define Z_DIR_PIN                             PF15 
+#define Z_ENABLE_PIN                        PA5
+#define Z_STEP_PIN                          PA6
+#define Z_DIR_PIN                           PF15
 
-#define E0_ENABLE_PIN                         PF14 
-#define E0_STEP_PIN                           PB1 
-#define E0_DIR_PIN                            PF13 
+#define E0_ENABLE_PIN                       PF14
+#define E0_STEP_PIN                         PB1
+#define E0_DIR_PIN                          PF13
 
-#define E1_ENABLE_PIN                         PG5 
-#define E1_STEP_PIN                           PD12 
-#define E1_DIR_PIN                            PG4 
+#define E1_ENABLE_PIN                       PG5
+#define E1_STEP_PIN                         PD12
+#define E1_DIR_PIN                          PG4
 
 //
 // Temperature Sensors
 //
-#define TEMP_0_PIN                            PC3   
-#define TEMP_1_PIN                            PC0
-#define TEMP_BED_PIN                          PC2   
+#define TEMP_0_PIN                          PC3
+#define TEMP_1_PIN                          PC0
+#define TEMP_BED_PIN                        PC2
 
 //
 // Heaters
 //
-#define HEATER_0_PIN                          PG7   // Hotend #1 Heater
-#define HEATER_1_PIN                          PA15  // Hotend #2 Heater
-#define HEATER_BED_PIN                        PE2
+#define HEATER_0_PIN                        PG7   // Hotend #1 Heater
+#define HEATER_1_PIN                        PA15  // Hotend #2 Heater
+#define HEATER_BED_PIN                      PE2
 
 //
 // Fans
 //
 #define FAN_SOFT_PWM_REQUIRED
 
-#define FAN0_PIN                              PG0   // Part Cooling Fan #1
-#define FAN1_PIN                              PB6   // Part Cooling Fan #2
-#define FAN2_PIN                              PG9   // Extruder/Hotend #1 Heatsink Fan
-#define FAN3_PIN                              PF10  // Extruder/Hotend #2 Heatsink Fan
-#define CONTROLLER_FAN_PIN                    PD7
+#define FAN0_PIN                            PG0   // Part Cooling Fan #1
+#define FAN1_PIN                            PB6   // Part Cooling Fan #2
+#define FAN2_PIN                            PG9   // Extruder/Hotend #1 Heatsink Fan
+#define FAN3_PIN                            PF10  // Extruder/Hotend #2 Heatsink Fan
+#define CONTROLLER_FAN_PIN                  PD7
 
 //
 // Laser / Servos
 //
-#define SPINDLE_LASER_ENA_PIN                 PB11  // WiFi Module TXD (Pin5)
-#define SPINDLE_LASER_PWM_PIN                 PB10  // WiFi Module RXD (Pin4)
+#define SPINDLE_LASER_ENA_PIN               PB11  // WiFi Module TXD (Pin5)
+#define SPINDLE_LASER_PWM_PIN               PB10  // WiFi Module RXD (Pin4)
 //
 // NOTE: The PWM pin definition const PinMap PinMap_PWM[] in PeripheralPins.c must be compounded here.
 // See PWM_PIN(x) definition for details.
@@ -171,22 +171,22 @@
 // TFT with FSMC interface
 //
 #if HAS_FSMC_TFT
-  #define TOUCH_CS_PIN                        PD11  
-  #define TOUCH_SCK_PIN                       PB13
-  #define TOUCH_MISO_PIN                      PB14
-  #define TOUCH_MOSI_PIN                      PB15
+  #define TOUCH_CS_PIN                      PD11
+  #define TOUCH_SCK_PIN                     PB13
+  #define TOUCH_MISO_PIN                    PB14
+  #define TOUCH_MOSI_PIN                    PB15
 
-  #define TFT_RESET_PIN                       PB12
-  #define TFT_BACKLIGHT_PIN                   PG8
+  #define TFT_RESET_PIN                     PB12
+  #define TFT_BACKLIGHT_PIN                 PG8
 
   #define LCD_USE_DMA_FSMC                    // Use DMA transfers to send data to the TFT
-  #define FSMC_DMA_DEV                        DMA2
-  #define FSMC_DMA_CHANNEL                    DMA_CH5
-  #define FSMC_CS_PIN                         PG12
-  #define FSMC_RS_PIN                         PG2
+  #define FSMC_DMA_DEV                      DMA2
+  #define FSMC_DMA_CHANNEL               DMA_CH5
+  #define FSMC_CS_PIN                       PG12
+  #define FSMC_RS_PIN                       PG2
 
-  #define TFT_CS_PIN                          FSMC_CS_PIN
-  #define TFT_RS_PIN                          FSMC_RS_PIN
+  #define TFT_CS_PIN                 FSMC_CS_PIN
+  #define TFT_RS_PIN                 FSMC_RS_PIN
 
   #if ENABLED(TFT_LVGL_UI)
     #define HAS_SPI_FLASH_FONT                    1
@@ -196,44 +196,44 @@
     #define HAS_BAK_VIEW_IN_FLASH                 0
     #define HAS_LOGO_IN_FLASH                     0
   #elif ANY(TFT_CLASSIC_UI, TFT_COLOR_UI)
-    //#define TFT_DRIVER                        ILI9488
-    #define TFT_BUFFER_WORDS                  14400
+    //#define TFT_DRIVER                 ILI9488
+    #define TFT_BUFFER_WORDS               14400
   #endif
 
   // Touch Screen calibration
   #if ENABLED(TFT_TRONXY_X5SA)
     #ifndef TOUCH_CALIBRATION_X
-      #define TOUCH_CALIBRATION_X             -17181
+      #define TOUCH_CALIBRATION_X         -17181
     #endif
     #ifndef TOUCH_CALIBRATION_Y
-      #define TOUCH_CALIBRATION_Y             11434
+      #define TOUCH_CALIBRATION_Y          11434
     #endif
     #ifndef TOUCH_OFFSET_X
-      #define TOUCH_OFFSET_X                    501
+      #define TOUCH_OFFSET_X                 501
     #endif
     #ifndef TOUCH_OFFSET_Y
-      #define TOUCH_OFFSET_Y                     -9
+      #define TOUCH_OFFSET_Y                  -9
     #endif
     #ifndef TOUCH_ORIENTATION
-      #define TOUCH_ORIENTATION     TOUCH_LANDSCAPE
+      #define TOUCH_ORIENTATION  TOUCH_LANDSCAPE
     #endif
   #endif
    
   #if ENABLED(MKS_ROBIN_TFT43)
     #ifndef TOUCH_CALIBRATION_X
-      #define TOUCH_CALIBRATION_X             17184
+      #define TOUCH_CALIBRATION_X          17184
     #endif
     #ifndef TOUCH_CALIBRATION_Y
-      #define TOUCH_CALIBRATION_Y             10604
+      #define TOUCH_CALIBRATION_Y          10604
     #endif
     #ifndef TOUCH_OFFSET_X
-      #define TOUCH_OFFSET_X                    -31
+      #define TOUCH_OFFSET_X                 -31
     #endif
     #ifndef TOUCH_OFFSET_Y
-      #define TOUCH_OFFSET_Y                    -29
+      #define TOUCH_OFFSET_Y                 -29
     #endif
     #ifndef TOUCH_ORIENTATION
-      #define TOUCH_ORIENTATION     TOUCH_LANDSCAPE
+      #define TOUCH_ORIENTATION  TOUCH_LANDSCAPE
     #endif
   #endif
 #else 
@@ -244,12 +244,12 @@
 // Power Loss
 //
 #if ENABLED(PSU_CONTROL)
-  #define PS_ON_PIN                           PG10
-  #define POWER_LOSS_PIN                      PE1
+  #define PS_ON_PIN                         PG10
+  #define POWER_LOSS_PIN                    PE1
 #endif
 
 //
 // Misc. Functions
 //
-//#define LED_PIN                               PG10
-#define BEEPER_PIN                            PA8
+//#define LED_PIN                           PG10
+#define BEEPER_PIN                          PA8
