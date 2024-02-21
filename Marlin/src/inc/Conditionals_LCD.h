038b1db36183a6a5d3fd6c169315e9d07e981961
classicrocker883 <andrewleduc88@yahoo.com>
2024-02-20 20:56:23 -0500

    grid_loop test (warning)

--- Marlin/src/inc/Conditionals_LCD.h
+++ Marlin/src/inc/Conditionals_LCD.h
@@ -1565,7 +1565,7 @@
 
 #ifdef GRID_MAX_POINTS_X
   #define GRID_MAX_POINTS ((GRID_MAX_POINTS_X) * (GRID_MAX_POINTS_Y))
-  #define GRID_LOOP(A,B) for (uint8_t A = 0; A < GRID_MAX_POINTS_X; ++A) for (uint8_t B = 0; B < GRID_MAX_POINTS_Y; ++B)
+  #define GRID_LOOP(A,B) for (uint8_t A = 0; A < GRID_MAX_POINTS_X + 1; ++A) for (uint8_t B = 0; B < GRID_MAX_POINTS_Y + 1; ++B)
 #endif
 
 // Slim menu optimizations
