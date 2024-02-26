9b3b10f957fab8f0ca85a0abadd533a6cc66e162
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2024-02-26 16:10:05 +0000

    Move "About Printer" to end of menu...
    ... because in most software the "About" menu always comes at the bottom (because it is least frequently accessed).
    
    (N.B. Also removing a comment which is no longer needed after #26172.)

--- Marlin/src/lcd/menu/menu_main.cpp
+++ Marlin/src/lcd/menu/menu_main.cpp
@@ -227,7 +227,6 @@ void menu_configuration();
 #endif // CUSTOM_MENU_MAIN
 
 #if ENABLED(ADVANCED_PAUSE_FEATURE)
-  // This menu item is last with an encoder. Otherwise, somewhere in the middle.
   #if E_STEPPERS == 1 && DISABLED(FILAMENT_LOAD_UNLOAD_GCODES)
     #define FILAMENT_CHANGE_ITEM() YESNO_ITEM(MSG_FILAMENTCHANGE, menu_change_filament, nullptr, \
                                     GET_TEXT_F(MSG_FILAMENTCHANGE), (const char *)nullptr, F("?"))
@@ -368,10 +367,6 @@ void menu_main() {
     }
   #endif
 
-  #if ENABLED(LCD_INFO_MENU)
-    SUBMENU(MSG_INFO_MENU, menu_info);
-  #endif
-
   #if ANY(LED_CONTROL_MENU, CASE_LIGHT_MENU)
     SUBMENU(MSG_LEDS, menu_led);
   #endif
@@ -501,6 +496,10 @@ void menu_main() {
         GET_TEXT_F(MSG_HOST_SHUTDOWN), (const char *)nullptr, F("?")
       );
     });
+  #endif
+
+  #if ENABLED(LCD_INFO_MENU)
+    SUBMENU(MSG_INFO_MENU, menu_info);
   #endif
 
   END_MENU();
