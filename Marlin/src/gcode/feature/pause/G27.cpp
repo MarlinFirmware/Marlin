9c8d9f22540817b549011365049240789a3b3f27
classicrocker883 <andrewleduc88@yahoo.com>
2024-02-29 14:13:24 -0500

    append G27 to include switch case

--- Marlin/src/gcode/feature/pause/G27.cpp
+++ Marlin/src/gcode/feature/pause/G27.cpp
@@ -39,10 +39,12 @@
  *             4 = No Z raise. Just XY parking.
  */
 void GcodeSuite::G27() {
-  // Don't allow nozzle parking without homing first, unless just Z raise (G27 P3)
+  // Don't allow nozzle parking without homing first, unless just Z raise (G27 P3) or just XY parking (G27 P4)
   const uint8_t pv = parser.ushortval('P');
-  if (pv != 3) {
-    if (homing_needed_error()) return;
+  switch (pv) {
+    case 3: break;
+    case 4: if (axis_is_trusted(X_AXIS) && axis_is_trusted(Y_AXIS)) break;
+    default: if (homing_needed_error()) return;
   }
   nozzle.park(pv);
 }
