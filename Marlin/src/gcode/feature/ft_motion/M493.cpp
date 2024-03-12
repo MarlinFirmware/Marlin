63c2473ba26607ee2b8294e2e34260e6b447884f
narno2202 <narno2202@gmail.com>
2024-03-13 00:06:41 +0100

    Add missing case in M493.cpp

--- Marlin/src/gcode/feature/ft_motion/M493.cpp
+++ Marlin/src/gcode/feature/ft_motion/M493.cpp
@@ -216,6 +216,8 @@ void GcodeSuite::M493() {
           case ftMotionCmpnstr_NONE:
           case ftMotionCmpnstr_ZV:
           case ftMotionCmpnstr_ZVD:
+          case ftMotionCmpnstr_ZVDD:
+          case ftMotionCmpnstr_ZVDDD:
           case ftMotionCmpnstr_EI:
           case ftMotionCmpnstr_2HEI:
           case ftMotionCmpnstr_3HEI:
@@ -239,6 +241,8 @@ void GcodeSuite::M493() {
             case ftMotionCmpnstr_NONE:
             case ftMotionCmpnstr_ZV:
             case ftMotionCmpnstr_ZVD:
+            case ftMotionCmpnstr_ZVDD:
+            case ftMotionCmpnstr_ZVDDD:
             case ftMotionCmpnstr_EI:
             case ftMotionCmpnstr_2HEI:
             case ftMotionCmpnstr_3HEI:
