f02d21900572e1e61733564b3c4db29bc765d116
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2024-03-15 11:32:15 +0000

    Add Dagoma D6 board as used in their DiscoUltimate v2 TMC.
    Taken from the Dagoma fork of Marlin DU_MC branch where it is called FYSETC_DAGOMA_F5 and explicitly confirmed by Dagoma as being the D6:
    
    "the BOARD_FYSETC_DAGOMA_F5 is effectively the definition for the D6"

---
+++ Marlin/src/pins/ramps/pins_DAGOMA_D6.h
@@ -0,0 +1,82 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#if HOTENDS > 2 || E_STEPPERS > 2
+  #error "Dagoma3D D6 supports only 2 hotends / E-steppers. Comment out this line to continue."
+#endif
+
+#define BOARD_INFO_NAME "Dagoma3D D6"
+
+//
+// Endstops
+//
+#define X_STOP_PIN                             2
+#define Y_STOP_PIN                             3
+#define Z_STOP_PIN                            15
+
+#define FIL_RUNOUT_PIN                        39
+#if EXTRUDERS > 1
+  #define FIL_RUNOUT2_PIN                     14
+#endif
+
+// Alter timing for graphical display
+#if IS_U8GLIB_ST7920
+  #define BOARD_ST7920_DELAY_1                 0
+  #define BOARD_ST7920_DELAY_2               250
+  #define BOARD_ST7920_DELAY_3               250
+#endif
+
+#define KILL_PIN                            -1  // NC
+
+#define LCD_CONTRAST_DEFAULT                 255
+
+//
+// Sensorless homing DIAG pin is not directly connected to the MCU. Close
+// the jumper next to the limit switch socket when using sensorless homing.
+//
+#if HAS_TMC_UART
+  /**
+   * TMC2208/TMC2209 stepper drivers
+   */
+  #define X_SERIAL_RX_PIN                     73
+  #define X_SERIAL_TX_PIN                     73
+  #define Y_SERIAL_RX_PIN                     73
+  #define Y_SERIAL_TX_PIN                     73
+  #define Z_SERIAL_RX_PIN                     73
+  #define Z_SERIAL_TX_PIN                     73
+  #define E0_SERIAL_RX_PIN                    73
+  #define E0_SERIAL_TX_PIN                    73
+  #define E1_SERIAL_RX_PIN                    12
+  #define E1_SERIAL_TX_PIN                    12
+
+  #define X_DIAG_PIN                          43
+  #define Y_DIAG_PIN                          41
+  #define Z_DIAG_PIN                          47
+  #define E0_DIAG_PIN                         21
+  #define E1_DIAG_PIN                         20
+#endif
+
+//
+// Import default RAMPS 1.4 pins
+//
+#include "pins_RAMPS.h"
