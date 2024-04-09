b934177743dc82536c59d2a212ff5b241ccde00b
classicrocker883 <andrewleduc88@yahoo.com>
2024-04-08 22:42:29 -0400

    revert to 2.1.1.1

--- Marlin/src/lcd/marlinui.h
+++ Marlin/src/lcd/marlinui.h
@@ -499,9 +499,7 @@ public:
    *
    * @param pfmt    A constant format P-string
    */
-  static void status_printf_P(int8_t level, PGM_P const pfmt, ...);
-
-  static void status_printf(int8_t level, FSTR_P const ffmt, ...) { status_printf_P(level, FTOP(ffmt)); }
+  static void status_printf(int8_t level, FSTR_P const pfmt, ...);
 
   // Periodic or as-needed display update
   static void update() IF_DISABLED(HAS_UI_UPDATE, {});
