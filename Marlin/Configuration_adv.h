e1ab911c356cb5b0b449fa4e6379dbe0a483c7a0
InsanityAutomation <38436470+InsanityAutomation@users.noreply.github.com>
2024-04-20 17:58:13 -0400

    📝Improve Freeze Feature comment (#26959)
    * Fix and Improve Freeze Feature comment

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4261,7 +4261,8 @@
 
 /**
  * Instant freeze / unfreeze functionality
- * Potentially useful for emergency stop that allows being resumed.
+ * Potentially useful for rapid stop that allows being resumed. Halts stepper movement.
+ * Note this does NOT pause spindles, lasers, fans, heaters or any other auxiliary device.
  * @section interface
  */
 //#define FREEZE_FEATURE
