ca7bfc08aa438ea4356ebf6623c796d8f5697b2c
noman1228 <jay.m.theriault@gmail.com>
2024-04-26 16:31:33 -0400

    UPDATE NEO BMP

--- Marlin/_Statusscreen.h
+++ Marlin/_Statusscreen.h
@@ -1,68 +1,34 @@
 /**
- * Marlin 3D Printer Firmware
- * Copyright (c) 2023 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
- *
- * Based on Sprinter and grbl.
- * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- */
-#pragma once
-
-/**
- * Custom Status Screen bitmap
- *
- * Place this file in the root with your configuration files
- * and enable CUSTOM_STATUS_SCREEN_IMAGE in Configuration.h.
- *
- * Use the Marlin Bitmap Converter to make your own:
- * https://marlinfw.org/tools/u8glib/converter.html
- */
-
-//
-// Status Screen Logo bitmap
-//
-/**
  * Made with Marlin Bitmap Converter
  * https://marlinfw.org/tools/u8glib/converter.html
  *
- * This bitmap from 40x18 C/C++ data
+ * This bitmap from the file 'NEO.bmp'
  */
+#pragma once
 
-#define STATUS_LOGO_Y 6
+#define STATUS_SCREEN_X 88
 #define STATUS_LOGO_WIDTH 40
 
 const unsigned char status_logo_bmp[] PROGMEM = {
-  B11111000,B00000001,B10000000,B00000000,B00001100, // #####..........##...................##..
-  B01001000,B00000000,B10000000,B00000000,B00010010, // .#..#...........#..................#..#.
-  B01000011,B11000011,B10001100,B11010000,B00000010, // .#....####....###...##..##.#..........#.
-  B01110001,B00100100,B10010010,B01100111,B11001100, // .###...#..#..#..#..#..#..##..#####..##..
-  B01000001,B00100100,B10011110,B01000000,B00000010, // .#.....#..#..#..#..####..#............#.
-  B01001001,B00100100,B10010000,B01000000,B00010010, // .#..#..#..#..#..#..#.....#.........#..#.
-  B11111011,B10110011,B11001110,B11100000,B00001100, // #####.###.##..####..###.###.........##..
-  B00000000,B00000000,B00000000,B00000000,B00000000, // ........................................
-  B00000000,B00000000,B00000000,B00000000,B00000000, // ........................................
-  B00000001,B11011100,B00000000,B00000000,B00000000, // .......###.###..........................
-  B00000000,B11001000,B00000000,B00000000,B00000000, // ........##..#...........................
-  B00000000,B11001000,B00000000,B00000000,B00000000, // ........##..#...........................
-  B00000000,B10101000,B11100001,B11000000,B00000000, // ........#.#.#...###....###..............
-  B00000000,B10101001,B00010010,B00100000,B00000000, // ........#.#.#..#...#..#...#.............
-  B00000000,B10101001,B11110010,B00100000,B00000000, // ........#.#.#..#####..#...#.............
-  B00000000,B10011001,B00000010,B00100000,B00000000, // ........#..##..#......#...#.............
-  B00000000,B10011001,B00010010,B00100000,B00000000, // ........#..##..#...#..#...#.............
-  B00000001,B11001000,B11100001,B11000000,B00000000  // .......###..#...###....###..............
+  B11111100,B01100110,B01111110,B01111110,B01111110,
+  B11000000,B01110110,B01100111,B01100000,B01100110,
+  B11000000,B01110110,B01100011,B01100000,B01100110,
+  B11111100,B01111110,B01100011,B01111110,B01111110,
+  B11111100,B01111110,B01100011,B01111110,B01111100,
+  B11000000,B01101110,B01100111,B01100000,B01100110,
+  B11111100,B01101110,B01111110,B01111110,B01100110,
+  B11111100,B01100110,B01111100,B01111110,B01100011,
+  B00000000,B00000000,B00000000,B00000000,B00000000,
+  B11110000,B11100011,B11111111,B10000011,B11110000,
+  B11110000,B11100011,B11111111,B10001111,B11111100,
+  B11111000,B11100011,B10000000,B00011110,B00011110,
+  B11111100,B11100011,B10000000,B00111100,B00001111,
+  B11111110,B11100011,B11111111,B00111000,B00000111,
+  B11101111,B11100011,B11111111,B00111000,B00000111,
+  B11100111,B11100011,B10000000,B00111100,B00001111,
+  B11100011,B11100011,B10000000,B00011110,B00011110,
+  B11100001,B11100011,B11111111,B10001111,B11111100,
+  B11100000,B11100011,B11111111,B10000011,B11110000
 };
 
 
@@ -72,10 +38,5 @@ const unsigned char status_logo_bmp[] PROGMEM = {
 #define STATUS_HOTEND_ANIM
 #define STATUS_BED_ANIM
 #define STATUS_HEATERS_XSPACE   20
-#if HOTENDS < 2
-  #define STATUS_HEATERS_X      48
-  #define STATUS_BED_X          72
-#else
-  #define STATUS_HEATERS_X      40
-  #define STATUS_BED_X          80
-#endif
+#define STATUS_HEATERS_X      48
+#define STATUS_BED_X          72
