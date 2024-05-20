3ea0ef101360d424e215f275b69ca9b01ca89bb0
Ryan P <rpower@sysreset.com>
2024-05-20 03:24:42 -0600

    If SKR_MINI_SCREEN_ADAPTER is defined use a different software SPI function to access the display

--- Marlin/src/HAL/STM32/u8g/LCD_defines.h
+++ Marlin/src/HAL/STM32/u8g/LCD_defines.h
@@ -25,8 +25,13 @@
  * STM32 LCD-specific defines
  */
 
-uint8_t u8g_com_std_sw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);         // See U8glib-HAL
-uint8_t u8g_com_stm32duino_hw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);  // See U8glib-HAL
+#if ENABLED(SKR_MINI_SCREEN_ADAPTER)
+    uint8_t u8g_com_HAL_STM32_sw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);
+    #define U8G_COM_HAL_SW_SPI_FN     u8g_com_HAL_STM32_sw_spi_fn
+#else
+    uint8_t u8g_com_std_sw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);         // See U8glib-HAL
+    #define U8G_COM_HAL_SW_SPI_FN u8g_com_std_sw_spi_fn
+#endif
 
-#define U8G_COM_HAL_SW_SPI_FN u8g_com_std_sw_spi_fn
+uint8_t u8g_com_stm32duino_hw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);  // See U8glib-HAL
 #define U8G_COM_HAL_HW_SPI_FN u8g_com_stm32duino_hw_spi_fn
