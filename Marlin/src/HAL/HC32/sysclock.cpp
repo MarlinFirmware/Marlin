2587f4336507d5ccfe9d59394ef70091e6f91763
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-05-20 14:18:22 +0200

    HC32 clock auto-configuration from a single constant

--- Marlin/src/HAL/HC32/sysclock.cpp
+++ Marlin/src/HAL/HC32/sysclock.cpp
@@ -26,11 +26,152 @@
 
 #ifdef ARDUINO_ARCH_HC32
 
-// Get BOARD_XTAL_FREQUENCY from configuration / pins
 #include "../../inc/MarlinConfig.h"
+#include "sysclock.h"
 
 #include <core_hooks.h>
 #include <drivers/sysclock/sysclock_util.h>
+
+/**
+ * @brief Constexpr for loop helper.
+ * @tparam Start The start value.
+ * @tparam End The end value.
+ * @tparam Inc The increment value.
+ * @tparam F The function to call.
+ * 
+ * @note Taken from https://artificial-mind.net/blog/2020/10/31/constexpr-for 
+ */
+template <auto Start, auto End, auto Inc, class F>
+constexpr void constexpr_for(F&& f)
+{
+    if constexpr (Start < End)
+    {
+        f(std::integral_constant<decltype(Start), Start>());
+        constexpr_for<Start + Inc, End, Inc>(f);
+    }
+}
+
+/***
+ * @brief Automatically calculate M, N, P values for the MPLL to reach a target frequency.
+ * @param input_frequency The input frequency.
+ * @param target_frequency The target frequency.
+ * @return The MPLL configuration structure. Q and R are not set.
+ * 
+ * @note
+ * Simplified MPLL block diagram, with intermediary clocks (1) = VCO_in, (2) = VCO_out:
+ * 
+ *  INPUT -> [/ M] -(1)-> [* N] -(2)-|-> [/ P] -> MPLL-P
+ */
+constexpr stc_clk_mpll_cfg_t get_mpll_config(const uint32_t input_frequency, const uint32_t target_frequency)
+{
+  uint32_t best_M = 0, 
+      best_N = 0, 
+      best_P = 0;
+
+  // FIXME don't consider M in auto-config, as otherwise we get 
+  //       "expression not folded to a constant due to excessive constexpr function call complexity"
+  const int M = 1;
+
+  // input clock divider: M in [1, 24]
+  //constexpr_for<1, 24, 1>([&input_frequency, &target_frequency, &best_M, &best_N, &best_P] (int M) {
+    // VCO multiplier: N in [20, 480]
+    constexpr_for<20, 480, 1>([&input_frequency, &target_frequency, &best_M, &best_N, &best_P, &M] (int N) {
+      // output "P" divider: P in [2, 16]
+      constexpr_for<2, 16, 1>([&input_frequency, &target_frequency, &best_M, &best_N, &best_P, &M, &N] (int P) {
+        // calculate actual frequencies given M, N, P
+        const double vco_in_frequency = static_cast<double>(input_frequency) / static_cast<double>(M);
+        const double vco_out_frequency = vco_in_frequency * static_cast<double>(N);
+        const double pll_out_frequency = vco_out_frequency / static_cast<double>(P);
+
+        // 1 <= VCO_in <= 25 MHz
+        if (vco_in_frequency < 1e6 || vco_in_frequency > 25e6) {
+          return;
+        }
+
+        // 240 <= VCO_out <= 480 MHz
+        if (vco_out_frequency < 240e6 || vco_out_frequency > 480e6) {
+          return;
+        }
+
+        // check if the calculate frequency matches the target
+        // only if no previous match has been found
+        if (pll_out_frequency == target_frequency && best_M == 0) {
+          best_M = M;
+          best_N = N;
+          best_P = P;
+        }
+      });
+    });
+  //});
+
+  return {
+    .PllpDiv = best_P,
+    .PllqDiv = best_P, // don't care for Q and R
+    .PllrDiv = best_P, // "
+    .plln = best_N,
+    .pllmDiv = best_M,
+  };
+}
+
+/**
+ * @brief Get the division factor required to get the target frequency from the input frequency.
+ * @tparam input_freq The input frequency.
+ * @tparam target_freq The target frequency.
+ * @return The division factor. 
+ */
+template <uint32_t input_freq, uint32_t target_freq>
+constexpr en_clk_sysclk_div_factor_t get_division_factor()
+{
+  // Calculate the divider to get the target frequency
+  constexpr float fdivider = static_cast<float>(input_freq) / static_cast<float>(target_freq);
+  constexpr int divider = static_cast<int>(fdivider);
+
+  // divider must be an integer
+  static_assert(fdivider == divider, "Target frequency not achievable, divider must be an integer");
+
+  // divider must be between 1 and 64 (enum range), and must be a power of 2
+  static_assert(divider >= 1 && divider <= 64, "Invalid divider, out of range");
+  static_assert((divider & (divider - 1)) == 0, "Invalid divider, not a power of 2");
+
+  // return the divider
+  switch (divider)
+  {
+    case 1:  return ClkSysclkDiv1;
+    case 2:  return ClkSysclkDiv2;
+    case 4:  return ClkSysclkDiv4;
+    case 8:  return ClkSysclkDiv8;
+    case 16: return ClkSysclkDiv16;
+    case 32: return ClkSysclkDiv32;
+    case 64: return ClkSysclkDiv64;
+  }
+}
+
+/**
+ * @brief Validate the runtime clocks match the expected values. 
+ */
+void validate_system_clocks()
+{
+  #define CLOCK_ASSERT(expected, actual)                  \
+    if (expected != actual) {                             \
+      SERIAL_ECHOPGM(                                     \
+        "Clock Mismatch for " #expected ": "              \
+        "expected ", expected,                            \
+        ", got ", actual                                  \
+        );                                                \
+      CORE_ASSERT_FAIL("Clock Mismatch: " #expected);     \
+    }
+
+  update_system_clock_frequencies();
+
+  CLOCK_ASSERT(F_SYSTEM_CLOCK, SYSTEM_CLOCK_FREQUENCIES.system);
+  CLOCK_ASSERT(F_HCLK, SYSTEM_CLOCK_FREQUENCIES.hclk);
+  CLOCK_ASSERT(F_EXCLK, SYSTEM_CLOCK_FREQUENCIES.exclk);
+  CLOCK_ASSERT(F_PCLK0, SYSTEM_CLOCK_FREQUENCIES.pclk0);
+  CLOCK_ASSERT(F_PCLK1, SYSTEM_CLOCK_FREQUENCIES.pclk1);
+  CLOCK_ASSERT(F_PCLK2, SYSTEM_CLOCK_FREQUENCIES.pclk2);
+  CLOCK_ASSERT(F_PCLK3, SYSTEM_CLOCK_FREQUENCIES.pclk3);
+  CLOCK_ASSERT(F_PCLK4, SYSTEM_CLOCK_FREQUENCIES.pclk4);
+}
 
 /**
  * @brief Configure HC32 system clocks.
@@ -45,53 +186,19 @@ void core_hook_sysclock_init() {
   sysclock_configure_flash_wait_cycles();
   sysclock_configure_sram_wait_cycles();
 
-  // Configure MPLLp to 200 MHz output, with different settings depending on XTAL availability
-  #if BOARD_XTAL_FREQUENCY == 8000000 // 8 MHz XTAL
-    // - M = 1   => 8 MHz / 1 = 8 MHz
-    // - N = 50  => 8 MHz * 50 = 400 MHz
-    // - P = 2   => 400 MHz / 2 = 200 MHz (sysclk)
-    // - Q,R = 4 => 400 MHz / 4 = 100 MHz (dont care)
-    stc_clk_mpll_cfg_t pllConf = {
-        .PllpDiv = 2u, // P
-        .PllqDiv = 4u, // Q
-        .PllrDiv = 4u, // R
-        .plln = 50u,   // N
-        .pllmDiv = 1u, // M
-    };
+  // Select MPLL input frequency based on clock availability
+  #if BOARD_XTAL_FREQUENCY == 8000000 || BOARD_XTAL_FREQUENCY == 16000000 // 8 MHz or 16 MHz XTAL
+    constexpr uint32_t mpll_input_clock = BOARD_XTAL_FREQUENCY;
+
     sysclock_configure_xtal();
-    sysclock_configure_mpll(ClkPllSrcXTAL, &pllConf);
-
-  #elif BOARD_XTAL_FREQUENCY == 16000000 // 16 MHz XTAL
-    // - M = 1   => 16 MHz / 1 = 16 MHz
-    // - N = 50  => 16 MHz * 25 = 400 MHz
-    // - P = 2   => 400 MHz / 2 = 200 MHz (sysclk)
-    // - Q,R = 4 => 400 MHz / 4 = 100 MHz (dont care)
-    stc_clk_mpll_cfg_t pllConf = {
-        .PllpDiv = 2u, // P
-        .PllqDiv = 4u, // Q
-        .PllrDiv = 4u, // R
-        .plln = 50u,   // N
-        .pllmDiv = 1u, // M
-    };
-    sysclock_configure_xtal();
-    sysclock_configure_mpll(ClkPllSrcXTAL, &pllConf);
-
-  #warning "HC32F460 with 16 MHz XTAL has not been tested."
-
+    
+    #if BOARD_XTAL_FREQUENCY == 16000000
+      #warning "HC32F460 with 16 MHz XTAL has not been tested."
+    #endif
   #else // HRC (16 MHz)
-    // - M = 1   => 16 MHz / 1 = 16 MHz
-    // - N = 25  => 16 MHz * 25 = 400 MHz
-    // - P = 2   => 400 MHz / 2 = 200 MHz (sysclk)
-    // - Q,R = 4 => 400 MHz / 4 = 100 MHz (dont care)
-    stc_clk_mpll_cfg_t pllConf = {
-        .PllpDiv = 2u, // P
-        .PllqDiv = 4u, // Q
-        .PllrDiv = 4u, // R
-        .plln = 25u,   // N
-        .pllmDiv = 1u, // M
-    };
+    constexpr uint32_t mpll_input_clock = 16000000;
+
     sysclock_configure_hrc();
-    sysclock_configure_mpll(ClkPllSrcHRC, &pllConf);
 
     // HRC could have been configured by ICG to 20 MHz
     // TODO: handle gracefully if HRC is not 16 MHz
@@ -99,29 +206,55 @@ void core_hook_sysclock_init() {
       panic("HRC is not 16 MHz");
     }
 
-    #ifdef BOARD_XTAL_FREQUENCY
+    #if defined(BOARD_XTAL_FREQUENCY)
       #warning "No valid XTAL frequency defined, falling back to HRC."
     #endif
   #endif
 
-  // MPLL output configured according to F_HCLK (200MHz)
-  constexpr uint32_t mpll_clock = F_HCLK;
+  // Automagically calculate MPLL configuration
+  constexpr stc_clk_mpll_cfg_t pllConf = get_mpll_config(mpll_input_clock, F_SYSTEM_CLOCK);
+  static_assert(pllConf.pllmDiv != 0 && pllConf.plln != 0 && pllConf.PllpDiv != 0, "MPLL auto-configuration failed");
+  sysclock_configure_mpll(ClkPllSrcXTAL, &pllConf);
 
-  // Setup clock divisors for sysclk = 200 MHz
-  // Note: PCLK1 is used for step+temp timers, and need to be kept at 50 MHz (until there is a better solution)
+  // Setup clock divisors
   constexpr stc_clk_sysclk_cfg_t sysClkConf = {
-      .enHclkDiv  = ClkSysclkDiv1, // HCLK  = 200 MHz (CPU)
-      .enExclkDiv = ClkSysclkDiv2, // EXCLK = 100 MHz (SDIO)
-      .enPclk0Div = ClkSysclkDiv2, // PCLK0 = 100 MHz (Timer6 (not used))
-      .enPclk1Div = ClkSysclkDiv4, // PCLK1 = 50 MHz (USART, SPI, I2S, Timer0 (step+temp), TimerA (Servo))
-      .enPclk2Div = ClkSysclkDiv8, // PCLK2 = 25 MHz (ADC)
-      .enPclk3Div = ClkSysclkDiv8, // PCLK3 = 25 MHz (I2C, WDT)
-      .enPclk4Div = ClkSysclkDiv2, // PCLK4 = 100 MHz (ADC ctl)
+      .enHclkDiv  = get_division_factor<F_SYSTEM_CLOCK, F_HCLK>(),
+      .enExclkDiv = get_division_factor<F_SYSTEM_CLOCK, F_EXCLK>(),
+      .enPclk0Div = get_division_factor<F_SYSTEM_CLOCK, F_PCLK0>(),
+      .enPclk1Div = get_division_factor<F_SYSTEM_CLOCK, F_PCLK1>(),
+      .enPclk2Div = get_division_factor<F_SYSTEM_CLOCK, F_PCLK2>(),
+      .enPclk3Div = get_division_factor<F_SYSTEM_CLOCK, F_PCLK3>(),
+      .enPclk4Div = get_division_factor<F_SYSTEM_CLOCK, F_PCLK4>(),
   };
+  sysclock_set_clock_dividers(&sysClkConf);
 
+  // Set power mode, before switch
+  power_mode_update_pre(F_SYSTEM_CLOCK);
+
+  // Switch to MPLL-P as system clock source
+  CLK_SetSysClkSource(CLKSysSrcMPLL);
+
+  // Set power mode, after switch
+  power_mode_update_post(F_SYSTEM_CLOCK);
+
+  // Verify clocks match expected values (at runtime)
+  #if ENABLED(MARLIN_DEV_MODE) || ENABLED(ALWAYS_VALIDATE_CLOCKS)
+    validate_system_clocks();
+  #endif
+
+  // Verify clock configuration (at compile time)
   #if ARDUINO_CORE_VERSION_INT >= GET_VERSION_INT(1, 2, 0)
+    assert_mpll_config_valid<
+      mpll_input_clock,
+      pllConf.pllmDiv,  
+      pllConf.plln,
+      pllConf.PllpDiv,
+      pllConf.PllqDiv,
+      pllConf.PllrDiv
+    >();
+    
     assert_system_clocks_valid<
-      mpll_clock,
+      F_SYSTEM_CLOCK,
       sysClkConf.enHclkDiv,
       sysClkConf.enPclk0Div,
       sysClkConf.enPclk1Div,
@@ -130,18 +263,14 @@ void core_hook_sysclock_init() {
       sysClkConf.enPclk4Div,
       sysClkConf.enExclkDiv
     >();
+
+    static_assert(get_mpll_output_clock(
+      mpll_input_clock,
+      pllConf.pllmDiv,
+      pllConf.plln,
+      pllConf.PllpDiv
+    ) == F_SYSTEM_CLOCK, "actual MPLL output clock does not match F_SYSTEM_CLOCK");
   #endif
-
-  sysclock_set_clock_dividers(&sysClkConf);
-
-  // Set power mode
-  power_mode_update_pre(mpll_clock);
-
-  // Switch to MPLL as system clock source
-  CLK_SetSysClkSource(CLKSysSrcMPLL);
-
-  // Set power mode
-  power_mode_update_post(mpll_clock);
 }
 
 #endif // ARDUINO_ARCH_HC32
