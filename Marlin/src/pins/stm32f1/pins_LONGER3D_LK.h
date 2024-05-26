a942c933365e55fc2d4a86265a17a0c042b8ba62
ellensp <530024+ellensp@users.noreply.github.com>
2024-05-27 05:09:18 +1200

    🩹 Longer3D LK has ONBOARD_SDIO (#27129)

--- Marlin/src/pins/stm32f1/pins_LONGER3D_LK.h
+++ Marlin/src/pins/stm32f1/pins_LONGER3D_LK.h
@@ -160,7 +160,8 @@
   #define LCD_BRIGHTNESS_DEFAULT TFT_BACKLIGHT_PWM
 #endif
 
-#if ENABLED(ONBOARD_SDIO)
+#if SD_CONNECTION_IS(ONBOARD)
+  #define ONBOARD_SDIO
   #define SD_SS_PIN                         -1    // else SDSS set to PA4 in M43 (spi_pins.h)
 #endif
 
