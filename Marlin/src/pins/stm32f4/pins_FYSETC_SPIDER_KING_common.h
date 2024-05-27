0de68481cfa00e6b0c4983dbeb956127aa360469
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2024-05-27 03:04:08 -0700

    Flip EEPROM conditional

--- Marlin/src/pins/stm32f4/pins_FYSETC_SPIDER_KING_common.h
+++ Marlin/src/pins/stm32f4/pins_FYSETC_SPIDER_KING_common.h
@@ -41,17 +41,6 @@
 // EEPROM
 //
 #if NOT_TARGET(STM32H7)
-  // Emulated EEPROM for STM32H7
-  #if ANY(NO_EEPROM_SELECTED, FLASH_EEPROM_EMULATION)
-    #undef NO_EEPROM_SELECTED
-    #ifndef FLASH_EEPROM_EMULATION
-      #define FLASH_EEPROM_EMULATION
-    #endif
-    #define EEPROM_PAGE_SIZE      (0x800UL) // 2K
-    #define EEPROM_START_ADDRESS  (0x8000000UL + (STM32_FLASH_SIZE) * 1024UL - (EEPROM_PAGE_SIZE) * 2UL)
-    #define MARLIN_EEPROM_SIZE    EEPROM_PAGE_SIZE
-  #endif
-#else
   // I2C EEPROM for STM32F4. Does not work on Spider King with STM32H7 MCU.
   #if NO_EEPROM_SELECTED
     #undef NO_EEPROM_SELECTED
@@ -65,6 +54,17 @@
     #define I2C_SCL_PIN                     PF1
     #define I2C_SDA_PIN                     PF0
     #define MARLIN_EEPROM_SIZE             0x1000  // 4KB
+  #endif
+#else
+  // Emulated EEPROM for STM32H7
+  #if ANY(NO_EEPROM_SELECTED, FLASH_EEPROM_EMULATION)
+    #undef NO_EEPROM_SELECTED
+    #ifndef FLASH_EEPROM_EMULATION
+      #define FLASH_EEPROM_EMULATION
+    #endif
+    #define EEPROM_PAGE_SIZE      (0x800UL) // 2K
+    #define EEPROM_START_ADDRESS  (0x8000000UL + (STM32_FLASH_SIZE) * 1024UL - (EEPROM_PAGE_SIZE) * 2UL)
+    #define MARLIN_EEPROM_SIZE    EEPROM_PAGE_SIZE
   #endif
 #endif
 
