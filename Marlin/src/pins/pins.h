d17125c15ed2f05b532b4fa41a46863949a47c6b
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2024-05-27 03:32:02 -0700

    env:FYSETC_SPIDER_KING723 => env:STM32H723ZG_fysetc

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -858,9 +858,9 @@
 #elif MB(BTT_KRAKEN_V1_0)
   #include "stm32h7/pins_BTT_KRAKEN_V1_0.h"              // STM32H7                         env:STM32H723ZG_btt
 #elif MB(FYSETC_SPIDER_KING_V1_H723)
-  #include "stm32h7/pins_FYSETC_SPIDER_KING_V1_H723.h"   // STM32H7                         env:FYSETC_SPIDER_KING723
+  #include "stm32h7/pins_FYSETC_SPIDER_KING_V1_H723.h"   // STM32H7                         env:STM32H723ZG_fysetc
 #elif MB(FYSETC_SPIDER_KING_V1_1_H723)
-  #include "stm32h7/pins_FYSETC_SPIDER_KING_V1_1_H723.h" // STM32H7                         env:FYSETC_SPIDER_KING723
+  #include "stm32h7/pins_FYSETC_SPIDER_KING_V1_1_H723.h" // STM32H7                         env:STM32H723ZG_fysetc
 #elif MB(TEENSY41)
   #include "teensy4/pins_TEENSY41.h"                     // Teensy-4.x                      env:teensy41
 #elif MB(T41U5XBB)
