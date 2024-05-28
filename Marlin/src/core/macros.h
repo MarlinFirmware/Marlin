7c4855381b852ce32ad19aa92e7636b212495495
Mihai <299015+mh-dm@users.noreply.github.com>
2024-05-29 02:22:39 +0300

    🐛 Prevent 'EVAL' macro collision (#27132)

--- Marlin/src/core/macros.h
+++ Marlin/src/core/macros.h
@@ -679,7 +679,7 @@
 #define DEFER4(M) M EMPTY EMPTY EMPTY EMPTY()()()()
 
 // Force define expansion
-#define EVAL           EVAL16
+#define EVAL(V...)     EVAL16(V)
 #define EVAL4096(V...) EVAL2048(EVAL2048(V))
 #define EVAL2048(V...) EVAL1024(EVAL1024(V))
 #define EVAL1024(V...) EVAL512(EVAL512(V))
