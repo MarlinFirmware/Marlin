94df08238b036967abbb151d6345cdb6f61bd358
Andrew <18502096+classicrocker883@users.noreply.github.com>
2024-05-29 03:35:08 -0400

    Update DGUSScreenHandler.cpp

--- Marlin/src/lcd/extui/dgus_reloaded/DGUSScreenHandler.cpp
+++ Marlin/src/lcd/extui/dgus_reloaded/DGUSScreenHandler.cpp
@@ -315,9 +315,10 @@ void DGUSScreenHandler::filamentRunout(const ExtUI::extruder_t extruder) {
       #endif
       #if ENABLED(PIDTEMPCHAMBER)
         case ExtUI::PID_CHAMBER_STARTED:
-          setStatusMessage(GET_TEXT_F(MSG_PID_AUTOTUNE));
-          break;
       #endif
+        setStatusMessage(GET_TEXT_F(MSG_PID_AUTOTUNE));
+        break;
+      
       case ExtUI::PID_BAD_HEATER_ID:
         setStatusMessage(GET_TEXT_F(MSG_PID_BAD_HEATER_ID));
         break;
