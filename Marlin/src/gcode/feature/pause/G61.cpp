591a7986f54ce62268d878d84cc9606f5f4b67b7
Evan Felix <karcaw@gmail.com>
2024-05-31 10:31:08 -0700

    Fix No Integer restore in G61
    This Fixes the case where no Integers were specified on the G61 command:
    ```
    G60
    G0 X100 Y100 Z20
    G61 X Y  
    ```
    does not return to the last location as :
    ```
    G60
    G0 X100 Y100 Z20
    G61 X0 Y0
    ```
    does.  this matches what the spec says:
    https://marlinfw.org/docs/gcode/G061.html
    which does not even document you can put an integer after the axis letters.

--- Marlin/src/gcode/feature/pause/G61.cpp
+++ Marlin/src/gcode/feature/pause/G61.cpp
@@ -86,7 +86,7 @@ void GcodeSuite::G61() {
       LOOP_NUM_AXES(i) {
         destination[i] = parser.seen(AXIS_CHAR(i))
           ? stored_position[slot][i] + parser.value_axis_units((AxisEnum)i)
-          : current_position[i];
+          : stored_position[slot][i];
         DEBUG_ECHO(C(' '), C(AXIS_CHAR(i)), p_float_t(destination[i], 2));
       }
       DEBUG_EOL();
