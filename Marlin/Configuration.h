9774fa3e2528a36deb0e29eeab28eaa2b935ed04
Gadgeteer <pmhunt1972@gmail.com>
2024-06-13 20:37:06 +0100

    Normalise Config. files
    Normalise Config. files

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1578,8 +1578,8 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 2
-#define EXTRA_PROBING    1
+//#define MULTIPLE_PROBING 2
+//#define EXTRA_PROBING    1
 
 /**
  * Z probes require clearance when deploying, stowing, and moving between
