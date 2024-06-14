9d5d8ff0e6352caafa97e99d293c66e2d06d5347
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-06-14 15:37:40 -0500

    fix cli exec

--- buildroot/share/PlatformIO/scripts/STM32F1_build_flags.py
+++ buildroot/share/PlatformIO/scripts/STM32F1_build_flags.py
@@ -1,56 +1,63 @@
+#!/usr/bin/env python
+#
+# STM32F1_build_flags.py
+# Add build_flags for the base STM32F1_maple environment (stm32f1-maple.ini)
+#
 from __future__ import print_function
 import sys
 
 # Dynamic build flags for generic compile options
 if __name__ == "__main__":
-  args = " ".join([ "-std=gnu++14",
-                    "-Os",
-                    "-mcpu=cortex-m3",
-                    "-mthumb",
 
-                    "-fsigned-char",
-                    "-fno-move-loop-invariants",
-                    "-fno-strict-aliasing",
-                    "-fsingle-precision-constant",
+    # Print these plus the given args when running directly on the command-line
+    args = [
+        "-std=gnu++14",
+        "-Os",
+        "-mcpu=cortex-m3",
+        "-mthumb",
 
-                    "--specs=nano.specs",
-                    "--specs=nosys.specs",
+        "-fsigned-char",
+        "-fno-move-loop-invariants",
+        "-fno-strict-aliasing",
+        "-fsingle-precision-constant",
 
-                    "-IMarlin/src/HAL/STM32F1",
+        "--specs=nano.specs",
+        "--specs=nosys.specs",
 
-                    "-MMD",
-                    "-MP",
-                    "-DTARGET_STM32F1"
-                  ])
+        "-MMD", "-MP",
 
-  for i in range(1, len(sys.argv)):
-    args += " " + sys.argv[i]
+        "-IMarlin/src/HAL/STM32F1",
 
-  print(args)
+        "-DTARGET_STM32F1",
+        "-DARDUINO_ARCH_STM32",
+        "-DPLATFORM_M997_SUPPORT"
+    ] + sys.argv[1:]
+
+    print(" ".join(args))
 
 else:
 
-  # Extra script for stm32f1-maple.ini build_flags
+    # Extra script for stm32f1-maple.ini build_flags
 
-  import pioutil
-  if pioutil.is_pio_build():
-    pioutil.env.Append(
-      ARFLAGS=["rcs"],
+    import pioutil
+    if pioutil.is_pio_build():
+        pioutil.env.Append(
+            ARFLAGS=["rcs"],
 
-      ASFLAGS=["-x", "assembler-with-cpp"],
+            ASFLAGS=["-x", "assembler-with-cpp"],
 
-      CXXFLAGS=[
-        "-fabi-version=0",
-        "-fno-use-cxa-atexit",
-        "-fno-threadsafe-statics"
-      ],
-      LINKFLAGS=[
-        "-Os",
-        "-mcpu=cortex-m3",
-        "-ffreestanding",
-        "-mthumb",
-        "--specs=nano.specs",
-        "--specs=nosys.specs",
-        "-u_printf_float",
-      ],
-    )
+            CXXFLAGS=[
+                "-fabi-version=0",
+                "-fno-use-cxa-atexit",
+                "-fno-threadsafe-statics"
+            ],
+            LINKFLAGS=[
+                "-Os",
+                "-mcpu=cortex-m3",
+                "-ffreestanding",
+                "-mthumb",
+                "--specs=nano.specs",
+                "--specs=nosys.specs",
+                "-u_printf_float",
+            ],
+        )
