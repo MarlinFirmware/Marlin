611ceedc1dcd5e4128a237350cd4b338dbcb63b9
Mihai <299015+mh-dm@users.noreply.github.com>
2024-06-14 07:05:05 +0300

    🐛  Fix step oversampling with NONLINEAR_EXTRUSION (#27171)
    Followup to #27113

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -2694,9 +2694,7 @@ hal_timer_t Stepper::block_phase_isr() {
       set_axis_moved_for_current_block();
 
       #if ENABLED(ADAPTIVE_STEP_SMOOTHING)
-        // Nonlinear Extrusion needs at least 2x oversampling to permit increase of E step rate
-        // Otherwise assume no axis smoothing (via oversampling)
-        oversampling_factor = TERN0(NONLINEAR_EXTRUSION, 1);
+        oversampling_factor = 0;
 
         // Decide if axis smoothing is possible
         if (stepper.adaptive_step_smoothing_enabled) {
