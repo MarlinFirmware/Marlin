1396b9658c4951777e9bdf28e6e79c49bf02805c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-06-21 19:59:12 -0500

    SPI only

--- Marlin/src/lcd/marlinui.h
+++ Marlin/src/lcd/marlinui.h
@@ -211,7 +211,11 @@ public:
     static void init_lcd() {}
   #endif
 
-  static void reinit_lcd() { TERN_(REINIT_NOISY_LCD, init_lcd()); }
+  static void reinit_lcd() {
+    #if REINIT_NOISY_SPI_LCD && (U8G_PARAM != (U8G_I2C_OPT_NONE | U8G_I2C_OPT_FAST))
+      init_lcd();
+    #endif
+  }
 
   #if HAS_WIRED_LCD
     static bool detected();
