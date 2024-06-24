1f62d63920060b3a8e94ffcff655f9b063e7b936
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-06-24 15:57:52 -0500

    more setter usage

--- Marlin/src/lcd/extui/mks_ui/wifi_module.cpp
+++ Marlin/src/lcd/extui/mks_ui/wifi_module.cpp
@@ -1011,14 +1011,8 @@ static void wifi_gcode_exec(uint8_t * const cmd_line) {
                 if (card.isFileOpen()) {
                   //saved_feedrate_percentage = feedrate_percentage;
                   feedrate_percentage = 100;
-                  #if HAS_EXTRUDERS
-                    planner.flow_percentage[0] = 100;
-                    planner.e_factor[0] = planner.flow_percentage[0] * 0.01f;
-                  #endif
-                  #if HAS_MULTI_EXTRUDER
-                    planner.flow_percentage[1] = 100;
-                    planner.e_factor[1] = planner.flow_percentage[1] * 0.01f;
-                  #endif
+                  TERN_(HAS_EXTRUDERS, planner.set_flow(0, 100));
+                  TERN_(HAS_MULTI_EXTRUDER, planner.set_flow(1, 100));
                   card.startOrResumeFilePrinting();
                   TERN_(POWER_LOSS_RECOVERY, recovery.prepare());
                   once_flag = false;
