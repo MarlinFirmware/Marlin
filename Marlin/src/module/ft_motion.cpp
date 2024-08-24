9265e72e1cd8d51c4f5da46b9ec134d678e54e72
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-08-24 13:49:53 -0500

    prettier

--- Marlin/src/module/ft_motion.cpp
+++ Marlin/src/module/ft_motion.cpp
@@ -92,11 +92,12 @@ uint32_t FTMotion::interpIdx = 0;               // Index of current data point b
 // Shaping variables.
 #if HAS_FTM_SHAPING
   FTMotion::shaping_t FTMotion::shaping = {
-    0,
-      x:{ false, { 0.0f }, { 0.0f }, { 0 }, 0 } // ena, d_zi[], Ai[], Ni[], max_i
+    0
+    #if HAS_X_AXIS
+      , x:{ false, { 0.0f }, { 0.0f }, { 0 }, 0 } // ena, d_zi[], Ai[], Ni[], max_i
+    #endif
     #if HAS_Y_AXIS
-      ,
-      y:{ false, { 0.0f }, { 0.0f }, { 0 }, 0 } // ena, d_zi[], Ai[], Ni[], max_i
+      , y:{ false, { 0.0f }, { 0.0f }, { 0 }, 0 } // ena, d_zi[], Ai[], Ni[], max_i
     #endif
   };
 #endif
