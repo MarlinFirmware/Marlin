03eb2e6c8042631cc8a276d8a48217ac177e81c6
classicrocker883 <andrewleduc88@yahoo.com>
2024-09-17 04:37:56 -0400

    update endstop

--- Marlin/src/lcd/e3v2/proui/endstop_diag.cpp
+++ Marlin/src/lcd/e3v2/proui/endstop_diag.cpp
@@ -61,7 +61,7 @@ void draw_es_label(FSTR_P const flabel=nullptr) {
 void draw_es_state(const bool is_hit, const bool is_fil) {
   const uint8_t LM = 130;
   DWINUI::cursor.x = LM;
-  dwinDrawRectangle(1, hmiData.colorPopupBg, LM, DWINUI::cursor.y, LM + 100, DWINUI::cursor.y + 20);
+  dwinDrawRectangle(1, hmiData.colorPopupBg, LM, DWINUI::cursor.y, LM + 120, DWINUI::cursor.y + 20);
   is_fil ? (is_hit ? DWINUI::drawString(RGB(16, 63, 16), F(STR_FILAMENT_PRESENT)) : DWINUI::drawString(RGB(31, 31, 16), F(STR_RUNOUT_DETECTED))) :
   (is_hit ? DWINUI::drawString(RGB(31, 31, 16), F(STR_ENDSTOP_HIT)) : DWINUI::drawString(RGB(16, 63, 16), F(STR_ENDSTOP_OPEN)));
   DWINUI::moveBy(0, 25);
