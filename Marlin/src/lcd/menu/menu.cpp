d9705d23d6861f06e778b5361c01b460a3c70634
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-09-23 19:26:15 -0500

    🩹 Fix missing _lcd_toggle_bed_leveling

--- Marlin/src/lcd/menu/menu.cpp
+++ Marlin/src/lcd/menu/menu.cpp
@@ -338,7 +338,7 @@ void _lcd_draw_homing() {
   }
 }
 
-#if ENABLED(LCD_BED_LEVELING) || (HAS_LEVELING && DISABLED(SLIM_LCD_MENUS))
+#if HAS_LEVELING
   void _lcd_toggle_bed_leveling() { set_bed_leveling_enabled(!planner.leveling_active); }
 #endif
 
