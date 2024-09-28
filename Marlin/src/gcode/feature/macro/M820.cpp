819ef88c38b0e302704d0ffe7ac4b05b32e0690e
fmuntean <fmuntean@gmail.com>
2024-09-28 10:30:29 -0400

    add gcode M820 to display the macros over serial

---
+++ Marlin/src/gcode/feature/macro/M820.cpp
@@ -0,0 +1,54 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "../../../inc/MarlinConfig.h"
+
+#if ENABLED(GCODE_MACROS)
+
+#include "../../gcode.h"
+#include "../../queue.h"
+#include "../../parser.h"
+
+//char gcode_macros[GCODE_MACROS_SLOTS][GCODE_MACROS_SLOT_SIZE + 1] = {{ 0 }};
+
+/**
+ * M810_819: Set/execute a G-code macro.
+ *
+ * Usage:
+ *   M810 <command>|...   Set Macro 0 to the given commands, separated by the pipe character
+ *   M810                 Execute Macro 0
+ *   M820 displays defined macros
+ */
+
+extern char gcode_macros[GCODE_MACROS_SLOTS][GCODE_MACROS_SLOT_SIZE + 1];
+
+void GcodeSuite::M820() {
+  for(byte i=0;i<GCODE_MACROS_SLOTS;i++){
+    char * const cmd = gcode_macros[i];
+    SERIAL_ECHOPGM("M81"); SERIAL_PRINT(i,DEC); SERIAL_ECHOPGM(" : ");
+    SERIAL_ECHOLN(cmd);
+
+  }
+}
+
+#endif // GCODE_MACROS
+
