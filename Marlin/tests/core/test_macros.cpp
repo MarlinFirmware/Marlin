dc61964f40ae5bd6eef26ae88dde7af5d71ed964
classicrocker883 <andrewleduc88@yahoo.com>
2024-10-15 10:09:05 -0400

    include array

--- Marlin/tests/core/test_macros.cpp
+++ Marlin/tests/core/test_macros.cpp
@@ -23,6 +23,7 @@
 #include "../test/unit_tests.h"
 #include <src/core/macros.h>
 #include <vector>
+#include <array>
 
 // These represent enabled and disabled configuration options for testing.
 // They will be used by multiple tests.
