8effaf49845fc08f6f21af25d4364c66b2abda30
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-10-20 12:55:24 -0500

    fixups

--- Marlin/src/gcode/feature/macro/M820.cpp
+++ Marlin/src/gcode/feature/macro/M820.cpp
@@ -28,27 +28,21 @@
 #include "../../queue.h"
 #include "../../parser.h"
 
-//char gcode_macros[GCODE_MACROS_SLOTS][GCODE_MACROS_SLOT_SIZE + 1] = {{ 0 }};
-
-/**
- * M810_819: Set/execute a G-code macro.
- *
- * Usage:
- *   M810 <command>|...   Set Macro 0 to the given commands, separated by the pipe character
- *   M810                 Execute Macro 0
- *   M820 displays defined macros
- */
-
 extern char gcode_macros[GCODE_MACROS_SLOTS][GCODE_MACROS_SLOT_SIZE + 1];
 
+/**
+ * M820: List defined M810 - M819 macros
+ */
 void GcodeSuite::M820() {
-  for(byte i=0;i<GCODE_MACROS_SLOTS;i++){
+  SERIAL_ECHOLNPGM(STR_M810_M819_MACROS);
+  for (uint8_t i = 0; i < GCODE_MACROS_SLOTS; ++i) {
     char * const cmd = gcode_macros[i];
-    SERIAL_ECHOPGM("M81"); SERIAL_PRINT(i,PrintBase::Dec); SERIAL_ECHOPGM(" : ");
-    SERIAL_ECHOLN(cmd);
-
+    if (*cmd) {
+      SERIAL_ECHO(F("M81"), i, C(' '));
+      while ((c = *cmd++)) SERIAL_CHAR(c == '\n' ? '|' : c);
+      SERIAL_EOL();
+    }
   }
 }
 
 #endif // GCODE_MACROS
-
