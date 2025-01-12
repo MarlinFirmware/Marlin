709cd93456c5357370f46dad2cc4287571b92245
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-01-11 23:37:10 -0600

    refine config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4433,15 +4433,16 @@
 
 /**
  * Freeze / unfreeze functionality
- * Potentially useful for rapid stop that allows being resumed. Halts stepper movement after decelerating to FREEZE_JERK.
- * Note this controls laser PWM but does NOT pause spindles, fans, heaters or any other auxiliary devices.
- * @section interface
+ * Potentially useful for rapid stop that allows being resumed.
+ * Rapidly decelerates and halts movement at FREEZE_JERK.
+ * NOTE: Controls Laser PWM but does NOT pause Spindle, Fans, Heaters or other devices.
+ * @section freeze
  */
 //#define FREEZE_FEATURE
 #if ENABLED(FREEZE_FEATURE)
-  #define FREEZE_PIN          5     // Override the default (KILL) pin here
-  #define FREEZE_JERK         2     // Completely halt when motion has decelerated below this value 
-  #define FREEZE_STATE        LOW   // State of pin indicating freeze
+  //#define FREEZE_PIN   -1   // Override the default (KILL) pin here
+  #define FREEZE_JERK     2   // (mm/s) Completely halt when motion has decelerated below this value
+  #define FREEZE_STATE  LOW   // State of pin indicating freeze
 #endif
 
 /**
