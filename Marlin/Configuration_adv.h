cf177d2fd721563445178ca91a6b398c73a216cd
David Buezas <david.buezas@gmail.com>
2025-02-26 22:30:57 +0100

    Remove accidentally default def of LIN_ADVANCE

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2335,7 +2335,7 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
-#define LIN_ADVANCE
+//#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   #if ENABLED(DISTINCT_E_FACTORS)
     #define ADVANCE_K { 0.22 }    // (mm) Compression length per 1mm/s extruder speed, per extruder
