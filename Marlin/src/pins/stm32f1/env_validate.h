27ab810c49acac86f53fc18331542d81c9e1a259
BMourit <bnmguy@gmail.com>
2025-03-15 23:28:40 -0700

    fix env_validate logic

--- Marlin/src/pins/stm32f1/env_validate.h
+++ Marlin/src/pins/stm32f1/env_validate.h
@@ -23,13 +23,13 @@
 #define ENV_VALIDATE_H
 
 #if NOT_TARGET(__STM32F1__, STM32F1)
-  #if DISABLED(ALLOW_STM32F4)
+  #if DISABLED(ALLOW_STM32F4) && DISABLED(ALLOW_GD32F3)
     #error "Oops! Select an STM32F1 board in 'Tools > Board.'"
-  #elif NOT_TARGET(STM32F4)
+  #elif ENABLED(ALLOW_STM32F4) && NOT_TARGET(STM32F4)
     #error "Oops! Select an STM32F4 board in 'Tools > Board.'"
+  #elif ENABLED(ALLOW_GD32F3) && NOT_TARGET(ARDUINO_ARCH_MFL)
+    #error "Oops! Make sure you have a GD32F303RE MFL environment selected."
   #endif
-#elif ENABLED(ALLOW_GD32F3) && NOT_TARGET(ARDUINO_ARCH_MFL)
-  #error "Oops! Make sure you have a GD32F303RE MFL environment selected."
 #endif
 
 #undef ALLOW_STM32F4
