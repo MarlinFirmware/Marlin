50d94c7ad1d1de92e7a86eee063b0200d8dc615b
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-03-17 15:17:12 -0500

    basic description

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1618,9 +1618,9 @@
 
   //#define SOUND_MENU_ITEM   // Add a mute option to the LCD menu
   #define SOUND_ON_DEFAULT    // Buzzer/speaker default enabled state
-  
+
   #if ENABLED(U8GLIB_SSD1309)
-    //#define USE_LCD_DOUBLE_BUFFERING    // Updates the display in a single batch and skips unchanged areas, enhancing performance. (costs 1kB of RAM)
+    //#define LCD_DOUBLE_BUFFER           // Optimize display updates. Costs ~1K of SRAM.
   #endif
 
   #if HAS_WIRED_LCD
