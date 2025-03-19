80715495120c8e21b3e98503c407db32472820fe
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-03-19 16:29:29 -0500

    move

--- Marlin/src/inc/Conditionals-5-post.h
+++ Marlin/src/inc/Conditionals-5-post.h
@@ -3360,10 +3360,6 @@
   #define HAS_FIXED_3POINT 1  // Points are defined for ABL/UBL. Else calculated in probe.get_three_points.
 #endif
 
-#if ALL(DWIN_LCD_PROUI, HAS_MESH)
-  #define USE_GRID_MESHVIEWER 1
-#endif
-
 /**
  * Buzzer/Speaker
  */
