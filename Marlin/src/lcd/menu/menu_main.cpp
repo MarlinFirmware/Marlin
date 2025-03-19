130dab6d1b67aed2b7f3d4eaa140def6ad374714
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-03-18 21:53:28 -0500

    post merge tweaks

--- Marlin/src/lcd/menu/menu_main.cpp
+++ Marlin/src/lcd/menu/menu_main.cpp
@@ -401,19 +401,6 @@ void menu_main() {
       GCODES_ITEM(MSG_SWITCH_PS_ON, F("M80"));
   #endif
 
-  /**
-   * Card Detected:
-   *  - Autostart
-   *  - Change Media
-   *  - Attach USB Drive
-   *  - Release Media
-   *  - Select From SD
-   * Card Not Detected:
-   *  - No Media
-   *  - Attach SD Card
-   *  - Attach USB Drive
-   *  - Attach Media
-   */
   #if HAS_MEDIA && DISABLED(MEDIA_MENU_AT_TOP)
     if (card_detected) {
       if (!card_open) {
@@ -422,11 +409,9 @@ void menu_main() {
         #endif
 
         #if HAS_SD_DETECT
+          GCODES_ITEM(MSG_CHANGE_MEDIA, F("M21" TERN_(HAS_MULTI_VOLUME, "S"))); // M21 Change Media
           #if HAS_MULTI_VOLUME
-            GCODES_ITEM(MSG_ATTACH_SD_MEDIA, F("M21S"));  // M21S Attach SD Card
-            GCODES_ITEM(MSG_ATTACH_USB_MEDIA, F("M21U")); // M21U Attach USB Drive
-          #else
-            GCODES_ITEM(MSG_CHANGE_MEDIA, F("M21" TERN_(HAS_MULTI_VOLUME, "S"))); // M21 Change Media
+            GCODES_ITEM(MSG_ATTACH_USB_MEDIA, F("M21U")); // M21 Attach USB Media
           #endif
         #else                                             // - or -
           ACTION_ITEM(MSG_RELEASE_MEDIA, []{              // M22 Release Media
@@ -447,7 +432,7 @@ void menu_main() {
       #else
         #if HAS_MULTI_VOLUME
           GCODES_ITEM(MSG_ATTACH_SD_MEDIA, F("M21S"));    // M21S Attach SD Card
-          GCODES_ITEM(MSG_ATTACH_USB_MEDIA, F("M21U"));   // M21U Attach USB Drive
+          GCODES_ITEM(MSG_ATTACH_USB_MEDIA, F("M21U"));   // M21U Attach USB Media
         #else
           GCODES_ITEM(MSG_ATTACH_MEDIA, F("M21"));        // M21 Attach Media
         #endif
@@ -485,7 +470,26 @@ void menu_main() {
     #endif
   #endif
 
-  #if HAS_GAMES && DISABLED(LCD_INFO_MENU)
+  #if HAS_MULTI_LANGUAGE
+    SUBMENU(LANGUAGE, menu_language);
+  #endif
+
+  #if ENABLED(HOST_SHUTDOWN_MENU_ITEM) && defined(SHUTDOWN_ACTION)
+    SUBMENU(MSG_HOST_SHUTDOWN, []{
+      MenuItem_confirm::select_screen(
+        GET_TEXT_F(MSG_BUTTON_PROCEED), GET_TEXT_F(MSG_BUTTON_CANCEL),
+        []{ ui.return_to_status(); hostui.shutdown(); }, nullptr,
+        GET_TEXT_F(MSG_HOST_SHUTDOWN), (const char *)nullptr, F("?")
+      );
+    });
+  #endif
+
+  #if ENABLED(LCD_INFO_MENU)
+
+    SUBMENU(MSG_INFO_MENU, menu_info);
+
+  #elif HAS_GAMES && DISABLED(LCD_INFO_MENU)
+
     #if ENABLED(GAMES_EASTER_EGG)
       SKIP_ITEM();
       SKIP_ITEM();
@@ -507,24 +511,7 @@ void menu_main() {
         #endif
       );
     }
-  #endif
 
-  #if HAS_MULTI_LANGUAGE
-    SUBMENU(LANGUAGE, menu_language);
-  #endif
-
-  #if ENABLED(LCD_INFO_MENU)
-    SUBMENU(MSG_INFO_MENU, menu_info);
-  #endif
-
-  #if ENABLED(HOST_SHUTDOWN_MENU_ITEM) && defined(SHUTDOWN_ACTION)
-    SUBMENU(MSG_HOST_SHUTDOWN, []{
-      MenuItem_confirm::select_screen(
-        GET_TEXT_F(MSG_BUTTON_PROCEED), GET_TEXT_F(MSG_BUTTON_CANCEL),
-        []{ ui.return_to_status(); hostui.shutdown(); }, nullptr,
-        GET_TEXT_F(MSG_HOST_SHUTDOWN), (const char *)nullptr, F("?")
-      );
-    });
   #endif
 
   END_MENU();
