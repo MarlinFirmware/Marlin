300e4fe54fb11a685ce7e0551a447f970d5a7238
ellensp <530024+ellensp@users.noreply.github.com>
2025-03-20 09:45:30 +1300

    🐛 Fix RAMPS AUX1 RX/TX pins (#27745)

--- Marlin/src/pins/ramps/pins_RAMPS.h
+++ Marlin/src/pins/ramps/pins_RAMPS.h
@@ -452,15 +452,15 @@
 #endif
 
 //
-// AUX1    5V  GND D2  D1
+// AUX1    5V  GND D1  D0
 //          2   4   6   8
 //          1   3   5   7
 //         5V  GND A3  A4
 //
 #define AUX1_05_PIN                           57  // (A3)
-#define AUX1_06_PIN                            2
+#define AUX1_06_PIN                            1  // TX0
 #define AUX1_07_PIN                           58  // (A4)
-#define AUX1_08_PIN                            1
+#define AUX1_08_PIN                            0  // RX0
 
 //
 // AUX2    GND A9 D40 D42 A11
