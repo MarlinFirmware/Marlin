88cd3b816b650f8d28cd89fb08acaa7aea987943
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-03-20 21:11:17 -0500

    precomp

--- Marlin/src/gcode/calibrate/M48.cpp
+++ Marlin/src/gcode/calibrate/M48.cpp
@@ -261,10 +261,12 @@ void GcodeSuite::M48() {
 
     #if HAS_STATUS_MESSAGE
       // Display M48 results in the status bar
-      if (MAX_MESSAGE_SIZE > 40)
-        ui.set_status_and_level(TS(GET_TEXT_F(MSG_M48_DEVIATION), F(": "), w_float_t(sigma, 2, 6), F(", "), GET_TEXT(MSG_M48_MAX_DELTA), F(": "), w_float_t(_MAX(mean - min, max - mean), 2, 3)));
-      else
-        ui.set_status_and_level(TS(GET_TEXT_F(MSG_M48_DEVIATION), F(": "), w_float_t(sigma, 2, 6)));
+      ui.set_status_and_level(TS(
+        GET_TEXT_F(MSG_M48_DEVIATION), F(": "), w_float_t(sigma, 2, 6)
+        #if MAX_MESSAGE_SIZE > 40
+          , F(", "), GET_TEXT(MSG_M48_MAX_DELTA), F(": "), w_float_t(_MAX(mean - min, max - mean), 2, 3)
+        #endif
+      ));
     #endif
   }
 
