f3f3a2f87837e266fa1efd7875310dcff5be2fa3
apanicek <adrian@panicek.sk>
2025-03-23 13:36:43 +0100

    Increase Z homing rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2284,7 +2284,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (10*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
