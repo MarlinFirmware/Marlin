766162f298b34d5de37e16d14411e5808caefb2f
erril007 <141030793+erril007@users.noreply.github.com>
2025-03-30 19:40:44 +0200

    EVENT_GCODE_AFTER_G34 inside loop

--- Marlin/src/gcode/calibrate/G34_M422.cpp
+++ Marlin/src/gcode/calibrate/G34_M422.cpp
@@ -445,6 +445,12 @@ void GcodeSuite::G34() {
         sync_plan_position();
       #endif
 
+      #ifdef EVENT_GCODE_AFTER_G34
+        if (DEBUGGING(LEVELING)) DEBUG_ECHOLNPGM("After G34 G-code: ", EVENT_GCODE_AFTER_G34);
+        planner.synchronize();
+        process_subcommands_now(F(EVENT_GCODE_AFTER_G34));
+      #endif
+
       probe.use_probing_tool(false);
 
       #if ALL(HAS_LEVELING, RESTORE_LEVELING_AFTER_G34)
@@ -452,12 +458,6 @@ void GcodeSuite::G34() {
       #endif
 
     }while(0);
-
-    #ifdef EVENT_GCODE_AFTER_G34
-      if (DEBUGGING(LEVELING)) DEBUG_ECHOLNPGM("After G34 G-code: ", EVENT_GCODE_AFTER_G34);
-      planner.synchronize();
-      process_subcommands_now(F(EVENT_GCODE_AFTER_G34));
-    #endif
 
     probe.use_probing_tool(false);
 
