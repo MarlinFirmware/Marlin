0108ca7e47bc674827a1ebbce953da35c6ca3d1e
BMourit <bnmguy@gmail.com>
2025-04-14 13:48:59 -0700

    Add Creality V4.2.7 Board support

--- Marlin/src/pins/gd32f3/pins_CREALITY_V427_GD32_MFL.h
+++ Marlin/src/pins/gd32f3/pins_CREALITY_V427_GD32_MFL.h
@@ -22,12 +22,27 @@
 #pragma once
 
 /**
- * Creality MFL GD32 V4.2.2 (GD32F303RE) board pin assignments
+ * Creality MFL GD32 V4.2.7 (GD32F303RE) board pin assignments
  */
 
 #define ALLOW_GD32F3
 
-#define BOARD_INFO_NAME      "Creality V4.2.2 MFL"
-#define DEFAULT_MACHINE_NAME "Ender-3 MFL"
+#define BOARD_INFO_NAME      "Creality V4.2.7 MFL"
+#define DEFAULT_MACHINE_NAME "Creality3D MFL"
+
+//
+// Steppers
+//
+#define X_STEP_PIN                          PB9
+#define X_DIR_PIN                           PC2
+
+#define Y_STEP_PIN                          PB7
+#define Y_DIR_PIN                           PB8
+
+#define Z_STEP_PIN                          PB5
+#define Z_DIR_PIN                           PB6
+
+#define E0_STEP_PIN                         PB3
+#define E0_DIR_PIN                          PB4
 
 #include "../stm32f1/pins_CREALITY_V4.h"
