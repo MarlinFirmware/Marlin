07b720b7b4e44ca5c0b4b96c0c70ec53ac6e8cf5
apanicek <adrian@panicek.sk>
2025-05-11 11:14:37 +0200

    Increase Y step rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1240,7 +1240,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 160, 400, 500 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
