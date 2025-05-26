77bfa57c52d20c318f5fade813eab6a30ed170ca
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-05-26 15:32:03 -0500

    add test

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -1105,7 +1105,7 @@ void Temperature::factory_reset() {
     do_z_clearance(MPC_TUNING_END_Z, false);
 
     #ifdef EVENT_GCODE_AFTER_MPC_TUNE
-      gcode.process_subcommands_now(F(EVENT_GCODE_AFTER_MPC_TUNE), false);
+      gcode.process_subcommands_now(F(EVENT_GCODE_AFTER_MPC_TUNE));
     #endif
 
     TERN_(TEMP_TUNING_MAINTAIN_FAN, adaptive_fan_slowing = true);
