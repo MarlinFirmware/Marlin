7885069c444f282a3c0842dff00aae601b9c2163
staff1010 <staff9090@outlook.com>
2025-05-27 17:23:17 +0800

    lvgl: Update to v6.1.2 with performance and UI optimizations
    - Update LVGL library to version 6.1.2
    - Fix image rendering issues (https://github.com/lvgl/lvgl/pull/8166)
    - Implement hash table for faster image lookup and rendering
    - Optimize SPI flash operations by removing redundant initializations
    - Improve memory management with adjusted buffer sizes
    - Enhance drawing routines with configurable parameters
    - Add image cache invalidation for better UI consistency
    - Optimize flash memory usage for improved performance

--- Marlin/src/lcd/extui/mks_ui/draw_ui.cpp
+++ Marlin/src/lcd/extui/mks_ui/draw_ui.cpp
@@ -664,25 +664,31 @@ char *creat_title_text() {
   void draw_default_preview(int xpos_pixel, int ypos_pixel, uint8_t sel) {
     int index;
     int y_off = 0;
-    W25QXX.init(SPI_QUARTER_SPEED);
-    for (index = 0; index < 10; index++) { // 200*200
+    // W25QXX.init(SPI_QUARTER_SPEED);
+    #define DRAW_COL_COUNT 40                     // Number of rows displayed each time, determines the size of bmp_public_buf
+    #define DRAW_COUNT (200 / DRAW_COL_COUNT)     // Total number of times to be displayed
+    #define PIXEL_COUNT (DEFAULT_VIEW_MAX_SIZE / DRAW_COUNT)  // Number of pixels read per time (uint8_t)
+    for (index = 0; index < DRAW_COUNT; index++) { // 200*200
       #if HAS_BAK_VIEW_IN_FLASH
         if (sel == 1) {
-          flash_view_Read(bmp_public_buf, 8000); // 20k
+          flash_view_Read(bmp_public_buf, PIXEL_COUNT); // 16k
         }
         else {
-          default_view_Read(bmp_public_buf, DEFAULT_VIEW_MAX_SIZE / 10); // 8k
+          default_view_Read(bmp_public_buf, PIXEL_COUNT); // 16k
         }
       #else
-        default_view_Read(bmp_public_buf, DEFAULT_VIEW_MAX_SIZE / 10); // 8k
+        default_view_Read(bmp_public_buf, PIXEL_COUNT); // 8k
       #endif
 
-      SPI_TFT.setWindow(xpos_pixel, y_off * 20 + ypos_pixel, 200, 20); // 200*200
-      SPI_TFT.tftio.writeSequence((uint16_t*)(bmp_public_buf), DEFAULT_VIEW_MAX_SIZE / 20);
+      SPI_TFT.setWindow(xpos_pixel, y_off * DRAW_COL_COUNT + ypos_pixel, 200, DRAW_COL_COUNT); // 200 * DRAW_COL_COUNT
+      SPI_TFT.tftio.writeSequence((uint16_t*)(bmp_public_buf), (uint16_t)(PIXEL_COUNT / 2));
 
       y_off++;
     }
-    W25QXX.init(SPI_QUARTER_SPEED);
+    #undef DRAW_COL_COUNT
+    #undef DRAW_COUNT
+    #undef PIXEL_COUNT
+    // W25QXX.init(SPI_QUARTER_SPEED);
   }
 
   void disp_pre_gcode(int xpos_pixel, int ypos_pixel) {
