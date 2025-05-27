7f63a6a8af913b9809032f749921c5c3d117621e
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-05-26 23:43:31 -0500

    use a bool

--- Marlin/src/gcode/config/M200-M205.cpp
+++ Marlin/src/gcode/config/M200-M205.cpp
@@ -124,7 +124,7 @@
  *  S<percent> : Speed factor percentage.
  */
 void GcodeSuite::M201() {
-  if (!parser.seen("T" STR_AXES_LOGICAL 
+  if (!parser.seen("T" STR_AXES_LOGICAL
     #ifdef XY_FREQUENCY_LIMIT
       "FS"
     #endif
@@ -152,7 +152,11 @@ void GcodeSuite::M201_report(const bool forReplay/*=true*/) {
   TERN_(MARLIN_SMALL_BUILD, return);
 
   report_heading_etc(forReplay, F(STR_MAX_ACCELERATION));
+
+  bool eol = false;
+
   #if NUM_AXES
+    eol = true;
     SERIAL_ECHOPGM_P(
       LIST_N(DOUBLE(NUM_AXES),
         PSTR("  M201 X"), LINEAR_UNIT(planner.settings.max_acceleration_mm_per_s2[X_AXIS]),
@@ -169,17 +173,17 @@ void GcodeSuite::M201_report(const bool forReplay/*=true*/) {
   #endif
 
   #if HAS_EXTRUDERS && DISABLED(DISTINCT_E_FACTORS)
+    eol = true;
     SERIAL_ECHOPGM_P(SP_E_STR, VOLUMETRIC_UNIT(planner.settings.max_acceleration_mm_per_s2[E_AXIS]));
   #endif
 
   #ifdef XY_FREQUENCY_LIMIT
+    eol = true;
     SERIAL_ECHOPGM_P(PSTR(" F"), planner.xy_freq_limit_hz);
     SERIAL_ECHOPGM_P(PSTR(" S"), (planner.xy_freq_min_speed_factor * 100));
   #endif
 
-  #if NUM_AXES || (HAS_EXTRUDERS && DISABLED(DISTINCT_E_FACTORS))
-    SERIAL_EOL();
-  #endif
+  if (eol) SERIAL_EOL();
 
   #if ENABLED(DISTINCT_E_FACTORS)
     for (uint8_t i = 0; i < E_STEPPERS; ++i) {
@@ -215,7 +219,11 @@ void GcodeSuite::M203_report(const bool forReplay/*=true*/) {
   TERN_(MARLIN_SMALL_BUILD, return);
 
   report_heading_etc(forReplay, F(STR_MAX_FEEDRATES));
+
+  bool eol = false;
+
   #if NUM_AXES
+    eol = true;
     SERIAL_ECHOPGM_P(
       LIST_N(DOUBLE(NUM_AXES),
         PSTR("  M203 X"), LINEAR_UNIT(planner.settings.max_feedrate_mm_s[X_AXIS]),
@@ -232,12 +240,11 @@ void GcodeSuite::M203_report(const bool forReplay/*=true*/) {
   #endif
 
   #if HAS_EXTRUDERS && DISABLED(DISTINCT_E_FACTORS)
+    eol = true;
     SERIAL_ECHOPGM_P(SP_E_STR, VOLUMETRIC_UNIT(planner.settings.max_feedrate_mm_s[E_AXIS]));
   #endif
 
-  #if NUM_AXES || (HAS_EXTRUDERS && DISABLED(DISTINCT_E_FACTORS))
-    SERIAL_EOL();
-  #endif
+  if (eol) SERIAL_EOL();
 
   #if ENABLED(DISTINCT_E_FACTORS)
     for (uint8_t i = 0; i < E_STEPPERS; ++i) {
