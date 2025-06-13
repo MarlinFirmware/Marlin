5ca1c733ed209068a4454198eb0e422218f93ecf
Andrew <andrewleduc88@yahoo.com>
2025-06-13 02:57:07 -0400

    fix buffer overflow warning - Marlin/src/lcd/menu/../../module/../libs/duration_t.h:137:26: note: ‘sprintf’ output between 15 and 22 bytes into a destination of size 21 - remove unused `VALUE_ITEM_F`

--- Marlin/src/lcd/menu/menu_info.cpp
+++ Marlin/src/lcd/menu/menu_info.cpp
@@ -38,8 +38,7 @@
   #include "game/game.h"
 #endif
 
-#define VALUE_ITEM(MSG, VALUE, STYL)    do{ char msg[21]; strcpy_P(msg, PSTR(": ")); strcpy(msg + 2, VALUE); STATIC_ITEM(MSG, STYL, msg); }while(0)
-#define VALUE_ITEM_F(MSG, PVALUE, STYL) do{ char msg[21]; strcpy_P(msg, PSTR(": ")); strcpy_P(msg + 2, PSTR(PVALUE)); STATIC_ITEM(MSG, STYL, msg); }while(0)
+#define VALUE_ITEM(MSG, VALUE, STYL)    do{ char msg[22]; strcpy_P(msg, PSTR(": ")); strcpy(msg + 2, VALUE); STATIC_ITEM(MSG, STYL, msg); }while(0)
 
 #if ENABLED(PRINTCOUNTER)
 
@@ -53,7 +52,7 @@
 
     printStatistics stats = print_job_timer.getStats();
 
-    char buffer[21];
+    char buffer[22];
 
     START_SCREEN();                                                                       // 12345678901234567890
     VALUE_ITEM(MSG_INFO_PRINT_COUNT, i16tostr3left(stats.totalPrints), SS_FULL);          // Print Count: 999
