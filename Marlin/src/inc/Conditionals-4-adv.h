399ac60ad8a14823a98832a141bcb70641c4e4a9
ellensp <530024+ellensp@users.noreply.github.com>
2025-06-22 14:39:07 +1200

    move SPI_FLASH_BACKUP to Conditionals-5 again

--- Marlin/src/inc/Conditionals-4-adv.h
+++ Marlin/src/inc/Conditionals-4-adv.h
@@ -1591,8 +1591,3 @@
   #undef AUTO_REPORT_SD_STATUS
   #define AUTO_REPORT_SD_STATUS
 #endif
-
-// SPI Flash Backup
-#if ALL(SPI_FLASH, HAS_MEDIA, MARLIN_DEV_MODE)
-  #define SPI_FLASH_BACKUP 1
-#endif
