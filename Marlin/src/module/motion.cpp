90542bf1352b8c213d02d66bbfdbb19e9899a9cd
narno2202 <narno2202@gmail.com>
2025-07-06 15:18:45 +0200

    Fix Microrpobe when HOME_BUMP_MM is not zero

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -2249,7 +2249,7 @@ void prepare_line_to_destination() {
     }
 
     #if ENABLED(FT_MOTION) && (ENABLED(BIQU_MICROPROBE_V1) || ENABLED(BIQU_MICROPROBE_V2))
-      if (axis == Z_AXIS && TERN0(HOMING_Z_WITH_PROBE, true) && ftMotion.cfg.active && is_home_dir) {
+      if (axis == Z_AXIS && TERN0(HOMING_Z_WITH_PROBE, true) && ftMotion.cfg.active) {
         planner.synchronize();
         endstops.hit_on_purpose(); // Reset the Z Endstop state
         endstops.z_homing_probing_active = true; // Set the Z Endstop homing state to active
