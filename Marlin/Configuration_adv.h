bfe63c4dc4d4eaf022bbc1c3eebb14cb001ab058
vovodroid <vovodroid@users.noreply.github.com>
2025-07-08 13:38:44 +0300

    PLR: XY home height

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1808,6 +1808,10 @@
       //#define POWER_LOSS_RETRACT_LEN   10 // (mm) Length of filament to retract on fail
     #endif
 
+    // (ms) Absolute height for XY homing to prevent collision with printed objects.
+    // Zero means no raise above stored position
+    #define POWER_LOSS_XYHOME_HEIGHT     0
+
     // Enable if Z homing is needed for proper recovery. 99.9% of the time this should be disabled!
     //#define POWER_LOSS_RECOVER_ZHOME
     #if ENABLED(POWER_LOSS_RECOVER_ZHOME)
