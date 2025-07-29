becb11e8d95b660fdbe97dba6443da3d732f3a6b
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-07-28 22:54:41 -0500

    followup

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1203,7 +1203,7 @@
                                                   //   ZVD, MZV : FTM_RATIO
                                                   //   2HEI     : FTM_RATIO * 3 / 2
                                                   //   3HEI     : FTM_RATIO * 2
-#endif
+#endif // FT_MOTION
 
 /**
  * Input Shaping
