b0c6127e067ba1f0760090e08a43f8becdde6fb5
12oclocker <73079144+12oclocker@users.noreply.github.com>
2025-09-04 14:47:30 -0400

    added Z_HOMING_WITH_PROBE_AFTER_Z_ENDSTOP
    added Z_HOMING_WITH_PROBE_AFTER_Z_ENDSTOP

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2040,7 +2040,7 @@
   // Commands to execute on filament runout.
   // With multiple runout sensors use the %c placeholder for the current tool in commands (e.g., "M600 T%c")
   // NOTE: After 'M412 H1' the host handles filament runout and this script does not apply.
-  #define FILAMENT_RUNOUT_SCRIPT "" //example "M600" which requires ADVANCED_PAUSE_FEATURE feature
+  #define FILAMENT_RUNOUT_SCRIPT " " //example "M600" which requires ADVANCED_PAUSE_FEATURE feature
 
   // After a runout is detected, continue printing this length of filament
   // before executing the runout script. Useful for a sensor at the end of
