f9aa814484214f1ccb7e5d99b187915e81bfd631
ellensp <530024+ellensp@users.noreply.github.com>
2025-09-24 12:52:27 +1200

    add M401_report

--- Marlin/src/gcode/probe/M401_M402.cpp
+++ Marlin/src/gcode/probe/M401_M402.cpp
@@ -57,6 +57,19 @@ void GcodeSuite::M401() {
   report_current_position();
 }
 
+void GcodeSuite::M401_report(const bool forReplay/*=true*/) {
+  #if HAS_BLTOUCH_HS_MODE
+  if (!forReplay) {
+    report_heading_etc(forReplay, F("BLTouch HS mode"));
+    SERIAL_ECHOPGM_P(
+      PSTR("  M401 S"), bltouch.high_speed_mode,
+      PSTR("; "), ON_OFF(bltouch.high_speed_mode)
+    );
+    SERIAL_EOL();
+  }
+  #endif
+}
+
 /**
  * M402: Deactivate and stow the Z probe
  *  R<bool> Remain in place after stowing (and before deactivating) the probe
