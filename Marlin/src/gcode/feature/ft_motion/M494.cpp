7b3ec9bdd41a304f00aa3b28eab901b5fc11cd52
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-09-25 14:56:56 -0500

    refine reports

--- Marlin/src/gcode/feature/ft_motion/M494.cpp
+++ Marlin/src/gcode/feature/ft_motion/M494.cpp
@@ -29,16 +29,16 @@
 
 void say_smoothing() {
   #if HAS_X_AXIS
-    SERIAL_ECHOPGM("  X smoothing time: ", p_float_t(ftMotion.cfg.smoothingTime.x, 3), "s\n");
+    SERIAL_ECHOLN(F("  "), C('X'), F(" smoothing time: "), p_float_t(ftMotion.cfg.smoothingTime.x, 3), C('s'));
   #endif
   #if HAS_Y_AXIS
-    SERIAL_ECHOPGM("  Y smoothing time: ", p_float_t(ftMotion.cfg.smoothingTime.y, 3), "s\n");
+    SERIAL_ECHOLN(F("  "), C('Y'), F(" smoothing time: "), p_float_t(ftMotion.cfg.smoothingTime.y, 3), C('s'));
   #endif
   #if HAS_Z_AXIS
-    SERIAL_ECHOPGM("  Z smoothing time: ", p_float_t(ftMotion.cfg.smoothingTime.z, 3), "s\n");
+    SERIAL_ECHOLN(F("  "), C('Z'), F(" smoothing time: "), p_float_t(ftMotion.cfg.smoothingTime.z, 3), C('s'));
   #endif
   #if HAS_EXTRUDERS
-    SERIAL_ECHOPGM("  E smoothing time: ", p_float_t(ftMotion.cfg.smoothingTime.e, 3), "s\n");
+    SERIAL_ECHOLN(F("  "), C('E'), F(" smoothing time: "), p_float_t(ftMotion.cfg.smoothingTime.e, 3), C('s'));
   #endif
 }
 
@@ -47,19 +47,18 @@ void GcodeSuite::M494_report(const bool forReplay/*=true*/) {
 
   report_heading_etc(forReplay, F("FTM Smoothing"));
   const ft_config_t &c = ftMotion.cfg;
-  SERIAL_ECHOLNPGM(
-    "  M494", ""
+  SERIAL_ECHOLN(F("  M494"),
     #if HAS_X_AXIS
-      , " X", c.smoothingTime.x
+      , F(" X"), c.smoothingTime.x
     #endif
     #if HAS_Y_AXIS
-      , " Y", c.smoothingTime.y
+      , F(" Y"), c.smoothingTime.y
     #endif
     #if HAS_Z_AXIS
-      , " Z", c.smoothingTime.z
+      , F(" Z"), c.smoothingTime.z
     #endif
     #if HAS_EXTRUDERS
-      , " E", c.smoothingTime.e
+      , F(" E"), c.smoothingTime.e
     #endif
   );
 }
