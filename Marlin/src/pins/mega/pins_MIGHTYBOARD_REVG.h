5a6b6d2afb38b983185ae9a1265624f4f0dcadff
Axel Sepulveda <ansepulveda@uc.cl>
2025-10-02 03:40:28 -0300

    clean pins

--- Marlin/src/pins/mega/pins_MIGHTYBOARD_REVG.h
+++ Marlin/src/pins/mega/pins_MIGHTYBOARD_REVG.h
@@ -47,11 +47,11 @@
 //
 // Limit Switches: X and Y go to max, Z to min 
 // 
-// #define X_MIN_PIN                             30  // C7 **
-#define X_MAX_PIN                             30  // C7 **
-// #define Y_MIN_PIN                             31  // C6 **
-#define Y_MAX_PIN                             31  // C6 **
-#define Z_MIN_PIN                             32  // C5 **
+// #define X_MIN_PIN                             30  // C7
+#define X_MAX_PIN                             30  // C7
+// #define Y_MIN_PIN                             31  // C6
+#define Y_MAX_PIN                             31  // C6
+#define Z_MIN_PIN                             32  // C5
 // #define Z_MAX_PIN                             42  // L7
 
 //
@@ -74,25 +74,25 @@
 //
 // Steppers
 //
-#define X_STEP_PIN                            83  // D6 *** does not have an equivalent in standard arduino core. uses mega extended
-#define X_DIR_PIN                             38  // D7 **
-#define X_ENABLE_PIN                          81  // D4 *** does not have an equivalent in standard arduino core. uses mega extended
+#define X_STEP_PIN                            83  // D6
+#define X_DIR_PIN                             38  // D7
+#define X_ENABLE_PIN                          81  // D4
 
-#define Y_STEP_PIN                            44  // L5 **
-#define Y_DIR_PIN                             42  // L7 **
-#define Y_ENABLE_PIN                          45  // L4 ** 
+#define Y_STEP_PIN                            44  // L5
+#define Y_DIR_PIN                             42  // L7
+#define Y_ENABLE_PIN                          45  // L4 
 
-#define Z_STEP_PIN                            48  // L1 **
-#define Z_DIR_PIN                             47  // L2 **
-#define Z_ENABLE_PIN                          49  // L0 **
+#define Z_STEP_PIN                            48  // L1
+#define Z_DIR_PIN                             47  // L2
+#define Z_ENABLE_PIN                          49  // L0
 
-#define E0_STEP_PIN                           25  // A3 **
-#define E0_DIR_PIN                            24  // A2 **
-#define E0_ENABLE_PIN                         27  // A5 **
+#define E0_STEP_PIN                           25  // A3
+#define E0_DIR_PIN                            24  // A2
+#define E0_ENABLE_PIN                         27  // A5
 
-#define E1_STEP_PIN                           22  // A0 **
-#define E1_DIR_PIN                            15  // K7 **
-#define E1_ENABLE_PIN                         23  // A1 **
+#define E1_STEP_PIN                           22  // A0
+#define E1_DIR_PIN                            15  // K7
+#define E1_ENABLE_PIN                         23  // A1
 
 //
 // I2C Digipots - MCP4018
@@ -100,12 +100,12 @@
 // Set from 0 - 127 with stop bit.
 // (Ex. 3F << 1 | 1)
 //
-#define DIGIPOTS_I2C_SCL                      28  // A6 *
-#define DIGIPOTS_I2C_SDA_X                    82  // D5 *** does not have an equivalent in standard arduino core. 
-#define DIGIPOTS_I2C_SDA_Y                    43  // L6 **
-#define DIGIPOTS_I2C_SDA_Z                    46  // L3 **
-#define DIGIPOTS_I2C_SDA_E0                   26  // A4 **
-#define DIGIPOTS_I2C_SDA_E1                   77  // J7 **
+#define DIGIPOTS_I2C_SCL                      28  // A6
+#define DIGIPOTS_I2C_SDA_X                    82  // D5
+#define DIGIPOTS_I2C_SDA_Y                    43  // L6
+#define DIGIPOTS_I2C_SDA_Z                    46  // L3
+#define DIGIPOTS_I2C_SDA_E0                   26  // A4
+#define DIGIPOTS_I2C_SDA_E1                   77  // J7
 
 #ifndef DIGIPOT_I2C_ADDRESS_A
   #define DIGIPOT_I2C_ADDRESS_A             0x2F  // unshifted slave address (5E <- 2F << 1)
@@ -114,7 +114,7 @@
 
 
 // Bed temp sensor pin 
-#define TEMP_BED_PIN                           3  // F3 ** It seems to be connected to arduino ADC, shouldn't it be connected to ads1118 too?
+#define TEMP_BED_PIN                           3  // F3
 
 /* 
 *  Temperature Sensors
@@ -122,12 +122,12 @@
 *  SPI for ADS1118 ADC, Uses software SPI
 *
 */ 
-#define TEMP_0_CS_PIN                         79  // E6 *** does not have an equivalent in standard arduino core. Maybe need to use https://github.com/MCUdude/MegaCore ? 
-#define TEMP_0_SCK_PIN                        78  // E2 *** does not have an equivalent in standard arduino core. Maybe need to use https://github.com/MCUdude/MegaCore ? 
-#define TEMP_0_MISO_PIN                       80  // E7 *** does not have an equivalent in standard arduino core. Maybe need to use https://github.com/MCUdude/MegaCore ? 
-#define TEMP_0_MOSI_PIN                       84  // H2 *** does not have an equivalent in standard arduino core. Maybe need to use https://github.com/MCUdude/MegaCore ? 
+#define TEMP_0_CS_PIN                         79  // E6
+#define TEMP_0_SCK_PIN                        78  // E2
+#define TEMP_0_MISO_PIN                       80  // E7
+#define TEMP_0_MOSI_PIN                       84  // H2
 
-#define TEMP_1_CS_PIN                          2  // E4 **
+#define TEMP_1_CS_PIN                          2  // E4
 #define TEMP_1_SCK_PIN            TEMP_0_SCK_PIN
 #define TEMP_1_MISO_PIN          TEMP_0_MISO_PIN
 #define TEMP_1_MOSI_PIN          TEMP_0_MOSI_PIN
@@ -136,20 +136,20 @@
 // FET Pin Mapping - FET A is closest to the input power connector
 //
 
-#define MOSFET_A_PIN                           3  // E5 ** 
-#define MOSFET_B_PIN                           5  // E3 ** 
-#define MOSFET_C_PIN                           8  // H5 ** 
-#define MOSFET_D_PIN                           7  // H4 ** 
-#define MOSFET_E_PIN                           2  // E4 ** 
-#define MOSFET_F_PIN                           4  // G5 ** 
+#define MOSFET_A_PIN                           3  // E5
+#define MOSFET_B_PIN                           5  // E3
+#define MOSFET_C_PIN                           8  // H5
+#define MOSFET_D_PIN                           7  // H4
+#define MOSFET_E_PIN                           2  // E4
+#define MOSFET_F_PIN                           4  // G5
 
 //
 // Heaters / Fans (24V)
 //
 
-#define HEATER_0_PIN                MOSFET_A_PIN  // E5 ** 
+#define HEATER_0_PIN                MOSFET_A_PIN  // E5
 #define HEATER_1_PIN                MOSFET_B_PIN  // E3 ?* 
-#define HEATER_BED_PIN              MOSFET_C_PIN  // H5 **
+#define HEATER_BED_PIN              MOSFET_C_PIN  // H5
 
 #ifndef E0_AUTO_FAN_PIN
   #define E0_AUTO_FAN_PIN           MOSFET_D_PIN
@@ -201,20 +201,20 @@
 
   #else
     // Replicator 2 and 2X uses a HD44780 SPI display, pins: mosi, sclk, miso (not used),  missing: latch, click, power
-    #define SPI_SOFT_MOSI_PIN                 37  // C0 **
-    #define SPI_SOFT_SCLK_PIN                 36  // C1 **
-    #define SPI_SOFT_MISO_PIN                 33  // C4 **
+    #define SPI_SOFT_MOSI_PIN                 37  // C0
+    #define SPI_SOFT_SCLK_PIN                 36  // C1
+    #define SPI_SOFT_MISO_PIN                 33  // C4
 
-    #define BTN_LATCH_PIN                     34  // C3 **
-    #define BTN_CLICK_PIN                     39  // G2 **
+    #define BTN_LATCH_PIN                     34  // C3
+    #define BTN_CLICK_PIN                     39  // G2
 
-    #define BTN_UP_PIN                        76  // J5 *** does not have an equivalent in standard arduino core. 
-    #define BTN_DOWN_PIN                      75  // J4 *** does not have an equivalent in standard arduino core. 
-    #define BTN_LEFT_PIN                      77  // J6/J13 *** does not have an equivalent in standard arduino core. Maybe need to use https://github.com/MCUdude/MegaCore ? 
+    #define BTN_UP_PIN                        76  // J5
+    #define BTN_DOWN_PIN                      75  // J4
+    #define BTN_LEFT_PIN                      77  // J6/J13
 
-    #define LCD_POWER_PIN                     29  // A7 **  
+    #define LCD_POWER_PIN                     29  // A7  
 
-    #define LCD_LED1_PIN                      35  // C2 **
+    #define LCD_LED1_PIN                      35  // C2
 
     // #define BTN_RIGHT_PIN                     14  // J1 ?    
         
@@ -223,16 +223,16 @@
     #define SR_CLK_PIN                        36  // C1 *
     #define SR_STROBE_PIN                     33  // C4 *
 
-    // #define BTN_UP                            75  // J5 *
-    // #define BTN_DOWN                          73  // J4 *
-    // #define BTN_LEFT                          72  // J6/J13 *
-    // #define BTN_RIGHT                         14  // J1 ?
+    // #define BTN_UP                            75  // J5
+    // #define BTN_DOWN                          73  // J4
+    // #define BTN_LEFT                          72  // J6/J13
+    // #define BTN_RIGHT                         14  // J1
     
     // Disable encoder
     #undef BTN_EN1
     #undef BTN_EN2
 
-    #define BEEPER_PIN                            6  // H3 **
+    #define BEEPER_PIN                            6  // H3
 
     // #define STAT_LED_RED_PIN                  32  // C2 *
     // #define STAT_LED_BLUE_PIN                 31  // C6 (Actually green)
