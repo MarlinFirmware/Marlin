434cf051d28e05de2b0ea536d02b8aa11a7419a1
narno2202 <130909513+narno2202@users.noreply.github.com>
2025-10-24 11:04:32 +0200

    Copy-Paste error

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -3568,24 +3568,23 @@ void Stepper::report_positions() {
     ftMotion.stepperCmdBuffHasData = (ftMotion.stepperCmdBuff_produceIdx != ftMotion.stepperCmdBuff_consumeIdx);
     if (!ftMotion.stepperCmdBuffHasData) return;
 
-    // "Pop" one command from current motion buffer
-    const ft_command_t command = ftMotion.stepperCmdBuff[ftMotion.stepperCmdBuff_consumeIdx];
-    if (++ftMotion.stepperCmdBuff_consumeIdx == (FTM_STEPPERCMD_BUFF_SIZE))
-      ftMotion.stepperCmdBuff_consumeIdx = 0;
+    // Get FT Motion command flags for axis STEP / DIR
+    #define _FTM_STEP(AXIS) TEST(command, FT_BIT_STEP_##AXIS)
+    #define _FTM_DIR(AXIS) TEST(command, FT_BIT_DIR_##AXIS)
 
-        #define _FTM_SET_MOVE(AXIS) axis_did_move.bset(_AXIS(AXIS), _FTM_STEP(AXIS));
+    #define _FTM_SET_MOVE(AXIS) axis_did_move.bset(_AXIS(AXIS), _FTM_STEP(AXIS));
 
     #define _FTM_SET_DIR(AXIS) if (_FTM_STEP(AXIS)) last_direction_bits.bset(_AXIS(AXIS), _FTM_DIR(AXIS));
-    
-    // Pop one command from the buffer
-    const ft_command_t command = ftMotion.stepperCmdBuff[ftMotion.stepperCmdBuff_consumeIdx];
+
+    // "Pop" one command from current motion buffer
+    ft_command_t command = ftMotion.stepperCmdBuff[ftMotion.stepperCmdBuff_consumeIdx];
     if (++ftMotion.stepperCmdBuff_consumeIdx == (FTM_STEPPERCMD_BUFF_SIZE))
       ftMotion.stepperCmdBuff_consumeIdx = 0;
     
     // Test command for a block start
     if((command == 0) && ftMotion.stepperCmdBuffItems()){
       // Start of a block, pick the next buffer command and just set MOVE and DIR bits
-      const ft_command_t command = ftMotion.stepperCmdBuff[ftMotion.stepperCmdBuff_consumeIdx];
+      command = ftMotion.stepperCmdBuff[ftMotion.stepperCmdBuff_consumeIdx];
       LOGICAL_AXIS_MAP(_FTM_SET_MOVE);
       LOGICAL_AXIS_MAP(_FTM_SET_DIR);
       return;
