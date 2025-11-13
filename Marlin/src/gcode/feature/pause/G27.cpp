06d63553dde80a1ecb9ff9a59886a1c077f85f41
Andrew <andrewleduc88@yahoo.com>
2025-11-13 04:53:13 -0500

    use byteval for G27

--- Marlin/src/gcode/feature/pause/G27.cpp
+++ Marlin/src/gcode/feature/pause/G27.cpp
@@ -44,7 +44,7 @@
 void GcodeSuite::G27() {
   // Don't allow nozzle parking without homing first
   if (homing_needed_error()) return;
-  nozzle.park(parser.ushortval('P'));
+  nozzle.park(parser.byteval('P'));
   TERN_(SOVOL_SV06_RTS, RTS_MoveAxisHoming());
 }
 
