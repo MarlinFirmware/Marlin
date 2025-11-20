f7c16889dc0579d13cc3d97ef345f58a7450d2bb
ellensp <530024+ellensp@users.noreply.github.com>
2025-11-20 16:00:11 +1300

    remove fancy double line characters

--- Marlin/src/pins/rp2040/pins_RASPBERRY_PI_PICO.h
+++ Marlin/src/pins/rp2040/pins_RASPBERRY_PI_PICO.h
@@ -100,72 +100,72 @@
 #endif // U8GLIB_SSD1306
 
 /*
-╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
-║                                  RASPBERRY PI PICO - PHYSICAL PIN MAPPING                                 ║
-╠══════════════╤═══════════╤═══════════╤═══════════╤═══════════╤════════════════════════════════════════════╣
-║ Physical Pin │   GPIO    │   ALT0    │   ALT1    │   ALT2    │                Marlin Usage                ║
-╠══════════════╪═══════════╪═══════════╪═══════════╪═══════════╪════════════════════════════════════════════╣
-║       1      │   GP0     │  I2C0_SDA │ SPI0_MISO │ UART0_TX  │ X_STOP_PIN                                 ║
-║       2      │   GP1     │  I2C0_SCL │ SPI0_CS   │ UART0_RX  │ Y_STOP_PIN                                 ║
-║       3      │   GND     │           │           │           │                                            ║
-║       4      │   GP2     │  I2C1_SDA │ SPI0_SCK  │           │ Z_STOP_PIN                                 ║
-║       5      │   GP3     │  I2C1_SCL │ SPI0_MOSI │           │ X_STEP_PIN                                 ║
-║       6      │   GP4     │  I2C0_SDA │ SPI0_MISO │ UART1_TX  │ SDA_PIN                                    ║
-║       7      │   GP5     │  I2C0_SCL │ SPI0_CS   │ UART1_RX  │ SCL_PIN                                    ║
-║       8      │   GND     │           │           │           │                                            ║
-║       9      │   GP6     │  I2C1_SDA │ SPI0_SCK  │           │ X_DIR_PIN                                  ║
-║      10      │   GP7     │  I2C1_SCL │ SPI0_MOSI │           │ X_ENABLE_PIN                               ║
-║      11      │   GP8     │  I2C0_SDA │ SPI1_MISO │ UART1_TX  │ Y_STEP_PIN                                 ║
-║      12      │   GP9     │  I2C0_SCL │ SPI1_CS   │ UART1_RX  │ Y_DIR_PIN                                  ║
-║      13      │   GND     │           │           │           │                                            ║
-║      14      │   GP10    │  I2C1_SDA │ SPI1_SCK  │           │ Z_STEP_PIN                                 ║
-║      15      │   GP11    │  I2C1_SCL │ SPI1_MOSI │           │ Z_DIR_PIN                                  ║
-║      16      │   GP12    │  I2C0_SDA │ SPI1_MISO │ UART0_TX  │ E0_STEP_PIN                                ║
-║      17      │   GP13    │  I2C0_SCL │ SPI1_CS   │ UART0_RX  │ E0_DIR_PIN                                 ║
-║      18      │   GND     │           │           │           │                                            ║
-║      19      │   GP14    │  I2C1_SDA │ SPI1_SCK  │           │ HEATER_0_PIN                               ║
-║      20      │   GP15    │  I2C1_SCL │ SPI1_MOSI │           │ HEATER_BED_PIN                             ║
-║      21      │   GP16    │  I2C0_SDA │ SPI0_MISO │ UART0_TX  │ SD_MISO_PIN                                ║
-║      22      │   GP17    │  I2C0_SCL │ SPI0_CS   │ UART0_RX  │ SD_SS_PIN                                  ║
-║      23      │   GND     │           │           │           │                                            ║
-║      24      │   GP18    │  I2C1_SDA │ SPI0_SCK  │           │ SD_SCK_PIN                                 ║
-║      25      │   GP19    │  I2C1_SCL │ SPI0_MOSI │           │ SD_MOSI_PIN                                ║
-║      26      │   GP20    │  I2C0_SDA │           │           │ BTN_EN1 (Encoder A)                        ║
-║      27      │   GP21    │  I2C0_SCL │           │           │ BTN_EN2 (Encoder B)                        ║
-║      28      │   GND     │           │           │           │                                            ║
-║      29      │   GP22    │           │           │           │ BTN_ENC (Encoder Button)                   ║
-║      30      │   RUN     │           │           │           │ Reset (Active Low)                         ║
-║      31      │   GP26    │   ADC0    │ I2C1_SDA  │           │ TEMP_0_PIN (A0)                            ║
-║      32      │   GP27    │   ADC1    │ I2C1_SCL  │           │ TEMP_BED_PIN (A1)                          ║
-║      33      │   GND     │           │           │           │                                            ║
-║      34      │   GP28    │   ADC2    │           │           │ FAN0_PIN                                   ║
-║      35      │   ADC_VREF│           │           │           │ ADC Reference Voltage                      ║
-║      36      │   3V3     │           │           │           │ 3.3V Power Output                          ║
-║      37      │   3V3_EN  │           │           │           │ 3.3V Enable (Active High)                  ║
-║      38      │   GND     │           │           │           │                                            ║
-║      39      │   VSYS    │           │           │           │ System Voltage Input (1.8-5.5V)            ║
-║      40      │   VBUS    │           │           │           │ USB Bus Voltage (5V when USB connected)    ║
-╠══════════════╧═══════════╧═══════════╧═══════════╧═══════════╧════════════════════════════════════════════╣
-║ Special pin notes for Raspberry Pi Pico                                                                   ║
-╠═══════════════════════════════════════════════════════════════════════════════════════════════════════════╣
-║ • GP23 OP Controls the on-board SMPS Power Save pin. Should not be used for other purposes.               ║
-║ • GP24 IP VBUS sense - high if VBUS is present, else low. Should not be used for other purposes.          ║
-║ • GP25 OP Connected to user LED. Can be used with board modifications, is not broken out.                 ║
-║ • GP29 IP Used in ADC mode (ADC3) to measure VSYS/3. Should not be used for other purposes.               ║
-║ • ADC4 (Internal) is connected to the chip's internal temperature sensor.                                 ║
-╠═══════════════════════════════════════════════════════════════════════════════════════════════════════════╣
-║ Marlin pin notes                                                                                          ║
-╠═══════════════════════════════════════════════════════════════════════════════════════════════════════════╣
-║ Shared enable pins:                                                                                       ║
-║ • GP7: X_ENABLE_PIN. All stepper drivers use a shared single enable pin X_ENABLE_PIN for pin conservation ║
-║                                                                                                           ║
-║ Hardware I2C and SPI configuration:                                                                       ║
-║   These are hardcoded in:                                                                                 ║
-║ .platformio/packages/framework-arduino-mbed/variants/RASPBERRY_RASPBERRY_PI_PICO/pins_arduino.h           ║
-║ • GP4: SDA           default: "#define PIN_WIRE_SDA        (4u)"                                          ║
-║ • GP5: SCL           default: "#define PIN_WIRE_SCL        (5u)"                                          ║
-║ • GP16: MISO         default: "#define PIN_SPI_MISO        (16u)"                                         ║
-║ • GP18: SCK          default: "#define PIN_SPI_SCK         (18u)"                                         ║
-║ • GP19: MOSI         default: "#define PIN_SPI_MOSI        (19u)"                                         ║
-╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+┌───────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+│                                  RASPBERRY PI PICO - PHYSICAL PIN MAPPING                                 │
+├──────────────┬───────────┬───────────┬───────────┬───────────┬────────────────────────────────────────────┤
+│ Physical Pin │   GPIO    │   ALT0    │   ALT1    │   ALT2    │                Marlin Usage                │
+├──────────────┼───────────┼───────────┼───────────┼───────────┼────────────────────────────────────────────┤
+│       1      │   GP0     │  I2C0_SDA │ SPI0_MISO │ UART0_TX  │ X_STOP_PIN                                 │
+│       2      │   GP1     │  I2C0_SCL │ SPI0_CS   │ UART0_RX  │ Y_STOP_PIN                                 │
+│       3      │   GND     │           │           │           │                                            │
+│       4      │   GP2     │  I2C1_SDA │ SPI0_SCK  │           │ Z_STOP_PIN                                 │
+│       5      │   GP3     │  I2C1_SCL │ SPI0_MOSI │           │ X_STEP_PIN                                 │
+│       6      │   GP4     │  I2C0_SDA │ SPI0_MISO │ UART1_TX  │ SDA_PIN                                    │
+│       7      │   GP5     │  I2C0_SCL │ SPI0_CS   │ UART1_RX  │ SCL_PIN                                    │
+│       8      │   GND     │           │           │           │                                            │
+│       9      │   GP6     │  I2C1_SDA │ SPI0_SCK  │           │ X_DIR_PIN                                  │
+│      10      │   GP7     │  I2C1_SCL │ SPI0_MOSI │           │ X_ENABLE_PIN                               │
+│      11      │   GP8     │  I2C0_SDA │ SPI1_MISO │ UART1_TX  │ Y_STEP_PIN                                 │
+│      12      │   GP9     │  I2C0_SCL │ SPI1_CS   │ UART1_RX  │ Y_DIR_PIN                                  │
+│      13      │   GND     │           │           │           │                                            │
+│      14      │   GP10    │  I2C1_SDA │ SPI1_SCK  │           │ Z_STEP_PIN                                 │
+│      15      │   GP11    │  I2C1_SCL │ SPI1_MOSI │           │ Z_DIR_PIN                                  │
+│      16      │   GP12    │  I2C0_SDA │ SPI1_MISO │ UART0_TX  │ E0_STEP_PIN                                │
+│      17      │   GP13    │  I2C0_SCL │ SPI1_CS   │ UART0_RX  │ E0_DIR_PIN                                 │
+│      18      │   GND     │           │           │           │                                            │
+│      19      │   GP14    │  I2C1_SDA │ SPI1_SCK  │           │ HEATER_0_PIN                               │
+│      20      │   GP15    │  I2C1_SCL │ SPI1_MOSI │           │ HEATER_BED_PIN                             │
+│      21      │   GP16    │  I2C0_SDA │ SPI0_MISO │ UART0_TX  │ SD_MISO_PIN                                │
+│      22      │   GP17    │  I2C0_SCL │ SPI0_CS   │ UART0_RX  │ SD_SS_PIN                                  │
+│      23      │   GND     │           │           │           │                                            │
+│      24      │   GP18    │  I2C1_SDA │ SPI0_SCK  │           │ SD_SCK_PIN                                 │
+│      25      │   GP19    │  I2C1_SCL │ SPI0_MOSI │           │ SD_MOSI_PIN                                │
+│      26      │   GP20    │  I2C0_SDA │           │           │ BTN_EN1 (Encoder A)                        │
+│      27      │   GP21    │  I2C0_SCL │           │           │ BTN_EN2 (Encoder B)                        │
+│      28      │   GND     │           │           │           │                                            │
+│      29      │   GP22    │           │           │           │ BTN_ENC (Encoder Button)                   │
+│      30      │   RUN     │           │           │           │ Reset (Active Low)                         │
+│      31      │   GP26    │   ADC0    │ I2C1_SDA  │           │ TEMP_0_PIN (A0)                            │
+│      32      │   GP27    │   ADC1    │ I2C1_SCL  │           │ TEMP_BED_PIN (A1)                          │
+│      33      │   GND     │           │           │           │                                            │
+│      34      │   GP28    │   ADC2    │           │           │ FAN0_PIN                                   │
+│      35      │   ADC_VREF│           │           │           │ ADC Reference Voltage                      │
+│      36      │   3V3     │           │           │           │ 3.3V Power Output                          │
+│      37      │   3V3_EN  │           │           │           │ 3.3V Enable (Active High)                  │
+│      38      │   GND     │           │           │           │                                            │
+│      39      │   VSYS    │           │           │           │ System Voltage Input (1.8-5.5V)            │
+│      40      │   VBUS    │           │           │           │ USB Bus Voltage (5V when USB connected)    │
+├──────────────┴───────────┴───────────┴───────────┴───────────┴────────────────────────────────────────────┤
+│ Special pin notes for Raspberry Pi Pico                                                                   │
+├───────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ • GP23 OP Controls the on-board SMPS Power Save pin. Should not be used for other purposes.               │
+│ • GP24 IP VBUS sense - high if VBUS is present, else low. Should not be used for other purposes.          │
+│ • GP25 OP Connected to user LED. Can be used with board modifications, is not broken out.                 │
+│ • GP29 IP Used in ADC mode (ADC3) to measure VSYS/3. Should not be used for other purposes.               │
+│ • ADC4 (Internal) is connected to the chip's internal temperature sensor.                                 │
+├───────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ Marlin pin notes                                                                                          │
+├───────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ Shared enable pins:                                                                                       │
+│ • GP7: X_ENABLE_PIN. All stepper drivers use a shared single enable pin X_ENABLE_PIN for pin conservation │
+│                                                                                                           │
+│ Hardware I2C and SPI configuration:                                                                       │
+│   These are hardcoded in:                                                                                 │
+│ .platformio/packages/framework-arduino-mbed/variants/RASPBERRY_RASPBERRY_PI_PICO/pins_arduino.h           │
+│ • GP4: SDA           default: "#define PIN_WIRE_SDA        (4u)"                                          │
+│ • GP5: SCL           default: "#define PIN_WIRE_SCL        (5u)"                                          │
+│ • GP16: MISO         default: "#define PIN_SPI_MISO        (16u)"                                         │
+│ • GP18: SCK          default: "#define PIN_SPI_SCK         (18u)"                                         │
+│ • GP19: MOSI         default: "#define PIN_SPI_MOSI        (19u)"                                         │
+└───────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 */
--- Marlin/src/pins/rp2040/pins_RP2040.h
+++ Marlin/src/pins/rp2040/pins_RP2040.h
@@ -559,26 +559,26 @@
 #endif // HAS_WIRED_LCD
 
 /*
-╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
-║                                     PIN CONFLICTS and ERRORS                                                  ║
-╠═══════════════════════════════════════════════════════════════════════════════════════════════════════════════╣
-║                                                                                                               ║
-║ VIRTUAL PINS - NOT REAL GPIO:                                                                                 ║
-║ • GP40+: E_MUX pins, TMC_SW pins are virtual/extended pins, not physical RP2040 GPIO                          ║
-║ • GP64, GP66: TMC Software SPI pins - these don't exist on RP2040 hardware                                    ║
-║                                                                                                               ║
-║ MULTIPLE ASSIGNMENTS - REVIEW NEEDED:                                                                         ║
-║ • GP4:  Z_DIR_PIN + Z_CS_PIN + SPINDLE_LASER_ENA_PIN (conditional conflicts)                                  ║
-║ • GP5:  Z_STEP_PIN + FILWIDTH_PIN + SPINDLE_DIR_PIN (conditional conflicts)                                   ║
-║ • GP6:  CASE_LIGHT_PIN + SPINDLE_LASER_PWM_PIN (conditional - both PWM)                                       ║
-║ • GP21: FAN1_PIN + FIL_RUNOUT_PIN (may conflict if both features enabled)                                     ║
-║ • GP29: ALL assigned to same pin:                                                                             ║
-║   - X_STEP_PIN, X_DIR_PIN, X_ENABLE_PIN, X_CS_PIN (4 pins)                                                    ║
-║   - Y_STEP_PIN, Y_DIR_PIN, Y_ENABLE_PIN, Y_CS_PIN (4 pins)                                                    ║
-║   - E0_STEP_PIN, E0_DIR_PIN, E0_ENABLE_PIN, E0_CS_PIN (4 pins)                                                ║
-║   - LCD_PINS_EN (CR10_STOCKDISPLAY), LCD_PINS_D7, DOGLCD_CS (ELB_FULL_GRAPHIC_CONTROLLER)                     ║
-║   → 12+ critical functions sharing ONE GPIO - PRINTER CANNOT FUNCTION!                                        ║
-╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+│                                     PIN CONFLICTS and ERRORS                                                  │
+├───────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│                                                                                                               │
+│ VIRTUAL PINS - NOT REAL GPIO:                                                                                 │
+│ • GP40+: E_MUX pins, TMC_SW pins are virtual/extended pins, not physical RP2040 GPIO                          │
+│ • GP64, GP66: TMC Software SPI pins - these don't exist on RP2040 hardware                                    │
+│                                                                                                               │
+│ MULTIPLE ASSIGNMENTS - REVIEW NEEDED:                                                                         │
+│ • GP4:  Z_DIR_PIN + Z_CS_PIN + SPINDLE_LASER_ENA_PIN (conditional conflicts)                                  │
+│ • GP5:  Z_STEP_PIN + FILWIDTH_PIN + SPINDLE_DIR_PIN (conditional conflicts)                                   │
+│ • GP6:  CASE_LIGHT_PIN + SPINDLE_LASER_PWM_PIN (conditional - both PWM)                                       │
+│ • GP21: FAN1_PIN + FIL_RUNOUT_PIN (may conflict if both features enabled)                                     │
+│ • GP29: ALL assigned to same pin:                                                                             │
+│   - X_STEP_PIN, X_DIR_PIN, X_ENABLE_PIN, X_CS_PIN (4 pins)                                                    │
+│   - Y_STEP_PIN, Y_DIR_PIN, Y_ENABLE_PIN, Y_CS_PIN (4 pins)                                                    │
+│   - E0_STEP_PIN, E0_DIR_PIN, E0_ENABLE_PIN, E0_CS_PIN (4 pins)                                                │
+│   - LCD_PINS_EN (CR10_STOCKDISPLAY), LCD_PINS_D7, DOGLCD_CS (ELB_FULL_GRAPHIC_CONTROLLER)                     │
+│   → 12+ critical functions sharing ONE GPIO - PRINTER CANNOT FUNCTION!                                        │
+└───────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 */
 
 #error "Pin assignments for this board are not for practical use. See comments in pins_RP2040.h"
