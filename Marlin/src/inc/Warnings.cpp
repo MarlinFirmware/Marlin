06c86a4d042c247bb81a3b194a27c87c838455af
David Buezas <david.buezas@gmail.com>
2025-11-24 13:31:04 +0100

    Warn about disabling FTM_SHAPER_E and FTM_SMOOTHING

--- Marlin/src/inc/Warnings.cpp
+++ Marlin/src/inc/Warnings.cpp
@@ -946,6 +946,12 @@
   #endif
   #if ENABLED(LIN_ADVANCE)
     #warning "Be aware that FT_MOTION K factor is now set with M900 K (same as LIN_ADVANCE)."
+    #if DISABLED(FTM_SMOOTHING)
+      #warning "Be aware that enabling FTM_SMOOTHING and setting FTM_SMOOTHING_TIME_E tames extruder acceleations due to linear advance and results in higher print quality."
+    #endif
+  #endif
+  #if DISABLED(FTM_SHAPER_E)
+    #warning "Be aware that enabling FTM_SHAPER_E (even if you let the shaper to NONE) allows synchronising all axes and results in higher print quality."
   #endif
 #endif
 #if ENABLED(FTM_HOME_AND_PROBE)
