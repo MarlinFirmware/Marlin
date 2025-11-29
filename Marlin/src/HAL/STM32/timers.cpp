ec0ffde0ccb40816644ca204a17907ce6fb82114
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-11-29 04:24:02 -0600

    🧑‍💻 Move HAL_TIMER_RATE

--- Marlin/src/HAL/STM32/timers.cpp
+++ Marlin/src/HAL/STM32/timers.cpp
@@ -137,6 +137,7 @@ void HAL_timer_start(const uint8_t timer_num, const uint32_t frequency) {
          */
 
         timer_instance[timer_num]->setPrescaleFactor(STEPPER_TIMER_PRESCALE); //the -1 is done internally
+        // (HAL_TIMER_RATE / STEPPER_TIMER_PRESCALE) == STEPPER_TIMER_RATE
         timer_instance[timer_num]->setOverflow(_MIN(HAL_TIMER_TYPE_MAX, hal_timer_t((HAL_TIMER_RATE) / (STEPPER_TIMER_PRESCALE) /* / frequency */)), TICK_FORMAT);
         break;
       case MF_TIMER_TEMP: // TEMP TIMER - any available 16bit timer
