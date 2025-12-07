e0be2f133f2b467bf2b0254486c9604495cc6298
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-12-06 18:31:06 -0600

    asserts

--- Marlin/src/module/ft_motion/stepping.h
+++ Marlin/src/module/ft_motion/stepping.h
@@ -32,11 +32,22 @@ FORCE_INLINE constexpr uint32_t a_times_b_shift_16(const uint32_t a, const uint3
 }
 #define FTM_NEVER uint32_t(UINT16_MAX)                               // Reserved number to indicate "no ticks in this frame" (FRAME_TICKS_FP+1 would work too)
 constexpr uint32_t FRAME_TICKS = STEPPER_TIMER_RATE / FTM_FS;        // Timer ticks per frame (by default, 1kHz)
-static_assert(FRAME_TICKS < FTM_NEVER, "(STEPPER_TIMER_RATE / FTM_FS) must be < " STRINGIFY(FTM_NEVER) " to fit 16-bit fixed-point numbers.");
-constexpr uint32_t FTM_Q_INT = 32u - __builtin_clz(FRAME_TICKS + 1); // Bits to represent the max value (duration of a frame, +1 one for FTM_NEVER).
+constexpr uint32_t TICKS_BITS = __builtin_clzl(FRAME_TICKS + 1UL);   // Bits to represent the max value (duration of a frame, +1 one for FTM_NEVER).
+constexpr uint32_t FTM_Q_INT = 32u - TICKS_BITS;                     // Bits remaining
                                                                      // "clz" counts leading zeroes.
 constexpr uint32_t FTM_Q = 16 - FTM_Q_INT;                           // uint16 interval fractional bits.
                                                                      // Intervals buffer has fixed point numbers with the point on this position
+
+static_assert(FRAME_TICKS < FTM_NEVER, "(STEPPER_TIMER_RATE / FTM_FS) must be < " STRINGIFY(FTM_NEVER) " to fit 16-bit fixed-point numbers.");
+
+constexpr bool assert_ftm_bits() {
+  if (FRAME_TICKS == 2000) {
+    static_assert(FTM_Q_INT == 11, "FTM_Q_INT should be 11");
+    static_assert(FTM_Q == 5, "FTM_Q should be 5");
+  }
+  return false;
+}
+constexpr bool result = assert_ftm_bits();
 
 // The _FP and _fp suffixes mean the number is in fixed point format with the point at the FTM_Q position.
 // See: https://en.wikipedia.org/wiki/Fixed-point_arithmetic
