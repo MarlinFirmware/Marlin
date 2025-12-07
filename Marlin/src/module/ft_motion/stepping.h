a72502bb6dcf69ebd9bd2aa07df456bce223ff15
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-12-07 03:50:09 -0600

    what if…

--- Marlin/src/module/ft_motion/stepping.h
+++ Marlin/src/module/ft_motion/stepping.h
@@ -34,15 +34,15 @@ FORCE_INLINE constexpr uint32_t a_times_b_shift_16(const uint32_t a, const uint3
 constexpr uint32_t FRAME_TICKS = STEPPER_TIMER_RATE / FTM_FS;   // Timer ticks per frame (by default, 1kHz)
 
 constexpr uint8_t TICK_0S = __builtin_clzl(FRAME_TICKS + 1UL);  // "clzl" counts leading zeros in uint32_t before (FRAME_TICKS+1) bits.
-constexpr uint8_t FTM_Q_INT = 32 - TICK_0S;                     // Bits to represent the max value (duration of a frame, +1 one for FTM_NEVER).
-constexpr uint8_t FTM_Q = 16 - FTM_Q_INT;                       // uint16 interval fractional bits.
+constexpr uint8_t FTM_Q = 32 - TICK_0S;                     // Bits to represent the max value (duration of a frame, +1 one for FTM_NEVER).
+//constexpr uint8_t FTM_Q = 16 - FTM_Q_INT;                       // uint16 interval fractional bits.
                                                                 // Intervals buffer has fixed point numbers with the point on this position
 
 static_assert(FRAME_TICKS < FTM_NEVER, "(STEPPER_TIMER_RATE / FTM_FS) (" STRINGIFY(STEPPER_TIMER_RATE) " / " STRINGIFY(FTM_FS) ") must be < " STRINGIFY(FTM_NEVER) " to fit 16-bit fixed-point numbers.");
 
 // Sanity check
-static_assert(FRAME_TICKS != 2000  || FTM_Q == 5, "FTM_Q should be 5");
-static_assert(FRAME_TICKS != 25000 || FTM_Q == 1, "FTM_Q should be 1");
+static_assert(FRAME_TICKS != 2000  || FTM_Q == 11, "FTM_Q should be 11");
+static_assert(FRAME_TICKS != 25000 || FTM_Q == 15, "FTM_Q should be 15");
 
 // The _FP and _fp suffixes mean the number is in fixed point format with the point at the FTM_Q position.
 // See: https://en.wikipedia.org/wiki/Fixed-point_arithmetic
