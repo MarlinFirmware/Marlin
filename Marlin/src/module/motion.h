6f77dbc602b63181548c8b44bff97b931df313ed
Andrew <andrewleduc88@yahoo.com>
2025-12-16 23:28:14 -0500

    revert pre-defining Z_AFTER_HOMING

--- Marlin/src/module/motion.h
+++ Marlin/src/module/motion.h
@@ -416,7 +416,11 @@ void restore_feedrate_and_scaling();
 
 #if HAS_Z_AXIS
   #ifndef Z_POST_CLEARANCE  // May be set by proui/dwin.h :-P
-    #define Z_POST_CLEARANCE Z_AFTER_HOMING
+    #ifdef Z_AFTER_HOMING
+      #define Z_POST_CLEARANCE Z_AFTER_HOMING
+    #else
+      #define Z_POST_CLEARANCE Z_CLEARANCE_FOR_HOMING
+    #endif
   #endif
   void do_z_clearance(const float zclear, const bool with_probe=true, const bool lower_allowed=false);
   void do_z_clearance_by(const float zclear);
