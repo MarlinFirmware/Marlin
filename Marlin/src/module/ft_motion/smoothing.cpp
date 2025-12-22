19e21b79b645edf0169420ffec776f78ad507c4d
David Buezas <dbuezas@users.noreply.github.com>
2025-12-22 08:26:24 +0100

    Change minimum smoothing time threshold
    Updated the minimum smoothing time threshold from 0.00001f to 0.0001f.

--- Marlin/src/module/ft_motion/smoothing.cpp
+++ Marlin/src/module/ft_motion/smoothing.cpp
@@ -28,13 +28,14 @@
 
 // Set smoothing time and recalculate alpha and delay.
 void AxisSmoothing::set_time(const float s_time) {
-  if (s_time >= 0.00001f) {
+  if (s_time >= 0.0001f) {
     alpha = 1.0f - expf(-(FTM_TS) * (FTM_SMOOTHING_ORDER) / s_time );
+    delay_samples = s_time * FTM_FS;
   }
   else {
     alpha = 1.0f;
+    delay_samples = 0;
   }
-  delay_samples = s_time * FTM_FS;
 }
 
 #endif // FTM_SMOOTHING
