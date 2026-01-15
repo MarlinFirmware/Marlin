bd424949a6229fee17bcc29c2d12fbb1b46dbb1d
narno2202 <130909513+narno2202@users.noreply.github.com>
2026-01-15 22:59:24 +0100

    Fix typo

--- Marlin/src/gcode/feature/resonance/M495_M496.cpp
+++ Marlin/src/gcode/feature/resonance/M495_M496.cpp
@@ -150,7 +150,7 @@ void GcodeSuite::M495() {
       if (p.axis != NO_AXIS_ENUM) {
         if (p.max_freq > p.min_freq) {
           SERIAL_ECHOLN(F("Starting "), F("Resonance Test"));
-          rtg.startt();
+          rtg.start();
           // The function returns immediately, the test runs in the background.
         }
         else {
