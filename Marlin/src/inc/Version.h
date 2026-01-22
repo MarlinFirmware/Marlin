fca481a6b2aa2dff5f6a14e893c47935973ca4a3
Scott Lahteine <thinkyhead@users.noreply.github.com>
2026-01-22 12:25:45 -0600

    🔖 Marlin 2.0.8.6

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.0.8.5"
+  #define SHORT_BUILD_VERSION "2.0.8.6"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2025-11-13"
+  #define STRING_DISTRIBUTION_DATE "2026-01-22"
 #endif
 
 /**
