40a93e506c985da45528aad85209d82e77765af2
Scott Lahteine <thinkyhead@users.noreply.github.com>
2026-01-22 12:02:39 -0600

    🔖 Marlin 2.1.0.4

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.1.0.3"
+//#define SHORT_BUILD_VERSION "2.1.0.4"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2025-11-13"
+//#define STRING_DISTRIBUTION_DATE "2026-01-22"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
