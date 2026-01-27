7a850179d8208a1839acc4a1befc438b3a00b5b6
Andrew <18502096+classicrocker883@users.noreply.github.com>
2026-01-27 03:50:02 -0500

    Change preheat bed temperature for PLA from 70 to 60

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2531,7 +2531,7 @@
 //
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
