29671b51372e369c0cfe79f9de12278edd645ca8
Scott Lahteine <thinkyhead@users.noreply.github.com>
2026-02-13 01:58:00 -0600

    undo

--- Marlin/src/module/motion.h
+++ Marlin/src/module/motion.h
@@ -347,7 +347,9 @@ public:
   #if HAS_SOFTWARE_ENDSTOPS
     static void apply_limits(xyz_pos_t &target);
     static void update_software_endstops(const AxisEnum axis
-      OPTARG(HAS_HOTEND_OFFSET, const uint8_t old_tool_index=0, const uint8_t new_tool_index=0)
+      #if HAS_HOTEND_OFFSET
+        , const uint8_t old_tool_index=0, const uint8_t new_tool_index=0
+      #endif
     );
     static void set_soft_endstop_loose(const bool loose) { soft_endstop._loose = loose; }
   #else
