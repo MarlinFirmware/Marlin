e38eb7f4b3e26199d48457016b8a2295c251d693
Andrew <18502096+classicrocker883@users.noreply.github.com>
2026-02-17 16:54:38 -0500

    add `TERN` to `smart_fill_wlsf`
    Used AVR_ATmega2560_FAMILY - maybe instead of AVR_ATmega2560?
    // O2 gives smaller code than Os on A2560

--- Marlin/src/feature/bedlevel/ubl/ubl.h
+++ Marlin/src/feature/bedlevel/ubl/ubl.h
@@ -106,8 +106,8 @@ public:
   static bool sanity_check();
   static void smart_fill_mesh();
 
-  static void G29() __O0;                           // O0 for no optimization
-  static void smart_fill_wlsf(const float ) __O2; // O2 gives smaller code than Os on A2560
+  static void G29() __O0; // O0 for no optimization
+  static void smart_fill_wlsf(const float ) TERN(AVR_ATmega2560_FAMILY, __O2, __Os); // O2 gives smaller code than Os on A2560
 
   static int8_t storage_slot;
 
