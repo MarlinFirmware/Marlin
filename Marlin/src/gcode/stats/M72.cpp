MarlinFirmware/Update-default-Junction-Deviation-numbers-to-match-Classic-Jerk-values

✨ Ender-5 S1 added code

Marlin/src/gcode/stats/M72.cpp
@@ -1,0 +1,52 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2024 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "../../inc/MarlinConfig.h"
+
+#if ALL(SDSUPPORT, CREALITY_RTS)
+
+#include "../gcode.h"
+#include "../../sd/cardreader.h"
+#include "../../lcd/marlinui.h"
+#include "../../lcd/rts/lcd_rts.h"
+
+#if HAS_FILAMENT_SENSOR
+  #include "../../feature/runout.h"
+#endif
+
+/**
+ * M72: Open a file
+ *
+ * The path is relative to the root directory
+ */
+void GcodeSuite::M72() {
+  // Simplify3D includes the size, so zero out all spaces (#7227)
+  for (char *fn = parser.string_arg; *fn; ++fn) if (*fn == ' ') *fn = '\0';
+
+  card.openFileRead(parser.string_arg);
+
+  RTS_OpenFileCloud();
+
+  TERN_(LCD_SET_PROGRESS_MANUALLY, ui.set_progress(0));
+}
+
+#endif // SDSUPPORT && CREALITY_RTS

MarlinFirmware/Marlin

Marlin/src/gcode/stats/M72.cpp
@@ -1,0 +1,52 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2024 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "../../inc/MarlinConfig.h"
+
+#if ALL(HAS_MEDIA, CREALITY_RTS)
+
+#include "../gcode.h"
+#include "../../sd/cardreader.h"
+#include "../../lcd/marlinui.h"
+#include "../../lcd/rts/lcd_rts.h"
+
+#if HAS_FILAMENT_SENSOR
+  #include "../../feature/runout.h"
+#endif
+
+/**
+ * M72: Open a file
+ *
+ * The path is relative to the root directory
+ */
+void GcodeSuite::M72() {
+  // Simplify3D includes the size, so zero out all spaces (#7227)
+  for (char *fn = parser.string_arg; *fn; ++fn) if (*fn == ' ') *fn = '\0';
+
+  card.openFileRead(parser.string_arg);
+
+  RTS_OpenFileCloud();
+
+  TERN_(LCD_SET_PROGRESS_MANUALLY, ui.set_progress(0));
+}
+
+#endif // HAS_MEDIA && CREALITY_RTS