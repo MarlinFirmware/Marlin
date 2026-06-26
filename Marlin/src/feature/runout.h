4ef873db2dec6f50e6abae4939754c63317a3e61
narno2202 <130909513+narno2202@users.noreply.github.com>
2026-06-26 08:46:20 +0200

    Remove FT_MOTION conditional for planner synchronization

--- Marlin/src/feature/runout.h
+++ Marlin/src/feature/runout.h
@@ -173,8 +173,6 @@ class TFilamentMonitor : public FilamentMonitorBase {
 
       filament_ran_out = true;
       event_filament_runout(extruder);
-      // FT_MOTION manages its own motion buffer separately; synchronize() causes deadlock
-      IF_DISABLED(FT_MOTION, planner.synchronize());
     }
 
     // Reset after a filament runout or upon resuming a job
