edf67d716d1f81db06a0f4aba4c8038394e30ef5
fanXbox <107966612+fanXbox@users.noreply.github.com>
2026-07-29 11:35:10 +0200

    Add files via upload

--- Marlin/src/lcd/language/language_it.h
+++ Marlin/src/lcd/language/language_it.h
@@ -46,10 +46,6 @@
   #define MEDIA_TYPE_IT "Media"
 #endif
 
-#ifndef MSG_MOTION_DISTANCE_MM
-  #define MSG_MOTION_DISTANCE_MM "Distanza mm"
-#endif
-
 namespace LanguageNarrow_it {
   using namespace Language_en; // Inherit undefined strings from English
 
