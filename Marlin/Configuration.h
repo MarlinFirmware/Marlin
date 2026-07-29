785a9661b9f18664a0f81b098d20ee8dfb14f9eb
fanXbox <107966612+fanXbox@users.noreply.github.com>
2026-07-29 14:15:41 +0200

    Add files via upload

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2367,7 +2367,7 @@
   //===========================================================================
 
   #define MESH_INSET 10          // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   //#define MESH_G28_REST_ORIGIN // After homing all axes ('G28' or 'G28 XYZ') rest Z at Z_MIN_POS
