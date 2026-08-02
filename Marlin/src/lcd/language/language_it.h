a8c9ea0244e07f81b1049fb77384e6a9edd6751e
fanXbox <107966612+fanXbox@users.noreply.github.com>
2026-08-02 09:49:31 +0200

    Fix Italian translation for bed leveling UI
    - Shorten the Italian translation for bed leveling messages to prevent text from overflowing the LCD screen.

--- Marlin/src/lcd/language/language_it.h
+++ Marlin/src/lcd/language/language_it.h
@@ -65,7 +65,7 @@ namespace LanguageNarrow_it {
   LSTR MSG_AUTO_HOME_Z                    = _UxGT("Azzera Z");                                 // Home Z
   LSTR MSG_LEVEL_BED_HOMING               = _UxGT("Home assi XYZ");                            // Homing XYZ
   LSTR MSG_LEVEL_BED_WAITING              = _UxGT("Premi per iniziare");                       // Click to Begin
-  LSTR MSG_LEVEL_BED_NEXT_POINT           = _UxGT("Punto successivo");                         // Next Point
+  LSTR MSG_LEVEL_BED_NEXT_POINT           = _UxGT("Rileva punto");                             // Next Point
   LSTR MSG_LEVEL_BED_DONE                 = _UxGT("Livel. terminato!");                        // Leveling Done!
   LSTR MSG_SET_HOME_OFFSETS               = _UxGT("Imp. offset home");                         // Set Home Offsets
   LSTR MSG_HOME_OFFSETS_APPLIED           = _UxGT("Offset applicato");                         // Offsets Applied
