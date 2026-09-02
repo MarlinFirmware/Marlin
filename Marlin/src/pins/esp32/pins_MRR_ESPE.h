e4b2e6aea3643c559ddfe1b6728073f1f53c68b2
ellensp <530024+ellensp@users.noreply.github.com>
2026-09-02 16:52:39 +1200

    🐛 Fix ESP32 SD Card SPI pins (#28556)

--- Marlin/src/pins/esp32/pins_MRR_ESPE.h
+++ Marlin/src/pins/esp32/pins_MRR_ESPE.h
@@ -113,7 +113,7 @@
 #define FAN1_PIN                             149
 
 //
-// MicroSD card
+// MicroSD card on VSPI (bus 3)
 //
 #define SD_MOSI_PIN                           23
 #define SD_MISO_PIN                           19
