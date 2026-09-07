18eb6a991b50869e90a87c75d9607ebc447734f0
Scott Lahteine <thinkyhead@users.noreply.github.com>
2026-09-07 15:26:06 -0500

    🎨 Outdent ControllerFan::update

--- Marlin/src/feature/controllerfan.cpp
+++ Marlin/src/feature/controllerfan.cpp
@@ -76,93 +76,93 @@ void ControllerFan::set_fan_speed(const uint8_t s) {
 }
 
 void ControllerFan::update() {
-  static millis_t lastComponentOn = 0,  // Last time a stepper, heater, etc. was turned on
-                  nextFanCheck = 0;     // Last time the state was checked
   const millis_t ms = millis();
-  if (ELAPSED(ms, nextFanCheck)) {
-    nextFanCheck = ms + 2500UL; // Not a time critical function, so only check every 2.5s
 
-    /**
-     * If any triggers for the controller fan are true...
-     *   - At least one stepper driver is enabled
-     *   - The heated bed (MOSFET) is enabled
-     *   - TEMP_SENSOR_BOARD is reporting >= CONTROLLER_FAN_MIN_BOARD_TEMP
-     *   - TEMP_SENSOR_SOC is reporting >= CONTROLLER_FAN_MIN_SOC_TEMP
-     */
-    const ena_mask_t axis_mask = TERN(CONTROLLER_FAN_USE_Z_ONLY, _BV(Z_AXIS), (ena_mask_t)~TERN0(CONTROLLER_FAN_IGNORE_Z, _BV(Z_AXIS)));
-    if ( (stepper.axis_enabled.bits & axis_mask)
-      #if ALL(HAS_HEATED_BED, CONTROLLER_FAN_BED_HEATING)
-        || thermalManager.temp_bed.soft_pwm_amount > 0
-      #endif
-      #ifdef CONTROLLER_FAN_MIN_BOARD_TEMP
-        || thermalManager.wholeDegBoard() >= CONTROLLER_FAN_MIN_BOARD_TEMP
-      #endif
-      #ifdef CONTROLLER_FAN_MIN_SOC_TEMP
-        || thermalManager.wholeDegSoc() >= CONTROLLER_FAN_MIN_SOC_TEMP
-      #endif
-    ) lastComponentOn = ms; //... set time to NOW so the fan will turn on
+  static millis_t nextFanCheck = 0;     // Last time the state was checked
+  if (PENDING(ms, nextFanCheck)) return;
+  nextFanCheck = ms + 2500UL; // Not a time critical function, so only check every 2.5s
 
-    /**
-     * Fan Settings. Set fan > 0:
-     *  - If AutoMode is on and hot components have been powered for CONTROLLERFAN_IDLE_TIME seconds.
-     *  - If System is on idle and idle fan speed settings is activated.
-     */
-    set_fan_speed(
-      settings.auto_mode && lastComponentOn && PENDING(ms, lastComponentOn, SEC_TO_MS(settings.duration))
-      ? settings.active_speed : settings.idle_speed
-    );
+  /**
+   * If any triggers for the controller fan are true...
+   *   - At least one stepper driver is enabled
+   *   - The heated bed (MOSFET) is enabled
+   *   - TEMP_SENSOR_BOARD is reporting >= CONTROLLER_FAN_MIN_BOARD_TEMP
+   *   - TEMP_SENSOR_SOC is reporting >= CONTROLLER_FAN_MIN_SOC_TEMP
+   */
+  static millis_t lastComponentOn = 0;  // Last time a stepper, heater, etc. was turned on
+  const ena_mask_t axis_mask = TERN(CONTROLLER_FAN_USE_Z_ONLY, _BV(Z_AXIS), (ena_mask_t)~TERN0(CONTROLLER_FAN_IGNORE_Z, _BV(Z_AXIS)));
+  if ( (stepper.axis_enabled.bits & axis_mask)
+    #if ALL(HAS_HEATED_BED, CONTROLLER_FAN_BED_HEATING)
+      || thermalManager.temp_bed.soft_pwm_amount > 0
+    #endif
+    #ifdef CONTROLLER_FAN_MIN_BOARD_TEMP
+      || thermalManager.wholeDegBoard() >= CONTROLLER_FAN_MIN_BOARD_TEMP
+    #endif
+    #ifdef CONTROLLER_FAN_MIN_SOC_TEMP
+      || thermalManager.wholeDegSoc() >= CONTROLLER_FAN_MIN_SOC_TEMP
+    #endif
+  ) lastComponentOn = ms; //... set time to NOW so the fan will turn on
 
-    speed = CALC_FAN_SPEED(speed);
+  /**
+   * Fan Settings. Set fan > 0:
+   *  - If AutoMode is on and hot components have been powered for CONTROLLERFAN_IDLE_TIME seconds.
+   *  - If System is on idle and idle fan speed settings is activated.
+   */
+  set_fan_speed(
+    settings.auto_mode && lastComponentOn && PENDING(ms, lastComponentOn, SEC_TO_MS(settings.duration))
+    ? settings.active_speed : settings.idle_speed
+  );
 
-    #if FAN_KICKSTART_TIME
-      static millis_t fan_kick_end = 0;
-      if (speed > FAN_OFF_PWM) {
-        if (!fan_kick_end) {
-          fan_kick_end = ms + FAN_KICKSTART_TIME; // May be longer based on slow update interval for controller fn check. Sets minimum
-          speed = FAN_KICKSTART_POWER;
-        }
-        else if (PENDING(ms, fan_kick_end))
-          speed = FAN_KICKSTART_POWER;
+  speed = CALC_FAN_SPEED(speed);
+
+  #if FAN_KICKSTART_TIME
+    static millis_t fan_kick_end = 0;
+    if (speed > FAN_OFF_PWM) {
+      if (!fan_kick_end) {
+        fan_kick_end = ms + FAN_KICKSTART_TIME; // May be longer based on slow update interval for controller fn check. Sets minimum
+        speed = FAN_KICKSTART_POWER;
       }
-      else
-        fan_kick_end = 0;
-    #endif
+      else if (PENDING(ms, fan_kick_end))
+        speed = FAN_KICKSTART_POWER;
+    }
+    else
+      fan_kick_end = 0;
+  #endif
 
-    #define SET_CONTROLLER_FAN(N) do { \
-      if (PWM_PIN(CONTROLLER_FAN##N##_PIN)) hal.set_pwm_duty(pin_t(CONTROLLER_FAN##N##_PIN), speed); \
-      else WRITE(CONTROLLER_FAN##N##_PIN, speed > 0);\
-    } while (0)
+  #define SET_CONTROLLER_FAN(N) do { \
+    if (PWM_PIN(CONTROLLER_FAN##N##_PIN)) hal.set_pwm_duty(pin_t(CONTROLLER_FAN##N##_PIN), speed); \
+    else WRITE(CONTROLLER_FAN##N##_PIN, speed > 0);\
+  } while (0)
 
-    #if ENABLED(FAN_SOFT_PWM)
-      soft_pwm_speed = speed >> 1;   // Controller Fan Soft PWM uses 0-127 as 0-100% so cut the 0-255 range in half.
-    #else
-      SET_CONTROLLER_FAN();
-      #if PIN_EXISTS(CONTROLLER_FAN2)
-        SET_CONTROLLER_FAN(2);
-      #endif
-      #if PIN_EXISTS(CONTROLLER_FAN3)
-        SET_CONTROLLER_FAN(3);
-      #endif
-      #if PIN_EXISTS(CONTROLLER_FAN4)
-        SET_CONTROLLER_FAN(4);
-      #endif
-      #if PIN_EXISTS(CONTROLLER_FAN5)
-        SET_CONTROLLER_FAN(5);
-      #endif
-      #if PIN_EXISTS(CONTROLLER_FAN6)
-        SET_CONTROLLER_FAN(6);
-      #endif
-      #if PIN_EXISTS(CONTROLLER_FAN7)
-        SET_CONTROLLER_FAN(7);
-      #endif
-      #if PIN_EXISTS(CONTROLLER_FAN8)
-        SET_CONTROLLER_FAN(8);
-      #endif
-      #if PIN_EXISTS(CONTROLLER_FAN9)
-        SET_CONTROLLER_FAN(9);
-      #endif
+  #if ENABLED(FAN_SOFT_PWM)
+    soft_pwm_speed = speed >> 1;   // Controller Fan Soft PWM uses 0-127 as 0-100% so cut the 0-255 range in half.
+  #else
+    SET_CONTROLLER_FAN();
+    #if PIN_EXISTS(CONTROLLER_FAN2)
+      SET_CONTROLLER_FAN(2);
     #endif
-  }
+    #if PIN_EXISTS(CONTROLLER_FAN3)
+      SET_CONTROLLER_FAN(3);
+    #endif
+    #if PIN_EXISTS(CONTROLLER_FAN4)
+      SET_CONTROLLER_FAN(4);
+    #endif
+    #if PIN_EXISTS(CONTROLLER_FAN5)
+      SET_CONTROLLER_FAN(5);
+    #endif
+    #if PIN_EXISTS(CONTROLLER_FAN6)
+      SET_CONTROLLER_FAN(6);
+    #endif
+    #if PIN_EXISTS(CONTROLLER_FAN7)
+      SET_CONTROLLER_FAN(7);
+    #endif
+    #if PIN_EXISTS(CONTROLLER_FAN8)
+      SET_CONTROLLER_FAN(8);
+    #endif
+    #if PIN_EXISTS(CONTROLLER_FAN9)
+      SET_CONTROLLER_FAN(9);
+    #endif
+  #endif
 }
 
 #endif // USE_CONTROLLER_FAN
