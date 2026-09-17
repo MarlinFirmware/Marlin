3935e65fa60eca44a88b05441a8845cdaf51acf5
Le-Syl21 <sylvain.gargasson@gmail.com>
2026-09-17 08:50:07 +0200

    🐛 M412: reset the countdowns after setting D and L
    The reset ran before the new lengths were applied, so the countdowns kept
    the previous ones until the next reset.

--- Marlin/src/gcode/feature/runout/M412.cpp
+++ Marlin/src/gcode/feature/runout/M412.cpp
@@ -49,14 +49,18 @@ void GcodeSuite::M412() {
       if (parser.seen('H')) runout.host_handling = parser.value_bool();
     #endif
     const bool seenR = parser.seen_test('R'), seenS = parser.seen('S');
-    if (seenR || seenS || TERN0(FILAMENT_SWITCH_AND_MOTION, parser.seen_test('L'))) runout.reset();
     if (seenS) runout.enabled = parser.value_bool();
     #if HAS_FILAMENT_RUNOUT_DISTANCE
       if (parser.seenval('D')) runout.set_runout_distance(parser.value_linear_units());
     #endif
     #if ENABLED(FILAMENT_SWITCH_AND_MOTION)
-      if (parser.seenval('L')) runout.set_motion_distance(parser.value_linear_units());
+      const bool seenL = parser.seenval('L');
+      if (seenL) runout.set_motion_distance(parser.value_linear_units());
+    #else
+      constexpr bool seenL = false;
     #endif
+    // Reset after the new distances are set, so the countdowns start from them
+    if (seenR || seenS || seenL) runout.reset();
   }
   else {
     SERIAL_ECHO_START();
