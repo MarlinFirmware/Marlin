cbeb2a895623c906d1aef788ac6db62d7be05e8f
qwq0 <82009862+qwq0@users.noreply.github.com>
2026-09-23 17:05:05 +0800

    fix 3 point leveling with PROBE_MANUALLY

--- Marlin/src/gcode/bedlevel/abl/G29.cpp
+++ Marlin/src/gcode/bedlevel/abl/G29.cpp
@@ -269,8 +269,13 @@ G29_TYPE GcodeSuite::G29() {
 
   // 3-point leveling gets points from the probe class
   #if ENABLED(AUTO_BED_LEVELING_3POINT)
-    vector_3 points[3];
-    probe.get_three_points(points);
+    #if ENABLED(PROBE_MANUALLY)
+      static vector_3 points[3];
+      if (!g29_in_progress) probe.get_three_points(points);
+    #elif
+      vector_3 points[3];
+      probe.get_three_points(points);
+    #endif
   #endif
 
   // Storage for ABL Linear results
