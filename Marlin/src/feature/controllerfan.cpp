4ee5976d2292bd528274f4aa4d6c22a49dcd2588
Scott Lahteine <thinkyhead@users.noreply.github.com>
2026-09-24 22:24:49 -0500

    Fix controller fan speed state
    Co-Authored-By: Reimnop <56994757+Reimnop@users.noreply.github.com>

--- Marlin/src/feature/controllerfan.cpp
+++ Marlin/src/feature/controllerfan.cpp
@@ -108,21 +108,23 @@ void ControllerFan::update() {
     ? settings.active_speed
     : settings.idle_speed;
 
+  set_fan_speed(s);
+
   // Convert 1-255 to the MIN-MAX PWM range
-  s = CALC_FAN_SPEED(s);
+  speed = CALC_FAN_SPEED(speed);
 
   // When the fan first starts up it can run at high power for a short period
   #if FAN_KICKSTART_TIME
 
     static millis_t kick_end_ms = 0;
 
-    if (s > FAN_OFF_PWM) {                      // Is the fan turned on?
+    if (speed > FAN_OFF_PWM) {                  // Is the fan turned on?
       if (!kick_end_ms) {                       // No kickstart yet?
         kick_end_ms = ms + FAN_KICKSTART_TIME;  // Set a future time at which to stop
-        s = FAN_KICKSTART_POWER;                // Override the power
+        speed = FAN_KICKSTART_POWER;            // Override the power
       }
       else if (PENDING(ms, kick_end_ms))        // Still waiting for end of kickstart time?
-        s = FAN_KICKSTART_POWER;                // Override the power
+        speed = FAN_KICKSTART_POWER;            // Override the power
     }
     else
       kick_end_ms = 0;                          // Reset kick_end_ms for kickstart on next enable
